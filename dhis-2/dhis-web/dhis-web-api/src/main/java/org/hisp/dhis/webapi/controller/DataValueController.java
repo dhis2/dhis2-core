be38c6da9dae4b917993429e4ada0a22b860eff2
Halvdan Hoem Grelland <halvdan@dhis2.org>
2016-09-02 16:35:37 +0200

    Fix bug with file upload fields and locked data sets in data entry (#25)
    * Remove locking validation from GET on api/dataSet
    
    * Properly handle 'locked' data sets in data entry for file uploads.

--- dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/DataValueController.java
+++ dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/DataValueController.java
@@ -386,12 +386,6 @@ public class DataValueController
         OrganisationUnit organisationUnit = getAndValidateOrganisationUnit( ou );
 
         // ---------------------------------------------------------------------
-        // Locking validation
-        // ---------------------------------------------------------------------
-
-        validateDataSetNotLocked( dataElement, period, organisationUnit, attributeOptionCombo );
-
-        // ---------------------------------------------------------------------
         // Get data value
         // ---------------------------------------------------------------------
 
@@ -442,12 +436,6 @@ public class DataValueController
         Period period = getAndValidatePeriod( pe );
 
         OrganisationUnit organisationUnit = getAndValidateOrganisationUnit( ou );
-
-        // ---------------------------------------------------------------------
-        // Locking validation
-        // ---------------------------------------------------------------------
-
-        validateDataSetNotLocked( dataElement, period, organisationUnit, attributeOptionCombo );
 
         // ---------------------------------------------------------------------
         // Get data value
