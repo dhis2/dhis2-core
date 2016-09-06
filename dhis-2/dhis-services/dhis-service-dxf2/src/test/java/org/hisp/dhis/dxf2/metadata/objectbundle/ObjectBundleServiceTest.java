e4efbb87b5343241d0375f7ad01471334cc56e2a
Morten Olav Hansen <mortenoh@gmail.com>
2016-09-06 10:31:43 +0700

    adds new validation check for duplicate IDs in payload, keep first object, then removes the rest (if duplicates are found)

--- dhis-2/dhis-services/dhis-service-dxf2/src/test/java/org/hisp/dhis/dxf2/metadata/objectbundle/ObjectBundleServiceTest.java
+++ dhis-2/dhis-services/dhis-service-dxf2/src/test/java/org/hisp/dhis/dxf2/metadata/objectbundle/ObjectBundleServiceTest.java
@@ -1309,20 +1309,40 @@ public class ObjectBundleServiceTest
         ObjectBundleParams params = new ObjectBundleParams();
         params.setObjectBundleMode( ObjectBundleMode.COMMIT );
         params.setImportStrategy( ImportStrategy.CREATE_AND_UPDATE );
+        params.setAtomicMode( AtomicMode.NONE );
         params.setObjects( metadata );
 
         ObjectBundle bundle = objectBundleService.create( params );
-        ObjectBundleValidationReport validate = objectBundleValidationService.validate( bundle );
-
-        System.err.println( "V: " + validate.getErrorReports() );
+        objectBundleValidationService.validate( bundle );
 
         objectBundleService.commit( bundle );
 
         assertEquals( 1, manager.getAll( DataElement.class ).size() );
 
-        // DataElement dataElement = manager.get( DataElement.class, "CCwk5Yx440o" );
-        // assertEquals( "SG4HuKlNEFH", dataElement.getUid() );
-        // assertEquals( "DataElementA", dataElement.getName() );
+        DataElement dataElement = manager.get( DataElement.class, "CCwk5Yx440o" );
+        assertEquals( "CCwk5Yx440o", dataElement.getUid() );
+        assertEquals( "DataElementB", dataElement.getName() );
+    }
+
+    @Test
+    public void testCreateMetadataWithDuplicateDataElementUidALL() throws IOException
+    {
+        createUserAndInjectSecurityContext( true );
+
+        Map<Class<? extends IdentifiableObject>, List<IdentifiableObject>> metadata = renderService.fromMetadata(
+            new ClassPathResource( "dxf2/de_duplicate_uid.json" ).getInputStream(), RenderFormat.JSON );
+
+        ObjectBundleParams params = new ObjectBundleParams();
+        params.setObjectBundleMode( ObjectBundleMode.COMMIT );
+        params.setImportStrategy( ImportStrategy.CREATE_AND_UPDATE );
+        params.setObjects( metadata );
+
+        ObjectBundle bundle = objectBundleService.create( params );
+        objectBundleValidationService.validate( bundle );
+
+        objectBundleService.commit( bundle );
+
+        assertEquals( 0, manager.getAll( DataElement.class ).size() );
     }
 
     @Test
