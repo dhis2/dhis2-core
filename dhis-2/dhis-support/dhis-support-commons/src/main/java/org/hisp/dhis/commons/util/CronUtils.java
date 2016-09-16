80b0ed02416735881b424180dcc944b21f7ff003
Stian Sandvold <stian@dhis2.org>
2016-09-16 15:46:22 +0200

    Added tests for CronUtils and fixed some bugs in CronUtils

--- dhis-2/dhis-support/dhis-support-commons/src/main/java/org/hisp/dhis/commons/util/CronUtils.java
+++ dhis-2/dhis-support/dhis-support-commons/src/main/java/org/hisp/dhis/commons/util/CronUtils.java
@@ -34,13 +34,13 @@ public class CronUtils
 {
     public enum Weekday
     {
+        SUNDAY( "SUN" ),
         MONDAY( "MON" ),
         TUESDAY( "TUE" ),
         WEDNESDAY( "WED" ),
         THURSDAY( "THU" ),
         FRIDAY( "FRI" ),
-        SATURDAY( "SAT" ),
-        SUNDAY( "SUN" );
+        SATURDAY( "SAT" );
 
         private final String name;
 
@@ -50,6 +50,13 @@ public class CronUtils
         }
     }
 
+    /**
+     * Generates a cron pattern that will execute every day at the given hour:minute
+     *
+     * @param minutes
+     * @param hours
+     * @return a cron pattern
+     */
     public static String getDailyCronExpression( int minutes, int hours )
     {
         return getCronExpression(
@@ -62,6 +69,14 @@ public class CronUtils
         );
     }
 
+    /**
+     * Generates a cron pattern that will execute every week at the dayOfWeek at hour:minute
+     *
+     * @param minutes
+     * @param hours
+     * @param dayOfWeek can be 0-7. 0 and 7 both resolve to sunday
+     * @return a cron pattern
+     */
     public static String getWeeklyCronExpression( int minutes, int hours, int dayOfWeek )
     {
         return getCronExpression(
@@ -70,10 +85,18 @@ public class CronUtils
             String.valueOf( hours ),
             null,
             null,
-            Weekday.values()[dayOfWeek].name
+            Weekday.values()[(dayOfWeek % 7)].name // both 0 and 7 are valid as Sunday in crontab patterns
         );
     }
 
+    /**
+     * Generates a cron pattern that will execute every month at the dayOfMonth at hour:minute
+     *
+     * @param minutes
+     * @param hours
+     * @param dayOfMonth
+     * @return a cron pattern
+     */
     public static String getMonthlyCronExpression( int minutes, int hours, int dayOfMonth )
     {
         return getCronExpression(
@@ -86,6 +109,17 @@ public class CronUtils
         );
     }
 
+    /**
+     * Joins together each segment of a cron pattern into a complete cron pattern.
+     *
+     * @param seconds a valid cron segment
+     * @param minutes a valid cron segment
+     * @param hours   a valid cron segment
+     * @param days    a valid cron segment
+     * @param months  a valid cron segment
+     * @param weekday a valid cron segment (MON-SUN)
+     * @return a cron pattern
+     */
     public static String getCronExpression( String seconds, String minutes, String hours, String days, String months,
         String weekday )
     {
