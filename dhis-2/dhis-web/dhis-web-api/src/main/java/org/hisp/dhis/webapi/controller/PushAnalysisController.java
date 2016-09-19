f9b16bb0524168e56b6e2e097ea9f965f64329be
Stian Sandvold <stian@dhis2.org>
2016-09-19 16:36:11 +0200

    Quickfix: Added better errorhandling for pushanalysis/render endpoint

--- dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/PushAnalysisController.java
+++ dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/PushAnalysisController.java
@@ -89,7 +89,7 @@ public class PushAnalysisController
         @PathVariable() String uid,
         HttpServletResponse response
     )
-        throws Exception
+        throws WebMessageException
     {
         PushAnalysis pushAnalysis = pushAnalysisService.getByUid( uid );
         if ( pushAnalysis == null )
@@ -103,10 +103,20 @@ public class PushAnalysisController
 
         logger.info(
             "User '" + currentUserService.getCurrentUser().getUsername() + "' started PushAnalysis for 'rendering'." );
-        String result = pushAnalysisService.generateHtmlReport( pushAnalysis, currentUserService.getCurrentUser(), null );
 
-        response.getWriter().write( result );
-        response.getWriter().close();
+        try
+        {
+            String result = pushAnalysisService.generateHtmlReport( pushAnalysis, currentUserService.getCurrentUser(), null );
+            response.getWriter().write( result );
+            response.getWriter().close();
+
+        }
+        catch ( Exception e )
+        {
+            logger.error( "Failed to render push analysis" );
+            e.printStackTrace();
+        }
+
     }
 
     /**
