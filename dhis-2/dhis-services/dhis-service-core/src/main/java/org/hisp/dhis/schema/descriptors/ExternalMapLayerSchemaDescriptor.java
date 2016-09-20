9c5631617a2ebdee6fe92d2af51f1395888616c2
vietnguyen <viet@dhis2.org>
2016-09-20 12:55:41 +0700

    add authorities for external map layer (#120)

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/schema/descriptors/ExternalMapLayerSchemaDescriptor.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/schema/descriptors/ExternalMapLayerSchemaDescriptor.java
@@ -30,9 +30,12 @@ package org.hisp.dhis.schema.descriptors;
  *
  */
 
+import com.google.common.collect.Lists;
 import org.hisp.dhis.mapping.ExternalMapLayer;
 import org.hisp.dhis.schema.Schema;
 import org.hisp.dhis.schema.SchemaDescriptor;
+import org.hisp.dhis.security.Authority;
+import org.hisp.dhis.security.AuthorityType;
 
 /**
  * @author Viet Nguyen <viet@dhis2.org>
@@ -53,6 +56,9 @@ public class ExternalMapLayerSchemaDescriptor implements SchemaDescriptor
         Schema schema = new Schema( ExternalMapLayer.class, SINGULAR, PLURAL );
         schema.setRelativeApiEndpoint( API_ENDPOINT );
         schema.setOrder( 2000 );
+
+        schema.getAuthorities().add( new Authority( AuthorityType.CREATE, Lists.newArrayList( "F_EXTERNALMAPLAYER_ADD" ) ) );
+        schema.getAuthorities().add( new Authority( AuthorityType.DELETE, Lists.newArrayList( "F_EXTERNALMAPLAYER_DELETE" ) ) );
 
         return schema;
     }
