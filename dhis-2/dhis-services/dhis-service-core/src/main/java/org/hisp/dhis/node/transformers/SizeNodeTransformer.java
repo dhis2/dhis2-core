dd7ee1818096bc691b814241a4a2d063ead989ec
Morten Olav Hansen <mortenoh@gmail.com>
2016-09-23 15:58:34 +0700

    removed NodeConverters, now all is using NodeTransformers (same syntax as before)

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/node/transformers/SizeNodeTransformer.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/node/transformers/SizeNodeTransformer.java
@@ -1,8 +1,8 @@
-package org.hisp.dhis.node;
+package org.hisp.dhis.node.transformers;
 
 /*
  * Copyright (c) 2004-2016, University of Oslo
- * All rights reserved.
+ *  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -28,29 +28,50 @@ package org.hisp.dhis.node;
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+import org.hisp.dhis.node.Node;
+import org.hisp.dhis.node.NodeTransformer;
+import org.hisp.dhis.node.types.SimpleNode;
 import org.hisp.dhis.schema.Property;
+import org.hisp.dhis.schema.PropertyType;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
  * @author Morten Olav Hansen <mortenoh@gmail.com>
  */
-public interface NodePropertyConverter
+@Component
+public class SizeNodeTransformer implements NodeTransformer
 {
-    /**
-     * @return Public/external name of this transformer.
-     */
-    String name();
+    @Override
+    public String name()
+    {
+        return "size";
+    }
 
-    /**
-     * @param property Property instance belonging to value
-     * @param value    Actual value to transform
-     * @return true of false depending on support
-     */
-    boolean canConvertTo( Property property, Object value );
+    @Override
+    public Node transform( Node node, List<String> args )
+    {
+        checkNotNull( node );
+        checkNotNull( node.getProperty() );
 
-    /**
-     * @param property Property instance belonging to value
-     * @param value    Actual value to transform
-     * @return Value transformed to a Node
-     */
-    Node convertTo( Property property, Object value );
+        Property property = node.getProperty();
+
+        if ( property.isCollection() )
+        {
+            return new SimpleNode( property.getCollectionName(), node.getChildren().size(), property.isAttribute() );
+        }
+        else if ( property.is( PropertyType.TEXT ) || property.is( PropertyType.TEXT ) )
+        {
+            return new SimpleNode( property.getName(), ((String) ((SimpleNode) node).getValue()).length(), property.isAttribute() );
+        }
+        else if ( property.is( PropertyType.INTEGER ) || property.is( PropertyType.NUMBER ) )
+        {
+            return new SimpleNode( property.getName(), ((SimpleNode) node).getValue(), property.isAttribute() );
+        }
+
+        return node;
+    }
 }
