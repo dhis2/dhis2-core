454b008fcb016302da4f0a4bb88cc5b2dca1774c
Halvdan Hoem Grelland <halvdanhg@gmail.com>
2016-10-21 18:18:28 +0200

    Added @Override to close()

--- dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/dataset/CompleteDataSetRegistration.java
+++ dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/dataset/CompleteDataSetRegistration.java
@@ -95,6 +95,7 @@ public class CompleteDataSetRegistration
             .toString();
     }
 
+    @Override
     public void close() {}
 
     //--------------------------------------------------------------------------
