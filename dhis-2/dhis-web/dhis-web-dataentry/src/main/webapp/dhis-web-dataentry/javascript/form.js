87b3e7f77e67411a0ba336d430220fbdd8928e0f
Lars Helge Øverland <lars@dhis2.org>
2016-11-04 17:51:44 +0100

    Data entry option set bugfix

--- dhis-2/dhis-web/dhis-web-dataentry/src/main/webapp/dhis-web-dataentry/javascript/form.js
+++ dhis-2/dhis-web/dhis-web-dataentry/src/main/webapp/dhis-web-dataentry/javascript/form.js
@@ -2971,10 +2971,9 @@ dhis2.de.setOptionNameInField = function( fieldId, value )
 		if ( obj && obj.optionSet && obj.optionSet.options ) {			
 			$.each( obj.optionSet.options, function( inx, option ) {
 				if ( option && option.code == value.val ) {
-          option.id = option.code;
-          option.text = option.name;
-          $( fieldId ).select2("val", option.text );
-					return false;
+                    option.id = option.code;
+                    option.text = option.name;
+                    $(fieldId).val(option.id).change();
 				}
 			} );
 		}		
