f99f33d16c2e09015f8559938ff0cb574326ebf1
Morten Olav Hansen <mortenoh@gmail.com>
2016-11-14 10:25:17 +0700

    minor logic fix in collection API (delete)

--- dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/AbstractCrudController.java
+++ dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/AbstractCrudController.java
@@ -1047,10 +1047,8 @@ public abstract class AbstractCrudController<T extends IdentifiableObject>
 
             items.forEach( item ->
             {
-                if ( !collection.contains( item ) ) collection.remove( item );
+                if ( collection.contains( item ) ) collection.remove( item );
             } );
-
-            manager.update( object );
         }
         else
         {
@@ -1063,7 +1061,7 @@ public abstract class AbstractCrudController<T extends IdentifiableObject>
                 {
                     Collection<IdentifiableObject> collection = (Collection<IdentifiableObject>) owningProperty.getGetterMethod().invoke( item );
 
-                    if ( !collection.contains( object ) )
+                    if ( collection.contains( object ) )
                     {
                         collection.remove( object );
                         manager.update( item );
@@ -1074,6 +1072,8 @@ public abstract class AbstractCrudController<T extends IdentifiableObject>
                 }
             } );
         }
+
+        manager.update( object );
 
         dbmsManager.clearSession();
         cacheManager.clearCache();
