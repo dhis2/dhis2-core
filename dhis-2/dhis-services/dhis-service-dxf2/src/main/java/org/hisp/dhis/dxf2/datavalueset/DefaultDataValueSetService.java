77518c82f778b2c34b3eff8d4d64ff44f2fffcae
Lars Helge Øverland <lars@dhis2.org>
2016-11-15 11:00:10 +0100

    ADX import, binding hibernate session to AdxPipedImporter
    (cherry picked from commit 48b6b8eab81e25392ce847fe4e27a8f1518009c6)

--- dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/datavalueset/DefaultDataValueSetService.java
+++ dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/datavalueset/DefaultDataValueSetService.java
@@ -801,7 +801,7 @@ public class DefaultDataValueSetService
                     categoryOptionCombo = fallbackCategoryOptionCombo;
                 }
             }
-
+            
             if ( attrOptionCombo == null )
             {
                 if ( requireAttrOptionCombo )
