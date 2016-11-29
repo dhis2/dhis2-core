7b0a7a47fe92ec9e49b4f45a4ff43b709fb12645
Jan Henrik Overland <janhenrik.overland@gmail.com>
2016-11-29 15:41:23 +0100

    legend display style backend
    (cherry picked from commit 7b0ea307199150d1ef3820bc8a5cbb5826ea89f7)

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/legend/LegendDisplayStyle.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/legend/LegendDisplayStyle.java
@@ -31,8 +31,8 @@ package org.hisp.dhis.legend;
 /**
  * @author Lars Helge Overland
  */
-public enum LegendSetDisplayType
+public enum LegendDisplayStyle
 {
-    TEXT, 
-    FILL;
+    FILL,
+    TEXT;
 }
--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/startup/TableAlteror.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/startup/TableAlteror.java
@@ -489,7 +489,7 @@ public class TableAlteror
         executeSql( "update reporttable set sortorder = 0 where sortorder is null" );
         executeSql( "update reporttable set toplimit = 0 where toplimit is null" );
         executeSql( "update reporttable set showhierarchy = false where showhierarchy is null" );
-        executeSql( "update reporttable set legendsetdisplaytype = 'TEXT' where legendsetdisplaytype is null" );
+        executeSql( "update reporttable set legenddisplaystyle = 'TEXT' where legenddisplaystyle is null" );
 
         // reporttable col/row totals = keep existing || copy from totals || true
         executeSql( "update reporttable set totals = true where totals is null" );
