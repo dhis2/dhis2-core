f1e389a30546a6fe8bbb6d757ae27c6467b0a0b2
Lars Helge Øverland <lars@dhis2.org>
2016-12-02 12:57:38 +0100

    Get query

--- dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/data/JdbcRawDataAnalyticsManager.java
+++ dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/data/JdbcRawDataAnalyticsManager.java
@@ -1,12 +1,39 @@
 package org.hisp.dhis.analytics.data;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.annotation.Resource;
+
 import org.hisp.dhis.analytics.DataQueryParams;
+import org.hisp.dhis.jdbc.StatementBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.google.common.collect.Lists;
 
 public class JdbcRawDataAnalyticsManager
 {
+    @Resource( name = "readOnlyJdbcTemplate" )
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private StatementBuilder statementBuilder;
+
     private String getSelectClause( DataQueryParams params )
     {
         return null;
     }
-
+    
+    private List<String> getColumns( DataQueryParams params )
+    {
+        //TODO pe, path
+        
+        List<String> columns = Lists.newArrayList( "dx", "ou", "level", "value", "textvalue" );
+        
+        List<String> dimensionCols = params.getDimensions().stream().map( d -> d.getDimension() ).collect( Collectors.toList() );
+        
+        return columns;
+    }
+    
 }
