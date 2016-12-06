5a5a225d5d7dfa4994adac4c51d08733d71c9e4d
abyot <abyota@gmail.com>
2016-12-06 13:33:50 +0100

    bug fix in autogenerating events during registration

--- dhis-2/dhis-web/dhis-web-apps/src/main/webapp/dhis-web-tracker-capture/components/registration/registration-controller.js
+++ dhis-2/dhis-web/dhis-web-apps/src/main/webapp/dhis-web-tracker-capture/components/registration/registration-controller.js
@@ -261,7 +261,8 @@ trackerCapture.controller('RegistrationController',
                             if(en.reference && en.status === 'SUCCESS'){                                
                                 enrollment.enrollment = en.reference;
                                 $scope.selectedEnrollment = enrollment;
-                                var dhis2Events = EventUtils.autoGenerateEvents($scope.tei.trackedEntityInstance, $scope.selectedProgram, $scope.selectedOrgUnit, enrollment, $scope.currentEvent);
+                                var availableEvent = $scope.currentEvent && $scope.currentEvent.event ? $scope.currentEvent : null;
+                                var dhis2Events = EventUtils.autoGenerateEvents($scope.tei.trackedEntityInstance, $scope.selectedProgram, $scope.selectedOrgUnit, enrollment, availableEvent);
                                 if(dhis2Events.events.length > 0){
                                     DHIS2EventFactory.create(dhis2Events).then(function(){
                                         notifyRegistrtaionCompletion(destination, $scope.tei.trackedEntityInstance);
