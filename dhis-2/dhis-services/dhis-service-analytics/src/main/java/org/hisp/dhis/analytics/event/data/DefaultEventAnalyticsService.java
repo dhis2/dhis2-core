3038d1202e8cc2926c0702f01b012a88b3981771
Bekkalizer <markus.bekken@gmail.com>
2016-12-13 11:05:53 +0100

    JdbcEnrollmentAnalyticsManager

--- dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/event/data/DefaultEventAnalyticsService.java
+++ dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/event/data/DefaultEventAnalyticsService.java
@@ -34,7 +34,6 @@ import static org.hisp.dhis.common.DimensionalObjectUtils.getDimensionalItemIds;
 import static org.hisp.dhis.common.DimensionalObjectUtils.asTypedList;
 import static org.hisp.dhis.organisationunit.OrganisationUnit.getParentGraphMap;
 import static org.hisp.dhis.organisationunit.OrganisationUnit.getParentNameGraphMap;
-
 import static org.hisp.dhis.analytics.DataQueryParams.NUMERATOR_ID;
 import static org.hisp.dhis.analytics.DataQueryParams.DENOMINATOR_ID;
 import static org.hisp.dhis.analytics.DataQueryParams.FACTOR_ID;
@@ -55,6 +54,7 @@ import org.hisp.dhis.analytics.AnalyticsMetaDataKey;
 import org.hisp.dhis.analytics.AnalyticsSecurityManager;
 import org.hisp.dhis.analytics.DataQueryParams;
 import org.hisp.dhis.analytics.Rectangle;
+import org.hisp.dhis.analytics.event.EnrollmentAnalyticsManager;
 import org.hisp.dhis.analytics.event.EventAnalyticsManager;
 import org.hisp.dhis.analytics.event.EventAnalyticsService;
 import org.hisp.dhis.analytics.event.EventDataQueryService;
@@ -75,6 +75,7 @@ import org.hisp.dhis.common.Pager;
 import org.hisp.dhis.common.QueryItem;
 import org.hisp.dhis.organisationunit.OrganisationUnit;
 import org.hisp.dhis.program.Program;
+import org.hisp.dhis.program.ProgramIndicatorAnalyticsType;
 import org.hisp.dhis.program.ProgramStage;
 import org.hisp.dhis.system.database.DatabaseInfo;
 import org.hisp.dhis.system.grid.ListGrid;
@@ -89,7 +90,10 @@ public class DefaultEventAnalyticsService
     implements EventAnalyticsService
 {
     @Autowired
-    private EventAnalyticsManager analyticsManager;
+    private EventAnalyticsManager eventAnalyticsManager;
+
+    @Autowired
+    private EnrollmentAnalyticsManager enrollmentAnalyticsManager;
     
     @Autowired
     private EventDataQueryService eventDataQueryService;
@@ -174,7 +178,15 @@ public class DefaultEventAnalyticsService
     
             for ( EventQueryParams query : queries )
             {
-                analyticsManager.getAggregatedEventData( query, grid, maxLimit );
+                if( query.getProgramIndicator() != null && 
+                    ProgramIndicatorAnalyticsType.ENROLLMENT.equals( query.getProgramIndicator().getProgramIndicatorAnalyticsType() ) )
+                {
+                    enrollmentAnalyticsManager.getAggregatedEventData( query, grid, maxLimit );
+                }
+                else
+                {
+                    eventAnalyticsManager.getAggregatedEventData( query, grid, maxLimit );
+                }
             }
             
             timer.getTime( "Got aggregated events" );
@@ -289,10 +301,10 @@ public class DefaultEventAnalyticsService
         {
             if ( params.isPaging() )
             {
-                count += analyticsManager.getEventCount( params );
+                count += eventAnalyticsManager.getEventCount( params );
             }
     
-            analyticsManager.getEvents( params, grid, queryPlanner.getMaxLimit() );
+            eventAnalyticsManager.getEvents( params, grid, queryPlanner.getMaxLimit() );
     
             timer.getTime( "Got events " + grid.getHeight() );
         }
@@ -361,7 +373,7 @@ public class DefaultEventAnalyticsService
 
         params = queryPlanner.planEventQuery( params );
 
-        analyticsManager.getEventClusters( params, grid, queryPlanner.getMaxLimit() );
+        eventAnalyticsManager.getEventClusters( params, grid, queryPlanner.getMaxLimit() );
         
         return grid;
     }
@@ -386,7 +398,7 @@ public class DefaultEventAnalyticsService
         
         params = queryPlanner.planEventQuery( params );
 
-        return analyticsManager.getRectangle( params );
+        return eventAnalyticsManager.getRectangle( params );
     }
 
     // -------------------------------------------------------------------------
