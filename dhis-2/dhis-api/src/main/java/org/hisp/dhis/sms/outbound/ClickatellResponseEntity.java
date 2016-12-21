a66137d0b54c720ab0d2769dd262d7fffb9a9162
zubaira <zubair@dhis2.org>
2016-12-21 17:01:01 +0100

    bug fix: related to non-functional gateway

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/sms/outbound/ClickatellResponseEntity.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/sms/outbound/ClickatellResponseEntity.java
@@ -44,9 +44,9 @@ import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 public class ClickatellResponseEntity
 {
 
-    private Map<String, List<Map<String, String>>> data;
+    private Map<Object, List<Map<Object, Object>>> data;
 
-    public ClickatellResponseEntity( Map<String, List<Map<String, String>>> data )
+    public ClickatellResponseEntity( Map<Object, List<Map<Object, Object>>> data )
     {
         super();
         this.data = data;
@@ -59,12 +59,12 @@ public class ClickatellResponseEntity
 
     @JsonProperty( value = "data" )
     @JacksonXmlProperty( localName = "data" )
-    public Map<String, List<Map<String, String>>> getData()
+    public Map<Object, List<Map<Object, Object>>> getData()
     {
         return data;
     }
 
-    public void setData( Map<String, List<Map<String, String>>> data )
+    public void setData( Map<Object, List<Map<Object, Object>>> data )
     {
         this.data = data;
     }
