b78dc31161631daa20fa7794db5a066d491e0057
Lars Helge Øverland <lars@dhis2.org>
2016-12-27 13:07:31 +0100

    Data value import, locking, performance improvement

--- dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/datavalueset/DefaultDataValueSetService.java
+++ dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/datavalueset/DefaultDataValueSetService.java
@@ -55,7 +55,7 @@ import org.hisp.dhis.dataelement.DataElementGroup;
 import org.hisp.dhis.dataset.CompleteDataSetRegistration;
 import org.hisp.dhis.dataset.CompleteDataSetRegistrationService;
 import org.hisp.dhis.dataset.DataSet;
-import org.hisp.dhis.dataset.DataSetService;
+import org.hisp.dhis.dataset.LockExceptionStore;
 import org.hisp.dhis.datavalue.DataValue;
 import org.hisp.dhis.datavalue.DataValueAudit;
 import org.hisp.dhis.dxf2.common.ImportOptions;
@@ -123,7 +123,7 @@ public class DefaultDataValueSetService
     private static final Log log = LogFactory.getLog( DefaultDataValueSetService.class );
 
     private static final String ERROR_OBJECT_NEEDED_TO_COMPLETE = "Must be provided to complete data set";
-    private static final int CACHE_MISS_THRESHOLD = 500;
+    private static final int CACHE_MISS_THRESHOLD = 250;
 
     @Autowired
     private IdentifiableObjectManager identifiableObjectManager;
@@ -136,9 +136,6 @@ public class DefaultDataValueSetService
 
     @Autowired
     private PeriodService periodService;
-
-    @Autowired
-    private DataSetService dataSetService;
 
     @Autowired
     private DataApprovalService approvalService;
@@ -158,6 +155,9 @@ public class DefaultDataValueSetService
     @Autowired
     private SystemSettingManager systemSettingManager;
 
+    @Autowired
+    private LockExceptionStore lockExceptionStore;
+    
     @Autowired
     private I18nManager i18nManager;
 
@@ -604,6 +604,9 @@ public class DefaultDataValueSetService
             .setImportOptions( importOptions );
 
         boolean isIso8601 = calendarService.getSystemCalendar().isIso8601();
+        boolean skipLockExceptionCheck = !lockExceptionStore.anyExists();
+        
+        log.info( String.format( "Is ISO calendar: %b, skip lock exception check: %b", isIso8601, skipLockExceptionCheck ) );
 
         I18n i18n = i18nManager.getI18n();
 
@@ -658,7 +661,7 @@ public class DefaultDataValueSetService
         CachingMap<String, Set<DataElementCategoryOptionCombo>> dataElementCategoryOptionComboMap = new CachingMap<>();
         CachingMap<String, Set<DataElementCategoryOptionCombo>> dataElementAttrOptionComboMap = new CachingMap<>();
         CachingMap<String, Boolean> dataElementOrgUnitMap = new CachingMap<>();
-        CachingMap<String, Boolean> periodLockedMap = new CachingMap<>();
+        CachingMap<String, Boolean> dataSetLockedMap = new CachingMap<>();
         CachingMap<String, Period> dataElementLatestFuturePeriodMap = new CachingMap<>();
         CachingMap<String, Boolean> orgUnitInHierarchyMap = new CachingMap<>();
         CachingMap<String, DateRange> attrOptionComboDateRangeMap = new CachingMap<>();
@@ -992,9 +995,9 @@ public class DefaultDataValueSetService
                 () -> dataElement.getApprovalDataSet() );
 
             if ( approvalDataSet != null ) // Data element is assigned to at least one data set
-            {
-                if ( periodLockedMap.get( approvalDataSet.getUid() + period.getUid() + orgUnit.getUid(),
-                    () -> dataSetService.isLocked( approvalDataSet, period, orgUnit, null ) ) )
+            {                
+                if ( dataSetLockedMap.get( approvalDataSet.getUid() + period.getUid() + orgUnit.getUid(), 
+                    () -> isLocked( approvalDataSet, period, orgUnit, skipLockExceptionCheck ) ) )
                 {
                     summary.getConflicts().add( new ImportConflict( period.getIsoDate(), "Current date is past expiry days for period " +
                         period.getIsoDate() + " and data set: " + approvalDataSet.getUid() ) );
@@ -1209,5 +1212,18 @@ public class DefaultDataValueSetService
         }
 
         summary.setDataSetComplete( DateUtils.getMediumDateString( completeDate ) );
+    }
+    
+    /**
+     * Checks whether the given data set is locked.
+     * 
+     * @param dataSet the data set.
+     * @param period the period.
+     * @param organisationUnit the organisation unit.
+     * @param skipLockExceptionCheck whether to skip lock exception check.
+     */
+    private boolean isLocked( DataSet dataSet, Period period, OrganisationUnit organisationUnit, boolean skipLockExceptionCheck )
+    {
+        return dataSet.isLocked( period, null ) && ( skipLockExceptionCheck || lockExceptionStore.getCount( dataSet, period, organisationUnit ) == 0L );
     }
 }
