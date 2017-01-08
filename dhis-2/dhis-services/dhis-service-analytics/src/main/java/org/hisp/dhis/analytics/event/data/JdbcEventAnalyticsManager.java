59855c46c13926d1745e860d5a81710f9b8a2c25
Lars Helge Øverland <lars@dhis2.org>
2017-01-08 10:36:00 +0100

    Event analytics, centralized code

--- dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/event/data/JdbcEventAnalyticsManager.java
+++ dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/event/data/JdbcEventAnalyticsManager.java
@@ -789,9 +789,7 @@ public class JdbcEventAnalyticsManager
     {
         String encodedFilter = statementBuilder.encode( filter.getFilter(), false );
         
-        String sqlFilter = filter.getSqlFilter( encodedFilter );
-        
-        return item.isText() ? sqlFilter.toLowerCase() : sqlFilter;
+        return item.getSqlFilter( filter, encodedFilter );
     }
 
     /**
