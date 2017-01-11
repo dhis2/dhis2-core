1760569ded32d1cf609b3c2528121ccdfb9d37ab
Halvdan Hoem Grelland <halvdanhg@gmail.com>
2017-01-11 15:35:03 +0100

    Catch error when resolving single value instead of failing entire process

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/notification/BaseNotificationMessageRenderer.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/notification/BaseNotificationMessageRenderer.java
@@ -74,6 +74,7 @@ public abstract class BaseNotificationMessageRenderer<T>
 
     protected static final String CONFIDENTIAL_VALUE_REPLACEMENT = "[CONFIDENTIAL]"; // TODO reconsider this...
     protected static final String MISSING_VALUE_REPLACEMENT = "[N/A]";
+    protected static final String VALUE_ON_ERROR = "[SERVER ERROR]";
 
     protected static final Pattern VAR_CONTENT_PATTERN = Pattern.compile( "^[A-Za-z0-9_]+$" );
     protected static final Pattern ATTR_CONTENT_PATTERN = Pattern.compile( "[A-Za-z][A-Za-z0-9]{10}" );
@@ -202,7 +203,17 @@ public abstract class BaseNotificationMessageRenderer<T>
             return StringUtils.EMPTY;
         }
 
-        String value = resolver.apply( entity );
+        String value;
+
+        try
+        {
+            value = resolver.apply( entity );
+        }
+        catch ( Exception ex )
+        {
+            log.warn( "Caught exception when running value resolver for variable: " + variableName , ex );
+            value = VALUE_ON_ERROR;
+        }
 
         return value != null ? value : StringUtils.EMPTY;
     }
