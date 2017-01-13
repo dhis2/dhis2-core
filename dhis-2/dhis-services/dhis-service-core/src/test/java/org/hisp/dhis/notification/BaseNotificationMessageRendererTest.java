597363a183b8f79fea54e7aa5f53aa0d5b0c9cf6
Halvdan Hoem Grelland <halvdanhg@gmail.com>
2017-01-13 17:23:25 +0100

    Added more unit tests

--- dhis-2/dhis-services/dhis-service-core/src/test/java/org/hisp/dhis/notification/BaseNotificationMessageRendererTest.java
+++ dhis-2/dhis-services/dhis-service-core/src/test/java/org/hisp/dhis/notification/BaseNotificationMessageRendererTest.java
@@ -30,15 +30,22 @@ package org.hisp.dhis.notification;
 
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Sets;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.RandomUtils;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 import org.hisp.dhis.common.DeliveryChannel;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+import java.util.EnumSet;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Function;
 
+import static java.lang.String.format;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
@@ -53,28 +60,15 @@ public class BaseNotificationMessageRendererTest
 
     private BaseNotificationMessageRenderer<Entity> renderer;
 
-    private NotificationTemplate template;
-
-    static final ImmutableMap<TemplateVariable, Function<Entity, String>> VARIABLE_RESOLVERS =
-        ImmutableMap.<TemplateVariable, Function<Entity, String>>builder()
-            .put( EntityTemplateVariable.a, e -> e.propertyA )
-            .put( EntityTemplateVariable.b, e -> e.propertyB )
-            .build();
-
-    static final ImmutableMap<String, String> ATTRIBUTE_VALUES =
-        ImmutableMap.<String, String>builder()
-            .put( "a1234567890", "Attribute A" )
-            .put( "b1234567890", "Attribute B" )
-            .build();
+    private static final Pair<String, String> ATTR_A = ImmutablePair.of( "a1234567890", "Attribute A value" );
+    private static final Pair<String, String> ATTR_B = ImmutablePair.of( "b1234567890", "Attribute B value" );
+    private static final Pair<String, String> ATTR_NULL = ImmutablePair.of( "n1234567890", null );
 
     @SuppressWarnings( "unchecked" )
     @Before
     public void setUpTest()
     {
-        // Mock
-
-        renderer = Mockito.spy( new MockNotificationMessageRenderer() );
-        template = new MockNotificationTemplate( "Subject V{a} V{b}", "Message V{a} V{b}" );
+        renderer = new MockNotificationMessageRenderer();
     }
 
     // -------------------------------------------------------------------------
@@ -82,29 +76,177 @@ public class BaseNotificationMessageRendererTest
     // -------------------------------------------------------------------------
 
     @Test
-    public void testRenderVariableRendersVariable()
+    public void testRenderVariablesRendersVariables()
     {
-        Entity e = new Entity( "Something A", "Something B" );
+        Entity e = entity();
+
+        String templateString = "V{a} V{b}";
+
+        NotificationTemplate template = template( templateString );
 
         NotificationMessage message = renderer.render( e, template );
 
+        String expected = format( "%s %s", e.propertyA, e.propertyB );
+
         assertNotNull( message );
-        assertEquals( "Subject Something A Something B", message.getSubject() );
-        assertEquals( "Message Something A Something B", message.getMessage() );
+        assertEquals( expected, message.getMessage() );
     }
 
     @Test
     public void testRenderAttributesRendersAttributes()
     {
-        Entity e = new Entity( "A", "B" );
-        template = new MockNotificationTemplate( "A{a1234567890}", "A{b1234567890}" );
+        Entity e = entity();
+        String templateString = format( "A{%s} A{%s}", ATTR_A.getKey(), ATTR_B.getKey() );
+
+        NotificationTemplate template = template( templateString );
+
+        NotificationMessage message = renderer.render( e, template );
+
+        String expected = format( "%s %s", ATTR_A.getValue(), ATTR_B.getValue() );
+
+        assertNotNull( message );
+        assertEquals( expected, message.getMessage() );
+    }
+
+    @Test
+    public void testRenderMixedContentRendersBoth()
+    {
+        Entity e = entity();
+        String templateMessage = format( "Boom V{a} shake A{%s} the room", ATTR_A.getKey() );
+
+        NotificationTemplate template = template( templateMessage, templateMessage );
+
+        NotificationMessage message = renderer.render( e, template );
+
+        String expected = format( "Boom %s shake %s the room", e.propertyA, ATTR_A.getValue() );
+
+        assertNotNull( message );
+        assertEquals( expected, message.getMessage() );
+    }
+
+    @Test
+    public void testRenderNullValuesAreReplacedWithEmptyStrings()
+    {
+        Entity e = entity( "Halvdan was here", null );
+
+        String templateString = format( "V{a} V{b} A{%s}", ATTR_NULL.getKey() );
+
+        NotificationTemplate template = template( templateString );
+
+        NotificationMessage message = renderer.render( e, template );
+
+        String expected = format( "%s  ", e.propertyA );
+
+        assertNotNull( message );
+        assertEquals( expected, message.getMessage() );
+    }
+
+    @Test
+    public void testNonSupportedExpressionsAreIgnored()
+    {
+        Entity e = entity();
+        String templateString = "X{abc} V{a} Y{}";
+
+        NotificationTemplate template = template( templateString );
+
+        NotificationMessage message = renderer.render( e, template );
+
+        String expected = format( "X{abc} %s Y{}", e.propertyA );
+
+        assertNotNull( message );
+        assertEquals( expected, message.getMessage() );
+    }
+
+    @Test
+    public void testNonExistingVariablesAreReplacedWithEmptyStrings()
+    {
+        Entity e = entity();
+        String templateString = "V{b} V{does_not_exist} V{a}";
+
+        NotificationTemplate template = template( templateString );
+
+        NotificationMessage message = renderer.render( e, template );
+
+        String expected = format( "%s  %s", e.propertyB, e.propertyA );
+
+        assertNotNull( message );
+        assertEquals( expected, message.getMessage() );
+    }
+
+    @Test
+    public void testSubjectLengthIsLimited()
+    {
+        Entity e = entity();
+        String templateString = RandomStringUtils.randomAlphanumeric( 101 );
+        NotificationTemplate template = template( templateString );
 
         NotificationMessage message = renderer.render( e, template );
 
         assertNotNull( message );
+        assertEquals( BaseNotificationMessageRenderer.SUBJECT_CHAR_LIMIT, message.getSubject().length() );
+    }
 
-        assertEquals( ATTRIBUTE_VALUES.get( "a1234567890" ), message.getSubject() );
-        assertEquals( ATTRIBUTE_VALUES.get( "b1234567890" ), message.getMessage() );
+    @Test
+    public void testMessageLengthIsLimitedWhenHasSmsRecipient()
+    {
+        Entity e = entity();
+
+        int tooLong = RandomUtils.nextInt(
+            BaseNotificationMessageRenderer.SMS_CHAR_LIMIT + 1,
+            BaseNotificationMessageRenderer.SMS_CHAR_LIMIT + 100
+        );
+
+        String templateString = RandomStringUtils.randomAlphanumeric( tooLong );
+        NotificationTemplate template = template( templateString );
+
+        NotificationMessage message = renderer.render( e, template );
+
+        assertNotNull( message );
+        assertEquals( BaseNotificationMessageRenderer.SMS_CHAR_LIMIT, message.getMessage().length() );
+    }
+
+    @Test
+    public void testMessageLengthIsLimitedForEmailRecipients()
+    {
+        Entity e = entity();
+
+        int tooLong = RandomUtils.nextInt(
+            BaseNotificationMessageRenderer.EMAIL_CHAR_LIMIT + 1,
+            BaseNotificationMessageRenderer.EMAIL_CHAR_LIMIT + 100
+        );
+
+        String templateString = RandomStringUtils.randomAlphanumeric( tooLong );
+        NotificationTemplate template = Mockito.spy( template( templateString ) );
+        Mockito.when( template.getDeliveryChannels() ).thenReturn( Sets.newHashSet( DeliveryChannel.EMAIL ) );
+
+        NotificationMessage message = renderer.render( e, template );
+
+        assertNotNull( message );
+        assertEquals( BaseNotificationMessageRenderer.EMAIL_CHAR_LIMIT, message.getMessage().length() );
+    }
+
+    // -------------------------------------------------------------------------
+    // Factory methods
+    // -------------------------------------------------------------------------
+
+    private static Entity entity( String a, String b )
+    {
+        return new Entity( a, b );
+    }
+
+    private static Entity entity()
+    {
+        return entity( "A", "B" );
+    }
+
+    private static MockNotificationTemplate template( String str )
+    {
+        return template( str, str );
+    }
+
+    private static MockNotificationTemplate template( String sub, String msg )
+    {
+        return new MockNotificationTemplate( sub, msg );
     }
 
     // -------------------------------------------------------------------------
@@ -116,6 +258,18 @@ public class BaseNotificationMessageRendererTest
      */
     static class MockNotificationMessageRenderer extends BaseNotificationMessageRenderer<Entity>
     {
+        static final ImmutableMap<TemplateVariable, Function<Entity, String>> VARIABLE_RESOLVERS =
+            ImmutableMap.<TemplateVariable, Function<Entity, String>>builder()
+                .put( EntityTemplateVariable.a, e -> e.propertyA )
+                .put( EntityTemplateVariable.b, e -> e.propertyB )
+                .build();
+
+        static final Map<String, String> ATTRIBUTE_VALUES = new HashMap<String, String>() {{
+            put( ATTR_A.getKey(), ATTR_A.getValue() );
+            put( ATTR_B.getKey(), ATTR_B.getValue() );
+            put( ATTR_NULL.getKey(), ATTR_NULL.getValue() );
+        }};
+
         @Override
         protected Map<TemplateVariable, Function<Entity, String>> getVariableResolvers()
         {
@@ -131,7 +285,10 @@ public class BaseNotificationMessageRendererTest
         @Override
         protected TemplateVariable fromVariableName( String name )
         {
-            return EntityTemplateVariable.valueOf( name );
+            return EnumSet.allOf( EntityTemplateVariable.class ).stream()
+                .filter( tv -> tv.name().equals( name ) )
+                .findFirst()
+                .orElse( null );
         }
 
         @Override
@@ -141,9 +298,6 @@ public class BaseNotificationMessageRendererTest
         }
     }
 
-    /**
-     * Our fake entity
-     */
     static class Entity
     {
         final String propertyA;
@@ -159,8 +313,7 @@ public class BaseNotificationMessageRendererTest
 
     enum EntityTemplateVariable implements TemplateVariable
     {
-        a ( "a" ),
-        b ( "b" );
+        a ( "a" ), b ( "b" );
 
         final String variableName;
 
