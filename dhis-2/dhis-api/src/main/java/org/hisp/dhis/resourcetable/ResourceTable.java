d4bad750740edf35ed0240bbc0803a58babd4e6d
Lars Helge Øverland <lars@dhis2.org>
2017-01-13 13:28:36 +0100

    Resource tables, running analyze after generation Incredible performance improvement for the full analytics table update process.

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/resourcetable/ResourceTable.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/resourcetable/ResourceTable.java
@@ -85,6 +85,11 @@ public abstract class ResourceTable<T>
     {
         return "alter table " + getTempTableName() + " rename to " + getTableName() + ";";
     }
+    
+    public final String getAnalyzeTableStatement()
+    {
+        return "analyze " + getTableName() + ";";
+    }
 
     // -------------------------------------------------------------------------
     // Protected methods
