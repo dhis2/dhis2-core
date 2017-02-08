fcd137d5ba7738d13800ec77441381572bff3f11
zubaira <zubair@dhis2.org>
2017-02-08 13:05:21 +0100

    gateway configuration fix

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/sms/config/GenericHttpGatewayConfig.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/sms/config/GenericHttpGatewayConfig.java
@@ -31,6 +31,7 @@ package org.hisp.dhis.sms.config;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.collect.Lists;
 
 public class GenericHttpGatewayConfig
     extends SmsGatewayConfig
@@ -41,7 +42,7 @@ public class GenericHttpGatewayConfig
 
     private String recipientParameter;
     
-    private List<GenericGatewayParameter> parameters;
+    private List<GenericGatewayParameter> parameters = Lists.newArrayList();
 
     public GenericHttpGatewayConfig()
     {
