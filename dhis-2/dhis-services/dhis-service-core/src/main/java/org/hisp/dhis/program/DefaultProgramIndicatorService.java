242e56c71b1842120c3c737ecda5d35e1d0ba349
Bekkalizer <markus.bekken@gmail.com>
2017-02-14 09:50:03 +0100

    Support == '' in program indicators

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/program/DefaultProgramIndicatorService.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/program/DefaultProgramIndicatorService.java
@@ -273,6 +273,8 @@ public class DefaultProgramIndicatorService
         }
 
         expression = TextUtils.removeNewlines( expression );
+        
+        expression = getSubstitutedNullOrEmptyAnalyticsSql( expression );
 
         expression = getSubstitutedVariablesForAnalyticsSql( expression );
 
@@ -322,6 +324,27 @@ public class DefaultProgramIndicatorService
             }
         }
 
+        return TextUtils.appendTail( matcher, buffer );
+    }
+    
+    private String getSubstitutedNullOrEmptyAnalyticsSql( String expression )
+    {
+        if ( expression == null )
+        {
+            return null;
+        }
+
+        StringBuffer buffer = new StringBuffer();
+
+        Matcher matcher = ProgramIndicator.NULLOREMPTY_PATTERN.matcher( expression );
+
+        while ( matcher.find() )
+        {
+            String expressionVariable = trim( matcher.group( 1 ) );
+            
+            matcher.appendReplacement( buffer, expressionVariable + " is null " );
+        }
+        
         return TextUtils.appendTail( matcher, buffer );
     }
 
