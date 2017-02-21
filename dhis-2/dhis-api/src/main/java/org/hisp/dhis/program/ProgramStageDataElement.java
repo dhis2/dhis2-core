6e286c69abffa3375d2cf4c76046248a37325509
Morten Olav Hansen <mortenoh@gmail.com>
2017-02-21 17:17:58 +0800

    Minor

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/program/ProgramStageDataElement.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/program/ProgramStageDataElement.java
@@ -35,7 +35,6 @@ import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import org.hisp.dhis.common.BaseIdentifiableObject;
 import org.hisp.dhis.common.DxfNamespaces;
 import org.hisp.dhis.common.IdentifiableObject;
-import org.hisp.dhis.common.LinkObject;
 import org.hisp.dhis.common.MergeMode;
 import org.hisp.dhis.dataelement.DataElement;
 
