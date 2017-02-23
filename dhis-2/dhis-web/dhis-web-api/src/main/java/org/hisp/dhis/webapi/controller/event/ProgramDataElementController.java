518debe8cfabc5841ac564cbf2d241056da76724
Morten Olav Hansen <mortenoh@gmail.com>
2017-02-23 11:31:44 +0800

    Cleanup in PDE schema descriptor (metadata = false, no authorities). Implemented PDE controller as a read-only controller

--- dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/event/ProgramDataElementController.java
+++ dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/event/ProgramDataElementController.java
@@ -28,37 +28,80 @@ package org.hisp.dhis.webapi.controller.event;
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+import com.google.common.collect.Lists;
+import org.hisp.dhis.common.DhisApiVersion;
+import org.hisp.dhis.common.Pager;
+import org.hisp.dhis.common.PagerUtils;
+import org.hisp.dhis.dxf2.common.OrderParams;
+import org.hisp.dhis.fieldfilter.FieldFilterService;
+import org.hisp.dhis.node.NodeUtils;
+import org.hisp.dhis.node.Preset;
+import org.hisp.dhis.node.types.RootNode;
 import org.hisp.dhis.program.ProgramDataElement;
 import org.hisp.dhis.program.ProgramService;
 import org.hisp.dhis.query.Order;
 import org.hisp.dhis.query.Query;
 import org.hisp.dhis.query.QueryParserException;
+import org.hisp.dhis.query.QueryService;
+import org.hisp.dhis.schema.Schema;
+import org.hisp.dhis.schema.SchemaService;
 import org.hisp.dhis.schema.descriptors.ProgramDataElementSchemaDescriptor;
-import org.hisp.dhis.webapi.controller.AbstractCrudController;
+import org.hisp.dhis.webapi.mvc.annotation.ApiVersion;
+import org.hisp.dhis.webapi.service.ContextService;
 import org.hisp.dhis.webapi.webdomain.WebMetadata;
 import org.hisp.dhis.webapi.webdomain.WebOptions;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Lars Helge Overland
  */
 @Controller
 @RequestMapping( value = ProgramDataElementSchemaDescriptor.API_ENDPOINT )
+@ApiVersion( { DhisApiVersion.DEFAULT, DhisApiVersion.ALL } )
 public class ProgramDataElementController
-    extends AbstractCrudController<ProgramDataElement>
 {
-    @Autowired
-    private ProgramService programService;
+    private final QueryService queryService;
+    private final FieldFilterService fieldFilterService;
+    private final ContextService contextService;
+    private final SchemaService schemaService;
+    private final ProgramService programService;
 
-    @Override
+    public ProgramDataElementController( QueryService queryService, FieldFilterService fieldFilterService,
+        ContextService contextService, SchemaService schemaService, ProgramService programService )
+    {
+        this.queryService = queryService;
+        this.fieldFilterService = fieldFilterService;
+        this.contextService = contextService;
+        this.schemaService = schemaService;
+        this.programService = programService;
+    }
+
+    @GetMapping
     @SuppressWarnings( "unchecked" )
-    protected List<ProgramDataElement> getEntityList( WebMetadata metadata, WebOptions options, List<String> filters, List<Order> orders )
+    public @ResponseBody RootNode getObjectList( @RequestParam Map<String, String> rpParameters, OrderParams orderParams )
         throws QueryParserException
     {
+        Schema schema = schemaService.getDynamicSchema( ProgramDataElement.class );
+
+        List<String> fields = Lists.newArrayList( contextService.getParameterValues( "fields" ) );
+        List<String> filters = Lists.newArrayList( contextService.getParameterValues( "filter" ) );
+        List<Order> orders = orderParams.getOrders( schema );
+
+        if ( fields.isEmpty() )
+        {
+            fields.addAll( Preset.defaultPreset().getFields() );
+        }
+
+        WebOptions options = new WebOptions( rpParameters );
+        WebMetadata metadata = new WebMetadata();
+
         List<ProgramDataElement> programDataElements;
         Query query = queryService.getQueryFromUrl( ProgramDataElement.class, filters, orders, options.getRootJunction() );
         query.setDefaultOrder();
@@ -72,6 +115,23 @@ public class ProgramDataElementController
 
         programDataElements = (List<ProgramDataElement>) queryService.query( query );
 
-        return programDataElements;
+        Pager pager = metadata.getPager();
+
+        if ( options.hasPaging() && pager == null )
+        {
+            pager = new Pager( options.getPage(), programDataElements.size(), options.getPageSize() );
+            programDataElements = PagerUtils.pageCollection( programDataElements, pager );
+        }
+
+        RootNode rootNode = NodeUtils.createMetadata();
+
+        if ( pager != null )
+        {
+            rootNode.addChild( NodeUtils.createPager( pager ) );
+        }
+
+        rootNode.addChild( fieldFilterService.filter( ProgramDataElement.class, programDataElements, fields ) );
+
+        return rootNode;
     }
 }
