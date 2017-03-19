d50b7734736bf064d91dfcab71b75374c901fb60
vietnguyen <viet@dhis2.org>
2017-03-20 00:51:43 +0700

    add compression option for metadata export with dependencies

--- dhis-2/dhis-web/dhis-web-importexport/src/main/webapp/dhis-web-importexport/javascript/metadataDependencyExport.js
+++ dhis-2/dhis-web/dhis-web-importexport/src/main/webapp/dhis-web-importexport/javascript/metadataDependencyExport.js
@@ -1,28 +1,30 @@
 var mde = {};
 
-$( document ).ready( function() {
-	mde.displayObjectList( "dataSets" );
-} );
+$(document).ready(function () {
+  mde.displayObjectList("dataSets");
+});
 
-mde.displayObjectList = function() {
-	var objectType = $( "#objectType" ).val(),
-		url = "../api/" + objectType + "?fields=id,displayName&paging=false";
-	
-	$.getJSON( url, function( json ) {
-		var html = "";
-		$.each( json[objectType], function( inx, val ) {
-			html += "<option value=\"" + val.id + "\">" + val.displayName + "</option>";
-		} );
-		
-		$( "#objectList" ).html( html );
-	} );
+mde.displayObjectList = function () {
+  var objectType = $("#objectType").val(),
+      url = "../api/" + objectType + "?fields=id,displayName&paging=false";
+
+  $.getJSON(url, function ( json ) {
+    var html = "";
+    $.each(json[objectType], function ( inx, val ) {
+      html += "<option value=\"" + val.id + "\">" + val.displayName + "</option>";
+    });
+
+    $("#objectList").html(html);
+  });
 }
 
-mde.exportMetadata = function() {
-	var objectType = $( "#objectType" ).val(),
-		objectId = $( "#objectList" ).val(),
-		format = $( "#format" ).val(),
-		url = "../api/" + objectType + "/" + objectId + "/metadata." + format;
-	
-	window.open( url, "_blank" );
+mde.exportMetadata = function () {
+  var objectType = $("#objectType").val(),
+      objectId = $("#objectList").val(),
+      format = $("#format").val(),
+      compression = $("#compression").val(),
+      url = "../api/" + objectType + "/" + objectId + "/metadata." + format + (compression ? compression : "" );
+
+
+  window.open(url, "_blank");
 }
