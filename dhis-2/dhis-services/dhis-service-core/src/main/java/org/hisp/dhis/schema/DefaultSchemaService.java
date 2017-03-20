533f13b1b2a6da0062f772530aac667d23d3d906
Morten Olav Hansen <mortenoh@gmail.com>
2017-03-20 14:21:40 +0700

    minor reverts

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/schema/DefaultSchemaService.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/schema/DefaultSchemaService.java
@@ -47,9 +47,7 @@ import org.springframework.core.OrderComparator;
 import org.springframework.util.StringUtils;
 
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -293,7 +291,7 @@ public class DefaultSchemaService
     public List<Schema> getSortedSchemas()
     {
         List<Schema> schemas = Lists.newArrayList( classSchemaMap.values() );
-        Collections.sort( schemas, OrderComparator.INSTANCE );
+        schemas.sort( OrderComparator.INSTANCE );
 
         return schemas;
     }
@@ -303,19 +301,8 @@ public class DefaultSchemaService
     {
         List<Schema> schemas = getSchemas();
 
-        Iterator<Schema> iterator = schemas.iterator();
-
-        while ( iterator.hasNext() )
-        {
-            Schema schema = iterator.next();
-
-            if ( !schema.isMetadata() )
-            {
-                iterator.remove();
-            }
-        }
-
-        Collections.sort( schemas, OrderComparator.INSTANCE );
+        schemas.removeIf( schema -> !schema.isMetadata() );
+        schemas.sort( OrderComparator.INSTANCE );
 
         return schemas;
     }
