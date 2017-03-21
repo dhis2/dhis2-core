6c976320bf26aa9da30cdad43d434bf5dd6522c7
Henning Håkonsen <henninghaakonsen@me.com>
2017-03-21 12:52:22 +0100

    First simple test

--- dhis-2/dhis-services/dhis-service-analytics/src/test/java/org/hisp/dhis/analytics/data/AnalyticsServiceTest.java
+++ dhis-2/dhis-services/dhis-service-analytics/src/test/java/org/hisp/dhis/analytics/data/AnalyticsServiceTest.java
@@ -1,25 +1,35 @@
 package org.hisp.dhis.analytics.data;
 
+import com.google.common.collect.Sets;
 import org.hisp.dhis.DhisTest;
 import org.hisp.dhis.analytics.*;
 import org.hisp.dhis.dataelement.*;
 import org.hisp.dhis.datavalue.DataValue;
 import org.hisp.dhis.datavalue.DataValueService;
-import org.hisp.dhis.jdbc.StatementBuilder;
+import org.hisp.dhis.dxf2.csv.CsvImportService;
+import org.hisp.dhis.dxf2.datavalueset.DataValueSetService;
 import org.hisp.dhis.organisationunit.OrganisationUnit;
 import org.hisp.dhis.organisationunit.OrganisationUnitGroup;
 import org.hisp.dhis.organisationunit.OrganisationUnitService;
 import org.hisp.dhis.period.MonthlyPeriodType;
 import org.hisp.dhis.period.Period;
 import org.hisp.dhis.period.PeriodType;
-import org.hisp.dhis.period.YearlyPeriodType;
 import org.hisp.dhis.user.UserService;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
-import static org.hisp.dhis.common.DimensionalObjectUtils.getList;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+//mer avanserte tester -> Groupset
+// mvn -Dtest=AnalyticsServiceTest test
+// Statementbuilder
+// ValidationNotificationServiceTest
 
 /**
  * Created by henninghakonsen on 13/03/2017.
@@ -39,6 +49,8 @@ public class AnalyticsServiceTest
     private Period peB;
     private Period peC;
 
+    List<DataElement> dataElements = new ArrayList<>();
+
     private DataElementCategoryCombo categoryComboDef;
     private DataElementCategoryOptionCombo ocDef;
 
@@ -47,11 +59,6 @@ public class AnalyticsServiceTest
     private OrganisationUnit ouC;
     private OrganisationUnit ouD;
     private OrganisationUnit ouE;
-    private OrganisationUnit ouF;
-    private OrganisationUnit ouG;
-    private OrganisationUnit ouH;
-    private OrganisationUnit ouI;
-    private OrganisationUnit ouJ;
 
     private OrganisationUnitGroup ouGroupA;
     private OrganisationUnitGroup ouGroupB;
@@ -68,6 +75,9 @@ public class AnalyticsServiceTest
 
     @Autowired
     private DataElementCategoryService categoryService;
+
+    @Autowired
+    private DataValueSetService datavalueSetService;
 
     @Autowired
     private DataValueService dataValueService;
@@ -90,7 +100,27 @@ public class AnalyticsServiceTest
     private UserService internalUserService;
 
     @Autowired
-    private StatementBuilder statementBuilder;
+    private CsvImportService csvImportService;
+
+    /*
+     * Configure org unit hierarchy like so:
+     *
+     *                   A
+     *                 /   \
+     *                B     C
+     *               / \
+     *              D   E
+     */
+    public void configureHierarchy(OrganisationUnit A, OrganisationUnit B, OrganisationUnit C, OrganisationUnit D, OrganisationUnit E)
+    {
+        A.getChildren().addAll( Sets.newHashSet( B, C ) );
+        B.setParent( A );
+        C.setParent( A );
+
+        B.getChildren().addAll( Sets.newHashSet( D, E ) );
+        D.setParent( B );
+        E.setParent( B );
+    }
 
     @Override
     public void setUpTest() {
@@ -107,20 +137,10 @@ public class AnalyticsServiceTest
         peC = createPeriod( PeriodType.getByNameIgnoreCase( MonthlyPeriodType.NAME ), getDate( 2017, 3, 1 ), getDate( 2017, 3, 31 ) );
 
         deA = createDataElement( 'A', categoryComboDef );
-        deA.setDisplayName("A");
-        deA.setUid("testA");
         deB = createDataElement( 'B', categoryComboDef );
-        deB.setDisplayName("B");
-        deB.setUid("testB");
         deC = createDataElement( 'C', categoryComboDef );
-        deC.setDisplayName("C");
-        deC.setUid("testC");
         deD = createDataElement( 'D', categoryComboDef );
-        deD.setDisplayName("D");
-        deD.setUid("testD");
         deE = createDataElement( 'E', categoryComboDef );
-        deE.setDisplayName("E");
-        deE.setUid("testE");
 
         dataElementService.addDataElement( deA );
         dataElementService.addDataElement( deB );
@@ -128,45 +148,13 @@ public class AnalyticsServiceTest
         dataElementService.addDataElement( deD );
         dataElementService.addDataElement( deE );
 
+
         ouA = createOrganisationUnit( 'A');
         ouB = createOrganisationUnit( 'B');
         ouC = createOrganisationUnit( 'C');
         ouD = createOrganisationUnit( 'D');
         ouE = createOrganisationUnit( 'E');
-        ouF = createOrganisationUnit( 'F');
-        ouG = createOrganisationUnit( 'G');
-        ouH = createOrganisationUnit( 'H');
-        ouI = createOrganisationUnit( 'I');
-        ouJ = createOrganisationUnit( 'j');
-
-        HashSet<OrganisationUnit> childOfA = new HashSet<>();
-        childOfA.add(ouB);
-        childOfA.add(ouC);
-
-        ouA.setChildren(childOfA);
-
-        HashSet<OrganisationUnit> childOfB = new HashSet<>();
-        childOfB.add(ouD);
-        childOfB.add(ouE);
-
-        ouB.setChildren(childOfB);
-
-        HashSet<OrganisationUnit> childOfC = new HashSet<>();
-        childOfC.add(ouG);
-
-        ouC.setChildren(childOfC);
-
-        HashSet<OrganisationUnit> childOfD = new HashSet<>();
-        childOfD.add(ouF);
-        childOfD.add(ouJ);
-
-        ouD.setChildren(childOfD);
-
-        HashSet<OrganisationUnit> childOfG = new HashSet<>();
-        childOfG.add(ouI);
-        childOfG.add(ouH);
-
-        ouG.setChildren(childOfG);
+        configureHierarchy(ouA, ouB, ouC, ouD, ouE);
 
         organisationUnitService.addOrganisationUnit( ouA );
         organisationUnitService.addOrganisationUnit( ouB );
@@ -174,47 +162,74 @@ public class AnalyticsServiceTest
         organisationUnitService.addOrganisationUnit( ouD );
         organisationUnitService.addOrganisationUnit( ouE );
 
-
+        dataElements.add(deA);
+        dataElements.add(deB);
+        dataElements.add(deC);
+        dataElements.add(deD);
     }
 
     @Test
-    public void testAnalyticsTable()
+    public void testSimpleAggregation()
     {
-        /*InputStream in = null;
-        try {
-            in = new ClassPathResource( "csv/dataValueSetA.csv" ).getInputStream();
-        } catch (IOException e) {
-            e.printStackTrace();
+        // Data values for A = 20
+        List<DataValue> dataValuesA = new ArrayList<>();
+        for(int i=1; i<5; i++) {
+            dataValuesA.add(new DataValue(dataElements.get(i-1), peA, ouA, ocDef, ocDef));
+            dataValuesA.get(i-1).setValue(i*2 + "");
+            dataValueService.addDataValue(dataValuesA.get(i-1));
         }
 
-        ImportSummary summary = dataValueSetService.saveDataValueSetCsv( in, null, null );*/
-        //mer avanserte tester -> Groupset
-        // Statementbuilder
-
-        DataValue dataValue = new DataValue( deA, peA, ouA, ocDef, ocDef );
-        dataValue.setValue("1");
-        dataValueService.addDataValue(dataValue);
-
-        DataValue dataValue1 = new DataValue( deA, peA, ouA, ocDef, ocDef );
-        dataValue1.setValue("2");
-        dataValueService.addDataValue(dataValue);
-
-        for(DataValue dv: dataValueService.getAllDataValues()) {
-            System.out.println(dv);
+        // Data values for B = 16
+        List<DataValue> dataValuesB = new ArrayList<>();
+        for(int i=1; i<5; i++) {
+            dataValuesB.add(new DataValue(dataElements.get(i-1), peA, ouB, ocDef, ocDef));
+            dataValuesB.get(i-1).setValue(i*2-1 + "");
+            dataValueService.addDataValue(dataValuesB.get(i-1));
         }
 
-        System.out.println("test statementBuilder.getAnalyze: '" + statementBuilder.getAnalyze("test") + "'");
+        // Data values for C = 50
+        List<DataValue> dataValuesC = new ArrayList<>();
+        for(int i=1; i<5; i++) {
+            dataValuesC.add(new DataValue(dataElements.get(i-1), peA, ouC, ocDef, ocDef));
+            dataValuesC.get(i-1).setValue(i*5 + "");
+            dataValueService.addDataValue(dataValuesC.get(i-1));
+        }
+
+        // Data values for E = 4
+        List<DataValue> dataValuesE = new ArrayList<>();
+        for(int i=1; i<5; i++) {
+            dataValuesE.add(new DataValue(dataElements.get(i-1), peA, ouE, ocDef, ocDef));
+            dataValuesE.get(i-1).setValue( "1" );
+            dataValueService.addDataValue(dataValuesE.get(i-1));
+        }
 
         analyticsTableGenerator.generateTables(null, null, null, false);
 
-
-        // Param to test analytics service
+        // Params: Sum for all org units for 2017
         Period y2017 = createPeriod( "2017" );
         DataQueryParams params = DataQueryParams.newBuilder()
-                .withDataElements( getList( createDataElement( 'A' ), createDataElement( 'B' ) ) )
-                .withPeriods( getList( y2017 ) )
-                .withOrganisationUnits( getList( createOrganisationUnit( 'A' ) ) )
-                .withDataPeriodType( new YearlyPeriodType() )
-                .withAggregationType( AggregationType.AVERAGE_SUM_INT_DISAGGREGATION ).build();
+                .withOrganisationUnits( organisationUnitService.getAllOrganisationUnits() )
+                .withAggregationType(AggregationType.SUM)
+                .withOutputFormat(OutputFormat.ANALYTICS)
+                .withPeriod(y2017)
+                .build();
+
+        Map<String, Object> aggregatedDataValueMapping =  analyticsService.getAggregatedDataValueMapping(params);
+
+        assertNotNull(aggregatedDataValueMapping);
+
+        assertNotNull(aggregatedDataValueMapping.get( "ouabcdefghA-2017" ));
+        assertEquals(90.0, aggregatedDataValueMapping.get( "ouabcdefghA-2017" ));
+
+        assertNotNull(aggregatedDataValueMapping.get( "ouabcdefghB-2017" ));
+        assertEquals(20.0, aggregatedDataValueMapping.get( "ouabcdefghB-2017" ));
+
+        assertNotNull(aggregatedDataValueMapping.get( "ouabcdefghC-2017" ));
+        assertEquals(50.0, aggregatedDataValueMapping.get( "ouabcdefghC-2017" ));
+
+        assertNull(aggregatedDataValueMapping.get( "ouabcdefghD-2017" ));
+
+        assertNotNull(aggregatedDataValueMapping.get( "ouabcdefghE-2017" ));
+        assertEquals(4.0, aggregatedDataValueMapping.get( "ouabcdefghE-2017" ));
     }
 }
