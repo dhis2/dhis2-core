d8bbc0ac9e8a052a34a122066b2359efbcc0cb36
Lars Helge Øverland <lars@dhis2.org>
2017-04-21 16:02:18 +0200

    Named regex groups

--- dhis-2/dhis-support/dhis-support-system/src/main/java/org/hisp/dhis/system/util/SmsUtils.java
+++ dhis-2/dhis-support/dhis-support-system/src/main/java/org/hisp/dhis/system/util/SmsUtils.java
@@ -41,8 +41,6 @@ import java.text.SimpleDateFormat;
 
 import org.apache.commons.lang3.StringUtils;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.hisp.dhis.commons.util.TextUtils;
 import org.hisp.dhis.organisationunit.OrganisationUnit;
 import org.hisp.dhis.sms.parse.SMSParserException;
@@ -55,8 +53,6 @@ import org.hisp.dhis.sms.incoming.IncomingSms;
  */
 public class SmsUtils
 {
-    private static final Log log = LogFactory.getLog( SmsUtils.class );
-
     private static int MAX_CHAR = 160;
 
     public static String getCommandString( IncomingSms sms )
