c6ea02ac8695adf99d3a84d598137fb5c2be2e94
Lars Helge Øverland <lars@dhis2.org>
2017-04-28 11:25:27 +0200

    SQL view, validation on get and execute

--- dhis-2/dhis-services/dhis-service-administration/src/test/java/org/hisp/dhis/sqlview/SqlViewServiceTest.java
+++ dhis-2/dhis-services/dhis-service-administration/src/test/java/org/hisp/dhis/sqlview/SqlViewServiceTest.java
@@ -102,7 +102,6 @@ public class SqlViewServiceTest
 
         assertEquals( idE, sqlViewE.getId() );
         assertEq( 'E', sqlViewE, sqlE );
-
     }
 
     @Test
@@ -182,7 +181,6 @@ public class SqlViewServiceTest
         SqlView sqlView = new SqlView( "Name", "WITH foo as (delete FROM dataelement returning *) SELECT * FROM foo;", SqlViewType.QUERY );
 
         sqlViewService.validateSqlView( sqlView, null, null );
-
     }
 
     @Test (expected = IllegalQueryException.class)
@@ -191,7 +189,6 @@ public class SqlViewServiceTest
         SqlView sqlView = new SqlView( "Name", "WITH foo as (SELECT * FROM organisationunit) commit", SqlViewType.QUERY );
 
         sqlViewService.validateSqlView( sqlView, null, null );
-
     }
 
     @Test( expected = IllegalQueryException.class )
@@ -217,7 +214,6 @@ public class SqlViewServiceTest
 
         sqlViewService.validateSqlView( sqlView, null, null );
     }
-
 
     @Test( expected = IllegalQueryException.class )
     public void testValidateMissingVariables()
@@ -254,6 +250,16 @@ public class SqlViewServiceTest
         sqlViewService.validateSqlView( sqlView, null, null );
     }
 
+    @Test( expected = IllegalQueryException.class )
+    public void testGetGridValidationFailure()
+    {
+        SqlView sqlView = new SqlView( "Name", "select * from dataelement; delete from dataelement", SqlViewType.QUERY );
+        
+        sqlViewService.saveSqlView( sqlView );
+        
+        sqlViewService.getSqlViewGrid( sqlView, null, null, null, null );
+    }
+    
     @Test
     public void testValidateSuccessA()
     {
@@ -288,6 +294,4 @@ public class SqlViewServiceTest
 
         sqlViewService.validateSqlView( sqlView, null, null );
     }
-
-
 }
