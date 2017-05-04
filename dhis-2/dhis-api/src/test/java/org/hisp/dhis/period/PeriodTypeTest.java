418c483c28332c2efa4226f0965208b813e7f72b
Morten Olav Hansen <mortenoh@gmail.com>
2017-05-04 15:03:11 +0700

    Minor fix

--- dhis-2/dhis-api/src/test/java/org/hisp/dhis/period/PeriodTypeTest.java
+++ dhis-2/dhis-api/src/test/java/org/hisp/dhis/period/PeriodTypeTest.java
@@ -38,7 +38,7 @@ public class PeriodTypeTest
     public void testGetByIndex()
     {
         assertNull( PeriodType.getByIndex( -1 ) );
-        assertEquals( new YearlyPeriodType(), PeriodType.getByIndex( 8 ) );
+        assertEquals( new YearlyPeriodType(), PeriodType.getByIndex( 12 ) );
         assertNull( PeriodType.getByIndex( 999 ) );
     }
 
