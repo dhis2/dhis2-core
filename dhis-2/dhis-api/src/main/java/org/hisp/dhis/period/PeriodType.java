a4860e8e8f58c9aaaafdd764e7e21fb087ac16cb
Morten Olav Hansen <mortenoh@gmail.com>
2017-05-05 12:59:48 +0700

    adds period type controller

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/period/PeriodType.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/period/PeriodType.java
@@ -28,6 +28,8 @@ package org.hisp.dhis.period;
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
@@ -222,6 +224,8 @@ public abstract class PeriodType
      *
      * @return a unique name for the PeriodType. E.g. "Monthly".
      */
+    @JsonProperty
+    @JacksonXmlProperty( namespace = DxfNamespaces.DXF_2_0 )
     public abstract String getName();
 
     /**
@@ -287,6 +291,8 @@ public abstract class PeriodType
         return toIsoPeriod( dateTimeUnit, dateTimeUnit );
     }
 
+    @JsonProperty
+    @JacksonXmlProperty( namespace = DxfNamespaces.DXF_2_0 )
     public abstract String getIso8601Duration();
 
     public abstract Period createPeriod( DateTimeUnit dateTimeUnit, org.hisp.dhis.calendar.Calendar calendar );
@@ -297,6 +303,8 @@ public abstract class PeriodType
      *
      * @return the frequency order.
      */
+    @JsonProperty
+    @JacksonXmlProperty( namespace = DxfNamespaces.DXF_2_0 )
     public abstract int getFrequencyOrder();
 
     /**
@@ -523,6 +531,8 @@ public abstract class PeriodType
      *
      * @return the iso8601 format.
      */
+    @JsonProperty
+    @JacksonXmlProperty( namespace = DxfNamespaces.DXF_2_0 )
     public abstract String getIsoFormat();
 
     // -------------------------------------------------------------------------
