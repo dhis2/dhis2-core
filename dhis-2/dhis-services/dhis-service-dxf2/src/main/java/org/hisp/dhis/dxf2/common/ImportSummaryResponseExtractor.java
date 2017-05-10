9dfcd5a810cf59e93d28869bb79fcf3c1b2616a2
Lars Helge Øverland <lars@dhis2.org>
2017-05-10 14:34:27 +0200

    Cleanup, removed unused class (#809)

--- dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/common/ImportSummaryResponseExtractor.java
+++ dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/common/ImportSummaryResponseExtractor.java
@@ -30,9 +30,7 @@ package org.hisp.dhis.dxf2.common;
 
 import org.hisp.dhis.dxf2.importsummary.ImportSummary;
 import org.hisp.dhis.render.DefaultRenderService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.client.ClientHttpResponse;
-import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.ResponseExtractor;
 
 import java.io.IOException;
@@ -51,10 +49,6 @@ public class ImportSummaryResponseExtractor
     @Override
     public ImportSummary extractData( ClientHttpResponse response ) throws IOException
     {
-        ImportSummary summary = DefaultRenderService.getJsonMapper().readValue( response.getBody(), ImportSummary.class );
-
-        HttpStatus status = response.getStatusCode();
-
-        return summary;
+        return DefaultRenderService.getJsonMapper().readValue( response.getBody(), ImportSummary.class );
     }
 }
