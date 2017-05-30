3113c1183b210cf1c5acd078140bf12bfa0a09ae
Lars Helge Øverland <lars@dhis2.org>
2017-05-30 16:40:46 +0200

    Replaced ExternalFileResourceTokenGenerator with CodeGenerator

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/common/CodeGenerator.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/common/CodeGenerator.java
@@ -1,5 +1,7 @@
 package org.hisp.dhis.common;
 
+import java.security.SecureRandom;
+
 /*
  * Copyright (c) 2004-2017, University of Oslo
  * All rights reserved.
@@ -31,6 +33,8 @@ package org.hisp.dhis.common;
 import java.util.Random;
 import java.util.regex.Pattern;
 
+import org.springframework.util.Base64Utils;
+
 /**
  * @author bobj
  */
@@ -45,6 +49,11 @@ public class CodeGenerator
     public static final int CODESIZE = 11;
 
     private static final Pattern CODE_PATTERN = Pattern.compile( "^[a-zA-Z]{1}[a-zA-Z0-9]{10}$" );
+
+    /**
+     * 192 bit, must be dividable by 3 to avoid padding "=".
+     */
+    private static final int URL_RANDOM_TOKEN_LENGTH = 24;
     
     /**
      * Generates a UID according to the following rules:
@@ -93,5 +102,19 @@ public class CodeGenerator
     public static boolean isValidUid( String code )
     {
         return code != null && CODE_PATTERN.matcher( code ).matches();
+    }
+    
+    /**
+     * Generates a random 32 character token to be used in URLs.
+     * 
+     * @return a token.
+     */
+    public static String getRandomUrlToken()
+    {
+        SecureRandom sr = new SecureRandom();
+        byte[] tokenBytes = new byte[ URL_RANDOM_TOKEN_LENGTH ];
+        sr.nextBytes( tokenBytes );
+
+        return Base64Utils.encodeToUrlSafeString( tokenBytes );
     }
 }
