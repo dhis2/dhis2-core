5428f32ada506d22a19fbbd49e3629d33e5768d2
Lars Helge Øverland <lars@dhis2.org>
2017-06-10 16:44:09 +0300

    File resource utils

---
+++ dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/utils/FileResourceUtils.java
@@ -0,0 +1,80 @@
+package org.hisp.dhis.webapi.utils;
+
+/*
+ * Copyright (c) 2004-2017, University of Oslo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * Neither the name of the HISP project nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import org.springframework.util.InvalidMimeTypeException;
+import org.springframework.util.MimeTypeUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * @author Lars Helge Overland
+ */
+public class FileResourceUtils
+{
+    /**
+     * Transfers the given multipart file content to a local temporary file.
+     *  
+     * @param multipartFile the multipart file.
+     * @return a temporary local file.
+     * @throws IOException if the file content could not be transferred.
+     */
+    public static File toTempFile( MultipartFile multipartFile )
+        throws IOException
+    {
+        File tmpFile = Files.createTempFile( "org.hisp.dhis", ".tmp" ).toFile();
+        tmpFile.deleteOnExit();
+        multipartFile.transferTo( tmpFile );
+        return tmpFile;
+    }
+
+    /**
+     * Indicates whether the content type represented by the given string
+     * is a valid, known content type.
+     * 
+     * @param contentType the content type string.
+     * @return true if the content is valid, false if not.
+     */
+    public static boolean isValidContentType( String contentType )
+    {
+        try
+        {
+            MimeTypeUtils.parseMimeType( contentType );
+        }
+        catch ( InvalidMimeTypeException ignored )
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
