0c465b8e86e981a9bc08d3bb7f384d76ef4a54d0
Morten Olav Hansen <mortenoh@gmail.com>
2017-06-14 14:33:55 +0700

    Js updates (#950)
    * updates jquery to 3x (including migrate), updates to latest jquery blockUI, updatest to latest jqueryUI
    
    * update $.expr[] to use $.expr.pseudos in dhis2.utils.js
    
    * update $.expr[] to use $.expr.pseudos in dhis2.utils.js
    
    * remove dataType from $.ajax calls for file resource

--- dhis-2/dhis-web/dhis-web-dataentry/src/main/webapp/dhis-web-dataentry/javascript/entry.fileresource.js
+++ dhis-2/dhis-web/dhis-web-dataentry/src/main/webapp/dhis-web-dataentry/javascript/entry.fileresource.js
@@ -40,7 +40,6 @@
             $.ajax( {
                 url: '../api/dataValues',
                 type: 'POST',
-                dataType: 'json',
                 data: postData,
                 success: function() {
                     $fileinfoName.text( '' );
