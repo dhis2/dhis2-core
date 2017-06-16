09d50c42cc5c05e8fdfaa3f09be5d81cce2148e7
Morten Olav Hansen <mortenoh@gmail.com>
2017-06-16 16:40:34 +0700

    minor updates, remove color from colorservice

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/color/DefaultColorService.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/color/DefaultColorService.java
@@ -38,13 +38,6 @@ import org.springframework.transaction.annotation.Transactional;
 public class DefaultColorService
     implements ColorService
 {
-    private GenericIdentifiableObjectStore<Color> colorStore;
-    
-    public void setColorStore( GenericIdentifiableObjectStore<Color> colorStore )
-    {
-        this.colorStore = colorStore;
-    }
-
     private GenericIdentifiableObjectStore<ColorSet> colorSetStore;
 
     public void setColorSetStore( GenericIdentifiableObjectStore<ColorSet> colorSetStore )
@@ -56,11 +49,6 @@ public class DefaultColorService
     // Dependencies
     // -------------------------------------------------------------------------
 
-    public Color getColor( String uid )
-    {
-        return colorStore.getByUid( uid );
-    }
-    
     public ColorSet getColorSet( String uid )
     {
         return colorSetStore.getByUid( uid );
