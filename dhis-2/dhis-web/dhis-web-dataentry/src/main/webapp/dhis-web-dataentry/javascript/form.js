5207bdb93440f056e7827acdfdb2ed8a7c27097d
Morten Olav Hansen <mortenoh@gmail.com>
2017-06-20 12:26:06 +0700

    include code/displayName for options in comment optionset search (#983)

--- dhis-2/dhis-web/dhis-web-dataentry/src/main/webapp/dhis-web-dataentry/javascript/form.js
+++ dhis-2/dhis-web/dhis-web-dataentry/src/main/webapp/dhis-web-dataentry/javascript/form.js
@@ -3154,7 +3154,7 @@ dhis2.de.searchOptionSet = function( uid, query, success )
 dhis2.de.getOptions = function( uid, query, success ) 
 {
     return $.ajax( {
-        url: '../api/optionSets/' + uid + '.json?links=false&q=' + query,
+        url: '../api/optionSets/' + uid + '.json?fields=:all,options[:all]&links=false&q=' + query,
         dataType: "json",
         cache: false,
         type: 'GET',
