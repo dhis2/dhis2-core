228a96969499d71d8d6920eb2e84d0988e86c079
zubaira <zubair@dhis2.org>
2017-07-20 12:56:35 +0200

    added Deletion Handler

--- dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/event/DataSetNotificationTemplateController.java
+++ dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/event/DataSetNotificationTemplateController.java
@@ -28,29 +28,14 @@ package org.hisp.dhis.webapi.controller.event;
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-import com.google.common.collect.Sets;
-import org.hisp.dhis.common.DeliveryChannel;
+
 import org.hisp.dhis.common.DhisApiVersion;
-import org.hisp.dhis.dataset.DataSet;
-import org.hisp.dhis.dataset.DataSetService;
-import org.hisp.dhis.dataset.notifications.DataSetNotificationRecipient;
 import org.hisp.dhis.dataset.notifications.DataSetNotificationTemplate;
-import org.hisp.dhis.dxf2.datavalueset.DataValueSetService;
-import org.hisp.dhis.program.notification.NotificationTrigger;
-import org.hisp.dhis.render.RenderService;
 import org.hisp.dhis.schema.descriptors.DataSetNotificationTemplateSchemaDescriptor;
-import org.hisp.dhis.user.UserGroupService;
 import org.hisp.dhis.webapi.controller.AbstractCrudController;
 import org.hisp.dhis.webapi.mvc.annotation.ApiVersion;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 /**
  * Created by zubair on 02.07.17.
@@ -61,40 +46,4 @@ import java.io.IOException;
 public class DataSetNotificationTemplateController extends
     AbstractCrudController<DataSetNotificationTemplate>
 {
-    @Autowired
-    private UserGroupService userGroupService;
-
-    @Autowired
-    private DataSetService dataSetService;
-
-    @Autowired
-    private DataValueSetService dataValueSetService;
-
-    @Autowired
-    private RenderService renderService;
-
-    @PreAuthorize( "hasRole('ALL')" )
-    @RequestMapping( value = "/get1", method = RequestMethod.GET, produces = "application/json" )
-    public void getDataSetNotificationTemplate(HttpServletRequest request, HttpServletResponse response )
-            throws IOException
-    {
-
-        DataSet dataSet = new DataSet();
-        dataSet.setAutoFields();
-
-
-
-        DataSetNotificationTemplate template = new DataSetNotificationTemplate();
-        template.setDeliveryChannels( Sets.newHashSet(DeliveryChannel.SMS ) );
-        template.setMessageTemplate( " message body" );
-        template.setSubjectTemplate( "subject body" );
-        template.setNotificationTrigger( NotificationTrigger.COMPLETION );
-        template.setNotificationRecipient( DataSetNotificationRecipient.ORGANISATION_UNIT_CONTACT );
-        template.setRecipientUserGroup( userGroupService.getAllUserGroups().get( 1 ) );
-        template.setDataSets( Sets.newHashSet( dataSet ) );
-
-        renderService.toJson( response.getOutputStream(), template );
-
-
-    }
 }
