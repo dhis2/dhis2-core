34872531a87342622441c31b85878e9262fe70bd
zubaira <zubair@dhis2.org>
2017-07-21 04:35:04 +0200

    added scheduled task

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/setting/SettingKey.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/setting/SettingKey.java
@@ -150,6 +150,7 @@ public enum SettingKey
     METADATA_SYNC_CRON( "metaDataSyncCron", String.class ),
     DATA_SYNC_CRON( "dataSyncCron", String.class ),
     LAST_SUCCESSFUL_SCHEDULED_PROGRAM_NOTIFICATIONS( "keyLastSuccessfulScheduledProgramNotifications", Date.class ),
+    LAST_SUCCESSFUL_SCHEDULED_DATASET_NOTIFICATIONS( "keyLastSuccessfulScheduledDataSetNotifications", Date.class ),
     REMOTE_METADATA_VERSION( "keyRemoteMetadataVersion", String.class ),
     SYSTEM_METADATA_VERSION( "keySystemMetadataVersion", String.class ),
     STOP_METADATA_SYNC( "keyStopMetadataSync", Boolean.FALSE, Boolean.class );
