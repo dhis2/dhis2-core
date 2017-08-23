38898c2c274b73d41cb31dd15c70d75a9b0bb59c
Henning Håkonsen <henninghaakonsen@me.com>
2017-08-23 10:25:25 +0200

    Initial commit scheduler. dhis2-2082

---
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/scheduling/Job.java
@@ -0,0 +1,104 @@
+package org.hisp.dhis.scheduling;
+
+import java.util.Date;
+
+/**
+ * @author Henning Håkonsen
+ */
+public class Job
+{
+    private String name;
+    private JobType jobType;
+    private String cronExpression;
+    private boolean activate;
+    private Date startTime;
+    private Date endTime;
+    private JobStatus status;
+
+    public Job(String name, JobType jobType, String cronExpression) {
+        this.name = name;
+        this.jobType = jobType;
+        this.cronExpression = cronExpression;
+
+        this.activate = true;
+        this.startTime = null;
+        this.endTime = null;
+        this.status = JobStatus.SCHEDULED;
+    }
+
+    public String toString()
+    {
+        return "Name: " + name + ", job type: " + jobType.name() + ", cronExpression: " + cronExpression + ", status: " + status;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public JobType getJobType()
+    {
+        return jobType;
+    }
+
+    public String getCronExpression()
+    {
+        return cronExpression;
+    }
+
+    public boolean isActivate()
+    {
+        return activate;
+    }
+
+    public Date getStartTime()
+    {
+        return startTime;
+    }
+
+    public Date getEndTime()
+    {
+        return endTime;
+    }
+
+    public JobStatus getStatus()
+    {
+        return status;
+    }
+
+    public void setName( String name )
+    {
+        this.name = name;
+    }
+
+    public void setJobType( JobType jobType )
+    {
+        this.jobType = jobType;
+    }
+
+    public void setCronExpression( String cronExpression )
+    {
+        this.cronExpression = cronExpression;
+    }
+
+    public void setActivate( boolean activate )
+    {
+        this.activate = activate;
+    }
+
+    public void setStartTime( Date startTime )
+    {
+        this.startTime = startTime;
+    }
+
+    public void setEndTime( Date endTime )
+    {
+        this.endTime = endTime;
+    }
+
+    public void setStatus( JobStatus status )
+    {
+        this.status = status;
+    }
+}
+
---
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/scheduling/JobStatus.java
@@ -0,0 +1,10 @@
+package org.hisp.dhis.scheduling;
+
+/**
+ * Created by henninghakonsen on 23/08/2017.
+ * Project: dhis-2.
+ */
+public enum JobStatus
+{
+    SCHEDULED, RUNNING, COMPLETED, FAILED;
+}
