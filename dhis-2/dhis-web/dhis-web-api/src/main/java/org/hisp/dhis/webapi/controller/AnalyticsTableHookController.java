117d5f5ddfe299607000f5bf9e3a83272af8188f
Lars Helge Øverland <lars@dhis2.org>
2017-12-06 15:41:52 +0100

    Minor

--- dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/AnalyticsTableHookController.java
+++ dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/AnalyticsTableHookController.java
@@ -30,10 +30,15 @@ package org.hisp.dhis.webapi.controller;
  */
 
 import org.hisp.dhis.analytics.AnalyticsTableHook;
+import org.hisp.dhis.schema.descriptors.AnalyticsTableHookSchemaDescriptor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * @author Morten Olav Hansen <mortenoh@gmail.com>
+ * @author Lars Helge Overland
  */
+@Controller
+@RequestMapping( value = AnalyticsTableHookSchemaDescriptor.API_ENDPOINT )
 public class AnalyticsTableHookController
     extends AbstractCrudController<AnalyticsTableHook>
 {
