d928b99166aa3a813ea3ff5d62523b8d148ad604
Morten Olav Hansen <mortenoh@gmail.com>
2017-12-14 11:56:09 +0700

    verify access on publicAccess and UA/UGA in AclService

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/security/acl/DefaultAclService.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/security/acl/DefaultAclService.java
@@ -499,6 +499,41 @@ public class DefaultAclService implements AclService
 
         Schema schema = schemaService.getSchema( object.getClass() );
 
+        if ( !schema.isDataShareable() )
+        {
+            ErrorReport errorReport = null;
+
+            if ( hasDataSharing( object.getPublicAccess() ) )
+            {
+                errorReport = new ErrorReport( object.getClass(), ErrorCode.E3011, object.getClass() );
+            }
+            else
+            {
+                for ( UserAccess userAccess : object.getUserAccesses() )
+                {
+                    if ( hasDataSharing( userAccess.getAccess() ) )
+                    {
+                        errorReport = new ErrorReport( object.getClass(), ErrorCode.E3011, object.getClass() );
+                        break;
+                    }
+                }
+
+                for ( UserGroupAccess userGroupAccess : object.getUserGroupAccesses() )
+                {
+                    if ( hasDataSharing( userGroupAccess.getAccess() ) )
+                    {
+                        errorReport = new ErrorReport( object.getClass(), ErrorCode.E3011, object.getClass() );
+                        break;
+                    }
+                }
+            }
+
+            if ( errorReport != null )
+            {
+                errorReports.add( errorReport );
+            }
+        }
+
         if ( schema.isImplicitPrivateAuthority() && !checkUser( user, object ) )
         {
             errorReports.add( new ErrorReport( object.getClass(), ErrorCode.E3001, user.getUsername(), object.getClass() ) );
@@ -637,5 +672,11 @@ public class DefaultAclService implements AclService
         }
 
         return false;
+    }
+
+    private boolean hasDataSharing( String access )
+    {
+        return AccessStringHelper.isEnabled( access, AccessStringHelper.Permission.DATA_READ )
+            || AccessStringHelper.isEnabled( access, AccessStringHelper.Permission.DATA_WRITE );
     }
 }
