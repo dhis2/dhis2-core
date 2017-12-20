6ef2099a0ec5a7fa876d821942391828daa9cc48
Henning Håkonsen <henninghaakonsen@me.com>
2017-12-20 13:26:11 +0100

    Fixed order for parameters to scheduler

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/scheduling/DefaultJobConfigurationService.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/scheduling/DefaultJobConfigurationService.java
@@ -88,7 +88,7 @@ public class DefaultJobConfigurationService
 
         for ( JobType jobType : values() )
         {
-            Map<String, Property> jobParameters = Maps.newHashMap();
+            Map<String, Property> jobParameters = new LinkedHashMap<>( );
 
             if ( !jobType.isConfigurable() )
             {
