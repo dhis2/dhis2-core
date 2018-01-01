99bd84a4f8342aac2c8bdf5ce7eb16072cdaff02
Adrian Quintana <adrianquintana@gmail.com>
2018-01-01 15:38:55 +0000

    change user properties

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/interpretation/Mention.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/interpretation/Mention.java
@@ -5,6 +5,13 @@ import java.io.Serializable;
 import java.util.Date;
 
 import org.hisp.dhis.common.DxfNamespaces;
+import org.hisp.dhis.common.annotation.Description;
+import org.hisp.dhis.schema.PropertyType;
+import org.hisp.dhis.schema.annotation.Property;
+import org.hisp.dhis.schema.annotation.PropertyRange;
+import org.hisp.dhis.schema.annotation.Property.Value;
+import org.hisp.dhis.user.User;
+import org.hisp.dhis.user.UserCredentials;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
@@ -14,23 +21,50 @@ import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 public class Mention implements Serializable
 {
 
-    private int userId;
+    private String username;
+    
+    private String userUid;
     
     private Date created;
 
+    @JsonProperty( value = "userId" )
+    @JacksonXmlProperty( localName = "userId")
+    public String getUserUid()
+    {
+        return userUid;
+    }
 
+    public void setUserUid( String userUid )
+    {
+        this.userUid = userUid;
+    }
+    
+    
     @JsonProperty
     @JacksonXmlProperty( namespace = DxfNamespaces.DXF_2_0 )
-    public int getUserId()
+    public String getUsername()
     {
-        return userId;
+        return username;
     }
 
-    public void setUserId( int userId )
+    public void setUsername( String username )
     {
-        this.userId = userId;
+        this.username = username;
     }
 
+//    @JsonProperty
+//    @JacksonXmlProperty( namespace = DxfNamespaces.DXF_2_0 )
+//    public User getUser() {
+//        User user =  new User();
+//        user.setUid( this.getUserId() );
+//        UserCredentials userCredentials = new UserCredentials();
+//        userCredentials.setUsername( this.getUsername() );
+//        user.setUserCredentials( userCredentials );
+//        
+//        return user;
+//    }
+
+    
 
     @JsonProperty
     @JacksonXmlProperty( namespace = DxfNamespaces.DXF_2_0 )
@@ -43,6 +77,9 @@ public class Mention implements Serializable
     {
         this.created = created;
     }
+
+    
+    
     
     
     
