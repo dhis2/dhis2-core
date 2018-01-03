a27b8a4b0388ef23a61d9bbcad06ae57bb51e3a8
Morten Olav Hansen <mortenoh@gmail.com>
2018-01-03 13:34:40 +0700

    update copyright to 2018 (#1433)

--- dhis-2/dhis-services/dhis-service-dxf2/src/test/java/org/hisp/dhis/dxf2/metadata/jobs/MetadataRetryContextTest.java
+++ dhis-2/dhis-services/dhis-service-dxf2/src/test/java/org/hisp/dhis/dxf2/metadata/jobs/MetadataRetryContextTest.java
@@ -1,5 +1,7 @@
+package org.hisp.dhis.dxf2.metadata.jobs;
+
 /*
- * Copyright (c) 2004-2016, University of Oslo
+ * Copyright (c) 2004-2018, University of Oslo
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -25,8 +27,6 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
-package org.hisp.dhis.dxf2.metadata.jobs;
 
 import org.hisp.dhis.DhisSpringTest;
 import org.hisp.dhis.feedback.Status;
