61fc3a440fcba79b756c000e2325f9ee270ad27a
zubaira <zubair@dhis2.org>
2018-01-03 12:08:44 +0100

    minor

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/programrule/engine/ProgramRuleEngine.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/programrule/engine/ProgramRuleEngine.java
@@ -82,7 +82,7 @@ public class ProgramRuleEngine
         {
             ruleEffects = ruleEngine.evaluate( ruleEnrollment  ).call();
 
-            ruleEffects.stream().map( ruleEffect -> ruleEffect.ruleAction() )
+            ruleEffects.stream().map( RuleEffect::ruleAction )
                 .forEach( action -> log.info( String.format( "RuleEngine triggered with result: %s", action.toString() ) ) );
         }
         catch ( Exception e)
@@ -118,7 +118,7 @@ public class ProgramRuleEngine
         {
             ruleEffects = ruleEngine.evaluate( programRuleEntityMapperService.toMappedRuleEvent( programStageInstance )  ).call();
 
-            ruleEffects.stream().map( ruleEffect -> ruleEffect.ruleAction() )
+            ruleEffects.stream().map( RuleEffect::ruleAction )
                 .forEach( action -> log.info( String.format( "RuleEngine triggered with result: %s", action.toString() ) ) );
         }
         catch ( Exception e )
