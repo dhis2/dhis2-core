6223b823bdea97d8a372908775bc68f5ac2f3d0f
Lars Helge Øverland <lars@dhis2.org>
2018-01-18 17:01:05 +0100

    Text type upgrade merge
    (cherry picked from commit 98257970a69dfeebe2b26de3ccd0c7af3b22195d)

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/startup/TableAlteror.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/startup/TableAlteror.java
@@ -997,6 +997,8 @@ public class TableAlteror
         //TODO: remove - not needed in release 2.26.
         executeSql( "update programindicator set analyticstype = programindicatoranalyticstype" );
         executeSql( "alter table programindicator drop programindicatoranalyticstype" );
+       
+        updateDimensionFilterToText();
 
         log.info( "Tables updated" );
     }
@@ -1699,5 +1701,12 @@ public class TableAlteror
 
         sql = " drop table maplegendsetmaplegend";
         executeSql( sql );
+    }
+    
+    private void updateDimensionFilterToText()
+    {
+        executeSql( "alter table trackedentityattributedimension alter column \"filter\" type text;" );
+        executeSql( "alter table trackedentitydataelementdimension alter column \"filter\" type text;" );
+        executeSql( "alter table trackedentityprogramindicatordimension alter column \"filter\" type text;" );
     }
 }
