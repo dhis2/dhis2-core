88de6a3e3dcb237fc1259388668e8b2428a475d6
Joao <joao.antunes@present-technologies.com>
2018-01-19 15:05:15 +0000

    dataSummary endpoint for DataStatistics at Data Administration App

--- dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/datasummary/DataSummaryController.java
+++ dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/datasummary/DataSummaryController.java
@@ -30,24 +30,19 @@ package org.hisp.dhis.webapi.controller.datasummary;
 
 import datasummary.DataSummary;
 import org.hisp.dhis.common.DhisApiVersion;
-import org.hisp.dhis.datavalue.DataValueService;
-import org.hisp.dhis.program.ProgramStageInstanceService;
-import org.hisp.dhis.statistics.StatisticsProvider;
-import org.hisp.dhis.user.UserInvitationStatus;
-import org.hisp.dhis.user.UserQueryParams;
-import org.hisp.dhis.user.UserService;
+import org.hisp.dhis.datastatistics.DataStatisticsService;
 import org.hisp.dhis.webapi.mvc.annotation.ApiVersion;
-import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-
+/**
+ * dataSummary endpoint to access System Statistics
+ *
+ * @author Joao Antunes
+ */
 @Controller
 @RequestMapping( value = DataSummaryController.RESOURCE_PATH )
 @ApiVersion( { DhisApiVersion.DEFAULT, DhisApiVersion.ALL } )
@@ -56,74 +51,12 @@ public class DataSummaryController
     public static final String RESOURCE_PATH = "/dataSummary";
 
     @Autowired
-    private StatisticsProvider statisticsProvider;
-
-    @Autowired
-    private UserService userService;
-
-    @Autowired
-    private DataValueService dataValueService;
-
-    @Autowired
-    private ProgramStageInstanceService programStageInstanceService;
+    private DataStatisticsService dataStatisticsService;
 
     @GetMapping
     public @ResponseBody
     DataSummary getStatistics()
     {
-        DataSummary statistics = new DataSummary();
-
-        /* database object counts */
-        Map<String, Integer> objectCounts = new HashMap<>(  );
-        statisticsProvider.getObjectCounts().forEach( (object, count) -> objectCounts.put( object.getValue(), count ));
-        statistics.setObjectCounts( objectCounts );
-
-        /* active users count */
-        Date lastHour = new DateTime().minusHours( 1 ).toDate();
-
-        Map<Integer, Integer> activeUsers = new HashMap<>(  );
-
-        activeUsers.put( 0,  userService.getActiveUsersCount( lastHour ));
-        activeUsers.put( 1,  userService.getActiveUsersCount( 0 ));
-        activeUsers.put( 2,  userService.getActiveUsersCount( 1 ));
-        activeUsers.put( 7,  userService.getActiveUsersCount( 7 ));
-        activeUsers.put( 30,  userService.getActiveUsersCount( 30 ));
-
-        statistics.setActiveUsers( activeUsers );
-
-        /* user invitations count */
-        Map<String, Integer> userInvitations = new HashMap<>(  );
-
-        UserQueryParams inviteAll = new UserQueryParams();
-        inviteAll.setInvitationStatus( UserInvitationStatus.ALL );
-        userInvitations.put( UserInvitationStatus.ALL.getValue(),  userService.getUserCount( inviteAll ) );
-
-        UserQueryParams inviteExpired = new UserQueryParams();
-        inviteExpired.setInvitationStatus( UserInvitationStatus.EXPIRED );
-        userInvitations.put( UserInvitationStatus.EXPIRED.getValue(),  userService.getUserCount( inviteExpired ) );
-
-        statistics.setUserInvitations( userInvitations );
-
-        /* data value count */
-        Map<Integer, Integer> dataValueCount = new HashMap<>(  );
-
-        dataValueCount.put( 0, dataValueService.getDataValueCount( 0 ));
-        dataValueCount.put( 1, dataValueService.getDataValueCount( 1 ));
-        dataValueCount.put( 7, dataValueService.getDataValueCount( 7 ));
-        dataValueCount.put( 30, dataValueService.getDataValueCount( 30 ));
-
-        statistics.setDataValueCount( dataValueCount );
-
-        /* event count */
-        Map<Integer, Long> eventCount = new HashMap<>(  );
-
-        eventCount.put( 0, programStageInstanceService.getProgramStageInstanceCount( 0 ) );
-        eventCount.put( 1, programStageInstanceService.getProgramStageInstanceCount( 1 ) );
-        eventCount.put( 7, programStageInstanceService.getProgramStageInstanceCount( 7 ) );
-        eventCount.put( 30, programStageInstanceService.getProgramStageInstanceCount( 30 ) );
-
-        statistics.setEventCount( eventCount );
-
-        return statistics;
+        return dataStatisticsService.getSystemStatisticsSummary();
     }
 }
