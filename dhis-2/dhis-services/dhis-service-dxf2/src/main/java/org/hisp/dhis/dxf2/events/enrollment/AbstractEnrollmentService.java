83acd86bc48902d89f69b8e587454850315b6145
Morten Olav Hansen <mortenoh@gmail.com>
2018-01-23 11:17:21 +0700

    speedups for EnrollmentService

--- dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/events/enrollment/AbstractEnrollmentService.java
+++ dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/events/enrollment/AbstractEnrollmentService.java
@@ -66,6 +66,10 @@ import org.hisp.dhis.program.ProgramStageInstance;
 import org.hisp.dhis.program.ProgramStageInstanceService;
 import org.hisp.dhis.program.ProgramStatus;
 import org.hisp.dhis.program.ProgramTrackedEntityAttribute;
+import org.hisp.dhis.query.Query;
+import org.hisp.dhis.query.QueryService;
+import org.hisp.dhis.query.Restrictions;
+import org.hisp.dhis.schema.SchemaService;
 import org.hisp.dhis.system.callable.IdentifiableObjectCallable;
 import org.hisp.dhis.system.util.DateUtils;
 import org.hisp.dhis.trackedentity.TrackedEntityAttribute;
@@ -81,12 +85,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @author Morten Olav Hansen <mortenoh@gmail.com>
@@ -139,6 +145,12 @@ public abstract class AbstractEnrollmentService
     @Autowired
     protected TrackerAccessManager trackerAccessManager;
 
+    @Autowired
+    protected SchemaService schemaService;
+
+    @Autowired
+    protected QueryService queryService;
+
     private CachingMap<String, OrganisationUnit> organisationUnitCache = new CachingMap<>();
 
     private CachingMap<String, Program> programCache = new CachingMap<>();
@@ -175,10 +187,7 @@ public abstract class AbstractEnrollmentService
         }
 
         List<ProgramInstance> programInstances = programInstanceService.getProgramInstances( params );
-
-        List listEnrollments = getEnrollments( programInstances );
-
-        enrollments.setEnrollments( listEnrollments );
+        enrollments.setEnrollments( getEnrollments( programInstances ) );
 
         return enrollments;
     }
@@ -314,26 +323,56 @@ public abstract class AbstractEnrollmentService
     @Override
     public ImportSummaries addEnrollments( List<Enrollment> enrollments, ImportOptions importOptions )
     {
-        User user = currentUserService.getCurrentUser();
-
         if ( importOptions == null )
         {
             importOptions = new ImportOptions();
         }
 
+        User user = currentUserService.getCurrentUser();
+        List<List<Enrollment>> partitions = Lists.partition( enrollments, FLUSH_FREQUENCY );
+
         ImportSummaries importSummaries = new ImportSummaries();
-        int counter = 0;
 
-        for ( Enrollment enrollment : enrollments )
+        for ( List<Enrollment> _enrollments : partitions )
         {
-            importSummaries.addImportSummary( addEnrollment( enrollment, importOptions, user ) );
+            // prepare caches
+            Collection<String> orgUnits = _enrollments.stream().map( Enrollment::getOrgUnit ).collect( Collectors.toSet() );
 
-            if ( counter % FLUSH_FREQUENCY == 0 )
+            if ( !orgUnits.isEmpty() )
             {
-                clearSession();
+                Query query = Query.from( schemaService.getDynamicSchema( OrganisationUnit.class ) );
+                query.setUser( user );
+                query.add( Restrictions.in( "id", orgUnits ) );
+                queryService.query( query ).forEach( ou -> organisationUnitCache.put( ou.getUid(), (OrganisationUnit) ou ) );
             }
 
-            counter++;
+            Collection<String> programs = _enrollments.stream().map( Enrollment::getProgram ).collect( Collectors.toSet() );
+
+            if ( !programs.isEmpty() )
+            {
+                Query query = Query.from( schemaService.getDynamicSchema( Program.class ) );
+                query.setUser( user );
+                query.add( Restrictions.in( "id", programs ) );
+                queryService.query( query ).forEach( pr -> programCache.put( pr.getUid(), (Program) pr ) );
+            }
+
+            Collection<String> trackedEntityAttributes = new HashSet<>();
+            _enrollments.forEach( e -> e.getAttributes().forEach( at -> trackedEntityAttributes.add( at.getAttribute() ) ) );
+
+            if ( !trackedEntityAttributes.isEmpty() )
+            {
+                Query query = Query.from( schemaService.getDynamicSchema( TrackedEntityAttribute.class ) );
+                query.setUser( user );
+                query.add( Restrictions.in( "id", trackedEntityAttributes ) );
+                queryService.query( query ).forEach( tea -> trackedEntityAttributeCache.put( tea.getUid(), (TrackedEntityAttribute) tea ) );
+            }
+
+            for ( Enrollment enrollment : _enrollments )
+            {
+                importSummaries.addImportSummary( addEnrollment( enrollment, importOptions, user ) );
+            }
+
+            clearSession();
         }
 
         return importSummaries;
@@ -529,9 +568,7 @@ public abstract class AbstractEnrollmentService
             return new ImportSummary( ImportStatus.ERROR, "Enrollment ID was not valid." ).incrementIgnored();
         }
 
-        Set<ImportConflict> importConflicts = new HashSet<>();
-        importConflicts.addAll( checkAttributes( enrollment, importOptions ) );
-
+        Set<ImportConflict> importConflicts = new HashSet<>( checkAttributes( enrollment, importOptions ) );
         importSummary.setConflicts( importConflicts );
 
         if ( !importConflicts.isEmpty() )
