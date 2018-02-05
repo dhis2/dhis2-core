a041195e00ab9a25e7bc802de6db3a962e75a4a0
zubaira <zubair@dhis2.org>
2018-02-05 14:16:22 +0100

    mockito testing for ProgramRuleEngineService

--- dhis-2/dhis-services/dhis-service-core/src/test/java/org/hisp/dhis/programrule/engine/ProgramRuleEngineServiceTest.java
+++ dhis-2/dhis-services/dhis-service-core/src/test/java/org/hisp/dhis/programrule/engine/ProgramRuleEngineServiceTest.java
@@ -29,8 +29,14 @@ package org.hisp.dhis.programrule.engine;
  */
 
 import org.hisp.dhis.DhisConvenienceTest;
+import org.hisp.dhis.organisationunit.OrganisationUnit;
 import org.hisp.dhis.program.Program;
 import org.hisp.dhis.program.ProgramInstance;
+import org.hisp.dhis.program.ProgramStage;
+import org.hisp.dhis.program.ProgramStageInstance;
+import org.hisp.dhis.programrule.ProgramRule;
+import org.hisp.dhis.programrule.ProgramRuleAction;
+import org.hisp.dhis.programrule.ProgramRuleActionType;
 import org.hisp.dhis.rules.models.RuleAction;
 import org.hisp.dhis.rules.models.RuleActionSendMessage;
 import org.hisp.dhis.rules.models.RuleEffect;
@@ -38,18 +44,19 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import org.mockito.Matchers;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.runners.MockitoJUnitRunner;
-import org.springframework.security.access.method.P;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyObject;
-import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
+import static org.junit.Assert.*;
+
 
 /**
  * Created by zubair@dhis2.org on 04.02.18.
@@ -63,59 +70,118 @@ public class ProgramRuleEngineServiceTest extends DhisConvenienceTest
     private ProgramRuleEngine programRuleEngine;
 
     @Mock
-    private RuleActionSendMessageImplementer actionSendMessage;
+    private RuleActionSendMessageImplementer ruleActionSendMessage;
+
+    @Spy
+    private ArrayList<RuleActionImplementer> ruleActionImplementers;
 
     @InjectMocks
     private DefaultProgramRuleEngineService service;
 
     private ProgramInstance programInstance;
 
-    private Program program;
+    private ProgramStageInstance programStageInstance;
 
-    private List<RuleAction> actions;
+    private ProgramRule programRuleA;
 
-    private List<RuleEffect> effects;
-
+    private ProgramRuleAction programRuleActionA;
 
     @Before
     public void initTest()
     {
-        actions = new ArrayList<>();
-
-        effects = new ArrayList<>();
+        List<RuleEffect> effects = new ArrayList<>();
         effects.add( RuleEffect.create( RuleActionSendMessage.create( NOTIFICATION_UID ) ) );
 
-        program = createProgram( 'A' );
-        programInstance = createProgramInstance( 'B' );
+        setUpInstances();
+
+        // fill up the spy
+        ruleActionImplementers.add( ruleActionSendMessage );
 
         // stub for programRuleEngine
+        when( programRuleEngine.evaluateEnrollment( any() ) ).thenReturn(effects);
+        when( programRuleEngine.evaluateEvent( any() ) ).thenReturn(effects);
 
-        when(
-                programRuleEngine.evaluateEnrollment( any() )
-        ).thenReturn(
-            effects
-        );
-
-        // stub for actionSendMessage
-
-        when( actionSendMessage.accept( any() ) ).thenReturn( true );
-
-        doReturn("foo").when( actionSendMessage ).implement( any( RuleAction.class ), any( ProgramInstance.class ) );
+        // stub for ruleActionSendMessage
+        when( ruleActionSendMessage.accept( any() ) ).thenReturn( true );
+        doNothing().when( ruleActionSendMessage ).implement( any( RuleAction.class ), any( ProgramInstance.class ) );
     }
 
     @Test
-    public void test_whenNoImpletableActionExist()
+    public void test_whenNoImpletableActionExist_programInstance()
     {
+        setProgramRuleActionType_ShowError();
+        List<RuleAction> actions = service.evaluate( programInstance );
 
+        assertEquals( 0, actions.size() );
     }
 
-    private ProgramInstance createProgramInstance( char c )
+    @Test
+    public void test_withImpletableActionExist_programInstance()
     {
-        ProgramInstance programInstance = new ProgramInstance();
-        programInstance.setAutoFields();
-        programInstance.setName("PI-1");
-        programInstance.setProgram( program );
+        setProgramRuleActionType_SendMessage();
+        List<RuleAction> actions = service.evaluate( programInstance );
 
-        return programInstance;
+        assertEquals( 1, actions.size() );
+
+        RuleAction action = actions.get( 0 );
+        if ( action instanceof  RuleActionSendMessage )
+        {
+            RuleActionSendMessage ruleActionSendMessage = (RuleActionSendMessage) action;
+
+            assertEquals( NOTIFICATION_UID, ruleActionSendMessage.notification() );
+        }
+    }
+
+    @Test
+    public void test_whenNoImplementableActionExist_programStageInstance()
+    {
+        setProgramRuleActionType_ShowError();
+        List<RuleAction> actions = service.evaluate( programStageInstance );
+
+        assertEquals( 0, actions.size() );
+    }
+
+    @Test
+    public void test_withImplementableActionExist_programStageInstance()
+    {
+        setProgramRuleActionType_SendMessage();
+        List<RuleAction> actions = service.evaluate( programStageInstance );
+
+        assertEquals( 1, actions.size() );
+    }
+
+    private void setUpInstances()
+    {
+        OrganisationUnit organisationUnitA = createOrganisationUnit( 'A' );
+
+        Program programA = createProgram('A', new HashSet<>(), organisationUnitA );
+        ProgramStage programStageA = createProgramStage('A', programA );
+
+        programRuleA = createProgramRule( 'R', programA );
+        programRuleActionA = createProgramRuleAction( 'T' );
+
+        programA.getProgramRules().add( programRuleA );
+
+        programInstance = new ProgramInstance();
+        programInstance.setProgram( programA );
+
+        programStageA = createProgramStage( 'S', programA );
+        programA.getProgramStages().add( programStageA );
+
+        programStageInstance = new ProgramStageInstance();
+        programStageInstance.setProgramStage( programStageA );
+        programStageInstance.setProgramInstance( programInstance );
+    }
+
+    private void setProgramRuleActionType_SendMessage()
+    {
+        programRuleActionA.setProgramRuleActionType( ProgramRuleActionType.SENDMESSAGE );
+        programRuleA.getProgramRuleActions().add( programRuleActionA );
+    }
+
+    private void setProgramRuleActionType_ShowError()
+    {
+        programRuleActionA.setProgramRuleActionType( ProgramRuleActionType.SHOWWARNING );
+        programRuleA.getProgramRuleActions().add( programRuleActionA );
     }
 }
