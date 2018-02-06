fa007b70baf926c28a27c949e2f6710f80bc2bd8
zubaira <zubair@dhis2.org>
2018-02-06 14:38:58 +0100

    removed imports

--- dhis-2/dhis-services/dhis-service-core/src/test/java/org/hisp/dhis/programrule/engine/RuleActionSendMessageImplementerTest.java
+++ dhis-2/dhis-services/dhis-service-core/src/test/java/org/hisp/dhis/programrule/engine/RuleActionSendMessageImplementerTest.java
@@ -52,9 +52,7 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Matchers;
 import org.mockito.Mock;
-import org.mockito.invocation.InvocationOnMock;
 import org.mockito.runners.MockitoJUnitRunner;
-import org.mockito.stubbing.Answer;
 
 import javax.annotation.Nonnull;
 
