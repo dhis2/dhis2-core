ee7a80de185dfb41bc270afa3b8e5f14139d632e
Stian Sandvold <stian@dhis2.org>
2018-02-15 11:29:02 +0100

    Added wildcard for RANDOM to get any of digit, uppercase and lowercase

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/textpattern/TextPatternMethodUtils.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/textpattern/TextPatternMethodUtils.java
@@ -28,9 +28,13 @@ package org.hisp.dhis.textpattern;
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * @author Stian Sandvold
@@ -41,6 +45,7 @@ public class TextPatternMethodUtils
     /**
      * Returns a random String based on the format. The format (As specified in TextPatternMethod) can contain '#' digits,
      * 'X' capital letters and 'x' lower case letters.
+     *
      * @param random a Random object to generate random numbers
      * @param format the format (as specified in TextPatternMethod)
      * @return the string generated
@@ -49,18 +54,33 @@ public class TextPatternMethodUtils
     {
         StringBuilder result = new StringBuilder();
 
+        List<Character> uppercase = IntStream.range( 0, 26 ).mapToObj( ( n ) -> (char) (n + 'A') )
+            .collect( Collectors.toList() );
+        List<Character> lowercase = IntStream.range( 0, 26 ).mapToObj( ( n ) -> (char) (n + 'A') )
+            .collect( Collectors.toList() );
+        List<Character> digits = IntStream.range( 0, 10 ).mapToObj( ( n ) -> (char) (n + '0') )
+            .collect( Collectors.toList() );
+        List<Character> all = new ArrayList<>();
+        all.addAll( uppercase );
+        all.addAll( lowercase );
+        all.addAll( digits );
+
         for ( char c : format.toCharArray() )
         {
             switch ( c )
             {
+            case '*':
+                result.append( all.get( random.nextInt( all.size() ) ) );
+                break;
             case '#':
-                result.append( random.nextInt( 10 ) );
+                result.append( digits.get( random.nextInt( digits.size() ) ) );
                 break;
             case 'X':
-                result.append( (char) (random.nextInt( 26 ) + 'A') );
+                result.append( uppercase.get( random.nextInt( uppercase.size() ) ) );
                 break;
             case 'x':
-                result.append( (char) (random.nextInt( 26 ) + 'a') );
+                result.append( lowercase.get( random.nextInt( lowercase.size() ) ) );
+                break;
             }
         }
 
@@ -74,7 +94,7 @@ public class TextPatternMethodUtils
      * Both '^' (start) and '$' end characters is present, but there is not an equal amount of '.' as characters in the text
      *
      * @param format the format defined (As specified in TextPatternMethod)
-     * @param text the text to perform the format on.
+     * @param text   the text to perform the format on.
      * @return the formatted text, or null if no match was found.
      */
     public static String formatText( String format, String text )
