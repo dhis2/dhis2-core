f5728551cadafbe20c3f1c60820875476db19b94
Mohamed Ameen <ameenmk23@gmail.com>
2018-02-26 17:35:19 +0100

    Ehcache port patch 2.27 (#1688)
    * Provision to specify remoteObjectPort for ehcache
    
    Added a new property to specify the remoteObjectPort for ehcache rmi peer listener factory
    
    * Changed to small case for config keys
    
    * Updated hibernate-cache names to reflect 2.27 state

--- dhis-2/dhis-support/dhis-support-external/src/main/java/org/hisp/dhis/external/conf/ConfigurationKey.java
+++ dhis-2/dhis-support/dhis-support-external/src/main/java/org/hisp/dhis/external/conf/ConfigurationKey.java
@@ -60,6 +60,7 @@ public enum ConfigurationKey
     META_DATA_SYNC_RETRY_TIME_FREQUENCY_MILLISEC( "metadata.sync.retry.time.frequency.millisec", "30000" ),
     CLUSTER_INSTANCE_HOSTNAME( "cluster.instance0.hostname" ),
     CLUSTER_INSTANCE_CACHE_PORT( "cluster.instance0.cache.port", "4001" ),
+    CLUSTER_INSTANCE_CACHE_REMOTE_OBJECT_PORT( "cluster.instance0.cache.remote.object.port", "0" ),
     CACHE_PROVIDER( "cache.provider", "ehcache" ),
     CACHE_SERVERS( "cache.servers", "localhost:11211" ),
     CACHE_TIME( "cache.time", "600" );
