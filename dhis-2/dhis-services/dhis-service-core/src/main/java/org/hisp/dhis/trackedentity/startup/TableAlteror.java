2fb3470749ab5f53d4cd721f2354c04a2f4bafc6
abyot <abyota@gmail.com>
2018-05-30 17:11:42 +0200

    allowAuditLog config for tracker programs

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/trackedentity/startup/TableAlteror.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/trackedentity/startup/TableAlteror.java
@@ -294,6 +294,7 @@ public class TableAlteror
         executeSql( "UPDATE trackedentitytype SET minattributesrequiredtosearch=1 where minattributesrequiredtosearch is null" );
         executeSql( "UPDATE trackedentitytype SET maxteicounttoreturn=0 where maxteicounttoreturn is null" );
         executeSql( "update trackedentitytype set allowauditlog = false where allowauditlog is null" );
+        executeSql( "update program set allowauditlog = false where allowauditlog is null" );
     }
 
     // -------------------------------------------------------------------------
