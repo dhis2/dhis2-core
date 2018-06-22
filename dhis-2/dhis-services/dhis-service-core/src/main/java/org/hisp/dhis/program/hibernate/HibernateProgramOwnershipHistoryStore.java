b3a8468c01589ffbd5cca1921f4d8e9d66bba359
Ameen <ameen@dhis2.org>
2018-06-22 17:07:13 +0200

    Minor spelling correction

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/program/hibernate/HibernateProgramOwnershipHistoryStore.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/program/hibernate/HibernateProgramOwnershipHistoryStore.java
@@ -51,7 +51,7 @@ public class HibernateProgramOwnershipHistoryStore implements ProgramOwnershipHi
     }
 
     // -------------------------------------------------------------------------
-    // ProgramTempOwnershipAuditStore implementation
+    // ProgramOwnershipHistoryStore implementation
     // -------------------------------------------------------------------------
 
     @Override
