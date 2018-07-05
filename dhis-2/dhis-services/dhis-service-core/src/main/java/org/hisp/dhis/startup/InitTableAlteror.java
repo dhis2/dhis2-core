a2d80db13e318e68bc5c1b93119e1df7807712a0
Stian Sandvold <stian@dhis2.org>
2018-07-05 14:29:20 +0200

    Added startup method for adding SQL-function, generate_uid()

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/startup/InitTableAlteror.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/startup/InitTableAlteror.java
@@ -162,6 +162,28 @@ public class InitTableAlteror
         //Remove createddate column from trackedentitycomment table
         executeSql( "UPDATE trackedentitycomment SET created = createddate WHERE created IS NOT NULL;" );
         executeSql( "ALTER TABLE trackedentitycomment DROP COLUMN createddate;" );
+
+        addGenerateUidFunction();
+    }
+
+    private void addGenerateUidFunction()
+    {
+        executeSql(
+            "create or replace function generate_uid()\n" +
+                "  returns text as\n" +
+                "$$\n" +
+                "declare\n" +
+                "  chars  text [] := '{0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z}';\n" +
+                "  result text := chars [11 + random() * (array_length(chars, 1) - 11)];\n" +
+                "begin\n" +
+                "  for i in 1..10 loop\n" +
+                "    result := result || chars [1 + random() * (array_length(chars, 1) - 1)];\n" +
+                "  end loop;\n" +
+                "  return result;\n" +
+                "end;\n" +
+                "$$\n" +
+                "language plpgsql;"
+        );
     }
 
     private void updateAndRemoveOldProgramStageInstanceCoordinates()
