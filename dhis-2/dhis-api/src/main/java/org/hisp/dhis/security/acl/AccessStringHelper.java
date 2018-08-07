607d9d7950ac05edf3d577c0edcd8e5085927cf1
David Katuscak <david@dhis2.org>
2018-08-07 09:48:39 +0200

    Code cleanup - DHIS2-4241

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/security/acl/AccessStringHelper.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/security/acl/AccessStringHelper.java
@@ -70,8 +70,9 @@ public class AccessStringHelper
 
     public static final String DEFAULT = "--------";
 
-    //This should be used only when creating a default DataElementCategoryOption, DataElementCategory, DataElementCategoryCombo and DataElementCategoryOptionCombo
+    //This should be used only when creating a default DataElementCategoryOption
     public static final String CATEGORY_OPTION_DEFAULT = "rwrw----";
+    //This should be used only when creating a default DataElementCategory, DataElementCategoryCombo and DataElementCategoryOptionCombo
     public static final String CATEGORY_NO_DATA_SHARING_DEFAULT = "rw------";
 
     public static final String READ = AccessStringHelper.newInstance()
