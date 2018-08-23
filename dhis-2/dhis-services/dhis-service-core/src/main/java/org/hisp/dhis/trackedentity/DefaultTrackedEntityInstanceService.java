2f110f390b691345a78cddeaec40bd816c21fc30
Ameen <ameen@dhis2.org>
2018-08-23 15:19:31 +0200

    Corrected condition for 0 min attributes

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/trackedentity/DefaultTrackedEntityInstanceService.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/trackedentity/DefaultTrackedEntityInstanceService.java
@@ -592,7 +592,7 @@ public class DefaultTrackedEntityInstanceService
             {
                 maxTeiLimit = params.getProgram().getMaxTeiCountToReturn();
 
-                if ( !params.hasFilters() || (params.hasFilters() && params.getFilters().size() < params.getProgram().getMinAttributesRequiredToSearch()) )
+                if ( isMinAttributesViolated( params ) )
                 {
                     throw new IllegalQueryException( "At least " + params.getProgram().getMinAttributesRequiredToSearch() + " attributes should be mentioned in the search criteria." );
                 }
@@ -613,6 +613,12 @@ public class DefaultTrackedEntityInstanceService
                 throw new IllegalQueryException( "maxteicountreached" );
             }
         }
+    }
+
+    private boolean isMinAttributesViolated( TrackedEntityInstanceQueryParams params )
+    {
+        return (!params.hasFilters() && params.getProgram().getMinAttributesRequiredToSearch() > 0)
+            || (params.hasFilters() && params.getFilters().size() < params.getProgram().getMinAttributesRequiredToSearch());
     }
 
     @Override
