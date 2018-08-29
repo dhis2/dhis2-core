5071bb0efb4c66c0e5e71784ae78bc8de001b956
David Katuscak <david@dhis2.org>
2018-08-29 13:28:54 +0200

    Code cleanup - DHIS2-4467

--- dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/datavalueset/SpringDataValueSetStore.java
+++ dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/datavalueset/SpringDataValueSetStore.java
@@ -227,38 +227,38 @@ public class SpringDataValueSetStore
 
         String deSql = idScheme.getDataElementIdScheme().isAttribute() ?
             "coalesce((" +
-                "select av.value as deid from attributevalue av " +
-                "inner join dataelementattributevalues deav on av.attributevalueid=deav.attributevalueid " +
-                "inner join attribute at on av.attributeid=at.attributeid and at.uid='" + idScheme.getDataElementIdScheme().getAttribute() + "' " +
-                "where dv.dataelementid=deav.dataelementid " +
-                "limit 1), de.uid) as deid" :
+            "select av.value as deid from attributevalue av " +
+            "inner join dataelementattributevalues deav on av.attributevalueid=deav.attributevalueid " +
+            "inner join attribute at on av.attributeid=at.attributeid and at.uid='" + idScheme.getDataElementIdScheme().getAttribute() + "' " +
+            "where dv.dataelementid=deav.dataelementid " +
+            "limit 1), de.uid) as deid" :
             "de." + deScheme + " as deid";
 
         String ouSql = idScheme.getOrgUnitIdScheme().isAttribute() ?
             "coalesce((" +
-                "select av.value as ouid from attributevalue av " +
-                "inner join organisationunitattributevalues ouav on av.attributevalueid=ouav.attributevalueid " +
-                "inner join attribute at on av.attributeid=at.attributeid and at.uid='" + idScheme.getOrgUnitIdScheme().getAttribute() + "' " +
-                "where dv.sourceid=ouav.organisationunitid " +
-                "limit 1), ou.uid) as ouid" :
+            "select av.value as ouid from attributevalue av " +
+            "inner join organisationunitattributevalues ouav on av.attributevalueid=ouav.attributevalueid " +
+            "inner join attribute at on av.attributeid=at.attributeid and at.uid='" + idScheme.getOrgUnitIdScheme().getAttribute() + "' " +
+            "where dv.sourceid=ouav.organisationunitid " +
+            "limit 1), ou.uid) as ouid" :
             "ou." + ouScheme + " as ouid";
 
         String cocSql = idScheme.getCategoryOptionComboIdScheme().isAttribute() ?
             "coalesce((" +
-                "select av.value as cocid from attributevalue av " +
-                "inner join categoryoptioncomboattributevalues cocav on av.attributevalueid=cocav.attributevalueid " +
-                "inner join attribute at on av.attributeid=at.attributeid and at.uid='" + idScheme.getCategoryOptionComboIdScheme().getAttribute() + "' " +
-                "where dv.categoryoptioncomboid=cocav.categoryoptioncomboid " +
-                "limit 1), coc.uid) as cocid" :
+            "select av.value as cocid from attributevalue av " +
+            "inner join categoryoptioncomboattributevalues cocav on av.attributevalueid=cocav.attributevalueid " +
+            "inner join attribute at on av.attributeid=at.attributeid and at.uid='" + idScheme.getCategoryOptionComboIdScheme().getAttribute() + "' " +
+            "where dv.categoryoptioncomboid=cocav.categoryoptioncomboid " +
+            "limit 1), coc.uid) as cocid" :
             "coc." + cocScheme + " as cocid";
 
         String aocSql = idScheme.getCategoryOptionComboIdScheme().isAttribute() ?
             "coalesce((" +
-                "select av.value as aocid from attributevalue av " +
-                "inner join categoryoptioncomboattributevalues cocav on av.attributevalueid=cocav.attributevalueid " +
-                "inner join attribute at on av.attributeid=at.attributeid and at.uid='" + idScheme.getCategoryOptionComboIdScheme().getAttribute() + "' " +
-                "where dv.attributeoptioncomboid=cocav.categoryoptioncomboid " +
-                "limit 1), aoc.uid) as aocid" :
+            "select av.value as aocid from attributevalue av " +
+            "inner join categoryoptioncomboattributevalues cocav on av.attributevalueid=cocav.attributevalueid " +
+            "inner join attribute at on av.attributeid=at.attributeid and at.uid='" + idScheme.getCategoryOptionComboIdScheme().getAttribute() + "' " +
+            "where dv.attributeoptioncomboid=cocav.categoryoptioncomboid " +
+            "limit 1), aoc.uid) as aocid" :
             "aoc." + cocScheme + " as aocid";
 
         //----------------------------------------------------------------------
@@ -267,14 +267,14 @@ public class SpringDataValueSetStore
 
         String sql =
             "select " + deSql + ", pe.startdate as pestart, pt.name as ptname, " + ouSql + ", " + cocSql + ", " + aocSql + ", " +
-                "dv.value, dv.storedby, dv.created, dv.lastupdated, dv.comment, dv.followup, dv.deleted " +
-                "from datavalue dv " +
-                "inner join dataelement de on (dv.dataelementid=de.dataelementid) " +
-                "inner join period pe on (dv.periodid=pe.periodid) " +
-                "inner join periodtype pt on (pe.periodtypeid=pt.periodtypeid) " +
-                "inner join organisationunit ou on (dv.sourceid=ou.organisationunitid) " +
-                "inner join categoryoptioncombo coc on (dv.categoryoptioncomboid=coc.categoryoptioncomboid) " +
-                "inner join categoryoptioncombo aoc on (dv.attributeoptioncomboid=aoc.categoryoptioncomboid) ";
+            "dv.value, dv.storedby, dv.created, dv.lastupdated, dv.comment, dv.followup, dv.deleted " +
+            "from datavalue dv " +
+            "inner join dataelement de on (dv.dataelementid=de.dataelementid) " +
+            "inner join period pe on (dv.periodid=pe.periodid) " +
+            "inner join periodtype pt on (pe.periodtypeid=pt.periodtypeid) " +
+            "inner join organisationunit ou on (dv.sourceid=ou.organisationunitid) " +
+            "inner join categoryoptioncombo coc on (dv.categoryoptioncomboid=coc.categoryoptioncomboid) " +
+            "inner join categoryoptioncombo aoc on (dv.attributeoptioncomboid=aoc.categoryoptioncomboid) ";
 
         //----------------------------------------------------------------------
         // Filters
