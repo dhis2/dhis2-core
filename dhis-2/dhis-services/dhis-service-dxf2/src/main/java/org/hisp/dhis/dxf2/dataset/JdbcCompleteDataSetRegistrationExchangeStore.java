037779aa4f7db0e42225d357f7801d044a7c13a0
Kaustav Chakraborty <kaustavc@thoughtworks.com>
2018-09-12 11:49:11 +0530

    [Kaustav] Revert "[Teja] #1755 solve the issue for PR"
    This reverts commit df3478a5790bde06718d9f8edbbb43dfb2f7ae31.

--- dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/dataset/JdbcCompleteDataSetRegistrationExchangeStore.java
+++ dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/dataset/JdbcCompleteDataSetRegistrationExchangeStore.java
@@ -68,7 +68,7 @@ public class JdbcCompleteDataSetRegistrationExchangeStore
     private static final Log log = LogFactory.getLog( JdbcCompleteDataSetRegistrationExchangeStore.class );
 
     @Autowired
-    private UserService userService;
+    UserService userService;
 
     //--------------------------------------------------------------------------
     // Id scheme parameters
--- dhis-2/dhis-web/dhis-web-dataentry/src/main/java/org/hisp/dhis/de/action/GetDataValuesForDataSetAction.java
+++ dhis-2/dhis-web/dhis-web-dataentry/src/main/java/org/hisp/dhis/de/action/GetDataValuesForDataSetAction.java
@@ -71,8 +71,6 @@ public class GetDataValuesForDataSetAction
     // Dependencies
     // -------------------------------------------------------------------------
 
-    private String lastUpdatedBy;
-
     private DataValueService dataValueService;
 
     public void setDataValueService( DataValueService dataValueService )
@@ -214,7 +212,9 @@ public class GetDataValuesForDataSetAction
     {
         return storedBy;
     }
-    
+
+    private String lastUpdatedBy;
+
     public String getLastUpdatedBy() { return lastUpdatedBy; }
 
     private Map<String, FileResource> dataValueFileResourceMap = new HashMap<>();
