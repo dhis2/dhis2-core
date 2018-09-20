59ace53bb176c324332f4cb4c280b58c901297eb
Morten Olav Hansen <mortenoh@gmail.com>
2018-09-20 08:29:28 +0700

    Minor

--- dhis-2/dhis-services/dhis-service-node/src/main/java/org/hisp/dhis/fieldfilter/DefaultFieldParser.java
+++ dhis-2/dhis-services/dhis-service-node/src/main/java/org/hisp/dhis/fieldfilter/DefaultFieldParser.java
@@ -49,8 +49,12 @@ public class DefaultFieldParser implements FieldParser
 
         StringBuilder builder = new StringBuilder();
 
-        for ( String c : fields.split( "" ) )
+        String[] fieldSplit = fields.split( "" );
+
+        for ( int i = 0; i < fieldSplit.length; i++ )
         {
+            String c = fieldSplit[i];
+
             if ( c.equals( "," ) )
             {
                 putInMap( fieldMap, joinedWithPrefix( builder, prefixList ) );
@@ -58,14 +62,14 @@ public class DefaultFieldParser implements FieldParser
                 continue;
             }
 
-            if ( c.equals( "[" ) )
+            if ( c.equals( "[" ) || c.equals( "(" ) )
             {
                 prefixList.add( builder.toString() );
                 builder = new StringBuilder();
                 continue;
             }
 
-            if ( c.equals( "]" ) )
+            if ( c.equals( "]" ) || c.equals( ")" ) )
             {
                 if ( !builder.toString().isEmpty() )
                 {
@@ -74,6 +78,42 @@ public class DefaultFieldParser implements FieldParser
 
                 prefixList.remove( prefixList.size() - 1 );
                 builder = new StringBuilder();
+                continue;
+            }
+
+            // if we reach a field transformer, parse it out here (necessary to allow for () to be used to handle transformer parameters)
+            if ( c.equals( ":" ) || c.equals( "~" ) )
+            {
+                boolean insideParameters = false;
+
+                for ( ; i < fieldSplit.length; i++ )
+                {
+                    c = fieldSplit[i];
+
+                    if ( StringUtils.isAlphanumeric( c ) || c.equals( ":" ) || c.equals( "~" ) )
+                    {
+                        builder.append( c );
+                    }
+                    else if ( c.equals( "(" ) ) // start parameter
+                    {
+                        insideParameters = true;
+                        builder.append( c );
+                    }
+                    else if ( insideParameters && c.equals( ";" ) ) // allow parameter separator
+                    {
+                        builder.append( c );
+                    }
+                    else if ( insideParameters && c.equals( ")" ) ) // end parameter
+                    {
+                        insideParameters = false;
+                        builder.append( c );
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
                 continue;
             }
 
