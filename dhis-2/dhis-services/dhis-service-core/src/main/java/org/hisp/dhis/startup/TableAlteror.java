748ce22953d9e935af1cff9ef9d55c4f620df5d1
Ameen <ameen@dhis2.org>
2018-09-25 11:03:36 +0200

    Upgrade scripts to make all keyjsonvalues public read write

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/startup/TableAlteror.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/startup/TableAlteror.java
@@ -1117,6 +1117,8 @@ public class TableAlteror
         
         // 2.31, migrate to Flyway
         executeSql( "alter table trackedentityattribute alter column shortname set not null" );
+        executeSql( "UPDATE keyjsonvalue SET publicaccess='rw------' WHERE publicaccess IS NULL;" );
+
         
         log.info( "Tables updated" );
 
