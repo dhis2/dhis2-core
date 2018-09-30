49331d83b2f5041a4d2eb49929ad742fd14ae77a
vietnguyen <viet@dhis2.org>
2018-10-01 04:03:10 +0700

    Add jpa api and unit test for ProgramInstanceAuditStore.

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/program/hibernate/HibernateProgramInstanceAuditStore.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/program/hibernate/HibernateProgramInstanceAuditStore.java
@@ -28,23 +28,32 @@ package org.hisp.dhis.program.hibernate;
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-import java.util.List;
-
 import org.hibernate.Criteria;
 import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
+import org.hisp.dhis.hibernate.HibernateGenericStore;
+import org.hisp.dhis.hibernate.JpaQueryParameters;
 import org.hisp.dhis.program.ProgramInstance;
 import org.hisp.dhis.program.ProgramInstanceAudit;
 import org.hisp.dhis.program.ProgramInstanceAuditQueryParams;
 import org.hisp.dhis.program.ProgramInstanceAuditStore;
+import org.hisp.dhis.trackedentityattributevalue.TrackedEntityAttributeValueAudit;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
 
 /**
  * @author Abyot Asalefew Gizaw <abyota@gmail.com>
  *
  */
 public class HibernateProgramInstanceAuditStore
+    extends HibernateGenericStore<ProgramInstanceAudit>
     implements ProgramInstanceAuditStore
 {
 
@@ -52,8 +61,6 @@ public class HibernateProgramInstanceAuditStore
     // Dependencies
     // -------------------------------------------------------------------------
     
-    private SessionFactory sessionFactory;
-
     public void setSessionFactory( SessionFactory sessionFactory )
     {
         this.sessionFactory = sessionFactory;
@@ -62,11 +69,11 @@ public class HibernateProgramInstanceAuditStore
     // -------------------------------------------------------------------------
     // ProgramInstanceAuditStore implementation
     // -------------------------------------------------------------------------
-    
+
     @Override
     public void addProgramInstanceAudit( ProgramInstanceAudit programInstanceAudit )
     {
-        sessionFactory.getCurrentSession().save( programInstanceAudit );
+        save( programInstanceAudit );
     }
 
     @Override
@@ -77,64 +84,67 @@ public class HibernateProgramInstanceAuditStore
     }
 
     @Override
-    @SuppressWarnings( "unchecked" )    
     public List<ProgramInstanceAudit> getProgramInstanceAudits( ProgramInstanceAuditQueryParams params )
     {
-        Criteria criteria = getProgramInstanceAuditCriteria( params );
-        criteria.addOrder( Order.desc( "created" ) );
-        
+        CriteriaBuilder builder = getCriteriaBuilder();
+
+        JpaQueryParameters<ProgramInstanceAudit> jpaParameters = newJpaParameters()
+            .addPredicates( getProgramInstanceAuditCriteria( params, builder ) )
+            .addOrder( root -> builder.desc( root.get( "created" ) ) );
+
         if( !params.isSkipPaging() )
         {
-            criteria.setFirstResult( params.getFirst() );
-            criteria.setMaxResults( params.getMax() );
+            jpaParameters.setFirstResult( params.getFirst() ).setMaxResults( params.getMax() );
         }
 
-        return criteria.list();
+        return getList( builder, jpaParameters );
     }
 
     @Override
     public int getProgramInstanceAuditsCount( ProgramInstanceAuditQueryParams params )
     {
-        return ((Number) getProgramInstanceAuditCriteria( params )
-            .setProjection( Projections.countDistinct( "id" ) ).uniqueResult()).intValue();
+        CriteriaBuilder builder = getCriteriaBuilder();
+
+        return getCount( builder, newJpaParameters()
+            .addPredicates( getProgramInstanceAuditCriteria( params, builder ) )
+            .setUseDistinct( true ) )
+            .intValue();
     }
-    
-    
-    private Criteria getProgramInstanceAuditCriteria( ProgramInstanceAuditQueryParams params )
-    {        
-        Criteria criteria = sessionFactory.getCurrentSession().createCriteria( ProgramInstanceAudit.class );
+
+    private List<Function<Root<ProgramInstanceAudit>, Predicate>> getProgramInstanceAuditCriteria( ProgramInstanceAuditQueryParams params, CriteriaBuilder builder )
+    {
+        List<Function<Root<ProgramInstanceAudit>, Predicate>> predicates = new ArrayList<>();
 
         if ( params.hasProgramInstances() )
         {
-            criteria.add( Restrictions.in( "programInstance", params.getProgramInstances() ) );
+            predicates.add( root -> root.get( "programInstance" ).in( params.getProgramInstances() ) );
         }
         
         if ( params.hasPrograms() )
         {
-            criteria.createAlias( "programInstance", "programInstance" );
-            criteria.add( Restrictions.in( "programInstance.program", params.getPrograms() ) );
+            predicates.add( root -> root.join( "programInstance" ).get( "program" ).in( params.getPrograms() ) );
         }
         
         if ( params.hasUsers() )
         {
-            criteria.add( Restrictions.in( "accessedBy", params.getUsers() ) );
+            predicates.add( root -> root.get( "accessedBy" ).in( params.getUsers() ) );
         }
         
         if ( params.hasAuditType() )
         {
-            criteria.add( Restrictions.eq( "auditType", params.getAuditType() ) );
+            predicates.add( root -> builder.equal( root.get( "auditType" ), params.getAuditType() ) );
         }
 
         if ( params.hasStartDate() )
         {
-            criteria.add(  Restrictions.ge( "created", params.getStartDate() ) );
+            predicates.add( root -> builder.greaterThanOrEqualTo( root.get( "created" ), params.getStartDate() ) );
         }
 
         if ( params.hasEndDate() )
         {
-            criteria.add(  Restrictions.le( "created", params.getEndDate() ) );
+            predicates.add(  root -> builder.lessThanOrEqualTo( root.get( "created" ), params.getEndDate() ) );
         }
 
-        return criteria;
+        return predicates;
     }
 }
