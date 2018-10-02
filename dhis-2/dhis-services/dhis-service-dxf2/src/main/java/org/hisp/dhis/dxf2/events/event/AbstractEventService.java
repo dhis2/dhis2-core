ea5730b144bd97ad415e59de5dc3df1b0f32adf8
Ameen <ameen@dhis2.org>
2018-10-02 10:21:30 +0200

    added isScheduledEmptyEvent to Event

--- dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/events/event/AbstractEventService.java
+++ dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/events/event/AbstractEventService.java
@@ -526,7 +526,7 @@ public abstract class AbstractEventService
         List<String> errors = trackerAccessManager.canWrite( importOptions.getUser(),
             new ProgramStageInstance( programInstance, programStage ).setOrganisationUnit( organisationUnit ) );
 
-        if ( !errors.isEmpty() && !event.isScheduledEvent() )
+        if ( !errors.isEmpty() && !event.isScheduledEmptyEvent() )
         {
             ImportSummary importSummary = new ImportSummary( ImportStatus.ERROR, errors.toString() );
             importSummary.incrementIgnored();
