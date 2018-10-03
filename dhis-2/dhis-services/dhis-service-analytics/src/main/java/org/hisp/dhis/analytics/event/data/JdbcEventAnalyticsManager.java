8f93853091a14dc0cfccc1edc68408df71b8becb
Lars Helge Øverland <lars@dhis2.org>
2018-10-03 23:24:50 +0200

    Event analytics clustering, wrong column name

--- dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/event/data/JdbcEventAnalyticsManager.java
+++ dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/event/data/JdbcEventAnalyticsManager.java
@@ -162,7 +162,7 @@ public class JdbcEventAnalyticsManager
         String clusterField = params.getCoordinateField();
         String quotedClusterField = quoteAlias( clusterField );
         
-        List<String> columns = Lists.newArrayList( "count(psi) as getCount",
+        List<String> columns = Lists.newArrayList( "count(psi) as count",
             "ST_AsText(ST_Centroid(ST_Collect(" + quotedClusterField + "))) as center", "ST_Extent(" + quotedClusterField + ") as extent" );
 
         columns.add( params.isIncludeClusterPoints() ?
