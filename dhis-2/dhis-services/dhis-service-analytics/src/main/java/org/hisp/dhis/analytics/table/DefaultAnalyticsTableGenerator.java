9fc4ed32d2c7a8c62bf89cb11a35c6254299c162
Lars Helge Øverland <lars@dhis2.org>
2018-10-09 23:39:35 +0200

    Analytics, drop table if exists

--- dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/table/DefaultAnalyticsTableGenerator.java
+++ dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/table/DefaultAnalyticsTableGenerator.java
@@ -92,7 +92,7 @@ public class DefaultAnalyticsTableGenerator
             .collect( Collectors.toSet() );
 
         log.info( String.format( "Found %d analytics table types: %s", availableTypes.size(), availableTypes ) );
-        log.info( String.format( "Skip %d analytics table types: %s", skipTypes.size(), skipTypes ) );
+        log.info( String.format( "Analytics table update: %s", params ) );
 
         try
         {
