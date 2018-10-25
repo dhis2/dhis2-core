317993b2716a5ef024b1333614f2b7bd39559f93
Stian Sandvold <stian@dhis2.org>
2018-10-25 16:54:20 +0200

    Revert "Flyway integration" (#2455)
    * Revert "Analytics, using inner union select query for cross-partition queries (#2454)"
    
    This reverts commit 12e396cd57a9389d8aa59fdf7e5a22e31bbf5809.
    
    * Revert "Flyway integration (#2429)"
    
    This reverts commit 32efd0945fa6716093a7a3ea7bae36d9801f98ab.

--- dhis-2/dhis-support/dhis-support-test/src/main/java/org/hisp/dhis/DhisSpringTest.java
+++ dhis-2/dhis-support/dhis-support-test/src/main/java/org/hisp/dhis/DhisSpringTest.java
@@ -33,6 +33,7 @@ import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
