9ff48b8036980c02b7d8108bd66990d3af6996c4
zubaira <zubair@dhis2.org>
2018-11-09 15:25:06 +0100

    added new user role for capture app

---
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/java/org/hisp/dhis/db/migration/v31/V2_31_13__Add_new_user_role.java
@@ -0,0 +1,106 @@
+package org.hisp.dhis.db.migration.v31;
+
+/*
+ * Copyright (c) 2004-2018, University of Oslo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * Neither the name of the HISP project nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.flywaydb.core.api.FlywayException;
+import org.flywaydb.core.api.migration.BaseJavaMigration;
+import org.flywaydb.core.api.migration.Context;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author Zubair Asghar.
+ */
+public class V2_31_13__Add_new_user_role extends BaseJavaMigration
+{
+    private static final Log log = LogFactory.getLog( V2_31_13__Add_new_user_role.class );
+
+    @Override
+    public void migrate( Context context ) throws Exception
+    {
+        List<Integer> legacyRoleIds = new ArrayList<>();
+        List<Integer> newRoleIds = new ArrayList<>();
+
+        try ( Statement statement = context.getConnection().createStatement() )
+        {
+            ResultSet legacyUserRole = statement.executeQuery( "select userroleid from userroleauthorities where authority='M_dhis-web-event-capture'" );
+
+            while ( legacyUserRole.next() )
+            {
+                legacyRoleIds.add( legacyUserRole.getInt( 1 ) );
+            }
+        }
+        catch ( SQLException ex )
+        {
+            log.error( "Flyway java migration error", ex );
+            throw new FlywayException( ex );
+        }
+
+        try ( Statement statement = context.getConnection().createStatement() )
+        {
+            ResultSet newUserRole = statement.executeQuery( "select userroleid from userroleauthorities where authority='M_dhis-web-capture'" );
+
+            while ( newUserRole.next() )
+            {
+                newRoleIds.add( newUserRole.getInt( 1 ) );
+            }
+        }
+        catch ( SQLException ex )
+        {
+            log.error( "Flyway java migration error", ex );
+            throw new FlywayException( ex );
+        }
+
+        legacyRoleIds.removeAll( newRoleIds ); // in case this new role has already been added
+
+        if( legacyRoleIds.size() > 0 )
+        {
+            try( PreparedStatement ps = context.getConnection().prepareStatement( "INSERT INTO userroleauthorities (userroleid, authority) VALUES (?, 'M_dhis-web-capture')" ) )
+            {
+                for ( Integer id : legacyRoleIds )
+                {
+                    ps.setInt( 1, id );
+                    ps.execute();
+                }
+            }
+            catch ( SQLException e )
+            {
+                log.error( "Flyway java migration error:", e );
+                throw new FlywayException( e );
+            }
+        }
+    }
+}
