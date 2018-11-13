844f678bcca1f6d1a5bbfafb999d75ef35e2762b
Viktor Varland <viktor@vardevs.se>
2018-11-13 19:37:18 +0100

    chore: update clone script

--- dhis-2/dhis-web/dhis-web-apps/clone-all.sh
+++ dhis-2/dhis-web/dhis-web-apps/clone-all.sh
@@ -60,6 +60,16 @@ readonly app_repos=(
 ## functions
 #
 
+function app_branch_name {
+    local repo=$1
+    local RHS=${repo#*\#}
+    if [[ "$repo" == "$RHS" ]]; then
+        echo "master"
+    else
+        echo "$RHS"
+    fi
+}
+
 function app_name {
     local repo=$1
     local name=$(echo "${repo}" | sed -n "s/^.*d2-ci\/\(.*\)\.git$/\1/p")
@@ -71,12 +81,15 @@ function clone {
     local repo=$1
     local path=$2
 
+    local branch=$(app_branch_name "$repo")
+
     if [[ ! -d "$path" ]]; then
-        git clone "${repo}" "${path}"
+        git clone --depth 1 -b "$branch" "$repo" "$path"
     else
         pushd "$path"
         git reset HEAD --hard
-        git fetch origin HEAD
+        git fetch origin "$branch"
+        git checkout "$branch"
         git merge
         popd
     fi
