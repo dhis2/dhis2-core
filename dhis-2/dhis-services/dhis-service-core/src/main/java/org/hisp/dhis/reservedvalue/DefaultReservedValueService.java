57f86264ce4ef8060c832c06678b38a1898a7119
Stian Sandvold <stian@dhis2.org>
2018-11-14 12:41:36 +0100

    Fixed an issue where generated ids was using a bad key for counting sequences
    (cherry picked from commit 7a3989f5f87667968003b2c53d0e0309d69ce558)

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/reservedvalue/DefaultReservedValueService.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/reservedvalue/DefaultReservedValueService.java
@@ -127,7 +127,7 @@ public class DefaultReservedValueService
 
                 while ( generatedValues.size() < numberOfValuesLeftToGenerate && maxGenerateAttempts-- > 0 )
                 {
-                    generatedValues.addAll( generateValues( textPattern, numberOfReservations - resultList.size() ) );
+                    generatedValues.addAll( generateValues( textPattern, key, numberOfReservations - resultList.size() ) );
                     generatedValues.removeAll( usedGeneratedValues );
                 }
 
@@ -181,7 +181,7 @@ public class DefaultReservedValueService
             .orElse( null );
     }
 
-    private List<String> generateValues( TextPattern textPattern, int numberOfValues )
+    private List<String> generateValues( TextPattern textPattern, String key, int numberOfValues )
     {
         List<String> generatedValues = new ArrayList<>();
         TextPatternSegment segment = getGeneratedSegment( textPattern );
@@ -189,7 +189,7 @@ public class DefaultReservedValueService
         if ( segment.getMethod().equals( TextPatternMethod.SEQUENTIAL ) )
         {
             generatedValues.addAll( sequentialNumberCounterStore
-                .getNextValues( textPattern.getOwnerUid(), segment.getParameter(), numberOfValues )
+                .getNextValues( textPattern.getOwnerUid(), key, numberOfValues )
                 .stream()
                 .map( ( n ) -> String.format( "%0" + segment.getParameter().length() + "d", n ) )
                 .collect( Collectors.toList() ) );
