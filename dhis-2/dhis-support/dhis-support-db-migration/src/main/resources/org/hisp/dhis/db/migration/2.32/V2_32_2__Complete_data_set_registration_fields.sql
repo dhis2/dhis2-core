5a97a412f9eaab6fcaad5561b368a26fb3064fa9
Lars Helge Øverland <lars@dhis2.org>
2018-11-15 12:49:32 +0100

    m

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.32/V2_32_2__Complete_data_set_registration_fields.sql
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.32/V2_32_2__Complete_data_set_registration_fields.sql
@@ -9,4 +9,5 @@ alter table completedatasetregistration
 add column if not exists completed boolean;
 
 update completedatasetregistration set completed = true where completed is null;
+
 alter table completedatasetregistration alter column completed set not null;
