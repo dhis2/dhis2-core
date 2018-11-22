58c62c8bba17b84266d903d1dbc43f6dadb53cb8
Stian Sandvold <stian@dhis2.org>
2018-11-22 15:31:27 +0100

    Update dhis-2/dhis-services/dhis-service-analytics/src/test/java/org/hisp/dhis/analytics/data/AnalyticsServiceTest.java
    Co-Authored-By: vilkg <vilkelyte.gintare@gmail.com>

--- dhis-2/dhis-services/dhis-service-analytics/src/test/java/org/hisp/dhis/analytics/data/AnalyticsServiceTest.java
+++ dhis-2/dhis-services/dhis-service-analytics/src/test/java/org/hisp/dhis/analytics/data/AnalyticsServiceTest.java
@@ -410,7 +410,7 @@ public class AnalyticsServiceTest
         validationResultStore.save( validationResultBBB );
         validationResultStore.save( validationResultBBA );
 
-        Thread.sleep(1000);
+        Thread.sleep( 1000 );
         // Generate analytics tables
         // --------------------------------------------------------------------
         analyticsTableGenerator.generateTables( AnalyticsTableUpdateParams.newBuilder().build() );
