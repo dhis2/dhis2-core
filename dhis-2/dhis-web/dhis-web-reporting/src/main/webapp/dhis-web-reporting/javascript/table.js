7b3fef290a275011cefd922b7f4063bf0272af45
MDHomem <mdhomem@gmail.com>
2018-11-28 15:28:33 +0000

    chore: remove reports app struts module

--- dhis-2/dhis-web/dhis-web-reporting/src/main/webapp/dhis-web-reporting/javascript/table.js
+++
@@ -1,7 +0,0 @@
-function removeTable( context ) {
-  removeItem(context.id, context.name, i18n_confirm_delete, "removeTable.action");
-}
-
-function runReportTable( context ) {
-  location.href = 'getReportParams.action?uid=' + context.uid + '&mode=table';
-}
