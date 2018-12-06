f27d1847796fbe7d542d2272369a13611ed6f9a0
Viktor Varland <viktor@vardevs.se>
2018-12-06 15:35:53 +0100

    fix: create target dir when no apps to clone so empty bundle works

--- dhis-2/dhis-web/dhis-web-apps/scripts/bundle-apps.js
+++ dhis-2/dhis-web/dhis-web-apps/scripts/bundle-apps.js
@@ -5,6 +5,7 @@
 const { promisify } = require('util')
 const path = require('path')
 const access = promisify(require('fs').access)
+const mkdir = promisify(require('fs').mkdir)
 
 const { clone_app, show_sha } = require('./git.js')
 const generate_index = require('./write-index-html.js')
@@ -36,7 +37,8 @@ async function main(opts = {}) {
     const { apps, artifact, build_dir, root } = opts
 
     // paths for resources
-    const bundle_path = path.join(build_dir, artifact, artifact, 'apps-bundle.json')
+    const target_path = path.join(build_dir, artifact)
+    const bundle_path = path.join(target_path, artifact, 'apps-bundle.json')
     const html_template_path = path.join(root, 'src', 'main', 'webapp', 'dhis-web-apps', 'template.html')
     const html_index_path = path.join(build_dir, artifact, artifact, 'index.html')
     const xml_template_path = path.join(root, 'src', 'main', 'resources', 'struts.xml')
@@ -48,6 +50,17 @@ async function main(opts = {}) {
         process.exit(0)
     } catch (err) {
         console.log(`${path.basename(bundle_path)} doesn't exist; run full bundle operation`)
+    }
+
+    try {
+        await mkdir(target_path)
+    } catch (err) {
+        if (err.code === 'EEXIST') {
+            console.log(`[bundle] ${target_path} exists already`)
+        } else {
+            console.error(`[bundle] could not create ${target_path}`, err)
+            process.exit(1)
+        }
     }
 
     const new_apps = []
