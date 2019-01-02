cc5c92cdcb9c4882d9e8e941cb7cd0bf27bdb3f2
Lars Helge Øverland <lars@dhis2.org>
2019-01-02 16:17:43 +0100

    Logging, set max file size 100mb and no backup files/rotated files

--- dhis-2/dhis-support/dhis-support-system/src/main/java/org/hisp/dhis/system/log/Log4JLogConfigInitializer.java
+++ dhis-2/dhis-support/dhis-support-system/src/main/java/org/hisp/dhis/system/log/Log4JLogConfigInitializer.java
@@ -48,10 +48,10 @@ import static org.apache.commons.lang3.StringUtils.isNotBlank;
  */
 public class Log4JLogConfigInitializer
     implements LogConfigInitializer
-{    
+{
     private static final PatternLayout PATTERN_LAYOUT = new PatternLayout( "* %-5p %d{ISO8601} %m (%F [%t])%n" );
-    
-    private static final String MAX_FILE_SIZE = "50MB";
+
+    private static final String MAX_FILE_SIZE = "100MB";
 
     private static final String LOG_DIR = "logs";
     private static final String ANALYTICS_TABLE_LOGGER_FILENAME = "dhis-analytics-table.log";
@@ -63,10 +63,10 @@ public class Log4JLogConfigInitializer
     private static final String LOG4J_CONF_PROP = "log4j.configuration";
 
     private static final Log log = LogFactory.getLog( Log4JLogConfigInitializer.class );
-    
+
     @Autowired
     private LocationManager locationManager;
-    
+
     @Override
     public void initConfig()
     {
@@ -75,87 +75,88 @@ public class Log4JLogConfigInitializer
             log.warn( "Could not initialize additional log configuration, external home directory not set" );
             return;
         }
-        
+
         if ( isNotBlank( System.getProperty( LOG4J_CONF_PROP ) ) )
         {
             log.info( "Aborting default log config, external config set through system prop " + LOG4J_CONF_PROP + ": " + System.getProperty( LOG4J_CONF_PROP ) );
             return;
         }
-        
+
         locationManager.buildDirectory( LOG_DIR );
 
         configureLoggers( ANALYTICS_TABLE_LOGGER_FILENAME, Lists.newArrayList( "org.hisp.dhis.resourcetable", "org.hisp.dhis.analytics.table" ) );
-        
+
         configureLoggers( DATA_EXCHANGE_LOGGER_FILENAME, Lists.newArrayList( "org.hisp.dhis.dxf2" ) );
-        
+
         configureLoggers( DATA_SYNC_LOGGER_FILENAME, Lists.newArrayList( "org.hisp.dhis.dxf2.synch" ) );
 
         configureLoggers( METADATA_SYNC_LOGGER_FILENAME, Lists.newArrayList( "org.hisp.dhis.dxf2.metadata" ) );
 
         configureLoggers( PUSH_ANALYSIS_LOGGER_FILENAME, Lists.newArrayList( "org.hisp.dhis.pushanalysis" ) );
-        
+
         configureRootLogger( GENERAL_LOGGER_FILENAME );
     }
-    
+
     /**
      * Configures rolling file loggers.
-     * 
+     *
      * @param filename the filename to output logging to.
      * @param loggers the logger names.
      */
     private void configureLoggers( String filename, List<String> loggers )
     {
         String file = getLogFile( filename );
-        
+
         RollingFileAppender appender = getRollingFileAppender( file );
 
         for ( String loggerName : loggers )
         {
             Logger logger = Logger.getRootLogger().getLoggerRepository().getLogger( loggerName );
-            
+
             logger.addAppender( appender );
-            
-            log.info( "Added logger: " + loggerName + " using file: " + file );     
-        }  
+
+            log.info( "Added logger: " + loggerName + " using file: " + file );
+        }
     }
-    
+
     /**
      * Configures a root file logger.
-     * 
+     *
      * @param filename the filename to output logging to.
      */
     private void configureRootLogger( String filename )
     {
         String file = getLogFile( filename );
-        
+
         RollingFileAppender appender = getRollingFileAppender( file );
-        
+
         Logger.getRootLogger().addAppender( appender );
-        
+
         log.info( "Added root logger using file: " + file );
     }
-    
+
     /**
      * Returns a rolling file appender.
-     * 
+     *
      * @param file the file to output to, including path and filename.
      */
     private RollingFileAppender getRollingFileAppender( String file )
     {
         RollingFileAppender appender = new RollingFileAppender();
-        
+
         appender.setThreshold( Level.INFO );
         appender.setFile( file );
         appender.setMaxFileSize( MAX_FILE_SIZE );
+        appender.setMaxBackupIndex( 0 );
         appender.setLayout( PATTERN_LAYOUT );
         appender.activateOptions();
-        
-        return appender;        
+
+        return appender;
     }
-    
+
     /**
      * Returns a file including path and filename.
-     * 
+     *
      * @param filename the filename to use for the file path.
      */
     private String getLogFile( String filename )
