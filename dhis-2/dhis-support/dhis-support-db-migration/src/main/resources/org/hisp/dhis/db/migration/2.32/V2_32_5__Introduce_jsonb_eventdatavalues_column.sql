b7329bc9b4cfee5ac9a81090eadd235594b0197e
David Katuscak <david@dhis2.org>
2019-01-07 09:19:22 +0100

    Adds and fills eventdatavalues jsonb column - DHIS2-4477
    Introduces a new jsonb `eventdatavalues` column in programstageinstance tables and fills it with the data covnerted from trackedentitydatavalue table.

---
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.32/V2_32_5__Introduce_jsonb_eventdatavalues_column.sql
@@ -0,0 +1,21 @@
+------------------------------------------------------------------------------------------------------------------
+---------- SQL commands converting trackedentitydatavalue into new json column in programstageinstance  ----------
+------------------------------------------------------------------------------------------------------------------
+
+alter table programstageinstance add column eventdatavalues jsonb;
+
+-- Fixes invalid timestamps in DB (problems found in Demo DB, but can be run on all DBs)
+update trackedentitydatavalue set created = substring( created::text, 1, 23 )::timestamp where created::text ~ '.*\.\d{6}$';
+update trackedentitydatavalue set lastupdated = substring( lastupdated::text, 1, 23 )::timestamp where lastupdated::text ~ '.*\.\d{6}$';
+
+-- Fetch data from trackedentitydatavalue table, process them, creates a jsonb representation of them and stores into eventdatavalues column
+-- in programstageinstance table.
+-- Various PostgreSql functions are used to get the required format of the JSON string. The string is then cast to jsonb type.
+-- Takes care of empty values. When there is no trackedentitydatavalue for given programstageinstance a null is saved. This can be changed
+-- and specified JSON string can be saved instead (change NULL for the given JSON String).
+update programstageinstance psi set eventdatavalues =
+  to_jsonb(coalesce(nullif(replace(array_to_string(array(
+    select jsonb_build_object(de.uid, jsonb_build_object('created', tedv.created, 'lastUpdated', tedv.lastupdated, 'value', tedv.value,
+                                                         'providedElsewhere', tedv.providedelsewhere, 'storedBy', tedv.storedby))
+    from trackedentitydatavalue tedv join dataelement de on tedv.dataelementid = de.dataelementid
+    where tedv.programstageinstanceid = psi.programstageinstanceid), ','), '}},{', '},'), ''), NULL)::json);
