62be91696cd0489782ab2e7e02d0983793ccbf4d
Jim Grace <jim@dhis2.org>
2019-01-13 23:57:44 -0500

    [wip] ExpressionParserService code cleanup

--- dhis-2/dhis-services/dhis-service-expression-parser/src/main/java/org/hisp/dhis/expressionparser/ExpressionVisitor.java
+++ dhis-2/dhis-services/dhis-service-expression-parser/src/main/java/org/hisp/dhis/expressionparser/ExpressionVisitor.java
@@ -28,7 +28,6 @@ package org.hisp.dhis.expressionparser;
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-import org.hisp.dhis.common.IdentifiableObject;
 import org.hisp.dhis.common.IdentifiableObjectManager;
 import org.hisp.dhis.constant.Constant;
 import org.hisp.dhis.constant.ConstantService;
