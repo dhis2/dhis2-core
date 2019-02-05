7a113932c74e1d5355c54628ef37aa953396daa9
abyot <abyota@gmail.com>
2019-02-05 11:14:21 +0100

    remove PI timestamp update

--- dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/events/event/AbstractEventService.java
+++ dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/events/event/AbstractEventService.java
@@ -278,8 +278,6 @@ public abstract class AbstractEventService
 
     private CachingMap<Class<? extends IdentifiableObject>, IdentifiableObject> defaultObjectsCache = new CachingMap<>();
 
-    private Set<ProgramInstance> programInstancesToUpdate = new HashSet<>();
-
     private Set<TrackedEntityInstance> trackedEntityInstancesToUpdate = new HashSet<>();
 
     // -------------------------------------------------------------------------
@@ -2355,10 +2353,7 @@ public abstract class AbstractEventService
 
     private void updateEntities( User user )
     {
-        programInstancesToUpdate.forEach( pi -> manager.update( pi, user ) );
         trackedEntityInstancesToUpdate.forEach( tei -> manager.update( tei, user ) );
-
-        programInstancesToUpdate.clear();
         trackedEntityInstancesToUpdate.clear();
     }
 
@@ -2414,8 +2409,6 @@ public abstract class AbstractEventService
             {
                 if ( !bulkUpdate )
                 {
-                    manager.update( programStageInstance.getProgramInstance(), user );
-
                     if ( programStageInstance.getProgramInstance().getEntityInstance() != null )
                     {
                         manager.update( programStageInstance.getProgramInstance().getEntityInstance(), user );
@@ -2423,8 +2416,6 @@ public abstract class AbstractEventService
                 }
                 else
                 {
-                    programInstancesToUpdate.add( programStageInstance.getProgramInstance() );
-
                     if ( programStageInstance.getProgramInstance().getEntityInstance() != null )
                     {
                         trackedEntityInstancesToUpdate.add( programStageInstance.getProgramInstance().getEntityInstance() );
