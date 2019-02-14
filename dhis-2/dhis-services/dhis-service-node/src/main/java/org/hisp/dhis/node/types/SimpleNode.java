67c3e3c2f260ca7c936ab2d8f115b176a6a98ebe
Paulo Martins <martins.tuga@gmail.com>
2019-02-14 13:00:09 +0000

    DHIS2-5466: API does not return the proper value while using GET in the collections -Implemented equals and hashCode in RootNode and Config -Fixed some formatting issues

--- dhis-2/dhis-services/dhis-service-node/src/main/java/org/hisp/dhis/node/types/SimpleNode.java
+++ dhis-2/dhis-services/dhis-service-node/src/main/java/org/hisp/dhis/node/types/SimpleNode.java
@@ -28,88 +28,94 @@ package org.hisp.dhis.node.types;
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+import java.util.Objects;
+
 import org.hisp.dhis.node.AbstractNode;
 import org.hisp.dhis.node.Node;
 import org.hisp.dhis.node.NodeType;
 import org.hisp.dhis.node.exception.InvalidTypeException;
 
-import java.util.Objects;
-
 /**
  * @author Morten Olav Hansen <mortenoh@gmail.com>
  */
 public class SimpleNode
-    extends AbstractNode
+    extends
+    AbstractNode
 {
-        /**
-         * Value of this node.
-         */
-        private final Object value;
+    /**
+     * Value of this node.
+     */
+    private final Object value;
 
-        /**
-         * Is this node considered a attribute.
-         */
-        private boolean attribute;
+    /**
+     * Is this node considered a attribute.
+     */
+    private boolean attribute;
 
-        public SimpleNode( String name, Object value )
+    public SimpleNode( String name, Object value )
+    {
+        super( name, NodeType.SIMPLE );
+        this.value = value;
+        this.attribute = false;
+    }
+
+    public SimpleNode( String name, Object value, boolean attribute )
+    {
+        this( name, value );
+        this.attribute = attribute;
+    }
+
+    public Object getValue()
+    {
+        return value;
+    }
+
+    public boolean isAttribute()
+    {
+        return attribute;
+    }
+
+    public void setAttribute( boolean attribute )
+    {
+        this.attribute = attribute;
+    }
+
+    @Override
+    public <T extends Node> T addChild( T child )
+    {
+        throw new InvalidTypeException();
+    }
+
+    @Override
+    public <T extends Node> void addChildren( Iterable<T> children )
+    {
+        throw new InvalidTypeException();
+    }
+
+    @Override
+    public boolean equals( Object o )
+    {
+        if ( this == o )
         {
-                super( name, NodeType.SIMPLE );
-                this.value = value;
-                this.attribute = false;
+            return true;
+        }
+        if ( o == null || getClass() != o.getClass() )
+        {
+            return false;
+        }
+        if ( !super.equals( o ) )
+        {
+            return false;
         }
 
-        public SimpleNode( String name, Object value, boolean attribute )
-        {
-                this( name, value );
-                this.attribute = attribute;
-        }
+        SimpleNode that = (SimpleNode) o;
 
-        public Object getValue()
-        {
-                return value;
-        }
+        return attribute == that.attribute && Objects.equals( value, that.value );
+    }
 
-        public boolean isAttribute()
-        {
-                return attribute;
-        }
-
-        public void setAttribute( boolean attribute )
-        {
-                this.attribute = attribute;
-        }
-
-        @Override
-        public <T extends Node> T addChild( T child )
-        {
-                throw new InvalidTypeException();
-        }
-
-        @Override
-        public <T extends Node> void addChildren( Iterable<T> children )
-        {
-                throw new InvalidTypeException();
-        }
-
-        @Override
-        public boolean equals( Object o )
-        {
-                if ( this == o )
-                        return true;
-                if ( o == null || getClass() != o.getClass() )
-                        return false;
-                if ( !super.equals( o ) )
-                        return false;
-
-                SimpleNode that = (SimpleNode) o;
-
-                return attribute == that.attribute &&
-                    Objects.equals( value, that.value );
-        }
-
-        @Override
-        public int hashCode()
-        {
-                return Objects.hash( super.hashCode(), value );
-        }
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash( super.hashCode(), value );
+    }
 }
