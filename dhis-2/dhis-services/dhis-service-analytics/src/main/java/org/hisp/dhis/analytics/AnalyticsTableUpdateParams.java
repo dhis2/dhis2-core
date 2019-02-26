b87700656b5a4ea8e87060061dba55ac31adfe0d
Lars Helge Øverland <lars@dhis2.org>
2019-02-26 11:34:17 +0100

    Minor

--- dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/AnalyticsTableUpdateParams.java
+++ dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/AnalyticsTableUpdateParams.java
@@ -130,10 +130,10 @@ public class AnalyticsTableUpdateParams
     }
 
     /**
-     * Returns the start date based on the last years property, i.e. the first
+     * Returns the from date based on the last years property, i.e. the first
      * day of year relative to the last years property.
      *
-     * @return the start date based on the last years property.
+     * @return the from date based on the last years property.
      */
     public Date getFromDate()
     {
