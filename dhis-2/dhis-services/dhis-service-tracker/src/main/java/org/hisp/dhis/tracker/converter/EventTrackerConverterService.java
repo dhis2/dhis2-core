5e540dcd0215fc8c24d6e9c715beff6f2d71f8e2
Morten Olav Hansen <mortenoh@gmail.com>
2019-03-05 12:55:16 +0700

    Adds support for converting PSIs to Events

--- dhis-2/dhis-services/dhis-service-tracker/src/main/java/org/hisp/dhis/tracker/converter/EventTrackerConverterService.java
+++ dhis-2/dhis-services/dhis-service-tracker/src/main/java/org/hisp/dhis/tracker/converter/EventTrackerConverterService.java
@@ -29,8 +29,13 @@ package org.hisp.dhis.tracker.converter;
  */
 
 import org.hisp.dhis.api.util.DateUtils;
+import org.hisp.dhis.category.CategoryOption;
 import org.hisp.dhis.common.IdentifiableObjectManager;
+import org.hisp.dhis.dxf2.events.enrollment.EnrollmentStatus;
+import org.hisp.dhis.dxf2.events.event.Coordinate;
+import org.hisp.dhis.dxf2.events.event.DataValue;
 import org.hisp.dhis.dxf2.events.event.Event;
+import org.hisp.dhis.eventdatavalue.EventDataValue;
 import org.hisp.dhis.organisationunit.OrganisationUnit;
 import org.hisp.dhis.program.Program;
 import org.hisp.dhis.program.ProgramInstance;
@@ -49,6 +54,8 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @author Morten Olav Hansen <mortenoh@gmail.com>
@@ -91,7 +98,75 @@ public class EventTrackerConverterService
     @Override
     public List<Event> to( TrackerPreheat preheat, List<ProgramStageInstance> programStageInstances )
     {
-        return null;
+        List<Event> events = new ArrayList<>();
+
+        programStageInstances.forEach( psi -> {
+            Event event = new Event();
+            event.setEvent( psi.getUid() );
+
+            if ( psi.getProgramInstance().getEntityInstance() != null )
+            {
+                event.setTrackedEntityInstance( psi.getProgramInstance().getEntityInstance().getUid() );
+            }
+
+            event.setFollowup( psi.getProgramInstance().getFollowup() );
+            event.setEnrollmentStatus( EnrollmentStatus.fromProgramStatus( psi.getProgramInstance().getStatus() ) );
+            event.setStatus( psi.getStatus() );
+            event.setEventDate( DateUtils.getIso8601NoTz( psi.getExecutionDate() ) );
+            event.setDueDate( DateUtils.getIso8601NoTz( psi.getDueDate() ) );
+            event.setStoredBy( psi.getStoredBy() );
+            event.setCompletedBy( psi.getCompletedBy() );
+            event.setCompletedDate( DateUtils.getIso8601NoTz( psi.getCompletedDate() ) );
+            event.setCreated( DateUtils.getIso8601NoTz( psi.getCreated() ) );
+            event.setCreatedAtClient( DateUtils.getIso8601NoTz( psi.getCreatedAtClient() ) );
+            event.setLastUpdated( DateUtils.getIso8601NoTz( psi.getLastUpdated() ) );
+            event.setLastUpdatedAtClient( DateUtils.getIso8601NoTz( psi.getLastUpdatedAtClient() ) );
+            event.setGeometry( psi.getGeometry() );
+            event.setDeleted( psi.isDeleted() );
+
+            // Lat and lnt deprecated in 2.30, remove by 2.33
+            if ( event.getGeometry() != null && event.getGeometry().getGeometryType().equals( "Point" ) )
+            {
+                com.vividsolutions.jts.geom.Coordinate geometryCoordinate = event.getGeometry().getCoordinate();
+                event.setCoordinate( new Coordinate( geometryCoordinate.x, geometryCoordinate.y ) );
+            }
+
+            OrganisationUnit ou = psi.getOrganisationUnit();
+
+            if ( ou != null )
+            {
+                event.setOrgUnit( ou.getUid() );
+                event.setOrgUnitName( ou.getName() );
+            }
+
+            Program program = psi.getProgramInstance().getProgram();
+
+            event.setProgram( program.getUid() );
+            event.setEnrollment( psi.getProgramInstance().getUid() );
+            event.setProgramStage( psi.getProgramStage().getUid() );
+            event.setAttributeOptionCombo( psi.getAttributeOptionCombo().getUid() );
+            event.setAttributeCategoryOptions( psi.getAttributeOptionCombo()
+                .getCategoryOptions().stream().map( CategoryOption::getUid ).collect( Collectors.joining( ";" ) ) );
+
+            Set<EventDataValue> dataValues = psi.getEventDataValues();
+
+            for ( EventDataValue dataValue : dataValues )
+            {
+                DataValue value = new DataValue();
+                value.setCreated( DateUtils.getIso8601NoTz( dataValue.getCreated() ) );
+                value.setLastUpdated( DateUtils.getIso8601NoTz( dataValue.getLastUpdated() ) );
+                value.setDataElement( dataValue.getDataElement() );
+                value.setValue( dataValue.getValue() );
+                value.setProvidedElsewhere( dataValue.getProvidedElsewhere() );
+                value.setStoredBy( dataValue.getStoredBy() );
+
+                event.getDataValues().add( value );
+            }
+
+            events.add( event );
+        } );
+
+        return events;
     }
 
     @Override
