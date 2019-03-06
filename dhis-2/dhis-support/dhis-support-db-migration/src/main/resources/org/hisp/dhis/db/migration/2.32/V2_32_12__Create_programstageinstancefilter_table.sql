79f14457d4e8433cb6d69937f7e8d9f7d0b92ff5
Ameen <ameen@dhis2.org>
2019-03-06 21:57:18 +0100

    Add basic model for programStageInstanceFilter

---
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.32/V2_32_12__Create_programstageinstancefilter_table.sql
@@ -0,0 +1,23 @@
+create table programstageinstancefilter (
+    programstageinstancefilterid bigint not NULL,
+    uid character varying(11) not NULL,
+    created timestamp without time zone not NULL,
+    lastupdated timestamp without time zone not NULL,
+    lastupdatedby bigint,
+    name character varying(230) not NULL,
+    description character varying(255),
+    style jsonb,
+    programid bigint,
+    programstageid bigint,
+    eventstatus character varying(50),
+    eventcreatedperiod jsonb,
+    eventdatavaluefilters jsonb
+);
+
+alter table ONLY programstageinstancefilter
+add constraint programstageinstancefilter_pkey primary key (programstageinstancefilterid),
+add constraint uk_programstageinstancefilter_uid unique (uid),
+add constraint fk_lastupdatedby_userid foreign key (lastupdatedby) references userinfo(userinfoid),
+add constraint fk_programstageinstancefilter_programid foreign key (programid) references program(programid),
+add constraint fk_programstageinstancefilter_programstageid foreign key (programstageid) references programstage(programstageid);
+
