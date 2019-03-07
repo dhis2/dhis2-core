5b6eaa38a66fa24e422761f15c798ab26434420c
Zubair Asghar <zubair@dhis2.org>
2019-03-07 10:35:00 +0100

    Remove deprecated BulkSMS API
    Replace deprecated Http API with new Restful API.
    
    PR: #2964

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/sms/config/BulkSmsGateway.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/sms/config/BulkSmsGateway.java
@@ -28,25 +28,16 @@ package org.hisp.dhis.sms.config;
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-import com.google.common.collect.ImmutableMap;
-import org.apache.commons.lang.StringUtils;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.hisp.dhis.outboundmessage.OutboundMessageResponse;
-import org.hisp.dhis.sms.outbound.GatewayResponse;
+import org.hisp.dhis.sms.outbound.BulkSmsRequestEntity;
 import org.hisp.dhis.outboundmessage.OutboundMessageBatch;
-import org.hisp.dhis.sms.outbound.SubmissionType;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.HttpServerErrorException;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
-import java.net.URI;
+import java.util.Base64;
 import java.util.List;
-import java.util.Random;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -56,26 +47,6 @@ import java.util.stream.Collectors;
 public class BulkSmsGateway
     extends SmsGateway
 {
-    private static final Log log = LogFactory.getLog( BulkSmsGateway.class );
-
-    private static final int MIN = 1;
-
-    private static final int MAX = 2147483647;
-
-    public static final ImmutableMap<String, GatewayResponse> BULKSMS_GATEWAY_RESPONSE_MAP = new ImmutableMap.Builder<String, GatewayResponse>()
-        .put( "0", GatewayResponse.RESULT_CODE_0 ).put( "1", GatewayResponse.RESULT_CODE_1 )
-        .put( "22", GatewayResponse.RESULT_CODE_22 ).put( "23", GatewayResponse.RESULT_CODE_23 )
-        .put( "24", GatewayResponse.RESULT_CODE_24 ).put( "25", GatewayResponse.RESULT_CODE_25 )
-        .put( "26", GatewayResponse.RESULT_CODE_26 ).put( "27", GatewayResponse.RESULT_CODE_27 )
-        .put( "40", GatewayResponse.RESULT_CODE_40 ).build();
-
-    // -------------------------------------------------------------------------
-    // Dependencies
-    // -------------------------------------------------------------------------
-
-    @Autowired
-    private RestTemplate restTemplate;
-
     // -------------------------------------------------------------------------
     // Implementation
     // -------------------------------------------------------------------------
@@ -97,120 +68,30 @@ public class BulkSmsGateway
     @Override
     public OutboundMessageResponse send( String subject, String text, Set<String> recipients, SmsGatewayConfig config )
     {
-        UriComponentsBuilder uriBuilder = createUri( (BulkSmsGatewayConfig) config, recipients, SubmissionType.SINGLE );
-        uriBuilder.queryParam( "message", stringToHex( text ) );
+        BulkSmsGatewayConfig bulkSmsGatewayConfig = (BulkSmsGatewayConfig) config;
 
-        return send( uriBuilder );
+        HttpEntity<BulkSmsRequestEntity> request =
+            new HttpEntity<>( new BulkSmsRequestEntity( text, recipients ), getRequestHeaderParameters( bulkSmsGatewayConfig ) );
+
+        HttpStatus httpStatus = send( bulkSmsGatewayConfig.getUrlTemplate(), request, String.class );
+
+        return wrapHttpStatus( httpStatus );
     }
 
     // -------------------------------------------------------------------------
     // Supportive methods
     // -------------------------------------------------------------------------
 
-    private UriComponentsBuilder createUri( BulkSmsGatewayConfig bulkSmsConfig, Set<String> recipients,
-        SubmissionType type )
+    private HttpHeaders getRequestHeaderParameters( BulkSmsGatewayConfig bulkSmsGatewayConfig )
     {
-        UriComponentsBuilder uriBuilder = buildBaseUrl( bulkSmsConfig, type );
-        uriBuilder.queryParam( "msisdn", getRecipients( recipients ) );
+        String credentials = bulkSmsGatewayConfig.getUsername().trim() + ":" + bulkSmsGatewayConfig.getPassword().trim();
+        String encodedCredentials = Base64.getEncoder().encodeToString( credentials.getBytes() );
 
-        return uriBuilder;
-    }
+        HttpHeaders headers = new HttpHeaders();
+        headers.set( HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE );
+        headers.set( HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE );
+        headers.set( HttpHeaders.AUTHORIZATION, BASIC + encodedCredentials );
 
-    private OutboundMessageResponse send( UriComponentsBuilder uriBuilder )
-    {
-        ResponseEntity<String> responseEntity = null;
-
-        try
-        {
-            URI url = uriBuilder.build().encode().toUri();
-
-            responseEntity = restTemplate.exchange( url, HttpMethod.POST, null, String.class );
-        }
-        catch ( HttpClientErrorException ex )
-        {
-            log.error( "Client error " + ex.getMessage() );
-        }
-        catch ( HttpServerErrorException ex )
-        {
-            log.error( "Server error " + ex.getMessage() );
-        }
-        catch ( Exception ex )
-        {
-            log.error( "Error " + ex.getMessage() );
-        }
-
-        return getResponse( responseEntity );
-    }
-
-    private UriComponentsBuilder buildBaseUrl( BulkSmsGatewayConfig bulkSmsConfiguration, SubmissionType type )
-    {
-        Random r = new Random();
-
-        int stopDuplicationID = r.nextInt( (MAX - MIN) + 1 ) + MIN;
-
-        UriComponentsBuilder uriBuilder = null;
-
-        if ( type.equals( SubmissionType.SINGLE ) )
-        {
-            uriBuilder = UriComponentsBuilder.fromHttpUrl( bulkSmsConfiguration.getUrlTemplate() );
-        }
-        else // SubmissionType.BATCH
-        {
-            uriBuilder = UriComponentsBuilder.fromHttpUrl( bulkSmsConfiguration.getUrlTemplateForBatchSms() );
-        }
-
-        uriBuilder.queryParam( "username", bulkSmsConfiguration.getUsername() )
-            .queryParam( "password", bulkSmsConfiguration.getPassword() )
-            .queryParam( "stop_dup_id", stopDuplicationID )
-            .queryParam( "dca", "16bit" );
-
-        return uriBuilder;
-    }
-
-    private OutboundMessageResponse getResponse( ResponseEntity<String> responseEntity )
-    {
-        OutboundMessageResponse status = new OutboundMessageResponse();
-
-        if ( responseEntity == null )
-        {
-            status.setResponseObject( GatewayResponse.FAILED );
-            status.setOk( false );
-
-            return status;
-        }
-
-        String response = responseEntity.getBody();
-
-        GatewayResponse gatewayResponse = BULKSMS_GATEWAY_RESPONSE_MAP.getOrDefault( StringUtils.split( response, "|" )[0], GatewayResponse.RESULT_CODE_40 );
-
-        gatewayResponse.setBatchId( StringUtils.split( response, "|" )[2] );
-
-        status.setResponseObject( gatewayResponse );
-        status.setDescription( gatewayResponse.getResponseMessage() );
-
-        return status;
-    }
-
-    private String getRecipients( Set<String> recipients )
-    {
-        return StringUtils.join( recipients, "," );
-    }
-
-    private String stringToHex( String text )
-    {
-        char[] chars = text.toCharArray();
-        String next;
-        StringBuffer output = new StringBuffer();
-        for (int i = 0; i < chars.length; i++ )
-        {
-            next = Integer.toHexString( (int)chars[i] );
-            // Unfortunately, toHexString doesn't pad with zeroes, so we have to.
-            for (int j = 0; j < ( 4-next.length() ); j++)
-            {
-                output.append( "0" );
-            }
-            output.append( next );
-        }
-        return output.toString();
+        return headers;
     }
 }
