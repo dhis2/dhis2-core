37485b7e5e40051977e49f75cc0e14f3b8ecc9e6
Ameen <ameen@dhis2.org>
2019-03-12 20:00:12 +0100

    Fix copyright year to 2019

--- dhis-2/dhis-support/dhis-support-test/src/main/java/org/hisp/dhis/container/DhisPostgisContainerProvider.java
+++ dhis-2/dhis-support/dhis-support-test/src/main/java/org/hisp/dhis/container/DhisPostgisContainerProvider.java
@@ -1,7 +1,7 @@
 package org.hisp.dhis.container;
 
 /*
- * Copyright (c) 2004-2018, University of Oslo
+ * Copyright (c) 2004-2019, University of Oslo
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
