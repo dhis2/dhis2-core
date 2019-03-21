b1de0a088aee2fe802a898cd6e4bc6953db66409
Lars Helge Øverland <lars@dhis2.org>
2019-03-21 20:58:04 +0100

    m

--- dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/table/JdbcAnalyticsTableManager.java
+++ dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/table/JdbcAnalyticsTableManager.java
@@ -260,7 +260,7 @@ public class JdbcAnalyticsTableManager
             sql += "and " + whereClause;
         }
 
-        invokeTimeAndLog( sql, String.format( "Populate %s, %s", tableName, valueTypes ) );
+        invokeTimeAndLog( sql, String.format( "Populate %s %s", tableName, valueTypes ) );
     }
 
     /**
