c72224c8445bf0e383609e23b75c4399d3453d55
Ameen <ameen@dhis2.org>
2019-03-21 08:35:54 +0100

    Add explicit sequences to remaining tables as per discussion

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.32/V2_32_13__Separate_sequence_generators_for_highly_used_tables.sql
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.32/V2_32_13__Separate_sequence_generators_for_highly_used_tables.sql
@@ -15,3 +15,21 @@ select setval('trackedentityinstanceaudit_sequence', max(trackedentityinstanceau
 
 create sequence if not exists programinstance_sequence;
 select setval('programinstance_sequence', max(programinstanceid)) FROM programinstance;
+
+create sequence if not exists deletedobject_sequence;
+select setval('deletedobject_sequence', max(deletedobjectid)) FROM deletedobject;
+
+create sequence if not exists reservedvalue_sequence;
+select setval('reservedvalue_sequence', max(reservedvalueid)) FROM reservedvalue;
+
+create sequence if not exists usermessage_sequence;
+select setval('usermessage_sequence', max(usermessageid)) FROM usermessage;
+
+create sequence if not exists messageconversation_sequence;
+select setval('messageconversation_sequence', max(messageconversationid)) FROM messageconversation;
+
+create sequence if not exists message_sequence;
+select setval('message_sequence', max(messageid)) FROM message;
+
+create sequence if not exists validationresult_sequence;
+select setval('validationresult_sequence', max(validationresultid)) FROM validationresult;
