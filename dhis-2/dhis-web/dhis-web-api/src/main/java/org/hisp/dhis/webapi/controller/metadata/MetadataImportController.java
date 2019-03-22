0b64f7d7a75bfce906eaa94169dd50c2180caaa7
David Katuscak <david@dhis2.org>
2019-03-22 10:50:26 +0100

    Ensures that MessageConversations are not imported in metadata import
    DHIS2-6315

--- dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/metadata/MetadataImportController.java
+++ dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/metadata/MetadataImportController.java
@@ -40,6 +40,7 @@ import org.hisp.dhis.dxf2.metadata.MetadataImportParams;
 import org.hisp.dhis.dxf2.metadata.MetadataImportService;
 import org.hisp.dhis.dxf2.metadata.feedback.ImportReport;
 import org.hisp.dhis.dxf2.webmessage.WebMessageUtils;
+import org.hisp.dhis.message.MessageConversation;
 import org.hisp.dhis.render.RenderFormat;
 import org.hisp.dhis.render.RenderService;
 import org.hisp.dhis.scheduling.JobConfiguration;
@@ -62,6 +63,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -116,6 +118,7 @@ public class MetadataImportController
             renderService.fromMetadata( StreamUtils.wrapAndCheckCompressionFormat( request.getInputStream() ), RenderFormat.JSON );
         // remove all data that cannot be exported explicitly and is not supported with other data formats
         objects.remove( JobConfiguration.class );
+        objects.remove( MessageConversation.class );
         params.setObjects( objects );
 
         response.setContentType( MediaType.APPLICATION_JSON_UTF8_VALUE );
@@ -147,6 +150,8 @@ public class MetadataImportController
         params.setCsvImportClass( CsvImportClass.valueOf( classKey ) );
 
         Metadata metadata = csvImportService.fromCsv( request.getInputStream(), params.getCsvImportClass() );
+        //messageConversations should not be imported anymore
+        metadata.setMessageConversations( Collections.emptyList() );
 
         params.addMetadata( schemaService.getMetadataSchemas(), metadata );
 
@@ -182,6 +187,8 @@ public class MetadataImportController
     {
         MetadataImportParams params = metadataImportService.getParamsFromMap( contextService.getParameterValuesMap() );
         Metadata metadata = renderService.fromXml( StreamUtils.wrapAndCheckCompressionFormat( request.getInputStream() ), Metadata.class );
+        //messageConversations should not be imported anymore
+        metadata.setMessageConversations( Collections.emptyList() );
         params.addMetadata( schemaService.getMetadataSchemas(), metadata );
         response.setContentType( MediaType.APPLICATION_XML_VALUE );
 
