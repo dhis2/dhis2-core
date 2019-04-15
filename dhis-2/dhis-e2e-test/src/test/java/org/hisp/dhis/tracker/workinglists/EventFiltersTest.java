70a19121b2cbb74a8c48229c5f0ad1ee250bbabb
Gintare Vilkelyte <vilkelyte.gintare@gmail.com>
2019-04-15 11:46:16 +0200

    Added happy path test for event filters

---
+++ dhis-2/dhis-e2e-test/src/test/java/org/hisp/dhis/tracker/workinglists/EventFiltersTest.java
@@ -0,0 +1,42 @@
+package org.hisp.dhis.tracker.workinglists;
+
+import com.google.gson.JsonObject;
+import org.hisp.dhis.ApiTest;
+import org.hisp.dhis.actions.LoginActions;
+import org.hisp.dhis.actions.RestApiActions;
+import org.hisp.dhis.actions.metadata.MetadataActions;
+import org.hisp.dhis.dto.ApiResponse;
+import org.hisp.dhis.helpers.ResponseValidationHelper;
+import org.hisp.dhis.helpers.file.FileReaderUtils;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+/**
+ * @author Gintare Vilkelyte <vilkelyte.gintare@gmail.com>
+ */
+public class EventFiltersTest extends ApiTest
+{
+    private RestApiActions eventFiltersActions;
+    private String pathToFile = "src/test/resources/tracker/workinglists/eventFilters.json";
+
+    @BeforeAll
+    public void beforeAll() {
+        eventFiltersActions = new RestApiActions( "/eventFilters" );
+
+        new LoginActions().loginAsDefaultUser();
+        new MetadataActions().importMetadata( new File( "src/test/resources/metadata/metadata.json" ) );
+    }
+
+    @Test
+    public void eventFilterCanBeSaved()
+        throws Exception
+    {
+        JsonObject body = new FileReaderUtils().readJsonAndGenerateData( new File(pathToFile) );
+
+        ApiResponse response = eventFiltersActions.post( body );
+
+        ResponseValidationHelper.validateObjectCreation( response );
+    }
+}
