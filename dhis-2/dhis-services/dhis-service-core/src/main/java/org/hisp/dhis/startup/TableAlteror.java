22781f0416a4bd5d5b6e402d0578a6a17d108073
Volker Schmidt <volker@dhis2.org>
2019-04-26 00:43:47 +0200

    Adds missing index for queries from options of categories.
    - Solves parts of the issues of DHIS2-6491.
    - Backport from 2.33, with limited changes.

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/startup/TableAlteror.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/startup/TableAlteror.java
@@ -1100,12 +1100,6 @@ public class TableAlteror
 
         assignJobConfigurationDistinctUids();
 
-        // Creates covering index for querying categories from category options
-        // Since the index is required just for a backport the covering part of the index is ordered descendant,
-        // which enables an easy exchange in version 2.33.
-        executeSql( "DROP INDEX IF EXISTS in_categories_categoryoptions_coid_bp;" );
-        executeSql( "CREATE INDEX in_categories_categoryoptions_coid_bp ON categories_categoryoptions(categoryoptionid, categoryid DESC);" );
-
         log.info( "Tables updated" );
 
     }
