5d35a06e40f2caa46454212c1e5d0a9bb7394db9
Jim Grace <jimgrace@users.noreply.github.com>
2019-05-01 05:37:27 -0400

    Add program indicator custom aggregate functions DHIS2-6822 (#3252)
    * Add program indicator parser validity tests
    
    * Add program indicator custom aggregate functions DHIS2-6822

--- dhis-2/dhis-services/dhis-service-core/src/test/java/org/hisp/dhis/program/ProgramSqlGeneratorItemsTest.java
+++ dhis-2/dhis-services/dhis-service-core/src/test/java/org/hisp/dhis/program/ProgramSqlGeneratorItemsTest.java
@@ -31,6 +31,7 @@ package org.hisp.dhis.program;
 import com.google.common.collect.ImmutableMap;
 import org.hisp.dhis.DhisConvenienceTest;
 import org.hisp.dhis.common.ValueType;
+import org.hisp.dhis.constant.Constant;
 import org.hisp.dhis.constant.ConstantService;
 import org.hisp.dhis.dataelement.DataElement;
 import org.hisp.dhis.dataelement.DataElementDomain;
@@ -39,9 +40,8 @@ import org.hisp.dhis.i18n.I18n;
 import org.hisp.dhis.jdbc.StatementBuilder;
 import org.hisp.dhis.jdbc.statementbuilder.PostgreSQLStatementBuilder;
 import org.hisp.dhis.organisationunit.OrganisationUnit;
-import org.hisp.dhis.parser.expression.CommonExpressionVisitor;
-import org.hisp.dhis.parser.expression.Parser;
-import org.hisp.dhis.parser.expression.ParserException;
+import org.hisp.dhis.parser.expression.*;
+import org.hisp.dhis.parser.expression.literal.DefaultLiteral;
 import org.hisp.dhis.parser.expression.literal.SqlLiteral;
 import org.hisp.dhis.relationship.RelationshipTypeService;
 import org.hisp.dhis.trackedentity.TrackedEntityAttribute;
@@ -71,11 +71,15 @@ public class ProgramSqlGeneratorItemsTest
 {
     private ProgramIndicator programIndicator;
 
+    private ProgramStage programStageA;
+
     private Program programA;
 
     private DataElement dataElementA;
 
     private TrackedEntityAttribute attributeA;
+
+    private Constant constantA;
 
     private Map<String, Double> constantMap;
 
@@ -119,11 +123,18 @@ public class ProgramSqlGeneratorItemsTest
         attributeA = createTrackedEntityAttribute( 'A', ValueType.NUMBER );
         attributeA.setUid( "Attribute0A" );
 
+        constantA = new Constant( "Constant A", 123.456 );
+        constantA.setUid( "constant00A" );
+
         constantMap = new ImmutableMap.Builder<String, Double>()
             .put( "constant00A", 123.456 )
             .build();
 
         OrganisationUnit organisationUnit = createOrganisationUnit( 'A' );
+
+        programStageA = new ProgramStage( "StageA", programA );
+        programStageA.setSortOrder( 1 );
+        programStageA.setUid( "ProgrmStagA" );
 
         programA = createProgram( 'A', new HashSet<>(), organisationUnit );
         programA.setUid( "Program000A" );
@@ -136,6 +147,8 @@ public class ProgramSqlGeneratorItemsTest
 
         when( dataElementService.getDataElement( dataElementA.getUid() ) ).thenReturn( dataElementA );
         when( attributeService.getTrackedEntityAttribute( attributeA.getUid() ) ).thenReturn( attributeA );
+        when( constantService.getConstant( constantA.getUid() ) ).thenReturn( constantA );
+        when( programStageService.getProgramStage( programStageA.getUid() ) ).thenReturn( programStageA );
     }
 
     @Test
@@ -207,11 +220,24 @@ public class ProgramSqlGeneratorItemsTest
 
     private String test( String expression )
     {
+        test( expression, new DefaultLiteral(), FUNCTION_EVALUATE, ITEM_GET_DESCRIPTIONS );
+
+        return castString( test( expression, new SqlLiteral(), FUNCTION_GET_SQL, ITEM_GET_SQL ) );
+    }
+
+    private Object test( String expression, ExprLiteral exprLiteral,
+        ExprFunctionMethod functionMethod, ExprItemMethod itemMethod )
+    {
+        Set<String> dataElementsAndAttributesIdentifiers = new LinkedHashSet<>();
+        dataElementsAndAttributesIdentifiers.add( BASE_UID + "a" );
+        dataElementsAndAttributesIdentifiers.add( BASE_UID + "b" );
+        dataElementsAndAttributesIdentifiers.add( BASE_UID + "c" );
+
         CommonExpressionVisitor visitor = CommonExpressionVisitor.newBuilder()
             .withFunctionMap( PROGRAM_INDICATOR_FUNCTIONS )
             .withItemMap( PROGRAM_INDICATOR_ITEMS )
-            .withFunctionMethod( FUNCTION_GET_SQL )
-            .withItemMethod( ITEM_GET_SQL )
+            .withFunctionMethod( functionMethod )
+            .withItemMethod( itemMethod )
             .withConstantService( constantService )
             .withProgramIndicatorService( programIndicatorService )
             .withProgramStageService( programStageService )
@@ -222,12 +248,13 @@ public class ProgramSqlGeneratorItemsTest
             .withI18n( new I18n( null, null ) )
             .buildForProgramIndicatorExpressions();
 
-        visitor.setExpressionLiteral( new SqlLiteral() );
+        visitor.setExpressionLiteral( exprLiteral );
         visitor.setProgramIndicator( programIndicator );
         visitor.setReportingStartDate( startDate );
         visitor.setReportingEndDate( endDate );
         visitor.setConstantMap( constantMap );
+        visitor.setDataElementAndAttributeIdentifiers( dataElementsAndAttributesIdentifiers );
 
-        return castString( Parser.visit( expression, visitor ) );
+        return Parser.visit( expression, visitor );
     }
 }
