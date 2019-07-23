d02fa2f2068ffc77d09253ce3d2e2669b5c2ec16
Jasper Timm <jasper.timm@ehealthafrica.org>
2019-07-23 16:45:27 +0000

    fix: working test for NewSMSListeners

--- dhis-2/dhis-services/dhis-service-core/src/test/java/org/hisp/dhis/sms/listener/NewSMSListenerTest.java
+++ dhis-2/dhis-services/dhis-service-core/src/test/java/org/hisp/dhis/sms/listener/NewSMSListenerTest.java
@@ -16,14 +16,17 @@ import java.util.Date;
 
 import org.hisp.dhis.DhisConvenienceTest;
 import org.hisp.dhis.category.CategoryService;
+import org.hisp.dhis.common.ValueType;
 import org.hisp.dhis.dataelement.DataElementService;
 import org.hisp.dhis.message.MessageSender;
 import org.hisp.dhis.organisationunit.OrganisationUnit;
 import org.hisp.dhis.organisationunit.OrganisationUnitService;
 import org.hisp.dhis.outboundmessage.OutboundMessageResponse;
 import org.hisp.dhis.program.Program;
+import org.hisp.dhis.program.ProgramInstance;
 import org.hisp.dhis.program.ProgramInstanceService;
 import org.hisp.dhis.program.ProgramService;
+import org.hisp.dhis.program.ProgramTrackedEntityAttribute;
 import org.hisp.dhis.sms.incoming.IncomingSms;
 import org.hisp.dhis.sms.incoming.IncomingSmsService;
 import org.hisp.dhis.smscompression.SMSCompressionException;
@@ -31,10 +34,13 @@ import org.hisp.dhis.smscompression.SMSSubmissionWriter;
 import org.hisp.dhis.smscompression.models.EnrollmentSMSSubmission;
 import org.hisp.dhis.smscompression.models.SMSAttributeValue;
 import org.hisp.dhis.smscompression.models.SMSMetadata;
+import org.hisp.dhis.trackedentity.TrackedEntityAttribute;
 import org.hisp.dhis.trackedentity.TrackedEntityAttributeService;
+import org.hisp.dhis.trackedentity.TrackedEntityInstance;
 import org.hisp.dhis.trackedentity.TrackedEntityInstanceService;
 import org.hisp.dhis.trackedentity.TrackedEntityType;
 import org.hisp.dhis.trackedentity.TrackedEntityTypeService;
+import org.hisp.dhis.trackedentityattributevalue.TrackedEntityAttributeValue;
 import org.hisp.dhis.user.User;
 import org.hisp.dhis.user.UserService;
 import org.junit.Before;
@@ -51,9 +57,11 @@ public class NewSMSListenerTest
     extends
     DhisConvenienceTest
 {
-    private static final String SUCCESS_MESSAGE = "Command has been processed successfully";
+    private static final String SUCCESS_MESSAGE = "1:0::Submission has been processed successfully";
 
     private static final String ORIGINATOR = "47400000";
+
+    private static final String ATTRIBUTE_VALUE = "TEST";
 
     @Rule
     public MockitoRule rule = MockitoJUnit.rule();
@@ -104,6 +112,16 @@ public class NewSMSListenerTest
 
     private Program program;
 
+    private ProgramInstance programInstance;
+
+    private TrackedEntityInstance trackedEntityInstance;
+
+    private TrackedEntityAttribute trackedEntityAttribute;
+
+    private TrackedEntityAttributeValue trackedEntityAttributeValue;
+
+    private ProgramTrackedEntityAttribute programTrackedEntityAttribute;
+
     private TrackedEntityType trackedEntityType;
 
     private User user;
@@ -119,8 +137,13 @@ public class NewSMSListenerTest
         setUpInstances();
 
         when( userService.getUser( anyString() ) ).thenReturn( user );
-        when( programService.getProgram( (anyString()) ) ).thenReturn( program );
-        when( trackedEntityTypeService.getTrackedEntityType( (anyString()) ) ).thenReturn( trackedEntityType );
+        when( programService.getProgram( anyString() ) ).thenReturn( program );
+        when( organisationUnitService.getOrganisationUnit( anyString() ) ).thenReturn( organisationUnit );
+        when( trackedEntityTypeService.getTrackedEntityType( anyString() ) ).thenReturn( trackedEntityType );
+        when( trackedEntityAttributeService.getTrackedEntityAttribute( anyString() ) )
+            .thenReturn( trackedEntityAttribute );
+        when( programInstanceService.enrollTrackedEntityInstance( any(), any(), any(), any(), any() ) )
+            .thenReturn( programInstance );
         when( smsSender.isConfigured() ).thenReturn( true );
         when( smsSender.sendMessage( any(), any(), anyString() ) ).thenAnswer( invocation -> {
             message = (String) invocation.getArguments()[1];
@@ -150,14 +173,28 @@ public class NewSMSListenerTest
     {
         trackedEntityType = createTrackedEntityType( 'T' );
         organisationUnit = createOrganisationUnit( 'O' );
-
         program = createProgram( 'P' );
-        program.getOrganisationUnits().add( organisationUnit );
-        program.setTrackedEntityType( trackedEntityType );
 
         user = createUser( 'U' );
         user.setPhoneNumber( ORIGINATOR );
         user.setOrganisationUnits( Sets.newHashSet( organisationUnit ) );
+
+        programTrackedEntityAttribute = createProgramTrackedEntityAttribute( 'Q' );
+        trackedEntityAttribute = createTrackedEntityAttribute( 'A', ValueType.TEXT );
+        program.getProgramAttributes().add( programTrackedEntityAttribute );
+        program.getOrganisationUnits().add( organisationUnit );
+        program.setTrackedEntityType( trackedEntityType );
+
+        programInstance = new ProgramInstance();
+        programInstance.setAutoFields();
+
+        trackedEntityInstance = createTrackedEntityInstance( organisationUnit );
+        trackedEntityInstance.getTrackedEntityAttributeValues().add( trackedEntityAttributeValue );
+        trackedEntityInstance.setOrganisationUnit( organisationUnit );
+
+        trackedEntityAttributeValue = createTrackedEntityAttributeValue( 'A', trackedEntityInstance,
+            trackedEntityAttribute );
+        trackedEntityAttributeValue.setValue( ATTRIBUTE_VALUE );
 
         EnrollmentSMSSubmission subm = createEnrollmentSubmission();
         SMSMetadata meta = new SMSMetadata();
@@ -175,15 +212,15 @@ public class NewSMSListenerTest
     {
         EnrollmentSMSSubmission subm = new EnrollmentSMSSubmission();
 
-        subm.setUserID( "uuuuuuuuuuu" );
-        subm.setOrgUnit( "ooooooooooo" );
-        subm.setTrackerProgram( "ppppppppppp" );
-        subm.setTrackedEntityType( "ttttttttttt" );
-        subm.setTrackedEntityInstance( "iiiiiiiiiii" );
-        subm.setEnrollment( "eeeeeeeeeee" );
+        subm.setUserID( user.getUid() );
+        subm.setOrgUnit( organisationUnit.getUid() );
+        subm.setTrackerProgram( program.getUid() );
+        subm.setTrackedEntityType( trackedEntityType.getUid() );
+        subm.setTrackedEntityInstance( trackedEntityInstance.getUid() );
+        subm.setEnrollment( programInstance.getUid() );
         subm.setTimestamp( new Date() );
         ArrayList<SMSAttributeValue> values = new ArrayList<>();
-        values.add( new SMSAttributeValue( "attr", "val" ) );
+        values.add( new SMSAttributeValue( trackedEntityAttribute.getUid(), ATTRIBUTE_VALUE ) );
         subm.setValues( values );
         subm.setSubmissionID( 1 );
 
