84c78ab57586327e732c07647cdca72b71c511cd
Stian Sandvold <stian@1x-193-157-186-49.uio.no>
2019-08-01 08:11:13 +0200

    Remove unwanted text in code

--- dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/FileResourceController.java
+++ dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/FileResourceController.java
@@ -131,13 +131,6 @@ public class FileResourceController
         response.setContentLength( new Long( fileResource.getContentLength() ).intValue() );
         response.setHeader( HttpHeaders.CONTENT_DISPOSITION, "filename=" + fileResource.getName() );
 
-<<<<<<< HEAD
-        // ---------------------------------------------------------------------
-        // Request signing is not available, stream content back to client
-        // ---------------------------------------------------------------------
-
-=======
->>>>>>> 902323aebeee3d76a0daeb12f9b4254fbd23dc03
         try
         {
             fileResourceService.copyFileResourceContent( fileResource, response.getOutputStream() );
