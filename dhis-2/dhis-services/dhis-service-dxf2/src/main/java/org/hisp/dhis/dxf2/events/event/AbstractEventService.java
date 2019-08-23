47c81188cfac115a9edff25b1f7a397cd27ac25b
Morten Olav Hansen <morten@dhis2.org>
2019-08-23 14:13:50 +0700

    Use correct events collection in event service

--- dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/events/event/AbstractEventService.java
+++ dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/events/event/AbstractEventService.java
@@ -299,11 +299,11 @@ public abstract class AbstractEventService
 
             if ( importOptions.getImportStrategy().isCreate() )
             {
-                create.addAll( events );
+                create.addAll( _events );
             }
             else if ( importOptions.getImportStrategy().isCreateAndUpdate() )
             {
-                for ( Event event : events )
+                for ( Event event : _events )
                 {
                     if ( StringUtils.isEmpty( event.getEvent() ) )
                     {
@@ -337,7 +337,7 @@ public abstract class AbstractEventService
             }
             else if ( importOptions.getImportStrategy().isDelete() )
             {
-                delete.addAll( events.stream().map( Event::getEvent ).collect( Collectors.toList() ) );
+                delete.addAll( _events.stream().map( Event::getEvent ).collect( Collectors.toList() ) );
             }
 
             importSummaries.addImportSummaries( addEvents( create, importOptions, true ) );
