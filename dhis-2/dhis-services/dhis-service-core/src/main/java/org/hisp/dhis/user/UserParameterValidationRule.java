94382e184f6204f3f2e3c27b554ef1e66da06013
zubaira <zubair@dhis2.org>
2019-09-05 16:57:10 +0200

    password validation fix

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/user/UserParameterValidationRule.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/user/UserParameterValidationRule.java
@@ -54,7 +54,8 @@ public class UserParameterValidationRule
         String password = credentialsInfo.getPassword();
         String username = credentialsInfo.getUsername();
 
-        if ( StringUtils.isBlank( email ) || StringUtils.isBlank( password ) || StringUtils.isBlank( username ) )
+        // will be skipped in case of new user
+        if ( !credentialsInfo.isNewUser() && ( StringUtils.isBlank( email ) || StringUtils.isBlank( password ) || StringUtils.isBlank( username ) ) )
         {
             return new PasswordValidationResult( MANDATORY_PARAMETER_MISSING, I18_MANDATORY_PARAMETER_MISSING, false );
         }
