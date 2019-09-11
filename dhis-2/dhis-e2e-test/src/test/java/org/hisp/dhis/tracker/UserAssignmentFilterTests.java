b8f8ecfa759ff6fce62f15fef16e96c563b45432
Gintare Vilkelyte <vilkelyte.gintare@gmail.com>
2019-09-11 14:48:42 +0200

    Alligned orgUnit names and ids with SL db.

--- dhis-2/dhis-e2e-test/src/test/java/org/hisp/dhis/tracker/UserAssignmentFilterTests.java
+++ dhis-2/dhis-e2e-test/src/test/java/org/hisp/dhis/tracker/UserAssignmentFilterTests.java
@@ -67,7 +67,8 @@ public class UserAssignmentFilterTests
     private String userUsername;
 
     private String programId = "BJ42SUrAvHo";
-    private String orgUnit = "r16njpPwUCb";
+
+    private String orgUnit = "O6uvpzGd5pu";
 
     private String userId;
 
