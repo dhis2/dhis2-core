d9ea6ee95f904f9a7e508168628f02df2281de79
Enrico Colasante <enrico.colasante@gmail.com>
2019-09-20 09:28:10 +0200

    Checkstyle fixes

--- dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/events/enrollment/AbstractEnrollmentService.java
+++ dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/events/enrollment/AbstractEnrollmentService.java
@@ -659,7 +659,7 @@ public abstract class AbstractEnrollmentService
             importSummary.setConflicts( importConflicts );
             importSummary.setStatus( ImportStatus.ERROR );
             importSummary.incrementIgnored();
-            importSummary.setReference(enrollment.getEnrollment());
+            importSummary.setReference( enrollment.getEnrollment() );
         }
 
         return importSummary;
@@ -726,7 +726,7 @@ public abstract class AbstractEnrollmentService
         {
             ImportSummary importSummary = new ImportSummary( ImportStatus.ERROR ).incrementIgnored();
             importSummary.setConflicts( importConflicts );
-            importSummary.setReference(enrollment.getEnrollment());
+            importSummary.setReference( enrollment.getEnrollment() );
             return importSummary;
         }
 
