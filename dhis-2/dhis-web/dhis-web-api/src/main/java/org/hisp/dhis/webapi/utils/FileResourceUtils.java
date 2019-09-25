653fee433105a227867a1877aaaa1a52a89579c1
Lars Helge Øverland <lars@dhis2.org>
2019-09-25 12:47:57 +0200

    Improve logging (#3833)

--- dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/utils/FileResourceUtils.java
+++ dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/utils/FileResourceUtils.java
@@ -33,7 +33,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.util.Date;
-import java.util.Optional;
 
 import javax.servlet.http.HttpServletResponse;
 
