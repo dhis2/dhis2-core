c677f81cf160f132a8aaf5c3281cf1b9bffc102a
zubaira <zubair@dhis2.org>
2019-09-26 11:32:01 +0200

    minor

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/user/CurrentUserService.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/user/CurrentUserService.java
@@ -94,5 +94,5 @@ public interface CurrentUserService
     /**
      * Expire all the sessions associated with current user.
      */
-    public void expireUserSessions();
+    void expireUserSessions();
 }
