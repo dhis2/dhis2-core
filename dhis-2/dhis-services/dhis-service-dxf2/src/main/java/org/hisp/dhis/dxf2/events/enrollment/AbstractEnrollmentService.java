f8c502bec746d4e55c2ce8e30287c4210d8b3225
Morten Olav Hansen <mortenoh@gmail.com>
2019-09-27 11:06:56 +0700

    Optimize cache for tei/en in tracker services

--- dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/events/enrollment/AbstractEnrollmentService.java
+++ dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/events/enrollment/AbstractEnrollmentService.java
@@ -182,6 +182,8 @@ public abstract class AbstractEnrollmentService
 
     private CachingMap<String, TrackedEntityAttribute> trackedEntityAttributeCache = new CachingMap<>();
 
+    private CachingMap<String, org.hisp.dhis.trackedentity.TrackedEntityInstance> trackedEntityInstanceCache = new CachingMap<>();
+
     // -------------------------------------------------------------------------
     // READ
     // -------------------------------------------------------------------------
@@ -383,7 +385,7 @@ public abstract class AbstractEnrollmentService
 
         List<Enrollment> validEnrollments = enrollments.stream()
             .filter( e -> !conflictingEnrollmentUids.contains( e.getEnrollment() ) )
-            .collect( Collectors.toList());
+            .collect( Collectors.toList() );
 
         List<List<Enrollment>> partitions = Lists.partition( validEnrollments, FLUSH_FREQUENCY );
 
@@ -618,7 +620,7 @@ public abstract class AbstractEnrollmentService
 
             Set<Enrollment> activeEnrollments = enrollments.stream()
                 .filter( e -> e.getStatus() == EnrollmentStatus.ACTIVE )
-                .collect( Collectors.toSet());
+                .collect( Collectors.toSet() );
 
             // When an enrollment with status COMPLETED or CANCELLED is being imported, no check whether there is already some ACTIVE one is needed
             if ( !activeEnrollments.isEmpty() && enrollment.getStatus() == EnrollmentStatus.ACTIVE )
@@ -637,7 +639,7 @@ public abstract class AbstractEnrollmentService
 
                 Set<Enrollment> activeOrCompletedEnrollments = enrollments.stream()
                     .filter( e -> e.getStatus() == EnrollmentStatus.ACTIVE || e.getStatus() == EnrollmentStatus.COMPLETED )
-                    .collect( Collectors.toSet());
+                    .collect( Collectors.toSet() );
 
                 if ( !activeOrCompletedEnrollments.isEmpty() )
                 {
@@ -1024,6 +1026,16 @@ public abstract class AbstractEnrollmentService
             query.setUser( user );
             query.add( Restrictions.in( "id", trackedEntityAttributes ) );
             queryService.query( query ).forEach( tea -> trackedEntityAttributeCache.put( tea.getUid(), (TrackedEntityAttribute) tea ) );
+        }
+
+        Collection<String> trackedEntityInstances = enrollments.stream().map( Enrollment::getTrackedEntityInstance ).collect( Collectors.toList() );
+
+        if ( !trackedEntityInstances.isEmpty() )
+        {
+            Query query = Query.from( schemaService.getDynamicSchema( org.hisp.dhis.trackedentity.TrackedEntityInstance.class ) );
+            query.setUser( user );
+            query.add( Restrictions.in( "id", trackedEntityInstances ) );
+            queryService.query( query ).forEach( te -> trackedEntityInstanceCache.put( te.getUid(), (org.hisp.dhis.trackedentity.TrackedEntityInstance) te ) );
         }
     }
 
