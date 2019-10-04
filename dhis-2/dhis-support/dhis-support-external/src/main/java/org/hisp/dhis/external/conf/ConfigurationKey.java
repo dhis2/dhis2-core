31bf1f01c7c3aeba1f6c6a07b3ccf8e872a12eeb
Viktor Varland <viktor@dhis2.org>
2019-10-04 16:52:23 +0200

    chore: update dhis-2/dhis-support/dhis-support-external/src/main/java/org/hisp/dhis/external/conf/ConfigurationKey.java
    Co-Authored-By: Stian Sandvold <stian@dhis2.org>

--- dhis-2/dhis-support/dhis-support-external/src/main/java/org/hisp/dhis/external/conf/ConfigurationKey.java
+++ dhis-2/dhis-support/dhis-support-external/src/main/java/org/hisp/dhis/external/conf/ConfigurationKey.java
@@ -101,7 +101,7 @@ public enum ConfigurationKey
     MONITORING_UPTIME_ENABLED( "monitoring.uptime.enabled", "off", false ),
     MONITORING_CPU_ENABLED( "monitoring.cpu.enabled", "off", false ),
     APP_STORE_URL( "appstore.base.url", "https://play.dhis2.org/appstore", false ),
-    APP_STORE_API_URL( "appstore.api.url", "https://play.dhis2.org/appstore/api", false),
+    APP_STORE_API_URL( "appstore.api.url", "https://play.dhis2.org/appstore/api", false ),
     APP_STORE_API_VERSION("appstore.api.version", "", false);
 
     private final String key;
