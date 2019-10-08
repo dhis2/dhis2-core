a905142896b67cb1b75b02ad85b39e8e912b4c02
Gintare Vilkelyte <vilkelyte.gintare@gmail.com>
2019-10-08 15:47:55 +0200

    Moving system actions

--- dhis-2/dhis-e2e-test/src/main/java/org/hisp/dhis/actions/SystemActions.java
+++ dhis-2/dhis-e2e-test/src/main/java/org/hisp/dhis/actions/SystemActions.java
@@ -25,9 +25,8 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package org.hisp.dhis.actions.system;
+package org.hisp.dhis.actions;
 
-import org.hisp.dhis.actions.RestApiActions;
 import org.hisp.dhis.dto.ApiResponse;
 import org.hisp.dhis.dto.ImportSummary;
 
