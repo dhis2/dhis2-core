c525cb89a71f13baaf4033140879a32199585158
Viktor Varland <viktor@dhis2.org>
2019-10-08 15:19:24 +0000

    refactor(docker): use a docker build script

---
+++ docker-build.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+#
+## bash environment
+#
+if test "$BASH" = "" || "$BASH" -uc "a=();true \"\${a[@]}\"" 2>/dev/null; then
+    # Bash 4.4, Zsh
+    set -euo pipefail
+else
+    # Bash 4.3 and older chokes on empty arrays with set -u.
+    set -eo pipefail
+fi
+shopt -s nullglob globstar
+
+#
+## script environment
+#
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+TOMCAT_IMAGE="tomcat"
+
+#
+## tomcat tags
+#
+
+latest_tomcat_tags=(
+    "9.0-jdk8-openjdk-slim"
+    "8.5-jdk8-openjdk-slim"
+    "8.0-jre8-slim"
+)
+
+#
+## builds
+#
+
+docker build --tag "${CORE_IMAGE}:${CORE_TAG}" --file "${DIR}/docker/core/Dockerfile" "$DIR"
+
+for TOMCAT_TAG in "${latest_tomcat_tags[@]}"; do
+    docker build \
+        --tag "${CORE_IMAGE}:${CORE_TAG}-${TOMCAT_TAG}" \
+        --file "${DIR}/docker/tomcat-debian/Dockerfile" "$DIR" \
+        --build-arg CORE_IMAGE="${CORE_IMAGE}:${CORE_TAG}" \
+        --build-arg TOMCAT_IMAGE="${TOMCAT_IMAGE}:${TOMCAT_TAG}"
+done
