6be7c33f5801e04f09566c21e71f6909999f0daa
Morten Olav Hansen <mortenoh@gmail.com>
2019-10-15 10:31:24 +0700

    minor fix

--- dhis-2/dhis-services/dhis-service-schema/src/main/java/org/hisp/dhis/schema/audit/MetadataAudit.java
+++ dhis-2/dhis-services/dhis-service-schema/src/main/java/org/hisp/dhis/schema/audit/MetadataAudit.java
@@ -29,7 +29,6 @@ package org.hisp.dhis.schema.audit;
  */
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonRawValue;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import com.google.common.base.MoreObjects;
