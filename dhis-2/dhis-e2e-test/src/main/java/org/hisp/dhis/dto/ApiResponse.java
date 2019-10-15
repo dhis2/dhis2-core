321f6d0153dbb7f5d17d3859026933e324e6bb9d
Gintare Vilkelyte <vilkelyte.gintare@gmail.com>
2019-10-15 15:24:08 +0200

    Fix: removes additional method introduced with merge

--- dhis-2/dhis-e2e-test/src/main/java/org/hisp/dhis/dto/ApiResponse.java
+++ dhis-2/dhis-e2e-test/src/main/java/org/hisp/dhis/dto/ApiResponse.java
@@ -170,11 +170,6 @@ public class ApiResponse
         return raw.asString();
     }
 
-    public String getAsString()
-    {
-        return raw.asString();
-    }
-
     public String getContentType()
     {
         return raw.getContentType();
