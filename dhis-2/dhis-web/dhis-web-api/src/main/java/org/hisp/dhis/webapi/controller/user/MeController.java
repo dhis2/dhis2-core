4473dfc978d089183d1edf86208ba4ce32b06cb0
Zubair Asghar <zubair@dhis2.org>
2019-10-17 14:42:43 +0200

    fix: Expire concurrent user sessions at password change (#3851)

--- dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/user/MeController.java
+++ dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/user/MeController.java
@@ -385,6 +385,8 @@ public class MeController
 
         updatePassword( currentUser, newPassword );
         manager.update( currentUser );
+
+        currentUserService.expireUserSessions();
     }
 
     @RequestMapping( value = "/verifyPassword", method = RequestMethod.POST, consumes = "text/*" )
