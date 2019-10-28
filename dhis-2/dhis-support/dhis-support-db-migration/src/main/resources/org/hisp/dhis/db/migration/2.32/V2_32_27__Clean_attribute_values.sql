6dff997d1fa0e41a382fe4e00f52c3203ba43bba
Restyled.io <commits@restyled.io>
2019-10-28 08:34:05 +0000

    Restyled by pg_format

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.32/V2_32_27__Clean_attribute_values.sql
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.32/V2_32_27__Clean_attribute_values.sql
@@ -1,13 +1,87 @@
+WITH nonRepeatedIds AS (
+    SELECT
+        min(av2.attributevalueid) AS id
+    FROM
+        attributevalue av2
+    LEFT JOIN dataelementattributevalues deav2 ON av2.attributevalueid = deav2.attributevalueid
+GROUP BY
+    deav2.dataelementid,
+    av2.value,
+    av2.attributeid
+),
+uniqueAttributeIds AS (
+    SELECT
+        av.attributevalueid AS id
+    FROM
+        attributevalue av
+        JOIN dataelementattributevalues deav ON av.attributevalueid = deav.attributevalueid
+    WHERE (dataelementid, attributeid, value)
+    IN (
+        SELECT
+            deav2.dataelementid, av2.attributeid, max(av2.value) AS value
+        FROM
+            attributevalue av2
+        LEFT JOIN dataelementattributevalues deav2 ON av2.attributevalueid = deav2.attributevalueid
+    GROUP BY
+        deav2.dataelementid,
+        av2.attributeid)
+),
+orphanAttributeValues AS (
+    SELECT
+        av.attributevalueid AS id
+    FROM
+        attributevalue av
+    LEFT JOIN dataelementattributevalues deav ON av.attributevalueid = deav.attributevalueid
+WHERE
+    deav.dataelementid IS NULL
+),
+relationsDeleted AS (
+    DELETE FROM dataelementattributevalues deav
+    WHERE deav.attributevalueid NOT IN (
+            SELECT
+                id
+            FROM
+                nonRepeatedIds
+            UNION
+            SELECT
+                id
+            FROM
+                uniqueAttributeIds)
+        RETURNING
+            attributevalueid
+),
+categoryOptionComboDeleted AS (
+    DELETE FROM categoryoptioncomboattributevalues
+    WHERE attributevalueid IN (
+            SELECT
+                id
+            FROM
+                orphanAttributeValues)
+),
+indicatorDeleted AS (
+    DELETE FROM indicatorattributevalues
+    WHERE attributevalueid IN (
+            SELECT
+                id
+            FROM
+                orphanAttributeValues)
+),
+attributeValuesDeleted AS (
+    DELETE FROM attributevalue
+    WHERE attributevalueid IN (
+            SELECT
+                id
+            FROM
+                orphanAttributeValues)
+) DELETE FROM attributevalue av
+WHERE av.attributevalueid NOT IN (
+        SELECT
+            id
+        FROM
+            nonRepeatedIds
+        UNION
+        SELECT
+            id
+        FROM
+            uniqueAttributeIds);
 
-WITH nonRepeatedIds AS (select min(av2.attributevalueid) as id from attributevalue av2 LEFT JOIN dataelementattributevalues deav2 ON av2.attributevalueid = deav2.attributevalueid
-        group by deav2.dataelementid, av2.value, av2.attributeid),
-    uniqueAttributeIds AS (select av.attributevalueid as id from attributevalue av JOIN dataelementattributevalues deav ON av.attributevalueid = deav.attributevalueid WHERE (dataelementid, attributeid, value) IN
-                            (select deav2.dataelementid, av2.attributeid, max(av2.value) as value from attributevalue av2 LEFT JOIN dataelementattributevalues deav2 ON av2.attributevalueid = deav2.attributevalueid
-                    group by deav2.dataelementid, av2.attributeid)),
-    orphanAttributeValues AS (select av.attributevalueid as id from attributevalue av LEFT JOIN dataelementattributevalues deav ON av.attributevalueid = deav.attributevalueid WHERE deav.dataelementid IS NULL),
-    relationsDeleted AS (delete from dataelementattributevalues deav where deav.attributevalueid NOT IN (SELECT id FROM nonRepeatedIds UNION SELECT id FROM uniqueAttributeIds) returning attributevalueid),
-    categoryOptionComboDeleted AS (delete from categoryoptioncomboattributevalues where attributevalueid in (select id from orphanAttributeValues)),
-    indicatorDeleted AS (delete from indicatorattributevalues where attributevalueid in (select id from orphanAttributeValues)),
-    attributeValuesDeleted AS (delete from attributevalue where attributevalueid in (select id from orphanAttributeValues))
-delete from attributevalue av
-where av.attributevalueid NOT IN (SELECT id FROM nonRepeatedIds UNION SELECT id FROM uniqueAttributeIds);
