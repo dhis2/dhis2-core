32b232a3f831e2e74f7af00f7d0a31ccdba99b89
Morten Olav Hansen <morten@dhis2.org>
2019-11-18 10:53:05 +0700

    fix(dhis2-7367) Updates deps to make dhis2 jdk11 compatible (#4187)
    * updates dep to make branch jdk11 compatible
    
    * lock maven version requirement with maven enforcer
    
    * minor version updates

--- dhis-2/dhis-support/dhis-support-test/src/main/java/org/hisp/dhis/DhisConvenienceTest.java
+++ dhis-2/dhis-support/dhis-support-test/src/main/java/org/hisp/dhis/DhisConvenienceTest.java
@@ -176,7 +176,7 @@ import java.util.stream.Collectors;
 /**
  * @author Lars Helge Overland
  */
-@ActiveProfiles( profiles = {"test"} )
+@ActiveProfiles( profiles = { "test" } )
 public abstract class DhisConvenienceTest
 {
     protected static final Log log = LogFactory.getLog( DhisConvenienceTest.class );
@@ -527,7 +527,7 @@ public abstract class DhisConvenienceTest
     }
 
     /**
-     * @param categoryCombo the category combo.
+     * @param categoryCombo   the category combo.
      * @param categoryOptions the category options.
      * @return CategoryOptionCombo
      */
@@ -827,11 +827,11 @@ public abstract class DhisConvenienceTest
         return unit;
     }
 
-    public static OrganisationUnit createOrganisationUnit(char uniqueCharacter, Geometry geometry)
+    public static OrganisationUnit createOrganisationUnit( char uniqueCharacter, Geometry geometry )
     {
         OrganisationUnit unit = createOrganisationUnit( uniqueCharacter );
 
-        unit.setGeometry(geometry);
+        unit.setGeometry( geometry );
 
         return unit;
     }
@@ -1092,8 +1092,8 @@ public abstract class DhisConvenienceTest
     }
 
     /**
-     * @param uniqueCharacter          A unique character to identify the object.
-     * @param expressionString         The expression string.
+     * @param uniqueCharacter  A unique character to identify the object.
+     * @param expressionString The expression string.
      */
     public static Expression createExpression2( char uniqueCharacter, String expressionString )
     {
@@ -1325,7 +1325,7 @@ public abstract class DhisConvenienceTest
 
     public static UserAuthorityGroup createUserAuthorityGroup( char uniqueCharacter )
     {
-        return createUserAuthorityGroup( uniqueCharacter, new String[] {} );
+        return createUserAuthorityGroup( uniqueCharacter, new String[]{} );
     }
 
     public static UserAuthorityGroup createUserAuthorityGroup( char uniqueCharacter, String... auths )
@@ -1748,7 +1748,7 @@ public abstract class DhisConvenienceTest
 
     /**
      * @param uniqueChar A unique character to identify the object.
-     * @param content The content of the file
+     * @param content    The content of the file
      * @return a fileResource object
      */
     public static FileResource createFileResource( char uniqueChar, byte[] content )
@@ -1767,7 +1767,7 @@ public abstract class DhisConvenienceTest
 
     /**
      * @param uniqueChar A unique character to identify the object.
-     * @param content The content of the file
+     * @param content    The content of the file
      * @return an externalFileResource object
      */
     public static ExternalFileResource createExternalFileResource( char uniqueChar, byte[] content )
@@ -1832,17 +1832,17 @@ public abstract class DhisConvenienceTest
     }
 
     public static ProgramNotificationTemplate createProgramNotificationTemplate(
-            String name, int days, NotificationTrigger trigger, ProgramNotificationRecipient recipient, Date scheduledDate )
+        String name, int days, NotificationTrigger trigger, ProgramNotificationRecipient recipient, Date scheduledDate )
     {
         return new ProgramNotificationTemplate(
-                name,
-                "Subject",
-                "Message",
-                trigger,
-                recipient,
-                Sets.newHashSet(),
-                days,
-                null, null
+            name,
+            "Subject",
+            "Message",
+            trigger,
+            recipient,
+            Sets.newHashSet(),
+            days,
+            null, null
         );
     }
 
@@ -2032,7 +2032,7 @@ public abstract class DhisConvenienceTest
         }
 
         @Override
-        public Iterator<?> getPrefixes( String namespaceURI )
+        public Iterator<String> getPrefixes( String namespaceURI )
         {
             return null;
         }
