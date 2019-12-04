fcd0b531e2184116a49cc6a077f3be9d3fb0cb0a
Zubair Asghar <zubair@dhis2.org>
2019-12-04 13:34:54 +0100

    fix: flyway migration conflict (#4431)

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/java/org/hisp/dhis/db/migration/v34/V2_34_7__Convert_push_analysis_job_parameters_into_list_of_string.java
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/java/org/hisp/dhis/db/migration/v34/V2_34_7__Convert_push_analysis_job_parameters_into_list_of_string.java
@@ -50,9 +50,9 @@ import java.sql.Statement;
 /**
  * @Author Zubair Asghar.
  */
-public class V2_34_6__Convert_push_analysis_job_parameters_into_list_of_string extends BaseJavaMigration
+public class V2_34_7__Convert_push_analysis_job_parameters_into_list_of_string extends BaseJavaMigration
 {
-    private static final Log log = LogFactory.getLog( V2_34_6__Convert_push_analysis_job_parameters_into_list_of_string.class );
+    private static final Log log = LogFactory.getLog( V2_34_7__Convert_push_analysis_job_parameters_into_list_of_string.class );
 
     @Override
     public void migrate( Context context ) throws Exception
