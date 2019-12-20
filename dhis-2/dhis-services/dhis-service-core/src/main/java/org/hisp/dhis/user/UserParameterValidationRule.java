c4f91ccd178b9132ed70f50417d43a7c7ab2fb04
zubaira <zubair@dhis2.org>
2019-12-20 09:02:01 +0100

    fix: user registration issue

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/user/UserParameterValidationRule.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/user/UserParameterValidationRule.java
@@ -52,7 +52,15 @@ public class UserParameterValidationRule
         String password = credentialsInfo.getPassword();
         String username = credentialsInfo.getUsername();
 
-        if ( StringUtils.isBlank( password ) || StringUtils.isBlank( username ) )
+        // other parameters will be skipped in case of new user
+        if ( credentialsInfo.isNewUser() )
+        {
+            if ( StringUtils.isBlank( password ) )
+            {
+                return new PasswordValidationResult( MANDATORY_PARAMETER_MISSING, I18_MANDATORY_PARAMETER_MISSING, false );
+            }
+        }
+        else if ( StringUtils.isBlank( password ) || StringUtils.isBlank( username ) )
         {
             return new PasswordValidationResult( MANDATORY_PARAMETER_MISSING, I18_MANDATORY_PARAMETER_MISSING, false );
         }
