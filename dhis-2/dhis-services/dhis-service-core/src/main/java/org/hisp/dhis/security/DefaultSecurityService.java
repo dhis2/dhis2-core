42a95cb2edd16b3f5d078823c99462d7f71d98b3
Viet Nguyen <viet@dhis2.org>
2020-01-03 19:45:33 +0700

    fix: minor method name fix for utf8UrlEncode

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/security/DefaultSecurityService.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/security/DefaultSecurityService.java
@@ -307,7 +307,7 @@ public class DefaultSecurityService
         vars.put( "applicationTitle", applicationTitle );
         vars.put( "restorePath", rootPath + RESTORE_PATH + restoreType.getAction() );
         vars.put( "token", result[0] );
-        vars.put( "username", CodecUtils.utf8UrLEncode( credentials.getUsername() ) );
+        vars.put( "username", CodecUtils.utf8UrlEncode( credentials.getUsername() ) );
         vars.put( "welcomeMessage", credentials.getUserInfo().getWelcomeMessage() );
 
         User user = credentials.getUserInfo();
