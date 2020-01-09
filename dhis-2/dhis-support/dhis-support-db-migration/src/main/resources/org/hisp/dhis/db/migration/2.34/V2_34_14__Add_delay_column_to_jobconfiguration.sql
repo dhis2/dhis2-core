099c9fcf6071ecbe85773906f000363d37769d27
Lars Helge Øverland <lars@dhis2.org>
2020-01-09 13:19:00 +0100

    SQL

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.34/V2_34_14__Add_delay_column_to_jobconfiguration.sql
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.34/V2_34_14__Add_delay_column_to_jobconfiguration.sql
@@ -1,2 +1,8 @@
 
+-- Add "delay" column
+
 alter table jobconfiguration add column "delay" integer;
+
+-- Remove not null constraint on "cronexpression" column
+
+alter table jobconfiguration alter column cronexpression drop not null;
