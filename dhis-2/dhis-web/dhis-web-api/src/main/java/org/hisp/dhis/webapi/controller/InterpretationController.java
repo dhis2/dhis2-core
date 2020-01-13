b0becbb50dc846127e62fc3317f064e53a565c82
Maikel Arabori <maikelarabori@gmail.com>
2020-01-13 13:53:22 +0100

    feat: Make Interpretation backward compatible [DHIS2-7946]

--- dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/InterpretationController.java
+++ dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/InterpretationController.java
@@ -29,6 +29,7 @@ package org.hisp.dhis.webapi.controller;
  */
 
 import com.google.common.collect.Lists;
+import org.hisp.dhis.chart.Chart;
 import org.hisp.dhis.common.AnalyticalObject;
 import org.hisp.dhis.common.IdentifiableObjectManager;
 import org.hisp.dhis.dataset.DataSet;
@@ -50,10 +51,12 @@ import org.hisp.dhis.query.Order;
 import org.hisp.dhis.query.Query;
 import org.hisp.dhis.query.QueryParserException;
 import org.hisp.dhis.query.Restrictions;
+import org.hisp.dhis.reporttable.ReportTable;
 import org.hisp.dhis.schema.Schema;
 import org.hisp.dhis.schema.descriptors.InterpretationSchemaDescriptor;
 import org.hisp.dhis.user.User;
 import org.hisp.dhis.visualization.Visualization;
+import org.hisp.dhis.visualization.VisualizationType;
 import org.hisp.dhis.webapi.webdomain.WebMetadata;
 import org.hisp.dhis.webapi.webdomain.WebOptions;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -74,6 +77,8 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Collection;
+
+import static org.apache.commons.collections4.CollectionUtils.isNotEmpty;
 
 /**
  * @author Lars Helge Overland
@@ -524,5 +529,73 @@ public class InterpretationController extends AbstractCrudController<Interpretat
             disjunctions.add( disjunction );
         }
         return disjunctions;
+    }
+
+    /**
+     * Logic required to keep the backward compatibility with Chart and ReporTable.
+     * Otherwise it would always return VISUALIZATION type for any Chart or ReportTable.
+     *
+     * Only needed during the transition from Chart/ReportTable APIs to Visualization API.
+     * Once the Visualization API is fully enabled this logic should be removed.
+     *
+     * @param interpretations
+     * @param options
+     * @param parameters
+     */
+    @Override
+    @Deprecated
+    protected void postProcessResponseEntities( final List<Interpretation> interpretations, final WebOptions options,
+        final java.util.Map<String, String> parameters )
+    {
+        if ( isNotEmpty( interpretations ) )
+        {
+            for ( final Interpretation interpretation : interpretations )
+            {
+                postProcessResponseEntity( interpretation, options, parameters );
+            }
+        }
+    }
+
+    /**
+     * Logic required to keep the backward compatibility with Chart and ReporTable.
+     * Otherwise it would always return VISUALIZATION type for any Chart or ReportTable.
+     *
+     * Only needed during the transition from Chart/ReportTable APIs to Visualization API.
+     * Once the Visualization API is fully enabled this logic should be removed.
+     *
+     * @param interpretation
+     * @param options
+     * @param parameters
+     */
+    @Override
+    @Deprecated
+    protected void postProcessResponseEntity( final Interpretation interpretation, final WebOptions options,
+        final java.util.Map<String, String> parameters )
+    {
+        if ( interpretation != null && interpretation.getVisualization() != null )
+        {
+            final VisualizationType type = interpretation.getVisualization().getType();
+
+            switch ( type )
+            {
+            case PIVOT_TABLE:
+                interpretation.setReportTable( new ReportTable() );
+                break;
+            case AREA:
+            case BAR:
+            case COLUMN:
+            case GAUGE:
+            case LINE:
+            case PIE:
+            case RADAR:
+            case SINGLE_VALUE:
+            case STACKED_BAR:
+            case STACKED_COLUMN:
+            case YEAR_OVER_YEAR_COLUMN:
+            case YEAR_OVER_YEAR_LINE:
+                interpretation.setChart( new Chart() );
+                break;
+            }
+        }
     }
 }
