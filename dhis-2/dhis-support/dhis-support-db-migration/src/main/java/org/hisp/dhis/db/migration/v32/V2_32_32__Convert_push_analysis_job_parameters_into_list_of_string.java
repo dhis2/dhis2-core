6c74acdcd9309c3380dbd3bc6f7acd20f85cd790
zubaira <zubair@dhis2.org>
2020-02-07 11:23:48 +0100

    remove new lines

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/java/org/hisp/dhis/db/migration/v32/V2_32_32__Convert_push_analysis_job_parameters_into_list_of_string.java
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/java/org/hisp/dhis/db/migration/v32/V2_32_32__Convert_push_analysis_job_parameters_into_list_of_string.java
@@ -13,10 +13,6 @@ package org.hisp.dhis.db.migration.v32;
  * this list of conditions and the following disclaimer in the documentation
  * and/or other materials provided with the distribution.
  * Neither the name of the HISP project nor the names of its contributors may
- *
- *
- *
- * 
  * be used to endorse or promote products derived from this software without
  * specific prior written permission.
  *
