f9f6f656db98e4083b47a25d6cc8dc1bb38c700a
zubaira <zubair@dhis2.org>
2020-02-07 09:50:40 +0100

    fix: flyway script version

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/java/org/hisp/dhis/db/migration/v32/V2_32_32__Convert_push_analysis_job_parameters_into_list_of_string.java
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/java/org/hisp/dhis/db/migration/v32/V2_32_32__Convert_push_analysis_job_parameters_into_list_of_string.java
@@ -49,9 +49,9 @@ import java.sql.Statement;
 /**
  * @Author Zubair Asghar.
  */
-public class V2_32_31__Convert_push_analysis_job_parameters_into_list_of_string extends BaseJavaMigration
+public class V2_32_32__Convert_push_analysis_job_parameters_into_list_of_string extends BaseJavaMigration
 {
-    private static final Log log = LogFactory.getLog( V2_32_31__Convert_push_analysis_job_parameters_into_list_of_string.class );
+    private static final Log log = LogFactory.getLog( V2_32_32__Convert_push_analysis_job_parameters_into_list_of_string.class );
 
     @Override
     public void migrate( Context context ) throws Exception
