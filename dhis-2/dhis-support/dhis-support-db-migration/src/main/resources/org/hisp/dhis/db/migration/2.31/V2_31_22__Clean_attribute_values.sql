5ab8c074bcf4f9aa3e325ac28521fda7f5f96fc5
Enrico Colasante <enrico.colasante@gmail.com>
2020-02-24 09:02:27 +0100

    Comment added to clarify the need for the flyway script

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.31/V2_31_22__Clean_attribute_values.sql
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.31/V2_31_22__Clean_attribute_values.sql
@@ -1,3 +1,8 @@
+-- After the fix of this bug https://jira.dhis2.org/browse/DHIS2-6837
+-- it was necessary to clean up duplicated attribute values for data elements.
+-- The script finds duplicates of attribute values of the same attribute for the same data element
+-- and delete the row from `dataelementattributevalues` table and `attributevalue` table
+
 WITH nonRepeatedIds AS (select min(av2.attributevalueid) as id from attributevalue av2 JOIN dataelementattributevalues deav2 ON av2.attributevalueid = deav2.attributevalueid
         group by deav2.dataelementid, av2.value, av2.attributeid),
     repeatedDataElementAttributeValues AS (select av.attributevalueid as id
