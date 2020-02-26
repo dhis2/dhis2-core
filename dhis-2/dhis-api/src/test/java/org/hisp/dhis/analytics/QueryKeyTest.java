00d6375361e2e4fd78694e0ed6bebb6c2d25a840
Lars Helge Øverland <lars@dhis2.org>
2020-02-26 13:27:06 +0100

    Add query key tests

--- dhis-2/dhis-api/src/test/java/org/hisp/dhis/analytics/QueryKeyTest.java
+++ dhis-2/dhis-api/src/test/java/org/hisp/dhis/analytics/QueryKeyTest.java
@@ -1,38 +1,9 @@
 package org.hisp.dhis.analytics;
 
-/*
- * Copyright (c) 2004-2020, University of Oslo
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- * Redistributions of source code must retain the above copyright notice, this
- * list of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice,
- * this list of conditions and the following disclaimer in the documentation
- * and/or other materials provided with the distribution.
- * Neither the name of the HISP project nor the names of its contributors may
- * be used to endorse or promote products derived from this software without
- * specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-import org.hisp.dhis.analytics.AggregationType;
-import org.hisp.dhis.analytics.QueryKey;
-import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import org.junit.Test;
 
 /**
  * @author Lars Helge Overland
@@ -51,5 +22,37 @@ public class QueryKeyTest
             .asPlainKey();
 
         assertEquals( "dimension:dx-dimension:pe-filter:ou-aggregationType:SUM-skipMeta:true", key );
+    }
+
+    @Test
+    public void testAsPlainKeyIgnoreNull()
+    {
+        String key = new QueryKey()
+            .add( "dimension", "dx" )
+            .add( "filter", "ou" )
+            .addIgnoreNull( "valueType", null )
+            .asPlainKey();
+
+        assertEquals( "dimension:dx-filter:ou", key );
+    }
+
+    @Test
+    public void testNoCollision()
+    {
+        String keyA = new QueryKey()
+            .add( "dimension", "dx" )
+            .add( "dimension", "aZASaK6ebLC" )
+            .add( "filter", "ou" )
+            .add( "aggregationType", AggregationType.SUM )
+            .asPlainKey();
+
+        String keyB = new QueryKey()
+            .add( "dimension", "dx" )
+            .add( "dimension", "aZASaK6ebLD" )
+            .add( "filter", "ou" )
+            .add( "aggregationType", AggregationType.SUM )
+            .asPlainKey();
+
+        assertNotEquals( keyA, keyB );
     }
 }
