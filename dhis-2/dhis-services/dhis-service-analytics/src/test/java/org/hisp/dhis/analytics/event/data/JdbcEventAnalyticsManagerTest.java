ff9d2213915629da65e46a316aa4101dfd8ea38d
Maikel Arabori <maikelarabori@gmail.com>
2020-02-26 12:43:58 +0100

    fix: (2.33) Events count for non-authorized user [DHIS2-7712]

--- dhis-2/dhis-services/dhis-service-analytics/src/test/java/org/hisp/dhis/analytics/event/data/JdbcEventAnalyticsManagerTest.java
+++ dhis-2/dhis-services/dhis-service-analytics/src/test/java/org/hisp/dhis/analytics/event/data/JdbcEventAnalyticsManagerTest.java
@@ -104,7 +104,7 @@ public class JdbcEventAnalyticsManagerTest
         final EventQueryParams theEventQueryParams = stubEventQueryParamsWithoutDimensions(
             newArrayList( aCategoryA ) );
 
-        final String theExpectedSql = " and ax.\"uid-cat-A\" in ('uid-opt-A', 'uid-opt-B') ";
+        final String theExpectedSql = " and ax.\"uid-cat-A\" not in ('uid-opt-A', 'uid-opt-B') ";
 
         // When
         final String actualSql = jdbcEventAnalyticsManager.getWhereClause( theEventQueryParams );
@@ -128,7 +128,7 @@ public class JdbcEventAnalyticsManagerTest
         final EventQueryParams theEventQueryParams = stubEventQueryParamsWithoutDimensions(
             newArrayList( aCategoryA, aCategoryB ) );
 
-        final String theExpectedSql = " and ax.\"uid-cat-A\" in ('uid-opt-A', 'uid-opt-B')  or ax.\"uid-cat-B\" in ('uid-opt-A', 'uid-opt-B') ";
+        final String theExpectedSql = " and ax.\"uid-cat-A\" not in ('uid-opt-A', 'uid-opt-B')  or ax.\"uid-cat-B\" not in ('uid-opt-A', 'uid-opt-B') ";
 
         // When
         final String actualSql = jdbcEventAnalyticsManager.getWhereClause( theEventQueryParams );
@@ -138,7 +138,7 @@ public class JdbcEventAnalyticsManagerTest
     }
 
     @Test
-    public void testQueryOnlyAllowedCategoryOptionEvents()
+    public void testFilterOutNotAuthorizedCategoryOptionEvents()
     {
         // Given
         final CategoryOption aCategoryOptionA = stubCategoryOption( "cat-option-A", "uid-opt-A" );
@@ -149,31 +149,31 @@ public class JdbcEventAnalyticsManagerTest
         final Category aCategoryB = stubCategory( "cat-B", "uid-cat-B",
             newArrayList( aCategoryOptionA, aCategoryOptionB ) );
 
-        final String theExpectedSql = " and ax.\"uid-cat-A\" in ('uid-opt-A', 'uid-opt-B')  or ax.\"uid-cat-B\" in ('uid-opt-A', 'uid-opt-B') ";
+        final String theExpectedSql = " and ax.\"uid-cat-A\" not in ('uid-opt-A', 'uid-opt-B')  or ax.\"uid-cat-B\" not in ('uid-opt-A', 'uid-opt-B') ";
 
         // When
         final String actualSql = jdbcEventAnalyticsManager
-            .queryOnlyAllowedCategoryOptionEvents( newArrayList( aCategoryA, aCategoryB ) );
+            .filterOutNotAuthorizedCategoryOptionEvents( newArrayList( aCategoryA, aCategoryB ) );
 
         // Then
         assertThat( actualSql, containsString( theExpectedSql ) );
     }
 
     @Test
-    public void testQueryOnlyAllowedCategoryOptionEventsWithNoCategories()
+    public void testFilterOutNotAuthorizedCategoryOptionEventsWithNoCategories()
     {
         // Given
         final List<Category> emptyCategoryList = newArrayList();
 
         // When
-        final String actualSql = jdbcEventAnalyticsManager.queryOnlyAllowedCategoryOptionEvents( emptyCategoryList );
+        final String actualSql = jdbcEventAnalyticsManager.filterOutNotAuthorizedCategoryOptionEvents( emptyCategoryList );
 
         // Then
         assertThat( actualSql, isEmptyString() );
     }
 
     @Test
-    public void testQueryOnlyAllowedCategoryOptionEventsWithNoCategoryOptions()
+    public void testFilterOutNotAuthorizedCategoryOptionEventsWithNoCategoryOptions()
     {
         // Given
         final Category aCategoryA = stubCategory( "cat-A", "uid-cat-A", newArrayList() );
@@ -181,7 +181,7 @@ public class JdbcEventAnalyticsManagerTest
 
         // When
         final String actualSql = jdbcEventAnalyticsManager
-            .queryOnlyAllowedCategoryOptionEvents( newArrayList( aCategoryA, aCategoryB ) );
+            .filterOutNotAuthorizedCategoryOptionEvents( newArrayList( aCategoryA, aCategoryB ) );
 
         // Then
         assertThat( actualSql, isEmptyString() );
