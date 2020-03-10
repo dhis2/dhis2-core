823211b1f943fe07cd2c5126750ac2870e00261f
Lars Helge Øverland <lars@dhis2.org>
2020-03-10 16:57:53 +0100

    Add tests

--- dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/event/data/AbstractAnalyticsService.java
+++ dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/event/data/AbstractAnalyticsService.java
@@ -151,7 +151,7 @@ public abstract class AbstractAnalyticsService
      * @param params the data query parameters.
      * @param grid the grid.
      */
-    protected void addMetadata(EventQueryParams params, Grid grid)
+    protected void addMetadata( EventQueryParams params, Grid grid )
     {
         if ( !params.isSkipMeta() )
         {
@@ -200,13 +200,13 @@ public abstract class AbstractAnalyticsService
         }
 
         params.getItemLegends().forEach( legend -> metadataItemMap.put( legend.getUid(),
-                new MetadataItem( legend.getDisplayName(), includeDetails ? legend.getUid() : null, legend.getCode() ) ) );
+            new MetadataItem( legend.getDisplayName(), includeDetails ? legend.getUid() : null, legend.getCode() ) ) );
 
         params.getItemOptions().forEach( option -> metadataItemMap.put( option.getUid(),
-                new MetadataItem( option.getDisplayName(), includeDetails ? option.getUid() : null, option.getCode() ) ) );
+            new MetadataItem( option.getDisplayName(), includeDetails ? option.getUid() : null, option.getCode() ) ) );
 
         params.getItemsAndItemFilters().forEach( item -> metadataItemMap.put( item.getItemId(),
-                new MetadataItem( item.getItem().getDisplayName(), includeDetails ? item.getItem() : null ) ) );
+            new MetadataItem( item.getItem().getDisplayName(), includeDetails ? item.getItem() : null ) ) );
 
         return metadataItemMap;
     }
@@ -223,7 +223,7 @@ public abstract class AbstractAnalyticsService
         Calendar calendar = PeriodType.getCalendar();
 
         List<String> periodUids = calendar.isIso8601() ?
-                getUids( params.getDimensionOrFilterItems( PERIOD_DIM_ID ) ) :
+            getUids( params.getDimensionOrFilterItems( PERIOD_DIM_ID ) ) :
                 getLocalPeriodIdentifiers( params.getDimensionOrFilterItems( PERIOD_DIM_ID ), calendar );
 
         Map<String, List<String>> dimensionItems = new HashMap<>();
