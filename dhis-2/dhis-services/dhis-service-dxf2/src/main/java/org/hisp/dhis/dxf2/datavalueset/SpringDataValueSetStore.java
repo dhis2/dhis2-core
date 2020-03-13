8f851e59e482e9de6e6b7c09e518dc2d5a7b4d9b
Lars Helge Øverland <lars@dhis2.org>
2020-03-13 17:21:59 +0100

    Minor

--- dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/datavalueset/SpringDataValueSetStore.java
+++ dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/datavalueset/SpringDataValueSetStore.java
@@ -352,6 +352,14 @@ public class SpringDataValueSetStore
         return sql;
     }
 
+    /**
+     * Returns an attribute option combo filter SQL clause. The filter enforces
+     * that only attribute option combinations which the given user has access
+     * to are returned.
+     *
+     * @param user the user.
+     * @return an SQL filter clause.
+     */
     private String getAttributeOptionComboClause( User user )
     {
         return
