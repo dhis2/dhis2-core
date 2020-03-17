805457db70e9d09569c85fd0859ae630f9d4147e
Stian Sandvold <stian@Stians-MacBook-Pro.local>
2020-03-17 19:15:44 +0100

    Added cleanup to the flyway script as well

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.31/V2_31_23__Upgrade_sequential_nuber_counter_key.sql
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.31/V2_31_23__Upgrade_sequential_nuber_counter_key.sql
@@ -1,1 +1,12 @@
 UPDATE sequentialnumbercounter SET key = substring(key, 12, (length(key)-12)) WHERE key LIKE 'SEQUENTIAL(%)';
+
+DELETE
+FROM sequentialnumbercounter
+WHERE id not in (
+    SELECT a.id
+    from sequentialnumbercounter a
+             inner join (
+        select owneruid, key, max(counter) counter
+        from sequentialnumbercounter
+        group by owneruid, key
+    ) b on a.key = b.key and a.owneruid = b.owneruid and a.counter = b.counter);
