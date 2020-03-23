5538b74cc41d8f0726a030559e1008b90a7a2af3
Maikel Arabori <maikelarabori@gmail.com>
2020-03-23 09:34:08 +0100

    fix: Failling unit test in other timezones in TimeToLive tests

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/util/DateUtils.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/util/DateUtils.java
@@ -41,6 +41,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
+import java.util.TimeZone;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
