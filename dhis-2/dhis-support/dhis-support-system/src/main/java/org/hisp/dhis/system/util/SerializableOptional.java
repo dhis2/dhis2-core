088fb920cb72b633f1a80815749e190e445f3cf6
Lars Helge Øverland <lars@dhis2.org>
2020-03-23 07:48:06 +0100

    Javaodc

--- dhis-2/dhis-support/dhis-support-system/src/main/java/org/hisp/dhis/system/util/SerializableOptional.java
+++ dhis-2/dhis-support/dhis-support-system/src/main/java/org/hisp/dhis/system/util/SerializableOptional.java
@@ -50,21 +50,42 @@ public class SerializableOptional
         this.value = value;
     }
 
+    /**
+     * Creates a {@link SerializableOptional} with the given value.
+     *
+     * @param value the value.
+     * @return a {@link SerializableOptional}.
+     */
     public static SerializableOptional of( Serializable value )
     {
         return new SerializableOptional( value );
     }
 
+    /**
+     * Returns a {@link SerializableOptional} with a null value.
+     *
+     * @return a {@link SerializableOptional} with a null value.
+     */
     public static SerializableOptional empty()
     {
         return new SerializableOptional();
     }
 
+    /**
+     * Indicates whether a value is present.
+     *
+     * @return true if a value is present.
+     */
     public boolean isPresent()
     {
         return value != null;
     }
 
+    /**
+     * Returns the value, may be null.
+     *
+     * @return the value.
+     */
     public Serializable get()
     {
         return value;
