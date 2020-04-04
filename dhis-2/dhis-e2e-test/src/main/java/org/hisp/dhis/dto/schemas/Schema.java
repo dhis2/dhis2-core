e28069dae533615e2303cf41839f66b0590aa45a
Luciano <luciano@dhis2.org>
2020-04-04 09:56:09 +0200

    chore: fixed date on header

--- dhis-2/dhis-e2e-test/src/main/java/org/hisp/dhis/dto/schemas/Schema.java
+++ dhis-2/dhis-e2e-test/src/main/java/org/hisp/dhis/dto/schemas/Schema.java
@@ -1,7 +1,7 @@
 package org.hisp.dhis.dto.schemas;
 
 /*
- * Copyright (c) 2004-2019, University of Oslo
+ * Copyright (c) 2004-2020, University of Oslo
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -27,7 +27,6 @@ package org.hisp.dhis.dto.schemas;
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
