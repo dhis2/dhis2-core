e219687470b9c158a6e1daecd6e4e7fcbb357745
Luciano Fiandesio <luciano@dhis2.org>
2020-04-16 16:32:06 +0200

    chore: Fix headers in API test project (#5305)
    * chore: fixed duplicated or missing headers
    
    * chore: fixed date on header

--- dhis-2/dhis-e2e-test/src/main/java/org/hisp/dhis/helpers/JsonParserUtils.java
+++ dhis-2/dhis-e2e-test/src/main/java/org/hisp/dhis/helpers/JsonParserUtils.java
@@ -1,5 +1,7 @@
+package org.hisp.dhis.helpers;
+
 /*
- * Copyright (c) 2004-2018, University of Oslo
+ * Copyright (c) 2004-2020, University of Oslo
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -26,7 +28,6 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.hisp.dhis.helpers;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
