f5071d39a238850be39766d0fa0b6ee701d9afb9
Maikel Arabori <51713408+maikelarabori@users.noreply.github.com>
2020-04-16 15:27:11 +0200

    fix: NPE when Chart.category() is null [DHIS2-8590] (#5304)
    * fix: NPE when Chart.category() is null [DHIS2-8590]
    
    Backporting the NPE work around to (2.32)
    
    * fix: Using defaultIfNull to check/set the Categories list [DHIS2-8590]

--- dhis-2/dhis-services/dhis-service-reporting/src/main/java/org/hisp/dhis/chart/impl/DefaultChartService.java
+++ dhis-2/dhis-services/dhis-service-reporting/src/main/java/org/hisp/dhis/chart/impl/DefaultChartService.java
@@ -82,6 +82,8 @@ import java.util.*;
 import java.util.List;
 import java.util.Map.Entry;
 
+import static java.util.Collections.emptyList;
+import static org.apache.commons.lang3.ObjectUtils.defaultIfNull;
 import static org.hisp.dhis.common.DimensionalObject.DIMENSION_SEP;
 import static org.hisp.dhis.commons.collection.ListUtils.getArray;
 
@@ -796,7 +798,7 @@ public class DefaultChartService
         valueMap = DimensionalObjectUtils.getSortedKeysMap( valueMap );
 
         List<NameableObject> seriez = new ArrayList<>( chart.series() );
-        List<NameableObject> categories = new ArrayList<>( chart.category() );
+        List<NameableObject> categories = new ArrayList<>( defaultIfNull( chart.category(), emptyList() ) );
 
         if ( chart.hasSortOrder() )
         {
