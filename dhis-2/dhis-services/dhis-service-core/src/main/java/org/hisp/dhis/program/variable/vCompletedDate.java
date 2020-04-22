552a53d389a4436870062618b0e94f4469331c66
zubaira <zubair@dhis2.org>
2020-04-22 12:24:28 +0200

    fix: SQL generation for Date type program variables

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/program/variable/vCompletedDate.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/program/variable/vCompletedDate.java
@@ -39,13 +39,6 @@ public class vCompletedDate extends ProgramDateVariable
     @Override
     public Object getSql( CommonExpressionVisitor visitor )
     {
-        if ( AnalyticsType.EVENT == visitor.getProgramIndicator().getAnalyticsType() )
-        {
-            return "completeddate";
-        }
-
-        return visitor.getStatementBuilder().getProgramIndicatorEventColumnSql(
-                null, "completeddate", visitor.getReportingStartDate(),
-                visitor.getReportingStartDate(), visitor.getProgramIndicator() );
+        return "completeddate";
     }
 }
--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/program/variable/vCreationDate.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/program/variable/vCreationDate.java
@@ -29,6 +29,7 @@ package org.hisp.dhis.program.variable;
  */
 
 import org.hisp.dhis.parser.expression.CommonExpressionVisitor;
+import org.hisp.dhis.program.AnalyticsType;
 
 /**
  * Program indicator variable: creation date
@@ -41,8 +42,13 @@ public class vCreationDate
     @Override
     public Object getSql( CommonExpressionVisitor visitor )
     {
-        return visitor.getStatementBuilder().getProgramIndicatorEventColumnSql(
-            null, "created", visitor.getReportingStartDate(),
-            visitor.getReportingStartDate(), visitor.getProgramIndicator() );
+        if ( AnalyticsType.ENROLLMENT == visitor.getProgramIndicator().getAnalyticsType() )
+        {
+            return visitor.getStatementBuilder().getProgramIndicatorEventColumnSql(
+                null, "created", visitor.getReportingStartDate(),
+                visitor.getReportingStartDate(), visitor.getProgramIndicator() );
+        }
+
+        return "created";
     }
 }
