481fef553799aebd9ce6c57324fc0b7a88e63f9e
Mohamed Ameen <ameen@dhis2.org>
2020-05-07 14:48:01 +0200

    fix: remove servlet spec 4.0 dependency for file resource download (2.33) (#5540)
    * Use servlet spec 3.1 instead of 4.0
    
    * Use servlet spec 3.1 instead of 4.0 for fileresource downloads

--- dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/FileResourceController.java
+++ dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/FileResourceController.java
@@ -130,7 +130,7 @@ public class FileResourceController
         }
 
         response.setContentType( fileResource.getContentType() );
-        response.setContentLengthLong( fileResourceService.getFileResourceContentLength( fileResource ) );
+        response.setHeader( HttpHeaders.CONTENT_LENGTH, String.valueOf( fileResourceService.getFileResourceContentLength( fileResource ) ) );
         response.setHeader( HttpHeaders.CONTENT_DISPOSITION, "filename=" + fileResource.getName() );
 
         try
