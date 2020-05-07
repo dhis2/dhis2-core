add441d820f5fd2fa0cae3bbdc9c9ca4286a8013
Ameen <ameen@dhis2.org>
2020-05-07 21:44:28 +0200

    fix: Add content length for file downloads supporting 3.1 servlet spec

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/fileresource/DefaultFileResourceService.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/fileresource/DefaultFileResourceService.java
@@ -117,7 +117,7 @@ public class DefaultFileResourceService
     {
         return checkStorageStatus( fileResourceStore.getByUid( uid ) );
     }
-
+    
     @Override
     @Transactional( readOnly = true )
     public List<FileResource> getFileResources( List<String> uids )
@@ -210,13 +210,20 @@ public class DefaultFileResourceService
     {
         return fileResourceContentStore.getFileResourceContent( fileResource.getStorageKey() );
     }
+    
+    @Override
+    @Transactional( readOnly = true )
+    public long getFileResourceContentLength( FileResource fileResource )
+    {
+        return fileResourceContentStore.getFileResourceContentLength( fileResource.getStorageKey() );
+    }
 
     @Override
     @Transactional( readOnly = true )
-    public long copyFileResourceContent( FileResource fileResource, OutputStream outputStream )
+    public void copyFileResourceContent( FileResource fileResource, OutputStream outputStream )
         throws IOException, NoSuchElementException
     {
-        return fileResourceContentStore.copyContent( fileResource.getStorageKey(), outputStream );
+        fileResourceContentStore.copyContent( fileResource.getStorageKey(), outputStream );
     }
 
     @Override
--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/fileresource/JCloudsFileResourceContentStore.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/fileresource/JCloudsFileResourceContentStore.java
@@ -208,6 +208,19 @@ public class JCloudsFileResourceContentStore
             return null;
         }
     }
+    
+    @Override
+    public long getFileResourceContentLength( String key )
+    {
+        final Blob blob = getBlob( key );
+
+        if ( blob == null )
+        {
+            return 0;
+        }
+
+        return blob.getMetadata().getContentMetadata().getContentLength();
+    }
 
     @Override
     public String saveFileResourceContent( FileResource fileResource, byte[] bytes )
@@ -237,7 +250,7 @@ public class JCloudsFileResourceContentStore
     @Override
     public String saveFileResourceContent( FileResource fileResource, File file )
     {
-        Blob blob = createBlob( fileResource, StringUtils.EMPTY, file );
+        Blob blob = createBlob( fileResource, StringUtils.EMPTY, file, fileResource.getContentMd5() );
 
         if ( blob == null )
         {
@@ -274,8 +287,6 @@ public class JCloudsFileResourceContentStore
         {
             File file = entry.getValue();
 
-            fileResource.setContentLength( file.length() );
-
             String contentMd5;
 
             try
@@ -289,8 +300,7 @@ public class JCloudsFileResourceContentStore
                 return null;
             }
 
-            fileResource.setContentMd5( contentMd5 );
-            blob = createBlob( fileResource, entry.getKey().getDimension(), file );
+            blob = createBlob( fileResource, entry.getKey().getDimension(), file, contentMd5 );
 
             if ( blob != null )
             {
@@ -356,7 +366,7 @@ public class JCloudsFileResourceContentStore
     }
 
     @Override
-    public long copyContent( String key, OutputStream output )
+    public void copyContent( String key, OutputStream output )
         throws IOException, NoSuchElementException
     {
         if ( !blobExists( key ) )
@@ -370,8 +380,6 @@ public class JCloudsFileResourceContentStore
         {
             IOUtils.copy( in, output );
         }
-
-        return blob.getMetadata().getContentMetadata().getContentLength();
     }
 
     // -------------------------------------------------------------------------
@@ -397,19 +405,19 @@ public class JCloudsFileResourceContentStore
     {
         return blobStore.blobBuilder( fileResource.getStorageKey() )
             .payload( bytes )
-            .contentLength( fileResource.getContentLength() )
+            .contentLength( bytes.length )
             .contentMD5( HashCode.fromString( fileResource.getContentMd5() ) )
             .contentType( fileResource.getContentType() )
             .contentDisposition( "filename=" + fileResource.getName() )
             .build();
     }
 
-    private Blob createBlob( FileResource fileResource, String fileDimension, File file )
+    private Blob createBlob( FileResource fileResource, String fileDimension, File file, String contentMd5 )
     {
         return blobStore.blobBuilder( StringUtils.join( fileResource.getStorageKey(), fileDimension ) )
             .payload( file )
-            .contentLength( fileResource.getContentLength() )
-            .contentMD5( HashCode.fromString( fileResource.getContentMd5() ) )
+            .contentLength( file.length() )
+            .contentMD5( HashCode.fromString( contentMd5 ) )
             .contentType( fileResource.getContentType() )
             .contentDisposition( "filename=" + fileResource.getName() + fileDimension )
             .build();
