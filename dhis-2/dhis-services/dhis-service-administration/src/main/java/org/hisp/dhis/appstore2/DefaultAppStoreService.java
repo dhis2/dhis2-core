65d469a920a5d0492499792468fa07a170bf6a7b
zubaira <zubair@dhis2.org>
2020-05-13 15:45:00 +0200

    fix: Use correct appStore API URL

--- dhis-2/dhis-services/dhis-service-administration/src/main/java/org/hisp/dhis/appstore2/DefaultAppStoreService.java
+++ dhis-2/dhis-services/dhis-service-administration/src/main/java/org/hisp/dhis/appstore2/DefaultAppStoreService.java
@@ -35,6 +35,7 @@ import org.hisp.dhis.appmanager.AppManager;
 import org.hisp.dhis.appmanager.AppStatus;
 import org.hisp.dhis.setting.SettingKey;
 
+import org.hisp.dhis.setting.SystemSettingManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.client.RestTemplate;
 
@@ -61,10 +62,15 @@ public class DefaultAppStoreService implements AppStoreService
     @Autowired
     private AppManager appManager;
 
+    @Autowired
+    private SystemSettingManager systemSettingManager;
+
     @Override
     public List<WebApp> getAppStore()
     {
-        WebApp[] apps = restTemplate.getForObject( SettingKey.APP_STORE_API_URL.getDefaultValue().toString(), WebApp[].class );
+        String keyAppStoreApiUrl = (String) systemSettingManager.getSystemSetting( SettingKey.APP_STORE_API_URL );
+
+        WebApp[] apps = restTemplate.getForObject( keyAppStoreApiUrl, WebApp[].class );
 
         return Arrays.asList( apps );
     }
