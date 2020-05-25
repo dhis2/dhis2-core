b542fa21172c116db2f0933f61cf364fef0ddaf2
Viet Nguyen <viet@dhis2.org>
2020-05-25 19:33:31 +0700

    chore: Clean up code in AuditObjectFactory (#5624)

--- dhis-2/dhis-support/dhis-support-artemis/src/main/java/org/hisp/dhis/artemis/audit/legacy/DefaultAuditObjectFactory.java
+++ dhis-2/dhis-support/dhis-support-artemis/src/main/java/org/hisp/dhis/artemis/audit/legacy/DefaultAuditObjectFactory.java
@@ -36,9 +36,6 @@ import org.hisp.dhis.audit.AuditAttribute;
 import org.hisp.dhis.audit.AuditAttributes;
 import org.hisp.dhis.audit.AuditScope;
 import org.hisp.dhis.audit.AuditType;
-import org.hisp.dhis.audit.payloads.MetadataAuditPayload;
-import org.hisp.dhis.audit.payloads.TrackedEntityAuditPayload;
-import org.hisp.dhis.common.BaseIdentifiableObject;
 import org.hisp.dhis.common.IdentifiableObject;
 import org.hisp.dhis.commons.util.DebugUtils;
 import org.hisp.dhis.system.util.AnnotationUtils;
@@ -53,8 +50,9 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
- * A factory for constructing @{@link org.hisp.dhis.audit.Audit} data payloads. This can be the object itself
- * (as is the case for metadata), or it can be a wrapper object collecting the parts wanted.
+ * A factory for constructing {@see org.hisp.dhis.audit.Audit} data payloads.
+ * This can be the object itself (as is the case for metadata), or it can be a
+ * wrapper object collecting the parts wanted.
  *
  * @author Luciano Fiandesio
  */
@@ -65,10 +63,11 @@ public class DefaultAuditObjectFactory implements AuditObjectFactory
     private final ObjectMapper objectMapper;
 
     /**
-     * Cache for Fields of {@link org.hisp.dhis.audit.Auditable} classes
-     * Key is class name. Value is Map of {@link AuditAttribute} Fields and its getter Method
+     * Cache for Fields of {@link org.hisp.dhis.audit.Auditable} classes Key is
+     * class name. Value is Map of {@link AuditAttribute} Fields and its getter
+     * Method
      */
-    private Map<String, Map<Field, Method>> cachedAuditAttributeFields = new ConcurrentHashMap<>();
+    private final Map<String, Map<Field, Method>> cachedAuditAttributeFields = new ConcurrentHashMap<>();
 
     public DefaultAuditObjectFactory( ObjectMapper objectMapper )
     {
@@ -84,11 +83,11 @@ public class DefaultAuditObjectFactory implements AuditObjectFactory
         switch ( auditScope )
         {
         case METADATA:
-            return handleMetadataAudit( auditType, object, user );
+            return handleMetadataAudit( object );
         case TRACKER:
-            return handleTracker( auditType, object, user );
+            return handleTracker( object );
         case AGGREGATE:
-            return handleAggregate( auditType, object, user );
+            return handleAggregate( object );
         }
         return null;
     }
@@ -98,17 +97,17 @@ public class DefaultAuditObjectFactory implements AuditObjectFactory
     {
         AuditAttributes auditAttributes = new AuditAttributes();
 
-        getAuditAttributeFields( auditObject.getClass() ).entrySet().forEach( entry -> {
+        getAuditAttributeFields( auditObject.getClass() ).forEach( ( key, value ) -> {
 
-            Object attributeObject = ReflectionUtils.invokeMethod( auditObject, entry.getValue() );
+            Object attributeObject = ReflectionUtils.invokeMethod( auditObject, value );
 
             if ( attributeObject instanceof IdentifiableObject )
             {
-                auditAttributes.put( entry.getKey().getName(), ( ( IdentifiableObject ) attributeObject).getUid() );
+                auditAttributes.put( key.getName(), ((IdentifiableObject) attributeObject).getUid() );
             }
             else
             {
-                auditAttributes.put( entry.getKey().getName(), attributeObject );
+                auditAttributes.put( key.getName(), attributeObject );
             }
         } );
 
@@ -128,55 +127,26 @@ public class DefaultAuditObjectFactory implements AuditObjectFactory
         return map;
     }
 
-    private Object handleTracker( AuditType auditType, Object object, String user )
-    {
-        if ( object instanceof TrackedEntityAttributeValue )
-        {
-            return  toJson( handleTrackedEntityAttributeValue( ( TrackedEntityAttributeValue ) object ) );
-        }
-
-        if ( object instanceof TrackedEntityInstance )
-        {
-            return  toJson( TrackedEntityAuditPayload.builder()
-                .trackedEntityInstance( ( TrackedEntityInstance ) object )
-                .build() );
-        }
-
-        return toJson( object );
-    }
-
-    private Object handleAggregate( AuditType auditType, Object object, String user )
+    private String handleTracker( Object object )
     {
         return toJson( object );
     }
 
-    private Object handleMetadataAudit( AuditType auditType, Object object, String user )
+    private String handleAggregate( Object object )
     {
-        if ( !(object instanceof IdentifiableObject) )
-        {
-            return null;
-        }
-
-        return toJson( MetadataAuditPayload.builder()
-            .identifiableObject( (IdentifiableObject) object )
-            .build() );
+        return toJson( object );
     }
 
-    private TrackedEntityAttributeValue handleTrackedEntityAttributeValue( TrackedEntityAttributeValue value )
+    private String handleMetadataAudit( Object object )
     {
-        return value.setAttribute( clearSharing( value.getAttribute() ) );
+        return toJson( object );
     }
 
     private String toJson( Object object )
     {
-        if ( object instanceof IdentifiableObject )
-        {
-            object = clearSharing( ( BaseIdentifiableObject ) object );
-        }
-
         try
         {
-            return  objectMapper.writeValueAsString( object );
+            return objectMapper.writeValueAsString( object );
         }
         catch ( JsonProcessingException e )
         {
@@ -184,12 +154,5 @@ public class DefaultAuditObjectFactory implements AuditObjectFactory
         }
 
         return null;
-    }
-
-    private <T extends BaseIdentifiableObject> T clearSharing( T identifiableObject )
-    {
-        identifiableObject.setUserGroupAccesses( null );
-        identifiableObject.setUserAccesses( null );
-        return identifiableObject;
     }
 }
