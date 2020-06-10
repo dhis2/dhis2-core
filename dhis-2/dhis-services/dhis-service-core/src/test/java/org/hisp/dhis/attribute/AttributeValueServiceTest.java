d22d96dd87165fa476ff97080495d9268e16e328
Viet Nguyen <viet@dhis2.org>
2020-06-10 22:58:04 +0700

    fix: Add DeletionHandler for Attribute

--- dhis-2/dhis-services/dhis-service-core/src/test/java/org/hisp/dhis/attribute/AttributeValueServiceTest.java
+++ dhis-2/dhis-services/dhis-service-core/src/test/java/org/hisp/dhis/attribute/AttributeValueServiceTest.java
@@ -33,6 +33,7 @@ import org.hisp.dhis.IntegrationTest;
 import org.hisp.dhis.TransactionalIntegrationTestBase;
 import org.hisp.dhis.attribute.exception.NonUniqueAttributeValueException;
 import org.hisp.dhis.category.CategoryService;
+import org.hisp.dhis.common.DeleteNotAllowedException;
 import org.hisp.dhis.common.IdentifiableObjectManager;
 import org.hisp.dhis.common.ValueType;
 import org.hisp.dhis.dataelement.DataElement;
@@ -372,5 +373,15 @@ public class AttributeValueServiceTest
         assertEquals( 2, values.size() );
         assertTrue( values.stream().anyMatch( av -> av.getValue().equals( "valueA" ) ) );
         assertTrue( values.stream().anyMatch( av -> av.getValue().equals( "valueB" ) ) );
+    }
+
+    @Test( expected = DeleteNotAllowedException.class )
+    public void testDeleteAttributeWithReferences()
+    {
+        AttributeValue avA = new AttributeValue( "valueA", attribute1 );
+
+        attributeService.addAttributeValue( dataElementA, avA );
+
+        attributeService.deleteAttribute( attribute1 );
     }
 }
