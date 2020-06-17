5b966af64027e61ef6d63fce90c4cf49d1c20803
Enrico Colasante <enrico.colasante@gmail.com>
2020-06-17 16:11:33 +0200

    Remove manual saving of data element operand in importer

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.35/V2_35_4__Add_delete_on_cascade_constraint_for_data_element_operand.sql
+++
@@ -1,7 +0,0 @@
-alter table dataelementoperand drop constraint fk_dataelementoperand_dataelement;
-
-alter table dataelementoperand
-    add constraint fk_dataelementoperand_dataelement
-        foreign key (dataelementid)
-            references dataelement (dataelementid)
-            on delete cascade;
