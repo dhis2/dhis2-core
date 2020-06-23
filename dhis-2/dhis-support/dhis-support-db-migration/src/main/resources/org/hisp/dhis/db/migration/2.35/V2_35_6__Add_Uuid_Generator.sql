57f01e68249e28ff8b1cc0351644ff4d891c2fa0
Morten Olav Hansen <mortenoh@gmail.com>
2020-06-23 12:38:44 +0200

    chore: remove commented out old random generator

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.35/V2_35_6__Add_Uuid_Generator.sql
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.35/V2_35_6__Add_Uuid_Generator.sql
@@ -1,18 +1,4 @@
 
-
-/*
-CREATE OR REPLACE FUNCTION gen_random_uuid() RETURNS uuid
-    AS $$
-        SELECT uuid_in(
-            overlay(
-                overlay(md5(random()::text || ':' || clock_timestamp()::text) placing '4' from 13)
-                placing to_hex(floor(random()*(11-8+1) + 8)::int)::text from 17
-            )::cstring
-        )::uuid
-    $$
-    LANGUAGE SQL;
-*/
-
 CREATE OR REPLACE FUNCTION gen_random_uuid() RETURNS uuid
     AS $$
         SELECT md5(random()::text || random()::text)::uuid
