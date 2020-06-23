41ff6d6fa8de353f9c1f5eb95e5c77c5b57403a2
Lars Helge Øverland <lars@dhis2.org>
2020-06-24 00:24:17 +0200

    Update migration

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.35/V2_35_8__Add_mapview_thematicmaptype.sql
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.35/V2_35_8__Add_mapview_thematicmaptype.sql
@@ -1,5 +1,8 @@
 
--- Add column 'thematicmaptype' to 'mapview', set to default value and set not null
+-- Add column 'thematicmaptype' to 'mapview'
 
 alter table mapview add column if not exists "thematicmaptype" varchar(50);
+
+-- Upgrade existing thematic map views to 'CHOROPLETH'
+
 update mapview set "thematicmaptype" = 'CHOROPLETH' where "thematicmaptype" is null and "layer" like 'thematic%';
