bdac6b03154d4bd4b419e973ecc4f60cded2e835
maikelarabori <maikelarabori@gmail.com>
2020-07-03 17:08:26 +0200

    fix: Adding code to reorder the sort_order in columns table [DHIS2-9106]

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.35/V2_35_10__Visualization_Move_Columns_To_Filters.sql
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.35/V2_35_10__Visualization_Move_Columns_To_Filters.sql
@@ -1,5 +1,5 @@
 
--- This script moves all visualization columns of "types" 'YEAR_OVER_YEAR_COLUMN' and 'YEAR_OVER_YEAR_LINE'
+-- This script moves all visualization columns of "type" 'YEAR_OVER_YEAR_COLUMN' and 'YEAR_OVER_YEAR_LINE'
 -- and "dimension" 'dx' into visualization filters. This is done by the store procedure below.
 -- This is achieved, basically, by moving records from visualization_columns table into visualization_filters table.
 -- For more details, see the ticket: DHIS2-9106
@@ -9,15 +9,24 @@
 DO $$
 
 DECLARE
+	-- Used for syncing-up sort_order values in visualization_columns table.
 	column_row record;
 	filter_row record;
 	sort_order int;
-	debug bool := false;
+
+	-- Used for synching-up sort_order values in visualization_columns table.
+ 	current_column record;
+	new_sort_order int := 0; -- Zero is the base value. See Visualization.hbm.xml.
+	total_sort_orders_updated bigint := 0;
+	total_sort_orders_refused bigint := 0;
+
+	-- Debug variable
+	debug bool := true;
 
 BEGIN
 	-- Iterate through each row, in columns table, that matches "type" equals 'YEAR_OVER_YEAR_COLUMN' or 'YEAR_OVER_YEAR_LINE', and dimension = "dx".
 	FOR column_row IN (
-		-- Selecting the columns to be moved into filters.
+		-- Selecting the columns to be moved to filters.
 		SELECT vc.visualizationid, vc.dimension, vc.sort_order FROM visualization_columns vc WHERE vc.visualizationid IN
 		(SELECT v.visualizationid FROM visualization v
 			WHERE UPPER(COALESCE(v.type, '')) = 'YEAR_OVER_YEAR_COLUMN' OR UPPER(COALESCE(v.type, '')) = 'YEAR_OVER_YEAR_LINE') AND LOWER(COALESCE(vc.dimension)) = 'dx'
@@ -47,13 +56,52 @@ BEGIN
 			INSERT INTO visualization_filters (visualizationid, dimension, sort_order)
 			VALUES (column_row.visualizationid, LOWER(column_row.dimension), sort_order );
 
-	        -- Once this column is copied into filters, remove it from columns table. The "moving" process is finally concluded for this particular column.
+	        -- Once the columns is copied into filters, remove it from columns table. The "moving" process is concluded for this column.
 	        DELETE FROM visualization_columns vc
 	        WHERE vc.visualizationid = column_row.visualizationid
 	        AND LOWER(COALESCE(vc.dimension, '')) = LOWER(column_row.dimension)
 	        AND vc.sort_order = column_row.sort_order;
 		END IF;
 	END LOOP;
+
+    -- Now reorganize all sort_order values, for each column, to fill-in eventual gaps of previously removed columns.
+    -- For each visualization id present in the columns table.
+    FOR column_row IN SELECT DISTINCT(visualizationid)
+        FROM visualization_columns ORDER BY visualizationid ASC
+    LOOP
+    	-- For each column related to the current visualization id.
+        FOR current_column IN SELECT vc.visualizationid, vc.sort_order, vc.dimension
+            FROM visualization_columns vc WHERE visualizationid = column_row.visualizationid ORDER BY vc.sort_order ASC
+        LOOP
+        	-- Try to update the current column by setting the correct sort_order.
+        	BEGIN
+	        	UPDATE visualization_columns SET sort_order=new_sort_order WHERE visualizationid=current_column.visualizationid AND LOWER(COALESCE(dimension, '')) = LOWER(current_column.dimension);
+
+	        	IF debug THEN
+		        	RAISE INFO '%','Updating sort_order for visualization: '||column_row.visualizationid;
+	        	END IF;
+
+	            total_sort_orders_updated := total_sort_orders_updated + 1;
+            EXCEPTION WHEN OTHERS THEN
+            	-- If the update fails it's because the pair visualizationid/sort_order relation is already present, so skip it.
+               	total_sort_orders_refused := total_sort_orders_refused + 1;
+
+				IF debug THEN
+	            	RAISE INFO '%','WARN: Relation already exists, skipping: '||'visualizationid: '||current_column.visualizationid||' | sort_order: '||new_sort_order;
+	            END IF;
+            END;
+
+           	-- Increment the sort_order to be set in the next column row for the current visualization id.
+            new_sort_order := new_sort_order + 1;
+	    END LOOP;
+	    -- Reset sort_oder to the base value so it can be used by the next visualization.
+        new_sort_order := 0;
+    END LOOP;
+
+	IF debug THEN
+		RAISE INFO '%', 'Total of sort_order updates: '||total_sort_orders_updated;
+		RAISE INFO '%', 'Total of sort_order refused: '||total_sort_orders_refused;
+	END IF;
 END;
 
 $$ LANGUAGE plpgsql;
