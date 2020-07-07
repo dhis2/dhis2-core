a03e8b484de3fbbfb237813fac05339930e70d1f
Stian Sandvold <stiansandvold@pop-os.localdomain>
2020-07-07 16:54:28 +0200

    Added new procedure for removing race conditions, and updated store to reflect the changes

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/reservedvalue/hibernate/HibernateSequentialNumberCounterStore.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/reservedvalue/hibernate/HibernateSequentialNumberCounterStore.java
@@ -58,20 +58,11 @@ public class HibernateSequentialNumberCounterStore
     {
         Session session = sessionFactory.getCurrentSession();
 
-        int count;
-
-        SequentialNumberCounter counter = (SequentialNumberCounter) session
-            .createQuery( "FROM SequentialNumberCounter WHERE owneruid = ? AND key = ?" ).setParameter( 0, uid )
-            .setParameter( 1, key ).uniqueResult();
-
-        if ( counter == null )
-        {
-            counter = new SequentialNumberCounter( uid, key, 1 );
-        }
-
-        count = counter.getCounter();
-        counter.setCounter( count + length );
-        session.saveOrUpdate( counter );
+        int count = (int) session.createNativeQuery( "SELECT * FROM incrementSequentialCounter(?0, ?1, ?2)" )
+            .setParameter( 0, uid )
+            .setParameter( 1, key )
+            .setParameter( 2, length )
+            .uniqueResult();
 
         return IntStream.range( count, count + length ).boxed().collect( Collectors.toList() );
 
