0d20d95d1ecc7e2f4798fb038923311ce75d9705
Stian Sandvold <stian@dhis2.org>
2020-07-10 10:09:40 +0200

    fix: Add assignedUser as parameter in query (#5827)
    Force merging, after reviewing failing check. (Will not do for this PR)

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/trackedentity/hibernate/HibernateTrackedEntityInstanceStore.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/trackedentity/hibernate/HibernateTrackedEntityInstanceStore.java
@@ -758,6 +758,10 @@ public class HibernateTrackedEntityInstanceStore
         {
             sql += " (psi.assigneduserid is null) and ";
         }
+        if ( params.isIncludeOnlyUnassignedEvents() )
+        {
+            sql += " (psi.assigneduserid is null) and ";
+        }
 
         if ( params.isIncludeOnlyAssignedEvents() )
         {
