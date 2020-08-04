295371663da20f453b9d92fa746c1bcd7ebab485
maikelarabori <maikelarabori@gmail.com>
2020-08-04 17:14:36 +0200

    fix: Removing trim function from migration [DHIS2-9218]

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.35/V2_35_12__Rename_All_AREA_Type_To_STACKED_AREA.sql
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.35/V2_35_12__Rename_All_AREA_Type_To_STACKED_AREA.sql
@@ -1,4 +1,4 @@
 
 -- Rename all AREA type to STACKED_AREA, in the "visualization" table.
 
-UPDATE visualization SET "type"='STACKED_AREA' WHERE trim("type")='AREA';
+UPDATE visualization SET "type"='STACKED_AREA' WHERE "type"='AREA';
