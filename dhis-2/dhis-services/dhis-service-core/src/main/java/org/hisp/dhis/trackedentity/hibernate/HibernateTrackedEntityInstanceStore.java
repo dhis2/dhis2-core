7bcc035a2ac92e46c3830ac20d43b0921d994f20
Ameen <ameen@dhis2.org>
2020-08-06 13:31:27 +0200

    feat: [DHIS2-9087] Add programStage filtering capability when querying events (2.34)

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/trackedentity/hibernate/HibernateTrackedEntityInstanceStore.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/trackedentity/hibernate/HibernateTrackedEntityInstanceStore.java
@@ -773,6 +773,11 @@ public class HibernateTrackedEntityInstanceStore
                 sql += " psi.duedate >= '" + start + "' and psi.duedate <= '" + end + "' " + "and psi.status = '" + EventStatus.SKIPPED.name() + "' and ";
             }
         }
+        
+        if ( params.hasProgramStage() )
+        {
+            sql += " psi.programstageid = " + params.getProgramStage().getId() + " and ";
+        }
 
         if ( params.hasAssignedUsers() )
         {
@@ -828,6 +833,11 @@ public class HibernateTrackedEntityInstanceStore
             {
                 hql += " psi.dueDate >= '" + start + "' and psi.dueDate <= '" + end + "' " + "and psi.status = '" + EventStatus.SKIPPED.name() + "' and ";
             }
+        }
+        
+        if ( params.hasProgramStage() )
+        {
+            hql += " psi.programStage.uid = " + params.getProgramStage().getUid() + " and ";
         }
 
         if ( params.hasAssignedUsers() )
