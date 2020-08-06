6c5e70f57d45381656708731be830fb962e728f6
Stian Sandvold <stian@dhis2.org>
2020-08-06 17:42:37 +0200

    Backported fixed to original PR

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.34/V2_34_23__Add_sequentialcounter_procedure.sql
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.34/V2_34_23__Add_sequentialcounter_procedure.sql
@@ -1,27 +1,24 @@
-CREATE OR REPLACE FUNCTION incrementSequentialCounter(counter_owner text, counter_key text, i integer) RETURNS integer AS $$
+-- remove primary key on id
+alter table sequentialnumbercounter drop constraint sequentialnumbercounter_pkey;
+-- create composite primary key
+alter table sequentialnumbercounter add constraint seqnumcount_pkey primary key (owneruid, key);
+
+CREATE OR REPLACE FUNCTION incrementSequentialCounter(counter_owner text, counter_key text, size integer) RETURNS integer AS $$
 DECLARE
-    -- Get the initial count, or 0 if it doesnt exist
     current_counter integer;
 BEGIN
-    -- Lock the row, so noone else gets an old count.
-    LOCK TABLE sequentialnumbercounter IN ROW EXCLUSIVE MODE;
-    -- Assign the variable
-    SELECT SNC.counter
-    INTO current_counter
-    FROM sequentialnumbercounter SNC
-    WHERE SNC.owneruid = counter_owner
-      AND SNC.key = counter_key;
-    -- If it's 0, we add a new row
-    IF current_counter IS NULL THEN
-        INSERT INTO sequentialnumbercounter (id, owneruid, key, counter)
-        VALUES(nextval('hibernate_sequence'), counter_owner, counter_key, (1 + i));
-        RETURN 1;
-    END IF;
-    -- If it's not null, we update with the increment
-    UPDATE sequentialnumbercounter
-    SET counter = counter + i
-    WHERE owneruid = counter_owner
-      AND key = counter_key;
+    INSERT INTO sequentialnumbercounter  (id, owneruid, key, counter)
+    VALUES(nextval('hibernate_sequence'), counter_owner, counter_key, (1 + size) )
+
+    ON CONFLICT (owneruid, key)
+        DO
+            UPDATE SET counter = (sequentialnumbercounter.counter + size)
+    WHERE sequentialnumbercounter.owneruid = counter_owner
+      AND sequentialnumbercounter.key = counter_key
+
+    RETURNING counter INTO current_counter;
+
     RETURN current_counter;
+
 END;
-$$ LANGUAGE plpgsql;
+$$ LANGUAGE plpgsql; 
