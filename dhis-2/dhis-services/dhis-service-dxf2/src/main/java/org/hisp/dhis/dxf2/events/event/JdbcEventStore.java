d197d1a82e78d4074196a9ea65efe9f5679bea82
Stian Sandvold <stian@dhis2.org>
2020-08-11 09:34:20 +0200

    Merged and reverted bad changes from previous commit

--- dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/events/event/JdbcEventStore.java
+++ dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/events/event/JdbcEventStore.java
@@ -36,13 +36,10 @@ import static org.hisp.dhis.dxf2.events.event.EventSearchParams.*;
 import static org.hisp.dhis.util.DateUtils.*;
 
 import java.io.IOException;
-import java.sql.SQLException;
 import java.util.*;
 import java.util.stream.Collectors;
 
-import com.google.inject.internal.cglib.core.$ProcessArrayCallback;
 import org.apache.commons.lang3.StringUtils;
-import org.hibernate.SessionFactory;
 import org.hisp.dhis.common.*;
 import org.hisp.dhis.commons.collection.CachingMap;
 import org.hisp.dhis.commons.util.SqlHelper;
@@ -81,7 +78,6 @@ import com.vividsolutions.jts.io.WKTReader;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- *
  * @author Morten Olav Hansen <mortenoh@gmail.com>
  */
 @Slf4j
@@ -117,8 +113,7 @@ public class JdbcEventStore
 
     private final IdentifiableObjectManager manager;
 
-    public JdbcEventStore( StatementBuilder statementBuilder,
-        @Qualifier( "readOnlyJdbcTemplate" ) JdbcTemplate jdbcTemplate,
+    public JdbcEventStore( StatementBuilder statementBuilder, @Qualifier( "readOnlyJdbcTemplate" ) JdbcTemplate jdbcTemplate,
         CurrentUserService currentUserService, IdentifiableObjectManager identifiableObjectManager )
     {
         checkNotNull( statementBuilder );
@@ -156,17 +151,7 @@ public class JdbcEventStore
         List<Event> events = new ArrayList<>();
 
         String sql = buildSql( params, organisationUnits, user );
-
-        SqlRowSet rowSet;
-
-        if ( params.hasAssignedUsers() && !params.getAssignedUsers().isEmpty() )
-        {
-            rowSet = jdbcTemplate.queryForRowSet( sql, getQuotedCommaDelimitedString( params.getAssignedUsers() ) );
-        }
-        else
-        {
-            rowSet = jdbcTemplate.queryForRowSet( sql );
-        }
+        SqlRowSet rowSet = jdbcTemplate.queryForRowSet( sql );
 
         log.debug( "Event query SQL: " + sql );
 
@@ -364,16 +349,7 @@ public class JdbcEventStore
     {
         String sql = buildGridSql( params, organisationUnits );
 
-        SqlRowSet rowSet;
-
-        if ( params.hasAssignedUsers() && !params.getAssignedUsers().isEmpty() )
-        {
-            rowSet = jdbcTemplate.queryForRowSet( sql, getQuotedCommaDelimitedString( params.getAssignedUsers() ) );
-        }
-        else
-        {
-            rowSet = jdbcTemplate.queryForRowSet( sql );
-        }
+        SqlRowSet rowSet = jdbcTemplate.queryForRowSet( sql );
 
         log.debug( "Event query SQL: " + sql );
 
@@ -418,17 +394,7 @@ public class JdbcEventStore
         List<EventRow> eventRows = new ArrayList<>();
 
         String sql = buildSql( params, organisationUnits, user );
-
-        SqlRowSet rowSet;
-
-        if ( params.hasAssignedUsers() && !params.getAssignedUsers().isEmpty() )
-        {
-            rowSet = jdbcTemplate.queryForRowSet( sql, getQuotedCommaDelimitedString( params.getAssignedUsers() ) );
-        }
-        else
-        {
-            rowSet = jdbcTemplate.queryForRowSet( sql );
-        }
+        SqlRowSet rowSet = jdbcTemplate.queryForRowSet( sql );
 
         log.debug( "Event query SQL: " + sql );
 
@@ -619,14 +585,7 @@ public class JdbcEventStore
 
         log.debug( "Event query count SQL: " + sql );
 
-        if ( params.hasAssignedUsers() && !params.getAssignedUsers().isEmpty() )
-        {
-            return jdbcTemplate.queryForObject( sql, new Object[] { params.getAssignedUsers() }, Integer.class );
-        }
-        else
-        {
-            return jdbcTemplate.queryForObject( sql, Integer.class );
-        }
+        return jdbcTemplate.queryForObject( sql, Integer.class );
     }
 
     private DataValue convertEventDataValueIntoDtoDataValue( EventDataValue eventDataValue )
@@ -934,7 +893,7 @@ public class JdbcEventStore
 
         if ( params.hasAssignedUsers() )
         {
-            sql += hlp.whereAnd() + " (au.uid in (?)) ";
+            sql += hlp.whereAnd() + " (au.uid in (" + getQuotedCommaDelimitedString( params.getAssignedUsers() ) + ")) ";
         }
 
         if ( params.isIncludeOnlyUnassignedEvents() )
@@ -1127,7 +1086,7 @@ public class JdbcEventStore
 
         if ( params.hasAssignedUsers() )
         {
-            sql += hlp.whereAnd() + " (au.uid in (?)) ";
+            sql += hlp.whereAnd() + " (au.uid in (" + getQuotedCommaDelimitedString( params.getAssignedUsers() ) + ")) ";
         }
 
         if ( params.isIncludeOnlyUnassignedEvents() )
@@ -1206,6 +1165,7 @@ public class JdbcEventStore
 
         return sql;
     }
+
 
     private String getEventPagingQuery( EventSearchParams params )
     {
