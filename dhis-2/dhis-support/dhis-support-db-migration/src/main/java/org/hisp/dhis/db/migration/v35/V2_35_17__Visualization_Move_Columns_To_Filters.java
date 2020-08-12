2ec2f0d39d84f6b2e17b86b34261502f94fd1b8a
Enrico Colasante <enrico@dhis2.org>
2020-08-12 16:47:46 +0200

    fix: Fix flyway scripts  order (#5918)

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/java/org/hisp/dhis/db/migration/v35/V2_35_17__Visualization_Move_Columns_To_Filters.java
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/java/org/hisp/dhis/db/migration/v35/V2_35_17__Visualization_Move_Columns_To_Filters.java
@@ -36,7 +36,7 @@ import java.sql.Statement;
 import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.flywaydb.core.api.migration.Context;
 
-public class V2_35_11__Visualization_Move_Columns_To_Filters extends BaseJavaMigration
+public class V2_35_17__Visualization_Move_Columns_To_Filters extends BaseJavaMigration
 {
     @Override
     public void migrate( final Context context )
