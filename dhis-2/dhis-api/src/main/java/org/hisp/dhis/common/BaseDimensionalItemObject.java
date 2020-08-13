bcb34d9e684de1c06dff8b74c29fea6e52a22d6e
Luciano Fiandesio <luciano@dhis2.org>
2020-08-13 08:56:55 +0200

    fix: invoke super.equals() on BaseDimensionalItemObject (#5920)
    The `equals` function on `BaseDimensionaltemObject` was using the default equals comparison: `if ( o == null || getClass() != o.getClass() )` which, in case of Hibernate proxied objects, will not work.
    This fix removes that checks and calls the `equals` on the super class (`BaseNameableObject` which uses `isAssignableFrom` to handle proxy classes.

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/common/BaseDimensionalItemObject.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/common/BaseDimensionalItemObject.java
@@ -184,16 +184,6 @@ public class BaseDimensionalItemObject
     @Override
     public boolean equals( Object o )
     {
-        if ( this == o )
-        {
-            return true;
-        }
-
-        if ( o == null || getClass() != o.getClass() )
-        {
-            return false;
-        }
-
         if ( !super.equals( o ) )
         {
             return false;
