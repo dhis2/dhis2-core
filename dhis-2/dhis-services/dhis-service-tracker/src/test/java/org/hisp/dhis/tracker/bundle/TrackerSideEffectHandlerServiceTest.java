3668e5ab9ec114f0eac07178ecc1e8cca372aa8e
zubaira <zubair@dhis2.org>
2020-08-15 15:45:01 +0200

    changes for new rule engine set up in tracker importer

--- dhis-2/dhis-services/dhis-service-tracker/src/test/java/org/hisp/dhis/tracker/bundle/TrackerSideEffectHandlerServiceTest.java
+++ dhis-2/dhis-services/dhis-service-tracker/src/test/java/org/hisp/dhis/tracker/bundle/TrackerSideEffectHandlerServiceTest.java
@@ -31,7 +31,6 @@ package org.hisp.dhis.tracker.bundle;
 import org.hisp.dhis.IntegrationTestBase;
 import org.hisp.dhis.common.IdentifiableObject;
 import org.hisp.dhis.common.IdentifiableObjectManager;
-import org.hisp.dhis.commons.collection.ListUtils;
 import org.hisp.dhis.dxf2.metadata.objectbundle.ObjectBundle;
 import org.hisp.dhis.dxf2.metadata.objectbundle.ObjectBundleMode;
 import org.hisp.dhis.dxf2.metadata.objectbundle.ObjectBundleParams;
@@ -39,24 +38,18 @@ import org.hisp.dhis.dxf2.metadata.objectbundle.ObjectBundleService;
 import org.hisp.dhis.dxf2.metadata.objectbundle.ObjectBundleValidationService;
 import org.hisp.dhis.dxf2.metadata.objectbundle.feedback.ObjectBundleValidationReport;
 import org.hisp.dhis.importexport.ImportStrategy;
-import org.hisp.dhis.mock.MockCurrentUserService;
 import org.hisp.dhis.program.notification.ProgramNotificationInstance;
 
 import org.hisp.dhis.render.RenderFormat;
 import org.hisp.dhis.render.RenderService;
-import org.hisp.dhis.tracker.TrackerType;
-import org.hisp.dhis.tracker.job.TrackerSideEffectDataBundle;
-import org.hisp.dhis.tracker.preheat.TrackerPreheatService;
-import org.hisp.dhis.tracker.report.TrackerBundleReport;
-import org.hisp.dhis.tracker.report.TrackerStatus;
-import org.hisp.dhis.user.CurrentUserService;
-import org.hisp.dhis.user.User;
+import org.hisp.dhis.tracker.TrackerImportParams;
+import org.hisp.dhis.tracker.TrackerImportService;
+import org.hisp.dhis.tracker.report.TrackerImportReport;
 import org.hisp.dhis.user.UserService;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.test.util.ReflectionTestUtils;
 
 import java.io.IOException;
 import java.util.List;
@@ -76,9 +69,6 @@ import static org.junit.Assert.assertTrue;
 public class TrackerSideEffectHandlerServiceTest extends IntegrationTestBase
 {
     @Autowired
-    private TrackerPreheatService trackerPreheatService;
-
-    @Autowired
     private ObjectBundleService objectBundleService;
 
     @Autowired
@@ -91,12 +81,10 @@ public class TrackerSideEffectHandlerServiceTest extends IntegrationTestBase
     private UserService _userService;
 
     @Autowired
-    private TrackerBundleService trackerBundleService;
+    private TrackerImportService trackerImportService;
 
     @Autowired
     private IdentifiableObjectManager manager;
-
-    protected CurrentUserService currentUserService;
 
     @Override
     protected void setUpTest()
@@ -105,15 +93,8 @@ public class TrackerSideEffectHandlerServiceTest extends IntegrationTestBase
         renderService = _renderService;
         userService = _userService;
 
-        User user = createUser( "testUser" );
-
-        currentUserService = new MockCurrentUserService( user );
-
-        ReflectionTestUtils.setField( trackerPreheatService, "currentUserService", currentUserService );
-
         Map<Class<? extends IdentifiableObject>, List<IdentifiableObject>> metadata = renderService.fromMetadata(
             new ClassPathResource("tracker/tracker_metadata_with_program_rules.json").getInputStream(), RenderFormat.JSON );
-
 
         ObjectBundleParams params = new ObjectBundleParams();
         params.setObjectBundleMode( ObjectBundleMode.COMMIT );
@@ -132,39 +113,24 @@ public class TrackerSideEffectHandlerServiceTest extends IntegrationTestBase
     public void testRuleEngineSideEffectHandlerService()
         throws IOException
     {
-        TrackerBundle trackerBundle = renderService
+        TrackerImportParams trackerImportParams = renderService
             .fromJson( new ClassPathResource( "tracker/enrollment_data_with_program_rule_side_effects.json" )
-            .getInputStream(), TrackerBundleParams.class )
-            .toTrackerBundle();
+            .getInputStream(), TrackerImportParams.class );
 
-        assertEquals( 0, trackerBundle.getEvents().size() );
-        assertEquals( 1, trackerBundle.getTrackedEntities().size() );
+        assertEquals( 0, trackerImportParams.getEvents().size() );
+        assertEquals( 1, trackerImportParams.getTrackedEntities().size() );
 
-        TrackerBundleParams params = TrackerBundleParams.builder()
-            .events( trackerBundle.getEvents() )
-            .enrollments( trackerBundle.getEnrollments() )
-            .trackedEntities( trackerBundle.getTrackedEntities() ).build();
+        TrackerImportParams params = TrackerImportParams.builder()
+            .events( trackerImportParams.getEvents() )
+            .enrollments( trackerImportParams.getEnrollments() )
+            .trackedEntities( trackerImportParams.getTrackedEntities() )
+            .build();
 
-        User user = userService.getUser( ADMIN_USER_UID );
-        params.setUser( user );
         params.setUserId( ADMIN_USER_UID );
 
-        List<TrackerBundle> trackerBundles = trackerBundleService.create( params );
+        TrackerImportReport trackerImportReport = trackerImportService.importTracker( params );
 
-        assertEquals( 1, trackerBundles.size() );
-        assertEquals( trackerBundle.getEvents().size(), trackerBundles.get( 0 ).getEventRuleEffects().size() );
-
-        TrackerBundleReport report = trackerBundleService.commit( trackerBundles.get( 0 ) );
-
-        List<TrackerSideEffectDataBundle> sideEffectDataBundles = ListUtils.union(
-            report.getTypeReportMap().get( TrackerType.ENROLLMENT ).getSideEffectDataBundles(),
-            report.getTypeReportMap().get( TrackerType.EVENT ).getSideEffectDataBundles() );
-
-        trackerBundleService.handleTrackerSideEffects( sideEffectDataBundles );
-
-        assertEquals( report.getStatus(), TrackerStatus.OK );
-
-        await().atMost( 2, TimeUnit.SECONDS ).until( () -> manager.getAll( ProgramNotificationInstance.class ).size() > 0 );
+        await().atMost( 10, TimeUnit.SECONDS ).until( () -> manager.getAll( ProgramNotificationInstance.class ).size() > 0 );
 
         List<ProgramNotificationInstance> instances = manager.getAll( ProgramNotificationInstance.class );
 
