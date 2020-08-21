72f2942805fbd9460b228df3349931ca3ada20a4
zubaira <zubair@dhis2.org>
2020-08-21 14:08:32 +0200

    update uid for sms tables

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.35/V2_35_20__Add_uid_column_in_sms_table.sql
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.35/V2_35_20__Add_uid_column_in_sms_table.sql
@@ -6,3 +6,7 @@ alter table outbound_sms add column if not exists "uid" varchar(255);
 -- change id column type from integer to long
 alter table incomingsms alter column id type bigint;
 alter table outbound_sms alter column id type bigint;
+
+-- update uid column with randomly generated uid values
+update incomingsms set uid = generate_uid();
+update outbound_sms set uid = generate_uid();
