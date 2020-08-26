1292fb2ec17891ea677eaa38072922f992e0541a
Luciano Fiandesio <luciano@fiandes.io>
2020-08-26 11:42:18 +0200

    fix: remove static get for `ExternalAccessVoter`

--- dhis-2/dhis-web/dhis-web-commons/src/main/java/org/hisp/dhis/security/vote/LogicalOrAccessDecisionManager.java
+++ dhis-2/dhis-web/dhis-web-commons/src/main/java/org/hisp/dhis/security/vote/LogicalOrAccessDecisionManager.java
@@ -28,17 +28,19 @@ package org.hisp.dhis.security.vote;
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-import lombok.extern.slf4j.Slf4j;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 import org.springframework.context.annotation.Primary;
 import org.springframework.security.access.AccessDecisionManager;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.ConfigAttribute;
 import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * AccessDecisionManager which delegates to other AccessDecisionManagers in a
@@ -51,20 +53,19 @@ import java.util.List;
  * @version $Id: LogicalOrAccessDecisionManager.java 6335 2008-11-20 11:11:26Z larshelg $
  */
 @Primary
+@Component
 @Slf4j
-public class LogicalOrAccessDecisionManager
-    implements AccessDecisionManager
+public class LogicalOrAccessDecisionManager implements AccessDecisionManager
 {
-    private List<AccessDecisionManager> accessDecisionManagers = Collections.emptyList();
+    private List<AccessDecisionManager> accessDecisionManagers;
 
     public LogicalOrAccessDecisionManager(
         List<AccessDecisionManager> accessDecisionManagers )
     {
-        this.accessDecisionManagers = accessDecisionManagers;
-    }
-
-    public void setAccessDecisionManagers( List<AccessDecisionManager> accessDecisionManagers )
-    {
+        if ( accessDecisionManagers == null )
+        {
+            accessDecisionManagers = Collections.emptyList();
+        }
         this.accessDecisionManagers = accessDecisionManagers;
     }
 
