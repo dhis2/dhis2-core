a8001d46241b76015aa18b488d3cafdf78c49fa0
Stian Sandvold <stian@dhis2.org>
2020-08-28 09:20:14 +0200

    fix: DHIS2-9268 AssignedUser in Event (New Tracker Importer) (#5985)
    * initial
    
    * Added specific serializer to assignedUser. Added override methods for hook
    
    * Update AssignedUserValidationHookTest.java
    
    * Update AssignedUserValidationHook.java

---
+++ dhis-2/dhis-services/dhis-service-tracker/src/main/java/org/hisp/dhis/tracker/validation/hooks/AssignedUserValidationHook.java
@@ -0,0 +1,90 @@
+package org.hisp.dhis.tracker.validation.hooks;
+
+/*
+ * Copyright (c) 2004-2020, University of Oslo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * Neither the name of the HISP project nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import org.hisp.dhis.common.CodeGenerator;
+import org.hisp.dhis.trackedentity.TrackedEntityAttributeService;
+import org.hisp.dhis.tracker.TrackerIdScheme;
+import org.hisp.dhis.tracker.domain.Enrollment;
+import org.hisp.dhis.tracker.domain.Event;
+import org.hisp.dhis.tracker.domain.Relationship;
+import org.hisp.dhis.tracker.domain.TrackedEntity;
+import org.hisp.dhis.tracker.report.TrackerErrorReport;
+import org.hisp.dhis.tracker.report.ValidationErrorReporter;
+import org.hisp.dhis.user.User;
+import org.springframework.stereotype.Component;
+
+import static org.hisp.dhis.tracker.report.TrackerErrorCode.E1118;
+
+@Component
+public class AssignedUserValidationHook
+    extends AbstractTrackerDtoValidationHook
+{
+    public AssignedUserValidationHook( TrackedEntityAttributeService teAttrService )
+    {
+        super( teAttrService );
+    }
+
+    @Override
+    public void validateEvent( ValidationErrorReporter reporter, Event event )
+    {
+        if ( event.getAssignedUser() != null )
+        {
+            String uid = event.getAssignedUser().getUid();
+
+            if ( !CodeGenerator.isValidUid( event.getAssignedUser().getUid() )
+                ||
+                reporter.getValidationContext().getBundle().getPreheat().get( TrackerIdScheme.UID, User.class, uid ) ==
+                    null )
+            {
+                TrackerErrorReport.TrackerErrorReportBuilder report = TrackerErrorReport.builder()
+                .errorCode( E1118 )
+                .errorKlass( Event.class )
+                .addArg( uid );
+
+                reporter.addError( report );
+            }
+        }
+    }
+
+    @Override
+    public void validateEnrollment( ValidationErrorReporter reporter, Enrollment enrollment )
+    {
+    }
+
+    @Override
+    public void validateRelationship( ValidationErrorReporter reporter, Relationship relationship )
+    {
+    }
+
+    @Override
+    public void validateTrackedEntity( ValidationErrorReporter reporter, TrackedEntity tei )
+    {
+    }
+}
