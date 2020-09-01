a9ce358961f633297d334ceaba5be2304f38983e
Mohamed Ameen <ameen@dhis2.org>
2020-09-01 14:26:23 +0200

    fix: [DHIS2-9087] eventStatus ACTIVE is not respected in tei filtering (2.34- KSOIDC) (#6039)
    * fix: eventStatus ACTIVE is not respected in tei filtering
    
    * event status in hql

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/trackedentity/hibernate/HibernateTrackedEntityInstanceStore.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/trackedentity/hibernate/HibernateTrackedEntityInstanceStore.java
@@ -753,7 +753,7 @@ public class HibernateTrackedEntityInstanceStore
                 sql += " psi.executiondate >= '" + start + "' and psi.executiondate <= '" + end + "' " + "and psi.status = '" + EventStatus.COMPLETED.name()
                     + "' and ";
             }
-            else if ( params.isEventStatus( EventStatus.VISITED ) )
+            else if ( params.isEventStatus( EventStatus.VISITED ) || params.isEventStatus( EventStatus.ACTIVE ) )
             {
                 sql += " psi.executiondate >= '" + start + "' and psi.executiondate <= '" + end + "' " + "and psi.status = '" + EventStatus.ACTIVE.name()
                     + "' and ";
@@ -814,7 +814,7 @@ public class HibernateTrackedEntityInstanceStore
                 hql += " psi.executionDate >= '" + start + "' and psi.executionDate <= '" + end + "' " + "and psi.status = '" + EventStatus.COMPLETED.name()
                     + "' and ";
             }
-            else if ( params.isEventStatus( EventStatus.VISITED ) )
+            else if ( params.isEventStatus( EventStatus.VISITED ) || params.isEventStatus( EventStatus.ACTIVE ) )
             {
                 hql += " psi.executionDate >= '" + start + "' and psi.executionDate <= '" + end + "' " + "and psi.status = '" + EventStatus.ACTIVE.name()
                     + "' and ";
