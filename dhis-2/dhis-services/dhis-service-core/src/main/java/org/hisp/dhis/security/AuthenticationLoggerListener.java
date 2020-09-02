59f729e67d4b6ff28f0b79c99e8be230cc0a1aef
Morten Svanaes <msvanaes@dhis2.org>
2020-09-02 17:39:54 +0800

    chore: add missing permitall to urls
    * Adds permit all rule to urls that are outside (before) authentication
    * Add missing service to DefaultSelectionTreeManager
    * Add null check for user details on AuthenticationLoggerListener.java (happens when you register user and login automatically, and then try to log out)
    
    Signed-off-by: Morten Svanaes <msvanaes@dhis2.org>

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/security/AuthenticationLoggerListener.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/security/AuthenticationLoggerListener.java
@@ -28,16 +28,14 @@ package org.hisp.dhis.security;
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+import com.google.common.base.Charsets;
+import com.google.common.hash.HashCode;
+import com.google.common.hash.Hashing;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
 import org.springframework.security.authentication.event.AbstractAuthenticationEvent;
 import org.springframework.security.authentication.event.AbstractAuthenticationFailureEvent;
 import org.springframework.util.ClassUtils;
-
-import com.google.common.base.Charsets;
-import com.google.common.hash.HashCode;
-import com.google.common.hash.Hashing;
-
-import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author Morten Svanæs <msvanaes@dhis2.org>
@@ -58,7 +56,8 @@ public class AuthenticationLoggerListener
 
             Object details = event.getAuthentication().getDetails();
 
-            if ( ForwardedIpAwareWebAuthenticationDetails.class.isAssignableFrom( details.getClass() ) )
+            if ( details != null &&
+                ForwardedIpAwareWebAuthenticationDetails.class.isAssignableFrom( details.getClass() ) )
             {
                 ForwardedIpAwareWebAuthenticationDetails authDetails = (ForwardedIpAwareWebAuthenticationDetails) details;
                 String ip = authDetails.getIp();
