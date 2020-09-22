42a2433b818ec05b1ee28fabe3d8eed5bddac230
Enrico Colasante <enrico@dhis2.org>
2020-09-22 14:04:11 +0200

    fix: [DHIS2-9023] Block programStage deletion when a program rule is linked to it (#6163)
    * fix: [DHIS2-9023] Block programStage deletion when a program rule is linked to it

--- dhis-2/dhis-services/dhis-service-program-rule/src/main/java/org/hisp/dhis/programrule/ProgramRuleDeletionHandler.java
+++ dhis-2/dhis-services/dhis-service-program-rule/src/main/java/org/hisp/dhis/programrule/ProgramRuleDeletionHandler.java
@@ -28,9 +28,16 @@ package org.hisp.dhis.programrule;
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+import org.apache.commons.lang3.StringUtils;
+import org.hisp.dhis.common.BaseIdentifiableObject;
 import org.hisp.dhis.program.Program;
+import org.hisp.dhis.program.ProgramStage;
 import org.hisp.dhis.system.deletion.DeletionHandler;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
@@ -71,5 +78,26 @@ public class ProgramRuleDeletionHandler
         {
             programRuleService.deleteProgramRule( programRule );
         }
+    }
+
+    @Override
+    public String allowDeleteProgramStage( ProgramStage programStage )
+    {
+        String programRuleVariables = programRuleService
+            .getProgramRule( programStage.getProgram() )
+            .stream()
+            .filter( pr -> isLinkedToProgramStage( programStage, pr ) )
+            .map( BaseIdentifiableObject::getName )
+            .collect( Collectors.joining( ", " ) );
+
+        return StringUtils.isBlank( programRuleVariables ) ? null : programRuleVariables;
+    }
+
+    private boolean isLinkedToProgramStage( ProgramStage programStage, ProgramRule programRule )
+    {
+        return Objects.equals( programRule.getProgramStage(), programStage ) ||
+            programRule.getProgramRuleActions()
+                .stream()
+                .anyMatch( pra -> Objects.equals( pra.getProgramStage(), programStage ) );
     }
 }
