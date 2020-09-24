8fe28490e4582c78c5e2273fa3b132aa265c9e58
Maikel Arabori <51713408+maikelarabori@users.noreply.github.com>
2020-09-24 14:38:53 +0200

    fix: [DHIS2-9520] Adding user context interceptor to the request flow (patch/2.35) (#6235)

--- dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/security/config/WebMvcConfig.java
+++ dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/security/config/WebMvcConfig.java
@@ -34,10 +34,12 @@ import org.apache.commons.lang3.ArrayUtils;
 import org.hisp.dhis.common.Compression;
 import org.hisp.dhis.node.DefaultNodeService;
 import org.hisp.dhis.node.NodeService;
+import org.hisp.dhis.user.CurrentUserService;
 import org.hisp.dhis.webapi.mvc.CurrentUserHandlerMethodArgumentResolver;
 import org.hisp.dhis.webapi.mvc.CurrentUserInfoHandlerMethodArgumentResolver;
 import org.hisp.dhis.webapi.mvc.CustomRequestMappingHandlerMapping;
 import org.hisp.dhis.webapi.mvc.DhisApiVersionHandlerMethodArgumentResolver;
+import org.hisp.dhis.webapi.mvc.interceptor.UserContextInterceptor;
 import org.hisp.dhis.webapi.mvc.messageconverter.CsvMessageConverter;
 import org.hisp.dhis.webapi.mvc.messageconverter.ExcelMessageConverter;
 import org.hisp.dhis.webapi.mvc.messageconverter.JsonMessageConverter;
@@ -69,6 +71,7 @@ import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.DelegatingWebMvcConfiguration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 import java.util.Arrays;
@@ -96,6 +99,9 @@ public class WebMvcConfig extends DelegatingWebMvcConfiguration
 
     @Autowired
     private ContextService contextService;
+
+    @Autowired
+    private CurrentUserService currentUserService;
 
     @Bean( "multipartResolver" )
     public MultipartResolver multipartResolver()
@@ -194,6 +200,12 @@ public class WebMvcConfig extends DelegatingWebMvcConfiguration
         mapping.setOrder( 0 );
         mapping.setContentNegotiationManager( mvcContentNegotiationManager() );
         return mapping;
+    }
+
+    @Override
+    public void addInterceptors( InterceptorRegistry registry )
+    {
+        registry.addInterceptor( new UserContextInterceptor( currentUserService ) );
     }
 
     private Map<String, MediaType> mediaTypeMap = new ImmutableMap.Builder<String, MediaType>()
