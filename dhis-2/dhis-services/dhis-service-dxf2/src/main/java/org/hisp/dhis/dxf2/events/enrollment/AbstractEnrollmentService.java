93b0ede1d6e34c97cf398ec22ee3a6178ae9e98c
Enrico Colasante <enrico@dhis2.org>
2020-10-02 09:17:15 +0200

    fix: [DHIS2-8872] Fix import summary count for events in enrollment (#6257)

--- dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/events/enrollment/AbstractEnrollmentService.java
+++ dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/events/enrollment/AbstractEnrollmentService.java
@@ -407,6 +407,7 @@ public abstract class AbstractEnrollmentService
 
                 if ( importSummary.isStatus( ImportStatus.SUCCESS ) )
                 {
+                    events.forEach( e -> e.setEnrollment( enrollment.getEnrollment() ) );
                     events.addAll( enrollment.getEvents() );
                 }
             }
@@ -731,7 +732,9 @@ public abstract class AbstractEnrollmentService
 
                 if ( importSummary.isStatus( ImportStatus.SUCCESS ) )
                 {
-                    events.addAll( enrollment.getEvents() );
+                    List<Event> enrollmentEvents = enrollment.getEvents();
+                    enrollmentEvents.forEach( e -> e.setEnrollment( enrollment.getEnrollment() ) );
+                    events.addAll( enrollmentEvents );
                 }
             }
 
