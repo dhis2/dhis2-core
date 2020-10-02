ec7480bc75f1374667f532c71ef2f970c309eb01
Viet Nguyen <viet@dhis2.org>
2020-10-02 14:49:59 +0700

    fix: lazy exception in AuditObjectFactory (#6176)

--- dhis-2/dhis-support/dhis-support-artemis/src/main/java/org/hisp/dhis/artemis/audit/legacy/DefaultAuditObjectFactory.java
+++ dhis-2/dhis-support/dhis-support-artemis/src/main/java/org/hisp/dhis/artemis/audit/legacy/DefaultAuditObjectFactory.java
@@ -40,6 +40,7 @@ import org.hisp.dhis.commons.util.DebugUtils;
 import org.hisp.dhis.system.util.AnnotationUtils;
 import org.hisp.dhis.system.util.ReflectionUtils;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Field;
@@ -145,7 +146,7 @@ public class DefaultAuditObjectFactory implements AuditObjectFactory
         }
         catch ( JsonProcessingException e )
         {
-            log.error( DebugUtils.getStackTrace( e ) );
+            log.debug( DebugUtils.getStackTrace( e ) );
         }
 
         return null;
