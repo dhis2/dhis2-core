dcc0a6637ff276a4e57aab127aa41059e7cc1e38
Lars Helge Øverland <lars@dhis2.org>
2020-10-16 12:00:36 +0200

    Remove unused imports

--- dhis-2/dhis-services/dhis-service-tracker/src/test/java/org/hisp/dhis/tracker/programrule/ProgramRuleIntegrationTest.java
+++ dhis-2/dhis-services/dhis-service-tracker/src/test/java/org/hisp/dhis/tracker/programrule/ProgramRuleIntegrationTest.java
@@ -30,7 +30,6 @@ package org.hisp.dhis.tracker.programrule;
 
 import static org.hisp.dhis.tracker.utils.ImportUtils.build;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
