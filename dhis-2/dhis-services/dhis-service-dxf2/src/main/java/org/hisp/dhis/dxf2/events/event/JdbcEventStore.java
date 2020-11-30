74ec45d6b67e5b3ed8783435cf47d63c13035251
Giuseppe Nespolino <g.nespolino@gmail.com>
2020-11-30 13:45:14 +0100

    fix: DHIS-7937 quotes in filter values depend on filter type when rendering sql string (#6604)
    (cherry picked from commit 08574f966f50a4667668edb4077cfd9e21bd9e10)

--- dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/events/event/JdbcEventStore.java
+++ dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/events/event/JdbcEventStore.java
@@ -827,15 +827,15 @@ public class JdbcEventStore
                         else
                         {
                             eventDataValuesWhereSql += " " + queryCol + " " + filter.getSqlOperator() + " "
-                            + StringUtils.lowerCase( StringUtils.isNumeric( encodedFilter ) ? encodedFilter :
+                            + StringUtils.lowerCase( item.isNumeric() ? encodedFilter :
                             filter.getSqlFilter( encodedFilter ) ) + " ";
                         }
                     }
                     else if ( QueryOperator.IN.getValue().equalsIgnoreCase( filter.getSqlOperator() ) )
                     {
                         sql += "and " + queryCol + " " + filter.getSqlOperator() + " "
-                            + StringUtils.lowerCase( StringUtils.isNumeric( encodedFilter ) ? encodedFilter :
-                            filter.getSqlFilter( encodedFilter ) ) + " ";
+                            + StringUtils.lowerCase( item.isNumeric() ? encodedFilter :
+                            filter.getSqlFilter( encodedFilter ) ) + " " ;
                     }
                     else if ( QueryOperator.LIKE.getValue().equalsIgnoreCase( filter.getSqlOperator() ) )
                     {
@@ -845,7 +845,7 @@ public class JdbcEventStore
                     else
                     {
                         sql += "and lower(" + optCol + DOT_NAME + " " + filter.getSqlOperator() + " "
-                            + StringUtils.lowerCase( StringUtils.isNumeric( encodedFilter ) ? encodedFilter :
+                            + StringUtils.lowerCase( item.isNumeric() ? encodedFilter :
                             filter.getSqlFilter( encodedFilter ) ) + " ";
                     }
                 }
