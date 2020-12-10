2b6a057f4aebf7d0b7b4153adcc2a5cfd422e15b
Lars Helge Øverland <lars@dhis2.org>
2020-12-10 16:45:00 +0100

    Update SQL

--- dhis-2/dhis-services/dhis-service-validation/src/main/java/org/hisp/dhis/outlierdetection/service/OutlierDetectionManager.java
+++ dhis-2/dhis-services/dhis-service-validation/src/main/java/org/hisp/dhis/outlierdetection/service/OutlierDetectionManager.java
@@ -64,48 +64,48 @@ public class OutlierDetectionManager
                 "stats.mean as mean, " +
                 "stats.std_dev as std_dev, " +
                 "abs(dvs.value::double precision - stats.mean) as mean_abs_dev, " +
-                "abs(dvs.value::double precision - stats.mean) / stats.std_dev as z_score," +
-                "stats.mean - (stats.std_dev * :threshold) as lower_bound," +
-                "stats.mean + (stats.std_dev * :threshold) as upper_bound" +
+                "abs(dvs.value::double precision - stats.mean) / stats.std_dev as z_score, " +
+                "stats.mean - (stats.std_dev * :threshold) as lower_bound, " +
+                "stats.mean + (stats.std_dev * :threshold) as upper_bound " +
             // Data value query
             "from (" +
-                "select dv.dataelementid, dv.sourceid, dv.categoryoptioncomboid, dv.attributeoptioncomboid," +
+                "select dv.dataelementid, dv.sourceid, dv.categoryoptioncomboid, dv.attributeoptioncomboid, " +
                 "de.uid as de_uid, dv.periodid, ou.uid as ou_uid, coc.uid as coc_uid, aoc.uid as aoc_uid, dv.value " +
-                "from datavalue dv" +
-                "inner join dataelement de on dv.dataelementid = de.dataelementid" +
+                "from datavalue dv " +
+                "inner join dataelement de on dv.dataelementid = de.dataelementid " +
                 "inner join categoryoptioncombo coc on dv.categoryoptioncomboid = coc.categoryoptioncomboid " +
                 "inner join categoryoptioncombo aoc on dv.categoryoptioncomboid = aoc.categoryoptioncomboid " +
                 "inner join period pe on dv.periodid = pe.periodid " +
                 "inner join organisationunit ou on dv.sourceid = ou.organisationunitid " +
-                "where dv.dataelementid in (:data_element_ids)" +
-                "and pe.startdate >= ':start_date'" +
-                "and pe.enddate <= ':end_date'" +
-                "and " + ouPathClause +
+                "where dv.dataelementid in (:data_element_ids) " +
+                "and pe.startdate >= ':start_date' " +
+                "and pe.enddate <= ':end_date' " +
+                "and " + ouPathClause + " " +
                 "and dv.deleted is false" +
             ") as dvs" +
             // Mean and std dev mapping query
             "inner join (" +
                 "select dv.dataelementid as dataelementid, dv.sourceid as sourceid, " +
                 "dv.categoryoptioncomboid as categoryoptioncomboid, " +
-                "dv.attributeoptioncomboid as attributeoptioncomboid," +
+                "dv.attributeoptioncomboid as attributeoptioncomboid, " +
                 "avg(dv.value::double precision) as mean, " +
-                "stddev_pop(dv.value::double precision) as std_dev" +
-                "from datavalue dv" +
+                "stddev_pop(dv.value::double precision) as std_dev " +
+                "from datavalue dv " +
                 "inner join organisationunit ou on dv.sourceid = ou.organisationunitid " +
-                "where dv.dataelementid in (:data_element_ids)" +
-                "and " + ouPathClause +
-                "and dv.deleted is false" +
+                "where dv.dataelementid in (:data_element_ids) " +
+                "and " + ouPathClause + " " +
+                "and dv.deleted is false " +
                 "group by dv.dataelementid, dv.sourceid, dv.categoryoptioncomboid, dv.attributeoptioncomboid" +
             ") as stats" +
             // Join data queries
-            "on dvs.dataelementid = stats.dataelementid" +
-            "and dvs.sourceid = stats.sourceid" +
-            "and dvs.categoryoptioncomboid = stats.categoryoptioncomboid" +
-            "and dvs.attributeoptioncomboid = stats.attributeoptioncomboid" +
-            "where stats.std_dev != 0.0" +
+            "on dvs.dataelementid = stats.dataelementid " +
+            "and dvs.sourceid = stats.sourceid " +
+            "and dvs.categoryoptioncomboid = stats.categoryoptioncomboid " +
+            "and dvs.attributeoptioncomboid = stats.attributeoptioncomboid " +
+            "where stats.std_dev != 0.0 " +
             // Filter on z score, order and limit
-            "and (abs(cast(dvs.value as double precision) - stats.mean) / stats.std_dev) > :threshold" +
-            "order by :order_by desc" +
+            "and (abs(cast(dvs.value as double precision) - stats.mean) / stats.std_dev) > :threshold " +
+            "order by :order_by desc " +
             "limit :max_results;";
 
         SqlParameterSource params = new MapSqlParameterSource()
