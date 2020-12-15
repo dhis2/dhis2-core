6acecd7babf126abe2c2d25fd286eac559c82c86
Lars Helge Øverland <lars@dhis2.org>
2020-12-15 13:40:29 +0100

    Update test

--- dhis-2/dhis-services/dhis-service-validation/src/test/java/org/hisp/dhis/outlierdetection/service/OutlierDetectionServiceTest.java
+++ dhis-2/dhis-services/dhis-service-validation/src/test/java/org/hisp/dhis/outlierdetection/service/OutlierDetectionServiceTest.java
@@ -20,7 +20,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.google.common.collect.Lists;
 
-public class OutlierDetectionSeviceTest
+public class OutlierDetectionServiceTest
     extends IntegrationTestBase
 {
     @Autowired
