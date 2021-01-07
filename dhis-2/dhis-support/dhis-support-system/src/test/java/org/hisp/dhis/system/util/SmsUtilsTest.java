c6f7df05a85b0fb915686b8980e79f576757fa09
zubaira <zubair@dhis2.org>
2021-01-07 15:48:49 +0100

    use static import

--- dhis-2/dhis-support/dhis-support-system/src/test/java/org/hisp/dhis/system/util/SmsUtilsTest.java
+++ dhis-2/dhis-support/dhis-support-system/src/test/java/org/hisp/dhis/system/util/SmsUtilsTest.java
@@ -53,7 +53,12 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNull;
 
 
 /**
