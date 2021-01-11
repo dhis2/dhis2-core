8ced4a4fc7c2eb2dbbc7f7dc73f64aff30082d6a
Morten Hansen <morten@dhis2.org>
2021-01-11 22:17:46 +0700

    chore: update copyright to 2004-2021 (#7082)

--- dhis-2/dhis-services/dhis-service-core/src/test/java/org/hisp/dhis/schema/transformers/UserPropertyTransformerTest.java
+++ dhis-2/dhis-services/dhis-service-core/src/test/java/org/hisp/dhis/schema/transformers/UserPropertyTransformerTest.java
@@ -1,7 +1,7 @@
 package org.hisp.dhis.schema.transformers;
 
 /*
- * Copyright (c) 2004-2020, University of Oslo
+ * Copyright (c) 2004-2021, University of Oslo
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
