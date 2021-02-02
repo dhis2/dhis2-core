197db268172d2ff152658d1bfacb50a857afd3fc
Lars Helge Øverland <lars@dhis2.org>
2021-02-02 09:14:36 +0100

    Minor

--- dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/datavalue/DataValueController.java
+++ dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/datavalue/DataValueController.java
@@ -72,7 +72,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -472,7 +472,7 @@ public class DataValueController
     // Follow-up
     // ---------------------------------------------------------------------
 
-    @PostMapping( value = "/followup" )
+    @PutMapping( value = "/followup" )
     @ResponseStatus( value = HttpStatus.OK )
     public void setDataValueFollowUp( @RequestBody DataValueFollowUpRequest request )
     {
