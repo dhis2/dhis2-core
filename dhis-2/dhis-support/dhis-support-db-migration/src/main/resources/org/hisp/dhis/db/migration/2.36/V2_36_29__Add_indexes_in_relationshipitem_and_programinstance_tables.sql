52b29c97507e41dcee1083d5c93039acf89953f6
Enrico Colasante <enrico@dhis2.org>
2021-02-12 15:02:10 +0100

    Update dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.36/V2_36_29__Add_indexes_in_relationshipitem_and_programinstance_tables.sql

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.36/V2_36_29__Add_indexes_in_relationshipitem_and_programinstance_tables.sql
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.36/V2_36_29__Add_indexes_in_relationshipitem_and_programinstance_tables.sql
@@ -1,4 +1,4 @@
-create index if not exists programinstance_trackedentityinstance on programinstance (trackedentityinstanceid);
-create index if not exists relationshipitem_trackedentityinstance on relationshipitem (trackedentityinstanceid);
-create index if not exists relationshipitem_programinstance on relationshipitem (programinstanceid);
-create index if not exists relationshipitem_programstageinstance on relationshipitem (programstageinstanceid);
+create index if not exists in_programinstance_trackedentityinstance on programinstance (trackedentityinstanceid);
+create index if not exists in_relationshipitem_trackedentityinstance on relationshipitem (trackedentityinstanceid);
+create index if not exists in_relationshipitem_programinstance on relationshipitem (programinstanceid);
+create index if not exists in_relationshipitem_programstageinstance on relationshipitem (programstageinstanceid);
