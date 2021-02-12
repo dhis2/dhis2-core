cf123106a950fd03a80a8f73bef8010602347950
Enrico Colasante <enrico@dhis2.org>
2021-02-12 15:23:50 +0100

    Update dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.35/V2_35_29__Add_indexes_in_relationshipitem_and_programinstance_tables.sql

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.35/V2_35_29__Add_indexes_in_relationshipitem_and_programinstance_tables.sql
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.35/V2_35_29__Add_indexes_in_relationshipitem_and_programinstance_tables.sql
@@ -1,4 +1,4 @@
-create index if not exists in_programinstance_trackedentityinstance on programinstance (trackedentityinstanceid);
-create index if not exists in_relationshipitem_trackedentityinstance on relationshipitem (trackedentityinstanceid);
-create index if not exists in_relationshipitem_programinstance on relationshipitem (programinstanceid);
-create index if not exists in_relationshipitem_programstageinstance on relationshipitem (programstageinstanceid);
+create index if not exists in_programinstance_trackedentityinstanceid on programinstance (trackedentityinstanceid);
+create index if not exists in_relationshipitem_trackedentityinstanceid on relationshipitem (trackedentityinstanceid);
+create index if not exists in_relationshipitem_programinstanceid on relationshipitem (programinstanceid);
+create index if not exists in_relationshipitem_programstageinstanceid on relationshipitem (programstageinstanceid);
