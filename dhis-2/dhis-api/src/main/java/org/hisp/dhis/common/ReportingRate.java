47276fa491385aa38ad1c02f07b276d477b59bad
maikelarabori <maikelarabori@gmail.com>
2021-02-15 16:16:36 +0100

    fix: DHIS2-10386: Use long dash instead of short one

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/common/ReportingRate.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/common/ReportingRate.java
@@ -48,6 +48,8 @@ import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 public class ReportingRate
     extends BaseDimensionalItemObject implements EmbeddedObject
 {
+    private static final String SPACED_LONG_DASH = " — ";
+
     private DataSet dataSet;
 
     private ReportingRateMetric metric;
@@ -83,7 +85,7 @@ public class ReportingRate
     {
         String metricName = metric != null ? metric.displayName() : ReportingRateMetric.REPORTING_RATE.displayName();
 
-        return dataSet.getName() + " - " + metricName;
+        return dataSet.getName() + SPACED_LONG_DASH + metricName;
     }
 
     @Override
@@ -91,7 +93,7 @@ public class ReportingRate
     {
         String metricName = metric != null ? metric.displayName() : ReportingRateMetric.REPORTING_RATE.displayName();
 
-        return dataSet.getShortName() + " - " + metricName;
+        return dataSet.getShortName() + SPACED_LONG_DASH + metricName;
     }
 
     @Override
