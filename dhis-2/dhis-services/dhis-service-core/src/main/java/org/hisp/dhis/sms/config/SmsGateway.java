e1cd352cd32823685a9681a0bdd43058a2b0c43a
Zubair Asghar <zubair@dhis2.org>
2021-02-17 12:18:57 +0500

    fix: Add basic authentication support to SMS gateway
    JIRA #DHIS2-10446

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/sms/config/SmsGateway.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/sms/config/SmsGateway.java
@@ -28,16 +28,21 @@ package org.hisp.dhis.sms.config;
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+import java.util.Base64;
 import java.util.List;
 import java.util.Set;
 
 import org.hisp.dhis.outboundmessage.OutboundMessageBatch;
 import org.hisp.dhis.outboundmessage.OutboundMessageResponse;
 import org.hisp.dhis.sms.outbound.GatewayResponse;
+import org.jasypt.encryption.pbe.PBEStringEncryptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
@@ -55,13 +60,17 @@ import lombok.extern.slf4j.Slf4j;
 public abstract class SmsGateway
 {
     protected static final String PROTOCOL_VERSION = "X-Version";
+
     protected static final String MAX_MESSAGE_PART = "?maxMessageParts=4";
+
     protected static final String BASIC = " Basic ";
+
     public static final String KEY_TEXT = "text";
+
     public static final String KEY_RECIPIENT = "recipients";
 
     public static final Set<HttpStatus> OK_CODES = ImmutableSet.of( HttpStatus.OK,
-        HttpStatus.ACCEPTED, HttpStatus.CREATED );
+            HttpStatus.ACCEPTED, HttpStatus.CREATED );
 
     private static final ImmutableMap<HttpStatus, GatewayResponse> GATEWAY_RESPONSE_MAP = new ImmutableMap.Builder<HttpStatus, GatewayResponse>()
         .put( HttpStatus.OK, GatewayResponse.RESULT_CODE_200 )
@@ -75,16 +84,23 @@ public abstract class SmsGateway
         .put( HttpStatus.METHOD_NOT_ALLOWED, GatewayResponse.RESULT_CODE_405 )
         .put( HttpStatus.GONE, GatewayResponse.RESULT_CODE_410 )
         .put( HttpStatus.SERVICE_UNAVAILABLE, GatewayResponse.RESULT_CODE_503 )
+        .put( HttpStatus.FORBIDDEN, GatewayResponse.RESULT_CODE_403 )
         .put( HttpStatus.INTERNAL_SERVER_ERROR, GatewayResponse.RESULT_CODE_504 ).build();
 
     @Autowired
     private RestTemplate restTemplate;
 
-    protected abstract List<OutboundMessageResponse> sendBatch( OutboundMessageBatch batch, SmsGatewayConfig gatewayConfig );
+    @Autowired
+    @Qualifier( "tripleDesStringEncryptor" )
+    private PBEStringEncryptor pbeStringEncryptor;
+
+    protected abstract List<OutboundMessageResponse> sendBatch( OutboundMessageBatch batch,
+                                                                SmsGatewayConfig gatewayConfig );
 
     protected abstract boolean accept( SmsGatewayConfig gatewayConfig );
 
-    protected abstract OutboundMessageResponse send( String subject, String text, Set<String> recipients, SmsGatewayConfig gatewayConfig );
+    protected abstract OutboundMessageResponse send( String subject, String text, Set<String> recipients,
+                                                     SmsGatewayConfig gatewayConfig );
 
     public HttpStatus send( String urlTemplate, HttpEntity<?> request, HttpMethod httpMethod, Class<?> klass )
     {
@@ -152,5 +168,19 @@ public abstract class SmsGateway
         status.setDescription( gatewayResponse.getResponseMessage() );
 
         return status;
+    }
+
+    protected HttpHeaders getAuthenticationHeaderParameters( SmsGatewayConfig config )
+    {
+        String credentials = config.getUsername().trim() + ":" +
+            pbeStringEncryptor.decrypt( config.getPassword().trim() );
+        String encodedCredentials = Base64.getEncoder().encodeToString( credentials.getBytes() );
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set( HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE );
+        headers.set( HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE );
+        headers.set( HttpHeaders.AUTHORIZATION, BASIC + encodedCredentials );
+
+        return headers;
     }
 }
