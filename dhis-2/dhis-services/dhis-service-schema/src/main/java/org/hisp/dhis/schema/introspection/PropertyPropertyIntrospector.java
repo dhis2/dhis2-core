f4bb64b593f934a4a34e23942d10ad8f383f57df
Jan Bernitt <jaanbernitt@gmail.com>
2021-02-19 14:43:54 +0100

    chore: move implementation of Property introspector from utils [DHIS2-10407]

--- dhis-2/dhis-services/dhis-service-schema/src/main/java/org/hisp/dhis/schema/introspection/PropertyPropertyIntrospector.java
+++ dhis-2/dhis-services/dhis-service-schema/src/main/java/org/hisp/dhis/schema/introspection/PropertyPropertyIntrospector.java
@@ -27,10 +27,30 @@
  */
 package org.hisp.dhis.schema.introspection;
 
-import java.util.Map;
+import static org.hisp.dhis.schema.PropertyType.BOOLEAN;
+import static org.hisp.dhis.schema.PropertyType.CONSTANT;
+import static org.hisp.dhis.schema.PropertyType.DATE;
+import static org.hisp.dhis.schema.PropertyType.REFERENCE;
+import static org.hisp.dhis.system.util.AnnotationUtils.getAnnotation;
+import static org.hisp.dhis.system.util.AnnotationUtils.isAnnotationPresent;
 
+import java.lang.reflect.Method;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Map;
+import java.util.Set;
+
+import org.hisp.dhis.common.IdentifiableObject;
 import org.hisp.dhis.schema.Property;
-import org.hisp.dhis.system.util.SchemaUtils;
+import org.hisp.dhis.schema.PropertyType;
+import org.hisp.dhis.schema.annotation.Property.Access;
+import org.hisp.dhis.schema.annotation.Property.Value;
+import org.hisp.dhis.schema.annotation.PropertyRange;
+import org.hisp.dhis.schema.annotation.PropertyTransformer;
+import org.springframework.util.Assert;
+
+import com.google.common.collect.Sets;
+import com.google.common.primitives.Primitives;
 
 /**
  * A {@link PropertyIntrospector} that reads
@@ -39,10 +59,13 @@ import org.hisp.dhis.system.util.SchemaUtils;
  *
  * It will not add {@link Property} values to the map.
  *
+ * @author Morten Olav Hansen <mortenoh@gmail.com> (original author)
  * @author Jan Bernitt (extracted from {@link JacksonPropertyIntrospector})
  */
 public class PropertyPropertyIntrospector implements PropertyIntrospector
 {
+    private static final Set<PropertyType> PROPS_IGNORE_MIN_MAX = Sets.newHashSet( REFERENCE, BOOLEAN, DATE, CONSTANT );
+
     @Override
     public void introspect( Class<?> klass, Map<String, Property> properties )
     {
@@ -50,8 +73,183 @@ public class PropertyPropertyIntrospector implements PropertyIntrospector
         {
             if ( property.getKlass() != null )
             {
-                SchemaUtils.updatePropertyTypes( property );
+                updatePropertyTypes( property );
             }
         }
     }
+
+    private static void updatePropertyTypes( Property property )
+    {
+        Assert.notNull( property, "Property cannot be null" );
+        Assert.notNull( property.getKlass(), "Property class cannot be null" );
+
+        property.setPropertyType( getPropertyType( property.getKlass() ) );
+
+        if ( property.isCollection() )
+        {
+            property.setItemPropertyType( getPropertyType( property.getItemKlass() ) );
+        }
+
+        if ( property.isWritable() )
+        {
+            initFromPropertyAnnotation( property );
+            initFromPropertyTransformerAnnotation( property );
+            initFromPropertyRangeAnnotation( property );
+            ensureMinMaxDefaults( property );
+        }
+        else
+        {
+            property.setMin( null );
+            property.setMax( null );
+        }
+    }
+
+    private static void ensureMinMaxDefaults( Property property )
+    {
+        if ( property.getMin() == null )
+        {
+            property.setMin( 0d );
+        }
+
+        if ( property.getMax() == null )
+        {
+            property.setMax( property.is( PropertyType.INTEGER, PropertyType.TEXT )
+                ? (double) Integer.MAX_VALUE
+                : Double.MAX_VALUE );
+        }
+
+        if ( !property.isCollection() && PROPS_IGNORE_MIN_MAX.contains( property.getPropertyType() ) )
+        {
+            property.setMin( null );
+            property.setMax( null );
+        }
+    }
+
+    private static void initFromPropertyRangeAnnotation( Property property )
+    {
+        if ( isAnnotationPresent( property.getGetterMethod(), PropertyRange.class ) )
+        {
+            PropertyRange range = getAnnotation( property.getGetterMethod(), PropertyRange.class );
+
+            double max = range.max();
+            double min = range.min();
+
+            if ( max > Integer.MAX_VALUE
+                && property.is( PropertyType.INTEGER, PropertyType.TEXT, PropertyType.COLLECTION ) )
+            {
+                max = Integer.MAX_VALUE;
+            }
+
+            if ( property.is( PropertyType.COLLECTION ) && min < 0 )
+            {
+                min = 0d;
+            }
+
+            // Max will be applied from PropertyRange annotation only if it
+            // is more restrictive than hibernate max (or its a collection)
+            if ( property.getMax() == null || max < property.getMax() || property.is( PropertyType.COLLECTION ) )
+            {
+                property.setMax( max );
+            }
+
+            // Min is not set by hibernate (always 0) hence the min from
+            // PropertyRange will always be applied.
+            property.setMin( min );
+        }
+    }
+
+    private static void initFromPropertyTransformerAnnotation( Property property )
+    {
+        Method getter = property.getGetterMethod();
+        if ( isAnnotationPresent( getter, PropertyTransformer.class ) )
+        {
+            property.setPropertyTransformer( getAnnotation( getter, PropertyTransformer.class ).value() );
+        }
+    }
+
+    private static void initFromPropertyAnnotation( Property property )
+    {
+        Method getter = property.getGetterMethod();
+        if ( isAnnotationPresent( getter, org.hisp.dhis.schema.annotation.Property.class ) )
+        {
+            org.hisp.dhis.schema.annotation.Property pAnnotation = getAnnotation( getter,
+                org.hisp.dhis.schema.annotation.Property.class );
+            property.setPropertyType( pAnnotation.value() );
+
+            if ( pAnnotation.required() != Value.DEFAULT )
+            {
+                property.setRequired( pAnnotation.required() == Value.TRUE );
+            }
+
+            if ( pAnnotation.persisted() != Value.DEFAULT )
+            {
+                property.setPersisted( pAnnotation.persisted() == Value.TRUE );
+            }
+
+            if ( pAnnotation.owner() != Value.DEFAULT )
+            {
+                property.setOwner( pAnnotation.owner() == Value.TRUE );
+            }
+
+            if ( Access.READ_ONLY == pAnnotation.access() )
+            {
+                property.setWritable( false );
+            }
+
+            if ( Access.WRITE_ONLY == pAnnotation.access() )
+            {
+                property.setReadable( false );
+            }
+        }
+    }
+
+    private static PropertyType getPropertyType( Class<?> klass )
+    {
+        if ( isAssignableFrom( klass, String.class )
+            || isAssignableFrom( klass, Character.class )
+            || isAssignableFrom( klass, Byte.class ) )
+        {
+            return PropertyType.TEXT;
+        }
+        if ( isAssignableFrom( klass, Integer.class ) )
+        {
+            return PropertyType.INTEGER;
+        }
+        if ( isAssignableFrom( klass, Boolean.class ) )
+        {
+            return PropertyType.BOOLEAN;
+        }
+        if ( isAssignableFrom( klass, Float.class )
+            || isAssignableFrom( klass, Double.class ) )
+        {
+            return PropertyType.NUMBER;
+        }
+        if ( isAssignableFrom( klass, Date.class )
+            || isAssignableFrom( klass, java.sql.Date.class ) )
+        {
+            return PropertyType.DATE;
+        }
+        if ( isAssignableFrom( klass, Enum.class ) )
+        {
+            return PropertyType.CONSTANT;
+        }
+        if ( isAssignableFrom( klass, IdentifiableObject.class ) )
+        {
+            return PropertyType.REFERENCE;
+        }
+        if ( isAssignableFrom( klass, Collection.class ) )
+        {
+            return PropertyType.COLLECTION;
+        }
+
+        // if klass is primitive (but unknown), fall back to text, if its not
+        // then assume reference
+        return Primitives.isWrapperType( klass ) ? PropertyType.TEXT : PropertyType.COMPLEX;
+    }
+
+    private static boolean isAssignableFrom( Class<?> propertyKlass, Class<?> klass )
+    {
+        return klass.isAssignableFrom( propertyKlass );
+    }
+
 }
