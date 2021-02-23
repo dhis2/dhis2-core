6fc448606bfff07d3286221c35edecc17bf0c253
Morten Svanaes <msvanaes@dhis2.org>
2021-02-24 01:09:36 +0800

    fix: Repair broken OAuth2 authorization_code flow
    Signed-off-by: Morten Svanaes <msvanaes@dhis2.org>

--- dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/security/config/OAuth2ConfirmAccessController.java
+++ dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/security/config/OAuth2ConfirmAccessController.java
@@ -1,3 +1,5 @@
+package org.hisp.dhis.webapi.security.config;
+
 /*
  * Copyright (c) 2004-2021, University of Oslo
  * All rights reserved.
@@ -25,8 +27,6 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
-package org.hisp.dhis.webapi.security.config;
 
 import java.util.HashMap;
 import java.util.Map;
