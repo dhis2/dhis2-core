2f4af82cf1813d42fa916a0887db5d8109ae8fcf
maikelarabori <maikelarabori@gmail.com>
2021-02-24 12:21:18 +0100

    fix: DHIS2-10079: Removing migration, not needed

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.36/V2_36_32__Clean_new_outlier_column_in_visualization.sql
+++
@@ -1,3 +0,0 @@
--- We are resetting the state of this new column.
--- This column was introduced in 2.36, but as we had changes in the JSONB object, we need to reset it.
-UPDATE visualization SET outlieranalysis = NULL;
