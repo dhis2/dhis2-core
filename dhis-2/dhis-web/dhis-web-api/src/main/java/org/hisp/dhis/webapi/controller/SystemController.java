aac2b356772caf4c4edc8f27e3e505243b456f8c
Enrico Colasante <enrico.colasante@gmail.com>
2021-03-01 18:00:45 +0100

    Fix formatting

--- dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/SystemController.java
+++ dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/SystemController.java
@@ -33,7 +33,6 @@ import static org.hisp.dhis.webapi.utils.ContextUtils.setNoStore;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
