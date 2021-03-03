a9bf309ec3251297bc7835958b98549bb68a6c4c
luca <lucacambi77@gmail.com>
2021-03-03 11:00:11 +0100

    feat: adds integration tests in EventImportValidationTest for option value check

--- dhis-2/dhis-services/dhis-service-tracker/src/test/java/org/hisp/dhis/tracker/validation/EventImportValidationTest.java
+++ dhis-2/dhis-services/dhis-service-tracker/src/test/java/org/hisp/dhis/tracker/validation/EventImportValidationTest.java
@@ -162,6 +162,22 @@ public class EventImportValidationTest
     }
 
     @Test
+    public void failValidationWhenTrackedEntityAttributeHasWrongOptionValue()
+        throws IOException
+    {
+        TrackerImportParams params = createBundleFromJson(
+            "tracker/validations/events-with_wrong_option_value.json" );
+
+        ValidateAndCommitTestUnit createAndUpdate = validateAndCommit( params, TrackerImportStrategy.CREATE );
+        TrackerValidationReport report = createAndUpdate.getValidationReport();
+        printReport( report );
+        assertEquals( 1, report.getErrorReports().size() );
+
+        assertThat( report.getErrorReports(),
+            everyItem( hasProperty( "errorCode", equalTo( TrackerErrorCode.E1125 ) ) ) );
+    }
+
+    @Test
     public void testEventValidationOkAll()
         throws IOException
     {
