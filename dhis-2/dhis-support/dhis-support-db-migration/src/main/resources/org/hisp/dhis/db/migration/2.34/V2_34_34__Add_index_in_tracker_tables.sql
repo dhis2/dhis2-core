6ff2fed250a6fc019a5f8e8aa8cd8777d0b5190e
Ameen <ameen@dhis2.org>
2021-03-08 16:28:45 +0100

    Add index to programstageinstance

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.34/V2_34_34__Add_index_in_tracker_tables.sql
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.34/V2_34_34__Add_index_in_tracker_tables.sql
@@ -1,3 +1,4 @@
 create unique index if not exists in_unique_trackedentityprogramowner_teiid_programid_ouid on trackedentityprogramowner (trackedentityinstanceid, programid, organisationunitid);
 create index if not exists in_programinstance_programid on programinstance (programid);
 create unique index if not exists in_trackedentityinstance_trackedentityattribute_value on trackedentityattributevalue (trackedentityinstanceid, trackedentityattributeid, lower(value));
+create index in_programstageinstance_status_executiondate on programstageinstance USING btree (status,executiondate);
