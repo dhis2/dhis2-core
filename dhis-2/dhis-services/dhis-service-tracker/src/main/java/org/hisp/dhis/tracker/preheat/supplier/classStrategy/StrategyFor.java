fcb331168e11ad742fa553615787de594f3fb9eb
Stian Sandvold <stian@dhis2.org>
2021-03-11 16:57:04 +0100

    fix: Adjust preheat caches for new tracker importer (#7606)
    * Add specific cache capacity for all preheat caches. lower the default cache capacity
    
    * fix: typo
    
    * fix: Fixed failing tests
    
    (cherry picked from commit 4b36ab6a72402f0342a160fcac5688482bb54a04)

--- dhis-2/dhis-services/dhis-service-tracker/src/main/java/org/hisp/dhis/tracker/preheat/supplier/classStrategy/StrategyFor.java
+++ dhis-2/dhis-services/dhis-service-tracker/src/main/java/org/hisp/dhis/tracker/preheat/supplier/classStrategy/StrategyFor.java
@@ -61,8 +61,11 @@ public @interface StrategyFor
     int ttl() default 5;
 
     /**
-     * The maximum number of entries hold by the cache. Defaults to
-     * Long.MAX_VALUE
+     * The maximum number of entries hold by the cache. Defaults to 5. The
+     * reason for the low default, is that certain objects can contain a lot of
+     * references and quickly consume memory. For most metadata, a capacity of 5
+     * is not necessarily small either. We should always specify capacity for
+     * each strategy, on not rely on the default.
      */
-    long capacity() default Long.MAX_VALUE;
+    long capacity() default 5;
 }
