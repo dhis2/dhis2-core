213beb8402b1c70774f755cfc5785b4646457c12
Stian Sandvold <stian@dhis2.org>
2021-03-11 15:41:19 +0100

    Add specific cache capacity for all preheat caches. lower the default cache capacity

--- dhis-2/dhis-services/dhis-service-tracker/src/main/java/org/hisp/dhis/tracker/preheat/supplier/classStrategy/StrategyFor.java
+++ dhis-2/dhis-services/dhis-service-tracker/src/main/java/org/hisp/dhis/tracker/preheat/supplier/classStrategy/StrategyFor.java
@@ -62,7 +62,10 @@ public @interface StrategyFor
 
     /**
      * The maximum number of entries hold by the cache. Defaults to
-     * Long.MAX_VALUE
+     * 5. The reason for the low default, is that certain objects can contain
+     * a lot of references and quickly consume memory. For most metadata, a
+     * capacity of 5 is not neccesarily small either. We should always specify
+     * capacity for each strategy, on not rely on the default.
      */
-    long capacity() default Long.MAX_VALUE;
+    long capacity() default 5;
 }
