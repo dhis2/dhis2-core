b2162b07fadc58d4aa36ecb17729c3ef8c6b1883
Jan Bernitt <jaanbernitt@gmail.com>
2021-03-11 13:26:17 +0100

    test: adds skeleton for MetadataImportExportController test [DHIS2-10657]

---
+++ dhis-2/dhis-web/dhis-web-api-test/src/test/java/org/hisp/dhis/webapi/controller/MetadataImportExportControllerTest.java
@@ -0,0 +1,10 @@
+package org.hisp.dhis.webapi.controller;
+
+import org.hisp.dhis.webapi.DhisControllerConvenienceTest;
+
+/**
+ * @author Jan Bernitt
+ */
+public class MetadataImportExportControllerTest extends DhisControllerConvenienceTest
+{
+}
