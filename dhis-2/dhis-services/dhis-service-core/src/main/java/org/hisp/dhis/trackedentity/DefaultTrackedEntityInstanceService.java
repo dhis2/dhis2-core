6ce7d1884395b0bff212763514cdddc974e1daf9
Ameen <ameen@dhis2.org>
2021-03-12 14:07:07 +0100

    minor

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/trackedentity/DefaultTrackedEntityInstanceService.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/trackedentity/DefaultTrackedEntityInstanceService.java
@@ -244,11 +244,11 @@ public class DefaultTrackedEntityInstanceService
     /**
      * This method handles any dynamic sort order columns in the params. These
      * has to be added to attribute list if there it is neither present in
-     * attribute list
+     * attribute list nor filter list.
      *
-     * For example, if attributes doesnt have a specific trackedentityattribute
-     * uid, but sorting has been requested for that tea uid, then we need to add
-     * them to the attribute list.
+     * For example, if attributes or filters doesnt have a specific
+     * trackedentityattribute uid, but sorting has been requested for that tea
+     * uid, then we need to add them to the attribute list.
      *
      * @param params The TEIQueryParams object
      */
@@ -262,9 +262,10 @@ public class DefaultTrackedEntityInstanceService
                     return attributeService.getTrackedEntityAttribute( orderParam.getField() );
                 } ).collect( Collectors.toList() );
 
-            // adding to attributes conditionally if they are not already
-            // present
-            params.addAttributesIfNotExist( QueryItem.getQueryItems( sortAttributes ) );
+            // adding to attributes conditionally if they are also not present
+            // in filters.
+            params.addAttributesIfNotExist( QueryItem.getQueryItems( sortAttributes ).stream()
+                .filter( sAtt -> !params.getFilters().contains( sAtt ) ).collect( Collectors.toList() ) );
         }
     }
 
