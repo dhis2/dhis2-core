9ddddbfc4d7344a66d42a5fea0d8e634776d5d58
zubaira <zubair@dhis2.org>
2021-03-17 15:38:27 +0100

    fix flyway

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.37/V2_37_2__Add_sendrepeatable_column.sql
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.37/V2_37_2__Add_sendrepeatable_column.sql
@@ -1,5 +1,5 @@
 -- Add sendrepeatable column to programnotificationtemplate table.
 -- For backward compatibility set sendrepeatable to false where null.
 
-alter table programnotificationtemplate add column if not exists sendrepeatable;
+alter table programnotificationtemplate add column if not exists sendrepeatable boolean;
 update programnotificationtemplate set sendrepeatable = false where sendrepeatable is null;
