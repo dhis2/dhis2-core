2af1aab124d3e78d9c35d637449cd455c3977cb9
Maikel Arabori <51713408+maikelarabori@users.noreply.github.com>
2021-03-18 10:14:36 +0100

    fix: DHIS2-9934: Adding correct support for shortName and displayShortName (#7572)

--- dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/dataitem/validator/OrderValidator.java
+++ dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/dataitem/validator/OrderValidator.java
@@ -30,18 +30,14 @@ package org.hisp.dhis.webapi.controller.dataitem.validator;
 import static org.apache.commons.collections4.CollectionUtils.isNotEmpty;
 import static org.apache.commons.lang3.StringUtils.trimToEmpty;
 import static org.hisp.dhis.feedback.ErrorCode.E2015;
-import static org.hisp.dhis.feedback.ErrorCode.E2036;
 import static org.hisp.dhis.feedback.ErrorCode.E2037;
 import static org.hisp.dhis.webapi.controller.dataitem.Order.Attribute.getNames;
 import static org.hisp.dhis.webapi.controller.dataitem.Order.Nature.getValues;
-import static org.hisp.dhis.webapi.controller.dataitem.validator.FilterValidator.FILTER_ATTRIBUTE_NAME;
 
 import java.util.Set;
 
 import org.hisp.dhis.common.IllegalQueryException;
 import org.hisp.dhis.feedback.ErrorMessage;
-import org.hisp.dhis.webapi.controller.dataitem.Filter;
-import org.hisp.dhis.webapi.controller.dataitem.Order;
 
 /**
  * Validator class responsible for validating order parameters.
@@ -98,61 +94,6 @@ public class OrderValidator
                 else
                 {
                     throw new IllegalQueryException( new ErrorMessage( E2015, orderParam ) );
-                }
-            }
-        }
-    }
-
-    /**
-     * Checks if the given of order params can be used in combination with the
-     * given filters. Currently the only valid combinations allowed when using
-     * "name" or "displayName" are:
-     *
-     * 1) "order=displayName:asc => filter=displayName:eq:something" 2)
-     * "order=name:asc => filter=name:eq:something"
-     *
-     * In the other hand these are NOT ALLOWED:
-     *
-     * 1) "order=displayName:asc => filter=name:eq:something" 2) "order=name:asc
-     * => filter=displayName:eq:something"
-     *
-     * In other words, if one filters by "name", it cannot be ordered by
-     * "displayName" and vice-versa.
-     *
-     * @param orderParams a set containing elements in the format
-     *        "attributeName:asc"
-     * @param filters the set of filters, where each element has the format
-     *        something like "attributeName:eq:value"
-     * @throws IllegalQueryException if some of the filters are not allowed with
-     *         any order param.
-     */
-    public static void checkOrderParamsAndFiltersAllowance( final Set<String> orderParams, final Set<String> filters )
-    {
-        if ( isNotEmpty( orderParams ) && isNotEmpty( filters ) )
-        {
-            for ( final String orderParam : orderParams )
-            {
-                final String[] orderAttributeValuePair = orderParam.split( ":" );
-                final String orderAttributeName = trimToEmpty( orderAttributeValuePair[ORDERING_ATTRIBUTE_NAME] );
-
-                for ( final String filter : filters )
-                {
-                    final String[] array = filter.split( ":" );
-
-                    final String filterAttributeName = trimToEmpty( array[FILTER_ATTRIBUTE_NAME] );
-
-                    if ( trimToEmpty( orderAttributeName ).equalsIgnoreCase( Order.Attribute.DISPLAY_NAME.getName() )
-                        && trimToEmpty( filterAttributeName ).equalsIgnoreCase( Filter.Attribute.NAME.getName() ) )
-                    {
-                        throw new IllegalQueryException( new ErrorMessage( E2036, orderParam + " + " + filter ) );
-                    }
-
-                    if ( trimToEmpty( orderAttributeName ).equalsIgnoreCase( Order.Attribute.NAME.getName() )
-                        && trimToEmpty( filterAttributeName )
-                            .equalsIgnoreCase( Filter.Attribute.DISPLAY_NAME.getName() ) )
-                    {
-                        throw new IllegalQueryException( new ErrorMessage( E2036, orderParam + " + " + filter ) );
-                    }
                 }
             }
         }
