c88f741b4705858144d6aab3a474cf46d591d609
LUCA <lucacambi77@gmail.com>
2021-03-23 13:09:16 +0100

    fix: adds not null on entity and db

---
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.33/V2_33_38__Add_not_null_trackedentitytypeattribute_foreign_keys.sql
@@ -0,0 +1,2 @@
+ALTER TABLE trackedentitytypeattribute ALTER COLUMN trackedentityattributeid SET NOT NULL;
+ALTER TABLE trackedentitytypeattribute ALTER COLUMN trackedentitytypeattributeid SET NOT NULL;
