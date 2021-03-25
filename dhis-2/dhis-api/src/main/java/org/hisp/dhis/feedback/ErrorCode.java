f1374a3e5f50a0a6313f43077233e28fa25a4902
Lars Helge Øverland <lars@dhis2.org>
2021-03-25 15:27:01 +0100

    fix: Set default job parameters when none exist for validation [DHIS2-10768] (#7770)
    For JobConfiguration there is a concept of adding a default instance of job parameters when no parameters exist. This was not set during the validation stage, leading to validation failing for job configurations which would otherwise import well.

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/feedback/ErrorCode.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/feedback/ErrorCode.java
@@ -190,17 +190,16 @@ public enum ErrorCode
 
     /* Scheduling */
     E7000(
-        "Failed to add/update job configuration, another job of the same job type is already scheduled with this cron expression: `{0}`" ),
+        "Failed to add/update job configuration, another job of the same type already scheduled with cron expression: `{0}`" ),
     E7002( "Failed to add/update job configuration, UID does not exist" ),
     E7003(
         "Failed to add/update job configuration, only interval can be configured for non configurable job type: `{0}`" ),
     E7004(
         "Failed to add/update job configuration, cron expression must be not null for job with scheduling type CRON: `{0}`" ),
     E7005( "Failed to add/update job configuration, cron expression is invalid: `{0}` " ),
-    E7006( "Failed to execute job `{0}`." ),
-    E7007(
-        "Failed to add/update job configuration - Delay must be not null for jobs with scheduling type FIXED_DELAY: `{0}`" ),
-    E7010( "Failed to validate job runtime - `{0}`" ),
+    E7006( "Failed to execute job: `{0}`." ),
+    E7007( "Failed to add/update job configuration, delay must be not null with scheduling type FIXED_DELAY: `{0}`" ),
+    E7010( "Failed to validate job runtime: `{0}`" ),
 
     /* Aggregate analytics */
     E7100( "Query parameters cannot be null" ),
