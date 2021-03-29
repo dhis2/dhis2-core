bd6a8f5c968ab639a0d7c387ee9a9e5d8e92bdfe
lucaCambi77 <lucacambi77@gmail.com>
2021-03-29 11:50:17 +0200

    fix: [DHIS2-10702] - Unexpected error querying tracked entity instance porting [2.35] (#7735)
    * fix: adds tracked entity type validation hook and tests
    
    * fix: sonarqube and format
    
    * fix: adds object bundle book and tests
    
    * fix: merge v_2-33-36_script
    
    * fix: adds clean up script for trackedentitytypeattribute table
    
    * fix: adds not null on entity and db
    
    * fix: renames trackedentitytypeattributeid to trackedentitytypeid
    
    * fix: sonarqube issue object cast on map
    
    * fix: removes redundant check on trackedentitytypeid
    
    * fix: merge to single sql scripts
    
    * fix: adds not already existing trackedEntityType
    
    * fix: apply spotless plugin
    
    * fix: adds sql clean up script
    
    * fix: adds not null on entity and db
    
    * fix: adds not null foreign keys
    
    * fix: remove tracked entity type id
    
    * fix: merge to single sql scripts
    
    * fix: adds not already existing trackedEntityType
    
    * fix: update copyright
    
    * fix: move sql script to 2_35 folder
    
    * fix: adds java doc to TrackedEntityTypeObjectBundleHook
    
    * fix: remove already present file in other versions

---
+++ dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/metadata/objectbundle/hooks/TrackedEntityTypeObjectBundleHook.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2004-2021, University of Oslo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * Neither the name of the HISP project nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.hisp.dhis.dxf2.metadata.objectbundle.hooks;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+import org.hisp.dhis.common.IdentifiableObject;
+import org.hisp.dhis.dxf2.metadata.objectbundle.ObjectBundle;
+import org.hisp.dhis.feedback.ErrorCode;
+import org.hisp.dhis.feedback.ErrorReport;
+import org.hisp.dhis.preheat.PreheatIdentifier;
+import org.hisp.dhis.trackedentity.TrackedEntityAttribute;
+import org.hisp.dhis.trackedentity.TrackedEntityType;
+import org.springframework.stereotype.Component;
+
+/**
+ * Validation hook for embedded TrackEntityAttribute in TrackedEntityType. If
+ * TrackEntityAttributes are associated, then we check if they exist in Preheat
+ * by identifier otherwise we report error
+ *
+ * @author Luca Cambi <luca@dhis2.org>
+ */
+@Component( "org.hisp.dhis.dxf2.metadata.objectbundle.hooks.TrackedEntityTypeObjectBundleHook" )
+public class TrackedEntityTypeObjectBundleHook extends AbstractObjectBundleHook
+{
+    @Override
+    public <T extends IdentifiableObject> List<ErrorReport> validate( T object, ObjectBundle bundle )
+    {
+        List<ErrorReport> errorReports = new ArrayList<>();
+
+        Optional.ofNullable( object ).filter( o -> o.getClass().isAssignableFrom( TrackedEntityType.class ) )
+            .map( TrackedEntityType.class::cast )
+            .flatMap( trackedEntityType -> Optional.ofNullable( trackedEntityType.getTrackedEntityAttributes() ) )
+            .ifPresent( teAttrs -> teAttrs.stream().filter( Objects::nonNull ).forEach( tea -> {
+                PreheatIdentifier preheatIdentifier = bundle.getPreheatIdentifier();
+
+                if ( bundle.getPreheat().get( preheatIdentifier, tea ) == null )
+                {
+                    errorReports.add(
+                        new ErrorReport( TrackedEntityAttribute.class, ErrorCode.E5001, preheatIdentifier,
+                            preheatIdentifier.getIdentifiersWithName( tea ) ).setErrorProperty( "id" )
+                                .setMainId( tea.getUid() ) );
+                }
+
+            } ) );
+
+        return errorReports;
+    }
+}
