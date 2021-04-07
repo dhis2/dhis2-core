d648554d47a9540fdcebff7f3b051451610fed45
Stian Sandvold <stian@dhis2.org>
2021-04-07 10:12:57 +0200

    fix: Tracker preheat cache disabled (#7706)
    * fix: Disable cache for new tracker importer. Not working as intended and potentially ignoring acl
    
    * fix: accidentally overwrote previous change; added back in
    
    * fix: Disabled preheat-cache related tests, since we disabled cache for now

--- dhis-2/dhis-services/dhis-service-tracker/src/main/java/org/hisp/dhis/tracker/preheat/cache/DefaultPreheatCacheService.java
+++ dhis-2/dhis-services/dhis-service-tracker/src/main/java/org/hisp/dhis/tracker/preheat/cache/DefaultPreheatCacheService.java
@@ -27,8 +27,6 @@
  */
 package org.hisp.dhis.tracker.preheat.cache;
 
-import static org.hisp.dhis.commons.util.SystemUtils.isTestRun;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -42,7 +40,6 @@ import org.cache2k.Cache;
 import org.cache2k.Cache2kBuilder;
 import org.hisp.dhis.common.IdentifiableObject;
 import org.hisp.dhis.common.event.ApplicationCacheClearedEvent;
-import org.hisp.dhis.external.conf.ConfigurationKey;
 import org.hisp.dhis.external.conf.DhisConfigurationProvider;
 import org.hisp.dhis.tracker.TrackerIdScheme;
 import org.springframework.context.event.EventListener;
@@ -156,7 +153,13 @@ public class DefaultPreheatCacheService implements PreheatCacheService
 
     private boolean isCacheEnabled()
     {
-        return !isTestRun( this.environment.getActiveProfiles() )
-            && config.isEnabled( ConfigurationKey.TRACKER_IMPORT_PREHEAT_CACHE_ENABLED );
+        return false;
+
+        // Due to concerns and issues with the current cache implementation, we
+        // decided to
+        // deactivate the cache in the preheat completely for now.
+        // return !isTestRun( this.environment.getActiveProfiles() )
+        // && config.isEnabled(
+        // ConfigurationKey.TRACKER_IMPORT_PREHEAT_CACHE_ENABLED );
     }
 }
