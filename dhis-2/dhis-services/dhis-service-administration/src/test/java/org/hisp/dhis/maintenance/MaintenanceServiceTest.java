dfd1be1b52e23951d22934514ab7b50959623c5e
zubaira <zubair@dhis2.org>
2021-04-13 16:36:43 +0200

    changes required for 2.35

--- dhis-2/dhis-services/dhis-service-administration/src/test/java/org/hisp/dhis/maintenance/MaintenanceServiceTest.java
+++ dhis-2/dhis-services/dhis-service-administration/src/test/java/org/hisp/dhis/maintenance/MaintenanceServiceTest.java
@@ -221,14 +221,16 @@ public class MaintenanceServiceTest
         programMessageRecipients.setPhoneNumbers( Sets.newHashSet( "testphone" ) );
         programMessageRecipients.setOrganisationUnit( organisationUnit );
 
-        ProgramMessage message = ProgramMessage.builder().subject( "subject" ).text( "text" )
-            .recipients( programMessageRecipients )
-            .deliveryChannels( Sets.newHashSet( DeliveryChannel.EMAIL ) )
-            .programStageInstance( programStageInstance ).build();
+        ProgramMessage pm = new ProgramMessage();
+        pm.setSubject( "subject" );
+        pm.setText( "text" );
+        pm.setRecipients( programMessageRecipients );
+        pm.setDeliveryChannels( Sets.newHashSet( DeliveryChannel.EMAIL ) );
+        pm.setProgramStageInstance( programStageInstance );
 
         long idA = programStageInstanceService.addProgramStageInstance( programStageInstance );
 
-        programMessageService.saveProgramMessage( message );
+        programMessageService.saveProgramMessage( pm );
 
         assertNotNull( programStageInstanceService.getProgramStageInstance( idA ) );
 
@@ -254,13 +256,15 @@ public class MaintenanceServiceTest
         programMessageRecipients.setOrganisationUnit( organisationUnit );
         programMessageRecipients.setTrackedEntityInstance( entityInstanceB );
 
-        ProgramMessage message = ProgramMessage.builder().subject( "subject" ).text( "text" )
-            .recipients( programMessageRecipients )
-            .deliveryChannels( Sets.newHashSet( DeliveryChannel.EMAIL ) ).build();
+        ProgramMessage pm = new ProgramMessage();
+        pm.setSubject( "subject" );
+        pm.setText( "text" );
+        pm.setRecipients( programMessageRecipients );
+        pm.setDeliveryChannels( Sets.newHashSet( DeliveryChannel.EMAIL ) );
 
         long idA = entityInstanceService.addTrackedEntityInstance( entityInstanceB );
 
-        programMessageService.saveProgramMessage( message );
+        programMessageService.saveProgramMessage( pm );
 
         assertNotNull( entityInstanceService.getTrackedEntityInstance( idA ) );
 
