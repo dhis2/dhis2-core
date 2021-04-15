0b24962c265905cafd404a93519ebcd38afcd30e
Giuseppe Nespolino <g.nespolino@gmail.com>
2021-04-15 12:59:47 +0200

    test: tests added

---
+++ dhis-2/dhis-support/dhis-support-json-filtering/src/test/java/squiggly/model/Item.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2004-2021, University of Oslo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * Neither the name of the HISP project nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package squiggly.model;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Item
+{
+
+    private final String id;
+
+    private final String name;
+
+    private final List<Item> items;
+
+    public Item( String id, String name )
+    {
+        this( id, name, Collections.<Item> emptyList() );
+    }
+
+    public Item( String id, String name, Item item )
+    {
+        this( id, name, Collections.singletonList( item ) );
+    }
+
+    public Item( String id, String name, List<Item> items )
+    {
+        this.id = id;
+        this.name = name;
+        this.items = items;
+    }
+
+    public String getId()
+    {
+        return id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public List<Item> getItems()
+    {
+        return items;
+    }
+
+    public static Item testItem()
+    {
+        Item item5 = new Item( "ITEM-5", "Binoculars" );
+        Item item4 = new Item( "ITEM-4", "Hoverboard", item5 );
+        Item item3 = new Item( "ITEM-3", "Milkshake", item4 );
+        Item item2 = new Item( "ITEM-2", "Life Preserver", item3 );
+
+        return new Item( "ITEM-1", "Nike Shoes", item2 );
+    }
+}
