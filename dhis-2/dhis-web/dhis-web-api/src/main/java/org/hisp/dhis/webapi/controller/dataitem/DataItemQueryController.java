de5977853ad5e29a17450e411c73bdbc5becc91d
maikelarabori <maikelarabori@gmail.com>
2021-04-22 16:54:07 +0200

    fix: DHIS2-10945: Sorting issue causing invalid SQL (2.36)

--- dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/dataitem/DataItemQueryController.java
+++ dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/dataitem/DataItemQueryController.java
@@ -147,8 +147,6 @@ public class DataItemQueryController
 
         checkNamesAndOperators( filters );
         checkOrderParams( orderParams.getOrders() );
-        // checkOrderParamsAndFiltersAllowance( orderParams.getOrders(), filters
-        // );
 
         // Extracting the target entities to be queried.
         final Set<Class<? extends BaseIdentifiableObject>> targetEntities = dataItemServiceFacade
@@ -165,7 +163,7 @@ public class DataItemQueryController
         final RootNode rootNode = createMetadata();
 
         responseHandler.addResultsToNode( rootNode, dimensionalItems, fields );
-        responseHandler.addPaginationToNode( rootNode, new ArrayList<>( targetEntities ), currentUser, options,
+        responseHandler.addPaginationToNode( rootNode, targetEntities, currentUser, options,
             filters );
 
         return new ResponseEntity<>( rootNode, OK );
