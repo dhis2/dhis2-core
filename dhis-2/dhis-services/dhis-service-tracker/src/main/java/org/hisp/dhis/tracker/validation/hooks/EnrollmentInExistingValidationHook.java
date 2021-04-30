e130ca73da763adc4a61a27ce72d80daa24b381d
LUCA <lucacambi77@gmail.com>
2021-04-30 10:53:41 +0200

    feat: updates validation hook

--- dhis-2/dhis-services/dhis-service-tracker/src/main/java/org/hisp/dhis/tracker/validation/hooks/EnrollmentInExistingValidationHook.java
+++ dhis-2/dhis-services/dhis-service-tracker/src/main/java/org/hisp/dhis/tracker/validation/hooks/EnrollmentInExistingValidationHook.java
@@ -30,29 +30,18 @@ package org.hisp.dhis.tracker.validation.hooks;
 import static com.google.api.client.util.Preconditions.checkNotNull;
 import static org.hisp.dhis.tracker.report.TrackerErrorCode.E1015;
 import static org.hisp.dhis.tracker.report.TrackerErrorCode.E1016;
-import static org.hisp.dhis.tracker.validation.hooks.TrackerImporterAssertErrors.PROGRAM_CANT_BE_NULL;
-import static org.hisp.dhis.tracker.validation.hooks.TrackerImporterAssertErrors.PROGRAM_INSTANCE_CANT_BE_NULL;
-import static org.hisp.dhis.tracker.validation.hooks.TrackerImporterAssertErrors.TRACKED_ENTITY_INSTANCE_CANT_BE_NULL;
-import static org.hisp.dhis.tracker.validation.hooks.TrackerImporterAssertErrors.USER_CANT_BE_NULL;
+import static org.hisp.dhis.tracker.validation.hooks.TrackerImporterAssertErrors.*;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-import org.hisp.dhis.common.OrganisationUnitSelectionMode;
-import org.hisp.dhis.program.Program;
-import org.hisp.dhis.program.ProgramInstance;
-import org.hisp.dhis.program.ProgramInstanceQueryParams;
-import org.hisp.dhis.program.ProgramInstanceService;
+import org.hisp.dhis.program.*;
 import org.hisp.dhis.trackedentity.TrackedEntityInstance;
-import org.hisp.dhis.trackedentity.TrackerOwnershipManager;
 import org.hisp.dhis.tracker.domain.Enrollment;
 import org.hisp.dhis.tracker.domain.EnrollmentStatus;
 import org.hisp.dhis.tracker.report.ValidationErrorReporter;
 import org.hisp.dhis.tracker.validation.TrackerImportValidationContext;
-import org.hisp.dhis.tracker.validation.service.TrackerImportAccessManager;
-import org.hisp.dhis.user.User;
 import org.springframework.stereotype.Component;
 
 /**
@@ -62,34 +51,17 @@ import org.springframework.stereotype.Component;
 public class EnrollmentInExistingValidationHook
     extends AbstractTrackerDtoValidationHook
 {
-    protected final TrackerOwnershipManager trackerOwnershipManager;
-
-    protected final ProgramInstanceService programInstanceService;
-
-    private final TrackerImportAccessManager trackerImportAccessManager;
-
-    public EnrollmentInExistingValidationHook( TrackerOwnershipManager trackerOwnershipManager,
-        ProgramInstanceService programInstanceService,
-        TrackerImportAccessManager trackerImportAccessManager )
-    {
-        checkNotNull( trackerOwnershipManager );
-        checkNotNull( programInstanceService );
-        checkNotNull( trackerImportAccessManager );
-
-        this.trackerOwnershipManager = trackerOwnershipManager;
-        this.programInstanceService = programInstanceService;
-        this.trackerImportAccessManager = trackerImportAccessManager;
-    }
-
     @Override
     public void validateEnrollment( ValidationErrorReporter reporter, Enrollment enrollment )
     {
-        TrackerImportValidationContext validationContext = reporter.getValidationContext();
+        checkNotNull( enrollment, ENROLLMENT_CANT_BE_NULL );
 
         if ( EnrollmentStatus.CANCELLED == enrollment.getStatus() )
         {
             return;
         }
+
+        TrackerImportValidationContext validationContext = reporter.getValidationContext();
 
         Program program = validationContext.getProgram( enrollment.getProgram() );
 
@@ -104,107 +76,67 @@ public class EnrollmentInExistingValidationHook
         validateTeiNotEnrolledAlready( reporter, enrollment, program );
     }
 
-    protected void validateTeiNotEnrolledAlready( ValidationErrorReporter reporter,
+    private void validateTeiNotEnrolledAlready( ValidationErrorReporter reporter,
         Enrollment enrollment, Program program )
     {
-        User user = reporter.getValidationContext().getBundle().getUser();
-
-        checkNotNull( user, USER_CANT_BE_NULL );
-        checkNotNull( program, PROGRAM_CANT_BE_NULL );
         checkNotNull( enrollment.getTrackedEntity(), TRACKED_ENTITY_INSTANCE_CANT_BE_NULL );
 
         TrackedEntityInstance tei = getTrackedEntityInstance( reporter, enrollment.getTrackedEntity() );
 
-        // TODO: Create a dedicated sql query....?
-        Set<Enrollment> activeAndCompleted = getAllEnrollments( reporter, program, tei.getUid() )
-            .stream()
-            .filter( e -> !e.getEnrollment().equals( enrollment.getEnrollment() )
-                && (EnrollmentStatus.ACTIVE == e.getStatus() || EnrollmentStatus.COMPLETED == e.getStatus()) )
+        Set<Enrollment> payloadEnrollment = reporter.getValidationContext().getBundle().getEnrollments()
+            .stream().filter( Objects::nonNull )
+            .filter( pi -> pi.getProgram().equals( program.getUid() ) )
+            .filter( pi -> pi.getTrackedEntity().equals( tei.getUid() )
+                && !pi.getUid().equals( enrollment.getEnrollment() ) )
+            .filter( pi -> EnrollmentStatus.ACTIVE == pi.getStatus() || EnrollmentStatus.COMPLETED == pi.getStatus() )
             .collect( Collectors.toSet() );
+
+        Set<Enrollment> dbEnrollment = reporter.getValidationContext().getBundle().getPreheat()
+            .getTrackedEntityToProgramInstanceMap().getOrDefault( enrollment.getTrackedEntity(), new ArrayList<>() )
+            .stream()
+            .filter( Objects::nonNull )
+            .filter( pi -> pi.getProgram().getUid().equals( program.getUid() )
+                && !pi.getUid().equals( enrollment.getEnrollment() ) )
+            .filter( pi -> ProgramStatus.ACTIVE == pi.getStatus() || ProgramStatus.COMPLETED == pi.getStatus() )
+            .collect( Collectors.toSet() ).stream().map( this::getEnrollmentFromProgramInstance )
+            .collect( Collectors.toSet() );
+
+        // Priority to payload
+        Collection<Enrollment> mergedEnrollments = Stream.of( payloadEnrollment, dbEnrollment )
+            .flatMap( Set::stream )
+            .collect( Collectors.toMap( Enrollment::getEnrollment,
+                p -> p,
+                ( Enrollment x, Enrollment y ) -> x ) )
+            .values();
 
         if ( EnrollmentStatus.ACTIVE == enrollment.getStatus() )
         {
-            Set<Enrollment> activeOnly = activeAndCompleted.stream()
+            Set<Enrollment> activeOnly = mergedEnrollments.stream()
                 .filter( e -> EnrollmentStatus.ACTIVE == e.getStatus() )
                 .collect( Collectors.toSet() );
 
             if ( !activeOnly.isEmpty() && !activeOnly.contains( enrollment ) )
             {
-                // TODO: How do we do this check on an import set, this only
-                // checks when the DB already contains it
                 addError( reporter, E1015, tei, program );
             }
         }
 
-        if ( Boolean.TRUE.equals( program.getOnlyEnrollOnce() ) && !activeAndCompleted.isEmpty() )
+        if ( !mergedEnrollments.isEmpty() )
         {
             addError( reporter, E1016, tei, program );
         }
     }
 
-    private List<Enrollment> getAllEnrollments( ValidationErrorReporter reporter, Program program,
-        String trackedEntityInstanceUid )
-    {
-        User user = reporter.getValidationContext().getBundle().getUser();
-
-        checkNotNull( user, USER_CANT_BE_NULL );
-        checkNotNull( program, PROGRAM_CANT_BE_NULL );
-        // checkNotNull( trackedEntityInstance,
-        // TRACKED_ENTITY_INSTANCE_CANT_BE_NULL );
-
-        ProgramInstanceQueryParams params = new ProgramInstanceQueryParams();
-        params.setOrganisationUnitMode( OrganisationUnitSelectionMode.ALL );
-        params.setSkipPaging( true );
-        params.setProgram( program );
-        params.setTrackedEntityInstanceUid( trackedEntityInstanceUid );
-        List<ProgramInstance> programInstances = programInstanceService.getProgramInstances( params );
-
-        List<Enrollment> all = new ArrayList<>();
-
-        for ( ProgramInstance programInstance : programInstances )
-        {
-            if ( trackerOwnershipManager
-                .hasAccess( user, programInstance.getEntityInstance(), programInstance.getProgram() ) )
-            {
-
-                ValidationErrorReporter localReporter = new ValidationErrorReporter( reporter.getValidationContext() );
-                trackerImportAccessManager.checkReadEnrollmentAccess( localReporter, programInstance.getProgram(),
-                    programInstance.getOrganisationUnit(), programInstance.getEntityInstance().getUid() );
-
-                if ( localReporter.hasErrors() )
-                {
-                    reporter.merge( localReporter );
-                }
-                else
-                {
-                    all.add( getEnrollmentFromProgramInstance( programInstance ) );
-                }
-            }
-        }
-
-        return all;
-    }
-
     public Enrollment getEnrollmentFromProgramInstance( ProgramInstance programInstance )
     {
-        checkNotNull( programInstance, PROGRAM_INSTANCE_CANT_BE_NULL );
-
         Enrollment enrollment = new Enrollment();
         enrollment.setEnrollment( programInstance.getUid() );
-
         enrollment.setStatus( EnrollmentStatus.fromProgramStatus( programInstance.getStatus() ) );
+
         return enrollment;
     }
 
-    /**
-     * Get a {@link TrackedEntityInstance} from the pre-heat or from the
-     * reference tree.
-     *
-     * @param reporter the {@link ValidationErrorReporter} object
-     * @param uid the UID of a {@link TrackedEntityInstance} object
-     * @return a TrackedEntityInstance
-     */
-    public TrackedEntityInstance getTrackedEntityInstance( ValidationErrorReporter reporter, String uid )
+    private TrackedEntityInstance getTrackedEntityInstance( ValidationErrorReporter reporter, String uid )
     {
         TrackedEntityInstance tei = reporter.getValidationContext().getTrackedEntityInstance( uid );
 
