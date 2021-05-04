e3979d994d5a452f8f3a928758c8fa4f493c35ed
LUCA <lucacambi77@gmail.com>
2021-05-04 15:38:07 +0200

    fix: add tests and solve conflicts

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/feedback/ErrorCode.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/feedback/ErrorCode.java
@@ -114,6 +114,8 @@ public enum ErrorCode
     E4028( "Option set `{0}` already contains option `{1}`" ),
     E4029( "Job parameters cannot be null for job type: {0}" ),
     E4030( "Object could not be deleted because it is associated with another object: {0}" ),
+    E4032( "A program rule variable with name `{0}` and program uid `{1}` already exists" ),
+    E4033( "Keywords (and | or | not) aren't allowed for program rule variable with name `{0}`" ),
 
     /* SQL views */
     E4300( "SQL query is null" ),
