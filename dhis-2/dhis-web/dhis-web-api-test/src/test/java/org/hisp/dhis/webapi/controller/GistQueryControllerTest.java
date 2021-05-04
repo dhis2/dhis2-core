abca922412e77295d7300550ea09154ae7ef2f12
Jan Bernitt <jaanbernitt@gmail.com>
2021-05-04 15:30:23 +0200

    test: adds tests for Gist API validation

--- dhis-2/dhis-web/dhis-web-api-test/src/test/java/org/hisp/dhis/webapi/controller/GistQueryControllerTest.java
+++ dhis-2/dhis-web/dhis-web-api-test/src/test/java/org/hisp/dhis/webapi/controller/GistQueryControllerTest.java
@@ -560,6 +560,45 @@ public class GistQueryControllerTest extends DhisControllerConvenienceTest
                 getSuperuserUid() ).content().string() );
     }
 
+    /*
+     * Validation
+     */
+
+    @Test
+    public void testGistObjectList_Filter_MisplacedArgument()
+    {
+        assertEquals( "Filter `surname:null:[value]` uses an unary operator and does not need an argument.",
+            GET( "/users/gist?filter=surname:null:value" ).error( HttpStatus.BAD_REQUEST ).getMessage() );
+    }
+
+    @Test
+    public void testGistObjectList_Filter_MissingArgument()
+    {
+        assertEquals( "Filter `surname:eq:[]` uses a binary operator that does need an argument.",
+            GET( "/users/gist?filter=surname:eq" ).error( HttpStatus.BAD_REQUEST ).getMessage() );
+    }
+
+    @Test
+    public void testGistObjectList_Filter_TooManyArguments()
+    {
+        assertEquals( "Filter `surname:gt:[a, b]` can only be used with a single argument.",
+            GET( "/users/gist?filter=surname:gt:[a,b]" ).error( HttpStatus.BAD_REQUEST ).getMessage() );
+    }
+
+    @Test
+    public void testGistObjectList_Filter_UserDoesNotExist()
+    {
+        assertEquals( "User for filter `surname:canread:[not-a-UID]` does not exist.",
+            GET( "/users/gist?filter=surname:canRead:not-a-UID" ).error( HttpStatus.BAD_REQUEST ).getMessage() );
+    }
+
+    @Test
+    public void testGistObjectList_Order_CollectionProperty()
+    {
+        assertEquals( "Property `userGroup` cannot be used as order property.",
+            GET( "/users/gist?order=userGroups" ).error( HttpStatus.BAD_REQUEST ).getMessage() );
+    }
+
     private void createDataSetsForOrganisationUnit( int count, String organisationUnitId, String namePrefix )
     {
         for ( int i = 0; i < count; i++ )
