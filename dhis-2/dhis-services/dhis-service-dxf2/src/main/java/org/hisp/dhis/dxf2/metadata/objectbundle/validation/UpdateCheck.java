d86ba9847cf2e4babe65f8ab6da1fc2acaff70c6
Jan Bernitt <jaanbernitt@gmail.com>
2021-05-06 17:27:48 +0200

    perf: avoid intermediate collections during dxf2 import validation [TECH-547]

--- dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/metadata/objectbundle/validation/UpdateCheck.java
+++ dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/metadata/objectbundle/validation/UpdateCheck.java
@@ -27,32 +27,31 @@
  */
 package org.hisp.dhis.dxf2.metadata.objectbundle.validation;
 
+import static org.hisp.dhis.dxf2.metadata.objectbundle.validation.ValidationUtils.createObjectReport;
+
 import java.util.List;
+import java.util.function.Consumer;
 
 import org.hisp.dhis.common.IdentifiableObject;
 import org.hisp.dhis.dxf2.metadata.objectbundle.ObjectBundle;
 import org.hisp.dhis.feedback.ErrorCode;
 import org.hisp.dhis.feedback.ErrorReport;
-import org.hisp.dhis.feedback.TypeReport;
+import org.hisp.dhis.feedback.ObjectReport;
 import org.hisp.dhis.importexport.ImportStrategy;
 
 /**
  * @author Luciano Fiandesio
  */
-public class UpdateCheck
-    implements
-    ValidationCheck
+public class UpdateCheck implements ObjectValidationCheck
 {
     @Override
-    public TypeReport check( ObjectBundle bundle, Class<? extends IdentifiableObject> klass,
+    public void check( ObjectBundle bundle, Class<? extends IdentifiableObject> klass,
         List<IdentifiableObject> persistedObjects, List<IdentifiableObject> nonPersistedObjects,
-        ImportStrategy importStrategy, ValidationContext ctx )
+        ImportStrategy importStrategy, ValidationContext ctx, Consumer<ObjectReport> addReports )
     {
-        TypeReport typeReport = new TypeReport( klass );
-
         if ( nonPersistedObjects == null || nonPersistedObjects.isEmpty() )
         {
-            return typeReport;
+            return;
         }
 
         for ( IdentifiableObject identifiableObject : nonPersistedObjects )
@@ -65,11 +64,9 @@ public class UpdateCheck
                     bundle.getPreheatIdentifier().getIdentifiersWithName( identifiableObject ) )
                         .setErrorProperty( "id" ).setMainId( identifiableObject.getUid() );
 
-                ValidationUtils.addObjectReport( errorReport, typeReport, object, bundle );
+                addReports.accept( createObjectReport( errorReport, object, bundle ) );
                 ctx.markForRemoval( object );
             }
         }
-
-        return typeReport;
     }
 }
