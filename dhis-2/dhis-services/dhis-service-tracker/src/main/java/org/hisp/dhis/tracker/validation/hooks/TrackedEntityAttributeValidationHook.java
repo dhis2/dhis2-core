2b95e0a76537ff7184e8b7b966b52801c7537610
LUCA <lucacambi77@gmail.com>
2021-05-10 10:15:06 +0200

    fix: missing uid on error for tracked entity

--- dhis-2/dhis-services/dhis-service-tracker/src/main/java/org/hisp/dhis/tracker/validation/hooks/TrackedEntityAttributeValidationHook.java
+++ dhis-2/dhis-services/dhis-service-tracker/src/main/java/org/hisp/dhis/tracker/validation/hooks/TrackedEntityAttributeValidationHook.java
@@ -160,7 +160,7 @@ public class TrackedEntityAttributeValidationHook extends AttributeValidationHoo
                         .findFirst() );
 
                 if ( optionalTea.isPresent() )
-                    addError( reporter, E1076, TrackedEntityAttribute.class.getSimpleName(), attribute );
+                    addError( reporter, E1076, TrackedEntityAttribute.class.getSimpleName(), attribute.getAttribute() );
 
                 continue;
             }
