f06137c838b36048d3bbb0219ea03e2fc3ca25ee
LUCA <lucacambi77@gmail.com>
2021-05-18 09:55:34 +0200

    fix: add media type to request

--- dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/event/TrackedEntityInstanceController.java
+++ dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/event/TrackedEntityInstanceController.java
@@ -462,6 +462,7 @@ public class TrackedEntityInstanceController
         TrackerEntityInstanceRequest trackerEntityInstanceRequest = TrackerEntityInstanceRequest.builder()
             .inputStream( inputStream )
             .importOptions( importOptions )
+            .mediaType( mediaType )
             .jobConfiguration( jobId ).build();
 
         ImportSummaries importSummaries = trackedEntityInstanceStrategyHandler
