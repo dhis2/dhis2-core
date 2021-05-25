08c21ab99ca8da0ff722019afbaa42b2aefd43a6
Lars Helge Øverland <lars@dhis2.org>
2021-05-25 15:28:56 +0200

    Update run API script

--- dhis-2/run-api.sh
+++ dhis-2/run-api.sh
@@ -4,8 +4,10 @@
 
 # Define DHIS2_HOME folder here or set it before you run this script
 # DHIS2_HOME=<directory>
+
 # Hostname or IP for DHIS2/Jetty to listen
 DHIS2_HOSTNAME=localhost
+
 # Port number for DHIS2/Jetty to listen
 DHIS2_PORT=9090
 
@@ -19,4 +21,3 @@ echo "Port: $DHIS2_PORT"
 # Compile API only and start the API server with embedded Jetty
 mvn clean install -Pdev -Pjdk11 -T 100C
 java -Djetty.host=$DHIS2_HOSTNAME -Djetty.http.port=$DHIS2_PORT -jar ./dhis-web-embedded-jetty/target/dhis-web-embedded-jetty.jar
-
