101d0adcdb6e64d0edc2cdbb2c6cf88f272b4c18
Lars Helge Øverland <lars@dhis2.org>
2021-05-26 22:56:16 +0200

    Add period handling

--- dhis-2/dhis-services/dhis-service-reporting/src/main/java/org/hisp/dhis/orgunitprofile/impl/DefaultOrgUnitProfileService.java
+++ dhis-2/dhis-services/dhis-service-reporting/src/main/java/org/hisp/dhis/orgunitprofile/impl/DefaultOrgUnitProfileService.java
@@ -89,6 +89,7 @@ public class DefaultOrgUnitProfileService
         // in the data store
 
         // Save org unit profile to data store
+        // Add a 'addOrUpdateKeyJsonValue' method to data store?
     }
 
     public OrgUnitProfile getOrgUnitProfile()
