b42644746d717c1ac3018e8acb746ac3b0926268
LUCA <lucacambi77@gmail.com>
2021-05-31 10:38:10 +0200

    fix: removes validation on generated values in old tracker

--- dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/events/trackedentity/AbstractTrackedEntityInstanceService.java
+++ dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/events/trackedentity/AbstractTrackedEntityInstanceService.java
@@ -1420,18 +1420,6 @@ public abstract class AbstractTrackedEntityInstanceService implements TrackedEnt
         }
     }
 
-    private void validateTextPatternValue( TrackedEntityAttribute attribute, String value, String oldValue,
-        Set<ImportConflict> importConflicts )
-    {
-        if ( !TextPatternValidationUtils.validateTextPatternValue( attribute.getTextPattern(), value )
-            && !reservedValueService.isReserved( attribute.getTextPattern(), value )
-            && !Objects.equals( value, oldValue ) )
-        {
-            importConflicts
-                .add( new ImportConflict( "Attribute.value", "Value does not match the attribute pattern" ) );
-        }
-    }
-
     private void checkAttributeUniquenessWithinScope( org.hisp.dhis.trackedentity.TrackedEntityInstance entityInstance,
         TrackedEntityAttribute trackedEntityAttribute, String value, OrganisationUnit organisationUnit,
         Set<ImportConflict> importConflicts )
@@ -1471,9 +1459,6 @@ public abstract class AbstractTrackedEntityInstanceService implements TrackedEnt
                 .forEach( attrVal -> fileValues.add( attrVal.getValue() ) );
         }
 
-        Map<String, TrackedEntityAttributeValue> teiAttributeValueMap = getTeiAttributeValueMap(
-            trackedEntityAttributeValueService.getTrackedEntityAttributeValues( daoEntityInstance ) );
-
         for ( Attribute attribute : dtoEntityInstance.getAttributes() )
         {
             if ( StringUtils.isNotEmpty( attribute.getValue() ) )
@@ -1497,18 +1482,6 @@ public abstract class AbstractTrackedEntityInstanceService implements TrackedEnt
                     importConflicts.add( new ImportConflict( "Attribute.value",
                         String.format( "Value exceeds the character limit of %s characters: '%s...'",
                             TEA_VALUE_MAX_LENGTH, attribute.getValue().substring( 0, 25 ) ) ) );
-                }
-
-                TrackedEntityAttributeValue trackedEntityAttributeValue = teiAttributeValueMap
-                    .get( daoEntityAttribute.getUid() );
-
-                if ( daoEntityAttribute.isGenerated() && daoEntityAttribute.getTextPattern() != null
-                    && !importOptions.isSkipPatternValidation() )
-                {
-
-                    validateTextPatternValue( daoEntityAttribute, attribute.getValue(),
-                        trackedEntityAttributeValue != null ? trackedEntityAttributeValue.getValue() : null,
-                        importConflicts );
                 }
 
                 if ( daoEntityAttribute.isUnique() )
