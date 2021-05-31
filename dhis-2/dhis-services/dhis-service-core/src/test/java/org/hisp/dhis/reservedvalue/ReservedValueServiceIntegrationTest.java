9a9b53f4f25b84e8ab97eee62f1c289f5eb3d8b6
LUCA <lucacambi77@gmail.com>
2021-05-31 10:07:09 +0200

    fix: solve conflicts

--- dhis-2/dhis-services/dhis-service-core/src/test/java/org/hisp/dhis/reservedvalue/ReservedValueServiceIntegrationTest.java
+++ dhis-2/dhis-services/dhis-service-core/src/test/java/org/hisp/dhis/reservedvalue/ReservedValueServiceIntegrationTest.java
@@ -56,8 +56,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.google.common.collect.Lists;
 
-public class DefaultReservedValueServiceTest
-    extends IntegrationTestBase
+public class ReservedValueServiceIntegrationTest extends IntegrationTestBase
 {
     @Autowired
     private ReservedValueService reservedValueService;
@@ -193,8 +192,7 @@ public class DefaultReservedValueServiceTest
 
         assertEquals( 1, res.stream()
             .filter( ( rv ) -> rv.getValue().indexOf( "TEST-" ) == 0 && rv.getValue().length() == 7 ).count() );
-        assertEquals( 1, reservedValueStore.getCount() );
-
+        assertEquals( 0, reservedValueStore.getCount() );
     }
 
     @Test
@@ -206,7 +204,7 @@ public class DefaultReservedValueServiceTest
 
         assertEquals( 50, res.stream()
             .filter( ( rv ) -> rv.getValue().indexOf( "TEST-" ) == 0 && rv.getValue().length() == 7 ).count() );
-        assertEquals( 50, reservedValueStore.getCount() );
+        assertEquals( 0, reservedValueStore.getCount() );
 
     }
 
@@ -221,7 +219,7 @@ public class DefaultReservedValueServiceTest
         assertEquals( 50,
             reserved.stream().filter( ( rv ) -> rv.getValue().indexOf( "TEST-" ) == 0 && rv.getValue().length() == 7 )
                 .count() );
-        assertEquals( 50, reservedValueStore.getCount() );
+        assertEquals( 0, reservedValueStore.getCount() );
 
         List<ReservedValue> res = reservedValueService.reserve( simpleSequentialTextPattern, 25, new HashMap<>(),
             future );
@@ -229,7 +227,7 @@ public class DefaultReservedValueServiceTest
         assertTrue( ListUtils.intersection( reserved, res ).isEmpty() );
         assertEquals( 25, res.stream()
             .filter( ( rv ) -> rv.getValue().indexOf( "TEST-" ) == 0 && rv.getValue().length() == 7 ).count() );
-        assertEquals( 75, reservedValueStore.getCount() );
+        assertEquals( 0, reservedValueStore.getCount() );
 
     }
 
