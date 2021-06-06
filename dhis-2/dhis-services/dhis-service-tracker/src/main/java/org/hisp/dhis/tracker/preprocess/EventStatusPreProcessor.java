945c689c1f4b164cf92e639ef400a78513224370
Abyot Asalefew Gizaw <abyota@gmail.com>
2021-06-06 12:05:51 +0200

    chore: Code style

--- dhis-2/dhis-services/dhis-service-tracker/src/main/java/org/hisp/dhis/tracker/preprocess/EventStatusPreProcessor.java
+++ dhis-2/dhis-services/dhis-service-tracker/src/main/java/org/hisp/dhis/tracker/preprocess/EventStatusPreProcessor.java
@@ -1,4 +1,3 @@
-package org.hisp.dhis.tracker.preprocess;
 /*
  * Copyright (c) 2004-2021, University of Oslo
  * All rights reserved.
@@ -26,6 +25,7 @@ package org.hisp.dhis.tracker.preprocess;
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+package org.hisp.dhis.tracker.preprocess;
 
 import org.hisp.dhis.event.EventStatus;
 import org.hisp.dhis.tracker.bundle.TrackerBundle;
@@ -46,7 +46,7 @@ public class EventStatusPreProcessor
     {
         for ( Event event : bundle.getEvents() )
         {
-        	if ( event.getStatus().equals( EventStatus.VISITED ) )
+            if ( event.getStatus().equals( EventStatus.VISITED ) )
             {
                 event.setStatus( EventStatus.ACTIVE );
             }
