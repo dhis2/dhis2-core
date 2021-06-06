ff59882ffb26cde15375763340b24c8b9089a457
Abyot Asalefew Gizaw <abyota@gmail.com>
2021-06-06 11:29:20 +0200

    chore: Migrate event status VISITED to ACTIVE

---
+++ dhis-2/dhis-services/dhis-service-tracker/src/test/java/org/hisp/dhis/tracker/preprocess/EventStatusPreProcessorTest.java
@@ -0,0 +1,89 @@
+package org.hisp.dhis.tracker.preprocess;
+
+/*
+ * Copyright (c) 2004-2021, University of Oslo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * Neither the name of the HISP project nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Collections;
+
+import org.hisp.dhis.event.EventStatus;
+import org.hisp.dhis.program.Program;
+import org.hisp.dhis.program.ProgramInstance;
+import org.hisp.dhis.program.ProgramStage;
+import org.hisp.dhis.tracker.TrackerIdentifier;
+import org.hisp.dhis.tracker.bundle.TrackerBundle;
+import org.hisp.dhis.tracker.domain.Event;
+import org.hisp.dhis.tracker.preheat.TrackerPreheat;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Abyot Asalefew Gizaw <abyota@gmail.com>
+ */
+public class EventStatusPreProcessorTest
+{
+    private EventStatusPreProcessor preProcessorToTest;
+
+    @Before
+    public void setUp()
+    {
+        this.preProcessorToTest = new EventStatusPreProcessor();
+    }
+
+    @Test
+    public void testVisitedStatusIsConvertedToActive()
+    {
+        // Given
+        Event event = new Event();
+        event.setStatus( EventStatus.VISITED );
+        event.setProgramStage( "programStageUid" );
+        TrackerBundle bundle = TrackerBundle.builder().events( Collections.singletonList( event ) ).build();
+
+        ProgramInstance programInstance = new ProgramInstance();
+        programInstance.setUid( "programInstanceUid" );
+
+        Program program = new Program();
+        program.setUid( "programUid" );
+        ProgramStage programStage = new ProgramStage();
+        programStage.setUid( "programStageUid" );
+        programStage.setProgram( program );
+
+        TrackerPreheat preheat = new TrackerPreheat();
+        preheat.putProgramInstancesWithoutRegistration( "programUid", programInstance );
+
+        preheat.put( TrackerIdentifier.UID, programStage );
+        bundle.setPreheat( preheat );
+
+        // When
+        preProcessorToTest.process( bundle );
+
+        // Then
+        assertEquals( EventStatus.ACTIVE, bundle.getEvents().get( 0 ).getStatus() );
+    }
+}
