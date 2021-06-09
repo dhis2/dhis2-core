9a590729949c11475cc3b025bf3d203f2a1c8ee7
Morten Hansen <morten@winterop.com>
2021-06-09 13:19:01 +0700

    code format

--- dhis-2/dhis-support/dhis-support-commons/src/test/java/org/hisp/dhis/commons/jackson/serialization/DataSetPeriodTypeSerializationTest.java
+++ dhis-2/dhis-support/dhis-support-commons/src/test/java/org/hisp/dhis/commons/jackson/serialization/DataSetPeriodTypeSerializationTest.java
@@ -25,7 +25,6 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
 package org.hisp.dhis.commons.jackson.serialization;
 
 import static org.junit.Assert.assertEquals;
