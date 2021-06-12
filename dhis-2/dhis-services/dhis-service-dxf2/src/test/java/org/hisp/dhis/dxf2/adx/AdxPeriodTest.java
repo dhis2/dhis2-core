21d3b676b261ebee98fe5c257e4bd6def3e02d53
Jim Grace <jimgrace@users.noreply.github.com>
2021-06-12 12:53:49 -0400

    fix: Standardize ADX export parameters [DHIS2-9313] (#8204)
    * fix: Standardize ADX export parameters [DHIS2-9313]
    
    (cherry picked from commit 0f3bc310d34283f3afbd5ea904d7feac6f795659)
    
    * fix: SonarCloud objected to assertContainsOnly
    
    (cherry picked from commit fbe529e5d257b8e685f864e147573cd058e9b5d4)

--- dhis-2/dhis-services/dhis-service-dxf2/src/test/java/org/hisp/dhis/dxf2/adx/AdxPeriodTest.java
+++ dhis-2/dhis-services/dhis-service-dxf2/src/test/java/org/hisp/dhis/dxf2/adx/AdxPeriodTest.java
@@ -27,114 +27,191 @@
  */
 package org.hisp.dhis.dxf2.adx;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
-import org.hisp.dhis.period.Period;
 import org.hisp.dhis.period.PeriodType;
 import org.junit.Test;
 
 /**
  *
  * @author bobj
+ * @author Jim Grace
  */
 public class AdxPeriodTest
 {
-    private Period period;
-
-    public AdxPeriodTest()
+    @Test
+    public void testParse()
     {
+        // P1D - Daily
+
+        assertEquals( "20240101", parse( "2024-01-01/P1D" ) );
+        assertEquals( "20240506", parse( "2024-05-06/P1D" ) );
+
+        // P7D - Weekly
+
+        assertEquals( "2024W1", parse( "2024-01-01/P7D" ) );
+        assertEquals( "2024W2", parse( "2024-01-08/P7D" ) );
+        assertEquals( "2024WedW1", parse( "2024-01-03/P7D" ) );
+        assertEquals( "2024ThuW1", parse( "2024-01-04/P7D" ) );
+        assertEquals( "2024SatW2", parse( "2024-01-06/P7D" ) );
+        assertEquals( "2024SunW2", parse( "2024-01-07/P7D" ) );
+
+        // P14D - BiWeekly
+
+        assertEquals( "2024BiW1", parse( "2024-01-01/P14D" ) );
+        assertEquals( "2024BiW2", parse( "2024-01-15/P14D" ) );
+
+        // P1M - Monthly
+
+        assertEquals( "202401", parse( "2024-01-01/P1M" ) );
+        assertEquals( "202405", parse( "2024-05-01/P1M" ) );
+
+        // P2M - BiMonthly
+
+        assertEquals( "202401B", parse( "2024-01-01/P2M" ) );
+        assertEquals( "202401B", parse( "2024-01-01/P2M" ) );
+
+        // P3M - Quarterly
+
+        assertEquals( "2024Q1", parse( "2024-01-01/P3M" ) );
+        assertEquals( "2024Q2", parse( "2024-04-01/P3M" ) );
+
+        // P6M - SixMonthly
+
+        assertEquals( "2024S1", parse( "2024-01-01/P6M" ) );
+        assertEquals( "2024S2", parse( "2024-07-01/P6M" ) );
+        assertEquals( "2024AprilS1", parse( "2024-04-01/P6M" ) );
+        assertEquals( "2024AprilS2", parse( "2024-10-01/P6M" ) );
+        assertEquals( "2025NovS1", parse( "2024-11-01/P6M" ) );
+        assertEquals( "2025NovS2", parse( "2025-05-01/P6M" ) );
+
+        // P1Y - Yearly
+
+        assertEquals( "2024", parse( "2024-01-01/P1Y" ) );
+        assertEquals( "2024April", parse( "2024-04-01/P1Y" ) );
+        assertEquals( "2024July", parse( "2024-07-01/P1Y" ) );
+        assertEquals( "2024Oct", parse( "2024-10-01/P1Y" ) );
+        assertEquals( "2025Nov", parse( "2024-11-01/P1Y" ) );
     }
 
     @Test
-    public void testParser()
+    public void testParseBadFormat()
     {
-        try
-        {
-            period = AdxPeriod.parse( "2015-01-01/P1Y" );
-            assertEquals( "2015", period.getIsoDate() );
-            period = AdxPeriod.parse( "2015-01-01/P1M" );
-            assertEquals( "201501", period.getIsoDate() );
-            period = AdxPeriod.parse( "2015-01-01/P1D" );
-            assertEquals( "20150101", period.getIsoDate() );
-            period = AdxPeriod.parse( "2015-01-01/P3M" );
-            assertEquals( "2015Q1", period.getIsoDate() );
-            period = AdxPeriod.parse( "2015-04-01/P3M" );
-            assertEquals( "2015Q2", period.getIsoDate() );
-            period = AdxPeriod.parse( "2015-01-01/P7D" );
-            assertEquals( "2015W1", period.getIsoDate() );
-            period = AdxPeriod.parse( "2015-01-05/P7D" );
-            assertEquals( "2015W2", period.getIsoDate() );
-        }
-        catch ( AdxException ex )
-        {
-            fail( ex.getMessage() );
-        }
+        failToParse( "2024-01-01/P1D/P1D" );
     }
 
     @Test
-    public void testBadDuration()
+    public void testParseBadDuration()
     {
-        try
-        {
-            period = AdxPeriod.parse( "2014-01-01/P1" );
-            fail( "Should have thrown exception parsing 2015-01-01/P1" );
-        }
-        catch ( Exception ex )
-        {
-            assertEquals( AdxException.class, ex.getClass() );
-        }
+        failToParse( "2024-01-01/P1" );
     }
 
     @Test
-    public void testFinancialTypes()
+    public void testParseBadWeekly()
     {
-        try
-        {
-            period = AdxPeriod.parse( "2015-01-01/P1Y" );
-            assertEquals( "2015", period.getIsoDate() );
-            period = AdxPeriod.parse( "2015-04-01/P1Y" );
-            assertEquals( "2015April", period.getIsoDate() );
-            period = AdxPeriod.parse( "2015-07-01/P1Y" );
-            assertEquals( "2015July", period.getIsoDate() );
-            period = AdxPeriod.parse( "2015-10-01/P1Y" );
-            assertEquals( "2015Oct", period.getIsoDate() );
-        }
-        catch ( AdxException ex )
-        {
-            fail( ex.getMessage() );
-        }
+        failToParse( "2024-01-02/P7D" );
     }
 
     @Test
-    public void testFailFinancialType()
+    public void testParseBadSixMonthly()
     {
-        try
-        {
-            period = AdxPeriod.parse( "2014-02-01/P1Y" );
-            fail( "Should have thrown exception parsing 2014-02-01/P1Y" );
-        }
-        catch ( Exception ex )
-        {
-            assertEquals( AdxException.class, ex.getClass() );
-        }
+        failToParse( "2014-02-01/P6M" );
+    }
+
+    @Test
+    public void testParseBadYearly()
+    {
+        failToParse( "2014-02-01/P1Y" );
     }
 
     @Test
     public void testSerialize()
     {
-        period = PeriodType.getPeriodFromIsoString( "2015" );
-        assertEquals( "2015-01-01/P1Y", AdxPeriod.serialize( period ) );
-        period = PeriodType.getPeriodFromIsoString( "201503" );
-        assertEquals( "2015-03-01/P1M", AdxPeriod.serialize( period ) );
-        period = PeriodType.getPeriodFromIsoString( "2015W1" );
-        assertEquals( "2014-12-29/P7D", AdxPeriod.serialize( period ) );
-        period = PeriodType.getPeriodFromIsoString( "2015Q2" );
-        assertEquals( "2015-04-01/P3M", AdxPeriod.serialize( period ) );
-        period = PeriodType.getPeriodFromIsoString( "2015April" );
-        assertEquals( "2015-04-01/P1Y", AdxPeriod.serialize( period ) );
-        period = PeriodType.getPeriodFromIsoString( "2015S2" );
-        assertEquals( "2015-07-01/P6M", AdxPeriod.serialize( period ) );
-        period = PeriodType.getPeriodFromIsoString( "2015AprilS2" );
-        assertEquals( "2015-10-01/P6M", AdxPeriod.serialize( period ) );
+        // Daily - P1D
+
+        assertEquals( "2022-01-01/P1D", serialize( "20220101" ) );
+        assertEquals( "2022-01-07/P1D", serialize( "20220107" ) );
+
+        // Weekly - P7D
+
+        assertEquals( "2022-01-03/P7D", serialize( "2022W1" ) );
+        assertEquals( "2022-01-10/P7D", serialize( "2022W2" ) );
+        assertEquals( "2021-12-29/P7D", serialize( "2022WedW1" ) );
+        assertEquals( "2021-12-30/P7D", serialize( "2022ThuW1" ) );
+        assertEquals( "2022-01-01/P7D", serialize( "2022SatW1" ) );
+        assertEquals( "2022-01-02/P7D", serialize( "2022SunW1" ) );
+
+        // BiWeekly - P14D
+
+        assertEquals( "2022-01-03/P14D", serialize( "2022BiW1" ) );
+        assertEquals( "2022-01-17/P14D", serialize( "2022BiW2" ) );
+
+        // Monthly - P1M
+
+        assertEquals( "2022-01-01/P1M", serialize( "202201" ) );
+        assertEquals( "2022-05-01/P1M", serialize( "202205" ) );
+
+        // BiMonthly - P2M
+
+        assertEquals( "2022-01-01/P2M", serialize( "202201B" ) );
+        assertEquals( "2022-03-01/P2M", serialize( "202202B" ) );
+
+        // Quarterly - P3M
+
+        assertEquals( "2022-01-01/P3M", serialize( "2022Q1" ) );
+        assertEquals( "2022-04-01/P3M", serialize( "2022Q2" ) );
+
+        // SixMonthly - P6M
+
+        assertEquals( "2022-01-01/P6M", serialize( "2022S1" ) );
+        assertEquals( "2022-07-01/P6M", serialize( "2022S2" ) );
+        assertEquals( "2022-04-01/P6M", serialize( "2022AprilS1" ) );
+        assertEquals( "2022-10-01/P6M", serialize( "2022AprilS2" ) );
+        assertEquals( "2022-05-01/P6M", serialize( "2022NovS2" ) );
+
+        // Yearly - P1Y
+
+        assertEquals( "2022-01-01/P1Y", serialize( "2022" ) );
+        assertEquals( "2022-04-01/P1Y", serialize( "2022April" ) );
+        assertEquals( "2022-07-01/P1Y", serialize( "2022July" ) );
+        assertEquals( "2022-10-01/P1Y", serialize( "2022Oct" ) );
+        assertEquals( "2021-11-01/P1Y", serialize( "2022Nov" ) );
+    }
+
+    // -------------------------------------------------------------------------
+    // Supportive methods
+    // -------------------------------------------------------------------------
+
+    private String parse( String adxPeriod )
+    {
+        try
+        {
+            return AdxPeriod.parse( adxPeriod ).getIsoDate();
+        }
+        catch ( AdxException ex )
+        {
+            fail( ex.getMessage() );
+        }
+
+        return null;
+    }
+
+    private void failToParse( String adxPeriod )
+    {
+        try
+        {
+            AdxPeriod.parse( adxPeriod ).getIsoDate();
+            fail( "Should have thrown exception parsing " + adxPeriod );
+        }
+        catch ( Exception ex )
+        {
+            assertEquals( AdxException.class, ex.getClass() );
+        }
+    }
+
+    private String serialize( String period )
+    {
+        return AdxPeriod.serialize( PeriodType.getPeriodFromIsoString( period ) );
     }
 }
