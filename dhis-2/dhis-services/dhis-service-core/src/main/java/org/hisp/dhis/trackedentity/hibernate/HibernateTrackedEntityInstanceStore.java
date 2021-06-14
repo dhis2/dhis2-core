086ee4efba05f4bf516ae50e20c9b443a6b1aaf3
Mohamed Ameen <ameen@dhis2.org>
2021-06-14 08:13:18 +0200

    fix: Minor syntax error

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/trackedentity/hibernate/HibernateTrackedEntityInstanceStore.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/trackedentity/hibernate/HibernateTrackedEntityInstanceStore.java
@@ -931,7 +931,8 @@ public class HibernateTrackedEntityInstanceStore
                     .append( SPACE )
                     .append( filter.getSqlOperator() )
                     .append( SPACE )
-                    .append( StringUtils.lowerCase( filter.getSqlFilter( filter.getFilter() ) ) );
+                    .append( StringUtils
+                        .lowerCase( filter.getSqlFilter( statementBuilder.encode( filter.getFilter(), false ) ) ) );
             }
         }
     }
