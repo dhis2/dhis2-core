23c48caf395bfc68a8a9426928bbc1b4d84511fd
maikelarabori <maikelarabori@gmail.com>
2021-06-24 14:08:15 +0200

    chore: Adding unit test for outputSchema [DHIS2-11181]

---
+++ dhis-2/dhis-services/dhis-service-analytics/src/test/java/org/hisp/dhis/analytics/event/data/DefaultEventAnalyticsServiceTest.java
@@ -0,0 +1,144 @@
+/*
+ * Copyright (c) 2004-2021, University of Oslo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * Neither the name of the HISP project nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.hisp.dhis.analytics.event.data;
+
+import static org.hisp.dhis.DhisConvenienceTest.createOrganisationUnit;
+import static org.hisp.dhis.DhisConvenienceTest.createPeriod;
+import static org.hisp.dhis.DhisConvenienceTest.createProgram;
+import static org.hisp.dhis.common.DimensionalObjectUtils.getList;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.atMost;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.hisp.dhis.analytics.AnalyticsSecurityManager;
+import org.hisp.dhis.analytics.Partitions;
+import org.hisp.dhis.analytics.cache.AnalyticsCache;
+import org.hisp.dhis.analytics.data.handler.SchemaIdResponseMapper;
+import org.hisp.dhis.analytics.event.EnrollmentAnalyticsManager;
+import org.hisp.dhis.analytics.event.EventAnalyticsManager;
+import org.hisp.dhis.analytics.event.EventDataQueryService;
+import org.hisp.dhis.analytics.event.EventQueryParams;
+import org.hisp.dhis.analytics.event.EventQueryPlanner;
+import org.hisp.dhis.analytics.event.EventQueryValidator;
+import org.hisp.dhis.common.IdScheme;
+import org.hisp.dhis.dataelement.DataElementService;
+import org.hisp.dhis.organisationunit.OrganisationUnit;
+import org.hisp.dhis.program.Program;
+import org.hisp.dhis.system.database.DatabaseInfo;
+import org.hisp.dhis.trackedentity.TrackedEntityAttributeService;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+import com.google.common.collect.Sets;
+
+/**
+ * Unit tests for DefaultEventAnalyticsService.
+ *
+ * @author maikel arabori
+ */
+public class DefaultEventAnalyticsServiceTest
+{
+    private DefaultEventAnalyticsService defaultEventAnalyticsService;
+
+    @Mock
+    private AnalyticsSecurityManager securityManager;
+
+    @Mock
+    private EventQueryValidator eventQueryValidator;
+
+    @Mock
+    private DataElementService dataElementService;
+
+    @Mock
+    private TrackedEntityAttributeService trackedEntityAttributeService;
+
+    @Mock
+    private EventAnalyticsManager eventAnalyticsManager;
+
+    @Mock
+    private EnrollmentAnalyticsManager enrollmentAnalyticsManager;
+
+    @Mock
+    private EventDataQueryService eventDataQueryService;
+
+    @Mock
+    private EventQueryPlanner queryPlanner;
+
+    @Mock
+    private DatabaseInfo databaseInfo;
+
+    @Mock
+    private AnalyticsCache analyticsCache;
+
+    @Mock
+    private SchemaIdResponseMapper schemaIdResponseMapper;
+
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+    @Before
+    public void setUp()
+    {
+        defaultEventAnalyticsService = new DefaultEventAnalyticsService( dataElementService,
+            trackedEntityAttributeService, eventAnalyticsManager, eventDataQueryService, securityManager, queryPlanner,
+            eventQueryValidator, databaseInfo, analyticsCache, enrollmentAnalyticsManager, schemaIdResponseMapper );
+    }
+
+    @Test
+    public void testOutputSchemeInvocation()
+    {
+        // Given mock variables
+        final OrganisationUnit mockOrgUnit = createOrganisationUnit( 'A' );
+        final Program mockProgram = createProgram( 'A', null, null, Sets.newHashSet( mockOrgUnit ), null );
+        final EventQueryParams mockParams = new EventQueryParams.Builder()
+            .withPeriods( getList( createPeriod( "2000Q1" ) ), "monthly" )
+            .withPartitions( new Partitions() )
+            .withOrganisationUnits( getList( mockOrgUnit ) )
+            .withProgram( mockProgram )
+            .withOutputIdScheme( IdScheme.CODE )
+            .build();
+
+        // Given mock calls
+        doNothing().when( securityManager ).decideAccessEventQuery( mockParams );
+        when( securityManager.withUserConstraints( mockParams ) ).thenReturn( mockParams );
+        doNothing().when( eventQueryValidator ).validate( mockParams );
+        when( queryPlanner.planEventQuery( any( EventQueryParams.class ) ) ).thenReturn( mockParams );
+
+        // When
+        defaultEventAnalyticsService.getEvents( mockParams );
+
+        // Then
+        verify( schemaIdResponseMapper, atMost( 1 ) ).getSchemeIdResponseMap( mockParams );
+    }
+}
