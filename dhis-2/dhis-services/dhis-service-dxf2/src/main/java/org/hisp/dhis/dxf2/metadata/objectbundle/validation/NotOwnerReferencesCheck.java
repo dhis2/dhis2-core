619b29caf0f2a8de113424bd9e3c9264414d7f0a
Morten Hansen <morten@winterop.com>
2021-07-12 12:32:17 +0700

    refactor: simplify complexity of checkReferences

--- dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/metadata/objectbundle/validation/NotOwnerReferencesCheck.java
+++ dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/metadata/objectbundle/validation/NotOwnerReferencesCheck.java
@@ -45,6 +45,7 @@ import org.hisp.dhis.hibernate.HibernateProxyUtils;
 import org.hisp.dhis.importexport.ImportStrategy;
 import org.hisp.dhis.preheat.PreheatErrorReport;
 import org.hisp.dhis.preheat.PreheatIdentifier;
+import org.hisp.dhis.schema.Property;
 import org.hisp.dhis.schema.PropertyType;
 import org.hisp.dhis.schema.Schema;
 import org.hisp.dhis.system.util.ReflectionUtils;
@@ -104,38 +105,49 @@ public class NotOwnerReferencesCheck implements ValidationCheck
             .forEach( p -> {
                 if ( !p.isCollection() )
                 {
-                    IdentifiableObject refObject = ReflectionUtils.invokeMethod( object, p.getGetterMethod() );
-
-                    if ( refObject != null )
-                    {
-                        preheatErrorReports.add( new PreheatErrorReport( identifier, object.getClass(),
-                            ErrorCode.E5006, identifier.getIdentifiersWithName( refObject ),
-                            identifier.getIdentifiersWithName( object ), p.getName() ) );
-                    }
+                    checkReference( object, identifier, preheatErrorReports, p );
                 }
                 else
                 {
-                    Collection<IdentifiableObject> refObjects = ReflectionUtils.invokeMethod( object,
-                        p.getGetterMethod() );
-
-                    if ( refObjects == null )
-                    {
-                        return;
-                    }
-
-                    for ( IdentifiableObject refObject : refObjects )
-                    {
-                        if ( refObject != null )
-                        {
-                            preheatErrorReports.add( new PreheatErrorReport( identifier, object.getClass(),
-                                ErrorCode.E5006, identifier.getIdentifiersWithName( refObject ),
-                                identifier.getIdentifiersWithName( object ), p.getCollectionName() ) );
-                        }
-                    }
-
+                    checkCollection( object, identifier, preheatErrorReports, p );
                 }
             } );
 
         return preheatErrorReports;
+    }
+
+    private void checkReference( IdentifiableObject object, PreheatIdentifier identifier,
+        List<PreheatErrorReport> preheatErrorReports, Property p )
+    {
+        IdentifiableObject refObject = ReflectionUtils.invokeMethod( object, p.getGetterMethod() );
+
+        if ( refObject != null )
+        {
+            preheatErrorReports.add( new PreheatErrorReport( identifier, object.getClass(),
+                ErrorCode.E5006, identifier.getIdentifiersWithName( refObject ),
+                identifier.getIdentifiersWithName( object ), p.getName() ) );
+        }
+    }
+
+    private void checkCollection( IdentifiableObject object, PreheatIdentifier identifier,
+        List<PreheatErrorReport> preheatErrorReports, Property p )
+    {
+        Collection<IdentifiableObject> refObjects = ReflectionUtils.invokeMethod( object,
+            p.getGetterMethod() );
+
+        if ( refObjects == null )
+        {
+            return;
+        }
+
+        for ( IdentifiableObject refObject : refObjects )
+        {
+            if ( refObject != null )
+            {
+                preheatErrorReports.add( new PreheatErrorReport( identifier, object.getClass(),
+                    ErrorCode.E5006, identifier.getIdentifiersWithName( refObject ),
+                    identifier.getIdentifiersWithName( object ), p.getCollectionName() ) );
+            }
+        }
     }
 }
