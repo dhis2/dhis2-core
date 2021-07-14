293441f1546e3b7f82a11e3a972c09c9d0227ddf
Dusan Bernat <mail@it-bernat.de>
2021-07-14 08:57:11 +0200

    feat: Category option in indicators [DHIS2-716-2.37] - third version (#8393)
    * post code review - version 3
    
    * post code review - version 3 - clean coding
    
    * clean coding (code smells)
    
    * post code review + clean coding
    
    * post code review + clean coding 2.0
    
    * post code review + clean coding 2.0
    
    * unit tests
    
    * unit tests
    
    * unit tests  -  clean coding
    
    * antlr and rule engine

--- dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/indicator/IndicatorController.java
+++ dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/indicator/IndicatorController.java
@@ -34,6 +34,7 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletResponse;
 
 import org.hisp.dhis.analytics.resolver.ExpressionResolver;
+import org.hisp.dhis.analytics.resolver.ExpressionResolverCollection;
 import org.hisp.dhis.dxf2.webmessage.DescriptiveWebMessage;
 import org.hisp.dhis.expression.ExpressionService;
 import org.hisp.dhis.expression.ExpressionValidationOutcome;
@@ -62,7 +63,7 @@ public class IndicatorController
     private ExpressionService expressionService;
 
     @Autowired
-    private ExpressionResolver resolver;
+    private ExpressionResolverCollection resolvers;
 
     @Autowired
     private I18nManager i18nManager;
@@ -73,7 +74,13 @@ public class IndicatorController
     {
         I18n i18n = i18nManager.getI18n();
 
-        String resolvedExpression = resolver.resolve( expression );
+        String resolvedExpression = expression;
+
+        for ( ExpressionResolver resolver : resolvers.getExpressionResolvers() )
+        {
+            resolvedExpression = resolver.resolve( resolvedExpression );
+        }
+
         ExpressionValidationOutcome result = expressionService.expressionIsValid( resolvedExpression,
             INDICATOR_EXPRESSION );
 
