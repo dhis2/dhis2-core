a61cd9a2e43fe3a8cdf7f8161442da49f2dd0c2d
zubaira <zubair@dhis2.org>
2021-07-22 07:35:20 +0200

    fix: (2.36) NPE when enrollment does not exist [DHIS2-11409]

--- dhis-2/dhis-services/dhis-service-node/src/main/java/org/hisp/dhis/fieldfilter/DefaultFieldFilterService.java
+++ dhis-2/dhis-services/dhis-service-node/src/main/java/org/hisp/dhis/fieldfilter/DefaultFieldFilterService.java
@@ -220,7 +220,7 @@ public class DefaultFieldFilterService implements FieldFilterService
                 .collect( Collectors.joining( "," ) );
         }
 
-        if ( params.getObjects().isEmpty() )
+        if ( params.getObjects().isEmpty() || objects.stream().noneMatch( Objects::nonNull ) )
         {
             return collectionNode;
         }
