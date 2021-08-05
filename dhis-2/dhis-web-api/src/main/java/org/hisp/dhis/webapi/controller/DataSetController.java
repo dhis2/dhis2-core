758128d461c79335dc41e90c3501be935749bdf4
Jan Bernitt <jaanbernitt@gmail.com>
2021-08-05 13:39:23 +0200

    refactor: replace RenderService.toJson with return + jackson in DataSetController [TECH-656]

--- dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/DataSetController.java
+++ dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/DataSetController.java
@@ -27,6 +27,7 @@
  */
 package org.hisp.dhis.webapi.controller;
 
+import static java.util.Collections.singletonMap;
 import static org.hisp.dhis.dxf2.webmessage.WebMessageUtils.badRequest;
 import static org.hisp.dhis.dxf2.webmessage.WebMessageUtils.conflict;
 import static org.hisp.dhis.dxf2.webmessage.WebMessageUtils.notFound;
@@ -39,7 +40,6 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -178,7 +178,10 @@ public class DataSetController
     }
 
     @GetMapping( "/{uid}/version" )
-    public void getVersion( @PathVariable( "uid" ) String uid, @RequestParam Map<String, String> parameters,
+    @ResponseBody
+    @ResponseStatus( HttpStatus.OK )
+    public Map<String, Integer> getVersion( @PathVariable( "uid" ) String uid,
+        @RequestParam Map<String, String> parameters,
         HttpServletResponse response )
         throws Exception
     {
@@ -189,10 +192,7 @@ public class DataSetController
             throw new WebMessageException( conflict( "Data set does not exist: " + uid ) );
         }
 
-        Map<String, Integer> versionMap = new HashMap<>();
-        versionMap.put( "version", dataSet.getVersion() );
-
-        renderService.toJson( response.getOutputStream(), versionMap );
+        return singletonMap( "version", dataSet.getVersion() );
     }
 
     @PostMapping( "/{uid}/version" )
@@ -266,7 +266,9 @@ public class DataSetController
     }
 
     @GetMapping( value = "/{uid}/form", produces = APPLICATION_JSON_VALUE )
-    public void getFormJson(
+    @ResponseBody
+    @ResponseStatus( HttpStatus.OK )
+    public Form getFormJson(
         @PathVariable( "uid" ) String uid,
         @RequestParam( value = "ou", required = false ) String orgUnit,
         @RequestParam( value = "pe", required = false ) String period,
@@ -293,9 +295,7 @@ public class DataSetController
 
         Period pe = PeriodType.getPeriodFromIsoString( period );
 
-        Form form = getForm( dataSets, ou, pe, categoryOptions, metaData );
-
-        renderService.toJson( response.getOutputStream(), form );
+        return getForm( dataSets, ou, pe, categoryOptions, metaData );
     }
 
     @GetMapping( value = "/{uid}/form", produces = { APPLICATION_XML_VALUE, TEXT_XML_VALUE } )
