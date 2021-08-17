c63abe163908789784e4800f951a879e5270e557
Jan Bernitt <jaanbernitt@gmail.com>
2021-08-17 14:14:23 +0200

    chore: context switch...

---
+++ dhis-2/dhis-web-api-test/src/test/java/org/hisp/dhis/webapi/controller/SynchronizationControllerTest.java
@@ -0,0 +1,18 @@
+package org.hisp.dhis.webapi.controller;
+
+import org.hisp.dhis.webapi.DhisControllerConvenienceTest;
+import org.junit.Test;
+
+/**
+ * Tests the {@link SynchronizationController} using (mocked) REST requests.
+ *
+ * @author Jan Bernitt
+ */
+public class SynchronizationControllerTest extends DhisControllerConvenienceTest
+{
+    @Test
+    public void testExecute()
+    {
+
+    }
+}
