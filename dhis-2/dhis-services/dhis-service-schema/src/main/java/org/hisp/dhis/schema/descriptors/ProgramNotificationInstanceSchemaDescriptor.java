ab4e4c13bfdb05bf92cf186214696636d4684b15
Zubair Asghar <zubair@dhis2.org>
2021-08-19 15:54:11 +0500

    refactor: Add separate controller for ProgramNotificationInstance (#8457)
    * refactor: Add separate controller for ProgramNotificationInstance
    
    * apply code style
    
    * minor
    
    * add parameter validation
    
    * add repository logic
    
    * fix sonar code review
    
    * add tests for ProgramNotificationInstanceService
    
    * fix integration tests
    
    * remove unused code
    
    * Add paging logic
    
    * Add PagingWrapper instaed of legacy pager object
    
    * fix integration tests
    
    * test fixes
    
    * minor
    
    * remove unused code
    
    * apply code style

--- dhis-2/dhis-services/dhis-service-schema/src/main/java/org/hisp/dhis/schema/descriptors/ProgramNotificationInstanceSchemaDescriptor.java
+++ dhis-2/dhis-services/dhis-service-schema/src/main/java/org/hisp/dhis/schema/descriptors/ProgramNotificationInstanceSchemaDescriptor.java
@@ -25,50 +25,39 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package org.hisp.dhis.program.notification.template.snapshot;
+package org.hisp.dhis.schema.descriptors;
 
-import java.util.Set;
+import org.hisp.dhis.program.notification.ProgramNotificationInstance;
+import org.hisp.dhis.schema.Schema;
+import org.hisp.dhis.schema.SchemaDescriptor;
+import org.hisp.dhis.security.Authority;
+import org.hisp.dhis.security.AuthorityType;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import com.google.common.collect.Lists;
 
-import org.hisp.dhis.common.DeliveryChannel;
-import org.hisp.dhis.program.notification.NotificationTrigger;
-import org.hisp.dhis.program.notification.ProgramNotificationRecipient;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-@Data
-@EqualsAndHashCode( callSuper = true )
-public class ProgramNotificationTemplateSnapshot extends IdentifiableObjectSnapshot
+/**
+ * @author Zubair Asghar
+ */
+public class ProgramNotificationInstanceSchemaDescriptor
+    implements SchemaDescriptor
 {
+    public static final String SINGULAR = "programNotificationInstance";
 
-    @JsonProperty
-    private NotificationTrigger notificationTrigger;
+    public static final String PLURAL = "programNotificationInstances";
 
-    @JsonProperty
-    private ProgramNotificationRecipient notificationRecipient;
+    public static final String API_ENDPOINT = "/" + PLURAL;
 
-    private IdentifiableObjectSnapshot recipientDataElement;
+    @Override
+    public Schema getSchema()
+    {
+        Schema schema = new Schema( ProgramNotificationInstance.class, SINGULAR, PLURAL );
+        schema.setRelativeApiEndpoint( API_ENDPOINT );
+        schema.setOrder( 1508 );
 
-    private IdentifiableObjectSnapshot recipientProgramAttribute;
+        schema.add( new Authority( AuthorityType.CREATE,
+            Lists.newArrayList( "F_PROGRAM_PUBLIC_ADD", "F_PROGRAM_PRIVATE_ADD" ) ) );
+        schema.add( new Authority( AuthorityType.DELETE, Lists.newArrayList( "F_PROGRAM_DELETE" ) ) );
 
-    private UserGroupSnapshot recipientUserGroup;
-
-    @JsonProperty
-    private String subjectTemplate;
-
-    @JsonProperty
-    private String messageTemplate;
-
-    @JsonProperty
-    private Set<DeliveryChannel> deliveryChannels;
-
-    private Boolean notifyUsersInHierarchyOnly;
-
-    private Boolean notifyParentOrganisationUnitOnly;
-
-    @JsonProperty
-    private boolean sendRepeatable;
-
+        return schema;
+    }
 }
