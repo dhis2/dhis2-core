54628e048f672db561c8c5659a37b61829cc1ffc
Morten Hansen <morten@winterop.com>
2021-08-23 14:44:16 +0200

    test: tests for FieldFilterParams

--- dhis-2/dhis-support/dhis-support-commons/src/test/java/org/hisp/dhis/commons/jackson/filter/FieldFilterParserTest.java
+++ dhis-2/dhis-support/dhis-support-commons/src/test/java/org/hisp/dhis/commons/jackson/filter/FieldFilterParserTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2004-2021, University of Oslo
+ * Copyright (c) 2004-2004-2021, University of Oslo
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -25,14 +25,13 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package org.hisp.dhis.commons.jackson.config;
+package org.hisp.dhis.commons.jackson.filter;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Set;
 
-import org.hisp.dhis.commons.jackson.filter.FieldFilterParser;
 import org.junit.Test;
 
 import com.google.common.collect.Sets;
