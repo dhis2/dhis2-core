8d6e22df177ba51d4d5950328f06f3457a6d5fde
maikelarabori <maikelarabori@gmail.com>
2021-08-25 18:26:39 +0200

    fix: Add default column value and prevent nulls [DHIS2-11057]

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.37/V2_37_26__Add_Fix_Columns_in_Visualization.sql
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.37/V2_37_26__Add_Fix_Columns_in_Visualization.sql
@@ -3,7 +3,9 @@
 -- Add new column "fixColumnHeaders"
 ALTER TABLE visualization ADD COLUMN IF NOT EXISTS fixColumnHeaders BOOLEAN;
 UPDATE visualization SET fixColumnHeaders = FALSE;
+ALTER TABLE visualization ALTER COLUMN fixColumnHeaders SET NOT NULL;
 
 -- Add new column "fixRowHeaders"
 ALTER TABLE visualization ADD COLUMN IF NOT EXISTS fixRowHeaders BOOLEAN;
 UPDATE visualization SET fixRowHeaders = FALSE;
+ALTER TABLE visualization ALTER COLUMN fixRowHeaders SET NOT NULL;
