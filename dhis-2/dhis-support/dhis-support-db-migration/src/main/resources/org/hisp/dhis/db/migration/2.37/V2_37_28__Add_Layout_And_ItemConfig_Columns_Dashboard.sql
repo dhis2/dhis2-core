e1e41a8302e828bde9a4b7b3922c5d6adebad062
Lars Helge Øverland <lars@dhis2.org>
2021-08-25 16:22:55 +0200

    Update V2_37_28__Add_Layout_And_ItemConfig_Columns_Dashboard.sql

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.37/V2_37_28__Add_Layout_And_ItemConfig_Columns_Dashboard.sql
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.37/V2_37_28__Add_Layout_And_ItemConfig_Columns_Dashboard.sql
@@ -1,6 +1,4 @@
--- This script is responsible for adding new jsonb columns into dashboard table.
--- See Feature DHIS2-11607.
+-- Add new JSONB columns "layout" and "itemconfig" to table dashboard
 
--- Add new JSONB columns("layout" and "itemconfig") into dashboard table;
 alter table dashboard add column if not exists "layout" jsonb;
 alter table dashboard add column if not exists "itemconfig" jsonb;
