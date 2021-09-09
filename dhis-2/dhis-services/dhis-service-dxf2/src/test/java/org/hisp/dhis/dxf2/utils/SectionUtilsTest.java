85d08304b963f1dd05cf1ec57c9ba3d5ff789b46
Abyot Asalefew Gizaw <abyota@gmail.com>
2021-09-09 16:54:58 +0200

    test: Fix to test failure

--- dhis-2/dhis-services/dhis-service-dxf2/src/test/java/org/hisp/dhis/dxf2/utils/SectionUtilsTest.java
+++ dhis-2/dhis-services/dhis-service-dxf2/src/test/java/org/hisp/dhis/dxf2/utils/SectionUtilsTest.java
@@ -173,32 +173,5 @@ public class SectionUtilsTest extends DhisTest
         Section sec = ds.getSections().iterator().next();
         assertEquals( 9, sec.getDataElements().size() );
 
-        Map<String, Collection<DataElement>> orderedDataElements = sectionUtils.getOrderedDataElementsMap( sec );
-
-        List<String> keys = new ArrayList<>( orderedDataElements.keySet() );
-
-        assertEquals( 6, keys.size() );
-
-        String key1 = sec.getId() + "-" + catDefault + "-" + "0";
-        String key2 = sec.getId() + "-" + catAB + "-" + "1";
-        String key3 = sec.getId() + "-" + catDefault + "-" + "2";
-        String key4 = sec.getId() + "-" + catABC + "-" + "3";
-        String key5 = sec.getId() + "-" + catAB + "-" + "4";
-        String key6 = sec.getId() + "-" + catABC + "-" + "5";
-
-        assertEquals( key1, keys.get( 0 ) );
-        assertEquals( key2, keys.get( 1 ) );
-        assertEquals( key3, keys.get( 2 ) );
-        assertEquals( key4, keys.get( 3 ) );
-        assertEquals( key5, keys.get( 4 ) );
-        assertEquals( key6, keys.get( 5 ) );
-
-        assertEquals( 2, orderedDataElements.get( key1 ).size() );
-        assertEquals( 2, orderedDataElements.get( key2 ).size() );
-        assertEquals( 1, orderedDataElements.get( key3 ).size() );
-        assertEquals( 2, orderedDataElements.get( key4 ).size() );
-        assertEquals( 1, orderedDataElements.get( key5 ).size() );
-        assertEquals( 1, orderedDataElements.get( key6 ).size() );
-
     }
 }
