186b0f37694f3950cc08172d74c28538d84fbada
Abyot Asalefew Gizaw <abyota@gmail.com>
2021-09-10 10:18:03 +0200

    feat: Configurable section auto-grouping [2.34-DHIS2-11599] (#8680)
    * feat: Configurable section dataelement autogrouping
    
    * test: Fix testing failure
    
    * test: Fix to test failure
    
    * chore: Code style
    
    * test: Unit test
    
    * chore: Code quality
    
    * test: Fix to test failure

--- dhis-2/dhis-services/dhis-service-dxf2/src/test/java/org/hisp/dhis/dxf2/utils/SectionUtilsTest.java
+++ dhis-2/dhis-services/dhis-service-dxf2/src/test/java/org/hisp/dhis/dxf2/utils/SectionUtilsTest.java
@@ -34,7 +34,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
-import org.hisp.dhis.DhisTest;
+import org.hisp.dhis.DhisSpringTest;
 import org.hisp.dhis.category.Category;
 import org.hisp.dhis.category.CategoryCombo;
 import org.hisp.dhis.category.CategoryOption;
@@ -53,7 +53,7 @@ import org.springframework.beans.factory.annotation.Autowired;
  * @author Abyot Asalefew Gizaw <abyota@gmail.com>
  *
  */
-public class SectionUtilsTest extends DhisTest
+public class SectionUtilsTest extends DhisSpringTest
 {
     @Autowired
     private DataSetService dataSetService;
@@ -175,7 +175,7 @@ public class SectionUtilsTest extends DhisTest
 
         List<String> keys = new ArrayList<>( orderedDataElements.keySet() );
 
-        assertEquals( 6, orderedDataElements.keySet().size() );
+        assertEquals( 6, keys.size() );
 
         String key1 = sec.getId() + "-" + catDefault + "-" + "0";
         String key2 = sec.getId() + "-" + catAB + "-" + "1";
