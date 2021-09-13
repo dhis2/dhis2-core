be5aef7de6e2a365f4e8b6f97fc0bef6d189ee1c
Lars Helge Øverland <lars@dhis2.org>
2021-09-13 22:58:42 +0200

    Update JdbcEventAnalyticsTableManager.java

--- dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/table/JdbcEventAnalyticsTableManager.java
+++ dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/table/JdbcEventAnalyticsTableManager.java
@@ -134,9 +134,9 @@ public class JdbcEventAnalyticsTableManager
         new AnalyticsTableColumn( quote( "latitude" ), DOUBLE,
             "CASE WHEN 'POINT' = GeometryType(psi.geometry) THEN ST_Y(psi.geometry) ELSE null END" ),
         new AnalyticsTableColumn( quote( "ou" ), CHARACTER_11, NOT_NULL, "ou.uid" ),
-        new AnalyticsTableColumn( quote( "ou_level" ), INTEGER, "ous.level" ),
         new AnalyticsTableColumn( quote( "ouname" ), TEXT, NOT_NULL, "ou.name" ),
         new AnalyticsTableColumn( quote( "oucode" ), TEXT, "ou.code" ),
+        new AnalyticsTableColumn( quote( "ou_level" ), INTEGER, "ous.level" ),
         new AnalyticsTableColumn( quote( "ougeometry" ), GEOMETRY, "ou.geometry" )
             .withIndexType( IndexType.GIST ),
         new AnalyticsTableColumn( quote( "pigeometry" ), GEOMETRY, "pi.geometry" )
