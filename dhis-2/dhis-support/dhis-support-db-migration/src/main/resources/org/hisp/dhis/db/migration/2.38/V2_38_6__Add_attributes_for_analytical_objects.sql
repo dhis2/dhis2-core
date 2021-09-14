a4f9d19245210c9335c63ffcb32f804e39caa444
Jan Bernitt <jaanbernitt@gmail.com>
2021-09-14 19:05:29 +0200

    fix: adds attribute columns with default false

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.38/V2_38_6__Add_attributes_for_analytical_objects.sql
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.38/V2_38_6__Add_attributes_for_analytical_objects.sql
@@ -3,7 +3,7 @@ alter table map add column if not exists attributevalues jsonb default '{}'::jso
 alter table eventreport add column if not exists attributevalues jsonb default '{}'::jsonb;
 alter table eventchart add column if not exists attributevalues jsonb default '{}'::jsonb;
 
-alter table attribute add column if not exists visualizationAttribute boolean;
-alter table attribute add column if not exists mapAttribute boolean;
-alter table attribute add column if not exists eventReportAttribute boolean;
-alter table attribute add column if not exists eventChartAttribute boolean;
+alter table attribute add column if not exists visualizationAttribute boolean not null default false;
+alter table attribute add column if not exists mapAttribute boolean not null default false;
+alter table attribute add column if not exists eventReportAttribute boolean not null default false;
+alter table attribute add column if not exists eventChartAttribute boolean not null default false;
