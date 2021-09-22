bf8a21209ca298b49ed64df16516757e7bdb09c1
d-bernat <mail@it-bernat.de>
2021-09-22 22:18:55 +0200

    post code review impl 1.0

--- dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/event/data/AbstractJdbcEventAnalyticsManager.java
+++ dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/event/data/AbstractJdbcEventAnalyticsManager.java
@@ -41,7 +41,6 @@ import static org.hisp.dhis.system.util.MathUtils.getRounded;
 
 import java.util.Date;
 import java.util.List;
-import java.util.Optional;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -65,8 +64,6 @@ import org.hisp.dhis.common.ValueType;
 import org.hisp.dhis.commons.util.TextUtils;
 import org.hisp.dhis.feedback.ErrorCode;
 import org.hisp.dhis.jdbc.StatementBuilder;
-import org.hisp.dhis.legend.Legend;
-import org.hisp.dhis.option.Option;
 import org.hisp.dhis.period.Period;
 import org.hisp.dhis.program.AnalyticsType;
 import org.hisp.dhis.program.ProgramIndicator;
@@ -86,10 +83,6 @@ import com.google.common.collect.Lists;
 @Slf4j
 public abstract class AbstractJdbcEventAnalyticsManager
 {
-    private static final String ITEM_NAME_SEP = ": ";
-
-    private static final String NA = "[N/A]";
-
     protected static final String COL_COUNT = "count";
 
     protected static final String COL_EXTENT = "extent";
@@ -402,7 +395,7 @@ public abstract class AbstractJdbcEventAnalyticsManager
 
                     String itemName = rowSet.getString( queryItem.getItemName() );
                     String itemValue = params.isCollapseDataDimensions()
-                        ? getCollapsedDataItemValue( queryItem, itemName )
+                        ? QueryItemHelper.getCollapsedDataItemValue( queryItem, itemName )
                         : itemName;
 
                     if ( params.getOutputIdScheme() == null || params.getOutputIdScheme() == IdScheme.NAME )
@@ -413,7 +406,7 @@ public abstract class AbstractJdbcEventAnalyticsManager
                     {
                         String value = null;
 
-                        String itemOptionValue = getItemOptionValue( itemValue, params );
+                        String itemOptionValue = QueryItemHelper.getItemOptionValue( itemValue, params );
 
                         if ( itemOptionValue != null && !itemOptionValue.trim().isEmpty() )
                         {
@@ -421,7 +414,7 @@ public abstract class AbstractJdbcEventAnalyticsManager
                         }
                         else
                         {
-                            String legendItemValue = getItemLegendValue( itemValue, params );
+                            String legendItemValue = QueryItemHelper.getItemLegendValue( itemValue, params );
 
                             if ( legendItemValue != null && !legendItemValue.trim().isEmpty() )
                             {
@@ -462,32 +455,6 @@ public abstract class AbstractJdbcEventAnalyticsManager
                 grid.addNullValues( NUMERATOR_DENOMINATOR_PROPERTIES_COUNT );
             }
         }
-    }
-
-    private String getItemLegendValue( String itemValue, EventQueryParams params )
-    {
-        Optional<Option> itemOption = params.getItemOptions().stream()
-            .filter( option -> option.getDisplayName().equalsIgnoreCase( itemValue ) )
-            .findFirst();
-
-        return itemOption.map( option -> params.getOutputIdScheme() == IdScheme.UID ? option.getUid()
-            : params.getOutputIdScheme() == IdScheme.CODE ? option.getCode()
-                : params.getOutputIdScheme() == IdScheme.NAME ? option.getName()
-                    : Long.toString( option.getId() ) )
-            .orElse( null );
-    }
-
-    private String getItemOptionValue( String itemValue, EventQueryParams params )
-    {
-        Optional<Legend> itemLegend = params.getItemLegends().stream()
-            .filter( legend -> legend.getDisplayName().equalsIgnoreCase( itemValue ) )
-            .findFirst();
-
-        return itemLegend.map( option -> params.getOutputIdScheme() == IdScheme.UID ? option.getUid()
-            : params.getOutputIdScheme() == IdScheme.CODE ? option.getCode()
-                : params.getOutputIdScheme() == IdScheme.NAME ? option.getName()
-                    : Long.toString( option.getId() ) )
-            .orElse( null );
     }
 
     /**
@@ -555,41 +522,6 @@ public abstract class AbstractJdbcEventAnalyticsManager
                     return "count(" + quoteAlias( "psi" ) + ")";
                 }
             }
-        }
-    }
-
-    /**
-     * Returns an item value for the given query, query item and value. Assumes
-     * that data dimensions are collapsed for the given query. Returns the short
-     * name of the given query item followed by the item value. If the given
-     * query item has a legend set, the item value is treated as an id and
-     * substituted with the matching legend name. If the given query item has an
-     * option set, the item value is treated as a code and substituted with the
-     * matching option name.
-     *
-     * @param item the {@link QueryItem}.
-     * @param itemValue the item value.
-     */
-    private String getCollapsedDataItemValue( QueryItem item, String itemValue )
-    {
-        String value = item.getItem().getDisplayShortName() + ITEM_NAME_SEP;
-
-        Legend legend;
-        Option option;
-
-        if ( item.hasLegendSet() && (legend = item.getLegendSet().getLegendByUid( itemValue )) != null )
-        {
-            return value + legend.getDisplayName();
-        }
-        else if ( item.hasOptionSet() && (option = item.getOptionSet().getOptionByCode( itemValue )) != null )
-        {
-            return value + option.getDisplayName();
-        }
-        else
-        {
-            itemValue = StringUtils.defaultString( itemValue, NA );
-
-            return value + itemValue;
         }
     }
 
