fde84207ac5d9e23c082c5d5b0c5923fe90dc6b3
d-bernat <mail@it-bernat.de>
2021-09-23 08:45:24 +0200

    post code review impl 1.1

--- dhis-2/dhis-services/dhis-service-analytics/src/test/java/org/hisp/dhis/analytics/data/QueryItemHelperTest.java
+++ dhis-2/dhis-services/dhis-service-analytics/src/test/java/org/hisp/dhis/analytics/data/QueryItemHelperTest.java
@@ -32,6 +32,7 @@ import static org.junit.Assert.*;
 import org.hisp.dhis.DhisConvenienceTest;
 import org.hisp.dhis.analytics.event.EventQueryParams;
 import org.hisp.dhis.analytics.event.data.QueryItemHelper;
+import org.hisp.dhis.common.CodeGenerator;
 import org.hisp.dhis.common.IdScheme;
 import org.hisp.dhis.common.QueryItem;
 import org.hisp.dhis.dataelement.DataElement;
@@ -47,9 +48,9 @@ import org.junit.Test;
  */
 public class QueryItemHelperTest extends DhisConvenienceTest
 {
-    private final String UID_A = "aaaaaaaa";
+    private final String UID_A = CodeGenerator.generateUid();;
 
-    private final String UID_B = "bbbbbbbb";
+    private final String UID_B = CodeGenerator.generateUid();;
 
     private final String OPTION_NAME_A = "OptionA";
 
