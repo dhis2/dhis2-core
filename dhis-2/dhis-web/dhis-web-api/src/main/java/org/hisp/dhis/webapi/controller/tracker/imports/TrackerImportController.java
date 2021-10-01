2992b277eb6fed6eebbd8b69b75bd5882ecc5587
Enrico Colasante <enrico@dhis2.org>
2021-10-01 13:25:45 +0200

    fix: fix compilation

--- dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/tracker/imports/TrackerImportController.java
+++ dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/tracker/imports/TrackerImportController.java
@@ -60,6 +60,7 @@ import org.hisp.dhis.webapi.strategy.tracker.imports.TrackerImportStrategyHandle
 import org.hisp.dhis.webapi.utils.ContextUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpStatusCodeException;
 
