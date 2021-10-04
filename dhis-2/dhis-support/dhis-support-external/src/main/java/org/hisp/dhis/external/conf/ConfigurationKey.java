ed25ff5b95743d1dda9c74f1a28274fa3363c6c3
Morten Hansen <morten@dhis2.org>
2021-10-04 09:16:14 +0200

    fix: don't log audits to stdout, only file by default (#8892)
    * fix: don't log audits to stdout, only file by default
    
    * fix: turn off database audit by default

--- dhis-2/dhis-support/dhis-support-external/src/main/java/org/hisp/dhis/external/conf/ConfigurationKey.java
+++ dhis-2/dhis-support/dhis-support-external/src/main/java/org/hisp/dhis/external/conf/ConfigurationKey.java
@@ -124,7 +124,7 @@ public enum ConfigurationKey
     AUDIT_USE_INMEMORY_QUEUE_ENABLED( "audit.inmemory-queue.enabled", Constants.OFF ),
     AUDIT_USE_IN_MEMORY_QUEUE_ENABLED( "audit.in_memory_queue.enabled", Constants.OFF ),
     AUDIT_LOGGER( "audit.logger", Constants.ON, false ),
-    AUDIT_DATABASE( "audit.database", Constants.ON, false ),
+    AUDIT_DATABASE( "audit.database", Constants.OFF, false ),
     AUDIT_METADATA_MATRIX( "audit.metadata", "", false ),
     AUDIT_TRACKER_MATRIX( "audit.tracker", "", false ),
     AUDIT_AGGREGATE_MATRIX( "audit.aggregate", "", false ),
