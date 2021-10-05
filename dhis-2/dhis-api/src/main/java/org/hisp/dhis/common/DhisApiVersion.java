af51b61182bcafa87f279a6b5ba6869640140c97
Morten Hansen <morten@winterop.com>
2021-10-05 12:30:54 +0200

    doc: remove

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/common/DhisApiVersion.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/common/DhisApiVersion.java
@@ -57,11 +57,6 @@ public enum DhisApiVersion
     V35( 35 ),
     V36( 36 ),
     V37( 37 ),
-    /**
-     * Note: When this becomes the DEFAULT version some e2e tests will start to
-     * fail because web API starts to wrap some responses in a WebMessage.
-     * Solution is to adopt the e2e tests.
-     */
     V38( 38 ),
     V39( 39 ),
     DEFAULT( V38.getVersion() );
