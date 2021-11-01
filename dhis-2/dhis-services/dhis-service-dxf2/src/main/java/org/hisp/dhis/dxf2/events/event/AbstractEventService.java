4d7b4d7d8937da567f23072f92c9aef6948eb3a1
Philip-Larsen-Donnelly <35666657+Philip-Larsen-Donnelly@users.noreply.github.com>
2021-11-01 21:51:50 +0100

    Merge pull request from GHSA-fvm5-gp3j-c7c6

--- dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/events/event/AbstractEventService.java
+++ dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/events/event/AbstractEventService.java
@@ -1189,6 +1189,12 @@ public abstract class AbstractEventService
         {
             events = new HashSet<>();
         }
+        else
+        {
+            events = events.stream()
+                    .filter( CodeGenerator::isValidUid )
+                    .collect( Collectors.toSet() );
+        }
 
         if ( filters != null )
         {
