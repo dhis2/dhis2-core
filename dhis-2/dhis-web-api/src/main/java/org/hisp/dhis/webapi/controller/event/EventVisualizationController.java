6dbf8f4ddf1cb5f46c689e9d5d267a41b36b84f8
maikelarabori <maikelarabori@gmail.com>
2021-11-01 19:29:21 +0100

    chore: Class javadoc + code clean-up [TECH-703]

--- dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/event/EventVisualizationController.java
+++ dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/event/EventVisualizationController.java
@@ -27,6 +27,8 @@
  */
 package org.hisp.dhis.webapi.controller.event;
 
+import static org.hisp.dhis.common.DhisApiVersion.ALL;
+import static org.hisp.dhis.common.DhisApiVersion.DEFAULT;
 import static org.hisp.dhis.common.DimensionalObjectUtils.getDimensions;
 import static org.hisp.dhis.common.cache.CacheStrategy.RESPECT_SYSTEM_SETTING;
 import static org.hisp.dhis.dxf2.webmessage.WebMessageUtils.notFound;
@@ -47,7 +49,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import lombok.AllArgsConstructor;
 
-import org.hisp.dhis.common.DhisApiVersion;
 import org.hisp.dhis.common.DimensionService;
 import org.hisp.dhis.dxf2.webmessage.WebMessageException;
 import org.hisp.dhis.eventvisualization.EventVisualization;
@@ -71,9 +72,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+/**
+ * Controller responsible for providing the basic CRUD endpoints for the model
+ * EventVisualization.
+ *
+ * @author maikel arabori
+ */
 @Controller
 @RequestMapping( value = API_ENDPOINT )
-@ApiVersion( { DhisApiVersion.DEFAULT, DhisApiVersion.ALL } )
+@ApiVersion( { DEFAULT, ALL } )
 @AllArgsConstructor
 public class EventVisualizationController
     extends
@@ -90,6 +97,40 @@ public class EventVisualizationController
     private final I18nManager i18nManager;
 
     private final ContextUtils contextUtils;
+
+    @GetMapping( value = { "/{uid}/data", "/{uid}/data.png" } )
+    void generateChart(
+        @PathVariable( "uid" ) String uid,
+        @RequestParam( value = "date", required = false ) Date date,
+        @RequestParam( value = "ou", required = false ) String ou,
+        @RequestParam( value = "width", defaultValue = "800", required = false ) int width,
+        @RequestParam( value = "height", defaultValue = "500", required = false ) int height,
+        @RequestParam( value = "attachment", required = false ) boolean attachment,
+        HttpServletResponse response )
+        throws IOException,
+        WebMessageException
+    {
+        // TODO no acl?
+        final EventVisualization eventVisualization = eventVisualizationService.getEventVisualization( uid );
+
+        if ( eventVisualization == null )
+        {
+            throw new WebMessageException( notFound( "Event visualization does not exist: " + uid ) );
+        }
+
+        doesNotAllowPivotAndReportChart( eventVisualization );
+
+        final OrganisationUnit unit = ou != null ? organisationUnitService.getOrganisationUnit( ou ) : null;
+
+        final JFreeChart jFreeChart = chartService.getJFreeChart( new PlotData( eventVisualization ), date, unit,
+            i18nManager.getI18nFormat() );
+
+        final String filename = filenameEncode( eventVisualization.getName() ) + ".png";
+
+        contextUtils.configureResponse( response, CONTENT_TYPE_PNG, RESPECT_SYSTEM_SETTING, filename, attachment );
+
+        writeChartAsPNG( response.getOutputStream(), jFreeChart, width, height );
+    }
 
     @Override
     protected EventVisualization deserializeJsonEntity( final HttpServletRequest request )
@@ -155,40 +196,6 @@ public class EventVisualizationController
         eventVisualization.getColumnDimensions().addAll( getDimensions( eventVisualization.getColumns() ) );
         eventVisualization.getRowDimensions().addAll( getDimensions( eventVisualization.getRows() ) );
         eventVisualization.getFilterDimensions().addAll( getDimensions( eventVisualization.getFilters() ) );
-    }
-
-    @GetMapping( value = { "/{uid}/data", "/{uid}/data.png" } )
-    public void generateChart(
-        @PathVariable( "uid" ) String uid,
-        @RequestParam( value = "date", required = false ) Date date,
-        @RequestParam( value = "ou", required = false ) String ou,
-        @RequestParam( value = "width", defaultValue = "800", required = false ) int width,
-        @RequestParam( value = "height", defaultValue = "500", required = false ) int height,
-        @RequestParam( value = "attachment", required = false ) boolean attachment,
-        HttpServletResponse response )
-        throws IOException,
-        WebMessageException
-    {
-        // TODO no acl?
-        final EventVisualization eventVisualization = eventVisualizationService.getEventVisualization( uid );
-
-        if ( eventVisualization == null )
-        {
-            throw new WebMessageException( notFound( "Event visualization does not exist: " + uid ) );
-        }
-
-        doesNotAllowPivotAndReportChart( eventVisualization );
-
-        final OrganisationUnit unit = ou != null ? organisationUnitService.getOrganisationUnit( ou ) : null;
-
-        final JFreeChart jFreeChart = chartService.getJFreeChart( new PlotData( eventVisualization ), date, unit,
-            i18nManager.getI18nFormat() );
-
-        final String filename = filenameEncode( eventVisualization.getName() ) + ".png";
-
-        contextUtils.configureResponse( response, CONTENT_TYPE_PNG, RESPECT_SYSTEM_SETTING, filename, attachment );
-
-        writeChartAsPNG( response.getOutputStream(), jFreeChart, width, height );
     }
 
     private void doesNotAllowPivotAndReportChart( final EventVisualization eventVisualization )
