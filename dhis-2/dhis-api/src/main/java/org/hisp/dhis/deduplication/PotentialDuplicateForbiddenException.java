d9802a26cdb16077c468cb8c1e23d94e55618409
LUCA <lucacambi77@gmail.com>
2021-11-04 22:16:37 +0100

    ref: move exception to api

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/deduplication/PotentialDuplicateForbiddenException.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/deduplication/PotentialDuplicateForbiddenException.java
@@ -29,17 +29,13 @@ package org.hisp.dhis.deduplication;
 
 import lombok.Getter;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 /**
  * @author Luca Cambi <luca@dhis2.org>
  */
 @Getter
-@ResponseStatus( HttpStatus.CONFLICT )
-public class PotentialDuplicateConflictException extends RuntimeException
+public class PotentialDuplicateForbiddenException extends Exception
 {
-    public PotentialDuplicateConflictException( String message )
+    public PotentialDuplicateForbiddenException( String message )
     {
         super( message );
     }
