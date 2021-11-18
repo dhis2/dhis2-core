7bbe7fd6ffe36bc354319599f00dab4892b4a4e3
zubaira <zubair@dhis2.org>
2021-11-18 07:58:22 +0100

    fix compilation error

--- dhis-2/dhis-support/dhis-support-hibernate/src/main/java/org/hisp/dhis/hibernate/HibernateUtils.java
+++ dhis-2/dhis-support/dhis-support-hibernate/src/main/java/org/hisp/dhis/hibernate/HibernateUtils.java
@@ -27,7 +27,6 @@
  */
 package org.hisp.dhis.hibernate;
 
-import java.beans.IntrospectionException;
 import java.beans.PropertyDescriptor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
@@ -42,6 +41,7 @@ import org.hibernate.collection.spi.PersistentCollection;
 import org.hibernate.proxy.HibernateProxy;
 import org.hibernate.proxy.pojo.javassist.SerializableProxy;
 import org.hisp.dhis.commons.util.DebugUtils;
+import org.springframework.beans.BeanUtils;
 
 import com.google.common.base.Preconditions;
 
@@ -119,7 +119,7 @@ public class HibernateUtils
             .forEach( f -> {
                 try
                 {
-                    PropertyDescriptor pd = new PropertyDescriptor( f.getName(), proxy.getClass() );
+                    PropertyDescriptor pd = BeanUtils.getPropertyDescriptor( proxy.getClass(), f.getName() );
 
                     Object persistentObject = pd.getReadMethod().invoke( proxy );
 
@@ -128,7 +128,7 @@ public class HibernateUtils
                         Hibernate.initialize( persistentObject );
                     }
                 }
-                catch ( IllegalAccessException | IntrospectionException | InvocationTargetException e )
+                catch ( IllegalAccessException | InvocationTargetException e )
                 {
                     DebugUtils.getStackTrace( e );
                 }
