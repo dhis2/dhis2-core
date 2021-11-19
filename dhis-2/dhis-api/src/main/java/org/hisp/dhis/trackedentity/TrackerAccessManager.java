208faf5ed36210260b9a88a2b5377035c067e7cc
Abyot Asalefew Gizaw <abyota@gmail.com>
2021-11-19 09:29:20 +0100

    fix: OuMode validation in event query

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/trackedentity/TrackerAccessManager.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/trackedentity/TrackerAccessManager.java
@@ -31,6 +31,7 @@ import java.util.List;
 
 import org.hisp.dhis.category.CategoryOptionCombo;
 import org.hisp.dhis.dataelement.DataElement;
+import org.hisp.dhis.organisationunit.OrganisationUnit;
 import org.hisp.dhis.program.Program;
 import org.hisp.dhis.program.ProgramInstance;
 import org.hisp.dhis.program.ProgramStageInstance;
@@ -99,4 +100,18 @@ public interface TrackerAccessManager
     List<String> canRead( User user, CategoryOptionCombo categoryOptionCombo );
 
     List<String> canWrite( User user, CategoryOptionCombo categoryOptionCombo );
+
+    /**
+     * Checks if user has access to organisation unit under defined tracker
+     * program protection level
+     *
+     * @param user the user to check access for
+     * @param program program to check against protection level
+     * @param orgUnit the org unit to be checked under user's scope and program
+     *        protection
+     *
+     * @return true if user has access to the org unit under the mentioned
+     *         program context, otherwise return false
+     */
+    boolean canAccess( User user, Program program, OrganisationUnit orgUnit );
 }
