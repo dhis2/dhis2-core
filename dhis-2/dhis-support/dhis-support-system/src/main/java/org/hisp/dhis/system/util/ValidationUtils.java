0402bb0f8ab0e6a651b0ea970fd108876dc2aa82
Enrico Colasante <enrico@dhis2.org>
2021-12-13 15:31:55 +0100

    fix: Fix default value for empty date

--- dhis-2/dhis-support/dhis-support-system/src/main/java/org/hisp/dhis/system/util/ValidationUtils.java
+++ dhis-2/dhis-support/dhis-support-system/src/main/java/org/hisp/dhis/system/util/ValidationUtils.java
@@ -31,6 +31,7 @@ import static org.apache.commons.lang3.StringUtils.trimToEmpty;
 import static org.hisp.dhis.system.util.MathUtils.parseDouble;
 
 import java.awt.geom.Point2D;
+import java.util.Date;
 import java.util.Locale;
 import java.util.Objects;
 import java.util.Set;
@@ -663,6 +664,10 @@ public class ValidationUtils
         else if ( valueType.isBoolean() )
         {
             return false;
+        }
+        else if ( valueType.isDate() )
+        {
+            return new Date();
         }
         else
         {
