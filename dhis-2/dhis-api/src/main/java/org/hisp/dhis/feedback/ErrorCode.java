41f5c9b318858a6ad4b7330147d1f5f2c4999a9e
Viet Nguyen <viet@dhis2.org>
2021-12-13 23:46:32 +0700

    fix: add validation for duplicate translations

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/feedback/ErrorCode.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/feedback/ErrorCode.java
@@ -44,6 +44,7 @@ public enum ErrorCode
     E1103( "Category option combo not found or not accessible: `{0}`" ),
     E1104( "Attribute option combo not found or not accessible: `{0}`" ),
     E1105( "Data set not found or not accessible: `{0}`" ),
+    E1106( "There are duplicate translation record for property `{0}` and locale `{1}`" ),
 
     /* Data */
     E2000( "Query parameters cannot be null" ),
---
+++ dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/metadata/objectbundle/validation/TranslationsCheck.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright (c) 2004-2021, University of Oslo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * Neither the name of the HISP project nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.hisp.dhis.dxf2.metadata.objectbundle.validation;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Consumer;
+
+import org.hisp.dhis.common.IdentifiableObject;
+import org.hisp.dhis.commons.collection.CollectionUtils;
+import org.hisp.dhis.dxf2.metadata.objectbundle.ObjectBundle;
+import org.hisp.dhis.feedback.ErrorCode;
+import org.hisp.dhis.feedback.ErrorReport;
+import org.hisp.dhis.feedback.ObjectReport;
+import org.hisp.dhis.importexport.ImportStrategy;
+import org.hisp.dhis.translation.Translation;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author viet@dhis2.org
+ */
+@Component
+public class TranslationsCheck implements ObjectValidationCheck
+{
+
+    @Override
+    public <T extends IdentifiableObject> void check( ObjectBundle bundle, Class<T> klass, List<T> persistedObjects,
+        List<T> nonPersistedObjects, ImportStrategy importStrategy, ValidationContext context,
+        Consumer<ObjectReport> addReports )
+    {
+        List<T> objects = selectObjects( persistedObjects, nonPersistedObjects, importStrategy );
+
+        if ( CollectionUtils.isEmpty( objects ) )
+        {
+            return;
+        }
+
+        objects.forEach( o -> run( o, klass, addReports ) );
+    }
+
+    private <T extends IdentifiableObject> void run( IdentifiableObject object, Class<T> klass,
+        Consumer<ObjectReport> addReports )
+    {
+        Set<Translation> translations = object.getTranslations();
+
+        if ( CollectionUtils.isEmpty( translations ) )
+        {
+            return;
+        }
+
+        Map<String, String> mapPropertyLocale = new HashMap<>();
+        int index = 0;
+
+        for ( Translation translation : translations )
+        {
+            ObjectReport objectReport = new ObjectReport( klass, index++ );
+
+            if ( mapPropertyLocale.containsKey( translation.getProperty() )
+                && mapPropertyLocale.get( translation.getProperty() ).equals( translation.getLocale() ) )
+            {
+                objectReport.addErrorReport(
+                    new ErrorReport( Translation.class, ErrorCode.E1106, translation.getProperty(),
+                        translation.getLocale() )
+                            .setErrorKlass( klass ) );
+            }
+            else
+            {
+                mapPropertyLocale.put( translation.getProperty(), translation.getLocale() );
+            }
+
+            if ( translation.getLocale() == null )
+            {
+                objectReport.addErrorReport(
+                    new ErrorReport( Translation.class, ErrorCode.E4000, "locale" ).setErrorKlass( klass ) );
+            }
+
+            if ( translation.getProperty() == null )
+            {
+                objectReport.addErrorReport( new ErrorReport( Translation.class, ErrorCode.E4000, "property" )
+                    .setErrorKlass( klass ) );
+            }
+
+            if ( translation.getValue() == null )
+            {
+                objectReport.addErrorReport(
+                    new ErrorReport( Translation.class, ErrorCode.E4000, "value" ).setErrorKlass( klass ) );
+            }
+
+            addReports.accept( objectReport );
+        }
+    }
+}
