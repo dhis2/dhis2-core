eecd9e9be1a182a16698f5716a2aa7a454612490
zubaira <zubair@dhis2.org>
2021-12-29 09:47:52 +0100

    add flyway script

---
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.38/V2_38_19__Add_ValueType_column_into_programrulevariable.sql
@@ -0,0 +1,7 @@
+-- This script relates to the issue https://jira.dhis2.org/browse/TECH-820
+-- Adds a new column "ValueTyp4" into ProgramRuleVariable table
+
+
+alter table programrulevariable add column if not exists "valuetype" character varying(50);
+update table programrulevariable set valuetype = 'TEXT' where valuetype is null;
+
