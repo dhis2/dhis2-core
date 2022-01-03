dbd1ff634a193db31742db18eb68f5244d339de5
zubaira <zubair@dhis2.org>
2022-01-03 13:58:14 +0100

    remove flyway java migration

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.38/V2_38_19__Add_ValueType_column_into_programrulevariable.sql
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.38/V2_38_19__Add_ValueType_column_into_programrulevariable.sql
@@ -3,6 +3,8 @@
 
 
 alter table programrulevariable add column if not exists "valuetype" character varying(50);
-update programrulevariable set valuetype = 'TEXT' where sourcetype = 'CALCULATED_VALUE';
+UPDATE programrulevariable prv SET valuetype = de.valuetype FROM dataelement de WHERE prv.dataelementid = de.dataelementid;
+UPDATE programrulevariable prv SET valuetype = attr.valuetype FROM trackedentityattribute attr WHERE prv.trackedentityattributeid = attr.trackedentityattributeid;
+UPDATE programrulevariable prv set valuetype = 'TEXT' where sourcetype = 'CALCULATED_VALUE';
 alter table programrulevariable alter column valuetype set not null;
 
