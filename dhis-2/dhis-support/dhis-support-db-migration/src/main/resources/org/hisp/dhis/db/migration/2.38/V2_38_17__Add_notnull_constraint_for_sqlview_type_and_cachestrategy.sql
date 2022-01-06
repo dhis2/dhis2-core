aa0f6334f4a4aade06eb903b69f92bcf5e1b4a9e
Viet Nguyen <viet@dhis2.org>
2022-01-06 14:45:35 +0700

    code styles

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.38/V2_38_17__Add_notnull_constraint_for_sqlview_type_and_cachestrategy.sql
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.38/V2_38_17__Add_notnull_constraint_for_sqlview_type_and_cachestrategy.sql
@@ -1,5 +1,5 @@
 update sqlview set type='VIEW' where type is null;
 update sqlview set cachestrategy='RESPECT_SYSTEM_SETTING' where cachestrategy is null;
 
-ALTER TABLE sqlview ALTER COLUMN type SET NOT NULL;
-ALTER TABLE sqlview ALTER COLUMN cachestrategy SET NOT NULL;
+alter table sqlview alter column type set not null;
+alter table sqlview alter column cachestrategy set not null;
