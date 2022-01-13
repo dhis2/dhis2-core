2d0a14b1b749334bddfc36f268bcdd1b9c82cd2e
maikelarabori <maikelarabori@gmail.com>
2022-01-13 18:38:35 +0100

    feat: Support event headers as GET param [DHIS2-12314]

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/common/EventDataQueryRequest.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/common/EventDataQueryRequest.java
@@ -29,6 +29,7 @@ package org.hisp.dhis.common;
 
 import java.util.Date;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 import lombok.AllArgsConstructor;
@@ -57,6 +58,8 @@ public class EventDataQueryRequest
     private Set<String> dimension;
 
     private Set<String> filter;
+
+    private Set<String> headers;
 
     private String value;
 
@@ -141,6 +144,7 @@ public class EventDataQueryRequest
         queryRequest.endDate = this.endDate;
         queryRequest.dimension = new HashSet<>( this.dimension );
         queryRequest.filter = new HashSet<>( this.filter );
+        queryRequest.headers = new LinkedHashSet<>( this.headers );
         queryRequest.value = this.value;
         queryRequest.aggregationType = this.aggregationType;
         queryRequest.skipMeta = this.skipMeta;
@@ -201,6 +205,7 @@ public class EventDataQueryRequest
                 .endDate( criteria.getEndDate() )
                 .eventStatus( criteria.getEventStatus() )
                 .filter( criteria.getFilter() )
+                .headers( criteria.getHeaders() )
                 .hierarchyMeta( criteria.isHierarchyMeta() )
                 .includeMetadataDetails( criteria.isIncludeMetadataDetails() )
                 .limit( criteria.getLimit() )
@@ -233,9 +238,9 @@ public class EventDataQueryRequest
             return startDate( criteria.getStartDate() )
                 .timeField( criteria.getTimeField() )
                 .endDate( criteria.getEndDate() )
-                .dimension(
-                    criteria.getDimension() )
+                .dimension( criteria.getDimension() )
                 .filter( criteria.getFilter() )
+                .headers( criteria.getHeaders() )
                 .ouMode( criteria.getOuMode() )
                 .asc( criteria.getAsc() )
                 .desc( criteria.getDesc() )
