362d29f46894ce510ef6a72e6f503c76e7ab490d
Enrico Colasante <enrico@dhis2.org>
2022-02-17 14:35:16 +0100

    Fix tests

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.38/V2_38_33__Add_allowedsearchtypes_column_to_trackedentityattribute.sql
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.38/V2_38_33__Add_allowedsearchtypes_column_to_trackedentityattribute.sql
@@ -1,1 +1,3 @@
-ALTER TABLE trackedentityattribute ADD COLUMN IF NOT EXISTS allowedsearchtypes jsonb default '["EQ","GT","GE","LT","LE","LIKE","IN","SW","EW"]'::jsonb;
+ALTER TABLE trackedentityattribute
+    ADD COLUMN IF NOT EXISTS allowedsearchtypes jsonb
+    default '["EQ","GT","GE","LT","LE","LIKE","IN","SW","EW","IEQ","NE","NIEQ","NLIKE","ILIKE","NILIKE"]'::jsonb;
