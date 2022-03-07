031e1898a9666958a28eea7aa330b7ace2ddb432
d-bernat <mail@it-bernat.de>
2022-03-07 19:22:26 +0100

    clean coding

--- dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/event/data/JdbcEventAnalyticsManager.java
+++ dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/event/data/JdbcEventAnalyticsManager.java
@@ -135,7 +135,7 @@ public class JdbcEventAnalyticsManager
         }
         else
         {
-            withExceptionHandling( () -> getEvents( params, grid, sql, maxLimit ) );
+            withExceptionHandling( () -> getEvents( params, grid, sql ) );
         }
 
         return grid;
