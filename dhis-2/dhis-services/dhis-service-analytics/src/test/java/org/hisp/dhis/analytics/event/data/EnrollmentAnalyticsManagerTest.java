48151c456075638eee10309439f8201d6e19b773
Giuseppe Nespolino <gnespolino@users.noreply.github.com>
2022-03-21 16:07:58 +0100

    fix: null numbers from database are returned as double NaN [DHIS2-12787] (#10235)
    * fix: null numbers from database are returned as double NaN [DHIS2-12787]
    
    * style: formatting [DHIS2-12787]
    
    (cherry picked from commit 3018b1c93c45fcc35ca2ede0b07f030371ad2809)

--- dhis-2/dhis-services/dhis-service-analytics/src/test/java/org/hisp/dhis/analytics/event/data/EnrollmentAnalyticsManagerTest.java
+++ dhis-2/dhis-services/dhis-service-analytics/src/test/java/org/hisp/dhis/analytics/event/data/EnrollmentAnalyticsManagerTest.java
@@ -197,7 +197,7 @@ class EnrollmentAnalyticsManagerTest extends
 
         if ( valueType == ValueType.NUMBER )
         {
-            subSelect = "coalesce(" + subSelect + ", null) as fWIAEtYVEGk";
+            subSelect = "coalesce(" + subSelect + ", double precision 'NaN') as fWIAEtYVEGk";
         }
         String expected = "ax.\"monthly\",ax.\"ou\"," + subSelect + "  from " + getTable( programA.getUid() )
             + " as ax where ax.\"monthly\" in ('2000Q1') and (uidlevel1 = 'ouabcdefghA' ) " + "and ps = '"
@@ -261,7 +261,8 @@ class EnrollmentAnalyticsManagerTest extends
             + programA.getUid() + ".pi = ax.pi and \"fWIAEtYVEGk\" is not null and ps = '"
             + programStage.getUid() + "' order by executiondate desc limit 1 )";
 
-        String expected = "ax.\"monthly\",ax.\"ou\"," + "coalesce(" + subSelect + ", null) as fWIAEtYVEGk" + "  from "
+        String expected = "ax.\"monthly\",ax.\"ou\"," + "coalesce(" + subSelect
+            + ", double precision 'NaN') as fWIAEtYVEGk" + "  from "
             + getTable( programA.getUid() )
             + " as ax where ax.\"monthly\" in ('2000Q1') and (uidlevel1 = 'ouabcdefghA' ) "
             + "and ps = '" + programStage.getUid() + "' and " + subSelect + " > '10' limit 10001";
