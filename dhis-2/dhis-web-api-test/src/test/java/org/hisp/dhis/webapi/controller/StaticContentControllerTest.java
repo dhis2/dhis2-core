beb0b7be13a1bed6a6ce5f585b65105d5fd367c0
Enrico Colasante <enrico@dhis2.org>
2022-03-24 10:57:01 +0100

    chore: Clean up all support modules to remove all the warnings from analyze [TECH-801] (#10276)
    * chore: Clean up all support modules to remove all the warnings from analyze [TECH-801]
    
    * Add missing dependency

--- dhis-2/dhis-web-api-test/src/test/java/org/hisp/dhis/webapi/controller/StaticContentControllerTest.java
+++ dhis-2/dhis-web-api-test/src/test/java/org/hisp/dhis/webapi/controller/StaticContentControllerTest.java
@@ -29,12 +29,6 @@ package org.hisp.dhis.webapi.controller;
 
 import static java.lang.Boolean.FALSE;
 import static java.lang.Boolean.TRUE;
-import static org.apache.http.HttpStatus.SC_BAD_REQUEST;
-import static org.apache.http.HttpStatus.SC_MOVED_TEMPORARILY;
-import static org.apache.http.HttpStatus.SC_NOT_FOUND;
-import static org.apache.http.HttpStatus.SC_NO_CONTENT;
-import static org.apache.http.HttpStatus.SC_OK;
-import static org.apache.http.HttpStatus.SC_UNSUPPORTED_MEDIA_TYPE;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.core.StringContains.containsString;
 import static org.hisp.dhis.fileresource.FileResourceDomain.DOCUMENT;
@@ -100,7 +94,7 @@ class StaticContentControllerTest extends DhisWebSpringTest
         throws Exception
     {
         mvc.perform( get( URL + "idontexist" ).session( session ).contentType( APPLICATION_JSON_UTF8 ) )
-            .andExpect( status().is( SC_NOT_FOUND ) );
+            .andExpect( status().isNotFound() );
     }
 
     @Test
@@ -109,7 +103,7 @@ class StaticContentControllerTest extends DhisWebSpringTest
     {
         mvc.perform( get( URL + LOGO_BANNER ).accept( TEXT_HTML_VALUE ).session( session ) )
             .andExpect( redirectedUrlPattern( "**/dhis-web-commons/css/light_blue/logo_banner.png" ) )
-            .andExpect( status().is( SC_MOVED_TEMPORARILY ) );
+            .andExpect( status().isMovedTemporarily() );
     }
 
     @Test
@@ -122,7 +116,7 @@ class StaticContentControllerTest extends DhisWebSpringTest
         systemSettingManager.saveSystemSetting( USE_CUSTOM_LOGO_BANNER, TRUE );
         mvc.perform( get( URL + LOGO_BANNER ).accept( TEXT_HTML_VALUE ).session( session ) )
             .andExpect( content().contentType( MIME_PNG ) ).andExpect( content().bytes( mockMultipartFile.getBytes() ) )
-            .andExpect( status().is( SC_OK ) );
+            .andExpect( status().isOk() );
     }
 
     @Test
@@ -201,7 +195,7 @@ class StaticContentControllerTest extends DhisWebSpringTest
         throws Exception
     {
         mvc.perform( multipart( URL + LOGO_BANNER ).file( mockMultipartFile ).session( session ) )
-            .andExpect( status().is( SC_NO_CONTENT ) );
+            .andExpect( status().isNoContent() );
     }
 
     @Test
@@ -212,7 +206,7 @@ class StaticContentControllerTest extends DhisWebSpringTest
         mvc.perform( multipart( URL + LOGO_BANNER )
             .file( new MockMultipartFile( "file", "testlogo.png", IMAGE_JPEG.toString(), "image".getBytes() ) )
             .session( session ) ).andExpect( content().json( error ) )
-            .andExpect( status().is( SC_UNSUPPORTED_MEDIA_TYPE ) );
+            .andExpect( status().isUnsupportedMediaType() );
     }
 
     @Test
@@ -221,7 +215,7 @@ class StaticContentControllerTest extends DhisWebSpringTest
     {
         final String error = buildResponse( "Bad Request", 400, "ERROR", "This key is not supported." );
         mvc.perform( multipart( URL + "idontexist" ).file( mockMultipartFile ).session( session ) )
-            .andExpect( content().json( error ) ).andExpect( status().is( SC_BAD_REQUEST ) );
+            .andExpect( content().json( error ) ).andExpect( status().isBadRequest() );
     }
 
     private String buildResponse( String httpStatus, int code, String status, String message )
