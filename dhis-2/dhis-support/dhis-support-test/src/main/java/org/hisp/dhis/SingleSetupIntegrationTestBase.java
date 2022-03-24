a5c050647ca0324c1e505d830c235977a553fe1b
Jim Grace <jimgrace@users.noreply.github.com>
2022-03-24 14:24:13 -0400

    refactor: add @Override for emptyDatabaseAfterTest

--- dhis-2/dhis-support/dhis-support-test/src/main/java/org/hisp/dhis/SingleSetupIntegrationTestBase.java
+++ dhis-2/dhis-support/dhis-support-test/src/main/java/org/hisp/dhis/SingleSetupIntegrationTestBase.java
@@ -66,6 +66,7 @@ public abstract class SingleSetupIntegrationTestBase
         nonTransactionalAfter();
     }
 
+    @Override
     protected boolean emptyDatabaseAfterTest()
     {
         return true;
