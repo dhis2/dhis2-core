c38756a7386dcfc6b4feba7ca1fe8ca4707760fa
d-bernat <mail@it-bernat.de>
2022-04-04 10:33:51 +0200

    fix: Paging issues in event/enrollment analytics [2.39.0-dhis2-12989]

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/program/ProgramIndicator.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/program/ProgramIndicator.java
@@ -140,7 +140,25 @@ public class ProgramIndicator
      */
     public AggregationType getAggregationTypeFallback()
     {
-        return aggregationType != null ? aggregationType : AggregationType.AVERAGE;
+        if ( aggregationType == null )
+        {
+            return AggregationType.AVERAGE;
+        }
+
+        switch ( aggregationType )
+        {
+        case AVERAGE_SUM_ORG_UNIT:
+        case FIRST:
+        case LAST:
+        case LAST_IN_PERIOD:
+            return AggregationType.SUM;
+        case FIRST_AVERAGE_ORG_UNIT:
+        case LAST_AVERAGE_ORG_UNIT:
+        case LAST_IN_PERIOD_AVERAGE_ORG_UNIT:
+            return AggregationType.AVERAGE;
+        default:
+            return aggregationType;
+        }
     }
 
     public void addProgramIndicatorGroup( ProgramIndicatorGroup group )
