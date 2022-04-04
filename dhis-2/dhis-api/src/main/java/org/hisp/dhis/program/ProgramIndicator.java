003ceb4bc77798e9fce5f9e8dd0b9a9ee19fd74a
d-bernat <mail@it-bernat.de>
2022-04-04 17:30:35 +0200

    fix: [D]"last value" aggregation type is broken in later versions for data elements and program indicators in the analytics API [2.39.0-dhis2-12989]

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/program/ProgramIndicator.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/program/ProgramIndicator.java
@@ -136,8 +136,8 @@ public class ProgramIndicator
     }
 
     /**
-     * Returns aggregation type (aggregation function supported by postgres), if
-     * not exists returns AVERAGE.
+     * Returns aggregation type (for example for aggregation function supported
+     * by postgres), if not exists returns AVERAGE.
      */
     public AggregationType getAggregationTypeFallback()
     {
