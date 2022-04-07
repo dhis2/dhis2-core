05a8fa31eddada4a25e19a47ea94f0f5c4a2a65c
Maikel Arabori <51713408+maikelarabori@users.noreply.github.com>
2022-04-07 13:09:29 +0200

    fix: Visualization migration issue [DHIS2-13002] (#10419)

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.37/V2_37_21__Rename_Column_in_Visualization.sql
+++
@@ -1,4 +0,0 @@
--- This script relates to the task https://jira.dhis2.org/browse/DHIS2-11347
-
--- Rename column: "legendhidekey" to "legendshowkey".
-ALTER TABLE visualization RENAME COLUMN legendhidekey TO legendshowkey;
