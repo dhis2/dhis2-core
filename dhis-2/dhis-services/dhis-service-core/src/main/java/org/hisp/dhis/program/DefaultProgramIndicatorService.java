a0d49811f08a81cca97c85b1ab5f8ab0f8814550
Jim Grace <jimgrace@users.noreply.github.com>
2022-04-19 18:39:05 -0400

    fix: V{value_count} in PI filter DHIS2-13099 (#10516) (#10534)
    * fix: V{value_count} in PI filter DHIS2-13099
    
    * fix: ProgramIndicatorServiceTest
    
    (cherry picked from commit efac7618ad1f3d685c66e344ecf701040c0b4374)

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/program/DefaultProgramIndicatorService.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/program/DefaultProgramIndicatorService.java
@@ -382,7 +382,9 @@ public class DefaultProgramIndicatorService
     private String _getAnalyticsSql( String expression, ProgramIndicator programIndicator, Date startDate, Date endDate,
         String tableAlias )
     {
-        Set<String> uids = getDataElementAndAttributeIdentifiers( expression, programIndicator.getAnalyticsType() );
+        // Get the uids from the expression even if this is the filter
+        Set<String> uids = getDataElementAndAttributeIdentifiers( programIndicator.getExpression(),
+            programIndicator.getAnalyticsType() );
 
         ProgramExpressionParams params = ProgramExpressionParams.builder()
             .programIndicator( programIndicator )
