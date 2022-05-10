2c99095c295a2ed270159ceb1581019a70f13eaa
zubaira <zubair@dhis2.org>
2022-05-10 07:57:43 +0200

    refactor migration script

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.39/V2_39_9__Update_programstageid_in_relationshipconstraint.sql
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.39/V2_39_9__Update_programstageid_in_relationshipconstraint.sql
@@ -2,6 +2,9 @@
 -- Update programstageid column in relationshipconstraint table (programstageid is now mandatory for event type relationshipconstraint)
 
 
-UPDATE relationshipconstraint rlc SET programstageid = ps.programstageid FROM programstage ps WHERE rlc.programid = ps.programid
-    and ps.programid in (select programid from program where programid in
-    (select programid from relationshipconstraint where entity='PROGRAM_STAGE_INSTANCE' and programstageid is null) and type='WITHOUT_REGISTRATION')
+UPDATE relationshipconstraint rlc SET programstageid = ps.programstageid FROM programstage ps
+        WHERE rlc.programid = ps.programid AND ps.programid IN
+                (SELECT programid FROM programWHERE type = 'WITHOUT_REGISTRATION')
+        AND rlc.programstageid IS NULL
+        AND rlc.programid IS NOT NULL
+        AND rlc.entity = 'PROGRAM_STAGE_INSTANCE'
