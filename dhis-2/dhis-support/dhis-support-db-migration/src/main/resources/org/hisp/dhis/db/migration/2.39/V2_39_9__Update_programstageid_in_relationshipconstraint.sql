d68936405cc1efd97a858c44257ed0bc054f99d2
zubaira <zubair@dhis2.org>
2022-05-10 08:09:10 +0200

    minor

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.39/V2_39_9__Update_programstageid_in_relationshipconstraint.sql
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.39/V2_39_9__Update_programstageid_in_relationshipconstraint.sql
@@ -4,7 +4,7 @@
 
 UPDATE relationshipconstraint rlc SET programstageid = ps.programstageid FROM programstage ps
         WHERE rlc.programid = ps.programid AND ps.programid IN
-                (SELECT programid FROM programWHERE type = 'WITHOUT_REGISTRATION')
+                (SELECT programid FROM program WHERE type = 'WITHOUT_REGISTRATION')
         AND rlc.programstageid IS NULL
         AND rlc.programid IS NOT NULL
         AND rlc.entity = 'PROGRAM_STAGE_INSTANCE'
