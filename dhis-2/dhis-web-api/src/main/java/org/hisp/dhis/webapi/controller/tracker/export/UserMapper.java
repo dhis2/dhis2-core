dhis2/dhis2-core

commit 5f63b52ac0a4f426145ae43affa58295f804812a

chore: split domain and view models DHIS2-12563 (#10379)
* chore: parameter name should make clear these are UIDs

* chore: remove unused field

* chore: TrackerIdSchemeParam.value is the attribute UID

* chore: split domain and view models

* chore: add MetadataIdentifier

* chore: deduplicate loading JSON test fixtures

* chore: Enrollment.program is a MetadataIdentifier

* chore: fix javadoc and test setup

* chore: remove todo

* test: adapt test fixtures to Enrollment.program

now being a MetadataIdentifier

* test: adapt test fixtures to Enrollment.program

now being a MetadataIdentifier

* test: adapt test fixtures to Enrollment.program

now being a MetadataIdentifier

* test: TrackerPreheat.get using MetadataIdentifier

* test: MetadataIdentifier

* chore: remove todo

dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/tracker/export/UserMapper.java
@@ -27,12 +27,11 @@
  */
 package org.hisp.dhis.webapi.controller.tracker.export;
 
-import org.hisp.dhis.program.UserInfoSnapshot;
-import org.hisp.dhis.tracker.domain.User;
+import org.hisp.dhis.webapi.controller.tracker.view.User;
 import org.mapstruct.Mapper;
 
 @Mapper
-public interface UserMapper extends DomainMapper<UserInfoSnapshot, User>
+public interface UserMapper extends ViewMapper<User, org.hisp.dhis.tracker.domain.User>
 {
-    User from( UserInfoSnapshot snapshot );
+    org.hisp.dhis.tracker.domain.User from( User user );
 }

dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/tracker/view/User.java
@@ -1,0 +1,64 @@
+/*
+ * Copyright (c) 2004-2022, University of Oslo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * Neither the name of the HISP project nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.hisp.dhis.webapi.controller.tracker.view;
+
+import static org.apache.commons.lang3.StringUtils.isBlank;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * @author Enrico Colasante
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User
+{
+    @JsonProperty
+    private String uid;
+
+    @JsonProperty
+    private String username;
+
+    @JsonProperty
+    private String firstName;
+
+    @JsonProperty
+    private String surname;
+
+    public boolean isEmpty()
+    {
+        return isBlank( uid ) && isBlank( username );
+    }
+}

dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/tracker/imports/UserMapper.java
@@ -1,0 +1,38 @@
+/*
+ * Copyright (c) 2004-2022, University of Oslo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * Neither the name of the HISP project nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.hisp.dhis.webapi.controller.tracker.imports;
+
+import org.hisp.dhis.webapi.controller.tracker.export.ViewMapper;
+import org.hisp.dhis.webapi.controller.tracker.view.User;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface UserMapper extends ViewMapper<User, org.hisp.dhis.tracker.domain.User>
+{
+    org.hisp.dhis.tracker.domain.User from( User user );
+}

commit 473b75ed7d24b0045fb3e5fc65433add26db487d

fix: export UserMapper needs to map from UserInfoSnapshot
to the view.User

Mistake was introduced in
https://github.com/dhis2/dhis2-core/commit/5f63b52ac0a4f426145ae43affa58295f804812a#diff-af6eb353ca81a11eb1327dc7da64ebc6544855dd94c87ce1dee94e80ca51a77aL30-R36

dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/tracker/export/UserMapper.java
@@ -27,11 +27,12 @@
  */
 package org.hisp.dhis.webapi.controller.tracker.export;
 
+import org.hisp.dhis.program.UserInfoSnapshot;
 import org.hisp.dhis.webapi.controller.tracker.view.User;
 import org.mapstruct.Mapper;
 
 @Mapper
-public interface UserMapper extends ViewMapper<User, org.hisp.dhis.tracker.domain.User>
+public interface UserMapper extends ViewMapper<UserInfoSnapshot, User>
 {
-    org.hisp.dhis.tracker.domain.User from( User user );
+    User from( UserInfoSnapshot user );
 }

dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/tracker/view/User.java
@@ -27,8 +27,6 @@
  */
 package org.hisp.dhis.webapi.controller.tracker.view;
 
-import static org.apache.commons.lang3.StringUtils.isBlank;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -56,9 +54,4 @@ public class User
 
     @JsonProperty
     private String surname;
-
-    public boolean isEmpty()
-    {
-        return isBlank( uid ) && isBlank( username );
-    }
 }