f3c84c4e5bd1d44f15ae645c6bfcc63a971ac316
teleivo <teleivo@users.noreply.github.com>
2022-05-19 11:34:47 +0200

    chore: use lambda

--- dhis-2/dhis-services/dhis-service-tracker/src/main/java/org/hisp/dhis/tracker/preheat/supplier/ProgramStageInstanceProgramStageMapSupplier.java
+++ dhis-2/dhis-services/dhis-service-tracker/src/main/java/org/hisp/dhis/tracker/preheat/supplier/ProgramStageInstanceProgramStageMapSupplier.java
@@ -82,7 +82,7 @@ public class ProgramStageInstanceProgramStageMapSupplier
         List<String> notRepeatableProgramStageUids = params.getEvents().stream()
             .map( Event::getProgramStage )
             .filter( Objects::nonNull )
-            .map( ps -> preheat.getProgramStage( ps ) )
+            .map( preheat::getProgramStage )
             .filter( Objects::nonNull )
             .filter( ps -> !ps.getRepeatable() )
             .map( ProgramStage::getUid )
