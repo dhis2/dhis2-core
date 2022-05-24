d402df372873837af6416fc23ca5061c94f7759c
Lars Helge Øverland <lars@dhis2.org>
2022-05-24 21:29:06 +0200

    Update nonnull

--- dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/webdomain/DataValuesFollowUpRequest.java
+++ dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/webdomain/DataValuesFollowUpRequest.java
@@ -30,7 +30,6 @@ package org.hisp.dhis.webapi.webdomain;
 import java.util.List;
 
 import lombok.Data;
-import lombok.NonNull;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -40,7 +39,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 @Data
 public class DataValuesFollowUpRequest
 {
-    @NonNull
     @JsonProperty
     private List<DataValueFollowUpRequest> values;
 }
