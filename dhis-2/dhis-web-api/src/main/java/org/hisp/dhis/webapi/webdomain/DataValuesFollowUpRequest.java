bd73c31a5e4e9d4962cc95870c2bcbde13ae3d4b
Lars Helge Øverland <lars@dhis2.org>
2022-05-24 20:59:22 +0200

    Add attribute to DataValueFollowUpRequest

--- dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/webdomain/DataValuesFollowUpRequest.java
+++ dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/webdomain/DataValuesFollowUpRequest.java
@@ -30,6 +30,7 @@ package org.hisp.dhis.webapi.webdomain;
 import java.util.List;
 
 import lombok.Data;
+import lombok.NonNull;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -39,6 +40,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 @Data
 public class DataValuesFollowUpRequest
 {
+    @NonNull
     @JsonProperty
     private List<DataValueFollowUpRequest> values;
 }
