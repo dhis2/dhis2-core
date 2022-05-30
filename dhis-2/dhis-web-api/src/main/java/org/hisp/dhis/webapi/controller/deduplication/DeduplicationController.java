e9b627e18b537cfb6c9878e7bf0f66bea3c324c3
LUCA <lucacambi77@gmail.com>
2022-05-30 12:00:48 +0200

    fix: clean code

--- dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/deduplication/DeduplicationController.java
+++ dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/deduplication/DeduplicationController.java
@@ -218,10 +218,10 @@ public class DeduplicationController
         DeduplicationStatus deduplicationStatus )
         throws BadRequestException
     {
-        if ( deduplicationStatus.isForMerge() )
+        if ( deduplicationStatus == DeduplicationStatus.MERGED )
             throw new BadRequestException(
                 "Can't update a potential duplicate to " + DeduplicationStatus.MERGED.name() );
-        if ( potentialDuplicate.getStatus().isForMerge() )
+        if ( potentialDuplicate.getStatus() == DeduplicationStatus.MERGED )
             throw new BadRequestException( "Can't update a potential duplicate that is already "
                 + DeduplicationStatus.MERGED.name() );
     }
