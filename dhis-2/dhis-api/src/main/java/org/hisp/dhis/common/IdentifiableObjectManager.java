47202773b5c04a8116d5bc6fa543dbd3604e6f8a
Philip-Larsen-Donnelly <35666657+Philip-Larsen-Donnelly@users.noreply.github.com>
2022-06-01 10:33:48 +0200

    fix: Merge pull request from GHSA-52vp-f7hj-cj92

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/common/IdentifiableObjectManager.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/common/IdentifiableObjectManager.java
@@ -109,7 +109,10 @@ public interface IdentifiableObjectManager
 
     <T extends IdentifiableObject> List<T> getByUid( Class<T> clazz, Collection<String> uids );
 
-    <T extends IdentifiableObject> List<T> getByUid( Collection<Class<? extends IdentifiableObject>> classes,
+    <T extends IdentifiableObject> List<T> getAndValidateByUid( Class<T> type, Collection<String> uids )
+        throws IllegalQueryException;
+
+    <T extends IdentifiableObject> List<T> getByUid( Collection<Class<? extends IdentifiableObject>> types,
         Collection<String> uids );
 
     <T extends IdentifiableObject> List<T> getById( Class<T> clazz, Collection<Long> ids );
--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/feedback/ErrorCode.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/feedback/ErrorCode.java
@@ -48,6 +48,7 @@ public enum ErrorCode
     E1107( "Object type `{0}` is not translatable." ),
     E1108( "Could not add item to collection: {0}" ),
     E1109( "Could not remove item from collection: {0}" ),
+    E1112( "Object(s) of type `{0}` not found or not accessible: `{1}`" ),
 
     /* Org unit merge */
     E1500( "At least two source orgs unit must be specified" ),
