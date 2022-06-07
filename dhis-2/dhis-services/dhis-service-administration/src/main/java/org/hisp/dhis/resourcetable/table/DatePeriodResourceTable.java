aced9e6fab5f819ed35971fc0a3119cdf0923851
Lars Helge Øverland <lars@dhis2.org>
2022-06-07 10:39:37 +0200

    Update switch statement

--- dhis-2/dhis-services/dhis-service-administration/src/main/java/org/hisp/dhis/resourcetable/table/DatePeriodResourceTable.java
+++ dhis-2/dhis-services/dhis-service-administration/src/main/java/org/hisp/dhis/resourcetable/table/DatePeriodResourceTable.java
@@ -92,8 +92,8 @@ public class DatePeriodResourceTable
 
         // TODO Create a dynamic solution instead of having fixed dates
 
-        Date startDate = new Cal( OLDEST_YEAR_PERIOD_SUPPORTED, 1, 1, true ).time();
-        Date endDate = new Cal( NEWEST_YEAR_PERIOD_SUPPORTED + 1, 1, 1, true ).time();
+        Date startDate = new Cal( FIRST_SUPPORTED_YEAR, 1, 1, true ).time();
+        Date endDate = new Cal( LATEST_YEAR_SUPPORTED + 1, 1, 1, true ).time();
 
         List<Period> dailyPeriods = new DailyPeriodType().generatePeriods( startDate, endDate );
 
