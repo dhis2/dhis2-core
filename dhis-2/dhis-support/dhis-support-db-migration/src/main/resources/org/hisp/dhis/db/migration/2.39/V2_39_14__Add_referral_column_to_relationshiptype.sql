a28f0b40087ad04d8f70dcb50d3b0d3d535d85d0
Enrico Colasante <enrico@dhis2.org>
2022-06-07 10:12:52 +0200

    Fix code review comment

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.39/V2_39_14__Add_referral_column_to_relationshiptype.sql
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.39/V2_39_14__Add_referral_column_to_relationshiptype.sql
@@ -1,6 +1,2 @@
-ALTER TABLE relationshiptype ADD COLUMN IF NOT EXISTS referral BOOLEAN;
-
-UPDATE relationshiptype SET referral = FALSE WHERE referral IS NULL;
-
-ALTER TABLE relationshiptype ALTER COLUMN referral SET NOT NULL;
+ALTER TABLE relationshiptype ADD COLUMN IF NOT EXISTS referral BOOLEAN DEFAULT false;
 
