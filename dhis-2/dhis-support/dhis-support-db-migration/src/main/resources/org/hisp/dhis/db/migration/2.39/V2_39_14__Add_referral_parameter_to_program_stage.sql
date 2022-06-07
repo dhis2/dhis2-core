134f186519ddbde8f87bae33a46b0261ae9cc975
zubaira <zubair@dhis2.org>
2022-06-07 10:17:08 +0200

    migration script changes

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.39/V2_39_14__Add_referral_parameter_to_program_stage.sql
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.39/V2_39_14__Add_referral_parameter_to_program_stage.sql
@@ -1,10 +1,4 @@
 -- https://jira.dhis2.org/browse/DHIS2-13301
 -- Adding referral parameter to ProgramStage
 
-ALTER TABLE programstage
-    ADD COLUMN if not exists referral boolean;
-
-UPDATE programstage SET referral = false WHERE referral IS NULL;
-
-ALTER TABLE programstage
-    ALTER COLUMN referral SET NOT NULL;
+ALTER TABLE programstage ADD COLUMN IF NOT EXISTS referral BOOLEAN DEFAULT false;
