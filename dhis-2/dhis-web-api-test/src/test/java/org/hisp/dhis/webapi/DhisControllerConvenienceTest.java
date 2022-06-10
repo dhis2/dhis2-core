197319cbde34a1d703f555f062742c4654d61d91
netroms <msvanaes@dhis2.org>
2022-06-10 12:54:03 +0800

    fix: Upgrade to latest Spring version (#10980)
    * fix: Upgrade to latest Spring version (#10394)

--- dhis-2/dhis-web-api-test/src/test/java/org/hisp/dhis/webapi/DhisControllerConvenienceTest.java
+++ dhis-2/dhis-web-api-test/src/test/java/org/hisp/dhis/webapi/DhisControllerConvenienceTest.java
@@ -63,7 +63,8 @@ import org.springframework.web.filter.CharacterEncodingFilter;
  */
 @RunWith( SpringRunner.class )
 @WebAppConfiguration
-@ContextConfiguration( classes = { MvcTestConfig.class, WebTestConfiguration.class } )
+@ContextConfiguration( classes = { WebTestDataSourceConfiguration.class, MvcTestConfig.class,
+    WebTestConfiguration.class } )
 @ActiveProfiles( "test-h2" )
 @Transactional
 public abstract class DhisControllerConvenienceTest extends DhisConvenienceTest implements WebClient
