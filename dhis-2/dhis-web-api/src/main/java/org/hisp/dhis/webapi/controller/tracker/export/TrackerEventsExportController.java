2cbb406e99fd598cb59bbc9699a6ea45c321fb77
LUCA <lucacambi77@gmail.com>
2022-06-16 09:20:00 +0200

    fix: remove circular dependency for events

--- dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/tracker/export/TrackerEventsExportController.java
+++ dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/tracker/export/TrackerEventsExportController.java
@@ -223,7 +223,8 @@ public class TrackerEventsExportController
         throws NotFoundException
     {
 
-        Event event = eventService.getEvent( programStageInstanceService.getProgramStageInstance( uid ) );
+        Event event = eventService.getEvent( programStageInstanceService.getProgramStageInstance( uid ),
+            teiParams.isIncludeRelationships() );
         if ( event == null )
         {
             throw new NotFoundException( "Event", uid );
