a0df341d95f4788dda558da5433acac69c02b1ce
Enrico Colasante <enrico@dhis2.org>
2022-06-27 16:25:53 +0200

    Fix test

--- dhis-2/dhis-test-integration/src/test/java/org/hisp/dhis/dataanalysis/MinMaxOutlierAnalysisServiceTest.java
+++ dhis-2/dhis-test-integration/src/test/java/org/hisp/dhis/dataanalysis/MinMaxOutlierAnalysisServiceTest.java
@@ -49,13 +49,13 @@ import org.hisp.dhis.organisationunit.OrganisationUnit;
 import org.hisp.dhis.organisationunit.OrganisationUnitService;
 import org.hisp.dhis.period.MonthlyPeriodType;
 import org.hisp.dhis.period.Period;
-import org.hisp.dhis.test.integration.TransactionalIntegrationTest;
+import org.hisp.dhis.test.integration.IntegrationTestBase;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.google.common.collect.Lists;
 
-class MinMaxOutlierAnalysisServiceTest extends TransactionalIntegrationTest
+class MinMaxOutlierAnalysisServiceTest extends IntegrationTestBase
 {
 
     @Autowired
