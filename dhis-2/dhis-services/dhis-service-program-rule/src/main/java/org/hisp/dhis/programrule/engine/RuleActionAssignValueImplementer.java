cbba8710a6d7fa7d82a5c4a4b5a22308df19d20d
Enrico Colasante <enrico@dhis2.org>
2022-07-11 11:31:32 +0200

    fix: Move assign rule log to debug [DHIS2-12854] (#10130) (#11268)

--- dhis-2/dhis-services/dhis-service-program-rule/src/main/java/org/hisp/dhis/programrule/engine/RuleActionAssignValueImplementer.java
+++ dhis-2/dhis-services/dhis-service-program-rule/src/main/java/org/hisp/dhis/programrule/engine/RuleActionAssignValueImplementer.java
@@ -126,7 +126,7 @@ public class RuleActionAssignValueImplementer implements RuleActionImplementer
             variable = matcher.group( 0 ).trim();
         }
 
-        log.info( "Assigning: " + variable + " with value: " + value );
+        log.debug( "Assigning: " + variable + " with value: " + value );
 
         if ( !variableMap.containsKey( programInstance.getUid() ) )
         {
