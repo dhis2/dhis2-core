695bffc21714df923594f4e96570a8a86096739d
Morten Hansen <morten@winterop.com>
2022-08-02 12:05:02 +0700

    fix: add audit _after_ creation

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/datavalue/DefaultDataValueService.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/datavalue/DefaultDataValueService.java
@@ -138,14 +138,6 @@ public class DefaultDataValueService
 
         DataValue softDelete = dataValueStore.getSoftDeletedDataValue( dataValue );
 
-        if ( config.isEnabled( CHANGELOG_AGGREGATE ) )
-        {
-            DataValueAudit dataValueAudit = new DataValueAudit( dataValue, dataValue.getAuditValue(),
-                dataValue.getStoredBy(), AuditType.CREATE );
-
-            dataValueAuditService.addDataValueAudit( dataValueAudit );
-        }
-
         if ( softDelete != null )
         {
             softDelete.mergeWith( dataValue );
@@ -156,6 +148,14 @@ public class DefaultDataValueService
         else
         {
             dataValueStore.addDataValue( dataValue );
+        }
+
+        if ( config.isEnabled( CHANGELOG_AGGREGATE ) )
+        {
+            DataValueAudit dataValueAudit = new DataValueAudit( dataValue, dataValue.getAuditValue(),
+                    dataValue.getStoredBy(), AuditType.CREATE );
+
+            dataValueAuditService.addDataValueAudit( dataValueAudit );
         }
 
         return true;
