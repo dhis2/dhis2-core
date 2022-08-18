10795cecf2b5be8671aee12a1047206ded01de2d
Jan Bernitt <jaanbernitt@gmail.com>
2022-08-18 09:51:12 +0200

    chore: revert changes in user [DHIS2-9761]

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/user/User.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/user/User.java
@@ -27,8 +27,6 @@
  */
 package org.hisp.dhis.user;
 
-import static java.util.Collections.emptySet;
-import static java.util.Collections.unmodifiableSet;
 import static org.springframework.beans.BeanUtils.copyProperties;
 
 import java.util.ArrayList;
@@ -289,20 +287,16 @@ public class User
      */
     public Set<String> getAllAuthorities()
     {
-        if ( userRoles.isEmpty() )
-        {
-            return emptySet();
-        }
-        if ( userRoles.size() == 1 )
-        {
-            return unmodifiableSet( userRoles.iterator().next().getAuthorities() );
-        }
         Set<String> authorities = new HashSet<>();
+
         for ( UserRole group : userRoles )
         {
             authorities.addAll( group.getAuthorities() );
         }
-        return Collections.unmodifiableSet( authorities );
+
+        authorities = Collections.unmodifiableSet( authorities );
+
+        return authorities;
     }
 
     /**
