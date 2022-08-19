083f7dbcefe3bd052056e1c515263948155f0745
teleivo <teleivo@users.noreply.github.com>
2022-08-19 09:30:09 +0200

    test: enable DataValueSetServiceIntegrationTest

--- dhis-2/dhis-test-integration/src/test/java/org/hisp/dhis/dxf2/datavalueset/DataValueSetServiceIntegrationTest.java
+++ dhis-2/dhis-test-integration/src/test/java/org/hisp/dhis/dxf2/datavalueset/DataValueSetServiceIntegrationTest.java
@@ -56,7 +56,6 @@ import org.hisp.dhis.period.PeriodType;
 import org.hisp.dhis.test.integration.IntegrationTestBase;
 import org.hisp.dhis.user.User;
 import org.hisp.dhis.user.UserService;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
@@ -66,7 +65,6 @@ import com.google.common.collect.Sets;
 /**
  * @author Lars Helge Overland
  */
-@Disabled( "Disabled to unclog GitHub PRs. Currently being worked on. " )
 class DataValueSetServiceIntegrationTest extends IntegrationTestBase
 {
     @Autowired
