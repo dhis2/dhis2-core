2166fdfe8504b215e3fededde359b832bb9ae234
Viet Nguyen <viet@dhis2.org>
2022-09-01 00:01:00 +0700

    fix: import user with legacy UserCredential

--- dhis-2/dhis-test-integration/src/test/java/org/hisp/dhis/dxf2/metadata/MetadataImportServiceTest.java
+++ dhis-2/dhis-test-integration/src/test/java/org/hisp/dhis/dxf2/metadata/MetadataImportServiceTest.java
@@ -823,6 +823,19 @@ class MetadataImportServiceTest extends TransactionalIntegrationTest
     }
 
     @Test
+    void testImportUserLegacyFormat()
+        throws IOException
+    {
+        Map<Class<? extends IdentifiableObject>, List<IdentifiableObject>> metadata = renderService.fromMetadata(
+            new ClassPathResource( "dxf2/create_user_with_legacy_format.json" ).getInputStream(),
+            RenderFormat.JSON );
+        MetadataImportParams params = createParams( ImportStrategy.CREATE_AND_UPDATE, metadata );
+        ImportReport report = importService.importMetadata( params );
+        assertEquals( Status.OK, report.getStatus() );
+        assertNotNull( manager.get( User.class, "sPWjoHSY03y" ) );
+    }
+
+    @Test
     void testImportMapCreateAndUpdate()
         throws IOException
     {
