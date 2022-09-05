3475e328bda3b47181c089bc8a5e87984f8ee0bf
teleivo <teleivo@users.noreply.github.com>
2022-09-05 14:58:32 +0200

    chore: split old and new tracker GET events param processing (#11653)
    so we can evolve /tracker/events without affecting the old tracker event
    endpoint

--- dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/tracker/export/TrackerEventsExportController.java
+++ dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/tracker/export/TrackerEventsExportController.java
@@ -56,7 +56,6 @@ import org.hisp.dhis.dxf2.webmessage.WebMessageException;
 import org.hisp.dhis.fieldfiltering.FieldFilterService;
 import org.hisp.dhis.node.Preset;
 import org.hisp.dhis.program.ProgramStageInstanceService;
-import org.hisp.dhis.webapi.controller.event.mapper.RequestToSearchParamsMapper;
 import org.hisp.dhis.webapi.controller.event.webrequest.PagingWrapper;
 import org.hisp.dhis.webapi.controller.event.webrequest.tracker.TrackerEventCriteria;
 import org.hisp.dhis.webapi.controller.exception.NotFoundException;
@@ -95,7 +94,7 @@ public class TrackerEventsExportController
     private final ContextService contextService;
 
     @NonNull
-    private final RequestToSearchParamsMapper requestToSearchParamsMapper;
+    private final EventRequestToSearchParamsMapper requestToSearchParams;
 
     @NonNull
     private final ProgramStageInstanceService programStageInstanceService;
@@ -113,7 +112,7 @@ public class TrackerEventsExportController
         throws WebMessageException
     {
 
-        EventSearchParams eventSearchParams = requestToSearchParamsMapper.map( eventCriteria );
+        EventSearchParams eventSearchParams = requestToSearchParams.map( eventCriteria );
 
         if ( areAllEnrollmentsInvalid( eventCriteria, eventSearchParams ) )
         {
@@ -156,7 +155,7 @@ public class TrackerEventsExportController
             fields.addAll( Preset.ALL.getFields() );
         }
 
-        EventSearchParams eventSearchParams = requestToSearchParamsMapper.map( eventCriteria );
+        EventSearchParams eventSearchParams = requestToSearchParams.map( eventCriteria );
 
         if ( areAllEnrollmentsInvalid( eventCriteria, eventSearchParams ) )
         {
