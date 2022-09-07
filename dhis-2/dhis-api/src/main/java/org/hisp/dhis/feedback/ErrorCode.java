7abe888ec604a8323cb9c74783568dd023871b10
Viet Nguyen <viet@dhis2.org>
2022-09-07 12:47:42 +0700

    Update ErrorCode.java

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/feedback/ErrorCode.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/feedback/ErrorCode.java
@@ -171,7 +171,7 @@ public enum ErrorCode
     E3023( "Invalid 2FA code" ),
     E3024( "Not allowed to disable 2FA" ),
     E3025( "User must have a secret" ),
-    E3026( "Couldn't generate QR code" ),
+    E3026( "Could not generate QR code" ),
     E3027( "No currentUser available" ),
 
     /* Metadata Validation */
