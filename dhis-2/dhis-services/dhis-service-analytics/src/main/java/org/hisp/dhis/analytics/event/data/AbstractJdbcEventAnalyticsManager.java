f0d3ab063845b759ebc46816c0b263fcaa0ba0f6
Dusan Bernat <mail@it-bernat.de>
2022-09-07 10:27:45 +0200

    fix: Program indicators filter operands in event reports [DHIS2-12490] (#11649)
    * fix:Filter program indicators in event reports [2.40-DHIS2-12490]
    
    * clean coding
    
    * clean coding
    
    * clean coding

--- dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/event/data/AbstractJdbcEventAnalyticsManager.java
+++ dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/event/data/AbstractJdbcEventAnalyticsManager.java
@@ -795,8 +795,22 @@ public abstract class AbstractJdbcEventAnalyticsManager
         if ( item.isProgramIndicator() )
         {
             ProgramIndicator programIndicator = (ProgramIndicator) item.getItem();
-            return programIndicatorService.getAnalyticsSql( programIndicator.getExpression(), NUMERIC, programIndicator,
+
+            return programIndicatorService.getAnalyticsSql( programIndicator.getExpression(), NUMERIC,
+                programIndicator,
                 startDate, endDate );
+        }
+        else
+        {
+            return filter.getSqlFilterColumn( getColumn( item ), item.getValueType() );
+        }
+    }
+
+    protected String getSelectSql( QueryFilter filter, QueryItem item, EventQueryParams params )
+    {
+        if ( item.isProgramIndicator() )
+        {
+            return getColumnAndAlias( item, params, false, false ).getColumn();
         }
         else
         {
@@ -1123,8 +1137,9 @@ public abstract class AbstractJdbcEventAnalyticsManager
      */
     private String toSql( QueryItem item, QueryFilter filter, EventQueryParams params )
     {
-        String field = getSelectSql( filter, item, params.getEarliestStartDate(),
-            params.getLatestEndDate() );
+        String field = item.hasAggregationType() ? getSelectSql( filter, item, params )
+            : getSelectSql( filter, item, params.getEarliestStartDate(),
+                params.getLatestEndDate() );
 
         if ( IN.equals( filter.getOperator() ) )
         {
