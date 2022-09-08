74751034f7e4f79c0e287395f66bd91cc2f513ba
Jan Bernitt <jaanbernitt@gmail.com>
2022-09-08 13:44:53 +0200

    fix: user settings value by key set header accept all [DHIS2-13711]

--- dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/UserSettingController.java
+++ dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/UserSettingController.java
@@ -115,7 +115,7 @@ public class UserSettingController
         return result;
     }
 
-    @GetMapping( value = "/{key}", produces = "text/plain" )
+    @GetMapping( value = "/{key}" )
     public String getUserSettingByKey(
         @PathVariable( value = "key" ) String key,
         @RequestParam( required = false, defaultValue = "true" ) boolean useFallback,
@@ -131,6 +131,7 @@ public class UserSettingController
             .get( key );
 
         response.setHeader( ContextUtils.HEADER_CACHE_CONTROL, CacheControl.noCache().cachePrivate().getHeaderValue() );
+        response.setHeader( "Content-Type", ContextUtils.CONTENT_TYPE_TEXT );
         return String.valueOf( value );
     }
 
