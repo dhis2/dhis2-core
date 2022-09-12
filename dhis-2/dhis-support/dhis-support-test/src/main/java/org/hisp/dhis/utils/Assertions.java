e76b881d0b4379a71fa9fd2ebb9253fa8399f870
teleivo <teleivo@users.noreply.github.com>
2022-09-12 11:09:19 +0200

    test: use suppliers in assertContainsOnly
    and fix 2 sonar issues of redundant call to toString()

--- dhis-2/dhis-support/dhis-support-test/src/main/java/org/hisp/dhis/utils/Assertions.java
+++ dhis-2/dhis-support/dhis-support-test/src/main/java/org/hisp/dhis/utils/Assertions.java
@@ -62,7 +62,7 @@ public final class Assertions
      */
     public static <E> void assertContainsOnly( Collection<E> expected, Collection<E> actual )
     {
-        assertNotNull( actual, String.format( "Expected collection to contain %s, got null instead", expected ) );
+        assertNotNull( actual, () -> String.format( "Expected collection to contain %s, got null instead", expected ) );
 
         List<E> missing = CollectionUtils.difference( expected, actual );
         List<E> extra = CollectionUtils.difference( actual, expected );
@@ -70,7 +70,7 @@ public final class Assertions
             () -> assertTrue( missing.isEmpty(), () -> String.format( "Expected %s to be in %s", missing, actual ) ),
             () -> assertTrue( extra.isEmpty(), () -> String.format( "Expected %s NOT to be in %s", extra, actual ) ),
             () -> assertEquals( expected.size(), actual.size(),
-                String.format( "Expected %s or actual %s contains unexpected duplicates", expected, actual ) ) );
+                () -> String.format( "Expected %s or actual %s contains unexpected duplicates", expected, actual ) ) );
     }
 
     public static <K, V> void assertMapEquals( Map<K, V> expected, Map<K, V> actual )
@@ -78,12 +78,12 @@ public final class Assertions
         for ( Map.Entry<K, V> e : expected.entrySet() )
         {
             assertEquals( e.getValue(), actual.get( e.getKey() ),
-                String.format( "Expected value not found in %s", actual.toString() ) );
+                String.format( "Expected value not found in %s", actual ) );
         }
         for ( Map.Entry<K, V> e : actual.entrySet() )
         {
             assertEquals( e.getValue(), expected.get( e.getKey() ),
-                String.format( "Did not expect value in %s", actual.toString() ) );
+                String.format( "Did not expect value in %s", actual ) );
         }
     }
 
