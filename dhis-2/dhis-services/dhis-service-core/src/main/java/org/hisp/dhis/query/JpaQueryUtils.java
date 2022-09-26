56bc6927ddb334218ca661d0f9e8fb8e462658f7
luca <lucacambi77@gmail.com>
2022-09-26 10:40:24 +0200

    fix: removed condition

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/query/JpaQueryUtils.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/query/JpaQueryUtils.java
@@ -396,6 +396,7 @@ public class JpaQueryUtils
             : "{" + String.join( ",", userGroupIds ) + "}";
 
         String sql = " ( %1$s->>'owner' is null or %1$s->>'owner' = '%2$s') "
+            + " or %1$s->>'public' like '%4$s' or %1$s->>'public' is null "
             + " or (" + JsonbFunctions.HAS_USER_ID + "( %1$s, '%2$s') = true "
             + " and " + JsonbFunctions.CHECK_USER_ACCESS + "( %1$s, '%2$s', '%4$s' ) = true )  "
             + (StringUtils.isEmpty( groupsIds ) ? ""
