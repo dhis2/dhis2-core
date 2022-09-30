506b91f3ade29974374532cf8142ce57e133035f
Morten Svanaes <msvanaes@dhis2.org>
2022-09-30 18:32:49 +0800

    fix: Inconsistent boolean parsing OIDC config

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/security/oidc/provider/GenericOidcProviderBuilder.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/security/oidc/provider/GenericOidcProviderBuilder.java
@@ -35,6 +35,8 @@ import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.tuple.Pair;
+
+import org.hisp.dhis.external.conf.DhisConfigurationProvider;
 import org.hisp.dhis.security.oidc.DhisOidcClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.core.AuthenticationMethod;
@@ -117,13 +119,13 @@ public class GenericOidcProviderBuilder extends AbstractOidcProvider
 
         metadata.put( EXTRA_REQUEST_PARAMETERS, getExtraRequestParameters( config ) );
 
-        if ( Boolean.parseBoolean( config.get( ENABLE_LOGOUT ) ) )
+        if ( DhisConfigurationProvider.isOn( config.get( ENABLE_LOGOUT ) ) )
         {
             metadata.put( END_SESSION_ENDPOINT,
                 StringUtils.defaultIfEmpty( config.get( END_SESSION_ENDPOINT ), "" ) );
         }
 
-        if ( Boolean.parseBoolean( config.get( ENABLE_PKCE ) ) )
+        if ( DhisConfigurationProvider.isOn( config.get( ENABLE_PKCE ) ) )
         {
             metadata.put( ENABLE_PKCE, Boolean.TRUE.toString() );
         }
