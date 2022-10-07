c75b8994fead576d4fbf18ec0a7dc970438ab7d6
Viet Nguyen <viet@dhis2.org>
2022-10-07 14:22:14 +0700

    code styles

--- dhis-2/dhis-web/dhis-web-api-test/src/test/java/org/hisp/dhis/webapi/controller/metadata/CrudControllerPagingTest.java
+++ dhis-2/dhis-web/dhis-web-api-test/src/test/java/org/hisp/dhis/webapi/controller/metadata/CrudControllerPagingTest.java
@@ -1,4 +1,33 @@
+/*
+ * Copyright (c) 2004-2022, University of Oslo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * Neither the name of the HISP project nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package org.hisp.dhis.webapi.controller.metadata;
+
+import static org.junit.Assert.assertEquals;
 
 import org.hisp.dhis.organisationunit.OrganisationUnitService;
 import org.hisp.dhis.webapi.DhisControllerConvenienceTest;
@@ -8,8 +37,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-
-import static org.junit.Assert.assertEquals;
 
 public class CrudControllerPagingTest extends DhisControllerConvenienceTest
 {
@@ -51,8 +78,8 @@ public class CrudControllerPagingTest extends DhisControllerConvenienceTest
     {
         JsonList<JsonOrganisationUnit> ous = GET(
             "/organisationUnits?order=displayName&paging=true&pageSize=2&page=2" )
-            .content( HttpStatus.OK )
-            .getList( "organisationUnits", JsonOrganisationUnit.class );
+                .content( HttpStatus.OK )
+                .getList( "organisationUnits", JsonOrganisationUnit.class );
         assertEquals( 2, ous.size() );
         assertEquals( "C", ous.get( 0 ).getDisplayName() );
         assertEquals( "D", ous.get( 1 ).getDisplayName() );
@@ -63,8 +90,8 @@ public class CrudControllerPagingTest extends DhisControllerConvenienceTest
     {
         JsonList<JsonOrganisationUnit> ous = GET(
             "/organisationUnits?order=displayName:desc&paging=true&pageSize=2&page=2" )
-            .content( HttpStatus.OK )
-            .getList( "organisationUnits", JsonOrganisationUnit.class );
+                .content( HttpStatus.OK )
+                .getList( "organisationUnits", JsonOrganisationUnit.class );
         assertEquals( 2, ous.size() );
         assertEquals( "C", ous.get( 0 ).getDisplayName() );
         assertEquals( "B", ous.get( 1 ).getDisplayName() );
@@ -75,7 +102,7 @@ public class CrudControllerPagingTest extends DhisControllerConvenienceTest
     {
         JsonList<JsonOrganisationUnit> ous = GET(
             "/organisationUnits?filter=displayName:in:[A,B,C]&paging=true&pageSize=2&page=2" ).content( HttpStatus.OK )
-            .getList( "organisationUnits", JsonOrganisationUnit.class );
+                .getList( "organisationUnits", JsonOrganisationUnit.class );
         assertEquals( 1, ous.size() );
         assertEquals( "C", ous.get( 0 ).getDisplayName() );
     }
@@ -96,7 +123,7 @@ public class CrudControllerPagingTest extends DhisControllerConvenienceTest
     {
         JsonList<JsonOrganisationUnit> ous = GET(
             "/organisationUnits?filter=name:in:[A,B,C]&paging=true&pageSize=2&page=2" ).content( HttpStatus.OK )
-            .getList( "organisationUnits", JsonOrganisationUnit.class );
+                .getList( "organisationUnits", JsonOrganisationUnit.class );
         assertEquals( 1, ous.size() );
         assertEquals( "C", ous.get( 0 ).getDisplayName() );
     }
