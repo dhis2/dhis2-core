3299f18ac8d3efb3cd3bb57ba253de13e15a5f88
teleivo <teleivo@users.noreply.github.com>
2022-10-10 09:06:56 +0200

    ci: fix linting in build-dev
    and allow running from any dir

--- dhis-2/build-dev.sh
+++ dhis-2/build-dev.sh
@@ -22,8 +22,6 @@ IMAGE=dhis2/core-dev
 TAG=local
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-ROOT="$DIR/.."
-ARTIFACTS="$ROOT/docker/artifacts"
 
 print() {
     echo -e "\033[1m$1\033[0m" 1>&2
@@ -32,15 +30,15 @@ print() {
 print "Building dhis2-core and Docker image..."
 
 export MAVEN_OPTS="-Dhttp.keepAlive=false -Dmaven.wagon.http.pool=false -Dmaven.wagon.http.retryHandler.class=standard -Dmaven.wagon.http.retryHandler.count=3 -Dmaven.wagon.httpconnectionManager.ttlSeconds=25"
-mvn clean install --threads 2C -DskipTests -Dmaven.test.skip=true -f dhis-2/pom.xml -pl -dhis-web-embedded-jetty,-dhis-test-integration,-dhis-test-coverage
-mvn clean install --threads 2C -DskipTests -Dmaven.test.skip=true -f dhis-2/dhis-web/pom.xml
-mvn -DskipTests -Dmaven.test.skip=true -f dhis-2/dhis-web/dhis-web-portal/pom.xml jib:dockerBuild
+mvn clean install --threads 2C -DskipTests -Dmaven.test.skip=true -f "${DIR}/pom.xml" -pl -dhis-web-embedded-jetty,-dhis-test-integration,-dhis-test-coverage
+mvn clean install --threads 2C -DskipTests -Dmaven.test.skip=true -f "${DIR}/dhis-web/pom.xml"
+mvn -DskipTests -Dmaven.test.skip=true -f "${DIR}/dhis-web/dhis-web-portal/pom.xml" jib:dockerBuild
 
-if test -z $D2CLUSTER; then
+if test -z "$D2CLUSTER"; then
     print "No cluster name specified, skipping deploy"
 else
     print "Deploying to d2 cluster $D2CLUSTER..."
 
-    d2 cluster up $D2CLUSTER --image $IMAGE:$TAG
-    d2 cluster logs $D2CLUSTER
+    d2 cluster up "$D2CLUSTER" --image $IMAGE:$TAG
+    d2 cluster logs "$D2CLUSTER"
 fi
