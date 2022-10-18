34923cd12e04caa9230d6a9c6fa10451190d8bcf
Abyot Asalefew Gizaw <abyota@gmail.com>
2022-10-18 15:12:43 +0200

    fix: Keep event datavalue dates if value is not changed [2.38-DHIS2-13632] (#12108)
    * fix: Keep event datavalue dates if value is not changed
    
    * test: Ensure eventdatavalue created dates are not changed on update

--- dhis-2/dhis-services/dhis-service-tracker/src/main/java/org/hisp/dhis/tracker/bundle/persister/EventPersister.java
+++ dhis-2/dhis-services/dhis-service-tracker/src/main/java/org/hisp/dhis/tracker/bundle/persister/EventPersister.java
@@ -182,14 +182,12 @@ public class EventPersister extends AbstractTrackerPersister<Event, ProgramStage
             // EventDataValue.dataElement contains a UID
             EventDataValue eventDataValue = dataValueDBMap.get( dataElement.getUid() );
 
-            ValuesHolder valuesHolder = getAuditParameters( eventDataValue, dv );
+            ValuesHolder valuesHolder = getAuditAndDateParameters( eventDataValue, dv );
 
             eventDataValue = valuesHolder.getEventDataValue();
 
             eventDataValue.setDataElement( dataElement.getUid() );
             eventDataValue.setStoredBy( dv.getStoredBy() );
-
-            handleDataValueCreatedUpdatedDates( dv, eventDataValue );
 
             if ( StringUtils.isEmpty( dv.getValue() ) )
             {
@@ -216,12 +214,6 @@ public class EventPersister extends AbstractTrackerPersister<Event, ProgramStage
             logTrackedEntityDataValueHistory( preheat.getUsername(), dataElement, psi,
                 new Date(), valuesHolder );
         } );
-    }
-
-    private void handleDataValueCreatedUpdatedDates( DataValue dv, EventDataValue eventDataValue )
-    {
-        eventDataValue.setCreated( getFromOrNewDate( dv, DataValue::getCreatedAt ) );
-        eventDataValue.setLastUpdated( getFromOrNewDate( dv, DataValue::getUpdatedAt ) );
     }
 
     private Date getFromOrNewDate( DataValue dv, Function<DataValue, Instant> dateGetter )
@@ -281,7 +273,7 @@ public class EventPersister extends AbstractTrackerPersister<Event, ProgramStage
         return !StringUtils.equals( dv.getValue(), eventDataValue.getValue() );
     }
 
-    private ValuesHolder getAuditParameters( EventDataValue eventDataValue, DataValue dv )
+    private ValuesHolder getAuditAndDateParameters( EventDataValue eventDataValue, DataValue dv )
     {
         String persistedValue;
 
@@ -290,6 +282,8 @@ public class EventPersister extends AbstractTrackerPersister<Event, ProgramStage
         if ( isNewDataValue( eventDataValue, dv ) )
         {
             eventDataValue = new EventDataValue();
+            eventDataValue.setCreated( getFromOrNewDate( dv, DataValue::getCreatedAt ) );
+            eventDataValue.setLastUpdated( getFromOrNewDate( dv, DataValue::getUpdatedAt ) );
             persistedValue = dv.getValue();
             auditType = AuditType.CREATE;
         }
@@ -300,11 +294,13 @@ public class EventPersister extends AbstractTrackerPersister<Event, ProgramStage
             if ( isUpdate( eventDataValue, dv ) )
             {
                 auditType = AuditType.UPDATE;
+                eventDataValue.setLastUpdated( getFromOrNewDate( dv, DataValue::getUpdatedAt ) );
             }
 
             if ( isDeletion( eventDataValue, dv ) )
             {
                 auditType = AuditType.DELETE;
+                eventDataValue.setLastUpdated( getFromOrNewDate( dv, DataValue::getUpdatedAt ) );
             }
         }
 
