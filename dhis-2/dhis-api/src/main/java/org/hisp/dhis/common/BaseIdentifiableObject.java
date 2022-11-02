722e9fa64919b8e9d32d70ec92a8be9ff6be2775
Jan Bernitt <jaanbernitt@gmail.com>
2022-11-02 09:22:33 +0100

    fix: revert accidental commit [DHIS2-7836]

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/common/BaseIdentifiableObject.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/common/BaseIdentifiableObject.java
@@ -61,7 +61,6 @@ import org.hisp.dhis.user.CurrentUserUtil;
 import org.hisp.dhis.user.User;
 import org.hisp.dhis.user.UserSettingKey;
 import org.hisp.dhis.user.sharing.Sharing;
-import org.hisp.dhis.util.AutoCorrectedStringDeserializer;
 import org.hisp.dhis.util.SharingUtils;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -281,7 +280,6 @@ public class BaseIdentifiableObject
 
     @Override
     @JsonProperty
-    @JsonDeserialize( using = AutoCorrectedStringDeserializer.class )
     @JacksonXmlProperty( isAttribute = true )
     @Description( "The name of this Object. Required and unique." )
     @PropertyRange( min = 1 )
