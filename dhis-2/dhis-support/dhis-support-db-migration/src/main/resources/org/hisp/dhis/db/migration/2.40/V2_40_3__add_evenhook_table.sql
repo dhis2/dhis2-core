a49d81d71b0e7b547e3cc26ad59d311dc48f73c4
Morten Hansen <morten@winterop.com>
2022-11-25 13:10:36 +0700

    feat: support EventHook pub/sub

---
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.40/V2_40_3__add_evenhook_table.sql
@@ -0,0 +1,29 @@
+
+-- Add eventhook table
+
+drop table if exists eventhook;
+
+create table eventhook (
+    eventhookid int8 not null,
+    uid varchar(11) null,
+    code varchar(100) null,
+    created timestamp null,
+    userid int8 null,
+    lastupdated timestamp null,
+    lastupdatedby int8 null,
+    translations jsonb null,
+    sharing jsonb null default '{}'::jsonb,
+    attributevalues jsonb null default '{}'::jsonb,
+    name varchar(230) not null,
+    constraint eventhook_uid_key unique (uid),
+    constraint eventhook_code_key unique (code),
+    constraint eventhook_name_key unique (name),
+    constraint eventhook_pkey primary key (eventhookid)
+);
+
+-- Add eventhook foreign keys
+
+alter table eventhook add constraint fk_eventhook_userid_userinfoid
+    foreign key (userid) references userinfo(userinfoid);
+alter table eventhook add constraint fk_eventhook_lastupdateby_userinfoid
+    foreign key (lastupdatedby) references userinfo(userinfoid);
