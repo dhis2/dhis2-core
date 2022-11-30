281312a56be42430a8e5e3f8b60f0303d3f1d9ba
maikelarabori <maikelarabori@gmail.com>
2022-11-30 14:12:17 +0100

    fix: Javadoc typo [DHIS2-14208]

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/analytics/AnalyticsExportSettings.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/analytics/AnalyticsExportSettings.java
@@ -74,7 +74,7 @@ public class AnalyticsExportSettings
      * exported. Two types are supported: UNLOGGED and REGULAR. See
      * {@link AnalyticsTableType}
      *
-     * @return the string representing of {@link AnalyticsTableType}.
+     * @return the string representation of {@link AnalyticsTableType}.
      */
     public String getTableType()
     {
