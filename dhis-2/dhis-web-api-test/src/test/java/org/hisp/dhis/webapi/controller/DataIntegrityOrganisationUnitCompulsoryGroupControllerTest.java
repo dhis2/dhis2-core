9b4cfa81ee4f9c55649fbd22ba6fbaaca254c73e
Jason Pickering <jason.p.pickering@gmail.com>
2022-12-06 06:26:27 +0100

    Cleanup

--- dhis-2/dhis-web-api-test/src/test/java/org/hisp/dhis/webapi/controller/DataIntegrityOrganisationUnitCompulsoryGroupControllerTest.java
+++ dhis-2/dhis-web-api-test/src/test/java/org/hisp/dhis/webapi/controller/DataIntegrityOrganisationUnitCompulsoryGroupControllerTest.java
@@ -25,8 +25,11 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
 package org.hisp.dhis.webapi.controller;
+
+import static org.hisp.dhis.web.WebClientUtils.assertStatus;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.hisp.dhis.web.HttpStatus;
 import org.hisp.dhis.webapi.DhisControllerIntegrationTest;
@@ -35,30 +38,34 @@ import org.hisp.dhis.webapi.json.domain.JsonIdentifiableObject;
 import org.hisp.dhis.webapi.json.domain.JsonWebMessage;
 import org.junit.jupiter.api.Test;
 
-import static org.hisp.dhis.web.WebClientUtils.assertStatus;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-public class DataIntegrityOrganisationUnitCompulsoryGroupControllerTest extends DhisControllerIntegrationTest {
+/**
+ * @author Jason P. Pickering
+ */
+public class DataIntegrityOrganisationUnitCompulsoryGroupControllerTest extends DhisControllerIntegrationTest
+{
     @Test
     void testOrgUnitInCompulsoryGroup()
     {
 
         //Create an orgunit group
         String test_orgunitgroup = assertStatus( HttpStatus.CREATED,
-                POST( "/organisationUnitGroups", "{'name': 'Type A', 'shortName': 'Type A'}" ) );
+            POST( "/organisationUnitGroups", "{'name': 'Type A', 'shortName': 'Type A'}" ) );
         //Add it to a group set
         String myougs = assertStatus( HttpStatus.CREATED,
-                POST( "/organisationUnitGroupSets", "{'name': 'Type', 'shortName': 'Type', 'compulsory' : 'true' , 'organisationUnitGroups' :[{'id' : '" + test_orgunitgroup + "'}]}" ) );
+            POST( "/organisationUnitGroupSets",
+                "{'name': 'Type', 'shortName': 'Type', 'compulsory' : 'true' , 'organisationUnitGroups' :[{'id' : '"
+                    + test_orgunitgroup + "'}]}" ) );
 
-        JsonIdentifiableObject test_ougs = GET( "/organisationUnitGroupSets/" + myougs ).content().as(JsonIdentifiableObject.class);
+        JsonIdentifiableObject test_ougs = GET( "/organisationUnitGroupSets/" + myougs ).content()
+            .as( JsonIdentifiableObject.class );
         //Create an orgunit, but do not add it to the compulsory group
         assertStatus( HttpStatus.CREATED,
-                POST( "/organisationUnits", "{ 'name': 'Fish District', 'shortName': 'Fish District', 'openingDate' : '2022-01-01'}" ) );
+            POST( "/organisationUnits",
+                "{ 'name': 'Fish District', 'shortName': 'Fish District', 'openingDate' : '2022-01-01'}" ) );
 
         postSummary( "orgunit_compulsory_group_count" );
         JsonDataIntegritySummary summary = GET( "/dataIntegrity/orgunit_compulsory_group_count/summary" ).content()
-                .as( JsonDataIntegritySummary.class );
+            .as( JsonDataIntegritySummary.class );
         assertTrue( summary.exists() );
         assertTrue( summary.isObject() );
         assertEquals( 1, summary.getCount() );
