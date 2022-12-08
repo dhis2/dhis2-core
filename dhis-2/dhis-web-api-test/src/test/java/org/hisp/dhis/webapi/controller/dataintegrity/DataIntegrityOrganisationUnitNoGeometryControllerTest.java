43a5b7cfadc037e23913661eeed7044f2e7c157e
Jason Pickering <jason.p.pickering@gmail.com>
2022-12-08 11:12:10 +0100

    Add check for orgunits with no coordinates

---
+++ dhis-2/dhis-web-api-test/src/test/java/org/hisp/dhis/webapi/controller/dataintegrity/DataIntegrityOrganisationUnitNoGeometryControllerTest.java
@@ -0,0 +1,218 @@
+/*
+ * Copyright (c) 2004-2022, University of Oslo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * Neither the name of the HISP project nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.hisp.dhis.webapi.controller.dataintegrity;
+
+import static org.hisp.dhis.web.WebClientUtils.assertStatus;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.hisp.dhis.jsontree.JsonArray;
+import org.hisp.dhis.jsontree.JsonList;
+import org.hisp.dhis.jsontree.JsonResponse;
+import org.hisp.dhis.web.HttpStatus;
+import org.hisp.dhis.webapi.json.domain.JsonDataIntegrityDetails;
+import org.hisp.dhis.webapi.json.domain.JsonDataIntegritySummary;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Checks for organisation units with no geometry.
+ *
+ * @author Jason P. Pickering
+ */
+class DataIntegrityOrganisationUnitsNoGeometryontrollerTest extends AbstractDataIntegrityIntegrationTest
+{
+
+    String clinicA;
+
+    String clinicB;
+
+    String districtA;
+
+    @Test
+    void testOrgunitsNoGeometry()
+    {
+
+        districtA = assertStatus( HttpStatus.CREATED, POST( "/organisationUnits",
+            "{ 'name': 'Offgrid District', 'shortName': 'Offgrid District', 'openingDate' : '2022-01-01' }" ) );
+
+        clinicA = assertStatus( HttpStatus.CREATED,
+            POST( "/organisationUnits",
+                "{ 'name': 'Clinic A', 'shortName': 'Clinic A', " +
+                    "'parent': {'id' : '" + districtA + "'}, " +
+                    "'openingDate' : '2022-01-01', 'geometry' : {'type' : 'Point', 'coordinates' : [1, 1]} }" ) );
+
+        clinicB = assertStatus( HttpStatus.CREATED,
+            POST( "/organisationUnits",
+                "{ 'name': 'Clinic B', 'shortName': 'Clinic B', " +
+                    "'parent': {'id' : '" + districtA + "'}, " +
+                    "'openingDate' : '2022-01-01', 'geometry' : {'type' : 'Point', 'coordinates' : [2, 2]} }" ) );
+
+        postSummary( "orgunit_no_coordinates" );
+
+        JsonDataIntegritySummary summary = GET( "/dataIntegrity/orgunit_no_coordinates/summary" )
+            .content()
+            .as( JsonDataIntegritySummary.class );
+        assertTrue( summary.exists() );
+        assertTrue( summary.isObject() );
+        assertEquals( 1, summary.getCount() );
+        assertEquals( 33, summary.getPercentage().intValue() );
+
+        postDetails( "orgunit_no_coordinates" );
+
+        JsonDataIntegrityDetails details = GET( "/dataIntegrity/orgunit_no_coordinates/details" )
+            .content()
+            .as( JsonDataIntegrityDetails.class );
+        assertTrue( details.exists() );
+        assertTrue( details.isObject() );
+        JsonList<JsonDataIntegrityDetails.JsonDataIntegrityIssue> issues = details.getIssues();
+        assertTrue( issues.exists() );
+        assertEquals( 1, issues.size() );
+        assertEquals( districtA, issues.get( 0 ).getId() );
+        assertEquals( "Offgrid District", issues.get( 0 ).getName() );
+        assertTrue( issues.get( 0 ).getName().toString().startsWith( "Offgrid" ) );
+        assertTrue( issues.get( 0 ).getComment().toString().contains( "1" ) );
+        assertEquals( "orgunits", details.getIssuesIdType() );
+
+    }
+
+    @Test
+    void testOrgunitsHasGeometry()
+    {
+
+        districtA = assertStatus( HttpStatus.CREATED,
+            POST( "/organisationUnits",
+                "{ 'name': 'District A', 'shortName': 'District A', " +
+                    "'openingDate' : '2022-01-01', 'geometry' : {'type' : 'Polygon', 'coordinates' : [[[0,0],[3,0],[3,3],[0,3],[0,0]]]} }" ) );
+
+        clinicA = assertStatus( HttpStatus.CREATED,
+            POST( "/organisationUnits",
+                "{ 'name': 'Clinic A', 'shortName': 'Clinic A', " +
+                    "'parent': {'id' : '" + districtA + "'}, " +
+                    "'openingDate' : '2022-01-01', 'geometry' : {'type' : 'Point', 'coordinates' : [1, 1]} }" ) );
+
+        clinicB = assertStatus( HttpStatus.CREATED,
+            POST( "/organisationUnits",
+                "{ 'name': 'Clinic B', 'shortName': 'Clinic B', " +
+                    "'parent': {'id' : '" + districtA + "'}, " +
+                    "'openingDate' : '2022-01-01', 'geometry' : {'type' : 'Point', 'coordinates' : [2, 2]} }" ) );
+
+        //Create an orgunit, but do not add it to the compulsory group
+        postSummary( "orgunit_no_coordinates" );
+
+        JsonDataIntegritySummary summary = GET( "/dataIntegrity/orgunit_no_coordinates/summary" )
+            .content()
+            .as( JsonDataIntegritySummary.class );
+        assertTrue( summary.exists() );
+        assertTrue( summary.isObject() );
+        assertEquals( 0, summary.getCount() );
+        assertEquals( 0, summary.getPercentage().intValue() );
+
+        postDetails( "orgunit_no_coordinates" );
+
+        JsonDataIntegrityDetails details = GET( "/dataIntegrity/orgunit_no_coordinates/details" )
+            .content()
+            .as( JsonDataIntegrityDetails.class );
+        assertTrue( details.exists() );
+        assertTrue( details.isObject() );
+        JsonList<JsonDataIntegrityDetails.JsonDataIntegrityIssue> issues = details.getIssues();
+        assertTrue( issues.exists() );
+        assertEquals( 0, issues.size() );
+
+    }
+
+    @Test
+    void testOrgunitsNoGeometryDivideByZero()
+    {
+
+        postSummary( "orgunit_no_coordinates" );
+
+        JsonDataIntegritySummary summary = GET( "/dataIntegrity/orgunit_no_coordinates/summary" )
+            .content()
+            .as( JsonDataIntegritySummary.class );
+        assertTrue( summary.exists() );
+        assertTrue( summary.isObject() );
+        assertEquals( 0, summary.getCount() );
+        assertNull( summary.getPercentage() );
+
+        postDetails( "orgunit_no_coordinates" );
+
+        JsonDataIntegrityDetails details = GET( "/dataIntegrity/orgunit_no_coordinates/details" )
+            .content()
+            .as( JsonDataIntegrityDetails.class );
+        assertTrue( details.exists() );
+        assertTrue( details.isObject() );
+        JsonList<JsonDataIntegrityDetails.JsonDataIntegrityIssue> issues = details.getIssues();
+        assertTrue( issues.exists() );
+        assertEquals( 0, issues.size() );
+
+    }
+
+    private void deleteAllOrgUnits()
+    {
+        GET( "/organisationUnits/gist?fields=id&headless=true" ).content().stringValues()
+            .forEach( id -> DELETE( "/organisationUnits/" + id ) );
+        JsonResponse response = GET( "/organisationUnits/" ).content();
+        JsonArray dimensions = response.getArray( "organisationUnits" );
+        assertEquals( 0, dimensions.size() );
+    }
+
+    @BeforeEach
+    public void setUp()
+    {
+        deleteAllOrgUnits();
+
+    }
+
+    private boolean DeleteMetadataObject( String endpoint, String uid )
+    {
+
+        if ( endpoint == null )
+        {
+            return false;
+        }
+
+        if ( uid != null )
+        {
+
+            assertStatus( HttpStatus.OK,
+                DELETE( "/" + endpoint + "/" + uid ) );
+            assertStatus( HttpStatus.NOT_FOUND,
+                GET( "/" + endpoint + "/" + uid ) );
+        }
+        return true;
+    }
+
+    @AfterEach
+    public void tearDown()
+    {
+        DeleteMetadataObject( "organisationUnits", clinicB );
+        DeleteMetadataObject( "organisationUnits", clinicA );
+        DeleteMetadataObject( "organisationUnits", districtA );
+    }
+}
