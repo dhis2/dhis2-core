9ad0ba8de21333d1d28c75872e1913fd69cd752c
Morten Hansen <morten@winterop.com>
2022-12-14 15:59:47 +0700

    test: add create EventHook test

--- dhis-2/dhis-web-api-test/src/test/java/org/hisp/dhis/webapi/controller/EventHookControllerTest.java
+++ dhis-2/dhis-web-api-test/src/test/java/org/hisp/dhis/webapi/controller/EventHookControllerTest.java
@@ -28,6 +28,7 @@
 package org.hisp.dhis.webapi.controller;
 
 import static org.hisp.dhis.web.WebClientUtils.assertStatus;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.hisp.dhis.jsontree.JsonObject;
@@ -58,5 +59,7 @@ class EventHookControllerTest extends DhisControllerIntegrationTest
         String id = assertStatus( HttpStatus.CREATED,
             POST( "/eventHooks/",
                 "{'id':'bRNvL6NMQXb','name':'hook1','source':{'path':'metadata','fields':'id,name'},'targets':[{'type':'webhook','url':'http://localhost:8081/api/gateway','auth':{'type':'api-token','token':'EB3F6799-AA5A-47E8-B6B7-97EA54EB3873'}}]}" ) );
+
+        assertEquals( "bRNvL6NMQXb", id );
     }
 }
