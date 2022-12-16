bcf1d876041eafb7c8376c6dcc7de62f2cd478fd
lucaCambi77 <lucacambi77@gmail.com>
2022-12-16 14:18:06 +0100

    fix: TEIs are not filterable by potentialDuplicate property [ DHIS2-11625 ] [ 2.39 ] (#12575)
    * fix: add potential duplicate as web parameter
    
    * fix: add e2e tests
    
    * fix: java doc

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/trackedentity/hibernate/HibernateTrackedEntityInstanceStore.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/trackedentity/hibernate/HibernateTrackedEntityInstanceStore.java
@@ -634,6 +634,13 @@ public class HibernateTrackedEntityInstanceStore
                 .append( "TEI.deleted IS FALSE " );
         }
 
+        if ( params.hasPotentialDuplicateFilter() )
+        {
+            trackedEntity
+                .append( whereAnd.whereAnd() ).append( "TEI.potentialduplicate=" )
+                .append( params.getPotentialDuplicate() ).append( SPACE );
+        }
+
         return trackedEntity.toString();
     }
 
