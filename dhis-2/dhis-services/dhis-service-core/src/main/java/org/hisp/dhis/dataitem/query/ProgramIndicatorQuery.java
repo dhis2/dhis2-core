497a7d38b71c20247f33baac2cda0d028761d483
Giuseppe Nespolino <g.nespolino@gmail.com>
2023-01-10 16:19:26 +0100

    fix: removed extra semicolon [DHIS2-14464]

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/dataitem/query/ProgramIndicatorQuery.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/dataitem/query/ProgramIndicatorQuery.java
@@ -73,7 +73,7 @@ public class ProgramIndicatorQuery implements DataItemQuery
         + " cast (null as text) as program_shortname, programindicator.uid as item_uid, programindicator.name as item_name,"
         + " programindicator.shortname as item_shortname, cast (null as text) as item_valuetype, programindicator.code as item_code,"
         + " programindicator.sharing as item_sharing, cast (null as text) as item_domaintype, cast ('PROGRAM_INDICATOR' as text) as item_type,"
-        + " cast (null as text) as expression";;
+        + " cast (null as text) as expression";
 
     private static final String COMMON_UIDS = "program.uid, programindicator.uid";
 
