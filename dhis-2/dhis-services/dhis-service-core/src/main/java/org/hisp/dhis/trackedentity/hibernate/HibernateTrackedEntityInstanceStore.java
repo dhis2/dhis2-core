f6d14147b785fc2263e74b0cee933d58c34c94bc
muilpp <muilpp@gmail.com>
2023-01-19 15:17:49 +0100

    fix: Use string.join [DHIS2-13981] [2.39]

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/trackedentity/hibernate/HibernateTrackedEntityInstanceStore.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/trackedentity/hibernate/HibernateTrackedEntityInstanceStore.java
@@ -28,7 +28,7 @@
 package org.hisp.dhis.trackedentity.hibernate;
 
 import static com.google.common.base.Preconditions.checkNotNull;
-import static java.util.Comparator.*;
+import static java.util.Comparator.comparing;
 import static org.hisp.dhis.common.IdentifiableObjectUtils.getIdentifiers;
 import static org.hisp.dhis.commons.util.TextUtils.getCommaDelimitedString;
 import static org.hisp.dhis.commons.util.TextUtils.getQuotedCommaDelimitedString;
@@ -103,7 +103,6 @@ import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Repository;
 
 import com.google.common.collect.Lists;
-import io.debezium.util.Strings;
 
 /**
  * @author Abyot Asalefew Gizaw
@@ -555,7 +554,7 @@ public class HibernateTrackedEntityInstanceStore
             }
         }
 
-        return "SELECT " + Strings.join( ", ", columns );
+        return "SELECT " + String.join( ", ", columns );
     }
 
     /**
