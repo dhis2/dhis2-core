b046d698be62c4dbc1a805385a24b116fc11c59f
teleivo <teleivo@users.noreply.github.com>
2023-01-27 15:43:52 +0100

    ci: automate local jib builds for mac arm architecture

--- dhis-2/build-dev.sh
+++ dhis-2/build-dev.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# Builds DHIS2 war and Docker image for development use
 
 #
 ## bash environment
@@ -23,6 +24,16 @@ TAG=local
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
+# Choosing this approach over automatically activating a maven profile based on the architecture.
+# This is to not risk running both the jibBuild and jibBuildArmOnly profiles in our pipelines.
+# There might be ways like using https://maven.apache.org/enforcer/enforcer-rules/requireActiveProfile.html
+# to prevent that but they would require more work.
+ARCH=$(mvn help:system | grep "os\.arch")
+JIB_PROFILE=
+if [[ "$ARCH" == *arm64* || "$ARCH" == *aarch64* ]]; then
+  JIB_PROFILE="-P jibBuildArmOnly"
+fi
+
 print() {
     echo -e "\033[1m$1\033[0m" 1>&2
 }
@@ -32,7 +43,7 @@ print "Building dhis2-core and Docker image..."
 export MAVEN_OPTS="-Dhttp.keepAlive=false -Dmaven.wagon.http.pool=false -Dmaven.wagon.http.retryHandler.class=standard -Dmaven.wagon.http.retryHandler.count=3 -Dmaven.wagon.httpconnectionManager.ttlSeconds=25"
 mvn clean install --threads 2C -DskipTests -Dmaven.test.skip=true -f "${DIR}/pom.xml" -pl -dhis-web-embedded-jetty,-dhis-test-integration,-dhis-test-coverage
 mvn clean install --threads 2C -DskipTests -Dmaven.test.skip=true -f "${DIR}/dhis-web/pom.xml"
-mvn -DskipTests -Dmaven.test.skip=true -f "${DIR}/dhis-web/dhis-web-portal/pom.xml" jib:dockerBuild
+mvn -DskipTests -Dmaven.test.skip=true -f "${DIR}/dhis-web/dhis-web-portal/pom.xml" jib:dockerBuild $JIB_PROFILE
 
 if test -z "$D2CLUSTER"; then
     print "No cluster name specified, skipping deploy"
