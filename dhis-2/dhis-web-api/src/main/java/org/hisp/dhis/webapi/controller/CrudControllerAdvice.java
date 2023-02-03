62390f4a52e4037e32487c452457112895f512c5
Enrico Colasante <enrico@dhis2.org>
2023-02-03 11:41:15 +0100

    fix: Return meaningful error message for wrong enum value in parameter [DHIS2-14182] (#12882)
    * fix: Return meaningful error message for wrong enum value in parameter [DHIS2-14182]
    
    * Add formatting
    
    * Sonar issue fix
    
    * Remove unrelated tests
    
    * Remove unrelated tests
    
    * Assert just on exception for controller with CrudControllerAdvice
    
    * Handle bad request when a parameter cannot be converted

--- dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/CrudControllerAdvice.java
+++ dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/CrudControllerAdvice.java
@@ -104,6 +104,7 @@ import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestClientException;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import io.github.classgraph.ClassGraph;
@@ -185,6 +186,27 @@ public class CrudControllerAdvice
     public WebMessage illegalQueryExceptionHandler( IllegalQueryException ex )
     {
         return conflict( ex.getMessage(), ex.getErrorCode() );
+    }
+
+    @ExceptionHandler( MethodArgumentTypeMismatchException.class )
+    @ResponseBody
+    public WebMessage methodArgumentTypeMismatchException( MethodArgumentTypeMismatchException ex )
+    {
+        if ( ex.getRequiredType() == null )
+        {
+            return handleBadRequest( ex );
+        }
+
+        if ( ex.getRequiredType().isEnum() )
+        {
+            String validValues = StringUtils
+                .join( Arrays.stream( ex.getRequiredType().getEnumConstants() ).map( Objects::toString )
+                    .collect( Collectors.toList() ), ", " );
+            String errorMessage = MessageFormat.format( "Value {0} is not a valid {1}. Valid values are: [{2}]",
+                ex.getValue(), ex.getName(), validValues );
+            return badRequest( errorMessage );
+        }
+        return handleBadRequest( ex );
     }
 
     @ExceptionHandler( InvalidEnumValueException.class )
