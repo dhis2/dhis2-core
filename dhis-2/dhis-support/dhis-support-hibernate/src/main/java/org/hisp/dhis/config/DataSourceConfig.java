d4748c4b772098346735b30848ed33e59dc660b1
Viet Nguyen <viet@dhis2.org>
2023-02-06 10:08:56 +0700

    feat: support jpa annotation mapping#

--- dhis-2/dhis-support/dhis-support-hibernate/src/main/java/org/hisp/dhis/config/DataSourceConfig.java
+++ dhis-2/dhis-support/dhis-support-hibernate/src/main/java/org/hisp/dhis/config/DataSourceConfig.java
@@ -27,18 +27,20 @@
  */
 package org.hisp.dhis.config;
 
-import com.google.common.base.MoreObjects;
 import java.beans.PropertyVetoException;
 import java.sql.SQLException;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
+
 import javax.sql.DataSource;
+
 import lombok.extern.slf4j.Slf4j;
 import net.ttddyy.dsproxy.listener.MethodExecutionContext;
 import net.ttddyy.dsproxy.listener.logging.DefaultQueryLogEntryCreator;
 import net.ttddyy.dsproxy.listener.logging.SLF4JLogLevel;
 import net.ttddyy.dsproxy.listener.logging.SLF4JQueryLoggingListener;
 import net.ttddyy.dsproxy.support.ProxyDataSourceBuilder;
+
 import org.hibernate.engine.jdbc.internal.FormatStyle;
 import org.hibernate.engine.jdbc.internal.Formatter;
 import org.hisp.dhis.common.CodeGenerator;
@@ -57,180 +59,194 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
+import com.google.common.base.MoreObjects;
+
 /**
  * @author Morten Svanæs <msvanaes@dhis2.org>
  */
 @Slf4j
 @Configuration
-public class DataSourceConfig {
-  @Autowired private DhisConfigurationProvider dhisConfig;
+public class DataSourceConfig
+{
+    @Autowired
+    private DhisConfigurationProvider dhisConfig;
 
-  @Bean
-  @DependsOn("dataSource")
-  public NamedParameterJdbcTemplate namedParameterJdbcTemplate(
-      @Qualifier("dataSource") DataSource dataSource) {
-    return new NamedParameterJdbcTemplate(dataSource);
-  }
-
-  @Bean("jdbcTemplate")
-  @DependsOn("dataSource")
-  @Primary
-  public JdbcTemplate jdbcTemplate(@Qualifier("dataSource") DataSource dataSource) {
-    JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-    jdbcTemplate.setFetchSize(1000);
-    return jdbcTemplate;
-  }
-
-  @Bean("executionPlanJdbcTemplate")
-  @DependsOn("dataSource")
-  public JdbcTemplate executionPlanJdbcTemplate(@Qualifier("dataSource") DataSource dataSource) {
-    JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-    jdbcTemplate.setFetchSize(1000);
-    jdbcTemplate.setQueryTimeout(10);
-    return jdbcTemplate;
-  }
-
-  @Bean("readOnlyJdbcTemplate")
-  @DependsOn("dataSource")
-  public JdbcTemplate readOnlyJdbcTemplate(@Qualifier("dataSource") DataSource dataSource) {
-    DefaultReadOnlyDataSourceManager manager = new DefaultReadOnlyDataSourceManager(dhisConfig);
-
-    JdbcTemplate jdbcTemplate =
-        new JdbcTemplate(MoreObjects.firstNonNull(manager.getReadOnlyDataSource(), dataSource));
-    jdbcTemplate.setFetchSize(1000);
-
-    return jdbcTemplate;
-  }
-
-  @Bean("actualDataSource")
-  public DataSource actualDataSource(
-      HibernateConfigurationProvider hibernateConfigurationProvider) {
-    String jdbcUrl = dhisConfig.getProperty(ConfigurationKey.CONNECTION_URL);
-    String username = dhisConfig.getProperty(ConfigurationKey.CONNECTION_USERNAME);
-    String dbPoolType = dhisConfig.getProperty(ConfigurationKey.DB_POOL_TYPE);
-
-    DatabasePoolUtils.PoolConfig poolConfig =
-        DatabasePoolUtils.PoolConfig.builder()
-            .dhisConfig(dhisConfig)
-            .hibernateConfig(hibernateConfigurationProvider)
-            .dbPoolType(dbPoolType)
-            .build();
-
-    try {
-      return DatabasePoolUtils.createDbPool(poolConfig);
-    } catch (SQLException | PropertyVetoException e) {
-      String message =
-          String.format(
-              "Connection test failed for main database pool, " + "jdbcUrl: '%s', user: '%s'",
-              jdbcUrl, username);
-
-      log.error(message);
-      log.error(DebugUtils.getStackTrace(e));
-
-      throw new IllegalStateException(message, e);
-    }
-  }
-
-  @Bean("dataSource")
-  @DependsOn("actualDataSource")
-  @Primary
-  public DataSource dataSource(@Qualifier("actualDataSource") DataSource actualDataSource) {
-    boolean enableQueryLogging = dhisConfig.isEnabled(ConfigurationKey.ENABLE_QUERY_LOGGING);
-
-    if (!enableQueryLogging) {
-      return actualDataSource;
+    @Bean
+    @DependsOn( "dataSource" )
+    public NamedParameterJdbcTemplate namedParameterJdbcTemplate( @Qualifier( "dataSource" ) DataSource dataSource )
+    {
+        return new NamedParameterJdbcTemplate( dataSource );
     }
 
-    PrettyQueryEntryCreator creator = new PrettyQueryEntryCreator();
-    creator.setMultiline(true);
+    @Bean( "jdbcTemplate" )
+    @Primary
+    @DependsOn( "dataSource" )
+    public JdbcTemplate jdbcTemplate( @Qualifier( "dataSource" ) DataSource dataSource )
+    {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate( dataSource );
+        jdbcTemplate.setFetchSize( 1000 );
+        return jdbcTemplate;
+    }
 
-    SLF4JQueryLoggingListener listener = new SLF4JQueryLoggingListener();
-    listener.setLogger("org.hisp.dhis.datasource.query");
-    listener.setLogLevel(SLF4JLogLevel.INFO);
-    listener.setQueryLogEntryCreator(creator);
+    @Bean( "executionPlanJdbcTemplate" )
+    @DependsOn( "dataSource" )
+    public JdbcTemplate executionPlanJdbcTemplate( @Qualifier( "dataSource" ) DataSource dataSource )
+    {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate( dataSource );
+        jdbcTemplate.setFetchSize( 1000 );
+        jdbcTemplate.setQueryTimeout( 10 );
+        return jdbcTemplate;
+    }
 
-    ProxyDataSourceBuilder builder =
-        ProxyDataSourceBuilder.create(actualDataSource)
-            .name(
-                "ProxyDS_DHIS2_"
-                    + dhisConfig.getProperty(ConfigurationKey.DB_POOL_TYPE)
-                    + "_"
-                    + CodeGenerator.generateCode(5))
+    @Bean( "readOnlyJdbcTemplate" )
+    @DependsOn( "dataSource" )
+    public JdbcTemplate readOnlyJdbcTemplate( @Qualifier( "dataSource" ) DataSource dataSource )
+    {
+        DefaultReadOnlyDataSourceManager manager = new DefaultReadOnlyDataSourceManager( dhisConfig );
+
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(
+            MoreObjects.firstNonNull( manager.getReadOnlyDataSource(), dataSource ) );
+        jdbcTemplate.setFetchSize( 1000 );
+
+        return jdbcTemplate;
+    }
+
+    @Bean( "actualDataSource" )
+    public DataSource actualDataSource( HibernateConfigurationProvider hibernateConfigurationProvider )
+    {
+        String jdbcUrl = dhisConfig.getProperty( ConfigurationKey.CONNECTION_URL );
+        String username = dhisConfig.getProperty( ConfigurationKey.CONNECTION_USERNAME );
+        String dbPoolType = dhisConfig.getProperty( ConfigurationKey.DB_POOL_TYPE );
+
+        DatabasePoolUtils.PoolConfig.PoolConfigBuilder builder = DatabasePoolUtils.PoolConfig.builder();
+        builder.dhisConfig( dhisConfig );
+        builder.hibernateConfig( hibernateConfigurationProvider );
+        builder.dbPoolType( dbPoolType );
+
+        try
+        {
+            return DatabasePoolUtils.createDbPool( builder.build() );
+        }
+        catch ( SQLException | PropertyVetoException e )
+        {
+            String message = String.format( "Connection test failed for main database pool, " +
+                "jdbcUrl: '%s', user: '%s'", jdbcUrl, username );
+
+            log.error( message );
+            log.error( DebugUtils.getStackTrace( e ) );
+
+            throw new IllegalStateException( message, e );
+        }
+    }
+
+    @Bean( "dataSource" )
+    @DependsOn( "actualDataSource" )
+    @Primary
+    public DataSource dataSource( @Qualifier( "actualDataSource" ) DataSource actualDataSource )
+    {
+        boolean enableQueryLogging = dhisConfig.isEnabled(
+            ConfigurationKey.ENABLE_QUERY_LOGGING );
+
+        if ( !enableQueryLogging )
+        {
+            return actualDataSource;
+        }
+
+        PrettyQueryEntryCreator creator = new PrettyQueryEntryCreator();
+        creator.setMultiline( true );
+
+        SLF4JQueryLoggingListener listener = new SLF4JQueryLoggingListener();
+        listener.setLogger( "org.hisp.dhis.datasource.query" );
+        listener.setLogLevel( SLF4JLogLevel.INFO );
+        listener.setQueryLogEntryCreator( creator );
+
+        ProxyDataSourceBuilder b = ProxyDataSourceBuilder
+
+            .create( actualDataSource )
+            .name( "ProxyDS_DHIS2_" + dhisConfig.getProperty(
+                ConfigurationKey.DB_POOL_TYPE ) +
+                "_" + CodeGenerator.generateCode( 5 ) )
+
             .logSlowQueryBySlf4j(
-                Integer.parseInt(
-                    dhisConfig.getProperty(ConfigurationKey.SLOW_QUERY_LOGGING_THRESHOLD_TIME_MS)),
-                TimeUnit.MILLISECONDS,
-                SLF4JLogLevel.WARN)
-            .listener(listener)
+                Integer.parseInt( dhisConfig.getProperty(
+                    ConfigurationKey.SLOW_QUERY_LOGGING_THRESHOLD_TIME_MS ) ),
+                TimeUnit.MILLISECONDS, SLF4JLogLevel.WARN )
+
+            .listener( listener )
             .proxyResultSet();
 
-    boolean elapsedTimeLogging =
-        dhisConfig.isEnabled(ConfigurationKey.ELAPSED_TIME_QUERY_LOGGING_ENABLED);
-    boolean methodLoggingEnabled =
-        dhisConfig.isEnabled(ConfigurationKey.METHOD_QUERY_LOGGING_ENABLED);
+        boolean elapsedTimeLogging = dhisConfig.isEnabled(
+            ConfigurationKey.ELAPSED_TIME_QUERY_LOGGING_ENABLED );
+        boolean methodLoggingEnabled = dhisConfig.isEnabled(
+            ConfigurationKey.METHOD_QUERY_LOGGING_ENABLED );
 
-    if (methodLoggingEnabled) {
-      builder.afterMethod(DataSourceConfig::executeAfterMethod);
+        if ( methodLoggingEnabled )
+        {
+            b.afterMethod( DataSourceConfig::executeAfterMethod );
+        }
+
+        if ( elapsedTimeLogging )
+        {
+            b.afterQuery(
+                ( execInfo, queryInfoList ) -> log.info( "Query took " +
+                    execInfo.getElapsedTime() + "msec" ) );
+        }
+
+        return b.build();
     }
 
-    if (elapsedTimeLogging) {
-      builder.afterQuery(
-          (execInfo, queryInfoList) ->
-              log.info("Query took " + execInfo.getElapsedTime() + "msec"));
+    private static void executeAfterMethod( MethodExecutionContext executionContext )
+    {
+        Thread thread = Thread.currentThread();
+        StackTraceElement[] stackTrace = thread.getStackTrace();
+
+        for ( int i = 0; i < stackTrace.length; i++ )
+        {
+            StackTraceElement stackTraceElement = stackTrace[i];
+            String methodName = stackTraceElement.getMethodName();
+            String className = stackTraceElement.getClassName();
+            int pos = className.lastIndexOf( '.' );
+            String packageName = className.substring( 0, pos );
+
+            if ( className.contains( "org.hisp.dhis.cacheinvalidation.KnownTransactionsService" ) || methodName.equals(
+                "getSingleResult" ) || methodName.equals( "doFilterInternal" ) )
+            {
+                break;
+            }
+
+            if ( packageName.startsWith( "org.hisp.dhis" ) && !methodName.equals( "executeAfterMethod" ) )
+            {
+                StackTraceElement nextElement = stackTrace[i - 1];
+                String methodName1 = nextElement.getMethodName();
+                String className1 = nextElement.getClassName();
+
+                log.info(
+                    "---- JDBC: " + className + "#" + methodName + " ---- \n ----" + className1 + "#" + methodName1 );
+                break;
+            }
+
+        }
+
     }
 
-    return builder.build();
-  }
+    private static class PrettyQueryEntryCreator extends DefaultQueryLogEntryCreator
+    {
+        // use hibernate to format queries
+        private final Formatter formatter = FormatStyle.HIGHLIGHT.getFormatter();
 
-  private static void executeAfterMethod(MethodExecutionContext executionContext) {
-    Thread thread = Thread.currentThread();
-    StackTraceElement[] stackTrace = thread.getStackTrace();
-
-    for (int i = 0; i < stackTrace.length; i++) {
-      StackTraceElement stackTraceElement = stackTrace[i];
-      String methodName = stackTraceElement.getMethodName();
-      String className = stackTraceElement.getClassName();
-      int pos = className.lastIndexOf('.');
-      String packageName = className.substring(0, pos);
-
-      if (className.contains("org.hisp.dhis.cacheinvalidation.KnownTransactionsService")
-          || methodName.equals("getSingleResult")
-          || methodName.equals("doFilterInternal")) {
-        break;
-      }
-
-      if (packageName.startsWith("org.hisp.dhis") && !methodName.equals("executeAfterMethod")) {
-        StackTraceElement nextElement = stackTrace[i - 1];
-        String methodName1 = nextElement.getMethodName();
-        String className1 = nextElement.getClassName();
-
-        log.info(
-            "JDBC: "
-                + className
-                + "#"
-                + methodName
-                + " ---- \n ----"
-                + className1
-                + "#"
-                + methodName1);
-        break;
-      }
+        @Override
+        protected String formatQuery( String query )
+        {
+            try
+            {
+                Objects.requireNonNull( query );
+                return this.formatter.format( query ) + "\n";
+            }
+            catch ( Exception e )
+            {
+                log.error( "Query formatter failed!", e );
+            }
+            return "FORMATTER ERROR!";
+        }
     }
-  }
-
-  private static class PrettyQueryEntryCreator extends DefaultQueryLogEntryCreator {
-    private final Formatter formatter = FormatStyle.HIGHLIGHT.getFormatter();
-
-    @Override
-    protected String formatQuery(String query) {
-      try {
-        Objects.requireNonNull(query);
-        return this.formatter.format(query) + "\n";
-      } catch (Exception e) {
-        log.error("Query formatter failed!", e);
-      }
-
-      return "Formatter error!";
-    }
-  }
 }
--- dhis-2/dhis-support/dhis-support-hibernate/src/main/java/org/hisp/dhis/config/HibernateConfig.java
+++ dhis-2/dhis-support/dhis-support-hibernate/src/main/java/org/hisp/dhis/config/HibernateConfig.java
@@ -27,21 +27,25 @@
  */
 package org.hisp.dhis.config;
 
-import static org.hibernate.cfg.AvailableSettings.DIALECT;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
-import javax.persistence.EntityManagerFactory;
+
+import javax.persistence.EntityManager;
 import javax.persistence.SharedCacheMode;
 import javax.persistence.ValidationMode;
 import javax.sql.DataSource;
+
+import lombok.RequiredArgsConstructor;
+
+import org.apache.commons.lang3.ArrayUtils;
 import org.hibernate.SessionFactory;
 import org.hisp.dhis.cache.DefaultHibernateCacheManager;
 import org.hisp.dhis.dbms.DbmsManager;
 import org.hisp.dhis.dbms.HibernateDbmsManager;
+import org.hisp.dhis.external.conf.ConfigurationKey;
 import org.hisp.dhis.external.conf.DhisConfigurationProvider;
 import org.hisp.dhis.hibernate.DefaultHibernateConfigurationProvider;
 import org.hisp.dhis.hibernate.HibernateConfigurationProvider;
@@ -50,7 +54,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
@@ -64,120 +70,149 @@ import org.springframework.transaction.support.TransactionTemplate;
  */
 @Configuration
 @EnableTransactionManagement
-public class HibernateConfig {
-  @Bean("hibernateConfigurationProvider")
-  public HibernateConfigurationProvider hibernateConfigurationProvider(
-      DhisConfigurationProvider dhisConfig) {
-    DefaultHibernateConfigurationProvider hibernateConfigurationProvider =
-        new DefaultHibernateConfigurationProvider();
-    hibernateConfigurationProvider.setConfigProvider(dhisConfig);
-    return hibernateConfigurationProvider;
-  }
-
-  //    @Bean
-  //    @DependsOn( "flyway" )
-  //    public LocalSessionFactoryBean sessionFactory( DataSource dataSource,
-  //        @Qualifier( "hibernateConfigurationProvider" ) HibernateConfigurationProvider
-  // hibernateConfigurationProvider )
-  //    {
-  //        Objects.requireNonNull( dataSource );
-  //        Objects.requireNonNull( hibernateConfigurationProvider );
-  //
-  //        Properties hibernateProperties =
-  // hibernateConfigurationProvider.getConfiguration().getProperties();
-  //        Objects.requireNonNull( hibernateProperties );
-  //
-  //        List<Resource> jarResources = hibernateConfigurationProvider.getJarResources();
-  //        List<Resource> directoryResources =
-  // hibernateConfigurationProvider.getDirectoryResources();
-  //
-  //        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-  //        sessionFactory.setDataSource( dataSource );
-  //        sessionFactory.setMappingJarLocations( jarResources.toArray( new Resource[0] ) );
-  //        sessionFactory.setMappingDirectoryLocations( directoryResources.toArray( new Resource[0]
-  // ) );
-  //        sessionFactory.setAnnotatedClasses( DeletedObject.class );
-  //        sessionFactory.setHibernateProperties( hibernateProperties );
-  //
-  //        return sessionFactory;
-  //    }
-
-  //    @Bean
-  //    public HibernateTransactionManager hibernateTransactionManager( DataSource dataSource,
-  //        SessionFactory sessionFactory )
-  //    {
-  //        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
-  //        transactionManager.setSessionFactory( sessionFactory );
-  //        transactionManager.setDataSource( dataSource );
-  //
-  //        return transactionManager;
-  //    }
-
-  @Bean
-  public PlatformTransactionManager jpaTransactionManager(
-      LocalContainerEntityManagerFactoryBean sessionFactory) {
-    JpaTransactionManager txManager = new JpaTransactionManager();
-    txManager.setEntityManagerFactory(sessionFactory.getObject());
-    return txManager;
-  }
-
-  @Bean
-  public TransactionTemplate transactionTemplate(PlatformTransactionManager transactionManager) {
-    return new TransactionTemplate(transactionManager);
-  }
-
-  @Bean
-  public DefaultHibernateCacheManager cacheManager(
-      LocalContainerEntityManagerFactoryBean sessionFactory) {
-    DefaultHibernateCacheManager cacheManager = new DefaultHibernateCacheManager();
-    cacheManager.setSessionFactory(
-        sessionFactory.getNativeEntityManagerFactory().unwrap(SessionFactory.class));
-
-    return cacheManager;
-  }
-
-  @Bean
-  public DbmsManager dbmsManager(
-      JdbcTemplate jdbcTemplate,
-      EntityManagerFactory sessionFactory,
-      DefaultHibernateCacheManager cacheManager) {
-    HibernateDbmsManager hibernateDbmsManager = new HibernateDbmsManager();
-    hibernateDbmsManager.setCacheManager(cacheManager);
-    hibernateDbmsManager.setSessionFactory(sessionFactory.unwrap(SessionFactory.class));
-    hibernateDbmsManager.setJdbcTemplate(jdbcTemplate);
-    return hibernateDbmsManager;
-  }
-
-  @Bean("org.hibernate.SessionFactory")
-  @DependsOn("flyway")
-  public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean(
-      DataSource dataSource,
-      @Qualifier("hibernateConfigurationProvider")
-          HibernateConfigurationProvider hibernateConfigurationProvider)
-      throws IOException {
-    List<Resource> jarResources = hibernateConfigurationProvider.getJarResources();
-
-    Map<String, Object> properties = new Hashtable<>();
-    properties.put("javax.persistence.schema-generation.database.action", "none");
-    HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-    adapter.setDatabasePlatform(
-        hibernateConfigurationProvider.getConfiguration().getProperty(DIALECT));
-    LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
-    factory.setJpaVendorAdapter(adapter);
-    factory.setDataSource(dataSource);
-    factory.setPackagesToScan("org.hisp.dhis");
-    factory.setSharedCacheMode(SharedCacheMode.ENABLE_SELECTIVE);
-    factory.setValidationMode(ValidationMode.NONE);
-    factory.setJpaPropertyMap(properties);
-    factory.setMappingResources(getMappingResources(jarResources));
-    return factory;
-  }
-
-  private String[] getMappingResources(List<Resource> jarResources) throws IOException {
-    List<String> files = new ArrayList<>();
-    for (Resource resource : jarResources) {
-      files.add(resource.getFile().getAbsolutePath());
+@RequiredArgsConstructor
+public class HibernateConfig
+{
+    @Bean( "hibernateConfigurationProvider" )
+    public HibernateConfigurationProvider hibernateConfigurationProvider( DhisConfigurationProvider dhisConfig )
+    {
+        DefaultHibernateConfigurationProvider hibernateConfigurationProvider = new DefaultHibernateConfigurationProvider();
+        hibernateConfigurationProvider.setConfigProvider( dhisConfig );
+        return hibernateConfigurationProvider;
     }
-    return files.toArray(new String[files.size()]);
-  }
+
+    @Bean
+    public TransactionTemplate transactionTemplate( HibernateTransactionManager transactionManager )
+    {
+        return new TransactionTemplate( transactionManager );
+    }
+
+    @Bean
+    public DefaultHibernateCacheManager cacheManager( SessionFactory sessionFactory )
+    {
+        DefaultHibernateCacheManager cacheManager = new DefaultHibernateCacheManager();
+        cacheManager.setSessionFactory( sessionFactory );
+        return cacheManager;
+    }
+
+    @Bean
+    public DbmsManager dbmsManager( JdbcTemplate jdbcTemplate, SessionFactory sessionFactory,
+        DefaultHibernateCacheManager cacheManager )
+    {
+        HibernateDbmsManager hibernateDbmsManager = new HibernateDbmsManager();
+        hibernateDbmsManager.setCacheManager( cacheManager );
+        hibernateDbmsManager.setSessionFactory( sessionFactory );
+        hibernateDbmsManager.setJdbcTemplate( jdbcTemplate );
+        return hibernateDbmsManager;
+    }
+
+    @Bean
+    @DependsOn( "entityManagerFactoryBean" )
+    public PlatformTransactionManager jpaTransactionManager( LocalContainerEntityManagerFactoryBean sessionFactory )
+    {
+        JpaTransactionManager txManager = new JpaTransactionManager();
+        txManager.setEntityManagerFactory( sessionFactory.getObject() );
+        return txManager;
+    }
+
+    @Bean
+    public TransactionTemplate transactionTemplate( PlatformTransactionManager transactionManager )
+    {
+        return new TransactionTemplate( transactionManager );
+    }
+
+    @Bean
+    @DependsOn( "entityManagerFactoryBean" )
+    public DefaultHibernateCacheManager cacheManager( LocalContainerEntityManagerFactoryBean entityManagerFactory )
+    {
+        DefaultHibernateCacheManager cacheManager = new DefaultHibernateCacheManager();
+        cacheManager.setSessionFactory( entityManagerFactory.getObject().unwrap( SessionFactory.class ) );
+
+        return cacheManager;
+    }
+
+    @Bean
+    @DependsOn( "entityManagerFactoryBean" )
+    public EntityManager entityManager( LocalContainerEntityManagerFactoryBean entityManagerFactory )
+    {
+        return entityManagerFactory.getObject().createEntityManager();
+    }
+
+    @Bean
+    @DependsOn( "entityManagerFactoryBean" )
+    public DbmsManager dbmsManager( JdbcTemplate jdbcTemplate,
+        LocalContainerEntityManagerFactoryBean entityManagerFactory,
+        DefaultHibernateCacheManager cacheManager )
+    {
+        HibernateDbmsManager hibernateDbmsManager = new HibernateDbmsManager();
+        hibernateDbmsManager.setCacheManager( cacheManager );
+        hibernateDbmsManager.setSessionFactory( entityManagerFactory.getObject().unwrap( SessionFactory.class ) );
+        hibernateDbmsManager.setJdbcTemplate( jdbcTemplate );
+        return hibernateDbmsManager;
+    }
+
+    @Bean
+    @DependsOn( "entityManagerFactoryBean" )
+    public SessionFactory hibernateSessionFactory(
+        @Qualifier( "entityManagerFactoryBean" ) LocalContainerEntityManagerFactoryBean entityManagerFactory )
+    {
+        return entityManagerFactory.getObject().unwrap( SessionFactory.class );
+    }
+
+    @Bean( "entityManagerFactoryBean" )
+    @DependsOn( { "flyway" } )
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean( DhisConfigurationProvider config,
+        DataSource dataSource )
+        throws IOException
+    {
+        Map<String, Object> properties = new Hashtable<>();
+        properties.put( "javax.persistence.schema-generation.database.action", "none" );
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabasePlatform( config.getProperty( ConfigurationKey.CONNECTION_DIALECT ) );
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setJpaVendorAdapter( adapter );
+        factory.setDataSource( dataSource );
+        factory.setPackagesToScan( "org.hisp.dhis" );
+        factory.setSharedCacheMode( SharedCacheMode.ENABLE_SELECTIVE );
+        factory.setValidationMode( ValidationMode.NONE );
+        factory.setJpaPropertyMap( properties );
+        factory.setMappingResources( loadResources() );
+        factory.afterPropertiesSet();
+        return factory;
+    }
+
+    private String[] loadResources()
+    {
+        try
+        {
+            PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+            Resource[] resources = resolver.getResources( "classpath*:org/hisp/dhis/**/hibernate/*.hbm.xml" );
+            //            Resource[] resources =  classLoader.getResources( "classpath*:org/hisp/dhis/**/hibernate/*.hbm.xml" );
+
+            List<String> list = new ArrayList<>();
+            for ( Resource resource : resources )
+            {
+                String url = resource.getURL().toString();
+                list.add( url );
+            }
+            return list.toArray( new String[0] );
+        }
+        catch ( IOException e )
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return ArrayUtils.EMPTY_STRING_ARRAY;
+    }
+
+    private String[] getMappingResources( List<Resource> jarResources )
+        throws IOException
+    {
+        List<String> files = new ArrayList<>();
+        for ( Resource resource : jarResources )
+        {
+            files.add( resource.getFile().getAbsolutePath() );
+        }
+        return files.toArray( new String[files.size()] );
+    }
 }
