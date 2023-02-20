78bf5d896139900d87495755dcdbf86e6200c247
d-bernat <mail@it-bernat.de>
2023-02-20 16:03:13 +0100

    integration test fix

--- dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/data/JdbcAnalyticsManager.java
+++ dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/data/JdbcAnalyticsManager.java
@@ -591,7 +591,7 @@ public class JdbcAnalyticsManager
 
         if ( params.isAggregation() )
         {
-            sql = "group by " + getCommaDelimitedQuotedColumns( params.getDimensions() ) + " nulls last ";
+            sql = "group by " + getCommaDelimitedQuotedColumns( params.getDimensions() ) + " ";
         }
 
         return sql;
