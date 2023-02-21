bcda366b527c284764105dbf02b447e63cc7ea67
maikelarabori <maikelarabori@gmail.com>
2023-02-21 11:03:13 +0100

    fix: Disable TEI generation by default

--- dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/ResourceTableController.java
+++ dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/ResourceTableController.java
@@ -32,6 +32,10 @@ import static org.hisp.dhis.analytics.AnalyticsTableType.COMPLETENESS_TARGET;
 import static org.hisp.dhis.analytics.AnalyticsTableType.DATA_VALUE;
 import static org.hisp.dhis.analytics.AnalyticsTableType.ENROLLMENT;
 import static org.hisp.dhis.analytics.AnalyticsTableType.EVENT;
+import static org.hisp.dhis.analytics.AnalyticsTableType.OWNERSHIP;
+import static org.hisp.dhis.analytics.AnalyticsTableType.TRACKED_ENTITY_INSTANCE;
+import static org.hisp.dhis.analytics.AnalyticsTableType.TRACKED_ENTITY_INSTANCE_ENROLLMENTS;
+import static org.hisp.dhis.analytics.AnalyticsTableType.TRACKED_ENTITY_INSTANCE_EVENTS;
 import static org.hisp.dhis.common.DhisApiVersion.ALL;
 import static org.hisp.dhis.common.DhisApiVersion.DEFAULT;
 import static org.hisp.dhis.dxf2.webmessage.WebMessageUtils.createWebMessage;
@@ -90,7 +94,7 @@ public class ResourceTableController
         @RequestParam( required = false ) boolean skipAggregate,
         @RequestParam( required = false ) boolean skipEvents,
         @RequestParam( required = false ) boolean skipEnrollment,
-        @RequestParam( required = false ) boolean skipTei,
+        @RequestParam( required = false ) boolean executeTei,
         @RequestParam( required = false ) boolean skipOrgUnitOwnership,
         @RequestParam( required = false ) Integer lastYears )
     {
@@ -116,14 +120,20 @@ public class ResourceTableController
 
         if ( skipOrgUnitOwnership )
         {
-            skipTableTypes.add( AnalyticsTableType.OWNERSHIP );
+            skipTableTypes.add( OWNERSHIP );
         }
 
-        if ( skipTei )
+        if ( !executeTei )
         {
-            skipTableTypes.add( AnalyticsTableType.TRACKED_ENTITY_INSTANCE );
-            skipTableTypes.add( AnalyticsTableType.TRACKED_ENTITY_INSTANCE_EVENTS );
-            skipTableTypes.add( AnalyticsTableType.TRACKED_ENTITY_INSTANCE_ENROLLMENTS );
+            skipTableTypes.add( TRACKED_ENTITY_INSTANCE );
+            skipTableTypes.add( TRACKED_ENTITY_INSTANCE_EVENTS );
+            skipTableTypes.add( TRACKED_ENTITY_INSTANCE_ENROLLMENTS );
+        }
+        else
+        {
+            // Ensure these tables are never skipped, as they are needed for the TEI tables/queries.
+            skipTableTypes.remove( EVENT );
+            skipTableTypes.remove( ENROLLMENT );
         }
 
         AnalyticsJobParameters analyticsJobParameters = new AnalyticsJobParameters( lastYears, skipTableTypes,
