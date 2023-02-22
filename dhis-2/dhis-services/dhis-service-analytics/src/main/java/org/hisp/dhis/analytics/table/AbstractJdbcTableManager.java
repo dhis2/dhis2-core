70365eda44d576f8d36431f293158b53edf56030
Abyot Asalefew Gizaw <abyota@gmail.com>
2023-02-22 15:28:14 +0100

    feat: Multi-calendar support in resource tables and analytics engine (#12247)

--- dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/table/AbstractJdbcTableManager.java
+++ dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/table/AbstractJdbcTableManager.java
@@ -57,7 +57,6 @@ import org.hisp.dhis.analytics.AnalyticsTablePhase;
 import org.hisp.dhis.analytics.AnalyticsTableType;
 import org.hisp.dhis.analytics.AnalyticsTableUpdateParams;
 import org.hisp.dhis.analytics.partition.PartitionManager;
-import org.hisp.dhis.calendar.Calendar;
 import org.hisp.dhis.category.CategoryService;
 import org.hisp.dhis.common.IdentifiableObjectManager;
 import org.hisp.dhis.common.ValueType;
@@ -70,6 +69,7 @@ import org.hisp.dhis.jdbc.StatementBuilder;
 import org.hisp.dhis.organisationunit.OrganisationUnitGroupSet;
 import org.hisp.dhis.organisationunit.OrganisationUnitLevel;
 import org.hisp.dhis.organisationunit.OrganisationUnitService;
+import org.hisp.dhis.period.PeriodDataProvider;
 import org.hisp.dhis.period.PeriodType;
 import org.hisp.dhis.resourcetable.ResourceTableService;
 import org.hisp.dhis.setting.SettingKey;
@@ -135,6 +135,8 @@ public abstract class AbstractJdbcTableManager
     protected final JdbcTemplate jdbcTemplate;
 
     protected final AnalyticsExportSettings analyticsExportSettings;
+
+    protected final PeriodDataProvider periodDataProvider;
 
     private static final String WITH_AUTOVACUUM_ENABLED_FALSE = "with(autovacuum_enabled = false)";
 
@@ -431,7 +433,6 @@ public abstract class AbstractJdbcTableManager
     protected AnalyticsTable getRegularAnalyticsTable( AnalyticsTableUpdateParams params, List<Integer> dataYears,
         List<AnalyticsTableColumn> dimensionColumns, List<AnalyticsTableColumn> valueColumns )
     {
-        Calendar calendar = PeriodType.getCalendar();
 
         List<Integer> years = ListUtils.mutableCopy( dataYears );
 
@@ -441,8 +442,8 @@ public abstract class AbstractJdbcTableManager
 
         for ( Integer year : years )
         {
-            table.addPartitionTable( year, PartitionUtils.getStartDate( calendar, year ),
-                PartitionUtils.getEndDate( calendar, year ) );
+            table.addPartitionTable( year, PartitionUtils.getStartDate( year ),
+                PartitionUtils.getEndDate( year ) );
         }
 
         return table;
