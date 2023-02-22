6e370663dfd1b73c587f72ae82f7a15123215c8f
Jan Bernitt <jaanbernitt@gmail.com>
2023-02-22 17:27:20 +0100

    fix: provide displayName for all data integrity checks [DHIS2-14770] (#13102)

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/dataintegrity/DataIntegrityCheck.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/dataintegrity/DataIntegrityCheck.java
@@ -58,6 +58,9 @@ public final class DataIntegrityCheck implements Serializable
     private final String name;
 
     @JsonProperty
+    private final String displayName;
+
+    @JsonProperty
     private final String section;
 
     @JsonProperty
