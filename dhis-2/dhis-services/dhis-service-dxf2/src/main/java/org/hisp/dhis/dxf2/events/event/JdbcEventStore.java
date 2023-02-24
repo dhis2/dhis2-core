5ed8f8228834141bc6bbd4084b50c7bc5406fe0f
muilpp <muilpp@gmail.com>
2023-02-24 10:50:06 +0100

    fix: Respect events sort order [DHIS2-13735]

--- dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/events/event/JdbcEventStore.java
+++ dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/events/event/JdbcEventStore.java
@@ -1932,11 +1932,6 @@ public class JdbcEventStore implements EventStore
             }
         }
 
-        for ( OrderParam order : params.getAttributeOrders() )
-        {
-            orderFields.add( order.getField() + "_value " + order.getDirection() );
-        }
-
         for ( OrderParam order : params.getOrders() )
         {
             if ( QUERY_PARAM_COL_MAP.containsKey( order.getField() ) )
@@ -1944,6 +1939,10 @@ public class JdbcEventStore implements EventStore
                 String orderText = QUERY_PARAM_COL_MAP.get( order.getField() );
                 orderText += " " + (order.getDirection().isAscending() ? "asc" : "desc");
                 orderFields.add( orderText );
+            }
+            else if ( params.getAttributeOrders().contains( order ) )
+            {
+                orderFields.add( order.getField() + "_value " + order.getDirection() );
             }
         }
 
