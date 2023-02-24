d47d43e3ac3ecbbfb93b1a11fac96a74f319bef6
zubaira <zubair@dhis2.org>
2023-02-24 07:09:01 +0100

    use getLongDateString

--- dhis-2/dhis-services/dhis-service-dxf2/src/test/java/org/hisp/dhis/dxf2/events/EventImportTest.java
+++ dhis-2/dhis-services/dhis-service-dxf2/src/test/java/org/hisp/dhis/dxf2/events/EventImportTest.java
@@ -108,7 +108,7 @@ import com.google.common.collect.Sets;
  */
 class EventImportTest extends TransactionalIntegrationTest
 {
-    private static final String DUE_DATE = "2022-12-12T10:25:40.000";
+    private static final String DUE_DATE = "2021-02-28T13:05:00";
 
     @Autowired
     private EventService eventService;
@@ -467,7 +467,7 @@ class EventImportTest extends TransactionalIntegrationTest
 
         ProgramStageInstance psi = programStageInstanceService.getProgramStageInstance( eventUid );
 
-        assertEquals( DUE_DATE, DateUtils.getIso8601NoTz( psi.getDueDate() ) );
+        assertEquals( DUE_DATE, DateUtils.getLongDateString( psi.getDueDate() ) );
     }
 
     @Test
