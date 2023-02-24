f214c61b294bf98cebcc26054c6b17efe5e7e1a5
zubaira <zubair@dhis2.org>
2023-02-24 12:52:21 +0100

    user optionset name instead of code

--- dhis-2/dhis-services/dhis-service-program-rule/src/main/java/org/hisp/dhis/programrule/engine/DefaultProgramRuleEntityMapperService.java
+++ dhis-2/dhis-services/dhis-service-program-rule/src/main/java/org/hisp/dhis/programrule/engine/DefaultProgramRuleEntityMapperService.java
@@ -38,10 +38,13 @@ import lombok.extern.slf4j.Slf4j;
 
 import org.apache.commons.lang3.ObjectUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.hisp.dhis.common.BaseIdentifiableObject;
 import org.hisp.dhis.common.ValueType;
 import org.hisp.dhis.constant.ConstantService;
 import org.hisp.dhis.dataelement.DataElement;
 import org.hisp.dhis.i18n.I18nManager;
+import org.hisp.dhis.option.Option;
+import org.hisp.dhis.option.OptionSet;
 import org.hisp.dhis.program.ProgramInstance;
 import org.hisp.dhis.program.ProgramStageInstance;
 import org.hisp.dhis.programrule.*;
@@ -234,9 +237,9 @@ public class DefaultProgramRuleEntityMapperService implements ProgramRuleEntityM
                 .build() ) );
 
         // program variables
-        RuleEngineUtils.ENV_VARIABLES.entrySet().forEach( var -> itemStore.put( var.getKey(), DataItem.builder()
-            .value( ObjectUtils.firstNonNull( i18nManager.getI18n().getString( var.getKey() ), var.getKey() ) )
-            .valueType( var.getValue() )
+        RuleEngineUtils.ENV_VARIABLES.forEach( ( key, value ) -> itemStore.put( key, DataItem.builder()
+            .value( ObjectUtils.firstNonNull( i18nManager.getI18n().getString( key ), key ) )
+            .valueType( value )
             .build() ) );
 
         return itemStore;
@@ -305,6 +308,17 @@ public class DefaultProgramRuleEntityMapperService implements ProgramRuleEntityM
             return null;
         }
 
+        List<ProgramRuleVariable> programRuleVariables = programRuleVariableService
+            .getProgramRuleVariable( psi.getProgramStage().getProgram() );
+
+        Map<String, ProgramRuleVariable> dataElementLinkedToProgramRuleVariables = programRuleVariables
+            .stream().collect( Collectors.toMap( programRuleVariable -> programRuleVariable.getDataElement().getUid(),
+                programRuleVariable -> programRuleVariable ) );
+
+        Map<String, DataElement> dataElementWithOptionSet = programRuleVariables.stream()
+            .map( ProgramRuleVariable::getDataElement ).filter( DataElement::hasOptionSet )
+            .collect( Collectors.toMap( BaseIdentifiableObject::getUid, de -> de ) );
+
         String orgUnit = getOrgUnit( psi );
         String orgUnitCode = getOrgUnitCode( psi );
 
@@ -316,8 +330,31 @@ public class DefaultProgramRuleEntityMapperService implements ProgramRuleEntityM
                 .stream()
                 .filter( Objects::nonNull )
                 .filter( dv -> dv.getValue() != null )
-                .map( dv -> RuleDataValue.create( ObjectUtils.defaultIfNull( psi.getExecutionDate(), psi.getDueDate() ),
-                    psi.getProgramStage().getUid(), dv.getDataElement(), dv.getValue() ) )
+                .map( dv -> {
+
+                    if ( dataElementWithOptionSet.containsKey( dv.getDataElement() )
+                        && dataElementLinkedToProgramRuleVariables.containsKey( dv.getDataElement() ) )
+                    {
+                        ProgramRuleVariable prv = dataElementLinkedToProgramRuleVariables.get( dv.getDataElement() );
+
+                        if ( !prv.getUseCodeForOptionSet() )
+                        {
+                            DataElement dataElement = dataElementWithOptionSet.get( dv.getDataElement() );
+
+                            OptionSet optionSet = dataElement.getOptionSet();
+
+                            Option option = optionSet.getOptionByCode( dv.getValue() );
+
+                            return RuleDataValue.create(
+                                ObjectUtils.defaultIfNull( psi.getExecutionDate(), psi.getDueDate() ),
+                                psi.getProgramStage().getUid(), dv.getDataElement(), option.getName() );
+                        }
+                    }
+
+                    return RuleDataValue.create( ObjectUtils.defaultIfNull( psi.getExecutionDate(), psi.getDueDate() ),
+                        psi.getProgramStage().getUid(), dv.getDataElement(), dv.getValue() );
+
+                } )
                 .collect( Collectors.toList() ),
             psi.getProgramStage().getName(), ObjectUtils.defaultIfNull( psi.getCompletedDate(), null ) );
     }
