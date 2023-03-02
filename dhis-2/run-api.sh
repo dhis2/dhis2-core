27aab82b8410791051fe06142c5c12836fbdde5c
Lars Helge Øverland <lars@dhis2.org>
2023-03-02 08:14:19 +0100

    fix: Options for run_api bash script for embedded Jetty (#13188)
    Refactors the run_api.sh script. Moves code into functions. Adds support for options with values, instead of relying on the order of arguments on the command line. Adds an option for setting the DHIS2_HOME variable. Adds an option for skipping compilation, to avoid having to press a key on every run.

--- dhis-2/run-api.sh
+++ dhis-2/run-api.sh
@@ -1,59 +1,96 @@
 #!/usr/bin/env bash
 
-set -e
+# Command for running DHIS 2 in an embedded Jetty container
 
 # Requires JDK 11
 
+# Supported options:
+#
+#   -d <directory> DHIS 2 home directory
+#   -h <hostname>  Hostname (default is localhost)
+#   -p <port>      Port number (default is 9090)
+#   -s             Skip compilation of source code
+
+set -e
+
+# DHIS 2 home directory
+DHIS2_HOME_DIR="/opt/dhis2"
 # Hostname or IP for DHIS2/Jetty to listen
-DHIS2_HOSTNAME=localhost
+DHIS2_HOSTNAME="localhost"
 # Port number for DHIS2/Jetty to listen
-DEFAULT_DHIS2_PORT=9090
-DEFAULT_DHIS2_HOME=/opt/dhis2
+DHIS2_PORT=9090
+# Skip compilation of DHIS 2 source code
+SKIP_COMPILE=0
 
-DHIS2_PORT=${2:-$DEFAULT_DHIS2_PORT}
-#DHIS2_HOME=${1:-$DEFAULT_DHIS2_HOME}
-
-echo -e "Usage: run-api.sh ([DHIS2_HOME_FOLDER] [DHIS2_PORT])\n"
-echo -e "Note: JDK 11 or later is required!\n"
-# Define DHIS2_HOME folder here or set it before you run this script
-if [[ -z "${DHIS2_HOME}" ]]; then
-  if [ -n "$1" ]; then
-    echo "DHIS2_HOME environment variable is not set, using supplied argument nr 1."
-    DHIS2_HOME=$1
-  else
-    echo -e "DHIS2_HOME is not set and no argument supplied either, using default '/opt/dhis2' as the DHIS2 home folder.\n"
-    DHIS2_HOME=$DEFAULT_DHIS2_HOME
-  fi
-else
-  echo -e "Environment variable DHIS2_HOME is set.\n"
+# Set DHIS 2 home directory to DHIS2_HOME env variable if set
+if [[ -v "$DHIS2_HOME" ]]; then
+  DHIS2_HOME_DIR=$DHIS2_HOME
 fi
 
-echo "JAVA_HOME: $JAVA_HOME"
-echo "DHIS2_HOME: $DHIS2_HOME"
-echo "Hostname: $DHIS2_HOSTNAME"
-echo -e "Port: $DHIS2_PORT\n"
+# Read command line options
+while getopts "d:h:p:s" OPT; do
+  case "$OPT" in
+    d)
+      DHIS2_HOME_DIR=$OPTARG
+      ;;
+    h)
+      DHIS2_HOSTNAME=$OPTARG
+      ;;
+    p)
+      DHIS2_PORT=$OPTARG
+      ;;
+    s)
+      SKIP_COMPILE=1
+      ;;
+    ?)
+      echo "Usage: $0 [-d dhis2_home] [-h hostname] [-p port] [-s]" >&2
+      echo "  -d <directory> DHIS 2 home directory" >&2
+      echo "  -h <hostname>  Hostname (default is localhost)" >&2
+      echo "  -p <port>      Port number (default is 9090)" >&2
+      echo "  -s             Skip compilation of source code" >&2
+      exit 1
+      ;;
+  esac
+done
+shift "$(($OPTIND -1))"
 
-[ ! -d $DHIS2_HOME ] && echo "DHIS2_HOME directory '$DHIS2_HOME' DOES NOT exists, aborting..." && exit 1;
-[ ! -f "$DHIS2_HOME/dhis.conf" ] && echo "dhis.conf in directory '$DHIS2_HOME' DOES NOT exists, aborting..." && exit 1;
-
-read -p "Do you want to skip compile? (if yes press y/Y) " -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]; then
+# Start DHIS 2 in embedded Jetty container
+function start_dhis2() {
   java \
-    -Ddhis2.home=$DHIS2_HOME \
+    -Ddhis2.home=$DHIS2_HOME_DIR \
     -Djetty.host=$DHIS2_HOSTNAME \
     -Djetty.http.port=$DHIS2_PORT \
     -jar "$(dirname "$0")/dhis-web-embedded-jetty/target/dhis-web-embedded-jetty.jar"
-  exit 0;
-fi
+}
 
-mvn clean install \
+# Build and install DHIS 2 source code without running tests
+function build_dhis2() {
+  mvn clean install \
     -f "$(dirname "$0")/pom.xml" \
     --batch-mode \
     -Pdev -T 100C \
     -DskipTests -Dmaven.test.skip=true -Dmaven.site.skip=true -Dmaven.javadoc.skip=true
-java \
-    -Ddhis2.home=$DHIS2_HOME \
-    -Djetty.host=$DHIS2_HOSTNAME \
-    -Djetty.http.port=$DHIS2_PORT \
-    -jar "$(dirname "$0")/dhis-web-embedded-jetty/target/dhis-web-embedded-jetty.jar"
+}
+
+echo -e "Note: JDK 11 or later is required!\n"
+
+# Print variables
+echo "JAVA_HOME: $JAVA_HOME"
+echo "DHIS2_HOME_DIR: $DHIS2_HOME"
+echo "HOSTNAME: $DHIS2_HOSTNAME"
+echo "PORT: $DHIS2_PORT"
+echo "SKIP_COMPILE: $SKIP_COMPILE"
+echo ""
+
+# Verify DHIS2_HOME variable
+[ ! -d $DHIS2_HOME_DIR ] && echo "DHIS2_HOME directory '$DHIS2_HOME' does not exist, aborting." && exit 1;
+[ ! -f "$DHIS2_HOME_DIR/dhis.conf" ] && echo "dhis.conf in directory '$DHIS2_HOME_DIR' does not exist, aborting." && exit 1;
+
+# Compile unless skip compile flag is set
+if [[ $SKIP_COMPILE == 0 ]]; then
+  build_dhis2
+fi
+
+# Start DHIS 2
+start_dhis2
+exit 0
