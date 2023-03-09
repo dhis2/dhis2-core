92a8cb8fc5be83eb9cef8a2f5b6a877a92734e44
Jan Bernitt <jaanbernitt@gmail.com>
2023-03-09 10:11:22 +0100

    fix: period date range check should consider end date as full day [DHIS2-14591] (#13229)
    * fix: period date range check should consider end date as full day [DHIS2-14591]
    
    * chore: consistent period range check and remove of unused methods [DHIS2-14591]
    
    * test: added new test scenarios [DHIS2-14591]

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/dataset/DataSet.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/dataset/DataSet.java
@@ -27,6 +27,8 @@
  */
 package org.hisp.dhis.dataset;
 
+import static org.hisp.dhis.util.DateUtils.addDays;
+
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -63,7 +65,6 @@ import org.hisp.dhis.schema.annotation.PropertyRange;
 import org.hisp.dhis.security.Authorities;
 import org.hisp.dhis.user.User;
 import org.hisp.dhis.user.UserGroup;
-import org.joda.time.DateTime;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
@@ -550,16 +551,13 @@ public class DataSet
      */
     public boolean isLocked( User user, Period period, Date now )
     {
-        if ( user != null && user.isAuthorized( Authorities.F_EDIT_EXPIRED.getAuthority() ) )
+        if ( expiryDays == DataSet.NO_EXPIRY
+            || user != null && user.isAuthorized( Authorities.F_EDIT_EXPIRED.getAuthority() ) )
         {
             return false;
         }
-
         Date date = now != null ? now : new Date();
-
-        return expiryDays != DataSet.NO_EXPIRY
-            && !Period.isDateInTimeFrame( null, new DateTime( period.getEndDate() ).plusDays( expiryDays ).toDate(),
-                date );
+        return !Period.isDateInTimeFrame( null, addDays( period.getEndDate(), expiryDays ), date );
     }
 
     /**
