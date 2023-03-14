df34e420ebd4043bcffa1e05922bbf824879fa64
Enrico <enrico@dhis2.org>
2023-03-14 09:06:10 +0100

    Fix test

--- dhis-2/dhis-services/dhis-service-program-rule/src/test/java/org/hisp/dhis/programrule/engine/ProgramRuleEngineServiceTest.java
+++ dhis-2/dhis-services/dhis-service-program-rule/src/test/java/org/hisp/dhis/programrule/engine/ProgramRuleEngineServiceTest.java
@@ -174,7 +174,7 @@ class ProgramRuleEngineServiceTest extends DhisConvenienceTest
 
         when( programInstanceService.getProgramInstance( anyLong() ) ).thenReturn( programInstance );
         when( programRuleEngine.evaluate( any(), any(), any() ) ).thenReturn( effects );
-        when( programRuleEngine.getProgramRules( any(), any() ) ).thenReturn( List.of( programRuleA ) );
+        when( programRuleEngine.getProgramRules( any() ) ).thenReturn( List.of( programRuleA ) );
 
         setProgramRuleActionType_SendMessage();
 
