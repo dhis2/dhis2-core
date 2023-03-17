9da6ec54f650049a47e80c1fea4b6c48c2de6186
teleivo <teleivo@users.noreply.github.com>
2023-03-17 11:22:24 +0100

    fix: event pageSize is not respected with more than 1 category DHIS2-13135 (#13253) (#13323)
    * fix: event pageSize is not respected with more than 1 category
    
    * orgUnitIdScheme is not respected
    * order by occuredAt uses enrollments occuredAt (pi.incidentdate)
      instead of events occuredAt (psi.executiondate)
    
    * chore: simplify query
    
    we can do the aggregation of CO strings, count and ACL check in one go
    
    * chore: remove TODO
    
    * chore: remove unused param
    
    * test: create org.dhis.feebdack.Assertions

--- dhis-2/dhis-services/dhis-service-tracker/src/test/java/org/hisp/dhis/tracker/EventExporterTest.java
+++ dhis-2/dhis-services/dhis-service-tracker/src/test/java/org/hisp/dhis/tracker/EventExporterTest.java
@@ -28,25 +28,40 @@
 package org.hisp.dhis.tracker;
 
 import static org.hisp.dhis.util.DateUtils.parseDate;
+import static org.hisp.dhis.utils.Assertions.assertContains;
 import static org.hisp.dhis.utils.Assertions.assertContainsOnly;
 import static org.hisp.dhis.utils.Assertions.assertIsEmpty;
+import static org.hisp.dhis.utils.Assertions.assertStartsWith;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
+import org.hisp.dhis.category.CategoryOption;
+import org.hisp.dhis.category.CategoryOptionCombo;
+import org.hisp.dhis.common.IdSchemes;
+import org.hisp.dhis.common.IdentifiableObject;
 import org.hisp.dhis.common.IdentifiableObjectManager;
+import org.hisp.dhis.common.Pager;
+import org.hisp.dhis.common.SlimPager;
 import org.hisp.dhis.dxf2.events.event.Event;
 import org.hisp.dhis.dxf2.events.event.EventSearchParams;
 import org.hisp.dhis.dxf2.events.event.EventService;
 import org.hisp.dhis.dxf2.events.event.Events;
 import org.hisp.dhis.organisationunit.OrganisationUnit;
+import org.hisp.dhis.program.Program;
 import org.hisp.dhis.user.User;
+import org.hisp.dhis.webapi.controller.event.mapper.OrderParam;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import com.google.common.collect.Sets;
 
 /**
  * @author Enrico Colasante
@@ -63,19 +78,31 @@ class EventExporterTest extends TrackerTest
     @Autowired
     private IdentifiableObjectManager manager;
 
-    private User userA;
-
     private OrganisationUnit orgUnit;
+
+    final Function<EventSearchParams, List<String>> eventsFunction = ( params ) -> eventService.getEvents( params )
+        .getEvents()
+        .stream().map( Event::getEvent ).collect( Collectors.toList() );
 
     @Override
     protected void initTest()
         throws IOException
     {
         setUpMetadata( "tracker/simple_metadata.json" );
-        userA = userService.getUser( "M5zQapPyTZI" );
-        TrackerImportParams enrollmentParams = fromJson( "tracker/event_and_enrollment.json", userA.getUid() );
-        assertNoImportErrors( trackerImportService.importTracker( enrollmentParams ) );
+        User superuser = userService.getUser( "M5zQapPyTZI" );
+        assertNoImportErrors(
+            trackerImportService.importTracker( fromJson( "tracker/event_and_enrollment.json", superuser.getUid() ) ) );
         orgUnit = manager.get( OrganisationUnit.class, "h4w96yEMlzO" );
+
+        // to test that events are only returned if the user has read access to
+        // ALL COs of an events COC
+        CategoryOption co1 = get( CategoryOption.class, "OUUdG3sdOqb" );
+        co1.getSharing().setOwner( "M5zQapPyTZI" );
+        manager.update( co1 );
+        CategoryOption co2 = get( CategoryOption.class, "yMj2MnmNI8L" );
+        co2.getSharing().setOwner( "o1HMTIzBGo7" );
+        manager.update( co2 );
+
         manager.flush();
     }
 
@@ -84,9 +111,11 @@ class EventExporterTest extends TrackerTest
     {
         EventSearchParams params = new EventSearchParams();
         params.setOrgUnit( orgUnit );
-        Events events = eventService.getEvents( params );
+
+        List<String> events = eventsFunction.apply( params );
+
         assertNotNull( events );
-        assertEquals( 2, events.getEvents().size() );
+        assertContainsOnly( events, "pTzf9KYMk72", "D9PbzJY8bJM" );
     }
 
     @Test
@@ -94,10 +123,11 @@ class EventExporterTest extends TrackerTest
     {
         EventSearchParams params = new EventSearchParams();
         params.setOrgUnit( orgUnit );
-        params.setProgramInstances( Sets.newHashSet( "TvctPPhpD8z" ) );
-        Events events = eventService.getEvents( params );
-        assertNotNull( events );
-        assertEquals( 1, events.getEvents().size() );
+        params.setProgramInstances( Set.of( "nxP7UnKhomJ" ) );
+
+        List<String> events = eventsFunction.apply( params );
+
+        assertContainsOnly( events, "pTzf9KYMk72" );
     }
 
     @Test
@@ -120,11 +150,9 @@ class EventExporterTest extends TrackerTest
         params.setOrgUnit( orgUnit );
         params.setDueDateStart( parseDate( "2018-02-28T13:05:00.000" ) );
 
-        Events events = eventService.getEvents( params );
+        List<String> events = eventsFunction.apply( params );
 
-        assertNotNull( events );
-        assertContainsOnly( List.of( "D9PbzJY8bJM", "D9PbzJY8bJO" ),
-            events.getEvents().stream().map( Event::getEvent ).toArray( String[]::new ) );
+        assertContainsOnly( events, "D9PbzJY8bJM", "pTzf9KYMk72" );
     }
 
     @Test
@@ -147,10 +175,252 @@ class EventExporterTest extends TrackerTest
         params.setOrgUnit( orgUnit );
         params.setDueDateEnd( parseDate( "2021-02-28T13:05:00.000" ) );
 
+        List<String> events = eventsFunction.apply( params );
+
+        assertContainsOnly( events, "D9PbzJY8bJM", "pTzf9KYMk72" );
+    }
+
+    @Test
+    void shouldReturnEventsNonSuperUserIsOwnerOrHasUserAccess()
+    {
+        // given events have a COC which has a CO which the
+        // user owns yMj2MnmNI8L and has user read access to OUUdG3sdOqb
+        injectSecurityContext( userService.getUser( "o1HMTIzBGo7" ) );
+
+        EventSearchParams params = new EventSearchParams();
+        params.setOrgUnit( get( OrganisationUnit.class, "DiszpKrYNg8" ) );
+        params.setEvents( Set.of( "lumVtWwwy0O", "cadc5eGj0j7" ) );
+
         Events events = eventService.getEvents( params );
 
-        assertNotNull( events );
-        assertContainsOnly( List.of( "D9PbzJY8bJM", "D9PbzJY8bJO" ),
-            events.getEvents().stream().map( Event::getEvent ).toArray( String[]::new ) );
+        assertContainsOnly( eventUids( events ), "lumVtWwwy0O", "cadc5eGj0j7" );
+        List<Executable> executables = events.getEvents().stream()
+            .map( e -> (Executable) () -> assertEquals( 2, e.getOptionSize(),
+                String.format( "got category options %s", e.getAttributeCategoryOptions() ) ) )
+            .collect( Collectors.toList() );
+        assertAll( "all events should have the optionSize set which is the number of COs in the COC", executables );
+    }
+
+    @Test
+    void shouldReturnNoEventsGivenUserHasNoAccess()
+    {
+        // given events have a COC which has a CO (OUUdG3sdOqb/yMj2MnmNI8L)
+        // which are not publicly readable, user is not the owner and has no
+        // user access
+        injectSecurityContext( userService.getUser( "CYVgFNKCaUS" ) );
+
+        EventSearchParams params = new EventSearchParams();
+        params.setOrgUnit( get( OrganisationUnit.class, "DiszpKrYNg8" ) );
+        params.setEvents( Set.of( "lumVtWwwy0O", "cadc5eGj0j7" ) );
+
+        List<String> events = eventsFunction.apply( params );
+
+        assertIsEmpty( events );
+    }
+
+    @Test
+    void shouldReturnPublicEventsWithMultipleCategoryOptionsGivenNonDefaultPageSize()
+    {
+        OrganisationUnit orgUnit = get( OrganisationUnit.class, "DiszpKrYNg8" );
+        Program program = get( Program.class, "iS7eutanDry" );
+
+        EventSearchParams params = new EventSearchParams();
+        params.setOrgUnit( orgUnit );
+        params.setProgram( program );
+
+        params.setOrders( List.of( orderBy( "occurredAt", OrderParam.SortDirection.DESC ) ) );
+        params.setPage( 1 );
+        params.setPageSize( 3 );
+
+        Events firstPage = eventService.getEvents( params );
+
+        assertAll( "first page",
+            () -> assertSlimPager( 1, 3, false, firstPage ),
+            () -> assertEquals( List.of( "ck7DzdxqLqA", "OTmjvJDn0Fu", "kWjSezkXHVp" ), eventUids( firstPage ) ) );
+
+        params = new EventSearchParams();
+        params.setOrgUnit( orgUnit );
+        params.setProgram( program );
+
+        params.setOrders( List.of( orderBy( "occurredAt", OrderParam.SortDirection.DESC ) ) );
+        params.setPage( 2 );
+        params.setPageSize( 3 );
+
+        Events secondPage = eventService.getEvents( params );
+
+        assertAll( "second (last) page",
+            () -> assertSlimPager( 2, 3, true, secondPage ),
+            () -> assertEquals( List.of( "lumVtWwwy0O", "QRYjLTiJTrA", "cadc5eGj0j7" ), eventUids( secondPage ) ) );
+
+        params = new EventSearchParams();
+        params.setOrgUnit( orgUnit );
+        params.setProgram( program );
+
+        params.setOrders( List.of( orderBy( "occurredAt", OrderParam.SortDirection.DESC ) ) );
+        params.setPage( 3 );
+        params.setPageSize( 3 );
+
+        assertIsEmpty( eventsFunction.apply( params ) );
+    }
+
+    private static OrderParam orderBy( String field, OrderParam.SortDirection direction )
+    {
+        return OrderParam.builder().field( field ).direction( direction ).build();
+    }
+
+    @Test
+    void shouldReturnEventsWithMultipleCategoryOptionsGivenNonDefaultPageSizeAndTotalPages()
+    {
+        OrganisationUnit orgUnit = get( OrganisationUnit.class, "DiszpKrYNg8" );
+        Program program = get( Program.class, "iS7eutanDry" );
+
+        EventSearchParams params = new EventSearchParams();
+        params.setOrgUnit( orgUnit );
+        params.setProgram( program );
+
+        params.setOrders( List.of( orderBy( "occurredAt", OrderParam.SortDirection.DESC ) ) );
+        params.setPage( 1 );
+        params.setPageSize( 2 );
+        params.setTotalPages( true );
+
+        Events events = eventService.getEvents( params );
+
+        assertAll( "first page",
+            () -> assertPager( 1, 2, 6, events ),
+            () -> assertEquals( List.of( "ck7DzdxqLqA", "OTmjvJDn0Fu" ), eventUids( events ) ) );
+    }
+
+    @Test
+    void shouldReturnEventsGivenCategoryOptionCombo()
+    {
+        EventSearchParams params = new EventSearchParams();
+        params.setOrgUnit( get( OrganisationUnit.class, "DiszpKrYNg8" ) );
+        params.setCategoryOptionCombo( get( CategoryOptionCombo.class, "cr89ebDZrac" ) );
+
+        Events events = eventService.getEvents( params );
+
+        assertContainsOnly( eventUids( events ), "kWjSezkXHVp", "OTmjvJDn0Fu" );
+        List<Executable> executables = events.getEvents().stream()
+            .map( e -> (Executable) () -> assertAll( "category options and combo of event " + e.getUid(),
+                () -> assertEquals( "cr89ebDZrac", e.getAttributeOptionCombo() ),
+                () -> assertEquals( "xwZ2u3WyQR0;M58XdOfhiJ7", e.getAttributeCategoryOptions() ),
+                () -> assertEquals( 2, e.getOptionSize(),
+                    String.format( "got category options %s", e.getAttributeCategoryOptions() ) ) ) )
+            .collect( Collectors.toList() );
+        assertAll( "all events should have the same category option combo and options", executables );
+    }
+
+    @Test
+    void shouldFailIfCategoryOptionComboOfGivenEventDoesNotHaveAValueForGivenIdScheme()
+    {
+        EventSearchParams params = new EventSearchParams();
+        params.setOrgUnit( get( OrganisationUnit.class, "DiszpKrYNg8" ) );
+        IdSchemes idSchemes = new IdSchemes();
+        idSchemes.setCategoryOptionComboIdScheme( "ATTRIBUTE:GOLswS44mh8" );
+        params.setIdSchemes( idSchemes );
+        params.setEvents( Set.of( "kWjSezkXHVp" ) );
+
+        IllegalStateException ex = assertThrows( IllegalStateException.class, () -> eventService.getEvents( params ) );
+        assertStartsWith( "CategoryOptionCombo", ex.getMessage() );
+        assertContains( "not have a value assigned for idScheme ATTRIBUTE:GOLswS44mh8", ex.getMessage() );
+    }
+
+    @Test
+    void shouldReturnEventsGivenIdSchemeCode()
+    {
+        EventSearchParams params = new EventSearchParams();
+        params.setOrgUnit( get( OrganisationUnit.class, "DiszpKrYNg8" ) );
+        params.setCategoryOptionCombo( get( CategoryOptionCombo.class, "cr89ebDZrac" ) );
+        IdSchemes idSchemes = new IdSchemes();
+        idSchemes.setProgramIdScheme( "code" );
+        idSchemes.setProgramStageIdScheme( "code" );
+        idSchemes.setOrgUnitIdScheme( "code" );
+        idSchemes.setCategoryOptionComboIdScheme( "code" );
+        params.setIdSchemes( idSchemes );
+
+        Events events = eventService.getEvents( params );
+
+        assertContainsOnly( eventUids( events ), "kWjSezkXHVp", "OTmjvJDn0Fu" );
+        List<Executable> executables = events.getEvents().stream()
+            .map( e -> (Executable) () -> assertAll( "event " + e.getUid(),
+                () -> assertEquals( "multi-program", e.getProgram() ),
+                () -> assertEquals( "multi-stage", e.getProgramStage() ),
+                () -> assertEquals( "DiszpKrYNg8", e.getOrgUnit() ), // TODO(ivo):
+                                                                     // this
+                                                                     // might be
+                                                                     // a bug
+                                                                     // caused
+                                                                     // by
+                                                                     // https://github.com/dhis2/dhis2-core/pull/12518
+                () -> assertEquals( "COC_1153452", e.getAttributeOptionCombo() ),
+                () -> assertEquals( "xwZ2u3WyQR0;M58XdOfhiJ7", e.getAttributeCategoryOptions() ) ) )
+            .collect( Collectors.toList() );
+        assertAll( "all events should have the same category option combo and options", executables );
+    }
+
+    @Test
+    void shouldReturnEventsGivenIdSchemeAttribute()
+    {
+        EventSearchParams params = new EventSearchParams();
+        params.setOrgUnit( get( OrganisationUnit.class, "DiszpKrYNg8" ) );
+        params.setCategoryOptionCombo( get( CategoryOptionCombo.class, "cr89ebDZrac" ) );
+        IdSchemes idSchemes = new IdSchemes();
+        idSchemes.setProgramIdScheme( "ATTRIBUTE:j45AR9cBQKc" );
+        idSchemes.setProgramStageIdScheme( "ATTRIBUTE:j45AR9cBQKc" );
+        idSchemes.setOrgUnitIdScheme( "ATTRIBUTE:j45AR9cBQKc" );
+        idSchemes.setCategoryOptionComboIdScheme( "ATTRIBUTE:j45AR9cBQKc" );
+        params.setIdSchemes( idSchemes );
+
+        Events events = eventService.getEvents( params );
+
+        assertContainsOnly( eventUids( events ), "kWjSezkXHVp", "OTmjvJDn0Fu" );
+        List<Executable> executables = events.getEvents().stream()
+            .map( e -> (Executable) () -> assertAll( "event " + e.getUid(),
+                () -> assertEquals( "multi-program-attribute", e.getProgram() ),
+                () -> assertEquals( "multi-program-stage-attribute", e.getProgramStage() ),
+                () -> assertEquals( "DiszpKrYNg8", e.getOrgUnit() ), // TODO(ivo):
+                                                                     // this
+                                                                     // might be
+                                                                     // a bug
+                                                                     // caused
+                                                                     // by
+                                                                     // https://github.com/dhis2/dhis2-core/pull/12518
+                () -> assertEquals( "COC_1153452-attribute", e.getAttributeOptionCombo() ),
+                () -> assertEquals( "xwZ2u3WyQR0;M58XdOfhiJ7", e.getAttributeCategoryOptions() ) ) )
+            .collect( Collectors.toList() );
+        assertAll( "all events should have the same category option combo and options", executables );
+    }
+
+    private <T extends IdentifiableObject> T get( Class<T> type, String uid )
+    {
+        T t = manager.get( type, uid );
+        assertNotNull( t, () -> String.format( "metadata with uid '%s' should have been created", uid ) );
+        return t;
+    }
+
+    private static List<String> eventUids( Events events )
+    {
+        return events.getEvents()
+            .stream().map( Event::getEvent ).collect( Collectors.toList() );
+    }
+
+    private static void assertSlimPager( int pageNumber, int pageSize, boolean isLast, Events events )
+    {
+        assertInstanceOf( SlimPager.class, events.getPager(), "SlimPager should be returned if totalPages=false" );
+        SlimPager pager = (SlimPager) events.getPager();
+        assertAll( "pagination details",
+            () -> assertEquals( pageNumber, pager.getPage(), "number of current page" ),
+            () -> assertEquals( pageSize, pager.getPageSize(), "page size" ),
+            () -> assertEquals( isLast, pager.isLastPage(),
+                isLast ? "should be the last page" : "should NOT be the last page" ) );
+    }
+
+    private static void assertPager( int pageNumber, int pageSize, int totalCount, Events events )
+    {
+        Pager pager = events.getPager();
+        assertAll( "pagination details",
+            () -> assertEquals( pageNumber, pager.getPage(), "number of current page" ),
+            () -> assertEquals( pageSize, pager.getPageSize(), "page size" ),
+            () -> assertEquals( totalCount, pager.getTotal(), "total page count" ) );
     }
 }
