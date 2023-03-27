17e58403c4d1884e8d95fcf6dbb6b8ea75b3addd
muilpp <muilpp@gmail.com>
2023-03-27 10:10:50 +0200

    fix: Rever order params commit [DHIS2-13725] [2.38]

--- dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/events/event/EventSearchParams.java
+++ dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/events/event/EventSearchParams.java
@@ -155,9 +155,9 @@ public class EventSearchParams
 
     private boolean includeRelationships;
 
-    private List<OrderParam> orders = Collections.emptyList();
+    private List<OrderParam> orders;
 
-    private List<OrderParam> gridOrders = Collections.emptyList();
+    private List<OrderParam> gridOrders;
 
     private boolean includeAttributes;
 
