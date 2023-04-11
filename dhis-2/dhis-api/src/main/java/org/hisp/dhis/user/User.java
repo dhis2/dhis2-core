10119538011f8097d22bf785b7e43dbab58ecb54
netroms <msvanaes@dhis2.org>
2023-04-11 16:33:15 +0800

    fix: backward comp for update analytics dimension restrictions [DHIS2-14870] (#13548)

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/user/User.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/user/User.java
@@ -1592,6 +1592,22 @@ public class User
                 newUser.setUserRoles( userRoles );
             }
 
+            Set<Category> oldCatDimCon = oldUser.getCatDimensionConstraints();
+            Set<CategoryOptionGroupSet> oldCogDimCon = oldUser.getCogsDimensionConstraints();
+
+            Set<Category> newCatDimCon = newUserCredentialsRaw.getCatDimensionConstraints();
+            Set<CategoryOptionGroupSet> newCogDimCon = newUserCredentialsRaw.getCogsDimensionConstraints();
+
+            if ( oldCatDimCon != null && newCatDimCon != null && !oldCatDimCon.equals( newCatDimCon ) )
+            {
+                newUser.setCatDimensionConstraints( newCatDimCon );
+            }
+
+            if ( oldCogDimCon != null && newCogDimCon != null && !oldCogDimCon.equals( newCogDimCon ) )
+            {
+                newUser.setCogsDimensionConstraints( newCogDimCon );
+            }
+
             newUser.removeLegacyUserCredentials();
         }
     }
