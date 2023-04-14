d83b65deba2fc09c71749f1d224bb3a75e5d6d30
Maikel Arabori <51713408+maikelarabori@users.noreply.github.com>
2023-04-14 10:04:00 +0200

    fix: EDIs name/shortName should not be unique [DHIS2-15106] (#13604)
    * fix: EDIs name/shortName should not be unique [DHIS2-15106]
    
    * fix: Migration for EDI [DHIS2-15106]
    
    * fix: Revert migration file [DHIS2-15106]

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.41/V2_41_4__Expression_Dim_Item_Remove_Names_Uniquiness.sql
+++
@@ -1,5 +0,0 @@
--- DHIS2-15106
--- Remove name and shortname uniqueness for the expressiondimensionitem table.
-
-alter table expressiondimensionitem drop constraint if exists expressiondimensionitem_shortname_key;
-alter table expressiondimensionitem drop constraint if exists expressiondimensionitem_name_key;
