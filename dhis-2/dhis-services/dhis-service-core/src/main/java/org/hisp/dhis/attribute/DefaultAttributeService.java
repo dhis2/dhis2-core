e09e3e57d4607632ba9d1550b6383bfeb5df3e9d
Viet Nguyen <viet@dhis2.org>
2023-04-27 00:59:21 +0700

    fix: hibernate lazy collection issue in attributeCache

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/attribute/DefaultAttributeService.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/attribute/DefaultAttributeService.java
@@ -32,7 +32,6 @@ import static com.google.common.base.Preconditions.checkNotNull;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.Set;
 
 import org.hisp.dhis.attribute.exception.NonUniqueAttributeValueException;
@@ -108,8 +107,14 @@ public class DefaultAttributeService
     @Transactional( readOnly = true )
     public Attribute getAttribute( String uid )
     {
-        Optional<Attribute> attribute = attributeCache.get( uid, attr -> attributeStore.getByUid( uid ) );
-        return attribute.orElse( null );
+        return attributeCache.get( uid, attr -> {
+            Attribute attribute = attributeStore.getByUid( uid );
+            if ( attribute != null )
+            {
+                HibernateProxyUtils.unproxy( attribute );
+            }
+            return attribute;
+        } ).orElse( null );
     }
 
     @Override
