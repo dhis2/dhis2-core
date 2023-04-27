df3d184edafc38879a3f9a099daea580bcb477b0
marc <muilpp@gmail.com>
2023-04-27 12:18:19 +0200

    feat: Create custom icon domain [DHIS2-14274] (#13723)
    * feat: Create custom icon domain [DHIS2-14274]
    
    * feat: Create custom icon domain [DHIS2-14274]
    
    * feat: Create custom icon domain [DHIS2-14274]
    
    * feat: Add imgscalr dependency [DHIS2-14274]
    
    * feat: Refactor file resize [DHIS2-14274]
    
    * feat: Delete tmp file when done [DHIS2-14274]
    
    * feat: Encapsulate resize target dimension [DHIS2-14274]
    
    * feat: Extract hardcoded values [DHIS2-14274]

--- dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/FileResourceController.java
+++ dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/FileResourceController.java
@@ -28,6 +28,8 @@
 package org.hisp.dhis.webapi.controller;
 
 import static org.hisp.dhis.dxf2.webmessage.WebMessageUtils.error;
+import static org.hisp.dhis.webapi.utils.FileResourceUtils.resizeToDefaultIconSize;
+import static org.hisp.dhis.webapi.utils.FileResourceUtils.validateCustomIconFile;
 
 import java.io.IOException;
 import java.util.List;
@@ -173,7 +175,16 @@ public class FileResourceController extends AbstractFullReadOnlyController<FileR
         throws WebMessageException,
         IOException
     {
-        FileResource fileResource = fileResourceUtils.saveFileResource( uid, file, domain );
+        FileResource fileResource;
+        if ( domain.equals( FileResourceDomain.CUSTOM_ICON ) )
+        {
+            validateCustomIconFile( file );
+            fileResource = fileResourceUtils.saveFileResource( uid, resizeToDefaultIconSize( file ), domain );
+        }
+        else
+        {
+            fileResource = fileResourceUtils.saveFileResource( uid, file, domain );
+        }
 
         WebMessage webMessage = new WebMessage( Status.OK, HttpStatus.ACCEPTED );
         webMessage.setResponse( new FileResourceWebMessageResponse( fileResource ) );
