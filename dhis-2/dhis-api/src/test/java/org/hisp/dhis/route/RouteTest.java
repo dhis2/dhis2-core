fbbf6ef466e6b35039ae05af384fc14515b831bf
Austin McGee <947888+amcgee@users.noreply.github.com>
2023-04-28 11:46:09 +0200

    feat: add support for subpath wildcards in route urls [DHIS2-15098] (#13710)
    Fixes [DHIS2-15098](https://dhis2.atlassian.net/browse/DHIS2-15098) - support path wildcard suffix `/**`
    Fixes [DHIS2-15191](https://dhis2.atlassian.net/browse/DHIS2-15191) - pass through non-2xx responses
    Fixes [DHIS2-15197](https://dhis2.atlassian.net/browse/DHIS2-15197) - disable cookie store
    Fixes [DHIS2-15198](https://dhis2.atlassian.net/browse/DHIS2-15198) - filter request and response headers to exclude transport-level metadata
    
    This adds support for sub-paths in Route URLs.  Subpaths are only supported if the route is configured with a URL ending in `/**`, and can be accessed via the route's Run API like so - `/api/routes/ABC123/run/sub/path/xyz`, which if route `ABC123` is configured with the URL `https://google.com/**` would proxy the request to `https://google.com/sub/path/xyz`
    
    Attempting to access a subpath for a route which doesn't have the `/**` path wildcard suffix will result in a 400 Bad Request error (I'm not sure if this should be a 404 instead...)
    
    This has been approved for 2.40.0 if we want to support it there (since this makes the routes API quite a bit more useful), though with this implementation it would also be a non-breaking extension to add `/**` support in v 40.1
    
    (cherry picked from commit 8ad5844aab98ead1c744dd57252b779dc49a20bc)

---
+++ dhis-2/dhis-api/src/test/java/org/hisp/dhis/route/RouteTest.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2004-2022, University of Oslo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * Neither the name of the HISP project nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.hisp.dhis.route;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RouteTest
+{
+    Route tldRoute = new Route();
+
+    Route routeWithSubPath = new Route();
+
+    Route routeWithDirectorySubPath = new Route();
+
+    Route routeWithPathWildcard = new Route();
+
+    Route routeWithSubPathAndPathWildcard = new Route();
+
+    @BeforeEach
+    void setUp()
+    {
+        tldRoute.setUrl( "https://thisisatest.com" );
+        routeWithSubPath.setUrl( "https://thisisatest.com/some/path/123" );
+        routeWithDirectorySubPath.setUrl( "https://thisisatest.com/some/path/123/" );
+        routeWithPathWildcard.setUrl( "https://thisisatest.com/**" );
+        routeWithSubPathAndPathWildcard.setUrl( "https://thisisatest.com/sub/path/**" );
+    }
+
+    @AfterEach
+    void tearDown()
+    {
+    }
+
+    @Test
+    void testAllowsSubpaths()
+    {
+        assertFalse( tldRoute.allowsSubpaths() );
+        assertFalse( routeWithSubPath.allowsSubpaths() );
+        assertFalse( routeWithDirectorySubPath.allowsSubpaths() );
+        assertTrue( routeWithPathWildcard.allowsSubpaths() );
+        assertTrue( routeWithSubPathAndPathWildcard.allowsSubpaths() );
+    }
+
+    @Test
+    void testGetBaseUrl()
+    {
+        assertEquals( "https://thisisatest.com", tldRoute.getBaseUrl() );
+        assertEquals( "https://thisisatest.com/some/path/123", routeWithSubPath.getBaseUrl() );
+        assertEquals( "https://thisisatest.com/some/path/123/", routeWithDirectorySubPath.getBaseUrl() );
+        assertEquals( "https://thisisatest.com/", routeWithPathWildcard.getBaseUrl() );
+        assertEquals( "https://thisisatest.com/sub/path/", routeWithSubPathAndPathWildcard.getBaseUrl() );
+    }
+}
