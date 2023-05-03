c56c06ed5824a6f685a39b68510dde217912d57c
Morten Svanaes <msvanaes@dhis2.org>
2023-05-03 16:22:14 +0800

    fix: Remove deprecated Spring Security OAuth2 library

--- dhis-2/dhis-e2e-test/src/test/java/org/hisp/dhis/LoginTests.java
+++ dhis-2/dhis-e2e-test/src/test/java/org/hisp/dhis/LoginTests.java
@@ -52,15 +52,9 @@ public class LoginTests
 {
     private LoginActions loginActions;
 
-    private RestApiActions oauth2Clients;
-
     private UaaActions uaaActions;
 
     private UserActions userActions;
-
-    private String oauthClientId = "demo" + DataGenerator.randomString();
-
-    private String secret = "1e6db50c-0fee-11e5-98d0-3c15c2c6caf6";
 
     private String userName = ("LoginTestsUser" + DataGenerator.randomString()).toLowerCase();
 
@@ -69,88 +63,11 @@ public class LoginTests
     @BeforeAll
     public void preconditions()
     {
-        oauth2Clients = new RestApiActions( "/oAuth2Clients" );
-        uaaActions = new UaaActions();
         loginActions = new LoginActions();
         userActions = new UserActions();
 
         loginActions.loginAsSuperUser();
 
-        addOAuthClient();
-
         userActions.addUser( userName, password );
-    }
-
-    @Test
-    @Disabled
-    public void shouldBeAbleToLoginWithOAuth2()
-    {
-
-        loginActions.addAuthenticationHeader( oauthClientId, secret );
-
-        ApiResponse response = uaaActions.post( "oauth/token", new JsonObject(),
-            new QueryParamsBuilder().addAll( "username=" + userName, "password=" + password, "grant_type=password" ) );
-
-        response.validate().statusCode( 200 )
-            .body( "access_token", notNullValue() )
-            .body( "token_type", notNullValue() )
-            .body( "refresh_token", notNullValue() )
-            .body( "expires_in", notNullValue() )
-            .body( "scope", notNullValue() );
-
-        loginActions.loginWithToken( response.extractString( "access_token" ) );
-
-        loginActions.getLoggedInUserInfo().validate()
-            .statusCode( 200 )
-            .body( "username", equalTo( userName ) );
-    }
-
-    @Test
-    @Disabled
-    public void shouldBeAbleToGetRefreshToken()
-    {
-        loginActions.addAuthenticationHeader( oauthClientId, secret );
-
-        JsonObject object = new JsonObject();
-        object.addProperty( "password", password );
-        object.addProperty( "grant_type", "password" );
-        object.addProperty( "username", userName );
-
-        ApiResponse passwordGrantTypeResponse = uaaActions.post( "oauth/token", new JsonObject(),
-            new QueryParamsBuilder().addAll( "password=" + password, "username=" + userName, "grant_type=password" ) );
-        passwordGrantTypeResponse.validate().statusCode( 200 );
-
-        loginActions.addAuthenticationHeader( oauthClientId, secret );
-        ApiResponse refreshTokenResponse = uaaActions.post( "oauth/token", new JsonObject(), new QueryParamsBuilder()
-            .addAll( "grant_type=refresh_token",
-                "refresh_token=" + passwordGrantTypeResponse.extractString( "refresh_token" ) ) );
-
-        refreshTokenResponse.validate().statusCode( 200 )
-            .body( "access_token", notNullValue() )
-            .body( "token_type", notNullValue() )
-            .body( "refresh_token", notNullValue() )
-            .body( "expires_in", notNullValue() )
-            .body( "scope", notNullValue() )
-            .body( "access_token", not( equalTo( passwordGrantTypeResponse.extractString( "access_token" ) ) ) );
-    }
-
-    private void addOAuthClient()
-    {
-        JsonObject client = new JsonObject();
-        client.addProperty( "name", "OAuth2 client" + DataGenerator.randomString() );
-        client.addProperty( "cid", oauthClientId );
-        client.addProperty( "secret", secret );
-
-        JsonArray grantTypes = new JsonArray();
-
-        grantTypes.add( "password" );
-        grantTypes.add( "refresh_token" );
-        grantTypes.add( "authorization_code" );
-
-        client.add( "grantTypes", grantTypes );
-
-        ApiResponse response = oauth2Clients.post( client );
-
-        ResponseValidationHelper.validateObjectCreation( response );
     }
 }
