567d17818241c10eddb65f4d12eb1072e5232eb7
lucaCambi77 <lucacambi77@gmail.com>
2023-06-08 11:27:30 +0200

    fix: missing foreign key drop (#14175)

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.41/V2_41_16__Fix_hibernate_validate_ddl.sql
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.41/V2_41_16__Fix_hibernate_validate_ddl.sql
@@ -1,4 +1,5 @@
 ALTER TABLE programstageworkinglist alter column description type text;
 ALTER TABLE userrolerestrictions alter column userroleid type int8;
+ALTER TABLE userrolerestrictions DROP CONSTRAINT IF EXISTS fk_userrolerestrictions_userroleid;
 ALTER TABLE userrolerestrictions
     ADD CONSTRAINT fk_userrolerestrictions_userroleid FOREIGN KEY (userroleid) REFERENCES userrole (userroleid);
