4ca895d8f0fa9692ac102dcabdfcbd79966f8fa1
Abyot Asalefew Gizaw <abyota@gmail.com>
2023-06-23 00:20:21 +0200

    Revert "refactor: Minor code refactoring"
    This reverts commit 425f27416450ed43bb6e2fbf7d8f9a6f2bc438f3.

--- dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/tracker/export/TrackerTrackedEntitiesExportController.java
+++ dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/tracker/export/TrackerTrackedEntitiesExportController.java
@@ -132,9 +132,15 @@ public class TrackerTrackedEntitiesExportController
 
         PagingWrapper<ObjectNode> pagingWrapper = new PagingWrapper<>();
 
-        if ( criteria.isPagingRequest() || criteria.isTotalPages() )
+        if ( criteria.isPagingRequest() )
         {
-            Long count = (long) trackedEntityInstanceService.getTrackedEntityInstanceCount( queryParams, true, true );
+
+            Long count = 0L;
+
+            if ( criteria.isTotalPages() )
+            {
+                count = (long) trackedEntityInstanceService.getTrackedEntityInstanceCount( queryParams, true, true );
+            }
 
             Pager pager = new Pager( queryParams.getPageWithDefault(), count, queryParams.getPageSizeWithDefault() );
 
