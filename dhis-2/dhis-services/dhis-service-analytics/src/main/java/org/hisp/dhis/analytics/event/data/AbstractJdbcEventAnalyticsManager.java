449fbdfd0cd34c3f2ce105cbb4b735316c4cf0cc
Maikel Arabori <51713408+maikelarabori@users.noreply.github.com>
2023-06-29 16:20:13 +0200

    fix: Boolean type aggregation [DHIS2-15440] (#14399)

--- dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/event/data/AbstractJdbcEventAnalyticsManager.java
+++ dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/event/data/AbstractJdbcEventAnalyticsManager.java
@@ -729,7 +729,7 @@ public abstract class AbstractJdbcEventAnalyticsManager
         {
             return function + "(value)";
         }
-        else if ( params.hasNumericValueDimension() )
+        else if ( params.hasNumericValueDimension() || params.hasBooleanValueDimension() )
         {
             String expression = quoteAlias( params.getValue().getUid() );
 
