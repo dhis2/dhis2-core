89c137dfd43f523e6443078abfb39cb2f50c639e
muilpp <muilpp@gmail.com>
2023-07-25 15:45:28 +0200

    fix: Check for empty list on ou mode violation [DHIS2-15394] [2.39]

--- dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/events/event/AbstractEventService.java
+++ dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/events/event/AbstractEventService.java
@@ -981,7 +981,7 @@ public abstract class AbstractEventService implements EventService {
       case SELECTED:
       case DESCENDANTS:
         violation =
-            params.getAccessibleOrgUnits() == null
+            params.getAccessibleOrgUnits().isEmpty()
                 ? "Organisation unit is required for ouMode: " + params.getOrgUnitSelectionMode()
                 : null;
         break;
