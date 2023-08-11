2bad17c21fba8209537f15e0036a399608ea281f
netroms <msvanaes@dhis2.org>
2023-08-11 17:27:56 +0800

    fix: not possible to change max concurrent user sessions [DHIS2-13453] (#14875)
    * fix: not possible to change max concurrent user sessions

--- dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/security/config/DhisWebApiWebSecurityConfig.java
+++ dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/security/config/DhisWebApiWebSecurityConfig.java
@@ -83,6 +83,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurer;
@@ -135,7 +136,7 @@ public class DhisWebApiWebSecurityConfig {
   @Autowired public DataSource dataSource;
 
   @Bean
-  public SessionRegistryImpl sessionRegistry() {
+  public SessionRegistry sessionRegistry() {
     return new SessionRegistryImpl();
   }
 
@@ -482,20 +483,6 @@ public class DhisWebApiWebSecurityConfig {
           .maximumSessions(
               Integer.parseInt(dhisConfig.getProperty(ConfigurationKey.MAX_SESSIONS_PER_USER)))
           .expiredUrl("/dhis-web-commons-security/logout.action");
-    }
-
-    private AuthenticationEntryPoint getBasicAuthenticationEntryPoint(String[] activeProfiles) {
-      if (!Arrays.asList(activeProfiles).contains("embeddedJetty")) {
-        return formLoginBasicAuthenticationEntryPoint();
-      } else {
-        return embeddedJettyBasicAuthenticationEntryPoint();
-      }
-    }
-
-    private void configureOAuthAuthorizationServer(HttpSecurity http) throws Exception {
-      if (dhisConfig.isEnabled(ConfigurationKey.ENABLE_OAUTH2_AUTHORIZATION_SERVER)) {
-        http.exceptionHandling().accessDeniedHandler(new OAuth2AccessDeniedHandler());
-      }
     }
 
     private void configureCspFilter(
