9b5e2b06b2d281ae265e91817b87d9d208349c2c
maikelarabori <maikelarabori@gmail.com>
2023-08-22 09:07:37 -0300

    fix: Default migration column/value

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.41/V2_41_27__Add_skip_rounding_to_eventvisualization.sql
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.41/V2_41_27__Add_skip_rounding_to_eventvisualization.sql
@@ -1,4 +1,5 @@
 -- https://dhis2.atlassian.net/browse/DHIS2-15689
 -- Adds a new column "skiprounding" into EventVisualization table.
 
-alter table eventvisualization add column if not exists "skiprounding" boolean;
+-- Our standard approach is to always default boolean types to "true" or "false" to avoid conversion problems.
+alter table eventvisualization add column if not exists "skiprounding" boolean not null default false;
