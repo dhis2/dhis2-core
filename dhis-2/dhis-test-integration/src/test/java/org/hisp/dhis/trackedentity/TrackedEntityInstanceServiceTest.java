29fd21a27c9d293d018739a42cc545d5674569bf
luca <lucacambi77@gmail.com>
2023-10-03 11:00:44 +0200

    fix: missing group by field for enrolledat

--- dhis-2/dhis-test-integration/src/test/java/org/hisp/dhis/trackedentity/TrackedEntityInstanceServiceTest.java
+++ dhis-2/dhis-test-integration/src/test/java/org/hisp/dhis/trackedentity/TrackedEntityInstanceServiceTest.java
@@ -27,6 +27,7 @@
  */
 package org.hisp.dhis.trackedentity;
 
+import static org.hisp.dhis.trackedentity.TrackedEntityInstanceQueryParams.OrderColumn.ENROLLED_AT;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -473,6 +474,34 @@ class TrackedEntityInstanceServiceTest
 
         assertEquals( List.of( entityInstanceB1.getId(), entityInstanceA1.getId(), entityInstanceD1.getId(),
             entityInstanceC1.getId() ), teiIdList );
+    }
+
+    @Test
+    void shouldOrderEntitiesByEnrolledAtDateAndAttributeValueWhenOrderHasAttributeAndEnrollmentDate()
+    {
+        injectSecurityContext( superUser );
+        TrackedEntityAttribute tea = createTrackedEntityAttribute();
+
+        addEntityInstances();
+
+        programInstanceService.addProgramInstance( programInstance );
+        addEnrollment( entityInstanceB1, programInstance.getEnrollmentDate(), 'B' );
+
+        // enrollments have the same enrollment date but different attribute value
+        createTrackedEntityInstanceAttribute( entityInstanceA1, tea, "B" );
+        createTrackedEntityInstanceAttribute( entityInstanceB1, tea, "A" );
+
+        TrackedEntityInstanceQueryParams params = new TrackedEntityInstanceQueryParams();
+
+        params.setOrganisationUnits( Set.of( organisationUnit ) );
+        params.setOrders(
+            List.of(
+                new OrderParam( ENROLLED_AT.getPropName(), OrderParam.SortDirection.DESC ),
+                new OrderParam( tea.getUid(), OrderParam.SortDirection.DESC ) ) );
+
+        List<Long> teiIdList = entityInstanceService.getTrackedEntityInstanceIds( params, true, true );
+
+        assertEquals( List.of( entityInstanceA1.getId(), entityInstanceB1.getId() ), teiIdList );
     }
 
     @Test
