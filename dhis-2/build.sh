bda703f9901973e670a532a72ea6da1ffaf9cfb0
Viet Nguyen <viet@dhis2.org>
2023-10-05 14:18:42 +0700

    git cache cleared

--- dhis-2/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Requires maven to be on the classpath
-# Skips test phase
-
-mvn clean install --batch-mode --no-transfer-progress -DskipTests=true -pl -dhis-web-embedded-jetty
-mvn clean install --batch-mode --no-transfer-progress -DskipTests=true -f dhis-web/pom.xml -U
-
-
