fa0077930ea78a34b184c5535c8f671ba1234851
Lars Helge Øverland <lars@dhis2.org>
2023-10-12 08:52:58 +0200

    feat: Import strategy and skipAudit support for ADE

--- dhis-2/dhis-services/dhis-service-data-exchange/src/main/java/org/hisp/dhis/dataexchange/client/Dhis2Client.java
+++ dhis-2/dhis-services/dhis-service-data-exchange/src/main/java/org/hisp/dhis/dataexchange/client/Dhis2Client.java
@@ -29,13 +29,11 @@ package org.hisp.dhis.dataexchange.client;
 
 import static java.lang.String.format;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.UncheckedIOException;
 import java.net.URI;
 import java.util.List;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.Validate;
+import java.util.Objects;
+
 import org.hisp.dhis.common.IdScheme;
 import org.hisp.dhis.common.IdSchemes;
 import org.hisp.dhis.commons.jackson.config.JacksonObjectMapperConfig;
@@ -57,6 +55,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * API client for DHIS 2.
@@ -84,8 +87,8 @@ public class Dhis2Client {
     this.authentication = authentication;
     this.restTemplate = new RestTemplate();
     this.objectMapper = JacksonObjectMapperConfig.jsonMapper;
-    Validate.notNull(url);
-    Validate.notNull(authentication);
+    Objects.requireNonNull(url);
+    Objects.requireNonNull(authentication);
   }
 
   /**
