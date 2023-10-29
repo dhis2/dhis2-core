2d0fe4007dae26e6b768594ea9a912d687d7975c
Viet Nguyen <viet@dhis2.org>
2023-10-29 07:17:30 +0700

    fix ProgramRuleVariableObjectBundleHookTest

--- dhis-2/dhis-services/dhis-service-dxf2/src/test/java/org/hisp/dhis/dxf2/metadata/objectbundle/hooks/ProgramRuleVariableObjectBundleHookTest.java
+++ dhis-2/dhis-services/dhis-service-dxf2/src/test/java/org/hisp/dhis/dxf2/metadata/objectbundle/hooks/ProgramRuleVariableObjectBundleHookTest.java
@@ -37,6 +37,7 @@ import static org.mockito.Mockito.when;
 
 import java.util.Collections;
 import java.util.List;
+import javax.persistence.EntityManager;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
@@ -62,7 +63,7 @@ class ProgramRuleVariableObjectBundleHookTest {
 
   @InjectMocks private ProgramRuleVariableObjectBundleHook programRuleVariableObjectBundleHook;
 
-  @Mock private SessionFactory sessionFactory;
+  @Mock private EntityManager entityManager;
 
   @Mock private Session session;
 
@@ -78,7 +79,7 @@ class ProgramRuleVariableObjectBundleHookTest {
 
   @BeforeEach
   public void setUp() {
-    when(sessionFactory.getCurrentSession()).thenReturn(session);
+    when(entityManager.unwrap(Session.class)).thenReturn(session);
     when(session.createQuery(anyString(), classArgumentCaptor.capture())).thenReturn(query);
     when(program.getUid()).thenReturn("uid");
   }
