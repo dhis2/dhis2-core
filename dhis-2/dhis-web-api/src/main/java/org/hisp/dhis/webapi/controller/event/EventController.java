2211034e9897a506aa939060ee0630fb8ba82a23
David Mackessy <dmackessy@dhis2.org>
2023-11-01 11:19:26 +0000

    fix: Async CSV Event import [DHIS2-12345]

--- dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/event/EventController.java
+++ dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/event/EventController.java
@@ -61,6 +61,7 @@ import javax.ws.rs.ForbiddenException;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.apache.commons.lang3.StringUtils;
+import org.hibernate.SessionFactory;
 import org.hisp.dhis.category.CategoryOptionCombo;
 import org.hisp.dhis.common.AssignedUserSelectionMode;
 import org.hisp.dhis.common.AsyncTaskExecutor;
@@ -187,6 +188,8 @@ public class EventController {
   private final ContextUtils contextUtils;
 
   private final DhisConfigurationProvider dhisConfig;
+
+  private final SessionFactory sessionFactory;
 
   private Schema schema;
 
@@ -1185,7 +1188,8 @@ public class EventController {
             EVENT_IMPORT,
             currentUserService.getCurrentUser().getUid(),
             true);
-    taskExecutor.executeTask(new ImportEventsTask(events, eventService, importOptions, jobId));
+    taskExecutor.executeTask(
+        new ImportEventsTask(events, eventService, importOptions, jobId, sessionFactory));
 
     return jobConfigurationReport(jobId).setLocation("/system/tasks/" + EVENT_IMPORT);
   }
