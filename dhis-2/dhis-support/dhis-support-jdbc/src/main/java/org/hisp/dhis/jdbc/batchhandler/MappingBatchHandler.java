0e5251d15341e48b05116056625c70cd9e484ac0
jason-p-pickering <jason.p.pickering@gmail.com>
2023-11-15 12:37:50 +0100

    Resolve issues with Lombok

--- dhis-2/dhis-support/dhis-support-jdbc/src/main/java/org/hisp/dhis/jdbc/batchhandler/MappingBatchHandler.java
+++ dhis-2/dhis-support/dhis-support-jdbc/src/main/java/org/hisp/dhis/jdbc/batchhandler/MappingBatchHandler.java
@@ -69,6 +69,10 @@ public class MappingBatchHandler extends AbstractBatchHandler<Map<String, Object
 
   @Builder.Default private List<String> uniqueColumns = emptyList();
 
+  @Builder.Default private boolean closed = false;
+
+  @Builder.Default private int addObjectCount = 0;
+
   // -------------------------------------------------------------------------
   // Constructor
   // -------------------------------------------------------------------------
