2c463872ed2026fe7129a0b7d6cbd59f1bd45182
Morten Svanaes <msvanaes@dhis2.org>
2023-11-15 19:27:01 +0800

    chore: enforce username format rules in additional places

--- dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/user/UserController.java
+++ dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/user/UserController.java
@@ -454,9 +454,8 @@ public class UserController extends AbstractCrudController<User> {
       return conflict("Username must be specified");
     }
 
-    if ( !ValidationUtils.usernameIsValid( username, false ) )
-    {
-      return conflict( "Username is not valid" );
+    if (!ValidationUtils.usernameIsValid(username, false)) {
+      return conflict("Username is not valid");
     }
 
     if (userService.getUserByUsername(username) != null) {
