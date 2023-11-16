a937b63b0a70de2a50be6060715ff6f9a7b0dd50
Andreas Jensen <andreas@dhis2.org>
2023-11-16 14:21:34 +0700

    ci: document how to test the stable pipeline

---
+++ jenkinsfiles/delete-remote-docker-image.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+ORGANIZATION=dhis2
+REPOSITORY=core
+TAG=4.1.2.3
+
+HUB_TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d "{\"username\": \"$HUB_USERNAME\", \"password\": \"$HUB_PASSWORD\"}" https://hub.docker.com/v2/users/login/ | jq -r .token)
+
+echo Delete $ORGANIZATION/$REPOSITORY:$TAG?
+read
+
+curl -H "Authorization: JWT $HUB_TOKEN" -X "DELETE" https://hub.docker.com/v2/repositories/$ORGANIZATION/$REPOSITORY/tags/$TAG/
