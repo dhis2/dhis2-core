6d8c03b0c596ae321ab0119b5035f660f47e93f6
Jan Bernitt <jaanbernitt@gmail.com>
2023-11-20 17:05:34 +0100

    chore: code formatting

--- dhis-2/dhis-test-e2e/src/test/java/org/hisp/dhis/helpers/TestCleanUp.java
+++ dhis-2/dhis-test-e2e/src/test/java/org/hisp/dhis/helpers/TestCleanUp.java
@@ -33,7 +33,6 @@ import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.hisp.dhis.TestRunStorage;
