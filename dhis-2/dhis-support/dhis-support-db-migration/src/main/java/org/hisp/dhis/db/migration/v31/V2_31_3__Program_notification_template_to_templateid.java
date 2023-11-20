a166ec66570ab8abc5638e8c4cad06db91d8d9d9
Jan Bernitt <jaanbernitt@gmail.com>
2023-11-20 17:02:46 +0100

    fix: revert changes in flyway script files

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/java/org/hisp/dhis/db/migration/v31/V2_31_3__Program_notification_template_to_templateid.java
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/java/org/hisp/dhis/db/migration/v31/V2_31_3__Program_notification_template_to_templateid.java
@@ -27,10 +27,7 @@
  */
 package org.hisp.dhis.db.migration.v31;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
