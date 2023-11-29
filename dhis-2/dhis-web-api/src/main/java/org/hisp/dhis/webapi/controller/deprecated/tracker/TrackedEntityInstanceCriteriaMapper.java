65065c3cc6d486b0d22ac364696cf80dd1c6607a
Marc <muilpp@gmail.com>
2023-11-29 13:47:39 +0100

    task: Extract org unit validation [TECH-1669]

--- dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/deprecated/tracker/TrackedEntityInstanceCriteriaMapper.java
+++ dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/deprecated/tracker/TrackedEntityInstanceCriteriaMapper.java
@@ -139,28 +139,7 @@ public class TrackedEntityInstanceCriteriaMapper {
       }
     }
 
-    validateOrgUnitParams(criteria.getOrgUnits(), criteria.getOuMode());
-
-    for (String orgUnit : criteria.getOrgUnits()) {
-      OrganisationUnit organisationUnit = organisationUnitService.getOrganisationUnit(orgUnit);
-
-      if (organisationUnit == null) {
-        throw new IllegalQueryException("Organisation unit does not exist: " + orgUnit);
-      }
-
-      if (user == null) {
-        throw new IllegalQueryException("Please log in and try again.");
-      }
-
-      if (!user.isSuper()
-          && !organisationUnitService.isInUserHierarchy(
-              organisationUnit.getUid(), possibleSearchOrgUnits)) {
-        throw new IllegalQueryException(
-            "Organisation unit is not part of the search scope: " + orgUnit);
-      }
-
-      params.getOrgUnits().add(organisationUnit);
-    }
+    params.getOrgUnits().addAll(validateOrgUnits(criteria, possibleSearchOrgUnits, user));
 
     if (criteria.getOuMode() == OrganisationUnitSelectionMode.CAPTURE && user != null) {
       params.getOrgUnits().addAll(user.getOrganisationUnits());
@@ -203,6 +182,45 @@ public class TrackedEntityInstanceCriteriaMapper {
         .setOrders(orderParams);
 
     return params;
+  }
+
+  private Set<OrganisationUnit> validateOrgUnits(
+      TrackedEntityInstanceCriteria criteria,
+      Set<OrganisationUnit> possibleSearchOrgUnits,
+      User user) {
+
+    Set<OrganisationUnit> organisationUnits = new HashSet<>();
+
+    if (!criteria.getOrgUnits().isEmpty()
+        && (criteria.getOuMode() == ACCESSIBLE || criteria.getOuMode() == CAPTURE)) {
+      throw new IllegalQueryException(
+          String.format(
+              "ouMode %s cannot be used with orgUnits. Please remove the ou parameter and try again.",
+              criteria.getOuMode()));
+    }
+
+    for (String orgUnit : criteria.getOrgUnits()) {
+      OrganisationUnit organisationUnit = organisationUnitService.getOrganisationUnit(orgUnit);
+
+      if (organisationUnit == null) {
+        throw new IllegalQueryException("Organisation unit does not exist: " + orgUnit);
+      }
+
+      if (user == null) {
+        throw new IllegalQueryException("Please log in and try again.");
+      }
+
+      if (!user.isSuper()
+          && !organisationUnitService.isInUserHierarchy(
+              organisationUnit.getUid(), possibleSearchOrgUnits)) {
+        throw new IllegalQueryException(
+            "Organisation unit is not part of the search scope: " + orgUnit);
+      }
+
+      organisationUnits.add(organisationUnit);
+    }
+
+    return organisationUnits;
   }
 
   /**
@@ -342,12 +360,5 @@ public class TrackedEntityInstanceCriteriaMapper {
     }
   }
 
-  private void validateOrgUnitParams(Set<String> orgUnits, OrganisationUnitSelectionMode ouMode) {
-    if (!orgUnits.isEmpty() && (ouMode == ACCESSIBLE || ouMode == CAPTURE)) {
-      throw new IllegalQueryException(
-          String.format(
-              "ouMode %s cannot be used with orgUnits. Please remove the ou parameter and try again.",
-              ouMode));
-    }
-  }
+  private void validateOrgUnitParams(Set<String> orgUnits, OrganisationUnitSelectionMode ouMode) {}
 }
