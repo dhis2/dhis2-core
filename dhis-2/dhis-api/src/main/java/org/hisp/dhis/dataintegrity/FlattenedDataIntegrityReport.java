0d3e66c668a082070b853a2e368a56aa7e385da7
jason-p-pickering <jason.p.pickering@gmail.com>
2023-12-14 11:16:14 +0100

    Add SQL based check for invalid category combos

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/dataintegrity/FlattenedDataIntegrityReport.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/dataintegrity/FlattenedDataIntegrityReport.java
@@ -123,8 +123,7 @@ public class FlattenedDataIntegrityReport implements WebMessageResponse {
     this.dataElementsWithoutGroups =
         listOfDisplayNameWithUid(detailsByName.get("data_elements_aggregate_no_groups"));
     this.invalidCategoryCombos =
-        listOfDisplayNameOrUid(
-            detailsByName.get(DataIntegrityCheckType.CATEGORY_COMBOS_BEING_INVALID.getName()));
+        listOfDisplayNameOrUid(detailsByName.get("invalid_category_combos"));
     this.dataSetsNotAssignedToOrganisationUnits =
         listOfDisplayNameOrUid(detailsByName.get("datasets_not_assigned_to_org_units"));
     this.indicatorsWithoutGroups =
