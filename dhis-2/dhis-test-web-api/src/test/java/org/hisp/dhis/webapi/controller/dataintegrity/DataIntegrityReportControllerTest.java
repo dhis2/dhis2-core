ffcfca3b93ef10e61748eab367eed3bee7b89d9f
Jason Pickering <jason.p.pickering@gmail.com>
2023-12-14 15:51:11 +0300

    chore: Replace Java based check for invalid category combos (#15941)
    * Add SQL based check for invalid category combos
    
    * Add test in integrity report

--- dhis-2/dhis-test-web-api/src/test/java/org/hisp/dhis/webapi/controller/dataintegrity/DataIntegrityReportControllerTest.java
+++ dhis-2/dhis-test-web-api/src/test/java/org/hisp/dhis/webapi/controller/dataintegrity/DataIntegrityReportControllerTest.java
@@ -376,6 +376,30 @@ class DataIntegrityReportControllerTest extends AbstractDataIntegrityIntegration
     assertEquals(List.of("Test"), results);
   }
 
+  @Test
+  void testInvalidCategoryCombos() {
+    String categoryTaste =
+        assertStatus(
+            HttpStatus.CREATED,
+            POST(
+                "/categories",
+                "{ 'name': 'Taste', 'shortName': 'Taste', 'dataDimensionType': 'DISAGGREGATION' }"));
+
+    String testCatCombo =
+        assertStatus(
+            HttpStatus.CREATED,
+            POST(
+                "/categoryCombos",
+                "{ 'name' : 'Tasteless', "
+                    + "'dataDimensionType' : 'DISAGGREGATION', 'categories' : ["
+                    + " {'id' : '"
+                    + categoryTaste
+                    + "'}]} "));
+    List<String> results =
+        getDataIntegrityReport().getInvalidCategoryCombos().toList(JsonString::string);
+    assertEquals(List.of("Tasteless" + ":" + testCatCombo), results);
+  }
+
   private JsonDataIntegrityReport getDataIntegrityReport() {
     return getDataIntegrityReport("/dataIntegrity");
   }
