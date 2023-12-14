4afc6320bd135bb9a2ecfdbbd893e5c932e0da7f
luca <lucacambi77@gmail.com>
2023-12-14 11:19:33 +0100

    fix: update tei

--- dhis-2/dhis-test-integration/src/test/java/org/hisp/dhis/dxf2/events/EventImportTest.java
+++ dhis-2/dhis-test-integration/src/test/java/org/hisp/dhis/dxf2/events/EventImportTest.java
@@ -31,6 +31,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
 import static org.hisp.dhis.user.UserRole.AUTHORITY_ALL;
+import static org.hisp.dhis.util.DateUtils.getIso8601NoTz;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -263,7 +264,7 @@ class EventImportTest extends TransactionalIntegrationTest {
             programB.getUid(),
             programStageB.getUid(),
             organisationUnitB.getUid(),
-            null,
+            trackedEntityInstanceMaleA.getTrackedEntityInstance(),
             dataElementB,
             "10");
     String uid = eventService.addEventsJson(is, null).getImportSummaries().get(0).getReference();
@@ -288,6 +289,8 @@ class EventImportTest extends TransactionalIntegrationTest {
     dataValueB.setStoredBy(superUser.getName());
 
     event.setDataValues(Set.of(dataValueA, dataValueB));
+
+    Date now = new Date();
 
     eventService.updateEventDataValues(event);
 
@@ -326,6 +329,11 @@ class EventImportTest extends TransactionalIntegrationTest {
     assertNotNull(eventDataValueB.getLastUpdatedByUserInfo());
     assertNotNull(eventDataValueB.getCreated());
     assertNotNull(eventDataValueB.getLastUpdated());
+
+    TrackedEntityInstance trackedEntityInstance =
+        trackedEntityInstanceService.getTrackedEntityInstance(
+            trackedEntityInstanceMaleA.getTrackedEntityInstance());
+    assertTrue(trackedEntityInstance.getLastUpdated().compareTo(getIso8601NoTz(now)) > 0);
   }
 
   @Test
