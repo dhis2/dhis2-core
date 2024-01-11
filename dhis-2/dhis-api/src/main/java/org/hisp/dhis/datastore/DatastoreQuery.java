b8477f86d8e4b593e1520063c44baf07b0715bf1
Jan Bernitt <jaanbernitt@gmail.com>
2024-01-11 15:03:55 +0100

    fix: do not remove pager wrapper without paging [DHIS2-16422]

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/datastore/DatastoreQuery.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/datastore/DatastoreQuery.java
@@ -292,7 +292,7 @@ public final class DatastoreQuery {
     int size = max(1, min(1000, params.getInt("pageSize", 50)));
     boolean isPaging = params.getBoolean("paging", true);
     return toBuilder()
-        .headless(params.getBoolean("headless", false) || !isPaging)
+        .headless(params.getBoolean("headless", false))
         .anyFilter(params.getString("rootJunction", "AND").equalsIgnoreCase("OR"))
         .order(Order.parse(params.getString("order", KEY_ASC.path)))
         .paging(isPaging)
