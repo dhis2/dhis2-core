2835488973678fc224cd82d5202687adfe7cf8b3
Jan Bernitt <jaanbernitt@gmail.com>
2024-01-11 16:10:21 +0100

    chore: code format

--- dhis-2/dhis-test-web-api/src/test/java/org/hisp/dhis/webapi/controller/DatastoreFieldsControllerTest.java
+++ dhis-2/dhis-test-web-api/src/test/java/org/hisp/dhis/webapi/controller/DatastoreFieldsControllerTest.java
@@ -9,7 +9,8 @@
  *
  * Redistributions in binary form must reproduce the above copyright notice,
  * this list of conditions and the following disclaimer in the documentation
- * and/or other materials provided with the distribution.* Neither the name of the HISP project nor the names of its contributors may
+ * and/or other materials provided with the distribution.
+ * Neither the name of the HISP project nor the names of its contributors may
  * be used to endorse or promote products derived from this software without
  * specific prior written permission.
  *
