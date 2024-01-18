1bde6935b64693255d97d6a22e1b99461b62a3fa
Lars Helge Øverland <lars@dhis2.org>
2024-01-18 21:47:24 +0100

    fix: Replace use of statement builder (#16226)

--- dhis-2/dhis-support/dhis-support-jdbc/src/main/java/org/hisp/dhis/jdbc/StatementBuilder.java
+++ dhis-2/dhis-support/dhis-support-jdbc/src/main/java/org/hisp/dhis/jdbc/StatementBuilder.java
@@ -27,9 +27,7 @@
  */
 package org.hisp.dhis.jdbc;
 
-import java.util.Collection;
 import java.util.Date;
-import java.util.List;
 import org.hisp.dhis.program.AnalyticsPeriodBoundary;
 import org.hisp.dhis.program.ProgramIndicator;
 
@@ -37,50 +35,6 @@ import org.hisp.dhis.program.ProgramIndicator;
  * @author Lars Helge Overland
  */
 public interface StatementBuilder {
-
-  /**
-   * Generates a derived table containing one column of literal strings.
-   *
-   * @param values (non-empty) String values for the derived table
-   * @param table the desired table name alias
-   * @param column the desired column name
-   * @return the derived literal table
-   */
-  String literalStringTable(Collection<String> values, String table, String column);
-
-  /**
-   * Generates a derived table containing literals in two columns: integer and string.
-   *
-   * @param longValue (non-empty) Integer values for the derived table
-   * @param strValues (same size) String values for the derived table
-   * @param table the desired table name alias
-   * @param longColumn the desired integer column name
-   * @param strColumn the desired string column name
-   * @return the derived literal table
-   */
-  String literalLongStringTable(
-      List<Long> longValue,
-      List<String> strValues,
-      String table,
-      String longColumn,
-      String strColumn);
-
-  /**
-   * Generates a derived table containing literals in two columns: integer and integer.
-   *
-   * @param long1Values (non-empty) 1st integer column values for the table
-   * @param long2Values (same size) 2nd integer column values for the table
-   * @param table the desired table name alias
-   * @param long1Column the desired 1st integer column name
-   * @param long2Column the desired 2nd integer column name
-   * @return the derived literal table
-   */
-  String literalLongLongTable(
-      List<Long> long1Values,
-      List<Long> long2Values,
-      String table,
-      String long1Column,
-      String long2Column);
 
   /**
    * Get SQL where-condition for a single analyticsPeriodBoundary in a program indicator.
