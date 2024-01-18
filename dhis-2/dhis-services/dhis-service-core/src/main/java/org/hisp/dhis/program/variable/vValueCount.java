bc36019941ac42ab9c3cf0a106d5e1c07ca4667d
Lars Helge Øverland <lars@dhis2.org>
2024-01-18 18:11:12 +0100

    refactor: Replace usage of statement builder (#16223)

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/program/variable/vValueCount.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/program/variable/vValueCount.java
@@ -52,9 +52,6 @@ public class vValueCount extends ProgramDoubleVariable {
       sql += "case when " + SqlUtils.quote(uid) + " is not null then 1 else 0 end + ";
     }
 
-    return TextUtils.removeLast(sql, "+").trim()
-        + ") as "
-        + visitor.getStatementBuilder().getDoubleColumnType()
-        + "),0)";
+    return TextUtils.removeLast(sql, "+").trim() + ") as double precision),0)";
   }
 }
