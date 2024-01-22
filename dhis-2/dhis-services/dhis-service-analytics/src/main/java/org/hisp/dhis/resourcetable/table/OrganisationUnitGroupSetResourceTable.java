008438f38d3d7cf655d623946463d37e7f0570f9
Lars Helge Overland <lars@dhis2.org>
2024-01-22 18:45:15 +0100

    fix: Update code

--- dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/resourcetable/table/OrganisationUnitGroupSetResourceTable.java
+++ dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/resourcetable/table/OrganisationUnitGroupSetResourceTable.java
@@ -28,11 +28,12 @@
 package org.hisp.dhis.resourcetable.table;
 
 import static org.hisp.dhis.commons.util.TextUtils.removeLastComma;
+import static org.hisp.dhis.system.util.SqlUtils.appendRandom;
 import static org.hisp.dhis.system.util.SqlUtils.quote;
 
-import com.google.common.collect.Lists;
 import java.util.List;
 import java.util.Optional;
+
 import org.hisp.dhis.db.model.Column;
 import org.hisp.dhis.db.model.DataType;
 import org.hisp.dhis.db.model.Index;
@@ -45,67 +46,74 @@ import org.hisp.dhis.organisationunit.OrganisationUnitGroupSet;
 import org.hisp.dhis.resourcetable.ResourceTable;
 import org.hisp.dhis.resourcetable.ResourceTableType;
 
+import com.google.common.collect.Lists;
+
 /**
  * @author Lars Helge Overland
  */
-public class OrganisationUnitGroupSetResourceTable extends ResourceTable<OrganisationUnitGroupSet> {
-  private final int organisationUnitLevels;
+public class OrganisationUnitGroupSetResourceTable extends ResourceTable<OrganisationUnitGroupSet>
+{
+    private final int organisationUnitLevels;
 
-  private final String tableType;
+    private final String tableType;
 
-  public OrganisationUnitGroupSetResourceTable(
-      List<OrganisationUnitGroupSet> objects, int organisationUnitLevels, String tableType) {
-    super(objects);
-    this.organisationUnitLevels = organisationUnitLevels;
-    this.tableType = tableType;
-  }
-
-  @Override
-  public Table getTable() {
-    List<Column> columns =
-        Lists.newArrayList(
-            new Column("organisationunitid", DataType.BIGINT, Nullable.NOT_NULL),
-            new Column("organisationunitname", DataType.VARCHAR_255, Nullable.NOT_NULL),
-            new Column("startdate", DataType.DATE));
-
-    for (OrganisationUnitGroupSet groupSet : objects) {
-      columns.addAll(
-          List.of(
-              new Column(groupSet.getShortName(), DataType.VARCHAR_255),
-              new Column(groupSet.getUid(), DataType.CHARACTER_11)));
+    public OrganisationUnitGroupSetResourceTable(
+        List<OrganisationUnitGroupSet> objects, int organisationUnitLevels, String tableType )
+    {
+        super( objects );
+        this.organisationUnitLevels = organisationUnitLevels;
+        this.tableType = tableType;
     }
 
-    List<String> primaryKey = List.of("organisationunitid");
+    @Override
+    public Table getTable()
+    {
+        List<Column> columns = Lists.newArrayList(
+            new Column( "organisationunitid", DataType.BIGINT, Nullable.NOT_NULL ),
+            new Column( "organisationunitname", DataType.VARCHAR_255, Nullable.NOT_NULL ),
+            new Column( "startdate", DataType.DATE ) );
 
-    return new Table("_organisationunitgroupsetstructure", columns, primaryKey, Logged.UNLOGGED);
-  }
+        for ( OrganisationUnitGroupSet groupSet : objects )
+        {
+            columns.addAll(
+                List.of(
+                    new Column( groupSet.getShortName(), DataType.VARCHAR_255 ),
+                    new Column( groupSet.getUid(), DataType.CHARACTER_11 ) ) );
+        }
 
-  @Override
-  public List<Index> getIndexes() {
-    return List.of(
-        new Index(
-            ("in_orgunitgroupsetstructure_not_null_" + getRandomSuffix()),
-            IndexType.BTREE,
-            Unique.NON_UNIQUE,
-            List.of("organisationunitid", "startdate"),
-            "startdate is not null"),
-        new Index(
-            ("in_orgunitgroupsetstructure_null_" + getRandomSuffix()),
-            IndexType.BTREE,
-            Unique.NON_UNIQUE,
-            List.of("organisationunitid", "startdate"),
-            "startdate is null"));
-  }
+        List<String> primaryKey = List.of( "organisationunitid" );
 
-  @Override
-  public ResourceTableType getTableType() {
-    return ResourceTableType.ORG_UNIT_GROUP_SET_STRUCTURE;
-  }
+        return new Table( "_organisationunitgroupsetstructure", columns, primaryKey, Logged.UNLOGGED );
+    }
 
-  @Override
-  public String getCreateTempTableStatement() {
-    String statement =
-        "create "
+    @Override
+    public List<Index> getIndexes()
+    {
+        return List.of(
+            new Index(
+                appendRandom( "in_orgunitgroupsetstructure_not_null" ),
+                IndexType.BTREE,
+                Unique.NON_UNIQUE,
+                List.of( "organisationunitid", "startdate" ),
+                "startdate is not null" ),
+            new Index(
+                appendRandom( "in_orgunitgroupsetstructure_null" ),
+                IndexType.BTREE,
+                Unique.NON_UNIQUE,
+                List.of( "organisationunitid", "startdate" ),
+                "startdate is null" ) );
+    }
+
+    @Override
+    public ResourceTableType getTableType()
+    {
+        return ResourceTableType.ORG_UNIT_GROUP_SET_STRUCTURE;
+    }
+
+    @Override
+    public String getCreateTempTableStatement()
+    {
+        String statement = "create "
             + tableType
             + " table "
             + getTempTableName()
@@ -114,131 +122,135 @@ public class OrganisationUnitGroupSetResourceTable extends ResourceTable<Organis
             + "organisationunitname varchar(230), "
             + "startdate date, ";
 
-    for (OrganisationUnitGroupSet groupSet : objects) {
-      statement += quote(groupSet.getShortName()) + " varchar(230), ";
-      statement += quote(groupSet.getUid()) + " character(11), ";
+        for ( OrganisationUnitGroupSet groupSet : objects )
+        {
+            statement += quote( groupSet.getShortName() ) + " varchar(230), ";
+            statement += quote( groupSet.getUid() ) + " character(11), ";
+        }
+
+        return removeLastComma( statement ) + ")";
     }
 
-    return removeLastComma(statement) + ")";
-  }
-
-  @Override
-  public Optional<String> getPopulateTempTableStatement() {
-    String sql =
-        "insert into "
+    @Override
+    public Optional<String> getPopulateTempTableStatement()
+    {
+        String sql = "insert into "
             + getTempTableName()
             + " "
             + "select ou.organisationunitid as organisationunitid, ou.name as organisationunitname, null as startdate, ";
 
-    for (OrganisationUnitGroupSet groupSet : objects) {
-      if (!groupSet.isIncludeSubhierarchyInAnalytics()) {
-        sql +=
-            "("
-                + "select oug.name from orgunitgroup oug "
-                + "inner join orgunitgroupmembers ougm on ougm.orgunitgroupid = oug.orgunitgroupid "
-                + "inner join orgunitgroupsetmembers ougsm on "
-                + "ougsm.orgunitgroupid = ougm.orgunitgroupid and ougsm.orgunitgroupsetid = "
-                + groupSet.getId()
-                + " "
-                + "where ougm.organisationunitid = ou.organisationunitid "
-                + "limit 1) as "
-                + quote(groupSet.getName())
-                + ", ";
+        for ( OrganisationUnitGroupSet groupSet : objects )
+        {
+            if ( !groupSet.isIncludeSubhierarchyInAnalytics() )
+            {
+                sql += "("
+                    + "select oug.name from orgunitgroup oug "
+                    + "inner join orgunitgroupmembers ougm on ougm.orgunitgroupid = oug.orgunitgroupid "
+                    + "inner join orgunitgroupsetmembers ougsm on "
+                    + "ougsm.orgunitgroupid = ougm.orgunitgroupid and ougsm.orgunitgroupsetid = "
+                    + groupSet.getId()
+                    + " "
+                    + "where ougm.organisationunitid = ou.organisationunitid "
+                    + "limit 1) as "
+                    + quote( groupSet.getName() )
+                    + ", ";
 
-        sql +=
-            "("
-                + "select oug.uid from orgunitgroup oug "
-                + "inner join orgunitgroupmembers ougm on ougm.orgunitgroupid = oug.orgunitgroupid "
-                + "inner join orgunitgroupsetmembers ougsm on "
-                + "ougsm.orgunitgroupid = ougm.orgunitgroupid and ougsm.orgunitgroupsetid = "
-                + groupSet.getId()
-                + " "
-                + "where ougm.organisationunitid = ou.organisationunitid "
-                + "limit 1) as "
-                + quote(groupSet.getUid())
-                + ", ";
-      } else {
-        sql += "coalesce(";
+                sql += "("
+                    + "select oug.uid from orgunitgroup oug "
+                    + "inner join orgunitgroupmembers ougm on ougm.orgunitgroupid = oug.orgunitgroupid "
+                    + "inner join orgunitgroupsetmembers ougsm on "
+                    + "ougsm.orgunitgroupid = ougm.orgunitgroupid and ougsm.orgunitgroupsetid = "
+                    + groupSet.getId()
+                    + " "
+                    + "where ougm.organisationunitid = ou.organisationunitid "
+                    + "limit 1) as "
+                    + quote( groupSet.getUid() )
+                    + ", ";
+            }
+            else
+            {
+                sql += "coalesce(";
 
-        for (int i = organisationUnitLevels; i > 0; i--) {
-          sql +=
-              "(select oug.name from orgunitgroup oug "
-                  + "inner join orgunitgroupmembers ougm on "
-                  + "ougm.orgunitgroupid = oug.orgunitgroupid and ougm.organisationunitid = ous.idlevel"
-                  + i
-                  + " "
-                  + "inner join orgunitgroupsetmembers ougsm on "
-                  + "ougsm.orgunitgroupid = ougm.orgunitgroupid and ougsm.orgunitgroupsetid = "
-                  + groupSet.getId()
-                  + " "
-                  + "limit 1),";
+                for ( int i = organisationUnitLevels; i > 0; i-- )
+                {
+                    sql += "(select oug.name from orgunitgroup oug "
+                        + "inner join orgunitgroupmembers ougm on "
+                        + "ougm.orgunitgroupid = oug.orgunitgroupid and ougm.organisationunitid = ous.idlevel"
+                        + i
+                        + " "
+                        + "inner join orgunitgroupsetmembers ougsm on "
+                        + "ougsm.orgunitgroupid = ougm.orgunitgroupid and ougsm.orgunitgroupsetid = "
+                        + groupSet.getId()
+                        + " "
+                        + "limit 1),";
+                }
+
+                if ( organisationUnitLevels == 0 )
+                {
+                    sql += "null";
+                }
+
+                sql = removeLastComma( sql ) + ") as " + quote( groupSet.getName() ) + ", ";
+
+                sql += "coalesce(";
+
+                for ( int i = organisationUnitLevels; i > 0; i-- )
+                {
+                    sql += "(select oug.uid from orgunitgroup oug "
+                        + "inner join orgunitgroupmembers ougm on "
+                        + "ougm.orgunitgroupid = oug.orgunitgroupid and ougm.organisationunitid = ous.idlevel"
+                        + i
+                        + " "
+                        + "inner join orgunitgroupsetmembers ougsm on "
+                        + "ougsm.orgunitgroupid = ougm.orgunitgroupid and ougsm.orgunitgroupsetid = "
+                        + groupSet.getId()
+                        + " "
+                        + "limit 1),";
+                }
+
+                if ( organisationUnitLevels == 0 )
+                {
+                    sql += "null";
+                }
+
+                sql = removeLastComma( sql ) + ") as " + quote( groupSet.getUid() ) + ", ";
+            }
         }
 
-        if (organisationUnitLevels == 0) {
-          sql += "null";
-        }
-
-        sql = removeLastComma(sql) + ") as " + quote(groupSet.getName()) + ", ";
-
-        sql += "coalesce(";
-
-        for (int i = organisationUnitLevels; i > 0; i--) {
-          sql +=
-              "(select oug.uid from orgunitgroup oug "
-                  + "inner join orgunitgroupmembers ougm on "
-                  + "ougm.orgunitgroupid = oug.orgunitgroupid and ougm.organisationunitid = ous.idlevel"
-                  + i
-                  + " "
-                  + "inner join orgunitgroupsetmembers ougsm on "
-                  + "ougsm.orgunitgroupid = ougm.orgunitgroupid and ougsm.orgunitgroupsetid = "
-                  + groupSet.getId()
-                  + " "
-                  + "limit 1),";
-        }
-
-        if (organisationUnitLevels == 0) {
-          sql += "null";
-        }
-
-        sql = removeLastComma(sql) + ") as " + quote(groupSet.getUid()) + ", ";
-      }
-    }
-
-    sql = removeLastComma(sql) + " ";
-    sql +=
-        "from organisationunit ou "
+        sql = removeLastComma( sql ) + " ";
+        sql += "from organisationunit ou "
             + "inner join _orgunitstructure ous on ous.organisationunitid = ou.organisationunitid";
 
-    return Optional.of(sql);
-  }
+        return Optional.of( sql );
+    }
 
-  @Override
-  public Optional<List<Object[]>> getPopulateTempTableContent() {
-    return Optional.empty();
-  }
+    @Override
+    public Optional<List<Object[]>> getPopulateTempTableContent()
+    {
+        return Optional.empty();
+    }
 
-  @Override
-  public List<String> getCreateIndexStatements() {
-    String nameA = "in_orgunitgroupsetstructure_not_null_" + getRandomSuffix();
-    String nameB = "in_orgunitgroupsetstructure_null_" + getRandomSuffix();
+    @Override
+    public List<String> getCreateIndexStatements()
+    {
+        String nameA = "in_orgunitgroupsetstructure_not_null_" + getRandomSuffix();
+        String nameB = "in_orgunitgroupsetstructure_null_" + getRandomSuffix();
 
-    // Two partial indexes as start date can be null
+        // Two partial indexes as start date can be null
 
-    String indexA =
-        "create index "
+        String indexA = "create index "
             + nameA
             + " on "
             + getTempTableName()
             + "(organisationunitid, startdate) "
             + "where startdate is not null";
-    String indexB =
-        "create index "
+        String indexB = "create index "
             + nameB
             + " on "
             + getTempTableName()
             + "(organisationunitid, startdate) "
             + "where startdate is null";
 
-    return Lists.newArrayList(indexA, indexB);
-  }
+        return Lists.newArrayList( indexA, indexB );
+    }
 }
