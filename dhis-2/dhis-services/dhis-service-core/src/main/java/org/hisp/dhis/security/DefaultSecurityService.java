19b53af16b4db678e8507567cd0e0884f5c9d66e
Morten Svanaes <msvanaes@dhis2.org>
2024-01-26 17:45:20 +0800

    fix: Pre-fill user-invite fields and align form fields with user-app form

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/security/DefaultSecurityService.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/security/DefaultSecurityService.java
@@ -336,6 +336,7 @@ public class DefaultSecurityService implements SecurityService {
   }
 
   @Override
+  @Transactional
   public String generateAndPersistTokens(User user, RestoreOptions restoreOptions) {
     RestoreType restoreType = restoreOptions.getRestoreType();
 
