57733d7fe0955fa81181c0d72ec490884290bf9e
Lars Helge Øverland <lars@dhis2.org>
2024-01-29 10:30:11 +0100

    fix: Update code

--- dhis-2/dhis-services/dhis-service-analytics/src/test/java/org/hisp/dhis/db/model/TableTest.java
+++ dhis-2/dhis-services/dhis-service-analytics/src/test/java/org/hisp/dhis/db/model/TableTest.java
@@ -27,9 +27,9 @@
  */
 package org.hisp.dhis.db.model;
 
-import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 import org.hisp.dhis.db.model.constraint.Nullable;
