77350cd542d74c306b3ebca48d29661c2d42bb08
Viet Nguyen <viet@dhis2.org>
2024-02-01 14:59:58 +0700

    fix: skip reference check createdBy when create new object

--- dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/metadata/objectbundle/validation/ReferencesCheck.java
+++ dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/metadata/objectbundle/validation/ReferencesCheck.java
@@ -163,7 +163,7 @@ public class ReferencesCheck implements ValidationCheck {
                   || "lastUpdatedBy".equals(property.getName())
                   || "createdBy".equals(property.getName()));
 
-      if (!(isUserReference && skipSharing)) {
+      if (!isUserReference) {
         PreheatErrorReport error =
             new PreheatErrorReport(
                 identifier,
--- dhis-2/dhis-test-web-api/src/test/java/org/hisp/dhis/webapi/controller/MetadataImportExportControllerTest.java
+++ dhis-2/dhis-test-web-api/src/test/java/org/hisp/dhis/webapi/controller/MetadataImportExportControllerTest.java
@@ -374,4 +374,34 @@ class MetadataImportExportControllerTest extends DhisControllerConvenienceTest {
     assertTrue(user.getString("username").exists());
     assertTrue(user.getString("userRoles").exists());
   }
+
+  @Test
+  void testImportWithInvalidCreatedBy() {
+    JsonResponse report =
+        POST(
+                "/metadata",
+                "{\"optionSets\":\n"
+                    + "    [{\"name\": \"Device category\",\"id\": \"RHqFlB1Wm4d\",\"version\": 2,\"valueType\": \"TEXT\",\"createdBy\": \"invalid\"}]}")
+            .content(HttpStatus.OK);
+
+    assertNotNull(report.get("response"));
+
+    JsonResponse optionSet = GET("/optionSets/{uid}", "RHqFlB1Wm4d").content(HttpStatus.OK);
+    assertTrue(optionSet.get("createdBy").exists());
+  }
+
+  @Test
+  void testImportWithInvalidCreatedByAndSkipSharing() {
+    JsonResponse report =
+        POST(
+                "/metadata?skipSharing=true",
+                "{\"optionSets\":\n"
+                    + "    [{\"name\": \"Device category\",\"id\": \"RHqFlB1Wm4d\",\"version\": 2,\"valueType\": \"TEXT\",\"createdBy\": \"invalid\"}]}")
+            .content(HttpStatus.OK);
+
+    assertNotNull(report.get("response"));
+
+    JsonResponse optionSet = GET("/optionSets/{uid}", "RHqFlB1Wm4d").content(HttpStatus.OK);
+    assertTrue(optionSet.get("createdBy").exists());
+  }
 }
