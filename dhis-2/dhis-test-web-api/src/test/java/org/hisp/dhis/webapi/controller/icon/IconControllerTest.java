4781b92e0ce05ae98cc6a0a2535f06f42101ec3d
zubaira <zubair@dhis2.org>
2024-02-05 13:14:35 +0100

    test

--- dhis-2/dhis-test-web-api/src/test/java/org/hisp/dhis/webapi/controller/icon/IconControllerTest.java
+++ dhis-2/dhis-test-web-api/src/test/java/org/hisp/dhis/webapi/controller/icon/IconControllerTest.java
@@ -167,6 +167,7 @@ class IconControllerTest extends DhisControllerIntegrationTest {
 
     assertEquals(3, pager.getTotal());
     assertEquals(2, pager.getPageSize());
+    assertEquals(2, pager.getPageCount());
 
     assertEquals(
         2,
