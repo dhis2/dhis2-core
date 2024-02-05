09e2cd6f65a1dc1f2eb683b903e4691cbdeee55b
Giuseppe Nespolino <g.nespolino@gmail.com>
2024-02-05 13:51:33 +0100

    feat: changes how logical operators are used in static dimensions [DHIS2-16726]

--- dhis-2/dhis-test-e2e/src/test/java/org/hisp/dhis/analytics/tei/TrackedEntityQueryTest.java
+++ dhis-2/dhis-test-e2e/src/test/java/org/hisp/dhis/analytics/tei/TrackedEntityQueryTest.java
@@ -3035,4 +3035,23 @@ public class TrackedEntityQueryTest extends AnalyticsApiTest {
     // Then
     response.validate().statusCode(200);
   }
+
+  @Test
+  public void multipleDateFiltersShouldUseOr() {
+    // Given
+    QueryParamsBuilder params =
+        new QueryParamsBuilder()
+            .add("headers=w75KJ2mc4zz,zDhUuAYrxNC")
+            .add("created=2014-04-28,2014-03-06")
+            .add("desc=lastUpdated");
+
+    // When
+    ApiResponse response = analyticsTeiActions.query().get("nEenWmSyUEp", JSON, JSON, params);
+
+    // Then
+    response.validate().statusCode(200);
+
+    validateRow(response, 0, List.of("John", "Kelly"));
+    validateRow(response, 1, List.of("John", "Doe"));
+  }
 }
