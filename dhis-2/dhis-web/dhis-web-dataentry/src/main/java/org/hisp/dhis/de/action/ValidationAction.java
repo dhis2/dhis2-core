a43621ea37d3352ba25fbe7236df23daeb8e7214
Lars Helge Øverland <lars@dhis2.org>
2024-02-07 14:15:17 +0100

    fix: Update code

--- dhis-2/dhis-web/dhis-web-dataentry/src/main/java/org/hisp/dhis/de/action/ValidationAction.java
+++ dhis-2/dhis-web/dhis-web-dataentry/src/main/java/org/hisp/dhis/de/action/ValidationAction.java
@@ -27,12 +27,15 @@
  */
 package org.hisp.dhis.de.action;
 
+import com.google.common.collect.Sets;
+import com.opensymphony.xwork2.Action;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+import lombok.extern.slf4j.Slf4j;
 import org.hisp.dhis.category.CategoryOptionCombo;
 import org.hisp.dhis.category.CategoryService;
 import org.hisp.dhis.dataanalysis.DataAnalysisService;
@@ -52,9 +55,6 @@ import org.hisp.dhis.validation.ValidationResult;
 import org.hisp.dhis.validation.ValidationService;
 import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
-import com.google.common.collect.Sets;
-import com.opensymphony.xwork2.Action;
-import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author Margrethe Store
