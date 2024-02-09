fe235415a2e4c71409e72b2e866bc83c117142b2
Lars Helge Øverland <lars@dhis2.org>
2024-02-09 14:58:51 +0100

    fix: Update code

--- dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/data/DefaultQueryPlanner.java
+++ dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/data/DefaultQueryPlanner.java
@@ -80,17 +80,11 @@ import org.springframework.stereotype.Component;
 public class DefaultQueryPlanner implements QueryPlanner {
   private final PartitionManager partitionManager;
 
-  // -------------------------------------------------------------------------
-  // QueryPlanner implementation
-  // -------------------------------------------------------------------------
-
   @Override
   public DataQueryGroups planQuery(DataQueryParams params, QueryPlannerParams plannerParams) {
     params = PeriodOffsetUtils.addShiftedPeriods(params);
 
-    // ---------------------------------------------------------------------
     // Group queries which can be executed together
-    // ---------------------------------------------------------------------
 
     params = withTableNameAndPartitions(params, plannerParams);
 
@@ -119,9 +113,7 @@ public class DefaultQueryPlanner implements QueryPlanner {
       currentQueries.forEach(query -> queries.addAll(grouper.apply(query)));
     }
 
-    // ---------------------------------------------------------------------
-    // Split queries until optimal number
-    // ---------------------------------------------------------------------
+    // Split queries until the optimal number is reached
 
     DataQueryGroups queryGroups = DataQueryGroups.newBuilder().withQueries(queries).build();
 
