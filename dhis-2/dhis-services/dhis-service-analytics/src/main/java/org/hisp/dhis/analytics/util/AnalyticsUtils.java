092ff09d9d4a178b7520665243e1c6cdc34d631c
Jason Pickering <jason.p.pickering@gmail.com>
2024-02-12 13:41:45 +0100

    fix: Do not export storedby with aggregate data exchanges [DHIS2-16761]

--- dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/util/AnalyticsUtils.java
+++ dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/util/AnalyticsUtils.java
@@ -479,7 +479,6 @@ public class AnalyticsUtils {
       dv.setAttributeOptionCombo(aoc != null ? String.valueOf(aoc) : null);
       dv.setValue(String.valueOf(row.get(vlInx)));
       dv.setComment(KEY_AGG_VALUE);
-      dv.setStoredBy(KEY_AGG_VALUE);
       dv.setCreated(created);
       dv.setLastUpdated(created);
 
@@ -533,7 +532,7 @@ public class AnalyticsUtils {
       objects.add(row.get(coInx));
       objects.add(row.get(aoInx));
       objects.add(row.get(vlInx));
-      objects.add(KEY_AGG_VALUE);
+      objects.add("");
       objects.add(created);
       objects.add(created);
       objects.add(KEY_AGG_VALUE);
