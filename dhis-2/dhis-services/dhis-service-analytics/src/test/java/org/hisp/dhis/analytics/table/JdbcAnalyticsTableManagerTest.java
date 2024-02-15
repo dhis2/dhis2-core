548c5b2244768a1b8950a8688dda6e99fd3cb081
Maikel Arabori <maikelarabori@gmail.com>
2024-02-15 11:53:24 -0300

    fix: Dependency issue [DHIS2-16809_2.41]

--- dhis-2/dhis-services/dhis-service-analytics/src/test/java/org/hisp/dhis/analytics/table/JdbcAnalyticsTableManagerTest.java
+++ dhis-2/dhis-services/dhis-service-analytics/src/test/java/org/hisp/dhis/analytics/table/JdbcAnalyticsTableManagerTest.java
@@ -28,8 +28,8 @@
 package org.hisp.dhis.analytics.table;
 
 import static org.hisp.dhis.db.model.Logged.LOGGED;
-import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
