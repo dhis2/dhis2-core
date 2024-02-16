a9e256a65dc3f3ec064ab4469d4c7ef848f8fe9c
Lars Helge Øverland <lars@dhis2.org>
2024-02-16 11:55:21 +0100

    fix: Update code

--- dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/db/sql/SqlBuilderProvider.java
+++ dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/db/sql/SqlBuilderProvider.java
@@ -60,6 +60,9 @@ public class SqlBuilderProvider {
   private SqlBuilder getSqlBuilder(AnalyticsTableSettings config) {
     Database database = config.getAnalyticsDatabase();
     Objects.requireNonNull(database);
-    return new PostgreSqlBuilder();
+    switch (database) {
+      default:
+        return new PostgreSqlBuilder();
+    }
   }
 }
