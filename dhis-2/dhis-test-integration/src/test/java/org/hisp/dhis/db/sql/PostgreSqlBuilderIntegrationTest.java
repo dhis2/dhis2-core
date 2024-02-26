c11aa6eedc07319fcf03fcd367b7cfc099234892
Lars Helge Øverland <lars@dhis2.org>
2024-02-26 11:38:39 +0100

    fix: Update code

--- dhis-2/dhis-test-integration/src/test/java/org/hisp/dhis/db/sql/PostgreSqlBuilderIntegrationTest.java
+++ dhis-2/dhis-test-integration/src/test/java/org/hisp/dhis/db/sql/PostgreSqlBuilderIntegrationTest.java
@@ -30,6 +30,7 @@ package org.hisp.dhis.db.sql;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.util.List;
 import org.hisp.dhis.db.model.Collation;
 import org.hisp.dhis.db.model.Column;
