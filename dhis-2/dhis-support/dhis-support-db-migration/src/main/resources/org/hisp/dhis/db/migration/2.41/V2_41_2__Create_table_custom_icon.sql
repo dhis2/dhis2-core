d650de69f69ed789a60b42a9e8269ae3e4606048
zubaira <zubair@dhis2.org>
2024-02-27 12:06:08 +0100

    make createdBy/lastUpdatedBy nullable

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.41/V2_41_2__Create_table_custom_icon.sql
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.41/V2_41_2__Create_table_custom_icon.sql
@@ -13,8 +13,8 @@ CREATE TABLE IF NOT EXISTS customicon
     keywords            jsonb          NULL,
     created             TIMESTAMP      NOT NULL DEFAULT CURRENT_TIMESTAMP,
     lastupdated         TIMESTAMP      NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    createdby           bigint         NOT NULL,
-    lastupdatedby       bigint         NOT NULL,
+    createdby           bigint         NULL,
+    lastupdatedby       bigint         NULL,
     custom              Boolean        NOT NULL,
     CONSTRAINT customicon_pkey PRIMARY KEY (id),
     CONSTRAINT customicon_ukey UNIQUE (iconkey),
@@ -26,9 +26,3 @@ CREATE TABLE IF NOT EXISTS customicon
 
 ALTER TABLE customicon DROP CONSTRAINT IF EXISTS fk_customicon_fileresource;
 ALTER TABLE customicon ADD CONSTRAINT fk_customicon_fileresource FOREIGN KEY (fileresourceid) REFERENCES fileresource (fileresourceid) ON DELETE CASCADE;
-
-ALTER TABLE customicon DROP CONSTRAINT IF EXISTS fk_createdby_userid;
-ALTER TABLE customicon ADD CONSTRAINT fk_createdby_userid FOREIGN KEY (createdby) REFERENCES userinfo (userinfoid);
-
-ALTER TABLE customicon DROP CONSTRAINT IF EXISTS fk_lastupdateby_userid;
-ALTER TABLE customicon ADD CONSTRAINT fk_lastupdateby_userid FOREIGN KEY (lastupdatedby) REFERENCES userinfo (userinfoid);
