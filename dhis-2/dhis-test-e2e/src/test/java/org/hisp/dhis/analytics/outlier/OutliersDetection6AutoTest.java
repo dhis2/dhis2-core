1c9f727004e4974fab9288de509fb3043fd94e26
d-bernat <mail@it-bernat.de>
2024-03-01 18:32:29 +0100

    e2e test fix

--- dhis-2/dhis-test-e2e/src/test/java/org/hisp/dhis/analytics/outlier/OutliersDetection6AutoTest.java
+++ dhis-2/dhis-test-e2e/src/test/java/org/hisp/dhis/analytics/outlier/OutliersDetection6AutoTest.java
@@ -30,8 +30,10 @@ package org.hisp.dhis.analytics.outlier;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hisp.dhis.analytics.ValidationHelper.validateHeader;
+import static org.hisp.dhis.analytics.ValidationHelper.validateRow;
 import static org.skyscreamer.jsonassert.JSONAssert.assertEquals;
 
+import java.util.List;
 import java.util.Map;
 import org.hisp.dhis.AnalyticsApiTest;
 import org.hisp.dhis.actions.analytics.AnalyticsOutlierDetectionActions;
@@ -50,17 +52,14 @@ public class OutliersDetection6AutoTest extends AnalyticsApiTest {
     // Given
     QueryParamsBuilder params =
         new QueryParamsBuilder()
-            .add(
-                "headers=dxname,cocname,pename,ouname,value,median,modifiedzscore,medianabsdeviation,lowerbound,upperbound")
-            .add("dx=fbfJHSPpUQD")
-            .add("pe=LAST_12_MONTHS")
-            .add("displayProperty=NAME")
-            .add("maxResults=20")
-            .add("ou=DvzKyuC0G4w")
-            .add("sortOrder=desc")
-            .add("orderBy=value")
+            .add("pe=THIS_YEAR")
+            .add("ou=eIQbndfxQMb")
+            .add("maxResults=5")
+            .add("orderBy=modifiedZScore")
             .add("threshold=3")
-            .add("algorithm=MODIFIED_Z_SCORE");
+            .add("ds=BfMAe6Itzgt")
+            .add("algorithm=MODIFIED_Z_SCORE")
+            .add("relativePeriodDate=2022-07-26");
 
     // When
     ApiResponse response = actions.query().get("", JSON, JSON, params);
@@ -69,46 +68,63 @@ public class OutliersDetection6AutoTest extends AnalyticsApiTest {
     response
         .validate()
         .statusCode(200)
-        .body("headers", hasSize(equalTo(10)))
-        .body("rows", hasSize(equalTo(0)))
-        .body("height", equalTo(0))
-        .body("width", equalTo(0))
-        .body("headerWidth", equalTo(10));
+        .body("headers", hasSize(equalTo(18)))
+        .body("rows", hasSize(equalTo(5)))
+        .body("height", equalTo(5))
+        .body("width", equalTo(18))
+        .body("headerWidth", equalTo(18));
 
     // Assert metaData.
     String expectedMetaData =
-        "{\"count\":0,\"orderBy\":\"VALUE\",\"threshold\":\"3.0\",\"maxResults\":20,\"algorithm\":\"MODIFIED_Z_SCORE\"}";
+        "{\"count\":5,\"orderBy\":\"MODIFIED_Z_SCORE\",\"threshold\":\"3.0\",\"maxResults\":5,\"algorithm\":\"MODIFIED_Z_SCORE\"}";
     String actualMetaData = new JSONObject((Map) response.extract("metaData")).toString();
     assertEquals(expectedMetaData, actualMetaData, false);
 
     // Assert headers.
-    validateHeader(response, 0, "dxname", "Data name", "TEXT", "java.lang.String", false, false);
+    validateHeader(response, 0, "dx", "Data", "TEXT", "java.lang.String", false, false);
+    validateHeader(response, 1, "dxname", "Data name", "TEXT", "java.lang.String", false, false);
+    validateHeader(response, 2, "pe", "Period", "TEXT", "java.lang.String", false, false);
+    validateHeader(response, 3, "pename", "Period name", "TEXT", "java.lang.String", false, false);
+    validateHeader(
+        response, 4, "ou", "Organisation unit", "TEXT", "java.lang.String", false, false);
+    validateHeader(
+        response, 5, "ouname", "Organisation unit name", "TEXT", "java.lang.String", false, false);
     validateHeader(
         response,
-        1,
+        6,
+        "ounamehierarchy",
+        "Organisation unit name hierarchy",
+        "TEXT",
+        "java.lang.String",
+        false,
+        false);
+    validateHeader(
+        response, 7, "coc", "Category option combo", "TEXT", "java.lang.String", false, false);
+    validateHeader(
+        response,
+        8,
         "cocname",
         "Category option combo name",
         "TEXT",
         "java.lang.String",
         false,
         false);
-    validateHeader(response, 2, "pename", "Period name", "TEXT", "java.lang.String", false, false);
     validateHeader(
-        response, 3, "ouname", "Organisation unit name", "TEXT", "java.lang.String", false, false);
-    validateHeader(response, 4, "value", "Value", "NUMBER", "java.lang.Double", false, false);
-    validateHeader(response, 5, "median", "Median", "NUMBER", "java.lang.Double", false, false);
+        response, 9, "aoc", "Attribute option combo", "TEXT", "java.lang.String", false, false);
     validateHeader(
         response,
-        6,
-        "modifiedzscore",
-        "Modified Z-score",
-        "NUMBER",
-        "java.lang.Double",
+        10,
+        "aocname",
+        "Attribute option combo name",
+        "TEXT",
+        "java.lang.String",
         false,
         false);
+    validateHeader(response, 11, "value", "Value", "NUMBER", "java.lang.Double", false, false);
+    validateHeader(response, 12, "median", "Median", "NUMBER", "java.lang.Double", false, false);
     validateHeader(
         response,
-        7,
+        13,
         "medianabsdeviation",
         "Median absolute deviation",
         "NUMBER",
@@ -116,10 +132,126 @@ public class OutliersDetection6AutoTest extends AnalyticsApiTest {
         false,
         false);
     validateHeader(
-        response, 8, "lowerbound", "Lower boundary", "NUMBER", "java.lang.Double", false, false);
+        response, 14, "absdev", "Absolute deviation", "NUMBER", "java.lang.Double", false, false);
     validateHeader(
-        response, 9, "upperbound", "Upper boundary", "NUMBER", "java.lang.Double", false, false);
+        response,
+        15,
+        "modifiedzscore",
+        "Modified Z-score",
+        "NUMBER",
+        "java.lang.Double",
+        false,
+        false);
+    validateHeader(
+        response, 16, "lowerbound", "Lower boundary", "NUMBER", "java.lang.Double", false, false);
+    validateHeader(
+        response, 17, "upperbound", "Upper boundary", "NUMBER", "java.lang.Double", false, false);
 
     // Assert rows.
+    validateRow(
+        response,
+        List.of(
+            "tU7GixyHhsv",
+            "Vitamin A given to < 5y",
+            "202206",
+            "202206",
+            "scc4QyxenJd",
+            "Makali CHC",
+            "Sierra Leone / Tonkolili / Kunike Barina / Makali CHC",
+            "Prlt0C1RF0s",
+            "Fixed, <1y",
+            "HllvX50cXC0",
+            "default",
+            "714.0",
+            "10.0",
+            "3.0",
+            "704.0",
+            "158.28",
+            "-3.34",
+            "23.34"));
+    validateRow(
+        response,
+        List.of(
+            "qw2sIef52Fu",
+            "Children getting therapeutic feeding",
+            "202205",
+            "202205",
+            "sesv0eXljBq",
+            "Yele CHC",
+            "Sierra Leone / Tonkolili / Gbonkonlenken / Yele CHC",
+            "Prlt0C1RF0s",
+            "Fixed, <1y",
+            "HllvX50cXC0",
+            "default",
+            "55.0",
+            "8.0",
+            "1.0",
+            "47.0",
+            "31.7",
+            "3.55",
+            "12.45"));
+    validateRow(
+        response,
+        List.of(
+            "pnL2VG8Bn7N",
+            "Weight for height 70-79 percent",
+            "202201",
+            "202201",
+            "Ahh47q8AkId",
+            "Mabang CHC",
+            "Sierra Leone / Tonkolili / Kholifa Mabang / Mabang CHC",
+            "Prlt0C1RF0s",
+            "Fixed, <1y",
+            "HllvX50cXC0",
+            "default",
+            "25.0",
+            "3.0",
+            "1.0",
+            "22.0",
+            "14.84",
+            "-1.45",
+            "7.45"));
+    validateRow(
+        response,
+        List.of(
+            "bTcRDVjC66S",
+            "Weight for age below lower line (red)",
+            "202201",
+            "202201",
+            "Ahh47q8AkId",
+            "Mabang CHC",
+            "Sierra Leone / Tonkolili / Kholifa Mabang / Mabang CHC",
+            "Prlt0C1RF0s",
+            "Fixed, <1y",
+            "HllvX50cXC0",
+            "default",
+            "24.0",
+            "4.0",
+            "1.0",
+            "20.0",
+            "13.49",
+            "-0.45",
+            "8.45"));
+    validateRow(
+        response,
+        List.of(
+            "tU7GixyHhsv",
+            "Vitamin A given to < 5y",
+            "202202",
+            "202202",
+            "wB4R3E1X6pC",
+            "Masanga Leprosy Hospital",
+            "Sierra Leone / Tonkolili / Kholifa Rowalla / Masanga Leprosy Hospital",
+            "psbwp3CQEhs",
+            "Fixed, >1y",
+            "HllvX50cXC0",
+            "default",
+            "26.0",
+            "10.0",
+            "1.0",
+            "16.0",
+            "10.79",
+            "5.55",
+            "14.45"));
   }
 }
