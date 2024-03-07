92be29fe518b683a7a61de579ee0e13a355cbe73
Birk Johansson <birk@dhis2.org>
2024-03-07 20:01:34 +0100

    style: fix code-style

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/icon/IconOperationParams.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/icon/IconOperationParams.java
@@ -32,7 +32,6 @@ import java.util.Date;
 import java.util.List;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import org.apache.commons.lang3.StringUtils;
 import org.hisp.dhis.common.Pager;
 
