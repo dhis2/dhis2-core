8358b4fadff1b8c267c76a3f067eb0d87e9c9938
Lars Helge Øverland <lars@dhis2.org>
2024-03-11 17:07:50 +0100

    fix: Update code

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/security/ldap/authentication/CustomLdapAuthenticationProvider.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/security/ldap/authentication/CustomLdapAuthenticationProvider.java
@@ -74,8 +74,7 @@ public class CustomLdapAuthenticationProvider extends LdapAuthenticationProvider
     UserDetails userDetails = userDetailsService.loadUserByUsername(user.getUsername());
 
     if (userDetails == null) {
-      String msg =
-          format("Could not find DHIS 2 user account with username: '{}'", user.getUsername());
+      String msg = format("Could not find DHIS 2 user with username: '{}'", user.getUsername());
       throw new UsernameNotFoundException(msg);
     }
 
