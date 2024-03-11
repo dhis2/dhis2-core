31da33e2b92bdc819f40192477318758d002516b
Lars Helge Øverland <lars@dhis2.org>
2024-03-11 21:15:10 +0100

    fix: Update code

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/security/ldap/authentication/CustomLdapAuthenticationProvider.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/security/ldap/authentication/CustomLdapAuthenticationProvider.java
@@ -28,11 +28,19 @@
 package org.hisp.dhis.security.ldap.authentication;
 
 import static com.google.common.base.Preconditions.checkNotNull;
+import static java.lang.String.format;
 
 import org.hisp.dhis.external.conf.DhisConfigurationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.ldap.authentication.LdapAuthenticationProvider;
 import org.springframework.security.ldap.authentication.LdapAuthenticator;
 import org.springframework.security.ldap.userdetails.LdapAuthoritiesPopulator;
+import org.springframework.security.ldap.userdetails.LdapUserDetailsImpl;
 import org.springframework.stereotype.Component;
 
 /**
@@ -42,14 +50,40 @@ import org.springframework.stereotype.Component;
 public class CustomLdapAuthenticationProvider extends LdapAuthenticationProvider {
   private final DhisConfigurationProvider configurationProvider;
 
+  private final UserDetailsService userDetailsService;
+
   public CustomLdapAuthenticationProvider(
       LdapAuthenticator authenticator,
+      UserDetailsService userDetailsService,
       LdapAuthoritiesPopulator authoritiesPopular,
       DhisConfigurationProvider configurationProvider) {
     super(authenticator, authoritiesPopular);
 
     checkNotNull(configurationProvider);
     this.configurationProvider = configurationProvider;
+    this.userDetailsService = userDetailsService;
+  }
+
+  @Override
+  public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+
+    Authentication authenticate = super.authenticate(authentication);
+
+    LdapUserDetailsImpl user = (LdapUserDetailsImpl) authenticate.getPrincipal();
+
+    UserDetails userDetails = userDetailsService.loadUserByUsername(user.getUsername());
+
+    if (userDetails == null) {
+      String msg = format("Could not find DHIS 2 user with username: '%s'", user.getUsername());
+      throw new UsernameNotFoundException(msg);
+    }
+
+    UsernamePasswordAuthenticationToken result =
+        UsernamePasswordAuthenticationToken.authenticated(
+            userDetails, user.getPassword(), userDetails.getAuthorities());
+    result.setDetails(authenticate.getDetails());
+
+    return result;
   }
 
   @Override
