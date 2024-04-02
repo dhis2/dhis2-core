c1e31e01d3c6ac5d41f5b25d646e134176ae2fad
Morten Svanaes <msvanaes@dhis2.org>
2024-04-02 19:25:51 +0800

    feat: removes Struts and migrate security config
    Signed-off-by: Morten Svanaes <msvanaes@dhis2.org>

--- dhis-2/dhis-test-e2e/src/test/java/org/hisp/dhis/uitest/LoginPageTest.java
+++ dhis-2/dhis-test-e2e/src/test/java/org/hisp/dhis/uitest/LoginPageTest.java
@@ -34,6 +34,7 @@ import java.net.URL;
 import org.hisp.dhis.helpers.config.TestConfiguration;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
