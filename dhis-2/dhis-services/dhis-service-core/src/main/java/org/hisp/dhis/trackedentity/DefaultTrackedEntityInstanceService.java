bd99aa9937f4f785821738398164375784fcf9a7
Enrico Colasante <enrico@dhis2.org>
2024-04-02 13:27:02 +0200

    fix: Validate event parameters together in TE endpoint [DHIS2-16927] (#17006)
    * fix: Validate event parameters together in TE endpoint [DHIS2-16927]
    
    * Update OpenAPI

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/trackedentity/DefaultTrackedEntityInstanceService.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/trackedentity/DefaultTrackedEntityInstanceService.java
@@ -568,6 +568,14 @@ public class DefaultTrackedEntityInstanceService implements TrackedEntityInstanc
       violation = "Event start and end date must be specified when event status is specified";
     }
 
+    if (!((params.hasEventStatus() && params.hasEventStartDate() && params.hasEventEndDate())
+        || (!params.hasEventStatus()
+            && !params.hasEventStartDate()
+            && !params.hasEventEndDate()))) {
+      violation =
+          "`eventOccurredAfter`, `eventOccurredBefore` and `eventStatus` must be specified together";
+    }
+
     if (params.getAssignedUserSelectionMode() != null
         && params.hasAssignedUsers()
         && !params.getAssignedUserSelectionMode().equals(AssignedUserSelectionMode.PROVIDED)) {
