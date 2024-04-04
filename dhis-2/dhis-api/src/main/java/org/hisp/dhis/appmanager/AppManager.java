bf75a859efc44c158827b6870dd92e71785e55ae
netroms <msvanaes@dhis2.org>
2024-04-04 19:17:52 +0800

    chore: move dhis-web modules to root and clean up struts removal (#17040)

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/appmanager/AppManager.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/appmanager/AppManager.java
@@ -74,11 +74,7 @@ public interface AppManager {
           "translations",
           "usage-analytics",
           "user",
-          "user-profile",
-          // Struts apps
-          "approval",
-          "dataentry",
-          "maintenance-mobile");
+          "user-profile");
 
   static final String WEB_MAINTENANCE_APPMANAGER_AUTHORITY = "M_dhis-web-app-management";
 
