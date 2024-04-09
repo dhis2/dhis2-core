873fde53d5903ad3634beaf14e528d1653853cce
Lars Helge Øverland <lars@dhis2.org>
2024-04-09 17:09:00 +0200

    refactor: Use SqlBuilder for getTablePartitions method [DHIS2-16705]

--- dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/db/sql/SqlBuilderProvider.java
+++ dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/db/sql/SqlBuilderProvider.java
@@ -60,7 +60,9 @@ public class SqlBuilderProvider {
    */
   private SqlBuilder getSqlBuilder(AnalyticsTableSettings config) {
     Database database = config.getAnalyticsDatabase();
+
     Objects.requireNonNull(database);
+
     return switch (database) {
       default -> new PostgreSqlBuilder();
     };
