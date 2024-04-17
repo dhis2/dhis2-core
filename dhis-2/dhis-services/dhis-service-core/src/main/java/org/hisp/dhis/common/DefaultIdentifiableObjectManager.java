b3ce5bd089783fa86ea5f19a17587a7a176a0f58
Jan Bernitt <jaanbernitt@gmail.com>
2024-04-17 13:26:32 +0200

    fix: resolve objects by ID identifier (UID alias) [DHIS2-17186] (#17140) (#17155)

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/common/DefaultIdentifiableObjectManager.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/common/DefaultIdentifiableObjectManager.java
@@ -847,6 +847,7 @@ public class DefaultIdentifiableObjectManager implements IdentifiableObjectManag
     }
 
     switch (property) {
+      case ID:
       case UID:
         return store.getByUid(identifiers);
       case CODE:
