fc94fa0597e4f34f05d87142493072d2c2fb43b5
Viet Nguyen <viet@dhis2.org>
2024-04-18 17:36:47 +0700

    fix: invalidate cache when delete usergroup

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/user/DefaultUserGroupService.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/user/DefaultUserGroupService.java
@@ -88,6 +88,7 @@ public class DefaultUserGroupService implements UserGroupService {
   @Transactional
   public void deleteUserGroup(UserGroup userGroup) {
     userGroupStore.delete(userGroup);
+    aclService.invalidateCurrentUserGroupInfoCache();
   }
 
   @Override
@@ -98,6 +99,7 @@ public class DefaultUserGroupService implements UserGroupService {
     // Clear query cache due to sharing and user group membership
 
     cacheManager.clearQueryCache();
+    aclService.invalidateCurrentUserGroupInfoCache();
   }
 
   @Override
@@ -150,6 +152,7 @@ public class DefaultUserGroupService implements UserGroupService {
         userGroupStore.updateNoAcl(userGroup);
       }
     }
+    aclService.invalidateCurrentUserGroupInfoCache();
   }
 
   @Override
@@ -162,6 +165,7 @@ public class DefaultUserGroupService implements UserGroupService {
         userGroupStore.updateNoAcl(userGroup);
       }
     }
+    aclService.invalidateCurrentUserGroupInfoCache();
   }
 
   @Override
