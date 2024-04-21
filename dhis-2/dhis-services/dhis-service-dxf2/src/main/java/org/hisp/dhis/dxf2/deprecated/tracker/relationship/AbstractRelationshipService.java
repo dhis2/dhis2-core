d3c5402c8518b6c63bfd4281ec7f4357e42c8a66
luca <lucacambi77@gmail.com>
2024-04-21 11:10:02 +0200

    fix: apply tracker data view to relationships [old-tracker]

--- dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/deprecated/tracker/relationship/AbstractRelationshipService.java
+++ dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/deprecated/tracker/relationship/AbstractRelationshipService.java
@@ -493,6 +493,18 @@ public abstract class AbstractRelationshipService implements RelationshipService
         tei =
             trackedEntityInstanceService.getTrackedEntityInstance(
                 dao.getTrackedEntity(), TrackedEntityInstanceParams.TRUE);
+
+        if (constraint.getTrackerDataView() != null) {
+          tei.setAttributes(
+              tei.getAttributes().stream()
+                  .filter(
+                      a ->
+                          constraint
+                              .getTrackerDataView()
+                              .getAttributes()
+                              .contains(a.getAttribute()))
+                  .toList());
+        }
       }
 
       relationshipItem.setTrackedEntityInstance(tei);
@@ -507,6 +519,18 @@ public abstract class AbstractRelationshipService implements RelationshipService
         enrollment.setEnrollment(uid);
       } else {
         enrollment = enrollmentService.getEnrollment(dao.getEnrollment(), EnrollmentParams.TRUE);
+
+        if (constraint.getTrackerDataView() != null) {
+          enrollment.setAttributes(
+              enrollment.getAttributes().stream()
+                  .filter(
+                      a ->
+                          constraint
+                              .getTrackerDataView()
+                              .getAttributes()
+                              .contains(a.getAttribute()))
+                  .toList());
+        }
       }
 
       relationshipItem.setEnrollment(enrollment);
@@ -520,6 +544,18 @@ public abstract class AbstractRelationshipService implements RelationshipService
         event.setEvent(uid);
       } else {
         event = eventService.getEvent(dao.getEvent(), EventParams.FALSE);
+
+        if (constraint.getTrackerDataView() != null) {
+          event.setDataValues(
+              event.getDataValues().stream()
+                  .filter(
+                      d ->
+                          constraint
+                              .getTrackerDataView()
+                              .getDataElements()
+                              .contains(d.getDataElement()))
+                  .collect(Collectors.toSet()));
+        }
       }
 
       relationshipItem.setEvent(event);
