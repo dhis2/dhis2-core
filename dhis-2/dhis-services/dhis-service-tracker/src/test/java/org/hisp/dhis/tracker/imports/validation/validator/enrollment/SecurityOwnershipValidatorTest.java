07511b645831f18221398622be1c6b2e9c3d8d38
marc <marc@dhis2.org>
2024-05-10 12:59:58 +0200

    fix: Validate unenrolled TE before importing [DHIS2-17280] (#17372) (#17384)
    * fix: Validate unenrolled TE before importing [DHIS2-17367]
    
    * fix: Validate unenrolled TE before importing [DHIS2-17367]
    
    * fix: Validate unenrolled TE before importing [DHIS2-17367]
    
    * fix: Validate unenrolled TE before importing [DHIS2-17367]

--- dhis-2/dhis-services/dhis-service-tracker/src/test/java/org/hisp/dhis/tracker/imports/validation/validator/enrollment/SecurityOwnershipValidatorTest.java
+++ dhis-2/dhis-services/dhis-service-tracker/src/test/java/org/hisp/dhis/tracker/imports/validation/validator/enrollment/SecurityOwnershipValidatorTest.java
@@ -107,6 +107,8 @@ class SecurityOwnershipValidatorTest extends DhisConvenienceTest {
 
   private Program program;
 
+  private TrackedEntity trackedEntity;
+
   private Map<String, Map<String, TrackedEntityProgramOwnerOrgUnit>> ownerOrgUnit;
 
   @BeforeEach
@@ -128,6 +130,9 @@ class SecurityOwnershipValidatorTest extends DhisConvenienceTest {
 
     ProgramStage programStage = createProgramStage('A', program);
     programStage.setUid(PS_ID);
+
+    trackedEntity = new TrackedEntity();
+    trackedEntity.setUid(TE_ID);
 
     TrackerIdSchemeParams idSchemes = TrackerIdSchemeParams.builder().build();
     reporter = new Reporter(idSchemes);
@@ -167,6 +172,7 @@ class SecurityOwnershipValidatorTest extends DhisConvenienceTest {
     when(bundle.getPreheat()).thenReturn(preheat);
     when(bundle.getStrategy(enrollment)).thenReturn(TrackerImportStrategy.CREATE_AND_UPDATE);
     when(preheat.getProgram(MetadataIdentifier.ofUid(PROGRAM_ID))).thenReturn(program);
+    when(preheat.getTrackedEntity(TE_ID)).thenReturn(trackedEntity);
     when(aclService.canDataWrite(user, program)).thenReturn(true);
     when(aclService.canDataRead(user, program.getTrackedEntityType())).thenReturn(true);
 
@@ -188,6 +194,7 @@ class SecurityOwnershipValidatorTest extends DhisConvenienceTest {
     when(bundle.getPreheat()).thenReturn(preheat);
     when(bundle.getStrategy(enrollment)).thenReturn(TrackerImportStrategy.CREATE);
     when(preheat.getProgram(MetadataIdentifier.ofUid(PROGRAM_ID))).thenReturn(program);
+    when(preheat.getTrackedEntity(TE_ID)).thenReturn(trackedEntity);
     when(preheat.getOrganisationUnit(MetadataIdentifier.ofUid(ORG_UNIT_ID)))
         .thenReturn(organisationUnit);
     when(organisationUnitService.isInUserHierarchyCached(user, organisationUnit)).thenReturn(true);
