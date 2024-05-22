8a3d8dcf202170234b176fb738c9c7ae46e07ac3
Enrico <enrico@dhis2.org>
2024-05-22 12:40:55 +0200

    Fix review comments

--- dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/tracker/export/event/EventRequestParams.java
+++ dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/tracker/export/event/EventRequestParams.java
@@ -46,6 +46,7 @@ import org.hisp.dhis.fieldfiltering.FieldFilterParser;
 import org.hisp.dhis.fieldfiltering.FieldPath;
 import org.hisp.dhis.organisationunit.OrganisationUnit;
 import org.hisp.dhis.program.Enrollment;
+import org.hisp.dhis.program.Event;
 import org.hisp.dhis.program.Program;
 import org.hisp.dhis.program.ProgramStage;
 import org.hisp.dhis.program.ProgramStatus;
@@ -53,8 +54,6 @@ import org.hisp.dhis.trackedentity.TrackedEntity;
 import org.hisp.dhis.user.User;
 import org.hisp.dhis.webapi.controller.event.webrequest.OrderCriteria;
 import org.hisp.dhis.webapi.controller.tracker.export.PageRequestParams;
-import org.hisp.dhis.webapi.controller.tracker.view.TrackedEntity;
-import org.hisp.dhis.webapi.controller.tracker.view.User;
 import org.hisp.dhis.webapi.webdomain.EndDateTime;
 import org.hisp.dhis.webapi.webdomain.StartDateTime;
 
@@ -189,10 +188,10 @@ public class EventRequestParams implements PageRequestParams {
    * @deprecated use {@link #events} instead which is comma instead of semicolon separated.
    */
   @Deprecated(since = "2.41")
-  @OpenApi.Property({UID[].class, org.hisp.dhis.program.Event.class})
+  @OpenApi.Property({UID[].class, Event.class})
   private String event;
 
-  @OpenApi.Property({UID[].class, org.hisp.dhis.program.Event.class})
+  @OpenApi.Property({UID[].class, Event.class})
   private Set<UID> events = new HashSet<>();
 
   /** Comma separated list of data element filters */
