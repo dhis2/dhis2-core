7b1c7025fee92da7fbe22589d348104ef691d3dc
Jan Bernitt <jaanbernitt@gmail.com>
2024-06-04 14:01:12 +0200

    fix: flaky test setup gist filters (#17695)

--- dhis-2/dhis-test-web-api/src/test/java/org/hisp/dhis/webapi/controller/GistFilterControllerTest.java
+++ dhis-2/dhis-test-web-api/src/test/java/org/hisp/dhis/webapi/controller/GistFilterControllerTest.java
@@ -126,7 +126,7 @@ class GistFilterControllerTest extends AbstractGistControllerTest {
   void testFilter_Like() {
     assertEquals(1, GET("/users/gist?filter=surname:like:mi&headless=true").content().size());
     assertEquals(
-        1, GET("/users/gist?filter=surname:like:?urnameuserA&headless=true").content().size());
+        1, GET("/users/gist?filter=surname:like:?urnameuserGist&headless=true").content().size());
     assertEquals(3, GET("/users/gist?filter=surname:like:Surna*&headless=true").content().size());
     assertEquals(0, GET("/users/gist?filter=surname:like:Zulu&headless=true").content().size());
   }
