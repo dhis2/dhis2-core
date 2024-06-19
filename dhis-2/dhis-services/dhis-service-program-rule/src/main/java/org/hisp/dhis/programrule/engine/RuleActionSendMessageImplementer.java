0524427763f149504337560ac965dd6aaaca9815
Enrico Colasante <enrico@dhis2.org>
2024-06-19 14:18:05 +0200

    fix: Remove publish application events for rule engine notifications [DHIS2-17243] (#17770)
    * fix: Remove publish application events for rule engine notifications [DHIS2-17243]
    
    * More clean up
    
    * Add tests
    
    * Format
    
    * Delete unused application events
    
    * fix: Run most preprocessors only for CREATE and UPDATE [DHIS2-17402]
    
    * Code review comments
    
    * Fix tests

--- dhis-2/dhis-services/dhis-service-program-rule/src/main/java/org/hisp/dhis/programrule/engine/RuleActionSendMessageImplementer.java
+++ dhis-2/dhis-services/dhis-service-program-rule/src/main/java/org/hisp/dhis/programrule/engine/RuleActionSendMessageImplementer.java
@@ -27,22 +27,16 @@
  */
 package org.hisp.dhis.programrule.engine;
 
-import static com.google.common.base.Preconditions.checkNotNull;
-
+import lombok.RequiredArgsConstructor;
 import org.hisp.dhis.notification.logging.ExternalNotificationLogEntry;
-import org.hisp.dhis.notification.logging.NotificationLoggingService;
-import org.hisp.dhis.notification.logging.NotificationTriggerEvent;
 import org.hisp.dhis.notification.logging.NotificationValidationResult;
 import org.hisp.dhis.program.Enrollment;
 import org.hisp.dhis.program.Event;
+import org.hisp.dhis.program.notification.ProgramNotificationService;
 import org.hisp.dhis.program.notification.ProgramNotificationTemplate;
-import org.hisp.dhis.program.notification.ProgramNotificationTemplateService;
-import org.hisp.dhis.program.notification.event.ProgramRuleEnrollmentEvent;
-import org.hisp.dhis.program.notification.event.ProgramRuleStageEvent;
 import org.hisp.dhis.programrule.ProgramRuleActionType;
 import org.hisp.dhis.rules.models.RuleAction;
 import org.hisp.dhis.rules.models.RuleEffect;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
 /**
@@ -57,21 +51,11 @@ import org.springframework.stereotype.Component;
  *
  * @author Zubair Asghar
  */
+@RequiredArgsConstructor
 @Component("org.hisp.dhis.programrule.engine.RuleActionSendMessageImplementer")
-public class RuleActionSendMessageImplementer extends NotificationRuleActionImplementer {
-  // -------------------------------------------------------------------------
-  // Dependencies
-  // -------------------------------------------------------------------------
-
-  private final ApplicationEventPublisher publisher;
-
-  public RuleActionSendMessageImplementer(
-      ProgramNotificationTemplateService programNotificationTemplateService,
-      NotificationLoggingService notificationLoggingService,
-      ApplicationEventPublisher publisher) {
-    super(programNotificationTemplateService, notificationLoggingService);
-    this.publisher = publisher;
-  }
+public class RuleActionSendMessageImplementer implements RuleActionImplementer {
+  private final NotificationHelper notificationHelper;
+  private final ProgramNotificationService programNotificationService;
 
   @Override
   public boolean accept(RuleAction ruleAction) {
@@ -80,68 +64,34 @@ public class RuleActionSendMessageImplementer extends NotificationRuleActionImpl
 
   @Override
   public void implement(RuleEffect ruleEffect, Enrollment enrollment) {
-    NotificationValidationResult result = validate(ruleEffect, enrollment);
+    ProgramNotificationTemplate template =
+        notificationHelper.getNotificationTemplate(ruleEffect.getRuleAction());
 
-    if (!result.isValid()) {
-      return;
+    NotificationValidationResult result = notificationHelper.validate(template, enrollment);
+
+    if (result.isValid()) {
+      programNotificationService.sendProgramRuleTriggeredNotifications(template, enrollment);
+
+      if (result.needsToCreateLogEntry()) {
+        notificationHelper.createLogEntry(template, enrollment);
+      }
     }
-
-    ProgramNotificationTemplate template = result.getTemplate();
-
-    String key = generateKey(template, enrollment);
-
-    publisher.publishEvent(new ProgramRuleEnrollmentEvent(this, template.getId(), enrollment));
-
-    if (result.getLogEntry() != null) {
-      return;
-    }
-
-    ExternalNotificationLogEntry entry = createLogEntry(key, template.getUid());
-    entry.setNotificationTriggeredBy(NotificationTriggerEvent.PROGRAM);
-    entry.setAllowMultiple(template.isSendRepeatable());
-
-    notificationLoggingService.save(entry);
   }
 
   @Override
   public void implement(RuleEffect ruleEffect, Event event) {
-    checkNotNull(event, "Event cannot be null");
+    ProgramNotificationTemplate template =
+        notificationHelper.getNotificationTemplate(ruleEffect.getRuleAction());
+    NotificationValidationResult result =
+        notificationHelper.validate(template, event.getEnrollment());
 
-    NotificationValidationResult result = validate(ruleEffect, event.getEnrollment());
+    if (result.isValid()) {
 
-    // For program without registration
-    if (event.getProgramStage().getProgram().isWithoutRegistration()) {
-      handleSingleEvent(ruleEffect, event);
-      return;
+      programNotificationService.sendProgramRuleTriggeredEventNotifications(template, event);
+
+      if (result.needsToCreateLogEntry()) {
+        notificationHelper.createLogEntry(template, event.getEnrollment());
+      }
     }
-
-    if (!result.isValid()) {
-      return;
-    }
-
-    ProgramNotificationTemplate template = result.getTemplate();
-    String key = generateKey(template, event.getEnrollment());
-
-    publisher.publishEvent(new ProgramRuleStageEvent(this, template.getId(), event));
-
-    if (result.getLogEntry() != null) {
-      return;
-    }
-
-    ExternalNotificationLogEntry entry = createLogEntry(key, template.getUid());
-    entry.setNotificationTriggeredBy(NotificationTriggerEvent.PROGRAM_STAGE);
-    entry.setAllowMultiple(template.isSendRepeatable());
-
-    notificationLoggingService.save(entry);
-  }
-
-  private void handleSingleEvent(RuleEffect ruleEffect, Event event) {
-    ProgramNotificationTemplate template = getNotificationTemplate(ruleEffect.getRuleAction());
-
-    if (template == null) {
-      return;
-    }
-
-    publisher.publishEvent(new ProgramRuleStageEvent(this, template.getId(), event));
   }
 }
