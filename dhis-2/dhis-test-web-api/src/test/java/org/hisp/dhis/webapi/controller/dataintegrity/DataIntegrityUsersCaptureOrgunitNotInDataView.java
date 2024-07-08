a79700179de4715d115a7801c0e26188ad23ea94
jason-p-pickering <jason.p.pickering@gmail.com>
2024-07-08 17:38:31 +0200

    Linting

--- dhis-2/dhis-test-web-api/src/test/java/org/hisp/dhis/webapi/controller/dataintegrity/DataIntegrityUsersCaptureOrgunitNotInDataView.java
+++ dhis-2/dhis-test-web-api/src/test/java/org/hisp/dhis/webapi/controller/dataintegrity/DataIntegrityUsersCaptureOrgunitNotInDataView.java
@@ -27,104 +27,110 @@
  */
 package org.hisp.dhis.webapi.controller.dataintegrity;
 
+import static java.util.stream.Collectors.toUnmodifiableSet;
+import static org.hisp.dhis.web.WebClientUtils.assertStatus;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Set;
 import org.hisp.dhis.jsontree.JsonList;
 import org.hisp.dhis.jsontree.JsonString;
 import org.hisp.dhis.web.HttpStatus;
 import org.hisp.dhis.webapi.json.domain.JsonDataIntegrityDetails;
 import org.junit.jupiter.api.Test;
 
-import java.util.Set;
-
-import static java.util.stream.Collectors.toUnmodifiableSet;
-import static org.hisp.dhis.web.WebClientUtils.assertStatus;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 /**
- * Integrity check to identify users who have a data view organisation unit, but who cannot access data which they have possibly entered
- * at a higher level of the hierarchy. {@see
+ * Integrity check to identify users who have a data view organisation unit, but who cannot access
+ * data which they have possibly entered at a higher level of the hierarchy. {@see
  * dhis-2/dhis-services/dhis-service-administration/src/main/resources/data-integrity-checks/users/users_capture_ou_not_in_data_view_ou.yaml}
  *
  * @author Jason P. Pickering
  */
 class DataIntegrityUsersCaptureOrgunitNotInDataView extends AbstractDataIntegrityIntegrationTest {
 
+  private static final String check = "users_capture_ou_not_in_data_view_ou";
 
-    private static final String check = "users_capture_ou_not_in_data_view_ou";
+  private static final String detailsIdType = "users";
 
-    private static final String detailsIdType = "users";
+  private String orgunitA_uid;
+  private String orgunitB_uid;
 
-    private String orgunitA_uid;
-    private String orgunitB_uid;
+  private String userA_uid;
+  private String userB_uid;
 
-    private String userA_uid;
-    private String userB_uid;
+  private String userRole_uid;
 
-    private String userRole_uid;
+  @Test
+  void testDataCatpureInDataViewHierarchy() {
 
-    @Test
-    void testDataCatpureInDataViewHierarchy() {
-
-        orgunitA_uid =
-            assertStatus(
-                HttpStatus.CREATED,
-                POST(
-                    "/organisationUnits",
-                    "{ 'name': 'Fish District', 'shortName': 'Fish District', 'openingDate' : '2022-01-01'}"));
-
-        orgunitB_uid =
-            assertStatus(
-                HttpStatus.CREATED,
-                POST(
-                    "/organisationUnits",
-                    "{ 'name': 'Pizza District', 'shortName': 'Pizza District', 'openingDate' : '2022-01-01'}"));
-
-        userRole_uid = assertStatus(HttpStatus.CREATED,
-            POST("/userRoles", "{ 'name': 'Test role', 'authorities': ['F_DATAVALUE_ADD'] }") );
-
-       assertStatus(
+    orgunitA_uid =
+        assertStatus(
             HttpStatus.CREATED,
             POST(
-                "/users",
-                "{ 'username': 'bobbytables' , 'password': 'District123+', 'firstName': 'Bobby', 'surname': 'Tables', 'organisationUnits' : [{'id' : '"
-                    + orgunitA_uid
-                    + "'}], 'dataViewOrganisationUnits' : [{'id' : '"
-                    + orgunitA_uid
-                    + "'}], 'userRoles' : [{'id' : '" + userRole_uid + "'}]}") );
+                "/organisationUnits",
+                "{ 'name': 'Fish District', 'shortName': 'Fish District', 'openingDate' : '2022-01-01'}"));
 
-        userB_uid = assertStatus( HttpStatus.CREATED,
+    orgunitB_uid =
+        assertStatus(
+            HttpStatus.CREATED,
+            POST(
+                "/organisationUnits",
+                "{ 'name': 'Pizza District', 'shortName': 'Pizza District', 'openingDate' : '2022-01-01'}"));
+
+    userRole_uid =
+        assertStatus(
+            HttpStatus.CREATED,
+            POST("/userRoles", "{ 'name': 'Test role', 'authorities': ['F_DATAVALUE_ADD'] }"));
+
+    assertStatus(
+        HttpStatus.CREATED,
+        POST(
+            "/users",
+            "{ 'username': 'bobbytables' , 'password': 'District123+', 'firstName': 'Bobby', 'surname': 'Tables', 'organisationUnits' : [{'id' : '"
+                + orgunitA_uid
+                + "'}], 'dataViewOrganisationUnits' : [{'id' : '"
+                + orgunitA_uid
+                + "'}], 'userRoles' : [{'id' : '"
+                + userRole_uid
+                + "'}]}"));
+
+    userB_uid =
+        assertStatus(
+            HttpStatus.CREATED,
             POST(
                 "/users",
                 "{ 'username': 'janedoe' , 'password': 'District123+', 'firstName': 'Jane', 'surname': 'Doe', 'organisationUnits' : [{'id' : '"
                     + orgunitA_uid
                     + "'}], 'dataViewOrganisationUnits' : [{'id' : '"
                     + orgunitB_uid
-                    + "'}], 'userRoles' : [{'id' : '" + userRole_uid + "'}]}") );
+                    + "'}], 'userRoles' : [{'id' : '"
+                    + userRole_uid
+                    + "'}]}"));
 
-        assertStatus( HttpStatus.CREATED,
-            POST(
-                "/users",
-                "{ 'username': 'tommytables' , 'password': 'District123+', 'firstName': 'Tommy', 'surname': 'Tables', 'organisationUnits' : [{'id' : '"
-                    + orgunitA_uid
-                    + "'}],  'userRoles' : [{'id' : '" + userRole_uid + "'}]}") );
+    assertStatus(
+        HttpStatus.CREATED,
+        POST(
+            "/users",
+            "{ 'username': 'tommytables' , 'password': 'District123+', 'firstName': 'Tommy', 'surname': 'Tables', 'organisationUnits' : [{'id' : '"
+                + orgunitA_uid
+                + "'}],  'userRoles' : [{'id' : '"
+                + userRole_uid
+                + "'}]}"));
 
+    // Note that there are already two users which exist due to the overall test setup, thus, five
+    // users in total. Only userB should be flagged.
+    assertHasDataIntegrityIssues(detailsIdType, check, 20, userB_uid, "janedoe", null, true);
 
-        //Note that there are already two users which exist due to the overall test setup, thus, five users in total. Only userB should be flagged.
-        assertHasDataIntegrityIssues(
-            detailsIdType, check, 20, userB_uid, "janedoe", null, true);
+    JsonDataIntegrityDetails details = getDetails(check);
+    JsonList<JsonDataIntegrityDetails.JsonDataIntegrityIssue> issues = details.getIssues();
+    assertEquals(1, issues.size());
 
-        JsonDataIntegrityDetails details = getDetails(check);
-        JsonList<JsonDataIntegrityDetails.JsonDataIntegrityIssue> issues = details.getIssues();
-        assertEquals(1, issues.size());
+    Set<String> detailsRefs =
+        issues.stream()
+            .flatMap(issue -> issue.getRefs().stream())
+            .map(JsonString::string)
+            .collect(toUnmodifiableSet());
 
-        Set<String> detailsRefs =
-            issues.stream().flatMap(issue -> issue.getRefs().stream())
-                .map(JsonString::string)
-                .collect(toUnmodifiableSet());
-
-        assertTrue( detailsRefs.contains( "Fish District" ) );
-
-
-    }
-
+    assertTrue(detailsRefs.contains("Fish District"));
+  }
 }
