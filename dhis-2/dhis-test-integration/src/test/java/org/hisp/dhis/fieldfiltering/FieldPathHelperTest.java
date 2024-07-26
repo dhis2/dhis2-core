2981c33d62f759a9f38d9fd7a6f163486e77c589
David Mackessy <dmackessy@dhis2.org>
2024-07-26 10:48:36 +0100

    fix: Add nameable field filter [DHIS17751]

--- dhis-2/dhis-test-integration/src/test/java/org/hisp/dhis/fieldfiltering/FieldPathHelperTest.java
+++ dhis-2/dhis-test-integration/src/test/java/org/hisp/dhis/fieldfiltering/FieldPathHelperTest.java
@@ -28,6 +28,7 @@
 package org.hisp.dhis.fieldfiltering;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -126,6 +127,38 @@ class FieldPathHelperTest extends SingleSetupIntegrationTestBase {
             .map(FieldPath::getName)
             .collect(Collectors.toList())
             .containsAll(List.of("username", "userRoles")));
+  }
+
+  @Test
+  @DisplayName("nameable field filter for DataElement returns expected fields")
+  void nameableFieldFilterTest() {
+    // given
+    FieldPath fieldPath = new FieldPath(Preset.NAMEABLE.getName(), List.of());
+    Map<String, FieldPath> fieldPathMap = new HashMap<>();
+
+    // when
+    helper.applyPresets(List.of(fieldPath), fieldPathMap, DataElement.class);
+
+    // then
+    assertEquals(7, fieldPathMap.size());
+    assertTrue(
+        Preset.NAMEABLE
+            .getFields()
+            .containsAll(fieldPathMap.values().stream().map(FieldPath::getName).toList()));
+  }
+
+  @Test
+  @DisplayName("persisted field filter for DataElement returns fields")
+  void persistedFieldFilterTest() {
+    // given
+    FieldPath fieldPath = new FieldPath(FieldPreset.PERSISTED, List.of());
+    Map<String, FieldPath> fieldPathMap = new HashMap<>();
+
+    // when
+    helper.applyPresets(List.of(fieldPath), fieldPathMap, DataElement.class);
+
+    // then
+    assertFalse(fieldPathMap.isEmpty());
   }
 
   private void assertPropertyExists(String propertyName, Map<String, FieldPath> fieldMapPath) {
