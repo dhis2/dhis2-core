883add0ed58d704b735fa9a9f3e8963a76072720
Enrico <enrico@dhis2.org>
2024-07-30 15:45:57 +0200

    Rename F_capture authority and label

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/startup/DefaultAdminUserPopulator.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/startup/DefaultAdminUserPopulator.java
@@ -47,7 +47,7 @@ public class DefaultAdminUserPopulator extends TransactionContextStartupRoutine 
   public static final Set<String> ALL_AUTHORITIES =
       Set.of(
           "ALL",
-          "F_capture",
+          "F_CAPTURE_DATASTORE_UPDATE",
           "F_VIEW_EVENT_ANALYTICS",
           "F_METADATA_EXPORT",
           "F_METADATA_IMPORT",
