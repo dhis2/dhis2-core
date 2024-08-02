d7e87e6a43e371922b5b84eb5950528360d8f36e
zubaira <zubair@dhis2.org>
2024-08-02 12:42:51 +0200

    throw exception without log message

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/program/message/DefaultProgramMessageService.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/program/message/DefaultProgramMessageService.java
@@ -263,9 +263,7 @@ public class DefaultProgramMessageService implements ProgramMessageService {
     }
 
     if (!violations.isEmpty()) {
-      String violationMessage = String.join(", ", violations);
-      log.info("Message validation failed: " + violationMessage);
-      throw new IllegalQueryException(violationMessage);
+      throw new IllegalQueryException(String.join(", ", violations));
     }
   }
 
