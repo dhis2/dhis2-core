500e1113597f8957938cffbd541953da244d7768
David Mackessy <dmackessy@dhis2.org>
2024-08-06 11:31:46 +0100

    refactor: Remove unnecessary deps, fix test

--- dhis-2/dhis-web-api/src/test/java/org/hisp/dhis/webapi/controller/user/UserControllerTest.java
+++ dhis-2/dhis-web-api/src/test/java/org/hisp/dhis/webapi/controller/user/UserControllerTest.java
@@ -51,6 +51,7 @@ import java.util.Set;
 import java.util.function.Consumer;
 import org.hisp.dhis.dxf2.metadata.feedback.ImportReport;
 import org.hisp.dhis.dxf2.webmessage.WebMessageException;
+import org.hisp.dhis.feedback.ForbiddenException;
 import org.hisp.dhis.feedback.Stats;
 import org.hisp.dhis.feedback.Status;
 import org.hisp.dhis.feedback.TypeReport;
@@ -283,8 +284,7 @@ class UserControllerTest {
 
     Exception ex =
         assertThrows(
-            UpdateAccessDeniedException.class,
-            () -> userController.expireUser(user.getUid(), new Date()));
+            ForbiddenException.class, () -> userController.expireUser(user.getUid(), new Date()));
     assertEquals("You don't have the proper permissions to update this object.", ex.getMessage());
   }
 
