693a5a27d818fab32b7f779b87163da6e4e9c251
David Mackessy <dmackessy@dhis2.org>
2024-08-07 14:45:11 +0100

    update e2e tests

--- dhis-2/dhis-test-e2e/src/test/java/org/hisp/dhis/datastore/DatastoreEntriesTest.java
+++ dhis-2/dhis-test-e2e/src/test/java/org/hisp/dhis/datastore/DatastoreEntriesTest.java
@@ -244,9 +244,14 @@ class DatastoreEntriesTest extends ApiTest {
     loginActions.loginAsUser(BASIC_USER, "Test1234!");
     ApiResponse getResponse =
         datastoreActions.get("/" + NAMESPACE + "/" + key1).validateStatus(403);
-    assertEquals(
-        "{\"httpStatus\":\"Forbidden\",\"httpStatusCode\":403,\"status\":\"ERROR\",\"message\":\"Access denied for key 'arsenal' in namespace 'football'\"}",
-        getResponse.getAsString());
+
+    getResponse
+        .validate()
+        .body("httpStatus", equalTo("Forbidden"))
+        .body("httpStatusCode", equalTo("403"))
+        .body("status", equalTo("ERROR"))
+        .body("message", equalTo("Access denied for key 'arsenal' in namespace 'football"))
+        .body("errorCode", equalTo("E1006"));
   }
 
   @Test
@@ -442,9 +447,14 @@ class DatastoreEntriesTest extends ApiTest {
     loginActions.loginAsUser(BASIC_USER, "Test1234!");
     ApiResponse getResponse =
         datastoreActions.get("/" + NAMESPACE + "/" + key1 + "/metaData").validateStatus(403);
-    assertEquals(
-        "{\"httpStatus\":\"Forbidden\",\"httpStatusCode\":403,\"status\":\"ERROR\",\"message\":\"Access denied for key 'arsenal' in namespace 'football'\"}",
-        getResponse.getAsString());
+
+    getResponse
+        .validate()
+        .body("httpStatus", equalTo("Forbidden"))
+        .body("httpStatusCode", equalTo("403"))
+        .body("status", equalTo("ERROR"))
+        .body("message", equalTo("Access denied for key 'arsenal' in namespace 'football"))
+        .body("errorCode", equalTo("E1006"));
   }
 
   @Test
