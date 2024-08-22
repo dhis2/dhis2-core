0f3773f1a36cc2183971d367fe4b2932836e8b68
Enrico Colasante <enrico@dhis2.org>
2024-08-22 10:39:13 +0200

    fix: Set CSP header for attachments [DHIS2-17310](2.40) (#18399)
    * fix: Set CSP header for attachments [DHIS2-17310]
    
    * Do not send attachment header for images

--- dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/event/TrackedEntityInstanceController.java
+++ dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/event/TrackedEntityInstanceController.java
@@ -72,6 +72,8 @@ import org.hisp.dhis.dxf2.importsummary.ImportSummaries;
 import org.hisp.dhis.dxf2.importsummary.ImportSummary;
 import org.hisp.dhis.dxf2.webmessage.WebMessage;
 import org.hisp.dhis.dxf2.webmessage.WebMessageException;
+import org.hisp.dhis.external.conf.ConfigurationKey;
+import org.hisp.dhis.external.conf.DhisConfigurationProvider;
 import org.hisp.dhis.feedback.BadRequestException;
 import org.hisp.dhis.fieldfilter.FieldFilterParams;
 import org.hisp.dhis.fieldfilter.FieldFilterService;
@@ -100,6 +102,7 @@ import org.hisp.dhis.webapi.strategy.old.tracker.imports.TrackedEntityInstanceSt
 import org.hisp.dhis.webapi.strategy.old.tracker.imports.request.TrackerEntityInstanceRequest;
 import org.hisp.dhis.webapi.utils.ContextUtils;
 import org.hisp.dhis.webapi.utils.FileResourceUtils;
+import org.hisp.dhis.webapi.utils.HeaderUtils;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -127,6 +130,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @ApiVersion({DhisApiVersion.DEFAULT, DhisApiVersion.ALL})
 @RequiredArgsConstructor
 public class TrackedEntityInstanceController {
+  private static final String FILE_CONTENT_DISPOSITION = "attachment; filename=";
+
+  private static final String IMAGE_CONTENT_DISPOSITION = "filename=";
+
   private final TrackedEntityInstanceService trackedEntityInstanceService;
 
   private final org.hisp.dhis.trackedentity.TrackedEntityInstanceService instanceService;
@@ -148,6 +155,8 @@ public class TrackedEntityInstanceController {
   private final TrackedEntityInstanceCriteriaMapper criteriaMapper;
 
   private final TrackedEntityInstanceStrategyHandler trackedEntityInstanceStrategyHandler;
+
+  private final DhisConfigurationProvider config;
 
   // -------------------------------------------------------------------------
   // READ
@@ -249,7 +258,7 @@ public class TrackedEntityInstanceController {
     FileResourceUtils.setImageFileDimensions(
         fileResource, MoreObjects.firstNonNull(dimension, ImageFileDimension.ORIGINAL));
 
-    setHttpResponse(response, fileResource);
+    setImageHttpResponse(response, fileResource);
 
     try (InputStream inputStream = fileResourceService.getFileResourceContent(fileResource)) {
       BufferedImage img = ImageIO.read(inputStream);
@@ -314,7 +323,7 @@ public class TrackedEntityInstanceController {
 
     validateFileResource(fileResource, value);
 
-    setHttpResponse(response, fileResource);
+    setFileHttpResponse(response, fileResource);
 
     try {
       fileResourceService.copyFileResourceContent(fileResource, response.getOutputStream());
@@ -596,9 +605,20 @@ public class TrackedEntityInstanceController {
     }
   }
 
-  private void setHttpResponse(HttpServletResponse response, FileResource fileResource) {
+  private void setFileHttpResponse(HttpServletResponse response, FileResource fileResource) {
+    setHttpResponse(response, fileResource, FILE_CONTENT_DISPOSITION);
+  }
+
+  private void setImageHttpResponse(HttpServletResponse response, FileResource fileResource) {
+    setHttpResponse(response, fileResource, IMAGE_CONTENT_DISPOSITION);
+  }
+
+  private void setHttpResponse(
+      HttpServletResponse response, FileResource fileResource, String contentDisposition) {
     response.setContentType(fileResource.getContentType());
     response.setContentLengthLong(fileResource.getContentLength());
-    response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "filename=" + fileResource.getName());
+    response.setHeader(
+        HttpHeaders.CONTENT_DISPOSITION, contentDisposition + fileResource.getName());
+    HeaderUtils.setSecurityHeaders(response, config.getProperty(ConfigurationKey.CSP_HEADER_VALUE));
   }
 }
