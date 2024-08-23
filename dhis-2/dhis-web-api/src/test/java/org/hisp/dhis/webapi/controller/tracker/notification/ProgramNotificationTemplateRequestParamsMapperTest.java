12f2d218f591905017290775dddd04c34638475d
zubaira <zubair@dhis2.org>
2024-08-23 09:32:08 +0200

    code style

--- dhis-2/dhis-web-api/src/test/java/org/hisp/dhis/webapi/controller/tracker/notification/ProgramNotificationTemplateRequestParamsMapperTest.java
+++ dhis-2/dhis-web-api/src/test/java/org/hisp/dhis/webapi/controller/tracker/notification/ProgramNotificationTemplateRequestParamsMapperTest.java
@@ -40,8 +40,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockito.junit.jupiter.MockitoSettings;
-import org.mockito.quality.Strictness;
 
 /**
  * @author Zubair Asghar
