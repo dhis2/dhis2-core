81f9fa09a5219696ebd707708d23694027209904
Kai Vandivier <kpvandivier@gmail.com>
2024-08-27 16:06:13 +0200

    chore: package route

--- dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/filter/HttpNoCacheFilter.java
+++ dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/filter/HttpNoCacheFilter.java
@@ -25,7 +25,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package org.hisp.dhis.servlet.filter;
+package org.hisp.dhis.webapi.filter;
 
 import java.io.IOException;
 import java.util.regex.Matcher;
