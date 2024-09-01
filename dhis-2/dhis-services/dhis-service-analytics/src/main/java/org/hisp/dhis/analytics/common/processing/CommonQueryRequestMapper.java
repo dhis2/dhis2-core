13ea6f1ca5c8121be2dc0d558b4fcd52f0f724fc
Giuseppe Nespolino <gnespolino@users.noreply.github.com>
2024-09-01 20:26:57 +0200

    fix: enforce TEI headers order - Weekly periods short labels [DHIS2-17964][DHIS2-17955] (#18482)
    * fix: fixes deprecated field usage [DHIS2-17908]
    
    Signed-off-by: Giuseppe Nespolino <g.nespolino@gmail.com>
    
    * Improve Data Visualizer with fixed period "Weekly (Start Sunday)" labels[2.42-DHIS2-17955]
    
    * Improve Data Visualizer with fixed period "Weekly (Start Sunday)" labels[2.42-DHIS2-17955]
    
    * e2e tests
    
    Signed-off-by: Giuseppe Nespolino <g.nespolino@gmail.com>
    
    * decoded urls
    
    * fix: merge [DHIS2-17974]
    
    Signed-off-by: Giuseppe Nespolino <g.nespolino@gmail.com>
    
    * fix: merge [DHIS2-17974]
    
    Signed-off-by: Giuseppe Nespolino <g.nespolino@gmail.com>
    
    ---------
    
    Signed-off-by: Giuseppe Nespolino <g.nespolino@gmail.com>
    Co-authored-by: d-bernat <mail@it-bernat.de>

--- dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/common/processing/CommonQueryRequestMapper.java
+++ dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/common/processing/CommonQueryRequestMapper.java
@@ -117,10 +117,7 @@ public class CommonQueryRequestMapper {
     // Adds all program attributes from all applicable programs as dimensions
     request
         .getDimension()
-        .addAll(
-            getProgramAttributes(programs)
-                .map(IdentifiableObject::getUid)
-                .collect(Collectors.toSet()));
+        .addAll(getProgramAttributes(programs).map(IdentifiableObject::getUid).toList());
 
     return CommonParams.builder()
         .programs(programs)
