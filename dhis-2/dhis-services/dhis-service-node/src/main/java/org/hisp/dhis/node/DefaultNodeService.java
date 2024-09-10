2f2930f06d9c45b73bc885181ef421ed8e56e5bc
Viet Nguyen <viet@dhis2.org>
2024-09-10 23:52:51 +0700

    code format

--- dhis-2/dhis-services/dhis-service-node/src/main/java/org/hisp/dhis/node/DefaultNodeService.java
+++ dhis-2/dhis-services/dhis-service-node/src/main/java/org/hisp/dhis/node/DefaultNodeService.java
@@ -33,7 +33,6 @@ import java.util.List;
 import java.util.Map;
 import javax.annotation.PostConstruct;
 import org.hisp.dhis.node.types.RootNode;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
