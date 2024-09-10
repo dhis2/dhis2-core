ce315966d075576b614b7d1f65a8727db4f42ec1
David Mackessy <dmackessy@dhis2.org>
2024-09-10 13:28:58 +0100

    fix: Add test for deleting ProgramStageSection with no ProgramStage ref [DHIS2-17889]

---
+++ dhis-2/dhis-test-web-api/src/test/java/org/hisp/dhis/webapi/controller/ProgramStageSectionControllerTest.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2004-2024, University of Oslo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * Neither the name of the HISP project nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.hisp.dhis.webapi.controller;
+
+import static org.hisp.dhis.test.webapi.Assertions.assertWebMessage;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.web.servlet.function.RequestPredicates.POST;
+
+import org.hisp.dhis.test.web.HttpStatus;
+import org.hisp.dhis.test.webapi.H2ControllerIntegrationTestBase;
+import org.hisp.dhis.test.webapi.json.domain.JsonProgramStageSection;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ProgramStageSectionControllerTest extends H2ControllerIntegrationTestBase {
+
+  @Test
+  @DisplayName("A ProgramStageSection with no ProgramStage reference can be deleted successfully")
+  void testCreateProgramStageWithoutProgram() {
+    // given
+    assertWebMessage(
+        HttpStatus.CREATED,
+        POST(
+            "/programStageSections/",
+            "{'name':'test programStageSection', 'sortOrder':1, 'id':'PssUid11112'}"));
+
+    JsonProgramStageSection pss =
+        GET("/programStageSections/PssUid11112")
+            .content(HttpStatus.OK)
+            .as(JsonProgramStageSection.class);
+    assertEquals("test programStageSection", pss.getName());
+
+    // when
+    assertWebMessage(HttpStatus.OK, DELETE("/programStageSections/PssUid11112"));
+
+    // then
+    assertWebMessage(
+        "Not Found",
+        404,
+        "ERROR",
+        "ProgramStageSection with id PssUid11112 could not be found.",
+        GET("/programStageSections/PssUid11112").content(HttpStatus.NOT_FOUND));
+  }
+}
