514349b14eaa6bdb444bec2b4992d3ce7e86c664
jason-p-pickering <jason.p.pickering@gmail.com>
2024-09-13 13:14:24 +0200

    Linting

--- dhis-2/dhis-services/dhis-service-tracker/src/test/java/org/hisp/dhis/tracker/export/OperationsParamsValidatorTest.java
+++ dhis-2/dhis-services/dhis-service-tracker/src/test/java/org/hisp/dhis/tracker/export/OperationsParamsValidatorTest.java
@@ -56,7 +56,6 @@ import org.hisp.dhis.user.UserDetails;
 import org.hisp.dhis.user.UserRole;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -299,7 +298,7 @@ class OperationsParamsValidatorTest {
         exception.getMessage());
   }
 
-@Test
+  @Test
   void shouldReturnTrackedEntityWhenUserHasAccessToTrackedEntity()
       throws ForbiddenException, BadRequestException {
     User user = new User();
@@ -311,7 +310,7 @@ class OperationsParamsValidatorTest {
     assertEquals(trackedEntity, paramsValidator.validateTrackedEntity(TRACKED_ENTITY_UID, user));
   }
 
-@Test
+  @Test
   void shouldThrowForbiddenExceptionWhenUserHasNoAccessToTrackedEntity() {
     User user = new User();
     TrackedEntityType trackedEntityType = new TrackedEntityType("trackedEntityType", "");
