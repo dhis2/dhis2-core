65a6ca042e2cc736affbe476fdba7896fb0b4c05
zubaira <zubair@dhis2.org>
2024-09-16 09:28:27 +0200

    use static import

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/program/notification/DefaultProgramNotificationTemplateStore.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/program/notification/DefaultProgramNotificationTemplateStore.java
@@ -27,6 +27,9 @@
  */
 package org.hisp.dhis.program.notification;
 
+import static org.hisp.dhis.program.notification.ProgramNotificationTemplateParam.DEFAULT_PAGE;
+import static org.hisp.dhis.program.notification.ProgramNotificationTemplateParam.DEFAULT_PAGE_SIZE;
+
 import java.math.BigInteger;
 import java.util.List;
 import javax.persistence.criteria.CriteriaBuilder;
@@ -189,12 +192,8 @@ public class DefaultProgramNotificationTemplateStore
     }
 
     if (!param.isSkipPaging()) {
-      int page =
-          param.getPage() != null ? param.getPage() : ProgramNotificationTemplateParam.DEFAULT_PAGE;
-      int pageSize =
-          param.getPageSize() != null
-              ? param.getPageSize()
-              : ProgramNotificationTemplateParam.DEFAULT_PAGE_SIZE;
+      int page = param.getPage() != null ? param.getPage() : DEFAULT_PAGE;
+      int pageSize = param.getPageSize() != null ? param.getPageSize() : DEFAULT_PAGE_SIZE;
 
       query.setFirstResult((page - 1) * pageSize);
       query.setMaxResults(pageSize);
