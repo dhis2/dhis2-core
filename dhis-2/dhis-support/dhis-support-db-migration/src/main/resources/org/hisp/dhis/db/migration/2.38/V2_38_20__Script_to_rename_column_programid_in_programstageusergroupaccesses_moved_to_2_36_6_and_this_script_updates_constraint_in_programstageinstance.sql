3afbcca602395227c9d2c94959f509c40ee7a5a6
ayobami <agbedaviesayobami@yahoo.com>
2024-10-17 01:23:05 +0200

    steps

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.38/V2_38_20__Script_to_rename_column_programid_in_programstageusergroupaccesses_moved_to_2_36_6_and_this_script_updates_constraint_in_programstageinstance.sql
+++
@@ -1,5 +0,0 @@
-alter table programstageinstance drop CONSTRAINT IF EXISTS fk_programstageinstance_assigneduserid;
-
-ALTER TABLE programstageinstance
-ADD CONSTRAINT fk_programstageinstance_assigneduserid FOREIGN KEY (assigneduserid)
-REFERENCES userinfo(userinfoid);
