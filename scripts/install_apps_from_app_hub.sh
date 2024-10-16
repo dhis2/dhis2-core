4735551af88e1107bb92d39518d0fb6c97a47fa2
Adel Bensaad <adel@dhis2.org>
2024-10-16 09:44:11 +0100

    fix install app script

--- scripts/install_apps_from_app_hub.sh
+++ scripts/install_apps_from_app_hub.sh
@@ -1,35 +1,63 @@
-#!/usr/bin/env bash
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
 
-function split_on_commas() {
-    local IFS=,
-    # strip " before splitting
-    local WORD_LIST=(${1//\"/})
-    # strip ' before splitting
-    WORD_LIST=(${WORD_LIST//\'/})
-    for word in "${WORD_LIST[@]}"; do
-        echo "$word"
-    done
+url=$url
+credentials=$credentials
+
+APP_IDS=('92b75fd0-34cc-451c-942f-3dd0f283bcbd' 'a4cd3827-e717-4e09-965d-ab05df2591e5')
+
+getLatestAvailableAppVersion() {
+  app_hub_id=$1
+  core_version=$(curl -u $credentials $url/api/system/info | jq -r '.version' | grep -o -E '2.[0-9]+')
+  latest_compatible_version_response=$(curl -u $credentials $url/api/appHub/v2/apps/$app_hub_id/versions?minDhisVersion=lte:$core_version | jq -r '.result[0] // empty')
+  echo "$latest_compatible_version_response"
 }
 
-function install_app() {
-    local CREDENTIALS="$USERNAME:$PASSWORD"
-    local APP_NAME=$1
-    local APP_ID=$(curl -fsSL --user $CREDENTIALS $BASE_URL/api/appHub | jq -r --arg name "${APP_NAME}" '.[] | select(.name==$name) | .id')
-    local DHIS2_VERSION=$(curl -fsSL --user $CREDENTIALS $BASE_URL/api/system/info | jq -r '.version' | cut -d '.' -f 1,2)
-    local LATEST_APP_VERSION_ID=$(curl -fsSL --user $CREDENTIALS $BASE_URL/api/appHub/v2/apps/$APP_ID/versions?minDhisVersion=lte:$DHIS2_VERSION | jq -r '.result[0].id // empty')
+installOrUpdate() {
+  app_hub_id=$1
+  app_response=$(curl -u $credentials $url/api/apps | jq --arg app_hub_id $app_hub_id -c '.[] | select(.app_hub_id==$app_hub_id)')
+  app_version=$(echo "$app_response" | jq -r .version)
 
-    if [[ -n "\$LATEST_APP_VERSION_ID" ]]; then
-        curl -fsSL --user $CREDENTIALS -X POST $BASE_URL/api/appHub/$LATEST_APP_VERSION_ID
-        echo "Installed $APP_NAME with ID $APP_ID"
-    else
-        echo "No compatible app version found for DHIS2 version $DHIS2_VERSION" 1>&2
-        exit 1
-    fi
+  latest_compatible_version_response="$(getLatestAvailableAppVersion $app_hub_id)"
+  latest_compatible_version=$(echo $latest_compatible_version_response | jq -r '.version // empty')
+  app_name=$(echo $latest_compatible_version_response | jq -r .slug)
+
+  if [[ -z "$latest_compatible_version" ]]; then 
+    echo "App $app_hub_id is not compatible with this instance. Skipping install."
+    return
+  fi 
+
+  if [ -z "$app_version" ] || [[ $app_version != $latest_compatible_version  ]];then 
+    echo "Installing $app_name app version $latest_compatible_version"
+    download_url=$(echo $latest_compatible_version_response | jq -r .downloadUrl )
+    download_name="$app_name.$latest_compatible_version.zip"
+    downloadApp $download_url $download_name
+    importApp $download_name
+
+  else
+    echo "$app_name app is up-to-date"
+  fi  
 }
 
-split_on_commas "$APPS_TO_INSTALL" | while read CURRENT_APP_NAME; do
-    install_app "$CURRENT_APP_NAME"
+downloadApp () {
+    download_name=$2
+    download_url=$1
+
+    curl -s -L $download_url --output $download_name
+
+}
+importApp() {
+  download_name=$1
+  status=$(curl -o /dev/null -u $credentials -F file=@$download_name $url/api/apps -w "%{http_code}")
+
+  if [ $status != 204 ];then
+    echo 'App install failed!'
+  fi
+}
+
+
+for i in "${APP_IDS[@]}";
+do
+  installOrUpdate $i
 done
-
-echo "All apps installed"
-exit 0
