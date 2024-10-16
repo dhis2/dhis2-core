bc18a7a0054baa20430b14782d5d5650fb7585c6
Lars Helge Øverland <lars@dhis2.org>
2024-10-16 20:10:35 +0200

    fix: Update code

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/dashboard/Dashboard.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/dashboard/Dashboard.java
@@ -68,9 +68,9 @@ public class Dashboard extends BaseNameableObject implements MetadataObject {
   private List<String> allowedFilters = new ArrayList<>();
 
   /**
-   * Provider of embedded dashboards, is never <code>null</code>. The {@link EmbeddedProvider#NONE}
-   * value refers to standard DHIS 2 dashboards. Other values indicate that this dashboard is
-   * sourced from an external provider and embedded in DHIS 2.
+   * Provider of embedded dashboards, is never <code>null</code>. The default {@link
+   * EmbeddedProvider#NONE} value refers to standard DHIS 2 dashboards. Other values indicate that
+   * this dashboard is sourced from an external provider and embedded in DHIS 2.
    */
   private EmbeddedProvider embeddedProvider;
 
