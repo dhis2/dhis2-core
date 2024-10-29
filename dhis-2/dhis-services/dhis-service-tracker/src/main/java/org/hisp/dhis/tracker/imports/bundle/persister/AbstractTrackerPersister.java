fa1428c19804c3c84d0fe60b432f64f6e1912073
Marc <marc@dhis2.org>
2024-10-29 16:06:57 +0100

    test

--- dhis-2/dhis-services/dhis-service-tracker/src/main/java/org/hisp/dhis/tracker/imports/bundle/persister/AbstractTrackerPersister.java
+++ dhis-2/dhis-services/dhis-service-tracker/src/main/java/org/hisp/dhis/tracker/imports/bundle/persister/AbstractTrackerPersister.java
@@ -122,14 +122,12 @@ public abstract class AbstractTrackerPersister<
         //
         persistOwnership(bundle, trackerDto, convertedDto);
 
-        updateDataValues(
-            entityManager, bundle.getPreheat(), trackerDto, convertedDto, bundle.getUser());
-
         //
         // Save or update the entity
         //
         if (isNew(bundle, trackerDto)) {
           entityManager.persist(convertedDto);
+          entityManager.flush();
           typeReport.getStats().incCreated();
           typeReport.addEntity(objectReport);
           updateAttributes(
@@ -148,6 +146,9 @@ public abstract class AbstractTrackerPersister<
                 .ifPresent(updatedTrackedEntities::add);
           }
         }
+
+        updateDataValues(
+            entityManager, bundle.getPreheat(), trackerDto, convertedDto, bundle.getUser());
 
         if (!bundle.isSkipSideEffects()) {
           notificationDataBundles.add(handleNotifications(bundle, convertedDto, triggers));
