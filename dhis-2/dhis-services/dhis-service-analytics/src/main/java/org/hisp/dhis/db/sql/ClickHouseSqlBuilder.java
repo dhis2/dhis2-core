3d84bc68a80aeab95e1b54298645c6d7bd974d0d
Lars Helge Øverland <lars@dhis2.org>
2024-11-15 10:51:40 +0100

    fix: Update code

--- dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/db/sql/ClickHouseSqlBuilder.java
+++ dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/db/sql/ClickHouseSqlBuilder.java
@@ -29,13 +29,13 @@ package org.hisp.dhis.db.sql;
 
 import java.util.Map;
 import java.util.Map.Entry;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.Validate;
 import org.hisp.dhis.db.model.Column;
 import org.hisp.dhis.db.model.Index;
 import org.hisp.dhis.db.model.Table;
 import org.hisp.dhis.db.model.constraint.Nullable;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
@@ -181,12 +181,10 @@ public class ClickHouseSqlBuilder extends AbstractSqlBuilder {
     return value.replace(SINGLE_QUOTE, (SINGLE_QUOTE + SINGLE_QUOTE));
   }
 
-  /**
-   * Uses the <code>postgresql</code> table function to query DHIS 2 PostgreSQL server.
-   */
+  /** Uses the <code>postgresql</code> table function to query DHIS 2 PostgreSQL server. */
   @Override
   public String qualifyTable(String name) {
-    return String.format("postgresql.%s", SCHEMA, quote(name));
+    return String.format("postgresql(%s, table=%s", quote(NAMED_COLLECTION), singleQuote(name));
   }
 
   @Override
