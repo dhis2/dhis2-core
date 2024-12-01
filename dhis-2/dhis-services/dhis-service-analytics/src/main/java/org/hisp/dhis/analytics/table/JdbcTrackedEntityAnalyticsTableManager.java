0c2ccc294f1d88c90b1d2f4c2cac7512858b4f98
Lars Helge Øverland <lars@dhis2.org>
2024-12-01 18:55:25 +0100

    fix: Update code

--- dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/table/JdbcTrackedEntityAnalyticsTableManager.java
+++ dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/table/JdbcTrackedEntityAnalyticsTableManager.java
@@ -353,11 +353,12 @@ public class JdbcTrackedEntityAnalyticsTableManager extends AbstractJdbcTableMan
     removeLastComma(sql)
         .append(" ")
         .append(
-            qualify(
+            replaceQualify(
                 """
                 from ${trackedentity} te \
                 left join analytics_rs_orgunitstructure ous on te.organisationunitid=ous.organisationunitid \
-                left join analytics_rs_organisationunitgroupsetstructure ougs on te.organisationunitid=ougs.organisationunitid"""));
+                left join analytics_rs_organisationunitgroupsetstructure ougs on te.organisationunitid=ougs.organisationunitid""",
+                Map.of()));
 
     ((List<TrackedEntityAttribute>)
             params.getExtraParam(trackedEntityType.getUid(), ALL_NON_CONFIDENTIAL_TET_ATTRIBUTES))
@@ -375,17 +376,17 @@ public class JdbcTrackedEntityAnalyticsTableManager extends AbstractJdbcTableMan
         .append(
             replaceQualify(
                 """
-            where te.trackedentitytypeid = ${tetId} \
-            and te.lastupdated < '${startTime}' \
-            and (ougs.startdate is null or dps.monthstartdate=ougs.startdate) \
-            and exists (select 1 from ${enrollment} en \
-                where en.trackedentityid = te.trackedentityid \
-                and exists (select 1 from ${event} ev \
-                where ev.enrollmentid = en.enrollmentid \
-                and ev.status in (${statuses}) \
-                and ev.deleted = false)) \
-            and te.created is not null \
-            and te.deleted = false""",
+                where te.trackedentitytypeid = ${tetId} \
+                and te.lastupdated < '${startTime}' \
+                and (ougs.startdate is null or dps.monthstartdate=ougs.startdate) \
+                and exists (select 1 from ${enrollment} en \
+                    where en.trackedentityid = te.trackedentityid \
+                    and exists (select 1 from ${event} ev \
+                    where ev.enrollmentid = en.enrollmentid \
+                    and ev.status in (${statuses}) \
+                    and ev.deleted = false)) \
+                and te.created is not null \
+                and te.deleted = false""",
                 Map.of(
                     "tetId", String.valueOf(trackedEntityType.getId()),
                     "startTime", toLongDate(params.getStartTime()),
