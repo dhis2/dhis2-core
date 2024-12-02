41adc0b33450ff5a06239d6158ad4d68983b2e72
Luciano Fiandesio <luciano@fiandesio.com>
2024-12-02 15:58:50 +0100

    Address PR comments #2

--- dhis-2/dhis-support/dhis-support-sql/src/main/java/org/hisp/dhis/db/sql/ClickHouseSqlBuilder.java
+++ dhis-2/dhis-support/dhis-support-sql/src/main/java/org/hisp/dhis/db/sql/ClickHouseSqlBuilder.java
@@ -233,7 +233,8 @@ public class ClickHouseSqlBuilder extends AbstractSqlBuilder {
   public String cast(String column, DataType dataType) {
     return switch (dataType) {
       case NUMERIC -> String.format("toDecimal64(%s, 8)", column); // 8 decimal places precision
-      case BOOLEAN -> String.format("toUInt8(%s) != 0", column);   // ClickHouse uses UInt8 for boolean
+      case BOOLEAN ->
+          String.format("toUInt8(%s) != 0", column); // ClickHouse uses UInt8 for boolean
       case TEXT -> String.format("toString(%s)", column);
     };
   }
