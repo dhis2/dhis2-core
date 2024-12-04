13024a3d2ba3113e5f532cdadb92343623820557
zubaira <zubair@dhis2.org>
2024-12-04 12:26:41 +0100

    minor

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.42/V2_42_26__Add_not_null_constraint_to_trackedentity.sql
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.42/V2_42_26__Add_not_null_constraint_to_trackedentity.sql
@@ -4,7 +4,7 @@ BEGIN
 ALTER TABLE trackedentity ALTER COLUMN trackedentitytypeid SET NOT NULL;
 EXCEPTION
      WHEN not_null_violation THEN
-         RAISE EXCEPTION 'Database contains inconsistent data. For more information about this issue and steps to resolve it, please refer to the migration notes: ' ||
-             'https://github.com/dhis2/dhis2-releases/blob/master/releases/2.42/migration-notes.md#null-trackedentitytype. Detailed error message: %', SQLERRM;
+         RAISE EXCEPTION 'Database contains inconsistent data. For more information about this issue and steps to resolve it, please refer to the migration notes:
+             https://github.com/dhis2/dhis2-releases/blob/master/releases/2.42/migration-notes.md#null-trackedentitytype. Detailed error message: %', SQLERRM;
 END
 $$;
