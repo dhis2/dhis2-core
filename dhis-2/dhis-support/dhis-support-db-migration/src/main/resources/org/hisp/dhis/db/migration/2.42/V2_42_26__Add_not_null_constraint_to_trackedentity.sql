148454536439e8486a8f39322cf4bfbd60a412d4
zubaira <zubair@dhis2.org>
2024-12-05 21:32:49 +0100

    add exception

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.42/V2_42_26__Add_not_null_constraint_to_trackedentity.sql
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.42/V2_42_26__Add_not_null_constraint_to_trackedentity.sql
@@ -26,6 +26,10 @@ IF inconsistent_records_count > 0 THEN
     RAISE NOTICE 'Updated trackedentity: programid = %, trackedentityid = %', results.programid, results.trackedentityid;
     END LOOP;
     ALTER TABLE IF EXISTS trackedentity ALTER COLUMN trackedentitytypeid SET NOT NULL;
+    EXCEPTION
+        WHEN not_null_violation THEN
+        RAISE EXCEPTION 'The database contains inconsistent data that must be resolved before re-running this migration. Please refer to the migration notes for detailed instructions on how to address this issue:
+             https://github.com/dhis2/dhis2-releases/blob/master/releases/2.42/migration-notes.md#null-tracked-entity-type. Detailed error message: %', SQLERRM;
 ELSE
      RAISE NOTICE 'No inconsistencies found, trackedentitytypeid is already populated.';
 END IF;
