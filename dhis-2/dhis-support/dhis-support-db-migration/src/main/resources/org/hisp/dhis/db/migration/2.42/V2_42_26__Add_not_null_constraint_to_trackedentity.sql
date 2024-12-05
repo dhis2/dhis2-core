72868f4171983a126dd00e076dfcfea55cd71f5e
zubaira <zubair@dhis2.org>
2024-12-05 20:29:40 +0100

    Find inconstent trackedentities, fix them and set trackentitytypeid not-null

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.42/V2_42_26__Add_not_null_constraint_to_trackedentity.sql
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.42/V2_42_26__Add_not_null_constraint_to_trackedentity.sql
@@ -1,10 +1,41 @@
 DO $$
+DECLARE
+results RECORD;
+inconsistent_records_count INTEGER;
 BEGIN
+SELECT COUNT(*) INTO inconsistent_records_count FROM trackedentity WHERE trackedentitytypeid IS NULL;
 
-ALTER TABLE IF EXISTS trackedentity ALTER COLUMN trackedentitytypeid SET NOT NULL;
-EXCEPTION
-     WHEN not_null_violation THEN
-         RAISE EXCEPTION 'The database contains inconsistent data that must be resolved before re-running this migration. Please refer to the migration notes for detailed instructions on how to address this issue:
-             https://github.com/dhis2/dhis2-releases/blob/master/releases/2.42/migration-notes.md#null-tracked-entity-type. Detailed error message: %', SQLERRM;
-END
-$$;
+IF inconsistent_records_count > 0 THEN
+        RAISE NOTICE 'Inconsistencies found: %', inconsistent_records_count;
+
+FOR results IN
+SELECT programid, trackedentityid FROM enrollment WHERE trackedentityid IN (
+    SELECT trackedentityid
+    FROM trackedentity
+    WHERE trackedentitytypeid IS NULL
+)
+    LOOP
+UPDATE trackedentity
+SET trackedentitytypeid = (
+    SELECT trackedentitytypeid
+    FROM program
+    WHERE programid = results.programid
+)
+WHERE trackedentityid = results.trackedentityid;
+
+RAISE NOTICE 'Updated trackedentity: programid = %, trackedentityid = %', results.programid, results.trackedentityid;
+END LOOP;
+
+SELECT COUNT(*) INTO inconsistent_records_count FROM trackedentity WHERE trackedentitytypeid IS NULL;
+
+IF inconsistent_records_count = 0 THEN
+    ALTER TABLE IF EXISTS trackedentity ALTER COLUMN trackedentitytypeid SET NOT NULL;
+    RAISE NOTICE 'trackedentitytypeid is now set to NOT NULL.';
+ELSE
+    RAISE NOTICE 'There are still NULL values in trackedentitytypeid. Please review the data.';
+END IF;
+
+ELSE
+     RAISE NOTICE 'No inconsistencies found, trackedentitytypeid is already populated.';
+END IF;
+END $$;
