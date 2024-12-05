746ec8be54ae43ebb1fcdc101a2f389e027a262c
zubaira <zubair@dhis2.org>
2024-12-05 20:44:16 +0100

    update

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.42/V2_42_26__Add_not_null_constraint_to_trackedentity.sql
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.42/V2_42_26__Add_not_null_constraint_to_trackedentity.sql
@@ -8,33 +8,24 @@ SELECT COUNT(*) INTO inconsistent_records_count FROM trackedentity WHERE tracked
 IF inconsistent_records_count > 0 THEN
         RAISE NOTICE 'Inconsistencies found: %', inconsistent_records_count;
 
-FOR results IN
-SELECT programid, trackedentityid FROM enrollment WHERE trackedentityid IN (
-    SELECT trackedentityid
-    FROM trackedentity
-    WHERE trackedentitytypeid IS NULL
-)
+    FOR results IN
+    SELECT programid, trackedentityid FROM enrollment WHERE trackedentityid IN (
+        SELECT trackedentityid
+        FROM trackedentity
+        WHERE trackedentitytypeid IS NULL
+    )
     LOOP
-UPDATE trackedentity
-SET trackedentitytypeid = (
-    SELECT trackedentitytypeid
-    FROM program
-    WHERE programid = results.programid
-)
-WHERE trackedentityid = results.trackedentityid;
+    UPDATE trackedentity
+    SET trackedentitytypeid = (
+        SELECT trackedentitytypeid
+        FROM program
+        WHERE programid = results.programid
+    )
+    WHERE trackedentityid = results.trackedentityid;
 
-RAISE NOTICE 'Updated trackedentity: programid = %, trackedentityid = %', results.programid, results.trackedentityid;
-END LOOP;
-
-SELECT COUNT(*) INTO inconsistent_records_count FROM trackedentity WHERE trackedentitytypeid IS NULL;
-
-IF inconsistent_records_count = 0 THEN
+    RAISE NOTICE 'Updated trackedentity: programid = %, trackedentityid = %', results.programid, results.trackedentityid;
+    END LOOP;
     ALTER TABLE IF EXISTS trackedentity ALTER COLUMN trackedentitytypeid SET NOT NULL;
-    RAISE NOTICE 'trackedentitytypeid is now set to NOT NULL.';
-ELSE
-    RAISE NOTICE 'There are still NULL values in trackedentitytypeid. Please review the data.';
-END IF;
-
 ELSE
      RAISE NOTICE 'No inconsistencies found, trackedentitytypeid is already populated.';
 END IF;
