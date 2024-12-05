e62c46be0ca06d5262e9139edd2f9c73efa17128
zubaira <zubair@dhis2.org>
2024-12-05 10:06:31 +0100

    correction in markdown

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.42/V2_42_26__Add_not_null_constraint_to_trackedentity.sql
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.42/V2_42_26__Add_not_null_constraint_to_trackedentity.sql
@@ -5,6 +5,6 @@ ALTER TABLE IF EXISTS trackedentity ALTER COLUMN trackedentitytypeid SET NOT NUL
 EXCEPTION
      WHEN not_null_violation THEN
          RAISE EXCEPTION 'The database contains inconsistent data that must be resolved before re-running this migration. Please refer to the migration notes for detailed instructions on how to address this issue:
-             https://github.com/dhis2/dhis2-releases/blob/master/releases/2.42/migration-notes.md#null-trackedentitytype. Detailed error message: %', SQLERRM;
+             https://github.com/dhis2/dhis2-releases/blob/master/releases/2.42/migration-notes.md#null-tracked-entity-type. Detailed error message: %', SQLERRM;
 END
 $$;
