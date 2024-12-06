964ed52a8440dc7cc5d4e46eb49c74be63d9d07d
zubaira <zubair@dhis2.org>
2024-12-06 12:54:23 +0100

    code review

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.42/V2_42_26__Add_not_null_constraint_to_trackedentity.sql
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.42/V2_42_26__Add_not_null_constraint_to_trackedentity.sql
@@ -3,18 +3,21 @@ DECLARE
 results RECORD;
 inconsistent_records_count INTEGER;
 BEGIN
+
 SELECT COUNT(*) INTO inconsistent_records_count FROM trackedentity WHERE trackedentitytypeid IS NULL;
 
 IF inconsistent_records_count > 0 THEN
-    RAISE NOTICE 'Inconsistencies found: %', inconsistent_records_count;
+        RAISE NOTICE 'Inconsistencies found: %', inconsistent_records_count;
 
     FOR results IN
-    SELECT programid, trackedentityid FROM enrollment WHERE trackedentityid IN (
+    SELECT programid, trackedentityid
+    FROM enrollment
+    WHERE trackedentityid IN (
         SELECT trackedentityid
         FROM trackedentity
         WHERE trackedentitytypeid IS NULL
     )
-    LOOP
+        LOOP
     UPDATE trackedentity
     SET trackedentitytypeid = (
         SELECT trackedentitytypeid
@@ -25,13 +28,13 @@ IF inconsistent_records_count > 0 THEN
 
     RAISE NOTICE 'Updated trackedentity: programid = %, trackedentityid = %', results.programid, results.trackedentityid;
     END LOOP;
-
-        ALTER TABLE trackedentity ALTER COLUMN trackedentitytypeid SET NOT NULL;
-        RAISE EXCEPTION 'The database contains inconsistent data that must be resolved before re-running this migration. Please refer to the migration notes for detailed instructions on how to address this issue:
-             https://github.com/dhis2/dhis2-releases/blob/master/releases/2.42/migration-notes.md#null-tracked-entity-type. Detailed error message: %', SQLERRM;
-    END IF;
+    ALTER TABLE trackedentity ALTER COLUMN trackedentitytypeid SET NOT NULL;
 ELSE
-     RAISE NOTICE 'No inconsistencies found, trackedentitytypeid is already populated.';
-     ALTER TABLE trackedentity ALTER COLUMN trackedentitytypeid SET NOT NULL;
+        RAISE NOTICE 'No inconsistencies found, trackedentitytypeid is already populated.';
+        ALTER TABLE trackedentity ALTER COLUMN trackedentitytypeid SET NOT NULL;
 END IF;
+
+EXCEPTION
+    WHEN not_null_violation THEN
+        RAISE EXCEPTION 'The database contains inconsistent data that must be resolved before re-running this migration. Please refer to the migration notes for detailed instructions: https://github.com/dhis2/dhis2-releases/blob/master/releases/2.42/migration-notes.md#null-tracked-entity-type. Detailed error message: %', SQLERRM;
 END $$;
