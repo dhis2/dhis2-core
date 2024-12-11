d5316306c0ea6ea7c88f447aea7b133e9e089926
David Mackessy <dmackessy@dhis2.org>
2024-12-11 13:54:15 +0000

    chore: add debug

--- dhis-2/dhis-test-e2e/src/test/java/org/hisp/dhis/ApiTest.java
+++ dhis-2/dhis-test-e2e/src/test/java/org/hisp/dhis/ApiTest.java
@@ -34,7 +34,6 @@ import org.hisp.dhis.helpers.extensions.CoverageLoggerExtension;
 import org.hisp.dhis.helpers.extensions.MetadataSetupExtension;
 import org.hisp.dhis.test.e2e.actions.LoginActions;
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 
@@ -50,11 +49,6 @@ public abstract class ApiTest {
   @AfterAll
   public void afterAll() {
     new LoginActions().loginAsDefaultUser();
-    new TestCleanUp().deleteCreatedEntities();
-  }
-
-  @AfterEach
-  public void cleanup() {
     new TestCleanUp().deleteCreatedEntities();
   }
 }
