78d66550cc247392a1297adc8e20b7825225ad71
David Mackessy <dmackessy@dhis2.org>
2024-12-13 10:30:56 +0000

    feat: Add data value store tests [DHIS2-18321]

--- dhis-2/dhis-test-integration/src/test/java/org/hisp/dhis/datavalue/DataValueStoreTest.java
+++ dhis-2/dhis-test-integration/src/test/java/org/hisp/dhis/datavalue/DataValueStoreTest.java
@@ -38,6 +38,7 @@ import java.util.Date;
 import java.util.List;
 import org.hisp.dhis.category.CategoryOption;
 import org.hisp.dhis.category.CategoryOptionCombo;
+import org.hisp.dhis.common.UID;
 import org.hisp.dhis.common.ValueType;
 import org.hisp.dhis.dataelement.DataElement;
 import org.hisp.dhis.organisationunit.OrganisationUnit;
@@ -124,6 +125,102 @@ class DataValueStoreTest extends PostgresIntegrationTestBase {
     assertFalse(
         allDataValuesByDataElement.contains(dv4),
         "retrieved data values do not contain a data value referencing any of the 2 data elements passed in");
+  }
+
+  @Test
+  @DisplayName("Get all DataValues by CategoryOptionCombo")
+  void getDataValuesByCoc() {
+    // given
+    Period p1 =
+        createPeriod(DateUtils.getDate(2023, 1, 1, 1, 1), DateUtils.getDate(2023, 2, 1, 1, 1));
+    Period p2 =
+        createPeriod(DateUtils.getDate(2023, 3, 1, 1, 1), DateUtils.getDate(2023, 4, 1, 1, 1));
+    Period p3 =
+        createPeriod(DateUtils.getDate(2023, 5, 1, 1, 1), DateUtils.getDate(2023, 6, 1, 1, 1));
+
+    CategoryOptionCombo coc1 = createCategoryOptionCombo('1');
+    coc1.setCategoryCombo(categoryService.getDefaultCategoryCombo());
+    categoryService.addCategoryOptionCombo(coc1);
+
+    CategoryOptionCombo coc2 = createCategoryOptionCombo('2');
+    coc2.setCategoryCombo(categoryService.getDefaultCategoryCombo());
+    categoryService.addCategoryOptionCombo(coc2);
+
+    CategoryOptionCombo coc3 = createCategoryOptionCombo('3');
+    coc3.setCategoryCombo(categoryService.getDefaultCategoryCombo());
+    categoryService.addCategoryOptionCombo(coc3);
+
+    DataValue dv1 = createDataValue('B', p1, "dv test 1");
+    dv1.setCategoryOptionCombo(coc1);
+    DataValue dv2 = createDataValue('C', p2, "dv test 2");
+    dv2.setCategoryOptionCombo(coc2);
+    DataValue dv3 = createDataValue('D', p3, "dv test 3");
+    dv3.setCategoryOptionCombo(coc3);
+
+    dataValueStore.addDataValue(dv1);
+    dataValueStore.addDataValue(dv2);
+    dataValueStore.addDataValue(dv3);
+
+    // when
+    List<DataValue> allDataValuesByCoc =
+        dataValueStore.getAllDataValuesByCatOptCombo(UID.of(coc1, coc2));
+
+    // then
+    assertEquals(2, allDataValuesByCoc.size());
+    assertTrue(
+        allDataValuesByCoc.containsAll(List.of(dv1, dv2)),
+        "retrieved data values contain 2 data values referencing the 2 category opt combos passed in");
+    assertFalse(
+        allDataValuesByCoc.contains(dv3),
+        "retrieved data values do not contain a data value referencing a COC not used in the query");
+  }
+
+  @Test
+  @DisplayName("Get all DataValues by AttributeOptionCombo")
+  void getDataValuesByAoc() {
+    // given
+    Period p1 =
+        createPeriod(DateUtils.getDate(2023, 1, 1, 1, 1), DateUtils.getDate(2023, 2, 1, 1, 1));
+    Period p2 =
+        createPeriod(DateUtils.getDate(2023, 3, 1, 1, 1), DateUtils.getDate(2023, 4, 1, 1, 1));
+    Period p3 =
+        createPeriod(DateUtils.getDate(2023, 5, 1, 1, 1), DateUtils.getDate(2023, 6, 1, 1, 1));
+
+    CategoryOptionCombo aoc1 = createCategoryOptionCombo('1');
+    aoc1.setCategoryCombo(categoryService.getDefaultCategoryCombo());
+    categoryService.addCategoryOptionCombo(aoc1);
+
+    CategoryOptionCombo aoc2 = createCategoryOptionCombo('2');
+    aoc2.setCategoryCombo(categoryService.getDefaultCategoryCombo());
+    categoryService.addCategoryOptionCombo(aoc2);
+
+    CategoryOptionCombo aoc3 = createCategoryOptionCombo('3');
+    aoc3.setCategoryCombo(categoryService.getDefaultCategoryCombo());
+    categoryService.addCategoryOptionCombo(aoc3);
+
+    DataValue dv1 = createDataValue('B', p1, "dv test 1");
+    dv1.setAttributeOptionCombo(aoc1);
+    DataValue dv2 = createDataValue('C', p2, "dv test 2");
+    dv2.setAttributeOptionCombo(aoc2);
+    DataValue dv3 = createDataValue('D', p3, "dv test 3");
+    dv3.setAttributeOptionCombo(aoc3);
+
+    dataValueStore.addDataValue(dv1);
+    dataValueStore.addDataValue(dv2);
+    dataValueStore.addDataValue(dv3);
+
+    // when
+    List<DataValue> allDataValuesByAoc =
+        dataValueStore.getAllDataValuesByAttrOptCombo(UID.of(aoc1, aoc2));
+
+    // then
+    assertEquals(2, allDataValuesByAoc.size());
+    assertTrue(
+        allDataValuesByAoc.containsAll(List.of(dv1, dv2)),
+        "retrieved data values contain 2 data values referencing the 2 attribute opt combos passed in");
+    assertFalse(
+        allDataValuesByAoc.contains(dv3),
+        "retrieved data values do not contain a data value referencing a AOC not used in the query");
   }
 
   private DataValue createDataValue(char uniqueChar, Period period, String value) {
