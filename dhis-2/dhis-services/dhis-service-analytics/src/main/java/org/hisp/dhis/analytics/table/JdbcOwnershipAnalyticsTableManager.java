059689a0935272bd3232ae531cbd3f7fe490bdcb
Lars Helge Øverland <lars@dhis2.org>
2024-12-17 13:55:31 +0100

    fix: Use in subquery instead of exists subquery for ownership [DHIS2-18417] (#19514)

--- dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/table/JdbcOwnershipAnalyticsTableManager.java
+++ dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/table/JdbcOwnershipAnalyticsTableManager.java
@@ -257,10 +257,10 @@ public class JdbcOwnershipAnalyticsTableManager extends AbstractEventJdbcTableMa
             select o.trackedentityid, '${trackedEntityOwnTableId}' as startdate, null as enddate, o.organisationunitid \
             from ${trackedentityprogramowner} o \
             where o.programid = ${programId} \
-            and exists (\
-            select 1 from ${programownershiphistory} p \
-            where o.trackedentityid = p.trackedentityid \
-            and p.programid = ${programId} \
+            and o.trackedentityid in (\
+            select distinct p.trackedentityid \
+            from ${programownershiphistory} p \
+            where p.programid = ${programId} \
             and p.organisationunitid is not null)) a \
             inner join ${trackedentity} te on a.trackedentityid = te.trackedentityid \
             inner join ${organisationunit} ou on a.organisationunitid = ou.organisationunitid \
