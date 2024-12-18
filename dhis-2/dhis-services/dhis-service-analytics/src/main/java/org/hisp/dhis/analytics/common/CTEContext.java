4ffcbb911bed241db1a556091cfd5736285416f6
Luciano Fiandesio <luciano@fiandesio.com>
2024-12-18 11:01:29 +0100

    WIP - spotless

--- dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/common/CTEContext.java
+++ dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/common/CTEContext.java
@@ -1,5 +1,31 @@
+/*
+ * Copyright (c) 2004-2024, University of Oslo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * Neither the name of the HISP project nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package org.hisp.dhis.analytics.common;
-
 
 import java.util.HashMap;
 import java.util.LinkedHashMap;
@@ -7,42 +33,39 @@ import java.util.Map;
 import java.util.Set;
 
 public class CTEContext {
-    private final Map<String, String> cteDefinitions = new LinkedHashMap<>();
-    private final Map<String, String> columnMappings = new HashMap<>();
+  private final Map<String, String> cteDefinitions = new LinkedHashMap<>();
+  private final Map<String, String> columnMappings = new HashMap<>();
 
-    public void addCTE(String cteName, String cteDefinition) {
-        cteDefinitions.put(cteName, cteDefinition);
+  public void addCTE(String cteName, String cteDefinition) {
+    cteDefinitions.put(cteName, cteDefinition);
+  }
+
+  public void addColumnMapping(String originalColumn, String cteReference) {
+    columnMappings.put(originalColumn, cteReference);
+  }
+
+  public String getCTEDefinition() {
+    if (cteDefinitions.isEmpty()) {
+      return "";
     }
 
-    public void addColumnMapping(String originalColumn, String cteReference) {
-        columnMappings.put(originalColumn, cteReference);
+    StringBuilder sb = new StringBuilder("WITH ");
+    boolean first = true;
+    for (Map.Entry<String, String> entry : cteDefinitions.entrySet()) {
+      if (!first) {
+        sb.append(", ");
+      }
+      sb.append(entry.getKey()).append(" AS (").append(entry.getValue()).append(")");
+      first = false;
     }
+    return sb.toString();
+  }
 
-    public String getCTEDefinition() {
-        if (cteDefinitions.isEmpty()) {
-            return "";
-        }
+  public Set<String> getCTENames() {
+    return cteDefinitions.keySet();
+  }
 
-        StringBuilder sb = new StringBuilder("WITH ");
-        boolean first = true;
-        for (Map.Entry<String, String> entry : cteDefinitions.entrySet()) {
-            if (!first) {
-                sb.append(", ");
-            }
-            sb.append(entry.getKey())
-                    .append(" AS (")
-                    .append(entry.getValue())
-                    .append(")");
-            first = false;
-        }
-        return sb.toString();
-    }
-
-    public Set<String> getCTENames() {
-        return cteDefinitions.keySet();
-    }
-
-    public String getColumnMapping(String columnId) {
-        return columnMappings.getOrDefault(columnId, columnId);
-    }
+  public String getColumnMapping(String columnId) {
+    return columnMappings.getOrDefault(columnId, columnId);
+  }
 }
