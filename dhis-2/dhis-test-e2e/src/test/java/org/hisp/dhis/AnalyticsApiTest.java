ca4d3d1cfb6d3b60b20977c9d7f4cbb3bfebeb53
Maikel Arabori <maikelarabori@gmail.com>
2024-12-19 14:18:00 +0100

    fix: Reverting configs [DHIS2-16265]

--- dhis-2/dhis-test-e2e/src/test/java/org/hisp/dhis/AnalyticsApiTest.java
+++ dhis-2/dhis-test-e2e/src/test/java/org/hisp/dhis/AnalyticsApiTest.java
@@ -54,8 +54,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
  *
  * <p>ie.: mvn -Djunit.jupiter.execution.timeout.mode=disabled test
  *
- * <p>All tests are written on top of the database Sierra Leone 2.39.0. It can be downloaded at
- * https://databases.dhis2.org/sierra-leone/2.39.0/analytics_be/dhis2-db-sierra-leone.sql.gz
+ * <p>All tests are written on top of the database Sierra Leone 2.39.6. It can be downloaded at
+ * https://databases.dhis2.org/sierra-leone/2.39.6/analytics_be/dhis2-db-sierra-leone.sql.gz
  *
  * <p>If some test is failing and some investigation is needed, we can simply download the database
  * version above and run the respective WAR of the DHIS2 failing branch on top of that DB. It's a
@@ -65,7 +65,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
  */
 @TestInstance(PER_CLASS)
 @ExtendWith(ConfigurationExtension.class)
-//@ExtendWith(AnalyticsSetupExtension.class)
+@ExtendWith(AnalyticsSetupExtension.class)
 @Timeout(value = AnalyticsApiTest.DEFAULT_LIMIT_EXECUTION_TIME, unit = MINUTES)
 @Tag("analytics")
 public abstract class AnalyticsApiTest {
