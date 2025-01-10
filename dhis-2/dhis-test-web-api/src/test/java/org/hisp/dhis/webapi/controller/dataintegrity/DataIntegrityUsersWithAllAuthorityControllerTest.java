a4e03df24d5952bf44859f67a64e5a5f5728b884
Jason Pickering <jason@dhis2.org>
2025-01-10 14:01:36 +0100

    Initial commit for PR

---
+++ dhis-2/dhis-test-web-api/src/test/java/org/hisp/dhis/webapi/controller/dataintegrity/DataIntegrityUsersWithAllAuthorityControllerTest.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2004-2022, University of Oslo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * Neither the name of the HISP project nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.hisp.dhis.webapi.controller.dataintegrity;
+
+import org.hisp.dhis.common.CodeGenerator;
+import org.hisp.dhis.jsontree.JsonList;
+import org.hisp.dhis.test.webapi.json.domain.JsonDataIntegrityDetails;
+import org.hisp.dhis.user.User;
+import org.hisp.dhis.user.UserRole;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Integrity check to identify users who have the ALL authority. {@see
+ * dhis-2/dhis-services/dhis-service-administration/src/main/resources/data-integrity-checks/users/users_with_all_authority.yaml}
+ *
+ * @author Jason P. Pickering
+ */
+class DataIntegrityUsersWithAllAuthorityControllerTest extends AbstractDataIntegrityIntegrationTest {
+
+  private static final String CHECK_NAME = "users_with_all_authority";
+
+  private static final String DETAILS_ID_TYPE = "users";
+
+
+  @Test
+  void testCanIdentifyUsersWithAllAuthority() {
+
+    List<String> auths = List.of("ALL");
+    UserRole role = new UserRole();
+    role.setName("Role_" + CodeGenerator.generateCode(5));
+    auths.forEach(auth -> role.getAuthorities().add(auth));
+
+    //Create a user with the ALL authority
+    User user = new User();
+    user.setUid(CodeGenerator.generateUid());
+    user.setFirstName("Bobby");
+    user.setSurname("Tables");
+    user.setUsername(DEFAULT_USERNAME + "_no_role_" + CodeGenerator.generateUid());
+    user.setPassword(DEFAULT_ADMIN_PASSWORD);
+    user.setDisabled(false);
+    user.setLastUpdated(new Date());
+    user.getUserRoles().add(role);
+    manager.persist(user);
+    dbmsManager.clearSession();
+
+    // Note that there is already one user which exists due to the overall test setup, thus, two
+    // users in total.
+    Integer userCount = userService.getUserCount();
+    assertEquals(2, userCount);
+
+    assertHasDataIntegrityIssues(
+        DETAILS_ID_TYPE,
+        CHECK_NAME,
+        100 / userCount,
+        user.getUid(),
+        user.getUsername(),
+        "disabled:false",
+        true);
+
+    JsonDataIntegrityDetails details = getDetails(CHECK_NAME);
+    JsonList<JsonDataIntegrityDetails.JsonDataIntegrityIssue> issues = details.getIssues();
+    assertEquals(1, issues.size());
+  }
+
+  @Test
+  void testDoNotFlagUsersWithRoles() {
+
+    assertHasNoDataIntegrityIssues(DETAILS_ID_TYPE, CHECK_NAME, true);
+  }
+}
