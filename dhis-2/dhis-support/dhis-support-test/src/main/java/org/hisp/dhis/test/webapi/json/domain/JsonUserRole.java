d0eddf295fbfe8aa74f772ef899cbf92ca3aa0ab
Jason Pickering <jason@dhis2.org>
2025-01-15 09:06:47 +0100

    feat: Integrity check for users with ALL (#19637)
    * Add integrity check for users with ALL authority

--- dhis-2/dhis-support/dhis-support-test/src/main/java/org/hisp/dhis/test/webapi/json/domain/JsonUserRole.java
+++ dhis-2/dhis-support/dhis-support-test/src/main/java/org/hisp/dhis/test/webapi/json/domain/JsonUserRole.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2004-2024, University of Oslo
+ * Copyright (c) 2004-2025, University of Oslo
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -25,37 +25,17 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package org.hisp.dhis.webapi.controller.dataintegrity;
+package org.hisp.dhis.test.webapi.json.domain;
 
-import static org.hisp.dhis.http.HttpAssertions.assertStatus;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.hisp.dhis.jsontree.JsonList;
+import org.hisp.dhis.jsontree.JsonString;
 
-import org.hisp.dhis.http.HttpStatus;
-import org.hisp.dhis.test.webapi.json.domain.JsonDataIntegritySummary;
-import org.junit.jupiter.api.Test;
+public interface JsonUserRole extends JsonIdentifiableObject {
+  default JsonList<JsonUser> getUsers() {
+    return getList("users", JsonUser.class);
+  }
 
-class DataIntegrityUserRolesNoUsers extends AbstractDataIntegrityIntegrationTest {
-  private static final String CHECK_NAME = "user_roles_with_no_users";
-
-  private static final String DETAILS_ID_TYPE = "userRoles";
-
-  private String userRoleUid;
-
-  @Test
-  void testUserRolesNoUsers() {
-    userRoleUid =
-        assertStatus(
-            HttpStatus.CREATED,
-            POST("/userRoles", "{ 'name': 'Test role', 'authorities': ['F_DATAVALUE_ADD'] }"));
-    // Note that two user roles already exist as part of the setup in the
-    // AbstractDataIntegrityIntegrationTest class
-    // Thus, there should be three roles in total, two of which are valid since they already have
-    // users associated with them.
-    postSummary(CHECK_NAME);
-
-    JsonDataIntegritySummary summary = getSummary(CHECK_NAME);
-    assertEquals(1, summary.getCount());
-    assertHasDataIntegrityIssues(
-        DETAILS_ID_TYPE, CHECK_NAME, 50, userRoleUid, "Test role", null, true);
+  default JsonList<JsonString> getAuthorities() {
+    return getList("authorities", JsonString.class);
   }
 }
