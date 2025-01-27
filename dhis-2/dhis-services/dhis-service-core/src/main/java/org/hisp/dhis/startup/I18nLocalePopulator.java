94f1b8b5e074da5efef8add475e6b21584cdc5a3
Philip-Larsen-Donnelly <35666657+Philip-Larsen-Donnelly@users.noreply.github.com>
2025-01-27 23:39:26 +0100

    Fix the strings to match existing implementations

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/startup/I18nLocalePopulator.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/startup/I18nLocalePopulator.java
@@ -55,8 +55,8 @@ public class I18nLocalePopulator extends TransactionContextStartupRoutine {
   private static final Set<String> DEFAULT_LOCALES =
       Set.of(
           "af", "ar", "bi", "am", "de", "dz", "en", "es", "fa", "fr", "gu", "hi", "id", "it", "km",
-          "lo", "my", "ne", "nl", "no", "ps", "pt", "ru", "rw", "sw", "tg", "uz__latn", "uz__cyrl", "vi", "zh"
-          );
+          "lo", "my", "ne", "nl", "no", "ps", "pt", "ru", "rw", "sw", "tg", "uz_UZ_Cyrl", 
+          "uz_UZ_Latn", "vi", "zh");
 
   @Override
   public void executeInTransaction() {
