e3bb5d2805fd95bfb528f5802adac3d9e301cab3
Viet Nguyen <viet@dhis2.org>
2025-01-30 08:52:52 -0600

    fix: remove Program fk when deleting MapView (#19810)

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/mapping/DefaultMappingService.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/mapping/DefaultMappingService.java
@@ -39,6 +39,8 @@ import org.hisp.dhis.organisationunit.OrganisationUnitService;
 import org.hisp.dhis.period.Period;
 import org.hisp.dhis.period.PeriodService;
 import org.hisp.dhis.period.RelativePeriods;
+import org.hisp.dhis.program.Program;
+import org.hisp.dhis.program.ProgramStage;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -183,6 +185,16 @@ public class DefaultMappingService extends GenericAnalyticalObjectService<MapVie
   @Transactional(readOnly = true)
   public int countMapViewMaps(MapView mapView) {
     return mapStore.countMapViewMaps(mapView);
+  }
+
+  @Override
+  public List<MapView> findByProgram(Program program) {
+    return mapViewStore.findByProgram(program);
+  }
+
+  @Override
+  public List<MapView> findByProgramStage(ProgramStage programStage) {
+    return mapViewStore.findByProgramStage(programStage);
   }
 
   // -------------------------------------------------------------------------
