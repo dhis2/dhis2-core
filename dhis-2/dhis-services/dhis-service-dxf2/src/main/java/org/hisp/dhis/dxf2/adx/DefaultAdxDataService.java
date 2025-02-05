7f5273854ec2198a936331fbb5c81f659e1162b3
Jan Bernitt <jaanbernitt@gmail.com>
2025-02-05 13:18:09 +0100

    fix: revert XML export format changes as test fail that are hard to adjust

--- dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/adx/DefaultAdxDataService.java
+++ dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/adx/DefaultAdxDataService.java
@@ -199,7 +199,6 @@ public class DefaultAdxDataService implements AdxDataService {
               currentPeriod = dv.getPeriod();
               currentOrgUnit = dv.getSource();
 
-              adxWriter.writeCharacters("\n");
               adxWriter.openElement(AdxDataService.GROUP);
               adxWriter.writeAttribute(AdxDataService.DATASET, dataSet.getPropertyValue(dsScheme));
               adxWriter.writeAttribute(AdxDataService.PERIOD, AdxPeriod.serialize(currentPeriod));
@@ -210,7 +209,6 @@ public class DefaultAdxDataService implements AdxDataService {
                 adxWriter.writeAttribute(e.getKey(), e.getValue());
               }
             }
-            adxWriter.writeCharacters("\n\t");
             adxWriter.openElement(AdxDataService.DATAVALUE);
 
             adxWriter.writeAttribute(
@@ -237,7 +235,6 @@ public class DefaultAdxDataService implements AdxDataService {
           }
 
           if (currentPeriod != null) {
-            adxWriter.writeCharacters("\n");
             adxWriter.closeElement(); // GROUP
           }
         }
