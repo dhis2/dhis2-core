677b880df38de31d91e451341f0284cf141fff28
cjmamo <823038+cjmamo@users.noreply.github.com>
2025-02-06 19:39:24 +0100

    refactor: add DB constraint

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.42/V2_42_36__Add_responsetimeout_route.sql
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.42/V2_42_36__Add_responsetimeout_route.sql
@@ -1,1 +1,1 @@
-ALTER TABLE route ADD COLUMN IF NOT EXISTS responsetimeout INTEGER;
+ALTER TABLE route ADD COLUMN IF NOT EXISTS responsetimeout INTEGER CHECK (responsetimeout > 0 AND responsetimeout <= 60);
