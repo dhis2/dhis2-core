90e00d92fdedba65a7aece461722851178e6734f
teleivo <teleivo@users.noreply.github.com>
2025-02-07 15:08:57 +0100

    fix: don't set hibernate auto-ddl DHIS2-18961

--- dhis-2/dhis-services/dhis-service-tracker/src/test/java/org/hisp/dhis/tracker/config/TrackerTestConfig.java
+++ dhis-2/dhis-services/dhis-service-tracker/src/test/java/org/hisp/dhis/tracker/config/TrackerTestConfig.java
@@ -29,7 +29,7 @@ package org.hisp.dhis.tracker.config;
 
 import static org.mockito.Mockito.mock;
 
-import org.hisp.dhis.config.ConfigProviderConfiguration;
+import org.hisp.dhis.config.H2TestConfig;
 import org.hisp.dhis.fileresource.FileResourceService;
 import org.hisp.dhis.i18n.I18nManager;
 import org.hisp.dhis.organisationunit.OrganisationUnitService;
@@ -46,7 +46,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({TrackerValidationConfig.class, ConfigProviderConfiguration.class})
+@Import({TrackerValidationConfig.class, H2TestConfig.class})
 @ComponentScan("org.hisp.dhis.tracker.validation")
 @ExtendWith(MockitoExtension.class)
 public class TrackerTestConfig {
--- dhis-2/dhis-support/dhis-support-external/src/main/java/org/hisp/dhis/external/conf/ConfigurationKey.java
+++ dhis-2/dhis-support/dhis-support-external/src/main/java/org/hisp/dhis/external/conf/ConfigurationKey.java
@@ -99,9 +99,6 @@ public enum ConfigurationKey {
   /** Sets 'hibernate.cache.use_query_cache'. (default: true) */
   USE_QUERY_CACHE("hibernate.cache.use_query_cache", "true", false),
 
-  /** Sets 'hibernate.hbm2ddl.auto', used in tests only. (default: none) */
-  CONNECTION_SCHEMA("connection.schema", "none", false),
-
   /** Max size of connection pool (default: 80). */
   CONNECTION_POOL_MAX_SIZE("connection.pool.max_size", "80", false),
 
