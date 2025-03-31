9e2777bb21bd5ab91af29c7aa6348b073e35b4c3
Luciano Fiandesio <luciano@fiandesio.com>
2025-03-31 09:34:06 +0200

    fix startup error

--- dhis-2/dhis-support/dhis-support-system/src/main/java/org/hisp/dhis/monitoring/metrics/Ehcache3MetricsConfig.java
+++ dhis-2/dhis-support/dhis-support-system/src/main/java/org/hisp/dhis/monitoring/metrics/Ehcache3MetricsConfig.java
@@ -1,19 +1,23 @@
 package org.hisp.dhis.monitoring.metrics;
 
-import static org.hisp.dhis.external.conf.ConfigurationKey.MONITORING_EHCACHE_ENABLED;
-
 import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.binder.MeterBinder;
 import io.micrometer.core.instrument.Tag;
-import java.util.List;
-import org.ehcache.Cache;
-import org.ehcache.CacheManager;
-import org.ehcache.core.statistics.CacheStatistics;
-import org.ehcache.core.spi.service.StatisticsService;
+import io.micrometer.core.instrument.Tags;
+import io.micrometer.core.instrument.binder.MeterBinder;
+import io.micrometer.core.instrument.binder.cache.JCacheMetrics;
+import lombok.extern.slf4j.Slf4j;
+import org.ehcache.jsr107.EhcacheCachingProvider;
 import org.hisp.dhis.external.conf.ConfigurationKey;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
+
+import javax.cache.Cache;
+import javax.cache.CacheManager;
+import javax.cache.Caching;
+
+import static org.hisp.dhis.external.conf.ConfigurationKey.MONITORING_EHCACHE_ENABLED;
 
 /**
  * Consolidated configuration for wiring up Ehcache3 metrics.
@@ -21,39 +25,62 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 @Conditional(Ehcache3MetricsConfig.EhcacheMetricsEnabledCondition.class)
-public class Ehcache3MetricsConfig implements MeterBinder {
+@Slf4j
+public class Ehcache3MetricsConfig  {
 
-    private final CacheManager cacheManager;
-    private final StatisticsService statisticsService;
-
-    @Autowired
-    public Ehcache3MetricsConfig(CacheManager cacheManager, StatisticsService statisticsService) {
-        this.cacheManager = cacheManager;
-        this.statisticsService = statisticsService;
+    @Bean
+    public EhcacheMeterBinder ehcacheMeterBinder() {
+        log.info("Creating EhcacheMeterBinder bean");
+        EhcacheCachingProvider provider = (EhcacheCachingProvider)
+                Caching.getCachingProvider("org.ehcache.jsr107.EhcacheCachingProvider");
+        CacheManager cacheManager = provider.getCacheManager();
+        return new EhcacheMeterBinder(cacheManager);
     }
 
-    @Override
-    public void bindTo(MeterRegistry registry) {
-        // Iterate over the names of configured caches
-        for (String cacheName : cacheManager.getRuntimeConfiguration().getCacheConfigurations().keySet()) {
-            // Optionally check that the cache exists
-            Cache<?, ?> cache = cacheManager.getCache(cacheName, Object.class, Object.class);
-            if (cache != null) {
-                CacheStatistics stats = statisticsService.getCacheStatistics(cacheName);
-                List<Tag> tags = List.of(Tag.of("cache", cacheName));
-                // Bind gauges for various metrics
-                registry.gauge("cache.gets", tags, stats, CacheStatistics::getCacheGets);
-                registry.gauge("cache.puts", tags, stats, CacheStatistics::getCachePuts);
-                registry.gauge("cache.evictions", tags, stats, CacheStatistics::getCacheEvictions);
-                registry.gauge("cache.removals", tags, stats, CacheStatistics::getCacheRemovals);
-                registry.gauge("cache.hit.count", tags, stats, CacheStatistics::getCacheHits);
-                registry.gauge("cache.miss.count", tags, stats, CacheStatistics::getCacheMisses);
-                registry.gauge("cache.hit.ratio", tags, stats, CacheStatistics::getCacheHitPercentage);
-                registry.gauge("cache.miss.ratio", tags, stats, CacheStatistics::getCacheMissPercentage);
+    @Autowired
+    public void bindToRegistry(MeterRegistry registry, EhcacheMeterBinder meterBinder) {
+        log.info("Explicitly binding Ehcache metrics to registry");
+        meterBinder.bindTo(registry);
+    }
+
+    // Inner class that provides the metrics binding logic
+    public static class EhcacheMeterBinder implements MeterBinder {
+        private final CacheManager cacheManager;
+
+        public EhcacheMeterBinder(CacheManager cacheManager) {
+            this.cacheManager = cacheManager;
+        }
+
+        @Override
+        public void bindTo(MeterRegistry registry) {
+            log.info("Binding Ehcache metrics to registry");
+
+            if (this.cacheManager == null) {
+                log.warn("JSR-107 CacheManager bean not found. Cannot bind JCache metrics.");
+                return;
+            }
+
+            log.info("Attempting to bind metrics for JCache caches managed by: {}",
+                    this.cacheManager.getClass().getName());
+
+            int boundCount = 0;
+            for (String cacheName : this.cacheManager.getCacheNames()) {
+                Cache<?, ?> cache = cacheManager.getCache(cacheName, Object.class, Object.class);
+                if (cache != null) {
+                    Iterable<Tag> tags = Tags.of("cache", cacheName);
+                    new JCacheMetrics<>(cache, tags).bindTo(registry);
+                    boundCount++;
+                }
+            }
+
+            if (boundCount > 0) {
+                log.info("Successfully bound metrics for {} JCache caches.", boundCount);
+            } else {
+                log.warn("No JCache caches found or bound via CacheManager: {}",
+                        this.cacheManager.getClass().getName());
             }
         }
     }
-
 
     static class EhcacheMetricsEnabledCondition extends MetricsEnabler {
         @Override
