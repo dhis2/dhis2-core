78c9c17ab80b8a273f2dd37753e136b4813163ed
Luciano Fiandesio <luciano@fiandesio.com>
2025-04-01 12:37:30 +0200

    test: e2e tests fix for Doris (DHIS2-19375)

--- dhis-2/dhis-test-e2e/src/test/java/org/hisp/dhis/analytics/generator/TestGenerator.java
+++ dhis-2/dhis-test-e2e/src/test/java/org/hisp/dhis/analytics/generator/TestGenerator.java
@@ -29,13 +29,13 @@
  */
 package org.hisp.dhis.analytics.generator;
 
-import org.hisp.dhis.analytics.generator.impl.TrackedEntityQueryGenerator;
+import org.hisp.dhis.analytics.generator.impl.EnrollmentQueryDorisGenerator;
 
 /** This class simply hold the generator implementation to be used during the code generation. */
 public class TestGenerator {
   static Generator get() {
-    return new TrackedEntityQueryGenerator(
-        "multiOrgUnitProgramStatusFilterMultipleInBoolean", "singleOrgUnitInBooleanFilter");
+    return new EnrollmentQueryDorisGenerator(
+        "queryWithProgramAndProgramStageWhenTotalPagesIsFalse");
 
     // To generate all e2e tests just return the default constructor.
     // ie.: return new TeiQueryGenerator();
