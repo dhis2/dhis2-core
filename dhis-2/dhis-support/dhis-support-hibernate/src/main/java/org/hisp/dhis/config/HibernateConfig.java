89dc872686a7b343e7ff7f60fa5ebc827e08a742
Morten Svanaes <msvanaes@dhis2.org>
2025-04-02 19:12:33 +0800

    fix: migrate old ehcache 2 config to 3
    Signed-off-by: Morten Svanaes <msvanaes@dhis2.org>

--- dhis-2/dhis-support/dhis-support-hibernate/src/main/java/org/hisp/dhis/config/HibernateConfig.java
+++ dhis-2/dhis-support/dhis-support-hibernate/src/main/java/org/hisp/dhis/config/HibernateConfig.java
@@ -176,9 +176,6 @@ public class HibernateConfig {
 
     properties.put(AvailableSettings.HBM2DDL_AUTO, Action.VALIDATE.getExternalHbm2ddlName());
 
-    properties.put(AvailableSettings.JMX_ENABLED, "true");
-    properties.put(AvailableSettings.GENERATE_STATISTICS, "true");
-
     // TODO: this is anti-pattern and should be turn off
     properties.put("hibernate.allow_update_outside_transaction", "true");
 
