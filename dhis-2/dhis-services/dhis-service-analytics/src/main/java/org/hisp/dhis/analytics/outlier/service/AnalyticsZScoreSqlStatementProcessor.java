8686d9d0833e60927d34d49a04fff01d53e808d0
Maikel Arabori <maikelarabori@gmail.com>
2025-04-07 13:05:49 -0300

    chore: Test performance

--- dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/outlier/service/AnalyticsZScoreSqlStatementProcessor.java
+++ dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/outlier/service/AnalyticsZScoreSqlStatementProcessor.java
@@ -77,7 +77,7 @@ public class AnalyticsZScoreSqlStatementProcessor implements OutlierSqlStatement
    */
   @Override
   public String getSqlStatement(OutlierRequest request) {
-    return getSqlStatement(request, true);
+    return getSqlStatement(request, false);
   }
 
   /**
