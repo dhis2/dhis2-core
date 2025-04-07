6d6b3da836d8e7fa4369545afdf8aa4c033e7d28
Jason Pickering <jason@dhis2.org>
2025-04-07 15:06:48 +0200

    ALlow for maxResults in validation rule analysis

--- dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/DataAnalysisController.java
+++ dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/DataAnalysisController.java
@@ -70,6 +70,7 @@ import org.hisp.dhis.datavalue.DataValueService;
 import org.hisp.dhis.datavalue.DeflatedDataValue;
 import org.hisp.dhis.dxf2.webmessage.WebMessageException;
 import org.hisp.dhis.expression.Operator;
+import org.hisp.dhis.feedback.BadRequestException;
 import org.hisp.dhis.i18n.I18n;
 import org.hisp.dhis.i18n.I18nFormat;
 import org.hisp.dhis.i18n.I18nManager;
@@ -154,7 +155,7 @@ public class DataAnalysisController {
   @ResponseStatus(HttpStatus.OK)
   public @ResponseBody List<ValidationResultView> performValidationRulesAnalysis(
       @RequestBody ValidationRulesAnalysisParams validationRulesAnalysisParams, HttpSession session)
-      throws WebMessageException {
+      throws WebMessageException, BadRequestException {
     I18nFormat format = i18nManager.getI18nFormat();
 
     ValidationRuleGroup group = null;
@@ -168,6 +169,16 @@ public class DataAnalysisController {
       throw new WebMessageException(badRequest("No organisation unit defined"));
     }
 
+    int maxResults =
+        validationRulesAnalysisParams.getMaxResults() != null
+            ? validationRulesAnalysisParams.getMaxResults()
+            : ValidationService.MAX_INTERACTIVE_ALERTS;
+
+    final int MAX_ALLOWED_RESULTS = 50000;
+    if (maxResults <= 0 || maxResults > MAX_ALLOWED_RESULTS) {
+      throw new BadRequestException("maxResults must be between 1 and " + MAX_ALLOWED_RESULTS);
+    }
+
     ValidationAnalysisParams params =
         validationService
             .newParamsBuilder(
@@ -178,7 +189,7 @@ public class DataAnalysisController {
             .withIncludeOrgUnitDescendants(true)
             .withPersistResults(validationRulesAnalysisParams.isPersist())
             .withSendNotifications(validationRulesAnalysisParams.isNotification())
-            .withMaxResults(ValidationService.MAX_INTERACTIVE_ALERTS)
+            .withMaxResults(maxResults)
             .build();
 
     List<ValidationResult> validationResults =
