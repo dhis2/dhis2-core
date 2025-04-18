c5f80b7a06670ccc035d7a4acb939d06612fb725
Luciano Fiandesio <luciano@fiandesio.com>
2025-04-18 09:20:52 +0200

    fix e2e test + infra for on-the-fly resource creation

--- dhis-2/dhis-test-e2e/src/test/java/org/hisp/dhis/analytics/aggregate/AnalyticsQueryDv15AutoTest.java
+++ dhis-2/dhis-test-e2e/src/test/java/org/hisp/dhis/analytics/aggregate/AnalyticsQueryDv15AutoTest.java
@@ -37,6 +37,8 @@ import java.util.List;
 import java.util.Map;
 import org.hisp.dhis.AnalyticsApiTest;
 import org.hisp.dhis.actions.RestApiActions;
+import org.hisp.dhis.dependsOn.CreatedResource;
+import org.hisp.dhis.dependsOn.DependsOn;
 import org.hisp.dhis.dto.ApiResponse;
 import org.hisp.dhis.helpers.QueryParamsBuilder;
 import org.json.JSONException;
@@ -54,8 +56,12 @@ public class AnalyticsQueryDv15AutoTest extends AnalyticsApiTest {
   }
 
   @Test
-  public void subExpressionIndicator() throws JSONException {
+  @DependsOn(
+      files = {"ind-expected-pregnancies.json"},
+      delete = true)
+  public void subExpressionIndicator(List<CreatedResource> deps) throws JSONException {
     // Given
+    String uid = deps.get(0).uid();
     QueryParamsBuilder params =
         new QueryParamsBuilder()
             .add("filter=ou:Rp268JB6Ne4;cDw53Ej8rju;iPcreOldeV9;jjtzkzrmG7s")
@@ -63,7 +69,7 @@ public class AnalyticsQueryDv15AutoTest extends AnalyticsApiTest {
             .add("includeNumDen=true")
             .add("displayProperty=NAME")
             .add("skipMeta=false")
-            .add("dimension=dx:KjElpUc6jeX,pe:2021");
+            .add("dimension=dx:%s,pe:2021".formatted(uid));
 
     // When
     ApiResponse response = actions.get(params);
@@ -80,7 +86,8 @@ public class AnalyticsQueryDv15AutoTest extends AnalyticsApiTest {
 
     // Assert metaData.
     String expectedMetaData =
-        "{\"items\":{\"cDw53Ej8rju\":{\"name\":\"Afro Arab Clinic\"},\"KjElpUc6jeX\":{\"name\":\"Expected Pregnancies\"},\"iPcreOldeV9\":{\"name\":\"Benguema MI Room\"},\"dx\":{\"name\":\"Data\"},\"pe\":{\"name\":\"Period\"},\"ou\":{\"name\":\"Organisation unit\"},\"2021\":{\"name\":\"2021\"},\"Rp268JB6Ne4\":{\"name\":\"Adonkia CHP\"},\"jjtzkzrmG7s\":{\"name\":\"Banana Island MCHP\"}},\"dimensions\":{\"dx\":[\"KjElpUc6jeX\"],\"pe\":[\"2021\"],\"ou\":[\"Rp268JB6Ne4\",\"cDw53Ej8rju\",\"iPcreOldeV9\",\"jjtzkzrmG7s\"],\"co\":[]}}";
+        "{\"items\":{\"cDw53Ej8rju\":{\"name\":\"Afro Arab Clinic\"},\"%s\":{\"name\":\"Expected Pregnancies\"},\"iPcreOldeV9\":{\"name\":\"Benguema MI Room\"},\"dx\":{\"name\":\"Data\"},\"pe\":{\"name\":\"Period\"},\"ou\":{\"name\":\"Organisation unit\"},\"2021\":{\"name\":\"2021\"},\"Rp268JB6Ne4\":{\"name\":\"Adonkia CHP\"},\"jjtzkzrmG7s\":{\"name\":\"Banana Island MCHP\"}},\"dimensions\":{\"dx\":[\"%s\"],\"pe\":[\"2021\"],\"ou\":[\"Rp268JB6Ne4\",\"cDw53Ej8rju\",\"iPcreOldeV9\",\"jjtzkzrmG7s\"],\"co\":[]}}"
+            .formatted(uid, uid);
     String actualMetaData = new JSONObject((Map) response.extract("metaData")).toString();
     assertEquals(expectedMetaData, actualMetaData, false);
 
@@ -98,12 +105,16 @@ public class AnalyticsQueryDv15AutoTest extends AnalyticsApiTest {
     validateHeader(response, 7, "divisor", "Divisor", "NUMBER", "java.lang.Double", false, false);
 
     // Assert rows.
-    validateRow(response, List.of("KjElpUc6jeX", "2021", "2.0", "2.0", "1.0", "1.0", "1", "1"));
+    validateRow(response, List.of(uid, "2021", "2.0", "2.0", "1.0", "1.0", "1", "1"));
   }
 
   @Test
-  public void subExpressionIndicatorSingleValue() throws JSONException {
+  @DependsOn(
+      files = {"ind-expected-pregnancies.json"},
+      delete = true)
+  public void subExpressionIndicatorSingleValue(List<CreatedResource> deps) throws JSONException {
     // Given
+    String uid = deps.get(0).uid();
     QueryParamsBuilder params =
         new QueryParamsBuilder()
             .add("filter=ou:Rp268JB6Ne4;cDw53Ej8rju;jjtzkzrmG7s;iPcreOldeV9,pe:2021")
@@ -112,11 +123,10 @@ public class AnalyticsQueryDv15AutoTest extends AnalyticsApiTest {
             .add("includeNumDen=false")
             .add("displayProperty=NAME")
             .add("skipMeta=false")
-            .add("dimension=dx:KjElpUc6jeX");
+            .add("dimension=dx:%s".formatted(uid));
 
     // When
     ApiResponse response = actions.get(params);
-    ;
 
     // Then
     response
@@ -130,12 +140,9 @@ public class AnalyticsQueryDv15AutoTest extends AnalyticsApiTest {
 
     // Assert metaData.
     String expectedMetaData =
-        "{\"items\":{\"cDw53Ej8rju\":{\"uid\":\"cDw53Ej8rju\",\"code\":\"OU_278371\",\"name\":\"Afro Arab Clinic\",\"dimensionItemType\":\"ORGANISATION_UNIT\",\"valueType\":\"TEXT\",\"totalAggregationType\":\"SUM\"},\"KjElpUc6jeX\":{\"uid\":\"KjElpUc6jeX\",\"name\":\"Expected Pregnancies\",\"dimensionItemType\":\"INDICATOR\",\"valueType\":\"NUMBER\",\"totalAggregationType\":\"SUM\",\"indicatorType\":{\"name\":\"Number (Factor 1)\",\"displayName\":\"Number (Factor 1)\",\"factor\":1,\"number\":true}},\"iPcreOldeV9\":{\"uid\":\"iPcreOldeV9\",\"code\":\"OU_278379\",\"name\":\"Benguema MI Room\",\"dimensionItemType\":\"ORGANISATION_UNIT\",\"valueType\":\"TEXT\",\"totalAggregationType\":\"SUM\"},\"dx\":{\"uid\":\"dx\",\"name\":\"Data\",\"dimensionType\":\"DATA_X\"},\"pe\":{\"uid\":\"pe\",\"name\":\"Period\",\"dimensionType\":\"PERIOD\"},\"ou\":{\"uid\":\"ou\",\"name\":\"Organisation unit\",\"dimensionType\":\"ORGANISATION_UNIT\"},\"Rp268JB6Ne4\":{\"uid\":\"Rp268JB6Ne4\",\"code\":\"OU_651071\",\"name\":\"Adonkia CHP\",\"dimensionItemType\":\"ORGANISATION_UNIT\",\"valueType\":\"TEXT\",\"totalAggregationType\":\"SUM\"},\"2021\":{\"uid\":\"2021\",\"code\":\"2021\",\"name\":\"2021\",\"dimensionItemType\":\"PERIOD\",\"valueType\":\"TEXT\",\"totalAggregationType\":\"SUM\",\"startDate\":\"2021-01-01T00:00:00.000\",\"endDate\":\"2021-12-31T00:00:00.000\"},\"jjtzkzrmG7s\":{\"uid\":\"jjtzkzrmG7s\",\"code\":\"OU_278384\",\"name\":\"Banana Island MCHP\",\"dimensionItemType\":\"ORGANISATION_UNIT\",\"valueType\":\"TEXT\",\"totalAggregationType\":\"SUM\"}},\"dimensions\":{\"dx\":[\"KjElpUc6jeX\"],\"pe\":[\"2021\"],\"ou\":[\"Rp268JB6Ne4\",\"cDw53Ej8rju\",\"jjtzkzrmG7s\",\"iPcreOldeV9\"],\"co\":[]}}";
+        "{\"items\":{\"cDw53Ej8rju\":{\"uid\":\"cDw53Ej8rju\",\"code\":\"OU_278371\",\"name\":\"Afro Arab Clinic\",\"dimensionItemType\":\"ORGANISATION_UNIT\",\"valueType\":\"TEXT\",\"totalAggregationType\":\"SUM\"},\"%s\":{\"uid\":\"%s\",\"name\":\"Expected Pregnancies\",\"dimensionItemType\":\"INDICATOR\",\"valueType\":\"NUMBER\",\"totalAggregationType\":\"SUM\",\"indicatorType\":{\"name\":\"Number (Factor 1)\",\"displayName\":\"Number (Factor 1)\",\"factor\":1,\"number\":true}},\"iPcreOldeV9\":{\"uid\":\"iPcreOldeV9\",\"code\":\"OU_278379\",\"name\":\"Benguema MI Room\",\"dimensionItemType\":\"ORGANISATION_UNIT\",\"valueType\":\"TEXT\",\"totalAggregationType\":\"SUM\"},\"dx\":{\"uid\":\"dx\",\"name\":\"Data\",\"dimensionType\":\"DATA_X\"},\"pe\":{\"uid\":\"pe\",\"name\":\"Period\",\"dimensionType\":\"PERIOD\"},\"ou\":{\"uid\":\"ou\",\"name\":\"Organisation unit\",\"dimensionType\":\"ORGANISATION_UNIT\"},\"Rp268JB6Ne4\":{\"uid\":\"Rp268JB6Ne4\",\"code\":\"OU_651071\",\"name\":\"Adonkia CHP\",\"dimensionItemType\":\"ORGANISATION_UNIT\",\"valueType\":\"TEXT\",\"totalAggregationType\":\"SUM\"},\"2021\":{\"uid\":\"2021\",\"code\":\"2021\",\"name\":\"2021\",\"dimensionItemType\":\"PERIOD\",\"valueType\":\"TEXT\",\"totalAggregationType\":\"SUM\",\"startDate\":\"2021-01-01T00:00:00.000\",\"endDate\":\"2021-12-31T00:00:00.000\"},\"jjtzkzrmG7s\":{\"uid\":\"jjtzkzrmG7s\",\"code\":\"OU_278384\",\"name\":\"Banana Island MCHP\",\"dimensionItemType\":\"ORGANISATION_UNIT\",\"valueType\":\"TEXT\",\"totalAggregationType\":\"SUM\"}},\"dimensions\":{\"dx\":[\"%s\"],\"pe\":[\"2021\"],\"ou\":[\"Rp268JB6Ne4\",\"cDw53Ej8rju\",\"jjtzkzrmG7s\",\"iPcreOldeV9\"],\"co\":[]}}"
+            .formatted(uid, uid, uid);
     String actualMetaData = new JSONObject((Map) response.extract("metaData")).toString();
     assertEquals(expectedMetaData, actualMetaData, false);
-
-    // Assert headers.
-
-    // Assert rows.
   }
 }
