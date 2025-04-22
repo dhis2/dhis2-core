1685199e4351c5a29f8bf0aeb754e8ecf49fd668
Morten Svanaes <msvanaes@dhis2.org>
2025-04-23 01:47:56 +0800

    feat: return bundled apps with new app names
    Signed-off-by: Morten Svanaes <msvanaes@dhis2.org>

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/appmanager/webmodules/WebModule.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/appmanager/webmodules/WebModule.java
@@ -33,7 +33,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 import lombok.Data;
 import org.hisp.dhis.appmanager.App;
-import org.hisp.dhis.appmanager.AppManager;
 import org.hisp.dhis.appmanager.AppShortcut;
 
 /**
@@ -74,17 +73,25 @@ public class WebModule {
   }
 
   public static WebModule getModule(App app) {
+    String basePath = app.getBasePath();
+    String launchUrl = app.getLaunchUrl();
+    if (app.isBundled()) {
+      basePath = "/api/apps/" + app.getKey();
+      launchUrl = launchUrl.replaceAll("dhis-web-", "api/apps/");
+    }
+
     boolean hasIcon = app.getIcons() != null && app.getIcons().getIcon48() != null;
 
-    String defaultAction = app.getLaunchUrl();
+    String defaultAction = launchUrl;
 
-    String icon = hasIcon ? app.getBaseUrl() + "/" + app.getIcons().getIcon48() : null;
+    String icon =
+        hasIcon ? basePath.replace("/api/", "/") + "/" + app.getIcons().getIcon48() : null;
 
     String description = subString(app.getDescription(), 0, 80);
 
-    String key = app.isBundled() ? AppManager.BUNDLED_APP_PREFIX + app.getKey() : app.getKey();
+    String key = app.getKey();
 
-    WebModule module = new WebModule(key, app.getBasePath(), defaultAction);
+    WebModule module = new WebModule(key, basePath, defaultAction);
     module.setIcon(icon);
     module.setDescription(description);
     module.setDisplayName(app.getName());
