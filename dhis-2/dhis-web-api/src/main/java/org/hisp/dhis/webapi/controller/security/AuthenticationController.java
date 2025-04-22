370482a5ddf09d23d1634f58130585f637eb3be0
Morten Svanaes <msvanaes@dhis2.org>
2025-04-23 02:08:06 +0800

    feat: return bundled apps with new app names
    Signed-off-by: Morten Svanaes <msvanaes@dhis2.org>

--- dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/security/AuthenticationController.java
+++ dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/security/AuthenticationController.java
@@ -238,7 +238,7 @@ public class AuthenticationController {
       Authentication authentication, HttpServletRequest request, HttpServletResponse response) {
     // Default redirect URL
     String redirectUrl =
-        request.getContextPath() + "/" + settingsProvider.getCurrentSettings().getStartModule();
+        request.getContextPath() + "/api/apps/" + settingsProvider.getCurrentSettings().getStartModule();
     if (!redirectUrl.endsWith("/")) {
       redirectUrl += "/";
     }
