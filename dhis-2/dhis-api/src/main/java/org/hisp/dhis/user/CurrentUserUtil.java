b4c7edbf6fe2a948a989f4db8c425ba527a44c22
Morten Svanaes <msvanaes@dhis2.org>
2025-04-28 21:05:56 +0800

    chore: debug jenkins
    Signed-off-by: Morten Svanaes <msvanaes@dhis2.org>

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/user/CurrentUserUtil.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/user/CurrentUserUtil.java
@@ -65,9 +65,8 @@ public class CurrentUserUtil {
       return userDetails.getUsername();
 
     } else {
-      return null;
-//      throw new IllegalStateException(
-//          "Authentication principal is not supported; principal:" + principal);
+      throw new IllegalStateException(
+          "Authentication principal is not supported; principal:" + principal);
     }
   }
 
@@ -128,7 +127,8 @@ public class CurrentUserUtil {
     return authentication != null
         && authentication.isAuthenticated()
         && authentication.getPrincipal() != null
-        && !authentication.getPrincipal().equals("anonymousUser");
+        && authentication.getPrincipal()
+            instanceof org.springframework.security.core.userdetails.UserDetails;
   }
 
   public static void injectUserInSecurityContext(UserDetails actingUser) {
