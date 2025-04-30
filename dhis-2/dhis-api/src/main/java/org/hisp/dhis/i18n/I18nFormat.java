b37ab399a2f55b953b1fc23a39f454fe618c0a0f
Abyot Asalefew Gizaw <abyot@dhis2.org>
2025-04-30 11:16:16 +0200

    chore: Code clean-up

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/i18n/I18nFormat.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/i18n/I18nFormat.java
@@ -36,7 +36,6 @@ import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.temporal.WeekFields;
 import java.util.Date;
-import java.util.List;
 import java.util.ResourceBundle;
 import org.hisp.dhis.calendar.Calendar;
 import org.hisp.dhis.calendar.DateTimeUnit;
@@ -48,11 +47,6 @@ import org.hisp.dhis.period.PeriodType;
 import org.hisp.dhis.period.QuarterlyPeriodType;
 import org.hisp.dhis.period.SixMonthlyAbstractPeriodType;
 import org.hisp.dhis.period.WeeklyAbstractPeriodType;
-import org.hisp.dhis.period.WeeklyPeriodType;
-import org.hisp.dhis.period.WeeklySaturdayPeriodType;
-import org.hisp.dhis.period.WeeklySundayPeriodType;
-import org.hisp.dhis.period.WeeklyThursdayPeriodType;
-import org.hisp.dhis.period.WeeklyWednesdayPeriodType;
 
 /**
  * @author Pham Thi Thuy
@@ -78,15 +72,6 @@ public class I18nFormat {
   public static final String START_DATE_POSTFIX = ".startDate";
 
   public static final String END_DATE_POSTFIX = ".endDate";
-
-  /** List of weekly period types. Used to determine if a period type is a weekly period type. */
-  private static final List<Class<? extends WeeklyAbstractPeriodType>> WEEKLY_PERIOD_TYPES =
-      List.of(
-          WeeklyPeriodType.class,
-          WeeklySaturdayPeriodType.class,
-          WeeklyThursdayPeriodType.class,
-          WeeklyWednesdayPeriodType.class,
-          WeeklySundayPeriodType.class);
 
   private ResourceBundle resourceBundle;
 
