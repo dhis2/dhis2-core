610705668dee38a5a3921ce9f369c3d28aee0a4a
Enrico <enrico@dhis2.org>
2025-05-23 15:31:01 +0200

    chore: Use tracker event services for get events [DHIS2-19578]

---
+++ dhis-2/dhis-services/dhis-service-tracker/src/main/java/org/hisp/dhis/tracker/export/trackerevent/TrackerEventChangeLogOperationParams.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2004-2024, University of Oslo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors 
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.hisp.dhis.tracker.export.trackerevent;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import org.apache.commons.lang3.tuple.Pair;
+import org.hisp.dhis.common.QueryFilter;
+import org.hisp.dhis.common.SortDirection;
+import org.hisp.dhis.tracker.export.Order;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class TrackerEventChangeLogOperationParams {
+
+  private Order order;
+  private Pair<String, QueryFilter> filter;
+
+  public static class TrackerEventChangeLogOperationParamsBuilder {
+
+    // Do not remove these unused methods. They hide the order and filter fields from the builder
+    // which Lombok
+    // does not support.
+    // They should be added via their respective orderBy and filterBy builder methods.
+    private TrackerEventChangeLogOperationParamsBuilder order(Order order) {
+      return this;
+    }
+
+    private TrackerEventChangeLogOperationParamsBuilder filter(Pair<String, QueryFilter> filter) {
+      return this;
+    }
+
+    public TrackerEventChangeLogOperationParamsBuilder orderBy(
+        String field, SortDirection direction) {
+      this.order = new Order(field, direction);
+      return this;
+    }
+
+    public TrackerEventChangeLogOperationParamsBuilder filterBy(String field, QueryFilter filter) {
+      this.filter = Pair.of(field, filter);
+      return this;
+    }
+  }
+}
