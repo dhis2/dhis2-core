0c538b27a3370488d9a82eccc88d7b4fd2633480
Marc <marc@dhis2.org>
2025-06-03 16:21:07 +0200

    chore: Program parameter mandatory for /enrollments [DHIS2-19683]

--- dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/tracker/export/event/TrackerEventRequestParamsMapper.java
+++ dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/tracker/export/event/TrackerEventRequestParamsMapper.java
@@ -34,6 +34,7 @@ import static org.hisp.dhis.util.ObjectUtils.applyIfNotNull;
 import static org.hisp.dhis.webapi.controller.tracker.RequestParamsValidator.validateDeprecatedParameter;
 import static org.hisp.dhis.webapi.controller.tracker.RequestParamsValidator.validateOrderParams;
 import static org.hisp.dhis.webapi.controller.tracker.RequestParamsValidator.validateOrgUnitModeForEnrollmentsAndEvents;
+import static org.hisp.dhis.webapi.controller.tracker.RequestParamsValidator.validateProgram;
 import static org.hisp.dhis.webapi.controller.tracker.export.FilterParser.parseFilters;
 
 import java.util.List;
@@ -75,7 +76,7 @@ public class TrackerEventRequestParamsMapper {
   public TrackerEventOperationParams map(
       EventRequestParams eventRequestParams, TrackerIdSchemeParams idSchemeParams)
       throws BadRequestException {
-    validateProgram(eventRequestParams);
+    validateProgram(eventRequestParams.getProgram());
     OrganisationUnitSelectionMode orgUnitMode =
         validateOrgUnitModeForEnrollmentsAndEvents(
             eventRequestParams.getOrgUnit() != null
@@ -222,12 +223,6 @@ public class TrackerEventRequestParamsMapper {
         && DateUtils.getDuration(eventRequestParams.getUpdatedWithin()) == null) {
       throw new BadRequestException(
           "Duration is not valid: " + eventRequestParams.getUpdatedWithin());
-    }
-  }
-
-  private void validateProgram(EventRequestParams eventRequestParams) throws BadRequestException {
-    if (eventRequestParams.getProgram() == null) {
-      throw new BadRequestException("Program is mandatory");
     }
   }
 }
