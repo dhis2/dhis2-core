691b24dbca6f82d7af4b5d3d2dff14d3324191e5
Maikel Arabori <maikelarabori@gmail.com>
2025-06-03 06:49:08 -0300

    fix: wrong call in equals [DHIS2-19028]

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/period/Period.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/period/Period.java
@@ -345,7 +345,7 @@ public class Period extends BaseDimensionalItemObject {
 
     result = result * prime + (startDate != null ? startDate.hashCode() : 0);
     result = result * prime + (endDate != null ? endDate.hashCode() : 0);
-    result = result * prime + (code != null ? code.hashCode() : 0);
+    result = result * prime + (getCode() != null ? getCode().hashCode() : 0);
     result = result * prime + (periodType != null ? periodType.hashCode() : 0);
 
     return result;
@@ -360,7 +360,7 @@ public class Period extends BaseDimensionalItemObject {
     return startDate.equals(other.getStartDate())
         && endDate.equals(other.getEndDate())
         && Objects.equals(getCode(), other.getCode())
-        && Objects.equals(isoPeriod, other.getIsoDate())
+        && Objects.equals(getIsoDate(), other.getIsoDate())
         && Objects.equals(periodType, other.periodType)
         && Objects.equals(dateField, other.getDateField());
   }
