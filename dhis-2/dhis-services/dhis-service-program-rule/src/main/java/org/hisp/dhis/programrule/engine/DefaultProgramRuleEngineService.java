ec625df6d599fa75b8cd8b4d6961d3911cd3bffb
zubaira <zubair@dhis2.org>
2025-06-03 13:09:44 +0200

    fix: (2.40.8) )ensure program rule notifications correctly resolve data elements

--- dhis-2/dhis-services/dhis-service-program-rule/src/main/java/org/hisp/dhis/programrule/engine/DefaultProgramRuleEngineService.java
+++ dhis-2/dhis-services/dhis-service-program-rule/src/main/java/org/hisp/dhis/programrule/engine/DefaultProgramRuleEngineService.java
@@ -85,7 +85,7 @@ public class DefaultProgramRuleEngineService implements ProgramRuleEngineService
     }
 
     List<ProgramRule> programRules =
-        programRuleEngine.getProgramRules(programInstance.getProgram());
+        programRuleEngine.getProgramRulesForEnrollment(programInstance.getProgram());
 
     if (programRules.isEmpty()) {
       return List.of();
