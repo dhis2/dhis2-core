1cb5e11d9aaa197e44a8e8dd2feb6959a776ca3b
Maikel Arabori <maikelarabori@gmail.com>
2025-06-30 08:16:34 -0300

    fix: Unit tests + Sonar issues [DHIS2-19028]

--- dhis-2/dhis-api/src/test/java/org/hisp/dhis/common/CombinationGeneratorTest.java
+++ dhis-2/dhis-api/src/test/java/org/hisp/dhis/common/CombinationGeneratorTest.java
@@ -29,7 +29,6 @@
  */
 package org.hisp.dhis.common;
 
-import static java.util.stream.Collectors.toList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -206,7 +205,7 @@ class CombinationGeneratorTest {
     // Given 3 Lists with 50 elements each...
     List<List<Integer>> input = new ArrayList<>();
     for (int i = 0; i < 3; i++) {
-      input.add(IntStream.range(0, 50).boxed().collect(toList()));
+      input.add(IntStream.range(0, 50).boxed().toList());
     }
 
     List<List<Integer>> permutations = CombinationGenerator.newInstance(input).getCombinations();
@@ -219,7 +218,7 @@ class CombinationGeneratorTest {
     // Given 3 Lists with 50 elements each...
     List<List<Integer>> input = new ArrayList<>();
     for (int i = 0; i < 3; i++) {
-      input.add(IntStream.range(0, 50).boxed().collect(toList()));
+      input.add(IntStream.range(0, 50).boxed().toList());
     }
 
     int combinations = CombinationGenerator.newInstance(input).countCombinations();
