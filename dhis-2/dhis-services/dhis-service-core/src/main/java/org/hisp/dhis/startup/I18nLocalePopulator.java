5509ef35d6ea86e8f6cc35bf6bce3585221b6801
Jason Pickering <jason@dhis2.org>
2025-07-15 17:30:24 +0200

    Add Uzbek language

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/startup/I18nLocalePopulator.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/startup/I18nLocalePopulator.java
@@ -56,8 +56,36 @@ public class I18nLocalePopulator extends TransactionContextStartupRoutine {
 
   private static final Set<String> DEFAULT_LOCALES =
       Set.of(
-          "af", "ar", "bi", "am", "de", "dz", "en", "es", "fa", "fr", "gu", "hi", "id", "it", "km",
-          "lo", "my", "ne", "nl", "no", "ps", "pt", "ru", "rw", "sw", "tg", "vi", "zh");
+          "af",
+          "ar",
+          "bi",
+          "am",
+          "de",
+          "dz",
+          "en",
+          "es",
+          "fa",
+          "fr",
+          "gu",
+          "hi",
+          "id",
+          "it",
+          "km",
+          "lo",
+          "my",
+          "ne",
+          "nl",
+          "no",
+          "ps",
+          "pt",
+          "ru",
+          "rw",
+          "sw",
+          "tg",
+          "uz-UZ-Cyrl",
+          "uz_UZ-Latn",
+          "vi",
+          "zh");
 
   @Override
   public void executeInTransaction() {
@@ -68,7 +96,8 @@ public class I18nLocalePopulator extends TransactionContextStartupRoutine {
     }
 
     for (String locale : DEFAULT_LOCALES) {
-      localeService.saveI18nLocale(new I18nLocale(new Locale(locale)), new SystemUser());
+      Locale javaLocale = Locale.forLanguageTag(locale);
+      localeService.saveI18nLocale(new I18nLocale(javaLocale), new SystemUser());
     }
 
     log.info("Populated default locales");
