ec0bb5630a8b013a5dd07146ad6f438c9c43d75e
Jason Pickering <jason@dhis2.org>
2025-07-18 18:10:19 +0200

    Linting

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/startup/I18nLocalePopulator.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/startup/I18nLocalePopulator.java
@@ -56,34 +56,8 @@ public class I18nLocalePopulator extends TransactionContextStartupRoutine {
 
   private static final Set<String> DEFAULT_LOCALES =
       Set.of(
-          "af",
-          "ar",
-          "bi",
-          "am",
-          "de",
-          "dz",
-          "en",
-          "es",
-          "fa",
-          "fr",
-          "gu",
-          "hi",
-          "id",
-          "it",
-          "km",
-          "lo",
-          "my",
-          "ne",
-          "nl",
-          "no",
-          "ps",
-          "pt",
-          "ru",
-          "rw",
-          "sw",
-          "tg",
-          "vi",
-          "zh");
+          "af", "ar", "bi", "am", "de", "dz", "en", "es", "fa", "fr", "gu", "hi", "id", "it", "km",
+          "lo", "my", "ne", "nl", "no", "ps", "pt", "ru", "rw", "sw", "tg", "vi", "zh");
 
   @Override
   public void executeInTransaction() {
