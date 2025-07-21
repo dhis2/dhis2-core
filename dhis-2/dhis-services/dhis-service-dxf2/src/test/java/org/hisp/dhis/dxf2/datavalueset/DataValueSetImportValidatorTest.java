82de0cecd1fa0a763fb1ff63286ddf5cfe0df7c7
Morten Svanaes <msvanaes@dhis2.org>
2025-07-22 01:20:12 +0800

    chore: migrate from user to userdetails
    Signed-off-by: Morten Svanaes <msvanaes@dhis2.org>

--- dhis-2/dhis-services/dhis-service-dxf2/src/test/java/org/hisp/dhis/dxf2/datavalueset/DataValueSetImportValidatorTest.java
+++ dhis-2/dhis-services/dhis-service-dxf2/src/test/java/org/hisp/dhis/dxf2/datavalueset/DataValueSetImportValidatorTest.java
@@ -81,6 +81,7 @@ import org.hisp.dhis.period.PeriodType;
 import org.hisp.dhis.period.PeriodTypeEnum;
 import org.hisp.dhis.security.acl.AclService;
 import org.hisp.dhis.user.User;
+import org.hisp.dhis.user.UserDetails;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -850,9 +851,10 @@ class DataValueSetImportValidatorTest {
         .summary(new ImportSummary())
         .strategy(ImportStrategy.CREATE)
         .importOptions(new ImportOptions())
-        .currentUser(currentUser)
+        .currentUser(UserDetails.fromUser(currentUser))
         .i18n(i18n)
-        .currentOrgUnitUids(valueContext == null ? null : singleton(valueContext.getOrgUnit()))
+        .currentOrgUnitUids(
+            valueContext == null ? null : singleton(valueContext.getOrgUnit().getUid()))
         .singularNameForType(DataValueSetImportValidatorTest::getSingularNameForType);
   }
 
