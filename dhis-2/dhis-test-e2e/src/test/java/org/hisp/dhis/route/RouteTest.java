444f0bd156f3cd839856a83a6fd8d74666e3aabc
cjmamo <823038+cjmamo@users.noreply.github.com>
2025-07-21 17:09:22 +0200

    test: fix route destination

--- dhis-2/dhis-test-e2e/src/test/java/org/hisp/dhis/route/RouteTest.java
+++ dhis-2/dhis-test-e2e/src/test/java/org/hisp/dhis/route/RouteTest.java
@@ -38,6 +38,8 @@ import org.hisp.dhis.test.e2e.dto.ApiResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class RouteTest extends ApiTest {
 
   private RouteActions routeActions;
@@ -56,12 +58,21 @@ public class RouteTest extends ApiTest {
   void testRunRoute() {
     JsonObject routeJsonObject = new JsonObject();
     routeJsonObject.addProperty("name", "route-under-test");
-    routeJsonObject.addProperty("url", "https://dhis2.org/");
+    routeJsonObject.addProperty("url", "http://web:8080/api/system/info");
+
+    JsonObject authJsonObject = new JsonObject();
+    authJsonObject.addProperty("type", "http-basic");
+    authJsonObject.addProperty("username", "admin");
+    authJsonObject.addProperty("password", "district");
+
+    routeJsonObject.add("auth", authJsonObject);
 
     ApiResponse postApiResponse = routeActions.post(routeJsonObject);
+    postApiResponse.validate().statusCode(201);
     String id = postApiResponse.getBody().getAsJsonObject("response").get("uid").getAsString();
 
     ApiResponse runApiResponse = routeActions.get(id + "/run");
     runApiResponse.validate().statusCode(200);
+    assertNotNull(runApiResponse.getBody().get("version").getAsString());
   }
 }
