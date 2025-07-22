1fcf455d0358e452591d8993943903b8cd580fb6
marc <marc@dhis2.org>
2025-07-22 13:12:33 +0200

    feat: Validate min characters to search on events [DHIS2-19757] (#21465)
    * feat: Validate min characters to search on events [DHIS2-19757]
    
    * feat: Rename methods after PR review [DHIS2-19757]

--- dhis-2/dhis-services/dhis-service-tracker/src/main/java/org/hisp/dhis/tracker/export/OperationsParamsValidator.java
+++ dhis-2/dhis-services/dhis-service-tracker/src/main/java/org/hisp/dhis/tracker/export/OperationsParamsValidator.java
@@ -33,10 +33,13 @@ import static org.hisp.dhis.audit.AuditOperationType.READ;
 import static org.hisp.dhis.security.Authorities.F_TRACKED_ENTITY_INSTANCE_SEARCH_IN_ALL_ORGUNITS;
 
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.hisp.dhis.common.IdentifiableObjectManager;
 import org.hisp.dhis.common.OrganisationUnitSelectionMode;
+import org.hisp.dhis.common.QueryFilter;
 import org.hisp.dhis.common.UID;
 import org.hisp.dhis.feedback.BadRequestException;
 import org.hisp.dhis.feedback.ForbiddenException;
@@ -46,6 +49,7 @@ import org.hisp.dhis.program.Program;
 import org.hisp.dhis.program.ProgramService;
 import org.hisp.dhis.security.acl.AclService;
 import org.hisp.dhis.trackedentity.TrackedEntity;
+import org.hisp.dhis.trackedentity.TrackedEntityAttribute;
 import org.hisp.dhis.trackedentity.TrackedEntityType;
 import org.hisp.dhis.trackedentity.TrackedEntityTypeService;
 import org.hisp.dhis.tracker.audit.TrackedEntityAuditService;
@@ -253,5 +257,28 @@ public class OperationsParamsValidator {
     }
 
     return orgUnits;
+  }
+
+  /**
+   * Validates the specified filter contains at least the minimum number of characters to start the
+   * search
+   *
+   * @throws BadRequestException if the filter doesn't contain enough characters
+   */
+  public static void validateMinimumCharactersToSearch(
+      Map.Entry<UID, List<QueryFilter>> attributeFilter, TrackedEntityAttribute tea)
+      throws BadRequestException {
+    List<QueryFilter> binaryFilters =
+        attributeFilter.getValue().stream().filter(qf -> qf.getOperator().isBinary()).toList();
+    for (QueryFilter queryFilter : binaryFilters) {
+      if (tea.getMinCharactersToSearch() > 0
+          && (queryFilter.getFilter() == null
+              || queryFilter.getFilter().length() < tea.getMinCharactersToSearch())) {
+        throw new BadRequestException(
+            String.format(
+                "At least %d character(s) should be present in the filter to start a search, but the filter for the tracked entity attribute %s doesn't contain enough.",
+                tea.getMinCharactersToSearch(), tea.getUid()));
+      }
+    }
   }
 }
