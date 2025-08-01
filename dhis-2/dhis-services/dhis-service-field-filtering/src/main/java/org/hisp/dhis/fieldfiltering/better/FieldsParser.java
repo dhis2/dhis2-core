69f801229a6241f99e03ad55c3955fb7530e4650
teleivo <teleivo@users.noreply.github.com>
2025-08-01 11:43:19 +0200

    simplify

--- dhis-2/dhis-services/dhis-service-field-filtering/src/main/java/org/hisp/dhis/fieldfiltering/better/FieldsParser.java
+++ dhis-2/dhis-services/dhis-service-field-filtering/src/main/java/org/hisp/dhis/fieldfiltering/better/FieldsParser.java
@@ -54,24 +54,27 @@ public class FieldsParser {
   private static final Pattern TOKEN_PATTERN =
       Pattern.compile(
 """
-          (?<COLONCOLON>::)|(?<TILDE>~)|(?<PIPE>\\|)|(?<BRACKETOPEN>\\[)|(?<BRACKETCLOSE>\\])|(?<PARENOPEN>\\()|(?<PARENCLOSE>\\))|(?<COMMA>,)|(?<NAME>!?(?:[^,\\[\\]()~|;:]|:(?!:))+)
+          (?<COLONCOLON>::)|(?<TILDE>~)|(?<PIPE>\\|)|(?<BRACKETOPEN>\\[)|(?<BRACKETCLOSE>\\])|(?<PARENOPEN>\\()|(?<PARENCLOSE>\\))|(?<COMMA>,)|(?<SEMICOLON>;)|(?<BANG>!)|(?<NAME>(?:[^,\\[\\]()~|;:!]|:(?!:))+)
 """
               .trim());
 
-  private static final Map<String, TokenType> GROUP_TO_TOKEN =
-      Map.of(
-          "NAME", TokenType.NAME,
-          "COMMA", TokenType.COMMA,
-          "COLONCOLON", TokenType.COLON_COLON,
-          "TILDE", TokenType.TILDE,
-          "PIPE", TokenType.PIPE,
-          "BRACKETOPEN", TokenType.BRACKET_OPEN,
-          "BRACKETCLOSE", TokenType.BRACKET_CLOSE,
-          "PARENOPEN", TokenType.PAREN_OPEN,
-          "PARENCLOSE", TokenType.PAREN_CLOSE);
+  private static final Map<String, TokenType> GROUP_TO_TOKEN;
 
-  private static final Set<TokenType> TRANSFORMERS =
-      Set.of(TokenType.COLON_COLON, TokenType.TILDE, TokenType.PIPE);
+  static {
+    Map<String, TokenType> map = new HashMap<>();
+    map.put("NAME", TokenType.NAME);
+    map.put("COMMA", TokenType.COMMA);
+    map.put("COLONCOLON", TokenType.COLON_COLON);
+    map.put("TILDE", TokenType.TILDE);
+    map.put("PIPE", TokenType.PIPE);
+    map.put("BRACKETOPEN", TokenType.BRACKET_OPEN);
+    map.put("BRACKETCLOSE", TokenType.BRACKET_CLOSE);
+    map.put("PARENOPEN", TokenType.PAREN_OPEN);
+    map.put("PARENCLOSE", TokenType.PAREN_CLOSE);
+    map.put("SEMICOLON", TokenType.SEMICOLON);
+    map.put("BANG", TokenType.BANG);
+    GROUP_TO_TOKEN = Map.copyOf(map);
+  }
 
   enum TokenType {
     NAME,
@@ -82,7 +85,9 @@ public class FieldsParser {
     BRACKET_OPEN,
     BRACKET_CLOSE,
     PAREN_OPEN,
-    PAREN_CLOSE
+    PAREN_CLOSE,
+    SEMICOLON,
+    BANG
   }
 
   record Token(TokenType type, String value, int start, int end) {}
@@ -119,83 +124,169 @@ public class FieldsParser {
 
   private static FieldsAccumulator parseFields(String input, Set<String> unexcludableTokens) {
     List<Token> tokens = tokenize(input);
-
     unexcludableTokens.add(TOKEN_ALL);
 
+    Parser parser = new Parser();
     FieldsAccumulator root = new FieldsAccumulator();
     Stack<FieldsAccumulator> stack = new Stack<>();
     stack.push(root);
 
-    tokens = mergeTransformerTokens(tokens);
-
-    Token currentField = null;
-    boolean isExclusion = false;
-
     for (Token token : tokens) {
       switch (token.type) {
-        case NAME:
-          if (currentField != null) {
-            // Process previous field
-            String fieldName = parseFieldName(currentField.value);
-            List<Fields.Transformation> transformers = parseTransformers(currentField.value);
-            stack.peek().add(fieldName, isExclusion, unexcludableTokens, transformers);
-          }
-
-          // Set up new field
-          currentField = token;
-          isExclusion = token.value.startsWith("!");
+        case BANG:
+          parser.isExclusion = true;
           break;
 
-        case COMMA:
-          if (currentField != null) {
-            String fieldName = parseFieldName(currentField.value);
-            List<Fields.Transformation> transformers = parseTransformers(currentField.value);
-            stack.peek().add(fieldName, isExclusion, unexcludableTokens, transformers);
-            currentField = null;
-            isExclusion = false;
+        case NAME:
+          if (parser.parsingTransformerParameters) {
+            parser.transformerParameters.add(token.value.trim());
+          } else if (parser.pendingTransformerName != null) {
+            if (parser.pendingTransformerName.isEmpty()) {
+              parser.pendingTransformerName = token.value.trim();
+            } else {
+              parser.currentTransformers.add(
+                  new Fields.Transformation(parser.pendingTransformerName));
+              parser.pendingTransformerName = token.value.trim();
+            }
+          } else if (parser.currentFieldName == null) {
+            parser.currentFieldName = token.value;
+          } else {
+            parser.commitCurrentField(stack.peek(), unexcludableTokens);
+            parser.reset();
+            parser.currentFieldName = token.value;
+          }
+          break;
+
+        case COLON_COLON:
+        case TILDE:
+        case PIPE:
+          if (parser.pendingTransformerName != null && !parser.pendingTransformerName.isEmpty()) {
+            parser.currentTransformers.add(
+                new Fields.Transformation(parser.pendingTransformerName));
+          }
+          parser.pendingTransformerName = "";
+          break;
+
+        case PAREN_OPEN:
+          if (parser.pendingTransformerName != null) {
+            parser.parsingTransformerParameters = true;
+            parser.transformerParameters.clear();
+          } else {
+            if (parser.currentFieldName == null) {
+              throw new IllegalArgumentException(
+                  "Block must have a field name like orgUnits[code]");
+            }
+            parser.commitCurrentField(stack.peek(), unexcludableTokens);
+            stack.push(stack.peek().getOrCreateChild(parser.currentFieldName));
+            parser.reset();
+          }
+          break;
+
+        case PAREN_CLOSE:
+          if (parser.parsingTransformerParameters) {
+            parser.currentTransformers.add(
+                new Fields.Transformation(
+                    parser.pendingTransformerName,
+                    parser.transformerParameters.toArray(new String[0])));
+            parser.pendingTransformerName = null;
+            parser.transformerParameters.clear();
+            parser.parsingTransformerParameters = false;
+          } else {
+            if (stack.size() == 1) {
+              throw new IllegalArgumentException("Unbalanced parens/brackets in input");
+            }
+            if (parser.currentFieldName != null) {
+              parser.commitCurrentField(stack.peek(), unexcludableTokens);
+              parser.reset();
+            }
+            stack.pop();
           }
           break;
 
         case BRACKET_OPEN:
-        case PAREN_OPEN:
-          if (currentField == null) {
+          if (parser.currentFieldName == null) {
             throw new IllegalArgumentException("Block must have a field name like orgUnits[code]");
           }
-
-          String parent = parseFieldName(currentField.value);
-          List<Fields.Transformation> parentTransformers = parseTransformers(currentField.value);
-          stack.peek().add(parent, isExclusion, unexcludableTokens, parentTransformers);
-          stack.push(stack.peek().getOrCreateChild(parent));
-          currentField = null;
-          isExclusion = false;
+          parser.commitCurrentField(stack.peek(), unexcludableTokens);
+          stack.push(stack.peek().getOrCreateChild(parser.currentFieldName));
+          parser.reset();
           break;
 
         case BRACKET_CLOSE:
-        case PAREN_CLOSE:
           if (stack.size() == 1) {
             throw new IllegalArgumentException("Unbalanced parens/brackets in input");
           }
-
-          if (currentField != null) {
-            String fieldName = parseFieldName(currentField.value);
-            List<Fields.Transformation> transformers = parseTransformers(currentField.value);
-            stack.peek().add(fieldName, isExclusion, unexcludableTokens, transformers);
-            currentField = null;
-            isExclusion = false;
+          if (parser.currentFieldName != null) {
+            parser.commitCurrentField(stack.peek(), unexcludableTokens);
+            parser.reset();
           }
-
           stack.pop();
+          break;
+
+        case COMMA:
+          if (!parser.parsingTransformerParameters) {
+            if (parser.currentFieldName != null) {
+              parser.commitCurrentField(stack.peek(), unexcludableTokens);
+              parser.reset();
+            }
+          }
+          break;
+
+        case SEMICOLON:
           break;
       }
     }
 
-    if (currentField != null) {
-      String fieldName = parseFieldName(currentField.value);
-      List<Fields.Transformation> transformers = parseTransformers(currentField.value);
-      stack.peek().add(fieldName, isExclusion, unexcludableTokens, transformers);
+    if (parser.currentFieldName != null) {
+      parser.commitCurrentField(stack.peek(), unexcludableTokens);
     }
 
     return root;
+  }
+
+  private static class Parser {
+    String currentFieldName = null;
+    List<Fields.Transformation> currentTransformers = new ArrayList<>();
+    boolean isExclusion = false;
+
+    String pendingTransformerName = null;
+    List<String> transformerParameters = new ArrayList<>();
+    boolean parsingTransformerParameters = false;
+
+    void commitCurrentField(FieldsAccumulator accumulator, Set<String> unexcludableTokens) {
+      if (currentFieldName != null && !currentFieldName.trim().isEmpty()) {
+        if (pendingTransformerName != null && !pendingTransformerName.isEmpty()) {
+          currentTransformers.add(new Fields.Transformation(pendingTransformerName));
+        }
+        String cleanFieldName = removeAllWhitespace(currentFieldName);
+        accumulator.add(
+            cleanFieldName, isExclusion, unexcludableTokens, new ArrayList<>(currentTransformers));
+      }
+    }
+
+    /**
+     * This is the behavior of the {@code FieldFilterParser} so we kept it for backwards
+     * compatibility.
+     */
+    private String removeAllWhitespace(String fieldName) {
+      StringBuilder sb = new StringBuilder(fieldName.length());
+      for (int i = 0; i < fieldName.length(); i++) {
+        char c = fieldName.charAt(i);
+        if (!Character.isWhitespace(c)) {
+          sb.append(c);
+        }
+      }
+      return sb.toString();
+    }
+
+    void reset() {
+      currentFieldName = null;
+      currentTransformers.clear();
+      isExclusion = false;
+      pendingTransformerName = null;
+      transformerParameters.clear();
+      parsingTransformerParameters = false;
+    }
   }
 
   private static List<Token> tokenize(String input) {
@@ -219,239 +310,6 @@ public class FieldsParser {
     }
 
     return tokens;
-  }
-
-  private static List<Token> mergeTransformerTokens(List<Token> tokens) {
-    List<Token> merged = new ArrayList<>();
-
-    for (int i = 0; i < tokens.size(); i++) {
-      Token token = tokens.get(i);
-
-      // Check if this is a field name that starts a transformer sequence
-      if (token.type == TokenType.NAME && hasTransformerSequence(tokens, i)) {
-
-        // Merge field name + transformer sequence + parameters
-        StringBuilder fullField = new StringBuilder(token.value);
-        int j = i + 1;
-
-        // Merge transformer tokens (::, ~, |) and transformer names and parameters
-        while (j < tokens.size()) {
-          Token t = tokens.get(j);
-
-          // If we hit a comma (field separator), stop merging
-          if (t.type == TokenType.COMMA) {
-            break;
-          }
-
-          // If we hit bracket delimiters (structural), stop merging
-          if (t.type == TokenType.BRACKET_OPEN || t.type == TokenType.BRACKET_CLOSE) {
-            break;
-          }
-
-          // Special case: if it's PAREN_OPEN after a transformer name, merge parameters
-          if (t.type == TokenType.PAREN_OPEN && isAfterTransformer(tokens, j)) {
-            // Merge parentheses and their contents, including semicolons as commas
-            int parenCount = 0;
-            while (j < tokens.size()) {
-              Token pToken = tokens.get(j);
-              if (pToken.type == TokenType.PAREN_OPEN) {
-                fullField.append(pToken.value);
-                parenCount++;
-              } else if (pToken.type == TokenType.PAREN_CLOSE) {
-                fullField.append(pToken.value);
-                parenCount--;
-              } else if (pToken.type == TokenType.NAME) {
-                // Add the parameter name
-                fullField.append(pToken.value);
-                // Check if next token might be a separator for this parameter
-                if (j + 1 < tokens.size()
-                    && tokens.get(j + 1).type == TokenType.NAME
-                    && parenCount > 0) {
-                  fullField.append(";"); // Add semicolon separator between parameters
-                }
-              }
-
-              j++;
-              if (parenCount == 0) break;
-            }
-            break;
-          }
-
-          // Merge transformer tokens and names
-          if (TRANSFORMERS.contains(t.type) || t.type == TokenType.NAME) {
-            fullField.append(t.value);
-            j++;
-          } else {
-            break;
-          }
-        }
-
-        // Create merged token
-        merged.add(
-            new Token(TokenType.NAME, fullField.toString(), token.start, tokens.get(j - 1).end));
-        i = j - 1; // Skip the merged tokens
-
-      } else {
-        merged.add(token);
-      }
-    }
-
-    return merged;
-  }
-
-  private static boolean hasTransformerSequence(List<Token> tokens, int startIndex) {
-    // Look ahead to see if there's a transformer sequence after this name
-    for (int i = startIndex + 1; i < tokens.size(); i++) {
-      Token t = tokens.get(i);
-      if (TRANSFORMERS.contains(t.type)) {
-        return true;
-      }
-      if (t.type != TokenType.NAME) {
-        break;
-      }
-    }
-    return false;
-  }
-
-  private static boolean isAfterTransformer(List<Token> tokens, int parenIndex) {
-    // Check if the previous non-NAME token was a transformer
-    for (int i = parenIndex - 1; i >= 0; i--) {
-      Token t = tokens.get(i);
-      if (TRANSFORMERS.contains(t.type)) {
-        return true;
-      }
-      if (t.type != TokenType.NAME) {
-        break;
-      }
-    }
-    return false;
-  }
-
-  /**
-   * Parses field name and extracts transformers. Returns the base field name without transformers.
-   */
-  private static String parseFieldName(String field) {
-    // Remove leading ! if present
-    if (field.startsWith("!")) {
-      field = field.substring(1);
-    }
-
-    // Extract base field name (everything before first transformer separator)
-    int transformerStart = -1;
-    for (int i = 0; i < field.length() - 1; i++) {
-      if ((field.charAt(i) == ':' && field.charAt(i + 1) == ':')
-          || field.charAt(i) == '~'
-          || field.charAt(i) == '|') {
-        transformerStart = i;
-        break;
-      }
-    }
-
-    String baseField = transformerStart >= 0 ? field.substring(0, transformerStart) : field;
-
-    // Remove whitespace from base field name
-    boolean hasWhitespace = false;
-    for (int j = 0; j < baseField.length(); j++) {
-      if (Character.isWhitespace(baseField.charAt(j))) {
-        hasWhitespace = true;
-        break;
-      }
-    }
-
-    if (!hasWhitespace) {
-      return baseField;
-    }
-
-    StringBuilder sb = new StringBuilder(baseField.length());
-    for (int j = 0; j < baseField.length(); j++) {
-      char c = baseField.charAt(j);
-      if (!Character.isWhitespace(c)) {
-        sb.append(c);
-      }
-    }
-    return sb.toString();
-  }
-
-  /** Parses transformers from a field string. */
-  private static List<Fields.Transformation> parseTransformers(String field) {
-    List<Fields.Transformation> transformers = new ArrayList<>();
-
-    // Remove leading ! if present
-    if (field.startsWith("!")) {
-      field = field.substring(1);
-    }
-
-    // Find transformer part
-    int transformerStart = -1;
-    for (int i = 0; i < field.length() - 1; i++) {
-      if ((field.charAt(i) == ':' && field.charAt(i + 1) == ':')
-          || field.charAt(i) == '~'
-          || field.charAt(i) == '|') {
-        transformerStart = i;
-        break;
-      }
-    }
-
-    if (transformerStart < 0) {
-      return transformers; // No transformers
-    }
-
-    String transformerPart = field.substring(transformerStart);
-
-    // Parse individual transformers
-    int i = 0;
-    while (i < transformerPart.length()) {
-      // Skip separator
-      if (transformerPart.charAt(i) == ':'
-          && i + 1 < transformerPart.length()
-          && transformerPart.charAt(i + 1) == ':') {
-        i += 2;
-      } else if (transformerPart.charAt(i) == '~' || transformerPart.charAt(i) == '|') {
-        i++;
-      }
-
-      // Extract transformer name
-      int nameStart = i;
-      while (i < transformerPart.length()
-          && Character.isJavaIdentifierPart(transformerPart.charAt(i))) {
-        i++;
-      }
-
-      if (nameStart == i) break; // No valid transformer name
-
-      String transformerName = transformerPart.substring(nameStart, i);
-
-      // Extract parameters if present
-      List<String> params = new ArrayList<>();
-      if (i < transformerPart.length() && transformerPart.charAt(i) == '(') {
-        i++; // Skip opening paren
-        int paramStart = i;
-        int parenCount = 1;
-
-        while (i < transformerPart.length() && parenCount > 0) {
-          if (transformerPart.charAt(i) == '(') {
-            parenCount++;
-          } else if (transformerPart.charAt(i) == ')') {
-            parenCount--;
-          }
-          i++;
-        }
-
-        if (parenCount == 0) {
-          String paramString = transformerPart.substring(paramStart, i - 1);
-          if (!paramString.isEmpty()) {
-            // Split by semicolon
-            for (String param : paramString.split(";")) {
-              params.add(param.trim());
-            }
-          }
-        }
-      }
-
-      transformers.add(new Fields.Transformation(transformerName, params.toArray(new String[0])));
-    }
-
-    return transformers;
   }
 
   private static void mapPresets(
