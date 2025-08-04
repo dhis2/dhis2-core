961d10476b7252de26ff00a6ae39087ea44d1203
Morten Svanaes <msvanaes@dhis2.org>
2025-08-05 03:03:13 +0800

    feat: add new settings for Azure maps API key
    Signed-off-by: Morten Svanaes <msvanaes@dhis2.org>

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/setting/SettingKey.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/setting/SettingKey.java
@@ -200,6 +200,7 @@ public enum SettingKey {
       String.class,
       false,
       false),
+  AZURE_MAPS_API_KEY("keyAzureMapsApiKey", "", String.class, false, false),
   LAST_SUCCESSFUL_METADATA_SYNC("keyLastMetaDataSyncSuccess", Date.class),
   METADATAVERSION_ENABLED("keyVersionEnabled", Boolean.FALSE, Boolean.class),
   METADATA_FAILED_VERSION("keyMetadataFailedVersion", String.class),
