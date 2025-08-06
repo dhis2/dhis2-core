09ea89e60092a3a6f29405dac108fe0ce46e0e06
teleivo <teleivo@users.noreply.github.com>
2025-08-06 14:34:21 +0200

    chore: can we make transformers simpler functions

---
+++ dhis-2/dhis-services/dhis-service-field-filtering/src/main/java/org/hisp/dhis/fieldfiltering/better/FieldsTransformer.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright (c) 2004-2025, University of Oslo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors 
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.hisp.dhis.fieldfiltering.better;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import java.util.Map;
+import org.springframework.util.StringUtils;
+
+public class FieldsTransformer {
+  public static Map<String, Function> TRANSFORMERS =
+      Map.of("isEmpty", FieldsTransformer::isEmpty, "isNotEmpty", FieldsTransformer::isNotEmpty);
+
+  public interface Function {
+    void apply(String field, JsonNode value, JsonNode parent);
+  }
+
+  // TODO(ivo) can I get rid of the guard as all will be called with an object
+  // TODO(ivo) change signature order to parent, value
+  public static void isEmpty(String field, JsonNode value, JsonNode parent) {
+    if (!parent.isObject()) {
+      return;
+    }
+
+    if (value.isArray()) {
+      // overwrite array node with true/false depending on empty status
+      ((ObjectNode) parent).put(field, value.isEmpty());
+    }
+  }
+
+  public static void isNotEmpty(String field, JsonNode value, JsonNode parent) {
+    if (!parent.isObject()) {
+      return;
+    }
+
+    if (value.isArray()) {
+      // overwrite array node with true/false depending on empty status
+      ((ObjectNode) parent).put(field, !value.isEmpty());
+    }
+
+    return;
+  }
+
+  // TODO(ivo) fix this by passing args into interface
+  public static void keyBy(String field, JsonNode value, JsonNode parent) {
+    if (!parent.isObject()) {
+      return;
+    }
+
+    String keyByFieldName = "id";
+
+    //    if (!fieldPathTransformer.getParameters().isEmpty()) {
+    //      keyByFieldName = fieldPathTransformer.getParameters().get(0);
+    //    }
+    //
+    //    String fieldName = getFieldName(path);
+
+    if (value.isArray()) {
+      ObjectNode objectNode = ((ArrayNode) value).arrayNode().objectNode();
+
+      for (JsonNode node : value) {
+        if (node.isObject() && node.has(keyByFieldName)) {
+          String propertyName = node.get(keyByFieldName).asText();
+
+          if (!objectNode.has(propertyName) && StringUtils.hasText(propertyName)) {
+            objectNode.set(propertyName, node);
+          } else if (objectNode.has(propertyName)) {
+            JsonNode jsonNode = objectNode.get(propertyName);
+
+            if (jsonNode.isArray()) {
+              ((ArrayNode) jsonNode).add(node);
+            } else {
+              ArrayNode arrayNode = objectNode.arrayNode();
+              arrayNode.add(jsonNode);
+              arrayNode.add(node);
+
+              objectNode.set(propertyName, arrayNode);
+            }
+          }
+        }
+      }
+
+      ((ObjectNode) parent).replace(field, objectNode);
+    }
+  }
+}
