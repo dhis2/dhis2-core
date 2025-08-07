33f2767edb558b0cecaf2f17c96bd059657bcd4c
David Mackessy <dmackessy@dhis2.org>
2025-08-07 08:39:19 +0100

    feat: Prevent CategoryCombo categories update when data exists [DHIS2-19942]

--- dhis-2/dhis-test-web-api/src/test/java/org/hisp/dhis/webapi/controller/MetadataImportExportControllerIntegrationTest.java
+++ dhis-2/dhis-test-web-api/src/test/java/org/hisp/dhis/webapi/controller/MetadataImportExportControllerIntegrationTest.java
@@ -35,10 +35,12 @@ import static org.hisp.dhis.test.utils.Assertions.assertStartsWith;
 import static org.hisp.dhis.test.webapi.Assertions.assertWebMessage;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.hisp.dhis.category.CategoryCombo;
 import org.hisp.dhis.http.HttpStatus;
 import org.hisp.dhis.test.webapi.PostgresControllerIntegrationTestBase;
 import org.hisp.dhis.test.webapi.json.domain.JsonImportSummary;
 import org.hisp.dhis.test.webapi.json.domain.JsonWebMessage;
+import org.hisp.dhis.user.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.transaction.annotation.Transactional;
@@ -84,15 +86,15 @@ class MetadataImportExportControllerIntegrationTest extends PostgresControllerIn
   @Test
   @DisplayName(
       "Importing an existing CategoryCombo, which has no data values, with an additional Category succeeds")
-  void importingCategoryComboNewCategoryTest() {
-    // Given an existing CategoryCombo exists with 2 Categories
+  void importingCategoryComboNewCategoryNoDataTest() {
+    // Given existing metadata (including 1 CategoryCombo exists with 2 Categories)
     JsonImportSummary initialImport =
-        POST("/metadata", Body(getCatComboWithCategories()))
+        POST("/metadata", Body(metadataImport()))
             .content()
             .get("response")
             .as(JsonImportSummary.class);
     assertEquals("OK", initialImport.getStatus());
-    assertEquals(3, initialImport.getStats().getCreated());
+    assertEquals(13, initialImport.getStats().getCreated());
 
     // When importing the existing CategoryCombo with an additional Category
     // Then the import should succeed with the expected message & stats
@@ -106,41 +108,265 @@ class MetadataImportExportControllerIntegrationTest extends PostgresControllerIn
     assertEquals(1, updateImport.getStats().getCreated());
   }
 
-  private String getCatComboWithCategories() {
+  @Test
+  @DisplayName(
+      "Importing an existing CategoryCombo, which has data values, with an additional Category fails")
+  void importingCategoryComboNewCategoryWitDataTest() {
+    // Given existing metadata (including 1 CategoryCombo exists with 2 Categories)
+    JsonImportSummary initialImport =
+        POST("/metadata", Body(metadataImport()))
+            .content()
+            .get("response")
+            .as(JsonImportSummary.class);
+    assertEquals("OK", initialImport.getStatus());
+    assertEquals(13, initialImport.getStats().getCreated());
+
+    User currentUser = getCurrentUser();
+
+    // add org unit to user
+    PATCH("/users/" + currentUser.getUid(), Body(updateUserOrgUnit("OrgUnitUid1")))
+        .content(HttpStatus.OK);
+
+    // and data value exists with one of the COCs
+    POST("/dataValues", Body(getDataValue())).content(HttpStatus.CREATED);
+
+    // When importing the existing CategoryCombo with an additional Category
+    // Then the import should fail with the expected message & stats
+    JsonImportSummary updateImport =
+        POST("/metadata", Body(getCatComboWithAdditionalCategory()))
+            .content(HttpStatus.CONFLICT)
+            .get("response")
+            .as(JsonImportSummary.class);
+    assertEquals("ERROR", updateImport.getStatus());
+    assertEquals(0, updateImport.getStats().getUpdated());
+    assertEquals(4, updateImport.getStats().getIgnored());
+    assertEquals(
+        "Update cannot be applied as it would make existing data values inaccessible",
+        updateImport
+            .getTypeReport(CategoryCombo.class)
+            .getObjectReports()
+            .get(0)
+            .getErrorReports()
+            .get(0)
+            .getMessage());
+  }
+
+  @Test
+  @DisplayName(
+      "Importing an existing CategoryCombo, which has data values, with its original Categories succeeds")
+  void importingCategoryComboNoChangeWitDataTest() {
+    // Given existing metadata (including 1 CategoryCombo exists with 2 Categories)
+    JsonImportSummary initialImport =
+        POST("/metadata", Body(metadataImport()))
+            .content()
+            .get("response")
+            .as(JsonImportSummary.class);
+    assertEquals("OK", initialImport.getStatus());
+    assertEquals(13, initialImport.getStats().getCreated());
+
+    User currentUser = getCurrentUser();
+
+    // add org unit to user
+    PATCH("/users/" + currentUser.getUid(), Body(updateUserOrgUnit("OrgUnitUid1")))
+        .content(HttpStatus.OK);
+
+    // and data value exists with one of the COCs
+    POST("/dataValues", Body(getDataValue())).content(HttpStatus.CREATED);
+
+    // When importing the existing CategoryCombo with its original Categories
+    // Then the import should succeed with the expected message & stats
+    JsonImportSummary updateImport =
+        POST("/metadata", Body(metadataImport()))
+            .content(HttpStatus.OK)
+            .get("response")
+            .as(JsonImportSummary.class);
+    assertEquals("OK", updateImport.getStatus());
+    assertEquals(13, updateImport.getStats().getUpdated());
+    assertEquals(0, updateImport.getStats().getIgnored());
+  }
+
+  private String updateUserOrgUnit(String orgUnit) {
+    return """
+        [
+          {
+            "op": "add",
+            "path": "/organisationUnits",
+            "value": [
+              {
+                "id": "%s"
+              }
+            ]
+          }
+        ]
+        """
+        .formatted(orgUnit);
+  }
+
+  private String getDataValue() {
     return """
       {
-          "categories": [
+          "dataElement": "DeUid000001",
+          "period": "20231101",
+          "orgUnit": "OrgUnitUid1",
+          "categoryOptionCombo": "CocUid00001",
+          "attributeOptionCombo": "HllvX50cXC0",
+          "value": "2000",
+          "followup": false
+      }
+      """;
+  }
+
+  private String metadataImport() {
+    return """
+      {
+          "organisationUnits":[
               {
-                  "id": "CategoUid01",
-                  "name": "cat 1",
-                  "shortName": "cat 1",
-                  "dataDimensionType": "DISAGGREGATION",
-                  "categoryOptions": []
-              },
-              {
-                  "id": "CategoUid02",
-                  "name": "cat 2",
-                  "shortName": "cat 2",
-                  "dataDimensionType": "DISAGGREGATION",
-                  "categoryOptions": []
+                  "id": "OrgUnitUid1",
+                  "name": "test org 1",
+                  "shortName": "test org 1",
+                  "openingDate": "2023-06-15"
               }
           ],
-          "categoryCombos": [
+          "dataElements":[
               {
-                  "id": "CatComUid01",
-                  "name": "cat combo 1",
-                  "dataDimensionType": "DISAGGREGATION",
-                  "categories": [
-                      {
-                          "id": "CategoUid01"
-                      },
-                      {
-                          "id": "CategoUid02"
-                      }
-                  ]
+                  "id": "DeUid000001",
+                  "aggregationType": "DEFAULT",
+                  "domainType": "AGGREGATE",
+                  "name": "test de 1 - central v1",
+                  "shortName": "test de 1 - central v1",
+                  "valueType": "TEXT"
               }
-          ]
-      }
+          ],
+           "categoryOptions": [
+               {
+                   "id": "CatOptUid01",
+                   "name": "cat opt 1",
+                   "shortName": "cat opt 1"
+               },
+               {
+                   "id": "CatOptUid02",
+                   "name": "cat opt 2",
+                   "shortName": "cat opt 2"
+               },
+               {
+                   "id": "CatOptUid03",
+                   "name": "cat opt 3",
+                   "shortName": "cat opt 3"
+               },
+               {
+                   "id": "CatOptUid04",
+                   "name": "cat opt 4",
+                   "shortName": "cat opt 4"
+               }
+           ],
+           "categories": [
+               {
+                   "id": "CategoUid01",
+                   "name": "cat 1",
+                   "shortName": "cat 1",
+                   "dataDimensionType": "DISAGGREGATION",
+                   "categoryOptions": [
+                       {
+                           "id": "CatOptUid01"
+                       },
+                       {
+                           "id": "CatOptUid02"
+                       }
+                   ]
+               },
+               {
+                   "id": "CategoUid02",
+                   "name": "cat 2",
+                   "shortName": "cat 2",
+                   "dataDimensionType": "DISAGGREGATION",
+                   "categoryOptions": [
+                       {
+                           "id": "CatOptUid03"
+                       },
+                       {
+                           "id": "CatOptUid04"
+                       }
+                   ]
+               }
+           ],
+           "categoryCombos": [
+               {
+                   "id": "CatComUid01",
+                   "name": "cat combo 1",
+                   "dataDimensionType": "DISAGGREGATION",
+                   "categories": [
+                       {
+                           "id": "CategoUid01"
+                       },
+                       {
+                           "id": "CategoUid02"
+                       }
+                   ]
+               }
+           ],
+           "categoryOptionCombos": [
+               {
+                   "name": "cat opt 1, cat opt 3",
+                   "id": "CocUid00001",
+                   "categoryCombo": {
+                       "id": "CatComUid01"
+                   },
+                   "categoryOptions": [
+                       {
+                           "id": "CatOptUid01"
+                       },
+                       {
+                           "id": "CatOptUid03"
+                       }
+                   ]
+               },
+               {
+                   "name": "cat opt 1, cat opt 4",
+                   "id": "CocUid00002",
+                   "categoryCombo": {
+                       "id": "CatComUid01"
+                   },
+                   "categoryOptions": [
+                       {
+                           "id": "CatOptUid01"
+                       },
+                       {
+                           "id": "CatOptUid04"
+                       }
+                   ]
+               },
+               {
+                   "name": "cat opt 2, cat opt 3",
+                   "id": "CocUid00003",
+                   "categoryCombo": {
+                       "id": "CatComUid01"
+                   },
+                   "categoryOptions": [
+                       {
+                           "id": "CatOptUid02"
+                       },
+                       {
+                           "id": "CatOptUid03"
+                       }
+                   ]
+               },
+               {
+                   "name": "cat opt 2, cat opt 4",
+                   "id": "CocUid00004",
+                   "categoryCombo": {
+                       "id": "CatComUid01"
+                   },
+                   "categoryOptions": [
+                       {
+                           "id": "CatOptUid02"
+                       },
+                       {
+                           "id": "CatOptUid04"
+                       }
+                   ]
+               }
+           ]
+       }
       """;
   }
 
