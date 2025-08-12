34db3ef5256edf0789204c88ab05ac094a93cde6
Morten Svanaes <msvanaes@dhis2.org>
2025-08-12 16:28:22 +0800

    fix: hide full username from UserDto
    Signed-off-by: Morten Svanaes <msvanaes@dhis2.org>

--- dhis-2/dhis-api/src/test/java/org/hisp/dhis/schema/transformer/UserPropertyTransformerTest.java
+++ dhis-2/dhis-api/src/test/java/org/hisp/dhis/schema/transformer/UserPropertyTransformerTest.java
@@ -52,8 +52,6 @@ class UserPropertyTransformerTest {
     UserPropertyTransformer transformer = new UserPropertyTransformer();
     UserPropertyTransformer.UserDto userDto =
         (UserPropertyTransformer.UserDto) transformer.transform(user);
-    // assertEquals( uuid.toString(), userDto.getId() );
     assertEquals(user.getUid(), userDto.getId());
-    assertEquals("test", userDto.getUsername());
   }
 }
