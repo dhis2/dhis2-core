ceb44771e99b5582723fe8586f521883f45a922a
zubaira <zubair@dhis2.org>
2025-08-14 06:36:38 +0200

    minor

--- dhis-2/dhis-test-web-api/src/test/java/org/hisp/dhis/webapi/controller/dataintegrity/DataIntegrityTrackerEntitiesInvalidSRIDControllerTest.java
+++ dhis-2/dhis-test-web-api/src/test/java/org/hisp/dhis/webapi/controller/dataintegrity/DataIntegrityTrackerEntitiesInvalidSRIDControllerTest.java
@@ -4,16 +4,14 @@
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice, this
+ * Redistributions of source code must retain the above copyright notice, this
  * list of conditions and the following disclaimer.
  *
- * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * Redistributions in binary form must reproduce the above copyright notice,
  * this list of conditions and the following disclaimer in the documentation
  * and/or other materials provided with the distribution.
- *
- * 3. Neither the name of the copyright holder nor the names of its contributors 
- * may be used to endorse or promote products derived from this software without
+ * Neither the name of the HISP project nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
  * specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
@@ -80,13 +78,14 @@ class DataIntegrityTrackerEntitiesInvalidSRIDControllerTest
     Program program = createProgram('P', Set.of(), organisationUnit);
     manager.save(program);
 
-    TrackedEntity trackedEntityWithValidSRID =
-        createTrackedEntity(organisationUnit, trackedEntityType);
+    TrackedEntity trackedEntityWithValidSRID = createTrackedEntity(organisationUnit);
+    trackedEntityWithValidSRID.setTrackedEntityType(trackedEntityType);
     Point point = new GeometryFactory().createPoint(new Coordinate(60.0, 30.0));
     point.setSRID(4326);
     trackedEntityWithValidSRID.setGeometry(point);
 
-    trackedEntityWithInvalidSRID = createTrackedEntity(organisationUnit, trackedEntityType);
+    trackedEntityWithInvalidSRID = createTrackedEntity(organisationUnit);
+    trackedEntityWithInvalidSRID.setTrackedEntityType(trackedEntityType);
     point = new GeometryFactory().createPoint(new Coordinate(60.0, 30.0));
     point.setSRID(0); // invalid SRID
     trackedEntityWithInvalidSRID.setGeometry(point);
@@ -105,8 +104,8 @@ class DataIntegrityTrackerEntitiesInvalidSRIDControllerTest
     Program program = createProgram('R', Set.of(), organisationUnit);
     manager.save(program);
 
-    TrackedEntity trackedEntityWithValidSRID =
-        createTrackedEntity(organisationUnit, trackedEntityType);
+    TrackedEntity trackedEntityWithValidSRID = createTrackedEntity(organisationUnit);
+    trackedEntityWithValidSRID.setTrackedEntityType(trackedEntityType);
     Point point = new GeometryFactory().createPoint(new Coordinate(60.0, 30.0));
     point.setSRID(4326);
     trackedEntityWithValidSRID.setGeometry(point);
