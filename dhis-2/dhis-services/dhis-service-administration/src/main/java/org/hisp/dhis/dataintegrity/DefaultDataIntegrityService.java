d16b68617e4e35634685e88804153a274d75a7cc
marc <marc@dhis2.org>
2025-08-20 09:57:18 +0200

    feat: Add integrity check for created trigram indexes [DHIS2-19852] (#21681)
    * feat: Add integrity check for created trigram indexes [DHIS2-19852]
    
    * feat: Address PR comments [DHIS2-19852]
    
    * feat: Rename data integrity trigram tests [DHIS2-19852]

--- dhis-2/dhis-services/dhis-service-administration/src/main/java/org/hisp/dhis/dataintegrity/DefaultDataIntegrityService.java
+++ dhis-2/dhis-services/dhis-service-administration/src/main/java/org/hisp/dhis/dataintegrity/DefaultDataIntegrityService.java
@@ -513,6 +513,7 @@ public class DefaultDataIntegrityService implements DataIntegrityService {
     checks.add("tracker_geometry_invalid_srid");
     checks.add("tracked_entity_attributes_invalid_trigram_search_configuration");
     checks.add("tracked_entity_attributes_trigram_index_out_of_sync");
+    checks.add("tracked_entity_attributes_trigram_index_overview");
     return checks;
   }
 
