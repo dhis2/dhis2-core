9130c7ba966c275f26de4f659e933486a8a704b2
Luciano Fiandesio <luciano@fiandesio.com>
2025-08-26 12:53:36 +0200

    Fix SonarQube issues

--- dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/event/data/EventAggregateService.java
+++ dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/event/data/EventAggregateService.java
@@ -197,7 +197,6 @@ public class EventAggregateService {
     if ((!params.isSkipData() || params.analyzeOnly())
         && !periods.isEmpty()
         && !queryRequiresOwnership(params)) {
-      // DimensionalObject periodDim = params.getDimension("pe");
       params =
           new EventQueryParams.Builder(params)
               .withPeriods(periods.stream().flatMap(p -> p.getItems().stream()).toList(), EMPTY)
