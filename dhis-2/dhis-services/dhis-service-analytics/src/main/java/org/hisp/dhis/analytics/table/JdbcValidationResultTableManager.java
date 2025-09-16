68fe777ac3d86ec5ce612fba71ba7ec3cfd69fe3
Luciano Fiandesio <luciano@fiandesio.com>
2025-09-16 11:33:40 +0200

    fix: specify columns for AnalyticsTable constructor [2.41]

--- dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/table/JdbcValidationResultTableManager.java
+++ dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/table/JdbcValidationResultTableManager.java
@@ -137,7 +137,7 @@ public class JdbcValidationResultTableManager extends AbstractJdbcTableManager {
   public List<AnalyticsTable> getAnalyticsTables(AnalyticsTableUpdateParams params) {
     AnalyticsTable table =
         params.isLatestUpdate()
-            ? new AnalyticsTable(AnalyticsTableType.VALIDATION_RESULT, List.of(), Logged.LOGGED)
+            ? new AnalyticsTable(AnalyticsTableType.VALIDATION_RESULT, getColumns(), Logged.LOGGED)
             : getRegularAnalyticsTable(params, getDataYears(params), getColumns());
 
     return table.hasTablePartitions() ? List.of(table) : List.of();
