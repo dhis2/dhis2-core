7fda5d143fd4f002d7e8f0a6b5d2a8da4c992ca2
Lars Helge Øverland <lars@dhis2.org>
2025-09-22 16:51:21 +0200

    fix: Replicate data approval tables in analytics database [DHIS2-18417] (#21993)

--- dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/resourcetable/DefaultResourceTableService.java
+++ dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/resourcetable/DefaultResourceTableService.java
@@ -132,6 +132,14 @@ public class DefaultResourceTableService implements ResourceTableService {
     }
   }
 
+  @Override
+  @Transactional
+  public void replicateDataApprovalResourceTables() {
+    for (ResourceTable table : getApprovalResourceTables()) {
+      tableReplicationStore.replicateAnalyticsDatabaseTable(table.getMainTable());
+    }
+  }
+
   /**
    * Returns a list of resource tables.
    *
