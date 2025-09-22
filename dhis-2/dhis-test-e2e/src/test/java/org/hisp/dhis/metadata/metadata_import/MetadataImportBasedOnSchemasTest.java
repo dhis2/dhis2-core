cd887e606aded6ddc2553707d6af08462d7f0d50
Viet Nguyen <viet@dhis2.org>
2025-09-22 05:55:58 -0500

    code format

--- dhis-2/dhis-test-e2e/src/test/java/org/hisp/dhis/metadata/metadata_import/MetadataImportBasedOnSchemasTest.java
+++ dhis-2/dhis-test-e2e/src/test/java/org/hisp/dhis/metadata/metadata_import/MetadataImportBasedOnSchemasTest.java
@@ -46,7 +46,6 @@ import org.hisp.dhis.test.e2e.dto.schemas.SchemaProperty;
 import org.hisp.dhis.test.e2e.utils.DataGenerator;
 import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
