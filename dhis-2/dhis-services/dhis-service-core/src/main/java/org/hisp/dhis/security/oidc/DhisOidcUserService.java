cbd2afccec1aaebf388022b7247f16459a55794b
Morten Svanaes <msvanaes@dhis2.org>
2025-09-25 22:44:16 +0800

    feat: esignet support
    Signed-off-by: Morten Svanaes <msvanaes@dhis2.org>

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/security/oidc/DhisOidcUserService.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/security/oidc/DhisOidcUserService.java
@@ -29,35 +29,53 @@
  */
 package org.hisp.dhis.security.oidc;
 
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.jwk.source.JWKSourceBuilder;
+import com.nimbusds.jose.proc.JWSKeySelector;
+import com.nimbusds.jose.proc.JWSVerificationKeySelector;
+import com.nimbusds.jose.proc.SecurityContext;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.proc.ConfigurableJWTProcessor;
+import com.nimbusds.jwt.proc.DefaultJWTProcessor;
+import java.net.URL;
 import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.hisp.dhis.user.User;
 import org.hisp.dhis.user.UserDetails;
 import org.hisp.dhis.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
-import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.OAuth2Error;
 import org.springframework.security.oauth2.core.oidc.IdTokenClaimNames;
 import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author Morten Svanæs <msvanaes@dhis2.org>
  */
 @Slf4j
 @Service
-public class DhisOidcUserService extends OidcUserService {
+public class DhisOidcUserService implements OAuth2UserService<OidcUserRequest, OidcUser> {
   @Autowired public UserService userService;
 
   @Autowired private DhisOidcProviderRepository clientRegistrationRepository;
 
-  @Override
-  public OidcUser loadUser(OidcUserRequest userRequest) throws OAuth2AuthenticationException {
-    OidcUser oidcUser = super.loadUser(userRequest);
+  private final RestTemplate restTemplate = new RestTemplate();
+
+  public OidcUser loadUserX(OidcUserRequest userRequest) throws OAuth2AuthenticationException {
+    OidcUser oidcUser = null; // = super.loadUser(userRequest);
 
     ClientRegistration clientRegistration = userRequest.getClientRegistration();
 
@@ -108,5 +126,96 @@ public class DhisOidcUserService extends OidcUserService {
         new OAuth2Error("could_not_map_oidc_user_to_dhis2_user", errorMessage, null);
 
     throw new OAuth2AuthenticationException(oauth2Error, oauth2Error.toString());
+  }
+
+  @Override
+  public OidcUser loadUser(OidcUserRequest userRequest) throws OAuth2AuthenticationException {
+    ClientRegistration clientRegistration = userRequest.getClientRegistration();
+
+    DhisOidcClientRegistration dhisClientReg =
+        clientRegistrationRepository.getDhisOidcClientRegistration(
+            clientRegistration.getRegistrationId());
+
+    String mappingClaimKey = dhisClientReg.getMappingClaimKey();
+
+    // 1. Get the UserInfo endpoint URI from the client registration
+    String userInfoUri =
+        dhisClientReg.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUri();
+
+    if (userInfoUri == null || userInfoUri.isEmpty()) {
+      throw new OAuth2AuthenticationException(
+          new OAuth2Error("missing_user_info_uri"),
+          "Missing UserInfo Endpoint URI in ClientRegistration");
+    }
+
+    // 2. Make the request to the UserInfo endpoint
+    HttpHeaders headers = new HttpHeaders();
+    headers.setBearerAuth(userRequest.getAccessToken().getTokenValue());
+    headers.setAccept(
+        java.util.Collections.singletonList(
+            MediaType.valueOf("application/jwt"))); // Crucial: Ask for JWT
+    HttpEntity<String> entity = new HttpEntity<>("", headers);
+
+    ResponseEntity<String> response;
+    try {
+      response = restTemplate.exchange(userInfoUri, HttpMethod.GET, entity, String.class);
+    } catch (Exception e) {
+      throw new OAuth2AuthenticationException(
+          new OAuth2Error("invalid_user_info_response"), "Failed to fetch UserInfo response", e);
+    }
+
+    String signedJwt = response.getBody();
+
+    try {
+      // 3. Configure a JWT processor to verify the signature
+      ConfigurableJWTProcessor<SecurityContext> jwtProcessor = new DefaultJWTProcessor<>();
+
+      //      JWK jwk = dhisClientReg.getJwk();
+      //      if (jwk != null) {
+      //        // If we have a static JWK, use it directly
+      //        JWKSource<SecurityContext> keySource =
+      //            (jwkSelector, context) -> java.util.Collections.singletonList(jwk);
+      //        JWSKeySelector<SecurityContext> keySelector =
+      //            new JWSVerificationKeySelector<>(JWSAlgorithm.RS256, keySource);
+      //        jwtProcessor.setJWSKeySelector(keySelector);
+      //      }
+
+      // http://localhost:8088/v1/esignet/oauth/.well-known/jwks.json
+
+      //      // 4. Fetch the JWK set from the IDP's jwks_uri
+      String jwksUri = userRequest.getClientRegistration().getProviderDetails().getJwkSetUri();
+
+      //      JWKSource<SecurityContext> keySource =
+      //          new RemoteJWKSet<>(
+      //              new URL("http://localhost:8088/v1/esignet/oauth/.well-known/jwks.json"));
+
+      JWKSourceBuilder<SecurityContext> securityContextJWKSourceBuilder =
+          JWKSourceBuilder.create(new URL(jwksUri));
+      JWKSource<SecurityContext> keySource = securityContextJWKSourceBuilder.build();
+      //
+      //      // 5. Create a key selector to pick the correct key for verification
+      //      // The JWS algorithm is taken from the ID token as a hint
+      JWSKeySelector<SecurityContext> keySelector =
+          new JWSVerificationKeySelector<>(JWSAlgorithm.RS256, keySource);
+
+      jwtProcessor.setJWSKeySelector(keySelector);
+
+      // 6. Process the JWT, which verifies the signature and decodes the payload
+      JWTClaimsSet claimsSet = jwtProcessor.process(signedJwt, null);
+
+      Map<String, Object> claims = claimsSet.toJSONObject();
+
+      // You can map authorities from claims here if needed
+      // For example: authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+
+      return new DhisOidcUser(null, claims, IdTokenClaimNames.SUB, userRequest.getIdToken());
+      // The OidcUser requires the original ID Token and the new UserInfo claims
+      //      return new DefaultOidcUser(authorities, userRequest.getIdToken(), claims);
+
+    } catch (Exception e) {
+      log.error("Error processing UserInfo JWT", e);
+      throw new OAuth2AuthenticationException(
+          new OAuth2Error("jwt_processing_error"), "Failed to process UserInfo JWT", e);
+    }
   }
 }
