0140f94568a8df4f19c4cb1e05e272cb07b3062e
vietnguyen <viet@dhis2.org>
2025-09-25 18:43:52 -0500

    refactor: apply annotation mapping for IndicatorGroupSet

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/indicator/IndicatorGroupSet.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/indicator/IndicatorGroupSet.java
@@ -29,19 +29,64 @@
  */
 package org.hisp.dhis.indicator;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.OrderColumn;
+import org.hibernate.annotations.ListIndexBase;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import lombok.Setter;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.Type;
+import org.hisp.dhis.attribute.AttributeValues;
+import org.hisp.dhis.attribute.AttributeValuesDeserializer;
+import org.hisp.dhis.attribute.AttributeValuesSerializer;
+import org.hisp.dhis.audit.AuditAttribute;
 import org.hisp.dhis.common.BaseIdentifiableObject;
+import org.hisp.dhis.common.BaseMetadataObject;
 import org.hisp.dhis.common.DxfNamespaces;
+import org.hisp.dhis.common.IdScheme;
+import org.hisp.dhis.common.IdentifiableObject;
+import org.hisp.dhis.common.IdentifiableProperty;
 import org.hisp.dhis.common.MetadataObject;
+import org.hisp.dhis.common.OpenApi;
+import org.hisp.dhis.common.Sortable;
+import org.hisp.dhis.common.TranslationProperty;
+import org.hisp.dhis.common.annotation.Description;
+import org.hisp.dhis.schema.PropertyType;
+import org.hisp.dhis.schema.annotation.Gist;
+import org.hisp.dhis.schema.annotation.Gist.Include;
+import org.hisp.dhis.schema.annotation.Property;
 import org.hisp.dhis.schema.annotation.PropertyRange;
+import org.hisp.dhis.schema.annotation.PropertyTransformer;
+import org.hisp.dhis.schema.transformer.UserPropertyTransformer;
+import org.hisp.dhis.translation.Translatable;
+import org.hisp.dhis.translation.Translation;
+import org.hisp.dhis.user.User;
+import org.hisp.dhis.user.sharing.Sharing;
 
 /**
  * An IndicatorGroupSet is a set of IndicatorGroups. It is by default exclusive, in the sense that
@@ -50,13 +95,51 @@ import org.hisp.dhis.schema.annotation.PropertyRange;
  * @author Lars Helge Overland
  */
 @JacksonXmlRootElement(localName = "indicatorGroupSet", namespace = DxfNamespaces.DXF_2_0)
-public class IndicatorGroupSet extends BaseIdentifiableObject implements MetadataObject {
+@Setter
+@Entity
+@Table(name = "indicatorgroupset")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+public class IndicatorGroupSet extends BaseMetadataObject implements IdentifiableObject, MetadataObject {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+  @Column(name = "indicatorgroupsetid")
+  private long id;
+
+  @Column(unique = true, nullable = true, length = 50)
+  private String code;
+
+  @Column(nullable = false, unique = false, length = 230)
+  private String name;
+
+  @Column(name = "shortname", nullable = false, unique = true, length = 50)
   private String shortName;
 
+  @Column(columnDefinition = "text")
   private String description;
 
+  @Column
   private Boolean compulsory = false;
 
+  @Embedded
+  private TranslationProperty translations = new TranslationProperty();
+
+  @Type(type = "jsbAttributeValues")
+  @AuditAttribute
+  private AttributeValues attributeValues = AttributeValues.empty();
+
+  @Type(type = "jsbObjectSharing")
+  private Sharing sharing = new Sharing();
+
+  @OneToMany(fetch = FetchType.LAZY)
+  @JoinTable(
+      name = "indicatorgroupsetmembers",
+      joinColumns = @JoinColumn(name = "indicatorgroupsetid", foreignKey = @ForeignKey(name = "fk_indicatorgroupsetmembers_indicatorgroupsetid")),
+      inverseJoinColumns = @JoinColumn(name = "indicatorgroupid", foreignKey = @ForeignKey(name = "fk_indicatorgroupset_indicatorgroupid"))
+  )
+  @OrderColumn(name = "sort_order", nullable = false)
+  @ListIndexBase(1)
+  @Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
   private List<IndicatorGroup> members = new ArrayList<>();
 
   // -------------------------------------------------------------------------
@@ -148,6 +231,39 @@ public class IndicatorGroupSet extends BaseIdentifiableObject implements Metadat
   // Getters and setters
   // -------------------------------------------------------------------------
 
+  @Override
+  @JsonIgnore
+  public long getId() {
+    return id;
+  }
+
+  @Override
+  @JsonProperty
+  @JacksonXmlProperty(isAttribute = true)
+  @Description("The unique code for this Object.")
+  @Property(PropertyType.IDENTIFIER)
+  public String getCode() {
+    return code;
+  }
+
+  @Override
+  @JsonProperty
+  @JacksonXmlProperty(isAttribute = true)
+  @Description("The name of this Object. Required and unique.")
+  @PropertyRange(min = 1)
+  public String getName() {
+    return name;
+  }
+
+  @Override
+  @Sortable(whenPersisted = false)
+  @JsonProperty
+  @JacksonXmlProperty(namespace = DxfNamespaces.DXF_2_0)
+  @Translatable(propertyName = "name", key = "NAME")
+  public String getDisplayName() {
+    return translations.getTranslation("NAME", name);
+  }
+
   @JsonProperty
   @JacksonXmlProperty(isAttribute = true)
   @PropertyRange(min = 1, max = 50)
@@ -184,6 +300,82 @@ public class IndicatorGroupSet extends BaseIdentifiableObject implements Metadat
     this.compulsory = compulsory;
   }
 
+  @Override
+  @OpenApi.Property(BaseIdentifiableObject.AttributeValue[].class)
+  @JsonProperty("attributeValues")
+  @JsonDeserialize(using = AttributeValuesDeserializer.class)
+  @JsonSerialize(using = AttributeValuesSerializer.class)
+  public AttributeValues getAttributeValues() {
+    return attributeValues;
+  }
+
+  @Override
+  public void setAttributeValues(AttributeValues attributeValues) {
+    this.attributeValues = attributeValues == null ? AttributeValues.empty() : attributeValues;
+  }
+
+  @Override
+  public void addAttributeValue(String attributeId, String value) {
+    this.attributeValues = attributeValues.added(attributeId, value);
+  }
+
+  @Override
+  public void removeAttributeValue(String attributeId) {
+    this.attributeValues = attributeValues.removed(attributeId);
+  }
+
+  @JsonIgnore
+  public String getAttributeValue(String attributeUid) {
+    return attributeValues.get(attributeUid);
+  }
+
+  @Gist(included = Include.FALSE)
+  @Override
+  @Sortable(value = false)
+  @JsonProperty
+  @JacksonXmlElementWrapper(localName = "translations", namespace = DxfNamespaces.DXF_2_0)
+  @JacksonXmlProperty(localName = "translation", namespace = DxfNamespaces.DXF_2_0)
+  public Set<Translation> getTranslations() {
+    return translations.getTranslations();
+  }
+
+  /** Clears out cache when setting translations. */
+  @Override
+  public void setTranslations(Set<Translation> translations) {
+    this.translations.setTranslations(translations);
+  }
+
+  @Override
+  @Sortable(value = false)
+  @Gist(included = Include.FALSE)
+  @JsonProperty
+  @JacksonXmlProperty(namespace = DxfNamespaces.DXF_2_0)
+  public Sharing getSharing() {
+    return sharing;
+  }
+
+  @Override
+  @OpenApi.Ignore
+  @JsonProperty
+  @JsonSerialize(using = UserPropertyTransformer.JacksonSerialize.class)
+  @JsonDeserialize(using = UserPropertyTransformer.JacksonDeserialize.class)
+  @PropertyTransformer(UserPropertyTransformer.class)
+  @JacksonXmlProperty(namespace = DxfNamespaces.DXF_2_0)
+  public User getUser() {
+    return createdBy;
+  }
+
+  @Override
+  public void setUser(User user) {
+    // TODO remove this after implementing functions for using Owner
+    setCreatedBy(createdBy == null ? user : createdBy);
+    setOwner(user != null ? user.getUid() : null);
+  }
+
+  public void setOwner(String ownerId) {
+    getSharing().setOwner(ownerId);
+  }
+
   @JsonProperty("indicatorGroups")
   @JsonSerialize(contentAs = BaseIdentifiableObject.class)
   @JacksonXmlElementWrapper(localName = "indicatorGroups", namespace = DxfNamespaces.DXF_2_0)
@@ -194,5 +386,85 @@ public class IndicatorGroupSet extends BaseIdentifiableObject implements Metadat
 
   public void setMembers(List<IndicatorGroup> members) {
     this.members = members;
+  }
+
+  // -------------------------------------------------------------------------
+  // Implementation of IdentifiableObject methods from BaseIdentifiableObject
+  // -------------------------------------------------------------------------
+
+  /**
+   * Returns the value of the property referred to by the given {@link IdScheme}.
+   *
+   * @param idScheme the {@link IdScheme}.
+   * @return the value of the property referred to by the {@link IdScheme}.
+   */
+  @Override
+  public String getPropertyValue(IdScheme idScheme) {
+    if (idScheme.isNull() || idScheme.is(IdentifiableProperty.UID)) {
+      return getUid();
+    } else if (idScheme.is(IdentifiableProperty.CODE)) {
+      return code;
+    } else if (idScheme.is(IdentifiableProperty.NAME)) {
+      return name;
+    } else if (idScheme.is(IdentifiableProperty.ID)) {
+      return id > 0 ? String.valueOf(id) : null;
+    } else if (idScheme.is(IdentifiableProperty.ATTRIBUTE)) {
+      return attributeValues.get(idScheme.getAttribute());
+    }
+    return null;
+  }
+
+  /**
+   * Returns the value of the property referred to by the given {@link IdScheme}. If this happens to
+   * refer to NAME, it returns the translatable/display version.
+   *
+   * @param idScheme the {@link IdScheme}.
+   * @return the value of the property referred to by the {@link IdScheme}.
+   */
+  @Override
+  public String getDisplayPropertyValue(IdScheme idScheme) {
+    if (idScheme.is(IdentifiableProperty.NAME)) {
+      return getDisplayName();
+    } else {
+      return getPropertyValue(idScheme);
+    }
+  }
+
+  // -------------------------------------------------------------------------
+  // hashCode and equals
+  // -------------------------------------------------------------------------
+
+  @Override
+  public int hashCode() {
+    int result = getUid() != null ? getUid().hashCode() : 0;
+    result = 31 * result + (getCode() != null ? getCode().hashCode() : 0);
+    result = 31 * result + (getName() != null ? getName().hashCode() : 0);
+
+    return result;
+  }
+
+  /** Class check uses isAssignableFrom and get-methods to handle proxied objects. */
+  @Override
+  public boolean equals(Object obj) {
+    return this == obj
+        || obj instanceof IdentifiableObject other
+            && this.getClass() == obj.getClass()
+            && typedEquals(other);
+  }
+
+  /**
+   * Equality check against typed identifiable object. This method is not vulnerable to proxy
+   * issues, where an uninitialized object class type fails comparison to a real class.
+   *
+   * @param other the identifiable object to compare this object against.
+   * @return true if equal.
+   */
+  public final boolean typedEquals(IdentifiableObject other) {
+    if (other == null) {
+      return false;
+    }
+    return Objects.equals(getUid(), other.getUid())
+        && Objects.equals(getCode(), other.getCode())
+        && Objects.equals(getName(), other.getName());
   }
 }
--- dhis-2/dhis-test-web-api/src/test/java/org/hisp/dhis/webapi/controller/AbstractCrudControllerTest.java
+++ dhis-2/dhis-test-web-api/src/test/java/org/hisp/dhis/webapi/controller/AbstractCrudControllerTest.java
@@ -1818,4 +1818,249 @@ class AbstractCrudControllerTest extends H2ControllerIntegrationTestBase {
     assertEquals(1, indicators.size());
     assertEquals(indicatorId, indicators.getObject(0).getString("id").string());
   }
+
+  // -------------------------------------------------------------------------
+  // IndicatorGroupSet tests
+  // -------------------------------------------------------------------------
+
+  @Test
+  void testIndicatorGroupSetCanBeCreatedSuccessfully() {
+    // Create an IndicatorGroupSet with simple properties
+    String indicatorGroupSetId =
+        assertStatus(
+            HttpStatus.CREATED,
+            POST(
+                "/indicatorGroupSets/",
+                """
+            {
+                'name': 'Test Indicator Group Set',
+                'shortName': 'TIGS',
+                'description': 'A test indicator group set',
+                'code': 'TEST_IGS',
+                'compulsory': false
+            }
+            """));
+
+    // Verify the indicator group set was created correctly
+    JsonObject indicatorGroupSet = GET("/indicatorGroupSets/" + indicatorGroupSetId).content(HttpStatus.OK).as(JsonObject.class);
+    assertNotNull(indicatorGroupSet);
+    assertEquals("Test Indicator Group Set", indicatorGroupSet.getString("name").string());
+    assertEquals("TIGS", indicatorGroupSet.getString("shortName").string());
+    assertEquals("A test indicator group set", indicatorGroupSet.getString("description").string());
+    assertEquals("TEST_IGS", indicatorGroupSet.getString("code").string());
+    assertFalse(indicatorGroupSet.getBoolean("compulsory").booleanValue());
+
+    // Verify that collections are present and empty upon creation
+    assertEquals(0, indicatorGroupSet.getArray("indicatorGroups").size());
+  }
+
+  @Test
+  void testIndicatorGroupSetCanBeCreatedWithMembers() {
+    // Create IndicatorType first (required for indicators)
+    String indicatorTypeId =
+        assertStatus(
+            HttpStatus.CREATED,
+            POST(
+                "/indicatorTypes/",
+                """
+            {
+                'name': 'Test Indicator Type',
+                'factor': 100
+            }
+            """));
+
+    // Create Indicators
+    String indicator1Id =
+        assertStatus(
+            HttpStatus.CREATED,
+            POST(
+                "/indicators/",
+                """
+            {
+                'name': 'Indicator 1',
+                'shortName': 'IND1',
+                'indicatorType': {
+                    'id': '%s'
+                },
+                'numerator': '1',
+                'denominator': '1'
+            }
+            """
+                    .formatted(indicatorTypeId)));
+
+    String indicator2Id =
+        assertStatus(
+            HttpStatus.CREATED,
+            POST(
+                "/indicators/",
+                """
+            {
+                'name': 'Indicator 2',
+                'shortName': 'IND2',
+                'indicatorType': {
+                    'id': '%s'
+                },
+                'numerator': '1',
+                'denominator': '1'
+            }
+            """
+                    .formatted(indicatorTypeId)));
+
+    // Create IndicatorGroups
+    String indicatorGroup1Id =
+        assertStatus(
+            HttpStatus.CREATED,
+            POST(
+                "/indicatorGroups/",
+                """
+            {
+                'name': 'Indicator Group 1',
+                'shortName': 'IG1',
+                'indicators': [
+                    {'id': '%s'}
+                ]
+            }
+            """
+                    .formatted(indicator1Id)));
+
+    String indicatorGroup2Id =
+        assertStatus(
+            HttpStatus.CREATED,
+            POST(
+                "/indicatorGroups/",
+                """
+            {
+                'name': 'Indicator Group 2',
+                'shortName': 'IG2',
+                'indicators': [
+                    {'id': '%s'}
+                ]
+            }
+            """
+                    .formatted(indicator2Id)));
+
+    // Create IndicatorGroupSet with members
+    String indicatorGroupSetId =
+        assertStatus(
+            HttpStatus.CREATED,
+            POST(
+                "/indicatorGroupSets/",
+                """
+            {
+                'name': 'Test IGS with Members',
+                'shortName': 'TIGSM',
+                'compulsory': true,
+                'indicatorGroups': [
+                    {'id': '%s'},
+                    {'id': '%s'}
+                ]
+            }
+            """
+                    .formatted(indicatorGroup1Id, indicatorGroup2Id)));
+
+    // Verify indicator group set has the members
+    JsonObject indicatorGroupSet = GET("/indicatorGroupSets/" + indicatorGroupSetId).content(HttpStatus.OK).as(JsonObject.class);
+    assertTrue(indicatorGroupSet.getBoolean("compulsory").booleanValue());
+
+    JsonArray indicatorGroups = indicatorGroupSet.getArray("indicatorGroups");
+    assertNotNull(indicatorGroups);
+    assertEquals(2, indicatorGroups.size());
+  }
+
+  @Test
+  void testIndicatorGroupSetSupportsAttributeValues() {
+    // Create attribute first
+    String attributeId =
+        assertStatus(
+            HttpStatus.CREATED,
+            POST(
+                "/attributes/",
+                """
+            {
+                'name': 'Test IGS Attribute',
+                'shortName': 'TIGSA',
+                'valueType': 'TEXT',
+                'indicatorGroupSetAttribute': true
+            }
+            """));
+
+    // Create IndicatorGroupSet with attribute value
+    String indicatorGroupSetId =
+        assertStatus(
+            HttpStatus.CREATED,
+            POST(
+                "/indicatorGroupSets/",
+                """
+            {
+                'name': 'IGS with Attributes',
+                'shortName': 'IGSWA',
+                'code': 'IGS001',
+                'attributeValues': [
+                    {
+                        'attribute': {'id': '%s'},
+                        'value': 'test igs attribute value'
+                    }
+                ]
+            }
+            """
+                    .formatted(attributeId)));
+
+    // Verify attribute values and code
+    JsonObject indicatorGroupSet = GET("/indicatorGroupSets/" + indicatorGroupSetId).content(HttpStatus.OK).as(JsonObject.class);
+    assertEquals("IGS001", indicatorGroupSet.getString("code").string());
+
+    JsonArray attributeValues = indicatorGroupSet.getArray("attributeValues");
+    assertNotNull(attributeValues);
+    assertEquals(1, attributeValues.size());
+    assertEquals("test igs attribute value", attributeValues.getObject(0).getString("value").string());
+  }
+
+  @Test
+  void testIndicatorGroupSetSupportsTranslations() {
+    // Create IndicatorGroupSet
+    String indicatorGroupSetId =
+        assertStatus(
+            HttpStatus.CREATED,
+            POST(
+                "/indicatorGroupSets/",
+                """
+            {
+                'name': 'IGS with Translations',
+                'shortName': 'IGSWT'
+            }
+            """));
+
+    // Add translations
+    assertStatus(
+        HttpStatus.NO_CONTENT,
+        PUT(
+            "/indicatorGroupSets/" + indicatorGroupSetId + "/translations",
+            """
+            {
+                'translations': [
+                    {
+                        'locale': 'sv',
+                        'property': 'name',
+                        'value': 'IGS med översättningar'
+                    },
+                    {
+                        'locale': 'fr',
+                        'property': 'name',
+                        'value': 'IGS avec traductions'
+                    }
+                ]
+            }
+            """));
+
+    // Verify translations were added
+    JsonArray translations = GET("/indicatorGroupSets/" + indicatorGroupSetId + "/translations").content().getArray("translations");
+    assertEquals(2, translations.size());
+
+    // Check that translations include the expected locales
+    Set<String> locales = Set.of(
+        translations.get(0, JsonTranslation.class).getLocale(),
+        translations.get(1, JsonTranslation.class).getLocale());
+    assertTrue(locales.contains("sv"));
+    assertTrue(locales.contains("fr"));
+  }
 }
