c8a8d504652a3240021dd227caecce67f1e3fa78
zubaira <zubair@dhis2.org>
2025-10-07 14:05:40 +0200

    use stageService to fetch missing stages

--- dhis-2/dhis-services/dhis-service-program-rule/src/main/java/org/hisp/dhis/programrule/action/validation/BaseProgramRuleActionValidator.java
+++ dhis-2/dhis-services/dhis-service-program-rule/src/main/java/org/hisp/dhis/programrule/action/validation/BaseProgramRuleActionValidator.java
@@ -198,10 +198,10 @@ public class BaseProgramRuleActionValidator implements ProgramRuleActionValidato
       return availableStages;
     }
 
-    List<ProgramStage> missingStages =
-        fetchMissingStages(validationContext, program, availableStages);
-
-    return combineStages(availableStages, missingStages);
+    return validationContext
+        .getProgramRuleActionValidationService()
+        .getProgramStageService()
+        .getProgramStagesByProgram(program);
   }
 
   private boolean hasAllProgramStages(
