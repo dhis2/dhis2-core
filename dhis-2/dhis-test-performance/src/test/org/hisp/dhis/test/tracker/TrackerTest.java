1ec714d39db01914d2de69ce4def192ee78a80bb
Enrico <enrico@dhis2.org>
2025-10-07 16:17:42 +0200

    fix: Use only selected program in single event query

--- dhis-2/dhis-test-performance/src/test/org/hisp/dhis/test/tracker/TrackerTest.java
+++ dhis-2/dhis-test-performance/src/test/org/hisp/dhis/test/tracker/TrackerTest.java
@@ -12,7 +12,7 @@
  * this list of conditions and the following disclaimer in the documentation
  * and/or other materials provided with the distribution.
  *
- * 3. Neither the name of the copyright holder nor the names of its contributors
+ * 3. Neither the name of the copyright holder nor the names of its contributors 
  * may be used to endorse or promote products derived from this software without
  * specific prior written permission.
  *
@@ -117,10 +117,7 @@ public class TrackerTest extends Simulation {
                 .responseTime()
                 .percentile(90)
                 .lte(100),
-            details("Access single event", "Get first event")
-                .responseTime()
-                .percentile(90)
-                .lte(25),
+            details("Access single event", "Get first event").responseTime().percentile(90).lte(25),
             details("Access single event", "Get relationships for first event")
                 .responseTime()
                 .percentile(90)
