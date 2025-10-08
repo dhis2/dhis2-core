53735ad0982705772c7e153157cd667f4720d7c9
Zubair Asghar <zubair@dhis2.org>
2025-10-08 17:35:45 +0500

    fix: correct PR/PRA validation for certain invalid combinations of program rule actions[DHIS2-20181] (#22166) (#22191)

--- dhis-2/dhis-services/dhis-service-program-rule/src/main/java/org/hisp/dhis/programrule/action/validation/ProgramRuleActionValidationContext.java
+++ dhis-2/dhis-services/dhis-service-program-rule/src/main/java/org/hisp/dhis/programrule/action/validation/ProgramRuleActionValidationContext.java
@@ -28,6 +28,7 @@
 package org.hisp.dhis.programrule.action.validation;
 
 import java.util.List;
+import javax.annotation.Nonnull;
 import lombok.Builder;
 import lombok.Data;
 import org.hisp.dhis.dataelement.DataElement;
@@ -66,5 +67,5 @@ public class ProgramRuleActionValidationContext {
 
   private ProgramRuleActionValidationService programRuleActionValidationService;
 
-  private List<ProgramStage> programStages;
+  @Nonnull private List<ProgramStage> programStages;
 }
