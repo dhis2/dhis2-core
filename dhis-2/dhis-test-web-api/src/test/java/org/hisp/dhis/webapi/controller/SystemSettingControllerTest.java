3d249e60c4665a564bccbd579ff013fb637d46d6
David Mackessy <dmackessy@dhis2.org>
2025-10-20 08:41:30 +0100

    test state cleanup

--- dhis-2/dhis-test-web-api/src/test/java/org/hisp/dhis/webapi/controller/SystemSettingControllerTest.java
+++ dhis-2/dhis-test-web-api/src/test/java/org/hisp/dhis/webapi/controller/SystemSettingControllerTest.java
@@ -40,6 +40,7 @@ import org.hisp.dhis.setting.SettingKey;
 import org.hisp.dhis.web.HttpStatus;
 import org.hisp.dhis.webapi.DhisControllerConvenienceTest;
 import org.hisp.dhis.webapi.utils.ContextUtils;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -48,6 +49,12 @@ import org.junit.jupiter.api.Test;
  * @author Jan Bernitt
  */
 class SystemSettingControllerTest extends DhisControllerConvenienceTest {
+
+  @AfterEach
+  void resetLocale() {
+    POST("/systemSettings/keyUiLocale?value=en");
+  }
+
   @Test
   void testSetSystemSettingOrTranslation_NoSuchObject() {
     assertWebMessage(
