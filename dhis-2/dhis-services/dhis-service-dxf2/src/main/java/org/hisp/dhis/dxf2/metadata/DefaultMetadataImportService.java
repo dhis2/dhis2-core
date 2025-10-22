f423e2d4f8d765021132136c225f12070f4028b2
Morten Svanaes <msvanaes@dhis2.org>
2025-10-22 21:32:57 +0800

    fix: DHIS2-20220 DHIS2-20243 DHIS2-20195
    Signed-off-by: Morten Svanaes <msvanaes@dhis2.org>

--- dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/metadata/DefaultMetadataImportService.java
+++ dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/metadata/DefaultMetadataImportService.java
@@ -43,7 +43,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.hibernate.Session;
 import org.hisp.dhis.common.BaseIdentifiableObject;
 import org.hisp.dhis.common.IdentifiableObject;
-import org.hisp.dhis.common.UID;
 import org.hisp.dhis.commons.timer.SystemTimer;
 import org.hisp.dhis.commons.timer.Timer;
 import org.hisp.dhis.dxf2.metadata.feedback.ImportReport;
@@ -165,9 +164,6 @@ public class DefaultMetadataImportService implements MetadataImportService {
     params.setSkipSharing(getBooleanWithDefault(parameters, "skipSharing", false));
     params.setSkipTranslation(getBooleanWithDefault(parameters, "skipTranslation", false));
     params.setSkipValidation(getBooleanWithDefault(parameters, "skipValidation", false));
-    params.setUserOverrideMode(
-        getEnumWithDefault(
-            UserOverrideMode.class, parameters, "userOverrideMode", UserOverrideMode.NONE));
     params.setImportMode(
         getEnumWithDefault(
             ObjectBundleMode.class, parameters, "importMode", ObjectBundleMode.COMMIT));
@@ -189,21 +185,6 @@ public class DefaultMetadataImportService implements MetadataImportService {
     params.setFirstRowIsHeader(getBooleanWithDefault(parameters, "firstRowIsHeader", true));
     params.setAsync(getBooleanWithDefault(parameters, "async", false));
 
-    if (params.getUserOverrideMode() == UserOverrideMode.SELECTED) {
-      UID overrideUser = null;
-
-      if (parameters.containsKey("overrideUser")) {
-        List<String> overrideUsers = parameters.get("overrideUser");
-        overrideUser = UID.of(overrideUsers.get(0));
-      }
-
-      if (overrideUser == null) {
-        throw new MetadataImportException(
-            "UserOverrideMode.SELECTED is enabled, but overrideUser parameter does not point to a valid user.");
-      }
-      params.setOverrideUser(overrideUser);
-    }
-
     return params;
   }
 
@@ -214,7 +195,6 @@ public class DefaultMetadataImportService implements MetadataImportService {
   public ObjectBundleParams toObjectBundleParams(MetadataImportParams importParams) {
     UserDetails currentUser = CurrentUserUtil.getCurrentUserDetails();
     ObjectBundleParams params = new ObjectBundleParams();
-    params.setUserOverrideMode(importParams.getUserOverrideMode());
     params.setSkipSharing(importParams.isSkipSharing());
     params.setSkipTranslation(importParams.isSkipTranslation());
     params.setSkipValidation(importParams.isSkipValidation());
@@ -231,14 +211,6 @@ public class DefaultMetadataImportService implements MetadataImportService {
             ? currentUser
             : userService.createUserDetails(
                 userService.getUser(importParams.getUser().getValue())));
-    params.setOverrideUser(
-        importParams.getOverrideUser() == null
-            ? null
-            : userService.createUserDetails(
-                userService.getUser(importParams.getOverrideUser().getValue())));
-    if (params.getUserOverrideMode() == UserOverrideMode.CURRENT) {
-      params.setOverrideUser(currentUser);
-    }
     return params;
   }
 
