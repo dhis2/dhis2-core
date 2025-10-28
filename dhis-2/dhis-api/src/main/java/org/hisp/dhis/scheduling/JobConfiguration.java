8a6e7420f35c01e1e2a9fdeba09d163ee5e52ab2
Jan Bernitt <jaanbernitt@gmail.com>
2025-10-28 13:50:16 +0100

    fix: Jobs always get unique auto-generated names [DHIS2-20366]

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/scheduling/JobConfiguration.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/scheduling/JobConfiguration.java
@@ -39,6 +39,7 @@ import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
+import java.util.concurrent.atomic.AtomicLong;
 import javax.annotation.CheckForNull;
 import javax.annotation.Nonnull;
 import lombok.Getter;
@@ -198,6 +199,9 @@ public class JobConfiguration extends BaseIdentifiableObject implements Secondar
     this(name, type, null);
   }
 
+  /** The largest timestamp values that has already been used */
+  private static final AtomicLong mostRecentUsedTimeMilli = new AtomicLong(0L);
+
   /**
    * Constructor to use for any type of {@link SchedulingType#ONCE_ASAP} execution.
    *
@@ -208,12 +212,15 @@ public class JobConfiguration extends BaseIdentifiableObject implements Secondar
   public JobConfiguration(
       @CheckForNull String name, @Nonnull JobType type, @CheckForNull String executedBy) {
     this.name =
-        name == null || name.isEmpty()
-            ? "%s (%d)".formatted(type.name(), Instant.now().toEpochMilli())
-            : name;
+        name == null || name.isEmpty() ? "%s (%d)".formatted(type.name(), nowUnique()) : name;
     this.jobType = type;
     this.executedBy = executedBy;
     setAutoFields();
+  }
+
+  static long nowUnique() {
+    long now = System.currentTimeMillis();
+    return mostRecentUsedTimeMilli.updateAndGet(val -> Math.max(val + 1, now));
   }
 
   // -------------------------------------------------------------------------
