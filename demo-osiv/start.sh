9f8ea2b82b60c00c6b6ae62fbde53aacbbcfd73e
teleivo <teleivo@users.noreply.github.com>
2025-11-21 12:25:34 +0100

    demo

---
+++ demo-osiv/start.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Start DHIS2 with OSIV demo configuration
+#
+# Prerequisites:
+#   1. Build DHIS2: ./dhis-2/build-dev.sh
+#   2. Start PostgreSQL: docker compose up -d db
+
+cd "$(dirname "$0")/.." || exit 1
+
+DHIS2_HOME=$(pwd)/demo-osiv java \
+  -Dlog4j2.configurationFile=$(pwd)/demo-osiv/log4j2.xml \
+  -Dcom.zaxxer.hikari.housekeeping.periodMs=5000 \
+  -jar dhis-2/dhis-web-server/target/dhis.war
