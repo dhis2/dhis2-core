f76cb31548c2a82a9f4b94a68b195ddb65c11cde
Enrico Colasante <enrico@dhis2.org>
2025-12-01 14:47:44 +0100

    chore: Migrate PotentialDuplicate to use annotations [DHIS2-20468] (#22503)
    * chore: Migrate deduplication and changelog to annotations [DHIS2-20468]
    
    * Code review comments

--- dhis-2/dhis-services/dhis-service-tracker/src/main/java/org/hisp/dhis/tracker/deduplication/PotentialDuplicate.java
+++ dhis-2/dhis-services/dhis-service-tracker/src/main/java/org/hisp/dhis/tracker/deduplication/PotentialDuplicate.java
@@ -30,32 +30,89 @@
 package org.hisp.dhis.tracker.deduplication;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
-import org.hisp.dhis.common.BaseIdentifiableObject;
-import org.hisp.dhis.common.DxfNamespaces;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import java.util.Date;
+import java.util.Set;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.Type;
+import org.hisp.dhis.attribute.AttributeValues;
+import org.hisp.dhis.common.IdScheme;
+import org.hisp.dhis.common.IdentifiableObject;
+import org.hisp.dhis.common.IdentifiableProperty;
 import org.hisp.dhis.common.UID;
+import org.hisp.dhis.security.acl.Access;
+import org.hisp.dhis.translation.Translation;
+import org.hisp.dhis.user.User;
+import org.hisp.dhis.user.sharing.Sharing;
 
-public class PotentialDuplicate extends BaseIdentifiableObject {
+@Entity
+@Table(name = "potentialduplicate")
+@Getter
+@Setter
+public class PotentialDuplicate implements IdentifiableObject {
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+  @SequenceGenerator(name = "potentialduplicatesequence")
+  @Column(name = "potentialduplicateid")
+  private long id;
+
+  @Column(name = "uid", unique = true, nullable = false, length = 11)
+  private String uid;
+
+  @JsonProperty
+  @Column(name = "created", nullable = false, updatable = false)
+  @Temporal(TemporalType.TIMESTAMP)
+  private Date created;
+
+  @JsonProperty
+  @Column(name = "lastUpdated", nullable = false)
+  @Temporal(TemporalType.TIMESTAMP)
+  private Date lastUpdated;
+
   /**
    * original represents the UID of a TrackedEntity. original is required. original is a potential
    * duplicate of duplicate.
    */
+  @JsonProperty
+  @Type(type = "org.hisp.dhis.hibernate.UIDUserType")
+  @Column(name = "original", nullable = false)
   private UID original;
 
   /**
    * duplicate represents the UID of a TrackedEntity. duplicate is required. duplicate is a
    * potential duplicate of original.
    */
+  @JsonProperty
+  @Type(type = "org.hisp.dhis.hibernate.UIDUserType")
+  @Column(name = "duplicate", nullable = false)
   private UID duplicate;
 
+  @JsonProperty
+  @Column(name = "lastupdatebyusername", nullable = false)
   protected String lastUpdatedByUserName;
 
+  @JsonProperty
+  @Column(name = "createdbyusername", nullable = false)
   protected String createdByUserName;
 
   /**
    * status represents the state of the PotentialDuplicate. all new Potential duplicates are OPEN by
    * default.
    */
+  @JsonProperty
+  @Enumerated(EnumType.STRING)
+  @Column(name = "status")
   private DeduplicationStatus status = DeduplicationStatus.OPEN;
 
   public PotentialDuplicate() {}
@@ -65,53 +122,171 @@ public class PotentialDuplicate extends BaseIdentifiableObject {
     this.duplicate = duplicate;
   }
 
-  @JsonProperty
-  @JacksonXmlProperty
-  public UID getOriginal() {
-    return original;
+  // -------------------------------------------------------------------------
+  // Not supported properties
+  // -------------------------------------------------------------------------
+  @Override
+  public String getPropertyValue(IdScheme idScheme) {
+    if (idScheme.isNull() || idScheme.is(IdentifiableProperty.UID)) {
+      return uid;
+    }
+    if (idScheme.is(IdentifiableProperty.ID)) {
+      return id > 0 ? String.valueOf(id) : null;
+    }
+    return null;
   }
 
-  public void setOriginal(UID original) {
-    this.original = original;
+  @Override
+  public String getDisplayPropertyValue(IdScheme idScheme) {
+    if (idScheme.is(IdentifiableProperty.NAME)) {
+      return getDisplayName();
+    } else {
+      return getPropertyValue(idScheme);
+    }
   }
 
-  @JsonProperty
-  @JacksonXmlProperty
-  public UID getDuplicate() {
-    return duplicate;
+  @Override
+  public String getDisplayName() {
+    return getName();
   }
 
-  public void setDuplicate(UID duplicate) {
-    this.duplicate = duplicate;
+  @Override
+  public User getLastUpdatedBy() {
+    return null;
   }
 
-  @JsonProperty
-  @JacksonXmlProperty
-  public DeduplicationStatus getStatus() {
-    return status;
+  @Override
+  public Sharing getSharing() {
+    return Sharing.empty();
   }
 
-  public void setStatus(DeduplicationStatus status) {
-    this.status = status;
+  @Override
+  public void setSharing(Sharing sharing) {
+    // not supported
   }
 
-  @JsonProperty
-  @JacksonXmlProperty(namespace = DxfNamespaces.DXF_2_0)
-  public String getLastUpdatedByUserName() {
-    return lastUpdatedByUserName;
+  @Override
+  public Set<Translation> getTranslations() {
+    return Set.of();
   }
 
-  public void setLastUpdatedByUserName(String lastUpdatedByUserName) {
-    this.lastUpdatedByUserName = lastUpdatedByUserName;
+  @Override
+  public void setAccess(Access access) {
+    // not supported
   }
 
-  @JsonProperty
-  @JacksonXmlProperty(namespace = DxfNamespaces.DXF_2_0)
-  public String getCreatedByUserName() {
-    return createdByUserName;
+  /**
+   * @param user
+   * @deprecated This method is replaced by {@link #setCreatedBy(User)} ()} Currently it is only
+   *     used for web api backward compatibility
+   */
+  @Override
+  public void setUser(User user) {
+    // not supported
   }
 
-  public void setCreatedByUserName(String createdByUserName) {
-    this.createdByUserName = createdByUserName;
+  @Override
+  public Access getAccess() {
+    return null;
+  }
+
+  @Override
+  public void setTranslations(Set<Translation> translations) {
+    // not supported
+  }
+
+  @Override
+  public void setLastUpdatedBy(User user) {
+    // not supported
+  }
+
+  @Override
+  public String getCode() {
+    return null;
+  }
+
+  @Override
+  public void setCode(String code) {
+    // not supported
+  }
+
+  @Override
+  public String getName() {
+    return null;
+  }
+
+  @Override
+  public void setName(String name) {
+    // not supported
+  }
+
+  /** PotentialDuplicate does not support sharing */
+  @Override
+  public boolean hasSharing() {
+    return false;
+  }
+
+  /** PotentialDuplicate does not support sharing */
+  @Override
+  public void setOwner(String owner) {
+    // not supported
+  }
+
+  /** PotentialDuplicate does not support AttributeValues */
+  @Override
+  public AttributeValues getAttributeValues() {
+    return AttributeValues.empty();
+  }
+
+  /** PotentialDuplicate does not support AttributeValues */
+  @Override
+  public void setAttributeValues(AttributeValues attributeValues) {
+    // not supported
+  }
+
+  /** PotentialDuplicate does not support AttributeValues */
+  @Override
+  public void addAttributeValue(String attributeUid, String value) {
+    // not supported
+  }
+
+  /** PotentialDuplicate does not support AttributeValues */
+  @Override
+  public void removeAttributeValue(String attributeId) {
+    // not supported
+  }
+
+  /**
+   * @deprecated PotentialDuplicate does not support createdBy, use storeBy instead.
+   */
+  @Override
+  public User getUser() {
+    return getCreatedBy();
+  }
+
+  /**
+   * @deprecated PotentialDuplicate does not support createdBy, use storeBy instead.
+   */
+  @Override
+  public void setCreatedBy(User createdBy) {
+    // not supported
+  }
+
+  /**
+   * @deprecated PotentialDuplicate does not support createdBy, use storeBy instead.
+   */
+  @Override
+  public User getCreatedBy() {
+    return null;
+  }
+
+  @Override
+  public String getHref() {
+    return "";
+  }
+
+  @Override
+  public void setHref(String link) {
+    // not supported
   }
 }
