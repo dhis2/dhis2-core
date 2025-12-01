3f7c5973db656083c5d1b9462edcc2c61b741b5b
teleivo <teleivo@users.noreply.github.com>
2025-12-01 15:44:50 +0100

    fix: exclude some APIs from OSIV DHIS2-20512 (#22521)

--- dhis-2/dhis-services/dhis-service-tracker/src/main/java/org/hisp/dhis/tracker/export/relationship/RelationshipItemMapper.java
+++ dhis-2/dhis-services/dhis-service-tracker/src/main/java/org/hisp/dhis/tracker/export/relationship/RelationshipItemMapper.java
@@ -67,7 +67,7 @@ public interface RelationshipItemMapper {
 
     RelationshipItem result = new RelationshipItem();
     result.setRelationship(map(fields, relationshipItem.getRelationship()));
-    // only one of these fields will be non null as it represents the entity in which the
+    // only one of these fields will be non-null as it represents the entity in which the
     // relationship originates. we might be able to optimize these mappings but
     // that could result in more programmer errors as in unexpected NPEs
     result.setTrackedEntity(
