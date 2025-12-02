b92cac79b3321669c47d9b1d9430ef84531ab147
teleivo <teleivo@users.noreply.github.com>
2025-12-02 08:12:41 +0100

    fix: remove RequestInfo classes and interceptor from 2.40
    RequestIdFilter replaces the functionality of RequestInfoInterceptor.
    Removed:
    - RequestInfo, RequestInfoService, DefaultRequestInfoService
    - RequestInfoInterceptor
    - All references from WebMvcConfig and MvcTestConfig

--- dhis-2/dhis-test-web-api/src/test/java/org/hisp/dhis/webapi/MvcTestConfig.java
+++ dhis-2/dhis-test-web-api/src/test/java/org/hisp/dhis/webapi/MvcTestConfig.java
@@ -36,7 +36,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import org.hisp.dhis.common.Compression;
-import org.hisp.dhis.common.DefaultRequestInfoService;
 import org.hisp.dhis.dxf2.metadata.MetadataExportService;
 import org.hisp.dhis.fieldfiltering.FieldFilterService;
 import org.hisp.dhis.fieldfiltering.FieldPathConverter;
@@ -44,15 +43,12 @@ import org.hisp.dhis.message.FakeMessageSender;
 import org.hisp.dhis.message.MessageSender;
 import org.hisp.dhis.node.DefaultNodeService;
 import org.hisp.dhis.node.NodeService;
-import org.hisp.dhis.system.SystemInfo;
-import org.hisp.dhis.system.SystemService;
 import org.hisp.dhis.system.database.DatabaseInfo;
-import org.hisp.dhis.user.CurrentUserService;
+import org.hisp.dhis.system.database.DatabaseInfoProvider;
 import org.hisp.dhis.user.UserSettingService;
 import org.hisp.dhis.webapi.mvc.CurrentUserHandlerMethodArgumentResolver;
 import org.hisp.dhis.webapi.mvc.CustomRequestMappingHandlerMapping;
 import org.hisp.dhis.webapi.mvc.DhisApiVersionHandlerMethodArgumentResolver;
-import org.hisp.dhis.webapi.mvc.interceptor.RequestInfoInterceptor;
 import org.hisp.dhis.webapi.mvc.interceptor.UserContextInterceptor;
 import org.hisp.dhis.webapi.mvc.messageconverter.CsvMessageConverter;
 import org.hisp.dhis.webapi.mvc.messageconverter.JsonMessageConverter;
@@ -61,7 +57,6 @@ import org.hisp.dhis.webapi.mvc.messageconverter.StreamingJsonRootMessageConvert
 import org.hisp.dhis.webapi.mvc.messageconverter.XmlMessageConverter;
 import org.hisp.dhis.webapi.mvc.messageconverter.XmlPathMappingJackson2XmlHttpMessageConverter;
 import org.hisp.dhis.webapi.view.CustomPathExtensionContentNegotiationStrategy;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
@@ -73,6 +68,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.converter.ByteArrayHttpMessageConverter;
 import org.springframework.http.converter.FormHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.ResourceHttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
@@ -99,11 +95,7 @@ import org.springframework.web.servlet.resource.ResourceUrlProviderExposingInter
 @EnableWebMvc
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class MvcTestConfig implements WebMvcConfigurer {
-  @Autowired private CurrentUserService currentUserService;
-
   @Autowired private UserSettingService userSettingService;
-
-  @Autowired public DefaultRequestInfoService requestInfoService;
 
   @Autowired private MetadataExportService metadataExportService;
 
@@ -135,8 +127,7 @@ public class MvcTestConfig implements WebMvcConfigurer {
     addInterceptors(registry);
     registry.addInterceptor(new ConversionServiceExposingInterceptor(mvcConversionService));
     registry.addInterceptor(new ResourceUrlProviderExposingInterceptor(mvcResourceUrlProvider));
-    registry.addInterceptor(new UserContextInterceptor(currentUserService, userSettingService));
-    registry.addInterceptor(new RequestInfoInterceptor(requestInfoService));
+    registry.addInterceptor(new UserContextInterceptor(userSettingService));
     mapping.setInterceptors(registry.getInterceptors().toArray());
 
     CustomPathExtensionContentNegotiationStrategy pathExtensionNegotiationStrategy =
@@ -198,17 +189,9 @@ public class MvcTestConfig implements WebMvcConfigurer {
     return new DefaultNodeService();
   }
 
-  @Bean("databaseInfo")
-  public DatabaseInfo databaseInfo() {
-    return new DatabaseInfo();
-  }
-
-  @Primary
-  @Bean("systemService")
-  public SystemService systemService() {
-    SystemService systemService = Mockito.mock(SystemService.class);
-    Mockito.when(systemService.getSystemInfo()).thenReturn(new SystemInfo());
-    return systemService;
+  @Bean
+  public DatabaseInfoProvider databaseInfoProvider() {
+    return () -> DatabaseInfo.builder().build();
   }
 
   @Override
@@ -216,8 +199,7 @@ public class MvcTestConfig implements WebMvcConfigurer {
     registry.addInterceptor(new ConversionServiceExposingInterceptor(mvcConversionService));
     registry.addInterceptor(new ResourceUrlProviderExposingInterceptor(mvcResourceUrlProvider));
 
-    registry.addInterceptor(new UserContextInterceptor(currentUserService, userSettingService));
-    registry.addInterceptor(new RequestInfoInterceptor(requestInfoService));
+    registry.addInterceptor(new UserContextInterceptor(userSettingService));
   }
 
   @Bean
@@ -268,6 +250,7 @@ public class MvcTestConfig implements WebMvcConfigurer {
 
     converters.add(mappingJackson2HttpMessageConverter());
     converters.add(mappingJackson2XmlHttpMessageConverter());
+    converters.add(new ResourceHttpMessageConverter());
   }
 
   @Override
