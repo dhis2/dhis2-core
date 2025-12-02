523f3e0b62f39203908f34a17e466da300510b79
Enrico <enrico@dhis2.org>
2025-12-02 15:25:56 -0300

    Code review comments

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.43/V2_43_34__split_changelog_sequence.sql
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.43/V2_43_34__split_changelog_sequence.sql
@@ -1,9 +1,8 @@
 create sequence if not exists trackereventchangelog_sequence;
 create sequence if not exists singleeventchangelog_sequence;
 
-select setval('trackereventchangelog_sequence', max(eventchangelogid)) from trackereventchangelog;
-select setval('singleeventchangelog_sequence', max(eventchangelogid)) from singleeventchangelog;
-
+select setval('trackereventchangelog_sequence', coalesce(max(eventchangelogid), 1)) from trackereventchangelog;
+select setval('singleeventchangelog_sequence', coalesce(max(eventchangelogid),1)) from singleeventchangelog;
 
 alter table trackereventchangelog alter column eventchangelogid drop default;
 alter table singleeventchangelog alter column eventchangelogid drop default;
