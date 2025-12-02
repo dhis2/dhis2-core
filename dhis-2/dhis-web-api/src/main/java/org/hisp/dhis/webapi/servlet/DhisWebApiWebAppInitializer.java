884624848d72e2e5a2a92f931d7a6525a66f542b
teleivo <teleivo@users.noreply.github.com>
2025-12-02 05:00:35 +0100

    fix: process X-Request-ID earlier DHIS2-20528 (#22542)

--- dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/servlet/DhisWebApiWebAppInitializer.java
+++ dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/servlet/DhisWebApiWebAppInitializer.java
@@ -102,6 +102,12 @@ public class DhisWebApiWebAppInitializer implements WebApplicationInitializer {
   public static void setupServlets(
       ServletContext context, AnnotationConfigWebApplicationContext webApplicationContext) {
 
+    // RequestIdFilter must run first to capture X-Request-ID for logging/MDC before any other
+    // filters (especially Spring Security)
+    context
+        .addFilter("requestIdFilter", new DelegatingFilterProxy("requestIdFilter"))
+        .addMappingForUrlPatterns(null, false, "/*");
+
     context
         .addFilter(
             "SpringSessionRepositoryFilter",
@@ -150,7 +156,7 @@ public class DhisWebApiWebAppInitializer implements WebApplicationInitializer {
         .addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/api/*");
 
     context
-        .addFilter("RequestIdentifierFilter", new DelegatingFilterProxy("requestIdentifierFilter"))
+        .addFilter("sessionIdFilter", new DelegatingFilterProxy("sessionIdFilter"))
         .addMappingForUrlPatterns(null, true, "/*");
 
     /* Intercept index.html, plugin.html, and other html requests to inject no-cache
