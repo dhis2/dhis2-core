0c95d4e103c4909e64bcfe8bf2c7dc6512151466
teleivo <teleivo@users.noreply.github.com>
2025-12-04 10:08:48 +0100

    fix: exclude some APIs from OSIV DHIS2-20512 [40] (#22556)
    * fix: exclude some APIs from OSIV DHIS2-20512 (#22521)
    
    * fix: use OpenSessionInViewFilter for 2.40 compatibility
    
    * refactor: rename filter to ConditionalOpenSessionInViewFilter
    
    The class now extends OpenSessionInViewFilter instead of
    OpenEntityManagerInViewFilter, so the name should reflect that.
    
    * fix: missing explicit transaction boundaries in user path
    
    * fix: add missing transaction annotations

--- dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/servlet/DhisWebApiWebAppInitializer.java
+++ dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/servlet/DhisWebApiWebAppInitializer.java
@@ -38,9 +38,9 @@ import org.hisp.dhis.external.conf.DefaultDhisConfigurationProvider;
 import org.hisp.dhis.external.conf.DhisConfigurationProvider;
 import org.hisp.dhis.external.location.DefaultLocationManager;
 import org.hisp.dhis.system.startup.StartupListener;
+import org.hisp.dhis.webapi.filter.ConditionalOpenSessionInViewFilter;
 import org.hisp.dhis.webapi.security.config.WebMvcConfig;
 import org.springframework.core.annotation.Order;
-import org.springframework.orm.hibernate5.support.OpenSessionInViewFilter;
 import org.springframework.security.web.session.HttpSessionEventPublisher;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
@@ -111,7 +111,7 @@ public class DhisWebApiWebAppInitializer implements WebApplicationInitializer {
         .addMappingForUrlPatterns(null, false, "/api/*");
 
     FilterRegistration.Dynamic openSessionInViewFilter =
-        context.addFilter("openSessionInViewFilter", OpenSessionInViewFilter.class);
+        context.addFilter("openSessionInViewFilter", ConditionalOpenSessionInViewFilter.class);
     openSessionInViewFilter.setInitParameter("sessionFactoryBeanName", "sessionFactory");
     openSessionInViewFilter.addMappingForUrlPatterns(null, false, "/*");
     openSessionInViewFilter.addMappingForServletNames(null, false, "dispatcher");
