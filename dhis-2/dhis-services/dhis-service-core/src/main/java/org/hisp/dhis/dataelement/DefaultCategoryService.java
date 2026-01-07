9a3416d917a08645945bba640e0cd3c824191d32
Jan Bernitt <jaanbernitt@gmail.com>
2026-01-07 11:58:15 +0100

    fix: revert to prior behaviour for api/categoryOptions/orgUnits?categoryOptions= (#22704)

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/dataelement/DefaultCategoryService.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/dataelement/DefaultCategoryService.java
@@ -778,6 +778,6 @@ public class DefaultCategoryService implements CategoryService {
   @Override
   public SetValuedMap<String, String> getCategoryOptionOrganisationUnitsAssociations(
       Set<String> uids) {
-    return jdbcOrgUnitAssociationsStore.getOrganisationUnitsAssociationsForCurrentUser(uids);
+    return jdbcOrgUnitAssociationsStore.getOrganisationUnitsAssociationsForCurrentUser(uids, false);
   }
 }
