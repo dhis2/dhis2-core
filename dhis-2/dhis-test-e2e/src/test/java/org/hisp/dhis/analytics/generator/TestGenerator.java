f3434dcaeb978cffb9c08b90c35ff55543ab9ad9
Luciano Fiandesio <luciano@fiandesio.com>
2026-01-15 15:37:38 +0100

    Revert changes to e2e property file

--- dhis-2/dhis-test-e2e/src/test/java/org/hisp/dhis/analytics/generator/TestGenerator.java
+++ dhis-2/dhis-test-e2e/src/test/java/org/hisp/dhis/analytics/generator/TestGenerator.java
@@ -29,12 +29,12 @@
  */
 package org.hisp.dhis.analytics.generator;
 
-import org.hisp.dhis.analytics.generator.impl.EnrollmentAggregatedGenerator;
+import org.hisp.dhis.analytics.generator.impl.EnrollmentQueryGenerator;
 
 /** This class simply hold the generator implementation to be used during the code generation. */
 public class TestGenerator {
   static Generator get() {
-    return new EnrollmentAggregatedGenerator("stageAndEventStatus");
+    return new EnrollmentQueryGenerator("stageAndEventStatusActiveAndEventDate2021");
 
     // To generate all e2e tests just return the default constructor.
     // ie.: return new TeiQueryGenerator();
