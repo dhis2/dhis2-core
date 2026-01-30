11d81164d67f0ff7a455bb2f40ea9e62546eaee3
Marc <marc@dhis2.org>
2026-01-30 09:25:38 +0100

    feat: Set column not null with default value [DHIS2-20658]

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.43/V2_43_45__add_enrollment_categoryoptioncombo.sql
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.43/V2_43_45__add_enrollment_categoryoptioncombo.sql
@@ -10,3 +10,18 @@ alter table enrollment
     add constraint fk_enrollment_categorycomboid
     foreign key (attributeoptioncomboid)
     references categoryoptioncombo(categoryoptioncomboid);
+
+-- Retrieves the default cat opt combo by first looking for the default UID, then the default name for older databases
+with default_cat_opt_combo as (
+    select coalesce ((
+                         select "categoryoptioncomboid" from categoryoptioncombo where uid = 'HllvX50cXC0'), (
+                         select "categoryoptioncomboid" from categoryoptioncombo where name = 'default')) as id
+)
+
+-- Sets the attributeoptioncomboid column to default for null values
+update enrollment
+set attributeoptioncomboid = (select id from default_cat_opt_combo)
+where attributeoptioncomboid is null;
+
+-- Sets the attributeoptioncomboid column to not null
+alter table enrollment alter column attributeoptioncomboid set not null;
