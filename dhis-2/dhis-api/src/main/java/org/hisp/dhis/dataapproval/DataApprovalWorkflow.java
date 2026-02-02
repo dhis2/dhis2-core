04f4dd902b46e8fcd09b3225b5a9ec668773ac1a
Viet Nguyen <viet@dhis2.org>
2026-02-01 23:31:47 -0600

    fix: add JsonIgnore annotation to DataApprovalWorkflow

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/dataapproval/DataApprovalWorkflow.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/dataapproval/DataApprovalWorkflow.java
@@ -29,6 +29,7 @@
  */
 package org.hisp.dhis.dataapproval;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
