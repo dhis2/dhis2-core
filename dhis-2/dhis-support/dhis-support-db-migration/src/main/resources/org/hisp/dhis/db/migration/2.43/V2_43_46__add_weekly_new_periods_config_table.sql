e170e184c8688d3dd5530563ab7c1e7de9dbd886
Maikel Arabori <maikelarabori@gmail.com>
2026-02-10 09:15:44 -0300

    test: Failing tests

--- dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.43/V2_43_46__add_weekly_new_periods_config_table.sql
+++ dhis-2/dhis-support/dhis-support-db-migration/src/main/resources/org/hisp/dhis/db/migration/2.43/V2_43_46__add_weekly_new_periods_config_table.sql
@@ -1,5 +1,20 @@
 -- Migration related to DHIS2-20837.
 
-insert into periodtype (periodtypeid, name) values (nextval('hibernate_sequence'), 'WeeklyFriday');
+-- Migrate required data into the new table.
+do
+$$
+declare
+    stmt TEXT;
+begin
+  -- Check if configuration table has row.
+  if not exists (
+    select 1
+    from periodtype where name = 'WeeklyFriday';
+    )
+  then
+    insert into periodtype (periodtypeid, name) values (nextval('hibernate_sequence'), 'WeeklyFriday');
+  else
+    RAISE INFO '%','WeeklyFriday already exists';
+  end if;
 
 insert into configuration_dataoutputperiodtype (periodtypeid, configurationid) select p.periodtypeid, c.configurationid from periodtype p, configuration c where p.name = 'WeeklyFriday';
