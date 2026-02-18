9498b013deba4d66cef9b0356d0aaac5530864a8
Austin McGee <947888+amcgee@users.noreply.github.com>
2026-02-18 11:49:50 +0100

    chore: fix integration test to expect default-src CSP policy

--- dhis-2/dhis-test-web-api/src/test/java/org/hisp/dhis/webapi/controller/tracker/export/event/EventsExportControllerTest.java
+++ dhis-2/dhis-test-web-api/src/test/java/org/hisp/dhis/webapi/controller/tracker/export/event/EventsExportControllerTest.java
@@ -649,7 +649,7 @@ class EventsExportControllerTest extends PostgresControllerIntegrationTestBase {
     assertEquals("no-cache, private", response.header("Cache-Control"));
     assertEquals(Long.toString(file.getContentLength()), response.header("Content-Length"));
     assertEquals("filename=" + file.getName(), response.header("Content-Disposition"));
-    assertContains("script-src 'none';", response.header("Content-Security-Policy"));
+    assertContains("default-src 'none';", response.header("Content-Security-Policy"));
     assertEquals("file content", response.content("text/plain"));
   }
 
@@ -697,7 +697,7 @@ class EventsExportControllerTest extends PostgresControllerIntegrationTestBase {
     assertEquals(HttpStatus.NOT_MODIFIED, response.status());
     assertEquals("\"" + file.getUid() + "\"", response.header("Etag"));
     assertEquals("no-cache, private", response.header("Cache-Control"));
-    assertContains("script-src 'none';", response.header("Content-Security-Policy"));
+    assertContains("default-src 'none';", response.header("Content-Security-Policy"));
     assertFalse(response.hasBody());
   }
 
