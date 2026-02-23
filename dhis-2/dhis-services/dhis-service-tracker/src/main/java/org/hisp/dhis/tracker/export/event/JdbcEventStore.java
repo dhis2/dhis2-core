4fc300b6b826540278eaeadd628d9fcd4845940b
Enrico <enrico@dhis2.org>
2026-02-23 10:15:41 +0100

    Code review fixes

--- dhis-2/dhis-services/dhis-service-tracker/src/main/java/org/hisp/dhis/tracker/export/event/JdbcEventStore.java
+++ dhis-2/dhis-services/dhis-service-tracker/src/main/java/org/hisp/dhis/tracker/export/event/JdbcEventStore.java
@@ -990,8 +990,8 @@ left join dataelement de on de.uid = eventdatavalue.dataelement_uid
 
     fromBuilder.append(eventStatusSql(params, sqlParameters, hlp));
 
-    if (params.getEvents() != null && !params.getEvents().isEmpty()) {
-      sqlParameters.addValue(COLUMN_EVENT_UID, UID.toValueSet(params.getEvents()));
+    if (!params.getEvents().isEmpty()) {
+      sqlParameters.addValue("ev_uid", UID.toValueSet(params.getEvents()));
       fromBuilder.append(hlp.whereAnd()).append(" (ev.uid in (").append(":ev_uid").append(")) ");
     }
 
