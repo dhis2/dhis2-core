13c29b1dd572c38a05687012040df4cc4c4dd2d7
Marc <marc@dhis2.org>
2026-02-26 11:00:32 +0100

    fix: Use tracked entity store to persist [DHIS2-20975]

--- dhis-2/dhis-tracker/src/main/java/org/hisp/dhis/tracker/acl/DefaultTrackedEntityProgramOwnerService.java
+++ dhis-2/dhis-tracker/src/main/java/org/hisp/dhis/tracker/acl/DefaultTrackedEntityProgramOwnerService.java
@@ -32,6 +32,7 @@ package org.hisp.dhis.tracker.acl;
 import java.util.Date;
 import javax.annotation.Nonnull;
 import lombok.RequiredArgsConstructor;
+import org.hisp.dhis.common.IdentifiableObjectStore;
 import org.hisp.dhis.feedback.BadRequestException;
 import org.hisp.dhis.organisationunit.OrganisationUnit;
 import org.hisp.dhis.program.Program;
@@ -48,6 +49,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Service("org.hisp.dhis.tracker.acl.TrackedEntityProgramOwnerService")
 public class DefaultTrackedEntityProgramOwnerService implements TrackedEntityProgramOwnerService {
   private final TrackedEntityProgramOwnerStore trackedEntityProgramOwnerStore;
+  private final IdentifiableObjectStore<TrackedEntity> trackedEntityStore;
 
   @Override
   @Transactional
@@ -105,6 +107,7 @@ public class DefaultTrackedEntityProgramOwnerService implements TrackedEntityPro
     updateTrackedEntityProgramOwner(teProgramOwner, orgUnit);
     trackedEntityProgramOwnerStore.update(teProgramOwner);
     trackedEntity.setLastUpdated(new Date());
+    trackedEntityStore.update(trackedEntity);
   }
 
   private void updateTrackedEntityProgramOwner(
