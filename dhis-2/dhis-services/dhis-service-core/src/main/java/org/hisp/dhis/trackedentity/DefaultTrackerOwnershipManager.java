2a60970e0abcb7a872c17a0c26923cf855a994c3
Marc <marc@dhis2.org>
2026-03-12 22:23:57 +0100

    fix: Skip access validation if requested [DHIS2-21095][2.41]

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/trackedentity/DefaultTrackerOwnershipManager.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/trackedentity/DefaultTrackerOwnershipManager.java
@@ -147,7 +147,7 @@ public class DefaultTrackerOwnershipManager implements TrackerOwnershipManager {
 
     User currentUser = userService.getUserByUsername(CurrentUserUtil.getCurrentUsername());
 
-    if (!currentUser.getTeiSearchOrganisationUnits().contains(orgUnit)) {
+    if (!skipAccessValidation && !currentUser.getTeiSearchOrganisationUnits().contains(orgUnit)) {
       throw new ForbiddenException(
           "Tracked entity not transferred. Org unit supplied is not in the user scope.");
     }
