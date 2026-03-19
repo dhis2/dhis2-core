214943076f1849b57e890251b4717ad5a0b18261
Marc <marc@dhis2.org>
2026-03-19 13:11:59 +0100

    fix: Validate org unit search scope on enrollment update [DHIS2-21103]

--- dhis-2/dhis-test-integration/src/test/java/org/hisp/dhis/tracker/imports/validation/EnrollmentImportValidationTest.java
+++ dhis-2/dhis-test-integration/src/test/java/org/hisp/dhis/tracker/imports/validation/EnrollmentImportValidationTest.java
@@ -34,6 +34,7 @@ import static org.hisp.dhis.tracker.Assertions.assertHasOnlyErrors;
 import static org.hisp.dhis.tracker.Assertions.assertNoErrors;
 import static org.hisp.dhis.tracker.imports.validation.Users.USER_2;
 import static org.hisp.dhis.tracker.imports.validation.Users.USER_4;
+import static org.hisp.dhis.tracker.imports.validation.Users.USER_5;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -120,22 +121,22 @@ class EnrollmentImportValidationTest extends PostgresIntegrationTestBase {
     secondTrackerObjects
         .getEnrollments()
         .forEach(
-            e -> {
-              assertTrue(
-                  e.getOrgUnit()
-                      .isEqualTo(
-                          preheat
-                              .getProgramOwner()
-                              .get(e.getTrackedEntity())
-                              .get(e.getProgram().getIdentifier())
-                              .getOrganisationUnit()));
-            });
+            e ->
+                assertTrue(
+                    e.getOrgUnit()
+                        .isEqualTo(
+                            preheat
+                                .getProgramOwner()
+                                .get(e.getTrackedEntity())
+                                .get(e.getProgram().getIdentifier())
+                                .getOrganisationUnit())));
   }
 
   @Test
-  void testNoWriteAccessToOrg() throws IOException {
+  void shouldFailWhenCreatingNewEnrollmentIfUserHasNoCaptureScopeAccessToOrgUnit()
+      throws IOException {
     User user = userService.getUser(USER_2);
-    TrackerImportParams params = new TrackerImportParams();
+    TrackerImportParams params = TrackerImportParams.builder().build();
     injectSecurityContextUser(user);
 
     ImportReport importReport =
@@ -143,6 +144,25 @@ class EnrollmentImportValidationTest extends PostgresIntegrationTestBase {
             params, testSetup.fromJson("tracker/validations/enrollments_te_enrollments-data.json"));
 
     assertHasErrors(importReport, 4, ValidationCode.E1000);
+  }
+
+  @Test
+  void shouldFailWhenUpdatingEnrollmentIfUserHasNoSearchScopeAccessToOrgUnit() throws IOException {
+    TrackerImportParams params = TrackerImportParams.builder().build();
+    assertNoErrors(
+        trackerImportService.importTracker(
+            params,
+            testSetup.fromJson("tracker/validations/enrollments_te_enrollments-data_2.json")));
+    User user = userService.getUser(USER_5);
+    injectSecurityContextUser(user);
+
+    ImportReport importReport =
+        trackerImportService.importTracker(
+            params,
+            testSetup.fromJson(
+                "tracker/validations/enrollments_te_enrollments-data_2-update.json"));
+
+    assertHasErrors(importReport, 1, ValidationCode.E1131);
   }
 
   @Test
