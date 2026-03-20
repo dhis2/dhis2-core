37957df3632fa197b4b17f5664886633fe0db226
vietnguyen <viet@dhis2.org>
2026-03-20 06:30:46 -0500

    code format

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/dashboard/DashboardItem.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/dashboard/DashboardItem.java
@@ -49,7 +49,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
-import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OrderColumn;
