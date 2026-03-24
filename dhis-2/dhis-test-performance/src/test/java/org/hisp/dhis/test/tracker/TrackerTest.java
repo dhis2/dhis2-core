d13e2c6b5c87fabbf20bfefd02cddd4981e7ab67
Enrico <enrico@dhis2.org>
2026-03-24 11:15:29 +0100

    chore: Import only program with no program rules

--- dhis-2/dhis-test-performance/src/test/java/org/hisp/dhis/test/tracker/TrackerTest.java
+++ dhis-2/dhis-test-performance/src/test/java/org/hisp/dhis/test/tracker/TrackerTest.java
@@ -12,7 +12,7 @@
  * this list of conditions and the following disclaimer in the documentation
  * and/or other materials provided with the distribution.
  *
- * 3. Neither the name of the copyright holder nor the names of its contributors 
+ * 3. Neither the name of the copyright holder nor the names of its contributors
  * may be used to endorse or promote products derived from this software without
  * specific prior written permission.
  *
@@ -432,9 +432,7 @@ public class TrackerTest extends Simulation {
   private PopulationBuilder importScenarios() {
     // entitiesPerLine: MNCH = 1 TE + 2 enrollments + 6 events, Child = 1 TE + 1 enrollment + 2
     // events
-    return importProgram("MNCH import", this.mnchFeeder, 9, "trackedEntities")
-        .andThen(importProgram("Child Programme import", this.childFeeder, 4, "trackedEntities"))
-        .andThen(importProgram("ANC import", this.ancFeeder, 1, "events"));
+    return importProgram("MNCH import", this.mnchFeeder, 9, "trackedEntities");
   }
 
   /**
