87dc330e4559171f9dc260242144510651fc7233
Luciano Fiandesio <luciano@fiandesio.com>
2026-05-15 14:27:11 +0200

    Remove debug code

--- dhis-2/dhis-test-e2e/src/test/java/org/hisp/dhis/analytics/enrollment/aggregate/EnrollmentsAggregate5AutoTest.java
+++ dhis-2/dhis-test-e2e/src/test/java/org/hisp/dhis/analytics/enrollment/aggregate/EnrollmentsAggregate5AutoTest.java
@@ -184,7 +184,6 @@ public class EnrollmentsAggregate5AutoTest extends AnalyticsApiTest {
 
     // When
     ApiResponse response = actions.aggregate().get("IpHINAT79UW", JSON, JSON, params);
-    System.out.println(response.prettyPrint());
     // Then
     // 1. Validate Response Structure (Counts, Headers, Height/Width)
     //    This helper checks basic counts and dimensions, adapting based on the runtime
