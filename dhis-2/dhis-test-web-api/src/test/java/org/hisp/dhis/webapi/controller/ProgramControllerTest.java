c6bfe3b988b509ad4601c069c372c0439706ea5c
Viet Nguyen <viet@dhis2.org>
2026-06-03 03:06:52 -0500

    fix: mapView should not be exported at root level  (40) (#24104)

--- dhis-2/dhis-test-web-api/src/test/java/org/hisp/dhis/webapi/controller/ProgramControllerTest.java
+++ dhis-2/dhis-test-web-api/src/test/java/org/hisp/dhis/webapi/controller/ProgramControllerTest.java
@@ -33,7 +33,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hisp.dhis.dataelement.DataElement;
-import org.hisp.dhis.jsontree.JsonResponse;
+import org.hisp.dhis.jsontree.JsonObject;
 import org.hisp.dhis.trackedentity.TrackedEntityAttribute;
 import org.hisp.dhis.web.HttpStatus;
 import org.hisp.dhis.webapi.DhisControllerConvenienceTest;
@@ -70,24 +70,26 @@ class ProgramControllerTest extends DhisControllerConvenienceTest {
 
   @Test
   void testDeleteWithMapView() {
-    String mapViewJson =
+
+    String mapJson =
         "{"
-            + "\"name\": \"test mapview\","
-            + "\"id\": \"mVIVRd23Jm9\","
-            + "\"organisationUnitLevels\": [],"
-            + "\"maps\": [],"
-            + "\"layer\": \"event\","
-            + "\"program\": {"
-            + "\"id\": \"PrZMWi7rBga\""
-            + "},"
-            + "\"programStage\": {"
-            + "\"id\": \"PSzMWi7rBga\""
-            + "}"
+            + "'name': 'test map',"
+            + "'id': 'mAPVRd23Jm9',"
+            + "'mapViews': [{"
+            + "'name': 'test mapview',"
+            + "'id': 'mVIVRd23Jm9',"
+            + "'organisationUnitLevels': [],"
+            + "'layer': 'event',"
+            + "'program': {'id': 'PrZMWi7rBga'},"
+            + "'programStage': {'id': 'PSzMWi7rBga'}"
+            + "}]"
             + "}";
-    POST("/mapViews", mapViewJson).content(HttpStatus.CREATED);
-    assertStatus(HttpStatus.OK, DELETE(String.format("/programs/%s", PROGRAM_UID)));
-    assertStatus(HttpStatus.NOT_FOUND, GET(String.format("/programs/%s", PROGRAM_UID)));
-    JsonResponse mapview = GET("/mapViews/mVIVRd23Jm9").content();
+    POST("/maps", mapJson).content(HttpStatus.CREATED);
+
+    assertStatus(HttpStatus.OK, DELETE("/programs/" + PROGRAM_UID));
+    assertStatus(HttpStatus.NOT_FOUND, GET("/programs/" + PROGRAM_UID));
+    JsonObject mapview = GET("/mapViews/mVIVRd23Jm9").content().as(JsonObject.class);
     assertFalse(mapview.has("program"));
+    assertFalse(mapview.has("programStage"));
   }
 }
