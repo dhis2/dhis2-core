b9b6ec1590df5af6bd6e6719750c5fd69bd2ad64
Enrico Colasante <enrico@dhis2.org>
2026-06-12 08:22:21 +0200

    fix: Refactor persistence layer in tracker importer [DHIS2-21378] (#24197)
    * feat: TrackedEntity JDBC update [DHIS2-21378]
    
    * feat: Enrollment JDBC insert/update [DHIS2-21378]
    
    * feat: Tracker event JDBC insert/update [DHIS2-21378]
    
    * feat: Single event JDBC insert/update [DHIS2-21378]
    
    * feat: Relationship JDBC insert/update [DHIS2-21378]
    
    * feat: TEAV JDBC insert/update [DHIS2-21378]
    
    * Code review fixes
    
    * Fix tests
    
    * Fix idScheme issue for TEAV
    
    * fix: Refactor persistence layer in tracker importer [DHIS2-21378]
    
    * Fix merge
    
    (cherry picked from commit 194dcd15363b1c8d6436f8b6bdad0a8efd0d91ab)

--- dhis-2/dhis-test-integration/src/test/java/org/hisp/dhis/tracker/imports/bundle/EnrollmentImportTest.java
+++ dhis-2/dhis-test-integration/src/test/java/org/hisp/dhis/tracker/imports/bundle/EnrollmentImportTest.java
@@ -100,7 +100,7 @@ class EnrollmentImportTest extends PostgresIntegrationTestBase {
 
     ImportReport importReport = trackerImportService.importTracker(params, trackerObjects);
 
-    dbmsManager.clearSession();
+    clearSession();
 
     assertNoErrors(importReport);
 
