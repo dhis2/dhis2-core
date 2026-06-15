4f3a4cfaf51910438dd2e3b844d6f08917660c8a
Enrico <enrico@dhis2.org>
2026-06-15 10:28:46 +0200

    Fix

--- dhis-2/dhis-test-performance/src/test/java/org/hisp/dhis/test/tracker/TrackerTest.java
+++ dhis-2/dhis-test-performance/src/test/java/org/hisp/dhis/test/tracker/TrackerTest.java
@@ -12,7 +12,7 @@
  * this list of conditions and the following disclaimer in the documentation
  * and/or other materials provided with the distribution.
  *
- * 3. Neither the name of the copyright holder nor the names of its contributors 
+ * 3. Neither the name of the copyright holder nor the names of its contributors
  * may be used to endorse or promote products derived from this software without
  * specific prior written permission.
  *
@@ -553,7 +553,7 @@ public class TrackerTest extends Simulation {
             + this.eventProgram
             + "&fields=dataValues,occurredAt,event,status,orgUnit,program,programType,updatedAt,createdAt,assignedUser"
             + "&paging=false"
-            + "&order=occurredAt:desc";
+            + "&order=updatedAt:desc";
 
     Request goToFirstPage =
         new Request(
