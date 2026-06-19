6a8ff1b2afeb9ba95439faa4b1b8f8dc9e240393
Maikel Arabori <maikelarabori@gmail.com>
2026-06-19 07:01:00 -0300

    test: Disable analytics cache

--- dhis-2/dhis-test-performance/src/test/java/org/hisp/dhis/test/analytics/sl/aggregate/AnalyticsAggregate1.java
+++ dhis-2/dhis-test-performance/src/test/java/org/hisp/dhis/test/analytics/sl/aggregate/AnalyticsAggregate1.java
@@ -68,8 +68,8 @@ public class AnalyticsAggregate1 extends Simulation implements AnalyticsSimulati
 
   public List<Assertion> buildAssertions() {
     return List.of(
-        details(GET_QUERY).responseTime().percentile(95).lt(4750),
-        details(GET_QUERY).responseTime().max().lt(4750),
+        details(GET_QUERY).responseTime().percentile(95).lt(255),
+        details(GET_QUERY).responseTime().max().lt(275),
         details(GET_QUERY).successfulRequests().percent().is(100D),
         details(GET_QUERY).successfulRequests().percent().is(100D));
   }
