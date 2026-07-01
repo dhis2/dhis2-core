d19a96a1945df1787a1ea9417538edefb3f1f0d1
Luciano Fiandesio <luciano@fiandesio.com>
2026-07-01 13:20:29 +0200

    feat: add tracked entity aggregate service [DHIS2-21732]

---
+++ dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/trackedentity/TrackedEntityAggregateService.java
@@ -0,0 +1,142 @@
+/*
+ * Copyright (c) 2004-2026, University of Oslo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors 
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.hisp.dhis.analytics.trackedentity;
+
+import static java.util.Collections.singleton;
+import static java.util.UUID.randomUUID;
+import static java.util.stream.Collectors.toList;
+import static org.hisp.dhis.analytics.trackedentity.query.TrackedEntityFields.getAggregateGridHeaders;
+import static org.hisp.dhis.analytics.util.AnalyticsUtils.withExceptionHandling;
+
+import java.util.List;
+import java.util.Optional;
+import javax.annotation.Nonnull;
+import lombok.RequiredArgsConstructor;
+import org.hisp.dhis.analytics.analyze.ExecutionPlanStore;
+import org.hisp.dhis.analytics.common.ContextParams;
+import org.hisp.dhis.analytics.common.QueryExecutor;
+import org.hisp.dhis.analytics.common.SqlQuery;
+import org.hisp.dhis.analytics.common.SqlQueryResult;
+import org.hisp.dhis.analytics.common.params.AnalyticsPagingParams;
+import org.hisp.dhis.analytics.common.params.CommonParsedParams;
+import org.hisp.dhis.analytics.common.processing.MetadataParamsHandler;
+import org.hisp.dhis.analytics.common.query.Field;
+import org.hisp.dhis.analytics.trackedentity.query.context.sql.SqlQueryCreator;
+import org.hisp.dhis.analytics.trackedentity.query.context.sql.SqlQueryCreatorService;
+import org.hisp.dhis.common.ExecutionPlan;
+import org.hisp.dhis.common.Grid;
+import org.hisp.dhis.common.GridHeader;
+import org.hisp.dhis.common.ValueType;
+import org.hisp.dhis.system.grid.ListGrid;
+import org.hisp.dhis.user.CurrentUserUtil;
+import org.hisp.dhis.user.User;
+import org.hisp.dhis.user.UserService;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Service;
+
+/** Executes tracked-entity aggregate (grouped) queries and assembles the grouped grid. */
+@Service
+@RequiredArgsConstructor
+public class TrackedEntityAggregateService {
+
+  private static final GridHeader VALUE_HEADER =
+      new GridHeader("value", "Value", ValueType.NUMBER, false, false);
+
+  private final QueryExecutor<SqlQuery, SqlQueryResult> queryExecutor;
+
+  private final SqlQueryCreatorService sqlQueryCreatorService;
+
+  private final ExecutionPlanStore executionPlanStore;
+
+  private final CommonParamsSecurityManager securityManager;
+
+  private final MetadataParamsHandler metadataParamsHandler;
+
+  private final UserService userService;
+
+  public Grid getGrid(
+      @Nonnull ContextParams<TrackedEntityRequestParams, TrackedEntityQueryParams> contextParams) {
+    CommonParsedParams commonParams = contextParams.getCommonParsed();
+    TrackedEntityQueryParams teParams = contextParams.getTypedParsed();
+
+    securityManager.decideAccess(commonParams, singleton(teParams.getTrackedEntityType()));
+    securityManager.applyOrganisationUnitConstraint(commonParams);
+    securityManager.applyDimensionConstraints(commonParams);
+
+    SqlQueryCreator queryCreator = sqlQueryCreatorService.getSqlQueryCreator(contextParams);
+
+    Optional<SqlQueryResult> result =
+        withExceptionHandling(() -> queryExecutor.find(queryCreator.createForSelect()));
+
+    long rowsCount = 0;
+    AnalyticsPagingParams paging = commonParams.getPagingParams();
+    if (paging.showTotalPages()) {
+      rowsCount =
+          withExceptionHandling(() -> queryExecutor.count(queryCreator.createForCount()))
+              .orElse(0L);
+    }
+
+    List<Field> selectFields = queryCreator.getRenderableSqlQuery().getSelectFields();
+    Grid grid = new ListGrid();
+    getAggregateGridHeaders(contextParams, selectFields).forEach(grid::addHeader);
+    grid.addHeader(VALUE_HEADER);
+
+    result.ifPresent(r -> addGroupedRows(grid, r.result()));
+
+    User currentUser = userService.getUserByUsername(CurrentUserUtil.getCurrentUsername());
+    metadataParamsHandler.handle(grid, contextParams, currentUser, rowsCount);
+    return grid;
+  }
+
+  private void addGroupedRows(Grid grid, SqlRowSet rs) {
+    List<String> columns = grid.getHeaders().stream().map(GridHeader::getName).collect(toList());
+    while (rs.next()) {
+      grid.addRow();
+      columns.forEach(col -> grid.addValue(rs.getObject(col)));
+    }
+  }
+
+  public Grid getGridExplain(
+      @Nonnull ContextParams<TrackedEntityRequestParams, TrackedEntityQueryParams> contextParams) {
+    Grid grid = new ListGrid();
+    String explainId = randomUUID().toString();
+    SqlQueryCreator queryCreator = sqlQueryCreatorService.getSqlQueryCreator(contextParams);
+
+    withExceptionHandling(
+        () -> executionPlanStore.addExecutionPlan(explainId, queryCreator.createForSelect()));
+    if (contextParams.getCommonParsed().getPagingParams().showTotalPages()) {
+      withExceptionHandling(
+          () -> executionPlanStore.addExecutionPlan(explainId, queryCreator.createForCount()));
+    }
+    List<ExecutionPlan> plans = executionPlanStore.getExecutionPlans(explainId);
+    grid.addPerformanceMetrics(plans);
+    return grid;
+  }
+}
---
+++ dhis-2/dhis-services/dhis-service-analytics/src/test/java/org/hisp/dhis/analytics/trackedentity/TrackedEntityAggregateServiceTest.java
@@ -0,0 +1,285 @@
+/*
+ * Copyright (c) 2004-2026, University of Oslo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors 
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.hisp.dhis.analytics.trackedentity;
+
+import static java.util.stream.Collectors.toList;
+import static org.hisp.dhis.common.IdScheme.UID;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import org.hisp.dhis.analytics.analyze.ExecutionPlanStore;
+import org.hisp.dhis.analytics.common.CommonRequestParams;
+import org.hisp.dhis.analytics.common.ContextParams;
+import org.hisp.dhis.analytics.common.QueryExecutor;
+import org.hisp.dhis.analytics.common.SqlQuery;
+import org.hisp.dhis.analytics.common.SqlQueryResult;
+import org.hisp.dhis.analytics.common.params.AnalyticsPagingParams;
+import org.hisp.dhis.analytics.common.params.CommonParsedParams;
+import org.hisp.dhis.analytics.common.params.dimension.DimensionIdentifier;
+import org.hisp.dhis.analytics.common.params.dimension.DimensionParam;
+import org.hisp.dhis.analytics.common.params.dimension.DimensionParamType;
+import org.hisp.dhis.analytics.common.params.dimension.ElementWithOffset;
+import org.hisp.dhis.analytics.common.processing.MetadataParamsHandler;
+import org.hisp.dhis.analytics.common.query.Field;
+import org.hisp.dhis.analytics.trackedentity.query.context.QueryContextConstants;
+import org.hisp.dhis.analytics.trackedentity.query.context.sql.RenderableSqlQuery;
+import org.hisp.dhis.analytics.trackedentity.query.context.sql.SqlQueryCreator;
+import org.hisp.dhis.analytics.trackedentity.query.context.sql.SqlQueryCreatorService;
+import org.hisp.dhis.common.BaseDimensionalObject;
+import org.hisp.dhis.common.DimensionType;
+import org.hisp.dhis.common.Grid;
+import org.hisp.dhis.common.GridHeader;
+import org.hisp.dhis.organisationunit.OrganisationUnit;
+import org.hisp.dhis.user.CurrentUserUtil;
+import org.hisp.dhis.user.UserDetails;
+import org.hisp.dhis.user.UserService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+
+@ExtendWith(MockitoExtension.class)
+class TrackedEntityAggregateServiceTest {
+  @Mock private QueryExecutor<SqlQuery, SqlQueryResult> queryExecutor;
+  @Mock private SqlQueryCreatorService sqlQueryCreatorService;
+  @Mock private ExecutionPlanStore executionPlanStore;
+  @Mock private CommonParamsSecurityManager securityManager;
+  @Mock private MetadataParamsHandler metadataParamsHandler;
+  @Mock private UserService userService;
+  @Mock private SqlQueryCreator queryCreator;
+
+  @InjectMocks private TrackedEntityAggregateService service;
+
+  @BeforeEach
+  void setUp() {
+    CurrentUserUtil.injectUserInSecurityContext(UserDetails.empty().username("tester").build());
+  }
+
+  @AfterEach
+  void tearDown() {
+    CurrentUserUtil.clearSecurityContext();
+  }
+
+  @Test
+  void getGridBuildsOuAndValueHeadersAndMapsGroupedRows() {
+    ContextParams<TrackedEntityRequestParams, TrackedEntityQueryParams> ctx =
+        aggregateContextParamsWithOuDimension();
+    RenderableSqlQuery renderable =
+        RenderableSqlQuery.builder().selectFields(List.of(ouField(), valueField())).build();
+
+    SqlQuery selectQuery = mock(SqlQuery.class);
+    SqlRowSet rowSet =
+        fakeRowSet(
+            new String[] {"ou", "value"},
+            List.of(new Object[] {"OU_UID_1", 42}, new Object[] {"OU_UID_2", 7}));
+
+    when(sqlQueryCreatorService.getSqlQueryCreator(ctx)).thenReturn(queryCreator);
+    when(queryCreator.getRenderableSqlQuery()).thenReturn(renderable);
+    when(queryCreator.createForSelect()).thenReturn(selectQuery);
+    when(queryExecutor.find(any())).thenReturn(new SqlQueryResult(rowSet));
+
+    Grid grid = service.getGrid(ctx);
+
+    assertEquals(
+        List.of("ou", "value"),
+        grid.getHeaders().stream().map(GridHeader::getName).collect(toList()));
+    assertEquals(2, grid.getHeight());
+    assertEquals("OU_UID_1", grid.getRow(0).get(0));
+    assertEquals(42, grid.getRow(0).get(1));
+    verify(securityManager).decideAccess(any(), any());
+    verify(metadataParamsHandler).handle(eq(grid), eq(ctx), any(), eq(0L));
+  }
+
+  @Test
+  void getGridAppendsValueHeaderLastForOuPlusAttribute() {
+    ContextParams<TrackedEntityRequestParams, TrackedEntityQueryParams> ctx =
+        aggregateContextParamsWithOuAndAttribute("w75KJ2mc4zz");
+    RenderableSqlQuery renderable =
+        RenderableSqlQuery.builder()
+            .selectFields(
+                List.of(ouField(), attributeField("w75KJ2mc4zz"), valueField()))
+            .build();
+    SqlRowSet rowSet =
+        fakeRowSet(
+            new String[] {"ou", "w75KJ2mc4zz", "value"},
+            List.<Object[]>of(new Object[] {"OU1", "M", 5}));
+
+    when(sqlQueryCreatorService.getSqlQueryCreator(ctx)).thenReturn(queryCreator);
+    when(queryCreator.getRenderableSqlQuery()).thenReturn(renderable);
+    when(queryCreator.createForSelect()).thenReturn(mock(SqlQuery.class));
+    when(queryExecutor.find(any())).thenReturn(new SqlQueryResult(rowSet));
+
+    Grid grid = service.getGrid(ctx);
+
+    List<String> names = grid.getHeaders().stream().map(GridHeader::getName).collect(toList());
+    assertEquals("value", names.get(names.size() - 1)); // value always last
+    assertEquals(List.of("ou", "w75KJ2mc4zz", "value"), names);
+  }
+
+  @Test
+  void getGridCountsGroupsWhenShowTotalPages() {
+    ContextParams<TrackedEntityRequestParams, TrackedEntityQueryParams> ctx =
+        aggregateContextParamsWithOuDimensionShowTotalPages();
+    RenderableSqlQuery renderable =
+        RenderableSqlQuery.builder().selectFields(List.of(ouField(), valueField())).build();
+    SqlRowSet rowSet =
+        fakeRowSet(new String[] {"ou", "value"}, List.<Object[]>of(new Object[] {"OU1", 3}));
+
+    when(sqlQueryCreatorService.getSqlQueryCreator(ctx)).thenReturn(queryCreator);
+    when(queryCreator.getRenderableSqlQuery()).thenReturn(renderable);
+    when(queryCreator.createForSelect()).thenReturn(mock(SqlQuery.class));
+    when(queryCreator.createForCount()).thenReturn(mock(SqlQuery.class));
+    when(queryExecutor.find(any())).thenReturn(new SqlQueryResult(rowSet));
+    when(queryExecutor.count(any())).thenReturn(9L);
+
+    service.getGrid(ctx);
+
+    verify(queryExecutor).count(any());
+    verify(metadataParamsHandler).handle(any(), eq(ctx), any(), eq(9L));
+  }
+
+  private ContextParams<TrackedEntityRequestParams, TrackedEntityQueryParams>
+      aggregateContextParamsWithOuDimension() {
+    TrackedEntityQueryParams trackedEntityQueryParams =
+        TrackedEntityQueryParams.builder().aggregate(true).build();
+
+    return ContextParams.<TrackedEntityRequestParams, TrackedEntityQueryParams>builder()
+        .typedParsed(trackedEntityQueryParams)
+        .commonRaw(new CommonRequestParams())
+        .commonParsed(
+            CommonParsedParams.builder()
+                .dimensionIdentifiers(List.of(stubOuDimension("ou1")))
+                .build())
+        .build();
+  }
+
+  private ContextParams<TrackedEntityRequestParams, TrackedEntityQueryParams>
+      aggregateContextParamsWithOuAndAttribute(String attribute) {
+    TrackedEntityQueryParams trackedEntityQueryParams =
+        TrackedEntityQueryParams.builder().aggregate(true).build();
+
+    return ContextParams.<TrackedEntityRequestParams, TrackedEntityQueryParams>builder()
+        .typedParsed(trackedEntityQueryParams)
+        .commonRaw(new CommonRequestParams())
+        .commonParsed(
+            CommonParsedParams.builder()
+                .dimensionIdentifiers(
+                    List.of(stubOuDimension("ou1"), stubAttributeDimension(attribute)))
+                .build())
+        .build();
+  }
+
+  private ContextParams<TrackedEntityRequestParams, TrackedEntityQueryParams>
+      aggregateContextParamsWithOuDimensionShowTotalPages() {
+    TrackedEntityQueryParams trackedEntityQueryParams =
+        TrackedEntityQueryParams.builder().aggregate(true).build();
+
+    return ContextParams.<TrackedEntityRequestParams, TrackedEntityQueryParams>builder()
+        .typedParsed(trackedEntityQueryParams)
+        .commonRaw(new CommonRequestParams())
+        .commonParsed(
+            CommonParsedParams.builder()
+                .dimensionIdentifiers(List.of(stubOuDimension("ou1")))
+                .pagingParams(AnalyticsPagingParams.builder().totalPages(true).build())
+                .build())
+        .build();
+  }
+
+  private Field ouField() {
+    return Field.of(QueryContextConstants.TRACKED_ENTITY_ALIAS, () -> "ou", "ou");
+  }
+
+  private Field attributeField(String attribute) {
+    return Field.of(QueryContextConstants.TRACKED_ENTITY_ALIAS, () -> attribute, attribute);
+  }
+
+  private Field valueField() {
+    return Field.of(QueryContextConstants.TRACKED_ENTITY_ALIAS, () -> "value", "value");
+  }
+
+  private DimensionIdentifier<DimensionParam> stubOuDimension(String ou) {
+    OrganisationUnit orgUnit = new OrganisationUnit();
+    orgUnit.setUid(ou);
+    DimensionParam dimensionParam =
+        DimensionParam.ofObject(
+            new BaseDimensionalObject("ou", DimensionType.ORGANISATION_UNIT, List.of(orgUnit)),
+            DimensionParamType.DIMENSIONS,
+            UID,
+            List.of(ou));
+    return DimensionIdentifier.of(
+            ElementWithOffset.emptyElementWithOffset(),
+            ElementWithOffset.emptyElementWithOffset(),
+            dimensionParam)
+        .withDefaultGroupId();
+  }
+
+  private DimensionIdentifier<DimensionParam> stubAttributeDimension(String attribute) {
+    DimensionParam dimensionParam =
+        DimensionParam.ofObject(
+            new BaseDimensionalObject(attribute, DimensionType.PROGRAM_ATTRIBUTE, List.of()),
+            DimensionParamType.DIMENSIONS,
+            UID,
+            List.of());
+    return DimensionIdentifier.of(
+            ElementWithOffset.emptyElementWithOffset(),
+            ElementWithOffset.emptyElementWithOffset(),
+            dimensionParam)
+        .withDefaultGroupId();
+  }
+
+  private SqlRowSet fakeRowSet(String[] columns, List<Object[]> rows) {
+    SqlRowSet rowSet = mock(SqlRowSet.class);
+    int[] currentRow = {-1};
+    when(rowSet.next())
+        .thenAnswer(
+            invocation -> {
+              currentRow[0]++;
+              return currentRow[0] < rows.size();
+            });
+    when(rowSet.getObject(anyString()))
+        .thenAnswer(
+            invocation -> {
+              String column = invocation.getArgument(0);
+              int columnIndex = List.of(columns).indexOf(column);
+              return rows.get(currentRow[0])[columnIndex];
+            });
+    return rowSet;
+  }
+}
