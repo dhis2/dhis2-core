b3796519504272bb229f5bebfed2e36cc4ad1660
Jan Bernitt <jaanbernitt@gmail.com>
2026-07-20 09:47:39 +0200

    fix: replace lombok with getter/setters

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/schema/Property.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/schema/Property.java
@@ -86,7 +86,7 @@ public class Property implements Ordered, Klass {
    */
   private boolean persisted;
 
-  @Getter @Setter private boolean isTransient;
+  private boolean isTransient;
 
   /** Name of collection wrapper. */
   private String collectionName;
@@ -329,6 +329,16 @@ public class Property implements Ordered, Klass {
 
   public void setPersisted(boolean persisted) {
     this.persisted = persisted;
+  }
+
+  @JsonProperty
+  @JacksonXmlProperty(namespace = DxfNamespaces.DXF_2_0)
+  public boolean isTransient() {
+    return isTransient;
+  }
+
+  public void setTransient(boolean isTransient) {
+    this.isTransient = isTransient;
   }
 
   @JsonProperty
