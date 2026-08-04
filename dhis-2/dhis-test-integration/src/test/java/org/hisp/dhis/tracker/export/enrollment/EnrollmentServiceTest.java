bdfbc70c6a0951733740676628d810d9bd543606
Enrico Colasante <enrico@dhis2.org>
2026-08-04 15:57:56 +0200

    feat: Apply sharing at attribute level [DHIS2-18467] (#24658)
    * feat: Apply sharing at attribute level [DHIS2-18467]
    
    * Refactor get readable attributes method

--- dhis-2/dhis-test-integration/src/test/java/org/hisp/dhis/tracker/export/enrollment/EnrollmentServiceTest.java
+++ dhis-2/dhis-test-integration/src/test/java/org/hisp/dhis/tracker/export/enrollment/EnrollmentServiceTest.java
@@ -395,6 +395,24 @@ class EnrollmentServiceTest extends PostgresIntegrationTestBase {
   }
 
   @Test
+  void shouldNotReturnAttributeValueWhenUserCannotReadAttribute() throws NotFoundException {
+    // Remove metadata read access to the attribute for the current user. The enrollment is still
+    // accessible, but its attribute value must not be returned.
+    trackedEntityAttributeA.getSharing().setOwner(admin);
+    trackedEntityAttributeA.getSharing().setPublicAccess(AccessStringHelper.DEFAULT);
+    manager.updateNoAcl(trackedEntityAttributeA);
+    manager.flush();
+    manager.clear();
+
+    EnrollmentFields fields = EnrollmentFields.builder().includeAttributes().build();
+
+    Enrollment enrollment = enrollmentService.getEnrollment(UID.of(enrollmentA), fields);
+
+    assertNotNull(enrollment);
+    assertTrue(attributeUids(enrollment).isEmpty());
+  }
+
+  @Test
   void shouldFailGettingEnrollmentWhenUserHasNoAccessToProgramsTrackedEntityType() {
     trackedEntityTypeA.getSharing().setOwner(admin);
     trackedEntityTypeA.getSharing().setPublicAccess(AccessStringHelper.DEFAULT);
