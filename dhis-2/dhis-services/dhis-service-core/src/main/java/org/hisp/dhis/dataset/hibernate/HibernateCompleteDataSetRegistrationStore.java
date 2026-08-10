00e2306f00c5028659965172e1333ccbafbb45b2
teleivo <teleivo@users.noreply.github.com>
2026-08-10 10:46:51 +0200

    refactor: use nativeSynchronizedQuery in complete data set registration store

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/dataset/hibernate/HibernateCompleteDataSetRegistrationStore.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/dataset/hibernate/HibernateCompleteDataSetRegistrationStore.java
@@ -135,9 +135,7 @@ public class HibernateCompleteDataSetRegistrationStore
           AND c.sourceid = (SELECT organisationunitid FROM organisationunit ou WHERE ou.uid = :ou)
           AND c.attributeoptioncomboid = (SELECT categoryoptioncomboid FROM categoryoptioncombo aoc WHERE aoc.uid = :aoc)
         """;
-    getSession()
-        .createNativeQuery(sql)
-        .addSynchronizedQuerySpace("completedatasetregistration")
+    nativeSynchronizedQuery(sql)
         .setParameter("ds", dataSet.getValue())
         .setParameter("pe", period.getIsoDate())
         .setParameter("ou", orgUnit.getValue())
