6645798f84b713bbd064c337c84c406037128041
Maikel Arabori <51713408+maikelarabori@users.noreply.github.com>
2026-09-03 08:50:16 -0300

    fix: Collection validation in scheme [DHIS2-22026] (#25018)
    * fix: Collection validation in scheme [DHIS2-22026]
    
    * test: Debug behavior
    
    * fix: Skip limit for collections only
    
    * chore: clean up

--- dhis-2/dhis-services/dhis-service-schema/src/main/java/org/hisp/dhis/schema/introspection/HibernatePropertyIntrospector.java
+++ dhis-2/dhis-services/dhis-service-schema/src/main/java/org/hisp/dhis/schema/introspection/HibernatePropertyIntrospector.java
@@ -31,6 +31,7 @@ package org.hisp.dhis.schema.introspection;
 
 import jakarta.persistence.EntityManagerFactory;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -188,7 +189,17 @@ public class HibernatePropertyIntrospector implements PropertyIntrospector {
       property.setUnique(column.isUnique());
       property.setRequired(!column.isNullable());
       property.setMin(0d);
-      property.setMax((double) column.getLength());
+
+      boolean isCollectionOrCustomType =
+          type instanceof CustomType
+              || (property.getGetterMethod() != null
+                  && Collection.class.isAssignableFrom(property.getGetterMethod().getReturnType()));
+
+      // Skip max length for collections.
+      if (!isCollectionOrCustomType) {
+        property.setMax((double) column.getLength());
+      }
+
       property.setLength(column.getLength());
 
       if (type instanceof TextType) {
