73d4f003313c832cc23941268b285a39008a15e2
Jan Bernitt <jaanbernitt@gmail.com>
2026-09-17 15:48:57 +0200

    perf: data entry key validation only check distinct keys and cache COCs for DS+DE

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/datavalue/hibernate/HibernateDataEntryStore.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/datavalue/hibernate/HibernateDataEntryStore.java
@@ -30,6 +30,7 @@
 package org.hisp.dhis.datavalue.hibernate;
 
 import static java.lang.Math.min;
+import static java.lang.System.currentTimeMillis;
 import static java.util.function.Predicate.not;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toMap;
@@ -42,6 +43,7 @@ import static org.hisp.dhis.user.CurrentUserUtil.getCurrentUsername;
 
 import jakarta.persistence.EntityManager;
 import java.sql.PreparedStatement;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -56,6 +58,8 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 import javax.annotation.CheckForNull;
 import javax.annotation.Nonnull;
+import org.cache2k.Cache;
+import org.cache2k.Cache2kBuilder;
 import org.hibernate.Session;
 import org.hibernate.query.NativeQuery;
 import org.hisp.dhis.common.DateRange;
@@ -417,39 +421,46 @@ public class HibernateDataEntryStore extends HibernateGenericStore<DataValue>
     return listAsStrings(sql, q -> q.setParameter("ou", ou).setParameter("ds", ds));
   }
 
-  @Override
-  public List<String> getCocNotInDataSet(UID dataSet, UID dataElement, Stream<UID> optionCombos) {
+  private record DsDeKey(String ds, String de) {}
+
+  @SuppressWarnings({"unchecked", "rawtypes"})
+  private static final Cache<DsDeKey, Set<String>> VALID_COCS_BY_DS_DE =
+      (Cache)
+          Cache2kBuilder.forUnknownTypes()
+              .name("validCocsByDsDe")
+              .entryCapacity(1000)
+              .expireAfterWrite(Duration.ofHours(1))
+              .build();
+
+  private Set<String> loadCocInDataSet(String ds, String de) {
     String sql =
         """
-      WITH coc_list(uid) AS ( SELECT DISTINCT UNNEST(:coc) AS uid ),
-      dsde_coc AS (
-          SELECT coc_cc.categoryoptioncomboid
-          FROM categorycombos_optioncombos coc_cc
-          WHERE coc_cc.categorycomboid = (
-              SELECT COALESCE(dse.categorycomboid, de.categorycomboid)
-              FROM datasetelement dse
-              JOIN dataelement de ON de.dataelementid = dse.dataelementid
-              JOIN dataset ds ON ds.datasetid = dse.datasetid
-              WHERE ds.uid = :ds
-                AND de.uid = :de
-          )
-      )
-      SELECT coc_list.uid
-      FROM coc_list
-      LEFT JOIN categoryoptioncombo coc ON coc_list.uid = coc.uid
-      LEFT JOIN dsde_coc excluded ON coc.categoryoptioncomboid = excluded.categoryoptioncomboid
-      WHERE excluded.categoryoptioncomboid IS NULL""";
-    String ds = dataSet.getValue();
-    String de = dataElement.getValue();
+        SELECT coc_cc.categoryoptioncomboid
+        FROM categorycombos_optioncombos coc_cc
+        WHERE coc_cc.categorycomboid = (
+            SELECT COALESCE(dse.categorycomboid, de.categorycomboid)
+            FROM datasetelement dse
+            JOIN dataelement de ON de.dataelementid = dse.dataelementid
+            JOIN dataset ds ON ds.datasetid = dse.datasetid
+            WHERE ds.uid = :ds
+              AND de.uid = :de
+        )""";
+    return Set.copyOf(listAsStrings(sql, q -> q.setParameter("ds", ds).setParameter("de", de)));
+  }
+
+  @Override
+  public List<String> getCocNotInDataSet(UID dataSet, UID dataElement, Stream<UID> optionCombos) {
     UID defaultCoc = getDefaultCategoryOptionComboUid();
-    String[] coc =
-        optionCombos
-            .map(id -> id == null ? defaultCoc : id)
-            .map(UID::getValue)
-            .distinct()
-            .toArray(String[]::new);
-    return listAsStrings(
-        sql, q -> q.setParameter("coc", coc).setParameter("ds", ds).setParameter("de", de));
+    DsDeKey key = new DsDeKey(dataSet.getValue(), dataElement.getValue());
+
+    Set<String> validUids =
+        VALID_COCS_BY_DS_DE.computeIfAbsent(key, k -> loadCocInDataSet(k.ds(), k.de()));
+    return optionCombos
+        .map(id -> id == null ? defaultCoc : id)
+        .map(UID::getValue)
+        .distinct()
+        .filter(uid -> !validUids.contains(uid))
+        .toList();
   }
 
   @Override
@@ -960,14 +971,26 @@ public class HibernateDataEntryStore extends HibernateGenericStore<DataValue>
     return getIdMap("categoryoptioncombo", ids);
   }
 
+  private UID defaultCocUid;
+  private long defaultCocId;
+  private long defaultCocValidUntil;
+
   private long getDefaultCategoryOptionComboId() {
+    long now = currentTimeMillis();
+    if (now < defaultCocValidUntil && defaultCocId != 0L) return defaultCocId;
     String sql = "select categoryoptioncomboid from categoryoptioncombo where name = 'default'";
-    return ((Number) getSession().createNativeQuery(sql).getSingleResult()).longValue();
+    defaultCocId = ((Number) getSession().createNativeQuery(sql).getSingleResult()).longValue();
+    defaultCocValidUntil = now + Duration.ofHours(1).toMillis();
+    return defaultCocId;
   }
 
   private UID getDefaultCategoryOptionComboUid() {
+    long now = currentTimeMillis();
+    if (now < defaultCocValidUntil && defaultCocUid != null) return defaultCocUid;
     String sql = "select uid from categoryoptioncombo where name = 'default'";
-    return UID.of((String) getSession().createNativeQuery(sql).getSingleResult());
+    defaultCocUid = UID.of((String) getSession().createNativeQuery(sql).getSingleResult());
+    defaultCocValidUntil = now + Duration.ofHours(1).toMillis();
+    return defaultCocUid;
   }
 
   private Map<String, Long> getOptionComboIdMap(List<DataEntryValue> values) {
