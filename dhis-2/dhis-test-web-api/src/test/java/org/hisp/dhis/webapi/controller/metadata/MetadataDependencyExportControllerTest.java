a22364884fb388bddf22177ee06a5ffa9b91d972
David Mackessy <dmackessy@dhis2.org>
2026-09-18 14:52:13 +0100

    remove unused import in test

--- dhis-2/dhis-test-web-api/src/test/java/org/hisp/dhis/webapi/controller/metadata/MetadataDependencyExportControllerTest.java
+++ dhis-2/dhis-test-web-api/src/test/java/org/hisp/dhis/webapi/controller/metadata/MetadataDependencyExportControllerTest.java
@@ -38,7 +38,6 @@ import java.util.List;
 import org.hisp.dhis.attribute.Attribute;
 import org.hisp.dhis.common.ValueType;
 import org.hisp.dhis.dataset.DataSet;
-import org.hisp.dhis.http.HttpClientAdapter.HttpResponse;
 import org.hisp.dhis.http.HttpStatus;
 import org.hisp.dhis.jsontree.JsonArray;
 import org.hisp.dhis.jsontree.JsonMixed;
