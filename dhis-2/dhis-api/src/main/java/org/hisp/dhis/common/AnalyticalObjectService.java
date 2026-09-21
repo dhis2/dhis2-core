9109924e0a64e3ed451588729f986989cefdcb7e
Viet Nguyen <viet@dhis2.org>
2026-09-21 08:29:40 -0500

    fix: clean up program indicator dimensions on PI deletion (v41) (#25313)

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/common/AnalyticalObjectService.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/common/AnalyticalObjectService.java
@@ -61,6 +61,8 @@ public interface AnalyticalObjectService<T extends AnalyticalObject> {
 
   List<T> getAnalyticalObjects(ProgramIndicator programIndicator);
 
+  List<T> getAnalyticalObjectsByDataDimension(ProgramIndicator programIndicator);
+
   List<T> getAnalyticalObjects(Period period);
 
   List<T> getAnalyticalObjects(OrganisationUnit organisationUnit);
