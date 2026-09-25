16851d2135881c105bdc04f8360a81e03b22a3dd
Enrico Colasante <enrico@dhis2.org>
2026-09-25 15:06:11 +0200

    fix: Filter out blank recipients for outbound messages [DHIS2-21836] (#25213)
    * fix: Filter out blank recipients for outbound messages [DHIS2-21836]
    
    * Review comments
    
    * Code review fixes
    
    * Code review fixes

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/sms/SmsMessageBatchCreatorService.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/sms/SmsMessageBatchCreatorService.java
@@ -31,6 +31,7 @@ package org.hisp.dhis.sms;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
 import org.hisp.dhis.common.DeliveryChannel;
 import org.hisp.dhis.outboundmessage.OutboundMessage;
 import org.hisp.dhis.outboundmessage.OutboundMessageBatch;
@@ -41,6 +42,7 @@ import org.springframework.stereotype.Service;
 /**
  * @author Zubair <rajazubair.asghar@gmail.com>
  */
+@Slf4j
 @Service("org.hisp.dhis.sms.SmsMessageBatchCreatorService")
 public class SmsMessageBatchCreatorService implements MessageBatchCreatorService {
   @Override
@@ -48,10 +50,23 @@ public class SmsMessageBatchCreatorService implements MessageBatchCreatorService
     List<OutboundMessage> messages =
         programMessages.parallelStream()
             .filter(pm -> pm.getDeliveryChannels().contains(DeliveryChannel.SMS))
+            .filter(this::hasPhoneNumber)
             .map(this::createSmsMessage)
             .collect(Collectors.toList());
 
     return new OutboundMessageBatch(messages, DeliveryChannel.SMS);
+  }
+
+  private boolean hasPhoneNumber(ProgramMessage programMessage) {
+    boolean hasPhoneNumber = !programMessage.getRecipients().getPhoneNumbers().isEmpty();
+
+    if (!hasPhoneNumber) {
+      log.warn(
+          "Skipping SMS for program message {}: no phone number to deliver to",
+          programMessage.getUid());
+    }
+
+    return hasPhoneNumber;
   }
 
   private OutboundMessage createSmsMessage(ProgramMessage programMessage) {
